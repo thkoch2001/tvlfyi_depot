d4ad5a153bbb8598c360e72c80622c07626a136b
William Carroll <wpcarro@gmail.com>
2016-08-02 15:33:25 -0400

    Adds functions for managing directories

--- .misc_functions.sh
+++ .misc_functions.sh
@@ -6,8 +6,23 @@ function wdownload {
   wget -O /tmp/"$FILENAME" $URL >/dev/null && open /tmp && echo "Downloaded to: /tmp/$FILENAME" || echo "Error ..."
 }
 
+
 # spell checker
 function wspcheck {
   if [ $# -ge 1 -a -f "$1" ] && input="$1" || input="-"
   cat "$input" | tr '[:upper:]' '[:lower:]' | tr -cd '[:alpha:]_ \n' | tr -s ' ' '\n' | sort | comm -23 - ~/english_words.txt
 }
+
+
+# fuzzily search through dirs stack
+function wfd {
+  dir=$(dirname $(fzf)) && pushd "$dir" >/dev/null
+}
+
+
+# pushd into a directory on your dirs stack
+function wpushd {
+  dir=$(dirs | tr ' ' '\n' | fzf | dirname) && pushd "$dir"
+}
+
+
