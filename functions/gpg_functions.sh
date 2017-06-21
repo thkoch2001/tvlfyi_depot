71bb47c7a32f412a6d207fd6e51d5a895c867782
William Carroll <wpcarro@gmail.com>
2017-06-21 14:18:44 -0400

    Adds helpers for single file symmetric encryption / decryption

--- functions/gpg_functions.sh
+++ functions/gpg_functions.sh
@@ -2,7 +2,9 @@
 
 function gpg-encrypt-dir() {
     dirname=$1
+    echo "Encrypting..."
     tar -cz "$dirname" | gpg --symmetric --output "$dirname.tar.gz.gpg"
+    echo "Done."
 }
 
 
@@ -14,7 +16,27 @@ function gpg-decrypt-dir() {
         echo "Output directory, $outdir, already exists and will be overwritten by this command. Aborting..."
         exit 1
     else
+        echo "Decrypting..."
         gpg --decrypt $dirname | tar -xv
+        echo "Done."
     fi
 
+}
+
+
+# WIP
+function gpg-encrypt-file() {
+    filename=$1
+    echo "Encrypting..."
+    gpg --symmetric $filename
+    echo "Done."
+}
+
+
+# WIP
+function gpg-decrypt-file() {
+    filename=$1
+    echo "Decrypting..."
+    gpg --decrypt $filename >"${filename%.gpg}"
+    echo "Done."
 }
