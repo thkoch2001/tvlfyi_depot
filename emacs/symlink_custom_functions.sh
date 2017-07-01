abb61dade20e43a1a9a13d878b66863393670ccf
William Carroll <wpcarro@gmail.com>
2017-07-01 16:46:46 -0400

    Updates symlinking script for custom emacs funcs

--- emacs/symlink_custom_functions.sh
+++ emacs/symlink_custom_functions.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env zsh
 
-ln -s ~/pc_settings/emacs/*.el ~/.emacs.d/
+SCRIPT_DIR="$HOME/pc_settings/emacs"
+EMACS_FUNC_DIR="$HOME/.emacs.d"
+
+for source in $(find $SCRIPT_DIR -type f -name '*.el'); do
+  filename=$(basename $source)
+  target="${EMACS_FUNC_DIR}/${filename}"
+
+  if [ ! -L $target ]; then
+    echo -n "Creating symlink for ${filename} ... " && \
+    ln -s $source $EMACS_FUNC_DIR && \
+    echo "Done."
+  else
+    echo "${filename} is already properly symlinked."
+  fi
+
+done
