c3c256f3c1aeb74da41a9fb8dc179bdc494e8ef5
William Carroll <wpcarro@gmail.com>
2017-09-15 11:55:45 -0400

    Lints aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -4,17 +4,17 @@ alias dir='find . -maxdepth 1 -type d -regex "\.\/[^.].+"'
 
 # aliases with dependencies
 command -v nvim >/dev/null && alias vim=nvim || \
-        echo "Missing dependency (nvim). Failed to alias vim -> nvim"
+        echo 'Missing dependency (nvim). Failed to alias vim -> nvim'
 command -v find >/dev/null && alias find='find -E' || \
-        echo "Missing dependency (find -E). Failed to alias find -> find -E"
+        echo 'Missing dependency (find -E). Failed to alias find -> find -E'
 command -v egrep >/dev/null && alias grep=egrep || \
-        echo "Missing dependency (egrep). Failed to alias grep -> egrep"
+        echo 'Missing dependency (egrep). Failed to alias grep -> egrep'
 command -v pygmentize >/dev/null && alias ccat='pygmentize -g' >/dev/null || \
-        echo "Missing dependency (pygmentize -g). Failed to alias ccat -> pygmentize -g"
+        echo 'Missing dependency (pygmentize -g). Failed to alias ccat -> pygmentize -g'
 command -v hub >/dev/null && alias git=hub || \
-        echo "Missing dependency (hub). Failed to alias git -> hub"
+        echo 'Missing dependency (hub). Failed to alias git -> hub'
 command -v tmux >/dev/null && alias tls='tmux list-sessions' || \
-        echo "Missing dependency (tmux). Failed to alias tls -> tmux list-sessions"
+        echo 'Missing dependency (tmux). Failed to alias tls -> tmux list-sessions'
 
 
 # exa-specific aliases
