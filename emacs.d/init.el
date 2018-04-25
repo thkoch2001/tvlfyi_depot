151acc19a79e78493e027e2a49d59919dfea5bde
William Carroll <wpcarro@gmail.com>
2018-04-25 13:55:55 -0400

    Shore up init.el
    init.el was previously a symlink. This replaces that with the actual
    code since the entire emacs.d dir is being symlinked.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,1 +1,29 @@
-/Users/wpcarro/dotfiles/init.el
+(require 'wpc-package "~/.emacs.d/wpc/packages/wpc-package.el")
+
+;; third-party libraries
+(use-package dash)
+(use-package s)
+(use-package f)
+
+;; my libraries
+(require 'variables)
+(require 'functions)
+(require 'string-functions)
+(require 'macros)
+(require 'casing)
+
+(require 'wpc-ui)
+(require 'wpc-keybindings)
+(require 'wpc-misc)
+(require 'wpc-dired)
+(require 'wpc-terminal)
+(require 'wpc-org)
+(require 'wpc-slack)
+(require 'wpc-company)
+(require 'wpc-flycheck)
+(require 'wpc-git)
+(require 'wpc-docker)
+(require 'wpc-lisp)
+(require 'wpc-haskell)
+(require 'wpc-clojure)
+(require 'wpc-javascript)
