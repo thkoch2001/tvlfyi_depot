9c6e1717d4ad3ef96c7b9a75a24dcaccc4c3e680
William Carroll <wpcarro@gmail.com>
2018-04-25 14:07:29 -0400

    Drop support for bins dir
    These just aren't being used anymore...

--- bins/bin/edit-file-in-emacs.sh
+++
@@ -1,7 +0,0 @@
-#/usr/bin/env zsh
-
-file=$(mktemp -t "$USER-XXXXXXXX.emacs-pager") || exit 127
-trap "rm -f $file" EXIT
-trap "exit 255" HUP INT QUIT TERM
-cat "$@" >"$file"
-emacsclient -e "(wc/edit-file-in-emacs \"$file\")"
--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env zsh
 
-source "${DOTFILES}/bins/setup"
 source "${DOTFILES}/configs/setup"
 ln -s "${DOTFILES}/emacs.d" "${HOME}/.emacs.d"
