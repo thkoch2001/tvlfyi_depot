88d75b97710bb2719008a279e08dd5cf89d0a9f5
William Carroll <wpcarro@gmail.com>
2018-07-17 15:29:07 -0400

    Remap KBDs for Paredit
    I wasn't using the previous KBDs, which were modelled after Vim's answer
    for Paredit KBDs.
    
    Some of the mappings bothered me, so I needed that made more sense to
    me.

--- emacs.d/wpc/packages/wpc-lisp.el
+++ emacs.d/wpc/packages/wpc-lisp.el
@@ -17,20 +17,28 @@
   'emacs-lisp-mode
   'ielm-mode)
 
+;; Here is some of the thinking behind some of the keybindings:
+;;
+;; slurp    s
+;; barf     S
+;; forward  )
+;; backward (
+;;
+;; Known concession: s and S eclipse Vim bindings. There is a precedent already
+;; for eclipsing the s binding for window splitting. Shift-s feel appropriate
+;; for barfing, since eclisping the b KBD feels less acceptable than eclisping
+;; the s KBD.
+
 ;; paredit LISP editing
 (use-package paredit
   :general
   (general-unbind paredit-mode-map "C-j" "M-q")
   (n paredit-mode-map
-     ">)" 'paredit-forward-slurp-sexp
-     "<(" 'paredit-backward-slurp-sexp
-     "<)" 'paredit-forward-barf-sexp
-     ">(" 'paredit-backward-barf-sexp
-     ">e" 'paredit-move-forward
-     "<e" 'paredit-move-backward
-     ">f" 'paredit-move-backward
-     "<f" 'paredit-move-backward
-     "go" 'paredit-raise-sexp)
+     "s)" 'paredit-forward-slurp-sexp
+     "s(" 'paredit-backward-slurp-sexp
+     "S)" 'paredit-forward-barf-sexp
+     "S(" 'paredit-backward-barf-sexp
+     "gr" 'paredit-raise-sexp)
   :ghook (wpc/lisp-mode-hooks #'enable-paredit-mode))
 
 (provide 'wpc-lisp)
