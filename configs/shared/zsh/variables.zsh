9da958d9201b2fef8908cca47cc4782c9b958ff7
William Carroll <wpcarro@gmail.com>
2019-03-04 17:52:55 +0000

    Support variables for common config file

--- configs/shared/zsh/variables.zsh
+++ configs/shared/zsh/variables.zsh
@@ -1,3 +1,13 @@
 EDITOR="emacs"
 BROWSER="chrome"
 NIXIFY="$HOME/programming/nixify"
+
+# commonly used config files
+v="$HOME/.config/nvim/init.vim"
+e="$HOME/.emacs.d/init.el"
+z="$HOME/.zshrc"
+a="$HOME/aliases.zsh"
+f="$HOME/functions.zsh"
+l="$HOME/variables.zsh" # v is taken by vim
+x="$HOME/.xsessionrc"
+i="$HOME/.config/i3/config"
