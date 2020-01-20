0c59df832714c09f9dbf881bf352507916999d2d
William Carroll <wpcarro@gmail.com>
2020-01-20 10:15:48 +0000

    Add tests for vector.el
    Adds a few tests for vector{set,set!} functions.

--- configs/shared/.emacs.d/wpc/vector.el
+++ configs/shared/.emacs.d/wpc/vector.el
@@ -21,6 +21,13 @@
 
 ;; TODO: Consider supporting an alias named tuple for vector.
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Library
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defconst vector/enable-tests? t
+  "When t, run the tests defined herein.")
+
 ;; TODO: Consider labelling variadic functions like `vector/concat*'
 ;; vs. `vector/concat'.
 (defun vector/concat (&rest args)
@@ -49,6 +56,21 @@ Returns a copy of `XS' with the updates."
   (let ((copy (vconcat [] xs)))
     (aset copy i v)
     copy))
+
+(defun vector/set! (i v xs)
+  "Set index `I' to value `V' in `XS'.
+This function mutates XS."
+  (aset xs i v))
+
+(when vector/enable-tests?
+  (let ((xs [1 2 3])
+        (ys [1 2 3]))
+    (prelude/assert (= 1 (vector/get 0 ys)))
+    (vector/set 0 4 ys)
+    (prelude/assert (= 1 (vector/get 0 ys)))
+    (prelude/assert (= 1 (vector/get 0 xs)))
+    (vector/set! 0 4 xs)
+    (prelude/assert (= 4 (vector/get 0 xs)))))
 
 ;; TODO: Decide between "remove" and "delete" as the appropriate verbs.
 ;; TODO: Implement this.
