2e3bb0435f2849e2a5d4da0ed8408c2993ca9052
William Carroll <wpcarro@gmail.com>
2020-01-23 21:56:24 +0000

    Package cl-arrows package
    Using Nix to package this library of Clojure-inspired threading macros.
    
    TODO: Send this patch to tazjin.

---
+++ third_party/lisp/cl-arrows.nix
@@ -0,0 +1,15 @@
+{ depot ? import <depot> {}, ... }:
+
+let
+  src = builtins.fetchGit {
+    url = "https://github.com/nightfly19/cl-arrows.git";
+    rev = "cbb46b69a7de40f1161c9caaf6cef93b3af9994f";
+  };
+in depot.nix.buildLisp.library {
+  name = "cl-arrows";
+  deps = [];
+  srcs = [
+    "${src}/packages.lisp"
+    "${src}/arrows.lisp"
+  ];
+}
