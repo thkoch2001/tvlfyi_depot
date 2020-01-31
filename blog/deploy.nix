184404d5421bd052519f757450d8dbd9466a6e14
William Carroll <wpcarro@gmail.com>
2020-01-31 16:30:56 +0000

    Add deploy.nix to blog
    Adding a deploy.nix to output docker images based on the <briefcase>.blog
    derivation. See the deploy/README.md docs for more information.

---
+++ blog/deploy.nix
@@ -0,0 +1,15 @@
+{
+  pkgs ? import <nixpkgs> {},
+  briefcase ? import <briefcase> {},
+  ...
+}:
+
+pkgs.dockerTools.buildLayeredImage {
+  name = "blog";
+  tag = "latest";
+  config.ExposedPorts = {
+    "4242" = {};
+  };
+  config.Cmd = [ "${briefcase.blog}/bin/server" ];
+  maxLayers = 120;
+}
