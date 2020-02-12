5ec5a6da8cbfe3c35558fd2c17ef779b5d0ccb54
William Carroll <wpcarro@gmail.com>
2020-02-12 15:53:21 +0000

    Complete InterviewCake reverse-string-in-place
    Wrote a function in TypeScript to reverse an array of characters mutatively.

--- deepmind/part_two/reverse-string-in-place.ts
+++ deepmind/part_two/reverse-string-in-place.ts
@@ -1,6 +1,13 @@
-function main(x: number) {
-  console.log(x + 1);
-  console.log("Hello, world.");
-}
+// Reverse array of characters, `xs`, mutatively.
+function reverse(xs: Array<string>) {
+  let i: number = 0;
+  let j: number = xs.length - 1;
 
-main(10);
+  while (i < j) {
+    let tmp = xs[i];
+    xs[i] = xs[j]
+    xs[j] = tmp
+    i += 1
+    j -= 1
+  }
+}
