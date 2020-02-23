6a076e8329a8befdaec817eda775b40c6ed34d56
William Carroll <wpcarro@gmail.com>
2020-02-23 20:08:19 +0000

    Expose depot and briefcase to rebuild script
    At the moment, I don't think nixos-rebuild is reading $NIX_PATH, which
    appropriately sets the paths for depot and briefcase. I'm going to explicitly
    expose these values in the rebuild script for now.

--- nixos/rebuild.nix
+++ nixos/rebuild.nix
@@ -5,5 +5,7 @@ pkgs.writeShellScriptBin "rebuild" ''
   sudo nixos-rebuild \
     -I nixos-config=/home/wpcarro/briefcase/nixos/configuration.nix \
     -I nixpkgs=/home/wpcarro/nixpkgs \
+    -I depot=/home/wpcarro/depot \
+    -I briefcase=/home/wpcarro/briefcase \
     switch
 ''
