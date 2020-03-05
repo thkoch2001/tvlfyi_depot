3b689f03de755f6a241f9c042270c0d7e579a657
William Carroll <wpcarro@gmail.com>
2020-03-05 14:06:14 +0000

    Support gitconfig with home-manager
    - Migrate configuration from .gitconfig to home-manager.
    - Delete .gitconfig.
    - Delete .gitignore because I'm not using OSX anymore.

--- configs/shared/.config/nixpkgs/home.nix
+++ configs/shared/.config/nixpkgs/home.nix
@@ -15,6 +15,27 @@
     path = builtins.toPath ~/home-manager;
   };
 
+  programs.git = {
+    enable = true;
+    userName = "William Carroll";
+    userEmail = "wpcarro@gmail.com";
+    aliases = {
+      today = "! git log --date=relative --since=00:00:00 --all --no-merges --oneline --author=\"$(git config --get user.email)\"";
+      yday = "! git log --since=yesterday.midnight --until=today.midnight --oneline --author=\"$(git config --get user.email)\"";
+      changed-files = "! git --no-pager diff --name-only $(current_branch) $(git merge-base $(current_branch) master)";
+      conflicts = "! git --no-pager diff --name-only --diff-filter=U";
+      unstage = "reset HEAD --";
+    };
+    extraConfig = {
+      push.default = "current";
+      rebase = {
+        autosquash = true;
+        autostash = true;
+      };
+      rerere.enabled = true;
+    };
+  };
+
   ##############################################################################
   # Services
   ##############################################################################
