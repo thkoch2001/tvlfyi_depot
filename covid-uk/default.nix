6787463e141bcd35d967eb902cf20f253c4ddc7f
William Carroll <wpcarro@gmail.com>
2020-03-19 11:53:57 +0000

    Update COVID-19 webpage
    - Prefer hosting on sandbox.wpcarro.dev; I would prefer to host it at
      sandbox.wpcarro.dev/covid-19, but I haven't figure out how to use Nginx to do
      serve locations like /covid-19 yet.
    - Splice the src directory: When I develop locally and index.html exists within
      ./src, I cannot access ./node_modules because ./node_modules is in a parent
      directory. I could fix this if I used a bundler like Parcel or Webpack, but I
      do not want to set that up at this time.
    - Introduce Tailwind for CSS. This complicates my build a bit as well. For now,
      I'm including output.css even though ideally I should not version-control this
      file. I haven't figured out how to `yarn install` and run commands like `npx
      tailwindcss build styles.css -o output.css` in a Nix derivation yet. Hopefully
      I will learn and refactor this.
    - Add some content about why I made this chart
    - Add some content about some of my covid-19 predictions
    - Add a footer to the webpage
    - Delete timeseries.json and prefer fetching the published data instead

--- covid-uk/default.nix
+++ covid-uk/default.nix
@@ -4,11 +4,13 @@ pkgs.stdenv.mkDerivation {
   name = "covid-uk";
   buildInputs = [];
   src = ./.;
+  # TODO(wpcarro): Need to run `yarn install` somehow.
+  # TODO(wpcarro): Need to run `npx tailwindcss build styles.css -o output.css`.
   buildPhase = ''
     mkdir -p $out
     mkdir -p $out/node_modules/chart.js/dist
     cp $src/node_modules/chart.js/dist/Chart.bundle.min.js $out/node_modules/chart.js/dist
-    cp $src/src/* $out
+    cp $src/index.html $src/output.css $out
   '';
   dontInstall = true;
 }
