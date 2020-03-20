f4fea2346e69235f8346ae626359fa1bf901282d
William Carroll <wpcarro@gmail.com>
2020-03-20 00:47:53 +0000

    Move sandbox into website
    Nest the sandbox work under ./website.

--- website/sandbox/default.nix
+++ website/sandbox/default.nix
@@ -1,15 +1,13 @@
 { pkgs, briefcase, ... }:
 
-let
-  covid-uk = briefcase.sandbox.covid-uk;
-in pkgs.stdenv.mkDerivation {
+pkgs.stdenv.mkDerivation {
   name = "covid-uk";
   buildInputs = [];
   src = ./.;
   buildPhase = ''
     mkdir -p $out
     cp $src/index.html $out
-    cp -r ${briefcase.sandbox.covid-uk} $out/covid-uk
+    cp -r ${briefcase.website.sandbox.covid-uk} $out/covid-uk
   '';
   dontInstall = true;
 }
