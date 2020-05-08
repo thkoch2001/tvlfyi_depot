bf0be392810b18b0ada2e91d990b18c761844e1b
Griffin Smith <root@gws.fyi>
2020-05-08 11:57:47 -0400

    start ssh agent

--- system/modules/common.nix
+++ system/modules/common.nix
@@ -51,6 +51,8 @@
 
   services.openssh.enable = true;
 
+  programs.ssh.startAgent = true;
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
