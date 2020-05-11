9628daf41331958e5cb217490bfb16a600af6dbb
Vincent Ambo <tazjin@google.com>
2020-05-11 02:39:46 +0100

    feat(fun/dt): Support arbitrary word counts

--- fun/dt/dt.cc
+++ fun/dt/dt.cc
@@ -1,56 +1,77 @@
 #include <iostream>
+#include <vector>
 
 #include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
 #include "absl/hash/hash.h"
-#include "absl/time/time.h"
-#include "absl/time/clock.h"
 #include "absl/strings/str_cat.h"
+#include "absl/time/clock.h"
+#include "absl/time/time.h"
+#include "absl/types/optional.h"
 #include "farmhash.h"
 
-ABSL_FLAG(std::string, one, "", "first word");
-ABSL_FLAG(std::string, two, "", "second word");
+ABSL_FLAG(std::vector<std::string>, word, {}, "words to use");
 ABSL_FLAG(int, range, 100, "operating range");
 
-void which(std::string one, std::string two) {
-  if (util::Fingerprint64(one) > util::Fingerprint64(two)) {
-    std::cout << one << std::endl;
-  } else {
-    std::cout << two << std::endl;
+struct Result {
+  std::string a;
+  absl::optional<std::string> p;
+};
+
+std::string which(const std::vector<std::string>& words) {
+  uint64_t fp;
+  std::string word;
+
+  for (const auto& w : words) {
+    auto nfp = util::Fingerprint64(w);
+    if (nfp > fp) {
+      fp = nfp;
+      word = w;
+    }
   }
+
+  return word;
 }
 
-std::string decide(std::string one, std::string two) {
-  auto date = absl::FormatTime("%Y%m%d", absl::Now(), absl::UTCTimeZone());
-  auto base = util::Fingerprint64(absl::StrCat(date, one, two))
-              % (absl::GetFlag(FLAGS_range) + 1);
-
-  if (base % 10 == 0) {
-    return "c2";
-  } else if (base % 9 == 0) {
-    which(one, two);
-    return "c1";
-  } else if (base % 8 == 0) {
-    return "e2";
-  } else if (base % 7 == 0) {
-    which(one, two);
-    return "e1";
-  } else if (base % 3 == 0) {
-    return "skip";
+Result decide(const std::vector<std::string>& words) {
+  auto input = absl::FormatTime("%Y%m%d", absl::Now(), absl::UTCTimeZone());
+  for (const auto& w : words) {
+    input += w;
   }
 
-  return "nope";
+  auto base = util::Fingerprint64(input) % (absl::GetFlag(FLAGS_range) + 1);
+
+  Result result = { "nope" };
+
+  if (base % 10 == 0) {
+    result.a = "ca";
+  } else if (base % 9 == 0) {
+    result.a = "c1";
+    result.p = which(words);
+  } else if (base % 8 == 0) {
+    result.a = "ea";
+  } else if (base % 7 == 0) {
+    result.a = "e1";
+    result.p = which(words);
+  } else if (base % 3 == 0) {
+    result.a = "skip";
+  }
+
+  return result;
 }
 
 int main(int argc, char *argv[]) {
   absl::ParseCommandLine(argc, argv);
-  auto one = absl::GetFlag(FLAGS_one);
-  auto two = absl::GetFlag(FLAGS_two);
 
-  if (one.empty() || two.empty()) {
-    std::cerr << "both are required!" << std::endl;
+  auto words = absl::GetFlag(FLAGS_word);
+  if (words.size() < 2) {
+    std::cerr << "needs at least two!" << std::endl;
     return 1;
   }
 
-  std::cout << decide(one, two) << std::endl;
+  auto result = decide(words);
+  std::cout << result.a
+            << (result.p.has_value() ? absl::StrCat(" ", "(", result.p.value(), ")")
+                                     : "")
+            << std::endl;
 }
