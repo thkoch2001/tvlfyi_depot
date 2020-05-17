40395860c721165f0cc36dfd15b06bc1ea8590bc
Vincent Ambo <tazjin@google.com>
2020-05-17 16:43:21 +0100

    chore(3p/nix): Remove old build system and other miscellanies
    There are still remnants of the old build system (for example, the
    build derivations are not yet updated at all), but we'll get there.

--- third_party/nix/bootstrap-meson.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-rm -rf build*
-meson build-gcc
-
-cd build-gcc
-ninja -j2
-
--- third_party/nix/src/libutil/local.mk
+++
@@ -1,9 +0,0 @@
-libraries += libutil
-
-libutil_NAME = libnixutil
-
-libutil_DIR := $(d)
-
-libutil_SOURCES := $(wildcard $(d)/*.cc)
-
-libutil_LDFLAGS = $(LIBLZMA_LIBS) -lbz2 -pthread $(OPENSSL_LIBS) $(LIBBROTLI_LIBS) $(BOOST_LDFLAGS) -lboost_context
