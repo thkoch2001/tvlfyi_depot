ef2f6f32c11f2a976595b85745071357aa624a52
Griffin Smith <root@gws.fyi>
2020-05-27 09:40:53 -0400

    work email in work repos

--- home/modules/development/urbint.nix
+++ home/modules/development/urbint.nix
@@ -37,6 +37,15 @@ in
     '';
   };
 
+  programs.git = {
+    includes = [{
+      condition = "gitdir:~/code/urb/";
+      contents = {
+        user.email = "grfn@urbint.com";
+      };
+    }];
+  };
+
   home.file.".ipython/profile_default/ipython_config.py".text = ''
     c.InteractiveShellApp.exec_lines = ['%autoreload 2']
     c.InteractiveShellApp.extensions = ['autoreload']
