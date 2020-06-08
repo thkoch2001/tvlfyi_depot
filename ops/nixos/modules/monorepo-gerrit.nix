4000a76678f37ad36ccaa9957eba0737c6e78607
Vincent Ambo <tazjin@google.com>
2020-06-08 00:35:45 +0000

    feat(monorepo-gerrit): Configure Gerrit for LDAP authentication

--- ops/nixos/modules/monorepo-gerrit.nix
+++ ops/nixos/modules/monorepo-gerrit.nix
@@ -11,6 +11,21 @@
       log.jsonLogging = true;
       log.textLogging = false;
       # TODO: gitweb config
+
+      # Configures integration with the locally running OpenLDAP
+      auth.type = "LDAP";
+      ldap = {
+        server = "ldap://localhost";
+        accountBase = "ou=users,dc=tvl,dc=fyi";
+        accountPattern = "(&(objectClass=organizationalPerson)(cn=\${username}))";
+        accountFullName = "cn";
+        accountEmailAddress = "mail";
+        groupBase = "ou=groups,dc=tvl,dc=fyi";
+        gerrit.canonicalWebUrl = "https://cl.tvl.fyi";
+
+        # TODO(tazjin): Assuming this is what we'll be doing ...
+        groupMemberPattern = "(&(objectClass=group)(member=\${dn}))";
+      };
     };
   };
 }
