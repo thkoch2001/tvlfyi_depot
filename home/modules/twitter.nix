5477cc1b2a71b11810e4dee195dea3f74a9942d3
Griffin Smith <root@gws.fyi>
2020-06-08 10:19:26 -0400

    some twitter functions

--- home/modules/twitter.nix
+++ home/modules/twitter.nix
@@ -9,7 +9,15 @@
     TWITTER_WHOAMI = "glittershark1";
   };
 
-  programs.zsh.shellAliases = {
-    "mytl" = "t tl $TWITTER_WHOAMI";
+  programs.zsh = {
+    shellAliases = {
+      "mytl" = "t tl $TWITTER_WHOAMI";
+    };
+
+    functions = {
+      favelast = "t fave $(t tl -l $1 | head -n1 | cut -d' ' -f1)";
+      rtlast = "t rt $(t tl -l $1 | head -n1 | cut -d' ' -f1)";
+      tthread = "t reply $(t tl -l $TWITTER_WHOAMI | head -n1 | cut -d' ' -f1) $@";
+    };
   };
 }
