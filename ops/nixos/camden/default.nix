7c0b179a1cc809c71519c1ed6342cd9cb2827de4
Kane York <kanepyork@gmail.com>
2020-06-11 18:19:51 -0700

    feat(camden): add builds shortlink
    Change-Id: Iedd524d775349f24c13fe7c118830b7d4dfdec49

--- ops/nixos/camden/default.nix
+++ ops/nixos/camden/default.nix
@@ -362,11 +362,12 @@ in lib.fix(self: {
       extraConfig = ''
         add_header Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" always;
 
+        rewrite ^/builds/?$ https://builds.sr.ht/~tazjin last;
         rewrite ^/meet/?$ https://meet.google.com/mng-biyw-xbb last;
 
         rewrite ^/monorepo-doc/?$ https://docs.google.com/document/d/1nnyByXcH0F6GOmEezNOUa2RFelpeRpDToBLYD_CtjWE/edit?usp=sharing last;
 
-	rewrite ^/irc/?$ ircs://chat.freenode.net:6697/##tvl;
+        rewrite ^/irc/?$ ircs://chat.freenode.net:6697/##tvl last;
 
         location ~* \.(webp|woff2)$ {
           add_header Cache-Control "public, max-age=31536000";
