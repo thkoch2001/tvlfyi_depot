8e45711f79b36ee78b3d53052334df615e59303a
Vincent Ambo <mail@tazj.in>
2020-06-14 05:02:09 +0100

    feat: Implement a submit rule to check commit message conformance
    We define commit message guidelines in CONTRIBUTING.md (loosely based
    on the Angular project's commit message format).
    
    This adds a submit rule check to ensure that the commit message's
    prefix matches the allowed commit "types".
    
    We only check the prefix because, presumably, if someone got that bit
    right they have at least read the docs.
    
    Change-Id: Id4525e49617d54433f87fee3c4ea3bb08f570e86
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/250
    Reviewed-by: lukegb <lukegb@tvl.fyi>
    Reviewed-by: isomer <isomer@tvl.fyi>

--- rules.pl
+++ rules.pl
@@ -5,6 +5,7 @@
 % - If a codepath has owners, the change must either have a +2 from one
 %   of the owners or be proposed by one of them.
 % - No unresolved comments exist.
+% - The commit message conforms to our guidelines.
 
 append([],L,L).
 append([H|T],L2,[H|L3]) :-
@@ -21,6 +22,15 @@ unresolved_comments(Check) :-
     Count > 0,
     Check = label('All-Comments-Resolved', need(_)).
 
+commit_message(Check) :-
+    gerrit:commit_message_matches('^(feat|fix|docs|style|refactor|test|chore)[\(:]'),
+    !,
+    gerrit:uploader(Uploader),
+    Check = label('Conformant-Commit-Message', ok(Uploader)).
+
+commit_message(Check) :-
+    Check = label('Conformant-Commit-Message', need(_)).
+
 submit_rule(S) :-
     gerrit:default_submit(D),
     D =.. [submit | DefaultChecks],
@@ -35,6 +45,10 @@ submit_rule(S) :-
     % Check for unresolved comments (this is necessary because it's
     % easy to miss them in previous patch sets)
     unresolved_comments(CommentsCheck),
-    append(ChecksWithOwners, [CommentsCheck], ChecksWithComments),
 
-    S =.. [submit | ChecksWithComments].
+    % Check that the commit message matches the format described in
+    % CONTRIBUTING.md
+    commit_message(CommitCheck),
+
+    append(ChecksWithOwners, [CommentsCheck, CommitCheck], AllChecks),
+    S =.. [submit | AllChecks].
