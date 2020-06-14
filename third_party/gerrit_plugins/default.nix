34e30bf7a6cda38a7f3c482f00a097c187739a28
Vincent Ambo <mail@tazj.in>
2020-06-14 18:27:12 +0100

    chore(3p/gerrit_plugins): Update plugins for Gerrit 3.2
    Change-Id: I0b1a2871768a8369dac7a3e2b06a38c07741e945
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/281
    Reviewed-by: lukegb <lukegb@tvl.fyi>

--- third_party/gerrit_plugins/default.nix
+++ third_party/gerrit_plugins/default.nix
@@ -4,13 +4,13 @@ let inherit (pkgs) fetchurl;
 in {
   # https://gerrit.googlesource.com/plugins/owners
   owners = fetchurl {
-    url = "https://storage.googleapis.com/tazjins-data/tvl/owners_3.1.jar";
-    sha256 = "1kp61hbz74irsxsbgrj9hk8jlavjx8jvkmbf3kwfa62wvzivc94b";
+    url = "https://storage.googleapis.com/tazjins-data/tvl/owners_3.2.jar";
+    sha256 = "1xw1q3g0353aw5jqxp69n85f8y57l2p51np37n8r34kzbn5r4iz7";
   } // { name = "owners"; };
 
   # https://gerrit.googlesource.com/plugins/owners
   owners-autoassign = fetchurl {
-    url = "https://storage.googleapis.com/tazjins-data/tvl/owners-autoassign_3.1.jar";
-    sha256 = "1kfrgbds62jx1bxwwnzmd6jrb1sg671kssk7sy4nkyb6p5vlq8hq";
+    url = "https://storage.googleapis.com/tazjins-data/tvl/owners-autoassign_3.2.jar";
+    sha256 = "1yr6rpzlac2kdxagjk8mfw6fj66fsn31v5h03bi129ri29ikppql";
   } // { name = "owners-autoassign"; };
 }
