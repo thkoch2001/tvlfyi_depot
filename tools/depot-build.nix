8660bb51e0c8e66bec0038aec787bbc1b358d6a2
Vincent Ambo <mail@tazj.in>
2020-06-15 20:03:47 +0100

    feat(depot-build): Add a tool to build at arbitrary tree locations
    Assuming you're in `/depot/fun/clbot`, you can do this:
    
        tazjin@frog /d/f/clbot (master)> depot-build
        Building //fun/clbot
        /nix/store/i1zc6g58wa4819kyhaqi12zsh3hr31ph-clbot
    
    It is automatically added to the $PATH using direnv, too.
    
    Change-Id: Ia3341704e6317c2b8de40a3fa1be3b680d21a42d

---
+++ tools/depot-build.nix
@@ -0,0 +1,8 @@
+# Utility script for building any arbitrary depot path in its folder.
+{ pkgs, ... }:
+
+pkgs.writeShellScriptBin "depot-build" ''
+  TARGET=$(git rev-parse --show-prefix | sed 's|/$||')
+  echo "Building //$TARGET"
+  nix-build -A $(echo $TARGET | sed 's|/|.|') $(${pkgs.git}/bin/git rev-parse --show-toplevel)
+''
