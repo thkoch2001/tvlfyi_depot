7068078dfd3b29cb0bf270777df4d22f5d099b04
eta <eta@theta.eu.org>
2020-06-15 21:03:40 +0100

    feat(tvldb): Import the tvldb/paroxysm source, add a Nix derivation
    - This imports the tvldb (actually a thing called 'paroxysm') code
      from https://git.theta.eu.org/eta/paroxysm into the monorepo.
    - Additionally, I did a nix thing, yay! \o/
      (well, with tazjin's help)
    - 3p/default.nix needed modifying to whitelist pgsql.
    
    Change-Id: Icdf13ca221650dde376f632bd2dd8a087af451bf

---
+++ fun/tvldb/src/cfg.rs
@@ -0,0 +1,11 @@
+use std::collections::HashSet;
+
+#[derive(Deserialize)]
+pub struct Config {
+    pub database_url: String,
+    pub irc_config_path: String,
+    #[serde(default)]
+    pub admins: HashSet<String>,
+    #[serde(default)]
+    pub log_filter: Option<String>,
+}
