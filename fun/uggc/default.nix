a6b05172c47cb96dc6b1cb02820deb8e7131cd1d
Vincent Ambo <mail@tazj.in>
2020-06-15 19:29:19 +0100

    feat(fun/uggc): Install desktop handler in Nix derivation
    This actually registers the desktop entry, so xdg-open etc. start
    working:
    
    tazjin@frog /depot> xdg-open 'uggcf://gjvggre.pbz/alnabgrpu/fgnghf/1272363652679524352'
    Opening in existing browser session.
    
    Change-Id: Ia754cb87470fce2f8d3340d1147f3f01b9914858

--- fun/uggc/default.nix
+++ fun/uggc/default.nix
@@ -1,14 +1,19 @@
-{ depot, ... }@args:
+{ depot, pkgs, ... }@args:
 
 let
-  gopkgs = depot.third_party.gopkgs;
-in
-depot.nix.buildGo.program {
-  name = "uggc";
-  srcs = [
-    ./main.go
-  ];
-  deps = [
-    gopkgs."github.com".pkg.browser.gopkg
-  ];
-}
+  inherit (pkgs) gopkgs;
+  uggc = depot.nix.buildGo.program {
+    name = "uggc";
+    srcs = [
+      ./main.go
+    ];
+    deps = [
+      gopkgs."github.com".pkg.browser.gopkg
+    ];
+  };
+in uggc.overrideAttrs(old: {
+  buildCommand = old.buildCommand + ''
+    install -D ${./uggc.desktop} $out/share/applications/uggc.desktop
+    sed "s|@out@|$out|g" -i $out/share/applications/uggc.desktop
+  '';
+})
