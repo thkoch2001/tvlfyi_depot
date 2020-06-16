555853cdd58e374f8fd0637893a77d748efc8f66
Griffin Smith <grfn@gws.fyi>
2020-06-15 20:54:26 -0400

    feat(bin/gerrit-cli): Add gerrit CLI script
    Add a script to run gerrit commands on the depot host, which reads the
    gerrit username from the TVL_USERNAME env var or $(whoami) by default.
    
    At some point this may want to solve the configuration problem in a more
    general fashion, but for now this seems relatively obvious.
    
    Change-Id: Ied91c1d26daf4770aef74b2e755d1760c486bb7b

---
+++ tools/gerrit-cli.nix
@@ -0,0 +1,13 @@
+# Utility script to run a gerrit command on the depot host via ssh.
+# Reads the username from TVL_USERNAME, or defaults to $(whoami)
+{ pkgs, ... }:
+
+pkgs.writeShellScriptBin "gerrit" ''
+  TVL_USERNAME=''${TVL_USERNAME:-$(whoami)}
+  if $(which ssh 2>/dev/null); then
+    ssh=ssh
+  else
+    ssh="${pkgs.openssh}/bin/ssh"
+  fi
+  $ssh $TVL_USERNAME@code.tvl.fyi -p 29418 -- gerrit $@
+''
