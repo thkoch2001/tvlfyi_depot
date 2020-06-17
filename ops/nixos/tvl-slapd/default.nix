05d06c68382349e9673beaccb1b4f440031dc27c
Vincent Ambo <mail@tazj.in>
2020-06-17 03:48:21 +0100

    refactor(tvl-slapd): Move user definitions into Nix code
    Implements a function that generates the LDIF record for each user and
    templates it into the configuration.
    
    This is slightly more user-friendly and less error-prone (people kept
    getting the DNs wrong) than editing the contents manually.
    
    Change-Id: Ic419d2ef464f9a94be5d54b666f7d53134b53eed

--- ops/nixos/tvl-slapd/default.nix
+++ ops/nixos/tvl-slapd/default.nix
@@ -1,19 +1,94 @@
 # Configures an OpenLDAP instance for TVL
 #
 # TODO(tazjin): Configure ldaps://
-{ pkgs, config, ... }:
+{ config, lib, pkgs, ... }:
 
-{
+with config.depot.nix.yants;
+
+let
+  user = struct {
+    username = string;
+    email = string;
+    password = string;
+    displayName = option string;
+  };
+
+  toLdif = defun [ user string ] (u: ''
+    dn: cn=${u.username},ou=users,dc=tvl,dc=fyi
+    objectClass: organizationalPerson
+    objectClass: inetOrgPerson
+    sn: ${u.username}
+    cn: ${u.username}
+    displayName: ${u.displayName or u.username}
+    mail: ${u.email}
+    userPassword: ${u.password}
+  '');
+
+  users = [
+    {
+      username = "cynthia";
+      email = "cynthia@tvl.fyi";
+      password = "{SSHA}aHx2keEnXv6u6oiV2xxqfXdxjom/K8CP";
+    }
+    {
+      username = "edef";
+      email = "edef@edef.eu";
+      password = "{SSHA}7w2XC6xxuhlUX2KvBpK4fD/X7ZCpfN/E";
+    }
+    {
+      username = "eta";
+      email = "eta@theta.eu.org";
+      password = "{SSHA}sOR5xzi7Lfv376XGQA8Hf6jyhTvo0XYc";
+    }
+    {
+      username = "glittershark";
+      email = "grfn@gws.fyi";
+      password = "{SSHA}i7PSAsXwJT3jjmmvU77aar/tU/YPDCEO";
+    }
+    {
+      username = "isomer";
+      email = "isomer@tvl.fyi";
+      password = "{SSHA}OhWQkPJgH1rRJqYIaMUbbKC4iLEzvCev";
+    }
+    {
+      username = "lukegb";
+      email = "lukegb@tvl.fyi";
+      password = "{SSHA}7a85VNhpFElFw+N5xcjgGmt4HnBsaGp4";
+    }
+    {
+      username = "nyanotech";
+      email = "nyanotechnology@gmail.com";
+      password = "{SSHA}NIJ2RCRb1+Q4Bs63cyE91VZyiN47DG6y";
+    }
+    {
+      username = "q3k";
+      email = "q3k@q3k.org";
+      password = "{SSHA}BEccJdtnhVLDzOn+pxNfayNi3QFcEABE";
+    }
+    {
+      username = "ericvolp12";
+      email = "ericvolp12@gmail.com";
+      password = "{SSHA}pSepaQ+/5KBLfJtRR5rfxGU8goAsXgvk";
+    }
+    {
+      username = "riking";
+      displayName = "Kane York";
+      email = "rikingcoding@gmail.com";
+      password = "{SSHA}6rPxMOofHMGNTEYdyBOYbza7NT/RmiGz";
+    }
+    {
+      username = "tazjin";
+      email = "mail@tazj.in";
+      password = "{SSHA}67H341jRfAFBDz/R9+T3fHQiPfjwTbpQ";
+    }
+  ];
+in {
   services.openldap = {
     enable = true;
     dataDir = "/var/lib/openldap";
     suffix = "dc=tvl,dc=fyi";
     rootdn = "cn=admin,dc=tvl,dc=fyi";
     rootpw = "{SSHA}yEEO6Ol2W3ritdiJzPSsjOtyPGxWF2JW";
-
-    # Contents are immutable at runtime, and adding user accounts etc.
-    # is done statically in the LDIF-formatted contents in this folder.
-    declarativeContents = builtins.readFile ./contents.ldif;
 
     # ACL configuration
     extraDatabaseConfig = ''
@@ -25,6 +100,32 @@
 
       # Allow default read access to other directory elements
       access to * by * read
+    '';
+
+    # Contents are immutable at runtime, and adding user accounts etc.
+    # is done statically in the LDIF-formatted contents in this folder.
+    declarativeContents = ''
+      dn: dc=tvl,dc=fyi
+      dc: tvl
+      o: TVL LDAP server
+      description: Root entry for tvl.fyi
+      objectClass: top
+      objectClass: dcObject
+      objectClass: organization
+
+      dn: ou=users,dc=tvl,dc=fyi
+      ou: users
+      description: All users in TVL
+      objectClass: top
+      objectClass: organizationalUnit
+
+      dn: ou=groups,dc=tvl,dc=fyi
+      ou: groups
+      description: All groups in TVL
+      objectClass: top
+      objectClass: organizationalUnit
+
+      ${lib.concatStringsSep "\n" (map toLdif users)}
     '';
   };
 }
