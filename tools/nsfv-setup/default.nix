f5060f0e06f9673350fd1435d0c393fe6b783876
Vincent Ambo <mail@tazj.in>
2020-06-26 04:38:26 +0100

    feat(tools/nsfv-setup): Move NSFV setup script out of //users
    This program can be used for noise-cancelling in arbitrary audio
    streams. See CL/576 and CL/577 for more details.
    
    This moves the script out of my users folder and to //tools, and adds
    two changes:
    
    * Existing sinks will be removed & reinitialised
    * The sink is changed from stereo->mono (which seems to make little
      difference but is more reliable)
    * The parameter has changed from 50 to 42 because I felt like it
    
    Change-Id: Id9ff285fefd682ccc4d9f18e46b5fde9cd056aa7

--- tools/nsfv-setup/default.nix
+++ tools/nsfv-setup/default.nix
@@ -7,12 +7,21 @@
 # There are some caveats, for example this will not distinguish
 # between noise from different participants and I have no idea what
 # happens if the default sink goes away.
+#
+# If this script is run while an NSFV sink exists, the existing sink
+# will first be removed.
 { pkgs, ... }:
 
 let
   inherit (pkgs) ripgrep pulseaudio nsfv;
 in pkgs.writeShellScriptBin "nsfv-setup" ''
+  export PATH="${ripgrep}/bin:${pulseaudio}/bin:$PATH"
+
+  if pacmd list-sinks | rg librnnoise_ladspa.so >/dev/null; then
+    pactl unload-module module-ladspa-sink
+  fi
+
   SINK=$(${pulseaudio}/bin/pacmd info | ${ripgrep}/bin/rg -r '$1' '^Default sink name: (.*)$')
   echo "Setting up NSFV filtering to sink ''${SINK}"
-  ${pulseaudio}/bin/pacmd load-module module-ladspa-sink sink_name=NSFV sink_master=''${SINK} label=noise_suppressor_stereo plugin=${pkgs.nsfv}/lib/ladspa/librnnoise_ladspa.so control=50
+  ${pulseaudio}/bin/pacmd load-module module-ladspa-sink sink_name=NSFV sink_master=''${SINK} label=noise_suppressor_mono plugin=${pkgs.nsfv}/lib/ladspa/librnnoise_ladspa.so control=42
 ''
