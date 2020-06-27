9079936e38b4d4c5b0bcdc317f3e997b8126cdfe
Profpatsch <mail@profpatsch.de>
2020-06-28 00:06:11 +0200

    refactor(nix/escapeExecline): move into own directory
    The escaping functions are going to be used by both `writeExecline`
    and `runExecline`, so let’s move them to their own namespace.
    
    Change-Id: Iccf69eaeca3062573e0751a17c548b7def86196d

--- nix/writeExecline/default.nix
+++ nix/writeExecline/default.nix
@@ -1,38 +1,10 @@
-{ pkgs, lib, ... }:
+{ pkgs, depot, ... }:
 
 # Write an execline script, represented as nested nix lists.
 # Everything is escaped correctly.
 # https://skarnet.org/software/execline/
 
 # TODO(Profpatsch) upstream into nixpkgs
-
-let
-  # replaces " and \ to \" and \\ respectively and quote with "
-  # e.g.
-  #   a"b\c -> "a\"b\\c"
-  #   a\"bc -> "a\\\"bc"
-  escapeExeclineArg = arg:
-    ''"${builtins.replaceStrings [ ''"'' ''\'' ] [ ''\"'' ''\\'' ] (toString arg)}"'';
-
-  # Escapes an execline (list of execline strings) to be passed to execlineb
-  # Give it a nested list of strings. Nested lists are interpolated as execline
-  # blocks ({}).
-  # Everything is quoted correctly.
-  #
-  # Example:
-  #   escapeExecline [ "if" [ "somecommand" ] "true" ]
-  #   == ''"if" { "somecommand" } "true"''
-  escapeExecline = execlineList: lib.concatStringsSep " "
-    (let
-      go = arg:
-        if      builtins.isString arg then [(escapeExeclineArg arg)]
-        else if builtins.isPath arg then [(escapeExeclineArg "${arg}")]
-        else if lib.isDerivation arg then [(escapeExeclineArg arg)]
-        else if builtins.isList arg then [ "{" ] ++ builtins.concatMap go arg ++ [ "}" ]
-        else abort "escapeExecline can only hande nested lists of strings, was ${lib.generators.toPretty {} arg}";
-     in builtins.concatMap go execlineList);
-
-in
 
 name:
 {
@@ -63,5 +35,5 @@ in
   # TODO(Profpatsch): rewrite `writeScript` with `runExecline`
   pkgs.writeScript name ''
     #!${pkgs.execline}/bin/execlineb -W${env}
-    ${escapeExecline argList}
+    ${depot.nix.escapeExecline argList}
   ''
