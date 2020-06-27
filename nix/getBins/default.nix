20c6ab0970aa338da240ba777b50f1e882c24071
Profpatsch <mail@profpatsch.de>
2020-06-27 23:14:41 +0200

    feat(nix/getBins): add getBins
    This is a simple-stupid “unix import system” for nix, for referencing
    binaries in `/bin/` by their name and lifting them to a nix attrset.
    
    Allows for simple aliasing of executable names.
    
    Change-Id: Ifa23cb377201c3b08050c5026e9751e736afaf56

---
+++ nix/getBins/default.nix
@@ -0,0 +1,16 @@
+{ lib, pkgs, depot, ... }:
+
+let
+
+  getBins = import ./getBins.nix { inherit lib; };
+
+  tests = import ./tests.nix {
+    inherit getBins;
+    inherit (pkgs) writeScriptBin;
+    inherit (depot.nix.runTestsuite) assertEq it runTestsuite;
+  };
+
+in {
+  __functor = getBins;
+  inherit tests;
+}
---
+++ nix/getBins/getBins.nix
@@ -0,0 +1,33 @@
+{ lib }:
+# Takes a derivation and a list of binary names
+# and returns an attribute set of `name -> path`.
+# The list can also contain renames in the form of
+# `{ use, as }`, which goes `as -> usePath`.
+#
+# It is usually used to construct an attrset `bins`
+# containing all the binaries required in a file,
+# similar to a simple import system.
+#
+# Example:
+#
+#   bins = getBins pkgs.hello [ "hello" ]
+#       // getBins pkgs.coreutils [ "printf" "ln" "echo" ]
+#       // getBins pkgs.execline
+#            [ { use = "if"; as = "execlineIf" } ]
+#       // getBins pkgs.s6-portable-utils
+#            [ { use = "s6-test"; as = "test" }
+#              { use = "s6-cat"; as = "cat" }
+#            ];
+#
+#   provides
+#     bins.{hello,printf,ln,echo,execlineIf,test,cat}
+#
+drv: xs:
+  let f = x:
+    # TODO(Profpatsch): typecheck
+    let x' = if builtins.isString x then { use = x; as = x; } else x;
+    in {
+      name = x'.as;
+      value = "${lib.getBin drv}/bin/${x'.use}";
+    };
+  in builtins.listToAttrs (builtins.map f xs)
