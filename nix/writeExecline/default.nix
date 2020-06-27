2ef31c7643ddfb4e39641398e6cc09f54e76e9b3
Profpatsch <mail@profpatsch.de>
2020-06-27 06:11:47 +0200

    feat(nix/writeExecline): add writeExecline
    This is a writer, similar to `pkgs.writeBashScript` or
    `pkgs.writers.writePython3`.
    
    The difference is that we can correctly write all execline scripts by
    using nix lists of lists, so we don’t have to care about escaping
    arguments.
    
    Change-Id: I2f2874cf61170ddca07b89b692f762725f4a75dc

---
+++ nix/writeExecline/default.nix
@@ -0,0 +1,43 @@
+{ pkgs, ... }:
+
+# Write an execline script, represented as nested nix lists.
+# Everything is escaped correctly.
+# https://skarnet.org/software/execline/
+
+# TODO(Profpatsch) upstream into nixpkgs
+
+let
+  escape = import ./escape.nix { inherit (pkgs) lib; };
+
+in
+name:
+{
+  # "var": substitute readNArgs variables and start $@
+  # from the (readNArgs+1)th argument
+  # "var-full": substitute readNArgs variables and start $@ from $0
+  # "env": don’t substitute, set # and 0…n environment vaariables, where n=$#
+  # "none": don’t substitute or set any positional arguments
+  # "env-no-push": like "env", but bypass the push-phase. Not recommended.
+  argMode ? "var",
+  # Number of arguments to be substituted as variables (passed to "var"/"-s" or "var-full"/"-S"
+  readNArgs ? 0,
+}:
+# Nested list of lists of commands.
+# Inner lists are translated to execline blocks.
+argList:
+
+let
+  env =
+    if      argMode == "var" then "s${toString readNArgs}"
+    else if argMode == "var-full" then "S${toString readNArgs}"
+    else if argMode == "env" then ""
+    else if argMode == "none" then "P"
+    else if argMode == "env-no-push" then "p"
+    else abort ''"${toString argMode}" is not a valid argMode, use one of "var", "var-full", "env", "none", "env-no-push".'';
+
+in
+  # TODO(Profpatsch): rewrite `writeScript` with `runExecline`
+  pkgs.writeScript name ''
+    #!${pkgs.execline}/bin/execlineb -W${env}
+    ${escape.escapeExecline argList}
+  ''
