998b0b050974e04a39a72613fc11ff3963f79388
William Carroll <wpcarro@gmail.com>
2020-06-27 14:06:59 +0100

    Add export script for groceries
    At this point, I may be taking this idea too far, but what the heck?

---
+++ scratch/groceries/export.hs
@@ -0,0 +1,11 @@
+module Main where
+
+import Data.Function ((&))
+import qualified Data.List as L
+
+-- | Run this to export the grocery list.
+main :: IO ()
+main = do
+  x <- readFile "./list.txt"
+  x & lines & filter (not . L.isPrefixOf "- 0x") & unlines & putStrLn
+  pure ()
