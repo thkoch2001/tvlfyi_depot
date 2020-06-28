8094e09d1792a18c1471b7c4b30648e5128d7353
Vincent Ambo <mail@tazj.in>
2020-06-28 02:54:41 +0100

    feat(ops/pipelines): Submit CL verification status to Gerrit
    WIP
    
    Change-Id: I2685f4af03dfeb9a4e5cce0828012c39ca58d1d9

--- ops/pipelines/depot.nix
+++ ops/pipelines/depot.nix
@@ -4,18 +4,27 @@
 # It outputs a "YAML" (actually JSON) file which is evaluated and
 # submitted to Buildkite at the start of each build. This means we can
 # dynamically configure the pipeline execution here.
-{ depot, pkgs, ... }:
+{ depot, lib, pkgs, ... }:
 
 let
-  inherit (builtins) toJSON;
+  inherit (builtins) getEnv toJSON;
   inherit (pkgs) writeText;
+
+  # This Nix expression is evaluated when a Buildkite pipeline is
+  # launched and, as you can see below, needs to do some impure things
+  # to decide the shape of the pipeline.
+  orString = s: d: if s == "" then d else s;
+  pipelineBranch = orString (getEnv "BUILDKITE_BRANCH") "canon";
+
+  gerritUpdate = lib.optionals (lib.hasPrefix "refs/changes/" pipelineBranch) [
+  ];
 
   # This defines the build pipeline, using the pipeline format
   # documented on https://buildkite.com/docs/pipelines/defining-steps
   pipeline.steps = [
     {
       command = "nix-build -A ciBuilds.__allTargets";
-      label = "all-targets";
+      label = "allTargets";
     }
-  ];
+  ] ++ gerritUpdate;
 in writeText "depot.yaml" (toJSON pipeline)
