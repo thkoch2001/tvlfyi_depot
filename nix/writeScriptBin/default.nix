14563ab27dd29a2ee210149f79debb8b4d323f52
Profpatsch <mail@profpatsch.de>
2020-06-28 04:52:08 +0200

    refactor(nix): use our own writeScript(Bin)
    We have this nice `runExecline` now, so we don’t need to use
    `runCommand` (which spawns bash) just to write a simple script.
    
    Change-Id: I2941ed8c1448fa1d7cc02dc18b24a8a945b2c38b

---
+++ nix/writeScriptBin/default.nix
@@ -0,0 +1,12 @@
+{ depot, ... }:
+
+# Like writeScript,
+# but put the script into `$out/bin/${name}`.
+
+name:
+script:
+
+depot.nix.binify {
+  exe = (depot.nix.writeScript name script);
+  inherit name;
+}
