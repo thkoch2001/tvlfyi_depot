3bc65bae16f899e0ead94e22460dfa387a1b8f4c
Griffin Smith <grfn@gws.fyi>
2020-06-28 21:38:41 -0400

    feat(ci-builds): Add overridden haskell packages
    Add all the haskell packages we've overridden to the ci targets, so we
    can check that they build successfully.
    
    Change-Id: I3c2f2d61f542cc06ac2266881e182e755fcb3774

--- ci-builds.nix
+++ ci-builds.nix
@@ -65,6 +65,16 @@ in lib.fix(self: {
     depot.nix.getBins.tests
   ];
 
+  # Haskell packages we've patched locally
+  haskellPackages = with depot.third_party.haskellPackages; [
+    generic-arbitrary
+    hgeometry
+    hgeometry-combinatorial
+    vinyl
+    comonad-extras
+    ghc-prof-flamegraph
+  ];
+
   # User-specific build targets
   tazjin = with depot.users.tazjin; [
     blog
