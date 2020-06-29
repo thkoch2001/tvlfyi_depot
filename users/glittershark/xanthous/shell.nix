457d47b952be4ecd4347f77ec9300cb0e5be2f67
Griffin Smith <grfn@gws.fyi>
2020-06-29 09:38:17 -0400

    refactor(xan): Simplify build infrastructure
    Simplify the build infrastructure for creating shells etc. for xanthous,
    including relying more heavily on stuff exposed in Depot anyway, using
    shellFor in the shell.nix, and using the haskell.nix branch of all-hies
    for a significantly simpler build. At some point the all-hies stuff will
    likely want to be promoted up to //third_party, but for now it's
    experimental enough that I don't want to bother other people with it.
    
    Change-Id: I43830de73830fdc07d48af9b87d7930f59599532

--- users/glittershark/xanthous/shell.nix
+++ users/glittershark/xanthous/shell.nix
@@ -1,35 +1,14 @@
-{ nixpkgs ? import ./nixpkgs.nix {}
-, pkgs ? nixpkgs.pkgs
-, compiler ? "ghc865"
-, withHoogle ? true
-}:
-let
-  pkg = import ./pkg.nix { inherit pkgs; };
+{ pkgs ? (import ../../../. {}).third_party, ... }:
 
-  packageSet = (
-    if compiler == "default"
-    then pkgs.haskellPackages
-    else pkgs.haskell.packages.${compiler}
-  );
-
-  haskellPackages = (
-    if withHoogle
-    then packageSet.override {
-      overrides = (self: super: {
-        ghc = super.ghc // { withPackages = super.ghc.withHoogle; };
-        ghcWithPackages = self.ghc.withPackages;
-      } // (import ../../../third_party/haskell_overlay { inherit pkgs; })
-        self super);
-    }
-    else packageSet
-  );
-
-  drv = pkgs.haskell.lib.doBenchmark (haskellPackages.callPackage pkg {});
-
-  inherit (pkgs.haskell.lib) addBuildTools;
-in
-(addBuildTools drv (with haskellPackages; [
-  cabal-install
-  ghc-prof-flamegraph
-  hp2pretty
-])).env
+(pkgs.haskellPackages.extend (pkgs.haskell.lib.packageSourceOverrides {
+  xanthous = pkgs.gitignoreSource ./.;
+})).shellFor {
+  packages = p: [p.xanthous];
+  withHoogle = true;
+  buildInputs = with pkgs.haskellPackages; [
+    cabal-install
+    ghc-prof-flamegraph
+    hp2pretty
+    (import ./hie.nix { inherit pkgs; })
+  ];
+}
