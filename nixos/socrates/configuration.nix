a276eddf54b1f00922b7b03e9c788b12b3b2a527
William Carroll <wpcarro@gmail.com>
2020-06-30 20:01:01 +0100

    Add auth to bitlbee
    Use Linux's PAM for Bitlbee's auth.

--- nixos/socrates/configuration.nix
+++ nixos/socrates/configuration.nix
@@ -100,6 +100,8 @@ in {
   nixpkgs.config.bitlbee.enableLibPurple = true;
   services.bitlbee = {
     interface = "0.0.0.0";
+    authBackend = "pam";
+    authMode = "Registered";
     enable = true;
     libpurple_plugins = [
       pkgs.telegram-purple
