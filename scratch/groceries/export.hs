bae02df2392a921d0e4f9dd02bb16ad43939bdd9
William Carroll <wpcarro@gmail.com>
2020-07-01 10:45:28 +0100

    Convert grocery list to an .org file
    Ensure that the export.hs script refers to the .org file.

--- scratch/groceries/export.hs
+++ scratch/groceries/export.hs
@@ -6,6 +6,6 @@ import qualified Data.List as L
 -- | Run this to export the grocery list.
 main :: IO ()
 main = do
-  x <- readFile "./list.txt"
+  x <- readFile "./list.org"
   x & lines & filter (not . L.isPrefixOf "- 0x") & unlines & putStrLn
   pure ()
