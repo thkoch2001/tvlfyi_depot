3839d0eea37a4f9176e2df8c0d66cd345c42f68b
Vincent Ambo <mail@tazj.in>
2020-07-02 23:10:10 +0100

    feat(tazjin): Add SSH key from vauxhall
    Change-Id: Ie897d9e2309f971154ba43e7dce73bb29f4ca9df

--- users/tazjin/keys.nix
+++ users/tazjin/keys.nix
@@ -3,5 +3,7 @@
 
 rec {
   frog = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKMZzRdcrHTuCPoaFy36MPr5IW/hnImlse/OBOn6udL/ tazjin@frog";
-  all = [ frog ];
+  vauxhall = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBHs+9QfZTD5qGsBQaWqp5whmXJ9qy/m9swE2M9QBaIQVoIYGemq3HXTzrQ6XekwudJCltP4EpM7h/Qc+Or309Yw=";
+
+  all = [ frog vauxhall ];
 }
