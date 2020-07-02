eb1af216cf6442138e46448c0f503276aba4d55f
William Carroll <wpcarro@gmail.com>
2020-07-02 14:39:08 +0100

    Clear $NIX_PATH
    Ensure that $NIX_PATH is empty using a .envrc file. nix.dev considers references
    to <nixpkgs>, <briefcase>, <depot>, <unstable> to be an anti-pattern because it
    hinders reproducibility by introducing an implicit environment.
    
    This repository is chock-full of references to
    <{nixpkgs,unstable,briefcase,depot}>, so right now everything is broken.

--- default.nix
+++ default.nix
@@ -1,10 +1,24 @@
 { ... }:
 
 let
-  readTree = import <depot/nix/readTree> {} {
-    pkgs      = import <nixpkgs> {};
-    depot     = import <depot> {};
-    briefcase = import <briefcase> {};
+  depot = import (builtins.fetchGit {
+    url = "https://cl.tvl.fyi/depot";
+    rev = "a2e86152401c7c531801c79347c3f15e1806aabc";
+  }) {};
+  readTree = depot.readTree {
+    pkgs = import (builtins.fetchGit {
+      url = "https://github.com/NixOS/nixpkgs";
+      rev = "f1a79c86358c5464c64b4fad00fca07a10e62a74";
+    }) {};
+    unstable = import (builtins.fetchGit {
+      url = "https://github.com/NixOS/nixpkgs";
+      rev = "b3251e04ee470c20f81e75d5a6080ba92dc7ed3f";
+    }) {};
+    briefcase = import (builtins.path {
+      path = ./.;
+      name = "briefcase";
+    }) {};
+    depot = depot;
   };
 in {
   nixos       = readTree ./nixos;
