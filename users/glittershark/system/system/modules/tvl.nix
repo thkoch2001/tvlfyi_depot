ae5c5b88e5cc894303b7f1552101be0299c361f0
Griffin Smith <grfn@gws.fyi>
2020-07-02 18:46:33 -0400

    feat(gs/system): Configure whitby as a remote builder
    This isn't working, but someday it might
    
    Change-Id: Iac20c05b033df3484a7c6908473601bb17c9fcfe
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/883
    Tested-by: BuildkiteCI
    Reviewed-by: lukegb <lukegb@tvl.fyi>
    Reviewed-by: BuildkiteCI

---
+++ users/glittershark/system/system/modules/tvl.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, ... }:
+
+{
+  nix.buildMachines = [{
+    hostName = "whitby.tvl.fyi";
+    sshUser = "grfn";
+    sshKey = "/root/.ssh/id_rsa.whitby";
+    system = "x86_64-linux";
+    maxJobs = 64;
+    supportedFeatures = ["big-parallel"];
+  }];
+
+  programs.ssh.knownHosts.whitby = {
+    hostNames = [ "whitby" "whitby.tvl.fyi" "49.12.129.211"];
+    publicKeyFile = pkgs.writeText "whitby.pub" ''
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILNh/w4BSKov0jdz3gKBc98tpoLta5bb87fQXWBhAl2I
+    '';
+  };
+}
