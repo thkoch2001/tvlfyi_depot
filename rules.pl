a08ecbaf17a7bd77b74df6b3209acdf9fb43975a
Vincent Ambo <mail@tazj.in>
2020-07-03 23:59:49 +0100

    feat(rules): Require CI builds to pass for submitting CLs
    We now have an always online CI system (on whitby) and build status no
    longer depends on frog, so it's time to enable this.
    
    Change-Id: I6a6e3d3d9beeae55daa5c325c0ca0477441ad92a

--- rules.pl
+++ rules.pl
@@ -6,6 +6,7 @@
 % - If a codepath has owners, the change must either have a +2 from one
 %   of the owners or be proposed by one of them.
 % - No unresolved comments exist.
+% - The CI build has run successfully.
 % - The commit message conforms to our guidelines.
 
 unresolved_comments(Check) :-
@@ -29,9 +30,13 @@ commit_message(Check) :-
     Check = label('Conformant-Commit-Message', need(_)).
 
 submit_rule(S) :-
-    % Implementation of Gerrit's default code-review policy (+2
-    % required for submit, -2 blocks submit).
+    % Code review with +2 is required, -2 blocks the submit process.
     gerrit:max_with_block(-2, 2, 'Code-Review', ReviewCheck),
+
+    % CI verification is required, broken builds block the submit
+    % process. The `depot-interventions` group can be used by admins
+    % to override the verification status if CI is on fire.
+    gerrit:max_with_block(-1, 1, 'Verified', BuildCheck),
 
     % Check for unresolved comments (this is necessary because it's
     % easy to miss them in previous patch sets)
@@ -46,7 +51,7 @@ submit_rule(S) :-
     findall(U, gerrit:commit_label(label('Code-Review', 2), U), Approvers),
     gerrit:uploader(Uploader),
     gerrit_owners:add_owner_approval([Uploader | Approvers],
-                                     [ReviewCheck, CommentsCheck, CommitCheck],
+                                     [ReviewCheck, BuildCheck, CommentsCheck, CommitCheck],
                                      AllChecks),
 
     S =.. [submit | AllChecks].
