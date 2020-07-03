ca947dfc6011f7bce740ee4e1689afbfc49edc81
Griffin Smith <grfn@gws.fyi>
2020-07-03 12:32:07 -0400

    feat(gs/system): Add whitby to trustedBinaryCaches
    This is needed as well to use it as a substituter it seems
    
    Change-Id: I6cefbccef2a61d665209131084bc58d7b56645f9

--- users/glittershark/system/system/modules/tvl.nix
+++ users/glittershark/system/system/modules/tvl.nix
@@ -3,6 +3,7 @@
 {
   nix = {
     binaryCaches = ["ssh://nix-ssh@whitby.tvl.fyi"];
+    trustedBinaryCaches = ["ssh://nix-ssh@whitby.tvl.fyi"];
     binaryCachePublicKeys = ["cache.tvl.fyi:fd+9d1ceCPvDX/xVhcfv8nAa6njEhAGAEe+oGJDEeoc="];
   };
 
