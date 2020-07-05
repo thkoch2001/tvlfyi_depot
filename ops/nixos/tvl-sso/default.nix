c5e12ca9b45cbb615c5d47aafd23052feab5d411
Luke Granger-Brown <git@lukegb.com>
2020-07-05 19:46:17 +0000

    feat(whitby): add apereo-cas
    Change-Id: I29f5e762852593f05b9936d5635aadcc7eba283e

---
+++ ops/nixos/tvl-sso/default.nix
@@ -0,0 +1,23 @@
+# Configures an Apereo CAS instance for TVL SSO
+{ config, ... }:
+
+let
+  inherit (config.depot.third_party) apereo-cas;
+in {
+  config = {
+    environment.systemPackages = [ apereo-cas ];
+    systemd.services.apereo-cas = {
+      description = "Apereo CAS Single Sign On server";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+      serviceConfig = {
+        User = "apereo-cas";
+        Group = "apereo-cas";
+        ExecStart = "${apereo-cas}";
+        Restart = "always";
+      };
+    };
+    users.users.apereo-cas = {};
+    users.groups.apereo-cas = {};
+  };
+}
