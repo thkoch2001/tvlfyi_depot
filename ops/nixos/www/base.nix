8c9fba24f7cb9e0a67ed418a6514a537b4c1abfe
Vincent Ambo <mail@tazj.in>
2020-07-12 14:30:42 +0100

    fix(www/base): Add nginx fix timer
    Change-Id: Iec66fea0f3991ba74aede3911ea9f6ae5adb0188

--- ops/nixos/www/base.nix
+++ ops/nixos/www/base.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   config = {
@@ -9,6 +9,28 @@
       recommendedTlsSettings = true;
       recommendedGzipSettings = true;
       recommendedProxySettings = true;
+    };
+
+    # NixOS 20.03 broke nginx and I can't be bothered to debug it
+    # anymore, all solution attempts have failed, so here's a
+    # brute-force fix.
+    #
+    # TODO(tazjin): Find a link to the upstream issue and see if
+    # they've sorted it after ~20.09
+    systemd.services.fix-nginx = {
+      script = "${pkgs.coreutils}/bin/chown -R nginx: /var/spool/nginx /var/cache/nginx";
+
+      serviceConfig = {
+        User = "root";
+        Type = "oneshot";
+      };
+    };
+
+    systemd.timers.fix-nginx = {
+      wantedBy = [ "multi-user.target" ];
+      timerConfig = {
+        OnCalendar = "minutely";
+      };
     };
   };
 }
