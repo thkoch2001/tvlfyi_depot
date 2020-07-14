588b9ce679a5ebff9b7b237a2eda6eeea23a5f3e
Griffin Smith <grfn@gws.fyi>
2020-07-14 15:36:19 -0400

    chore(gs/system): reformat tvl.nix
    Change-Id: Icc2f953450956a01b32872e6575976e49f1be0b4

--- users/glittershark/system/system/modules/tvl.nix
+++ users/glittershark/system/system/modules/tvl.nix
@@ -2,15 +2,23 @@
 
 {
   nix = {
-    binaryCaches = ["ssh://nix-ssh@whitby.tvl.fyi"];
-    trustedBinaryCaches = ["ssh://nix-ssh@whitby.tvl.fyi"];
-    binaryCachePublicKeys = ["cache.tvl.fyi:fd+9d1ceCPvDX/xVhcfv8nAa6njEhAGAEe+oGJDEeoc="];
+    binaryCaches = [
+      "ssh://nix-ssh@whitby.tvl.fyi"
+      "https://cache.nixos.org"
+    ];
+    trustedBinaryCaches = [
+      "ssh://nix-ssh@whitby.tvl.fyi"
+      "https://cache.nixos.org"
+    ];
+    binaryCachePublicKeys = [
+      "cache.tvl.fyi:fd+9d1ceCPvDX/xVhcfv8nAa6njEhAGAEe+oGJDEeoc="
+    ];
   };
 
   programs.ssh.knownHosts.whitby = {
     hostNames = [ "whitby" "whitby.tvl.fyi" "49.12.129.211"];
     publicKeyFile = pkgs.writeText "whitby.pub" ''
-    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILNh/w4BSKov0jdz3gKBc98tpoLta5bb87fQXWBhAl2I
+      ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILNh/w4BSKov0jdz3gKBc98tpoLta5bb87fQXWBhAl2I
     '';
   };
 }
