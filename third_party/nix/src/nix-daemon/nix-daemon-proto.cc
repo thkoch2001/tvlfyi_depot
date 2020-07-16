8bcfbc48eb9c4d8c8817e73083653aa9cc71ddfe
Vincent Ambo <mail@tazj.in>
2020-07-16 23:34:01 +0100

    feat(3p/nix/nix-daemon): Implement Worker::IsValidPath handler
    Change-Id: I741c2b9b58f234a21850640e2b0c071ff4441234

---
+++ third_party/nix/src/nix-daemon/nix-daemon-proto.cc
@@ -0,0 +1,30 @@
+#include "libproto/worker.grpc.pb.h"
+#include "libproto/worker.pb.h"
+#include "libstore/store-api.hh"
+
+namespace nix::daemon {
+
+using ::nix::proto::StorePath;
+using ::nix::proto::Worker;
+using ::grpc::Status;
+
+class WorkerServiceImpl final : public Worker::Service {
+ public:
+  WorkerServiceImpl(nix::Store* store) : store_(store) {}
+
+  Status IsValidPath(grpc::ServerContext* context,
+                           const ::nix::proto::StorePath* request,
+                           nix::proto::IsValidPathResponse* response) {
+    const auto& path = request->path();
+    store_->assertStorePath(path);
+    response->set_is_valid(store_->isValidPath(path));
+
+    return Status::OK;
+  }
+
+ private:
+  // TODO(tazjin): Who owns the store?
+  nix::Store* store_;
+};
+
+}  // namespace nix::daemon
