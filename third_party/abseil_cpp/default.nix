52bfbdfdf3ab39556f81d642d83233796d63f7ef
Kane York <kanepyork@gmail.com>
2020-07-18 16:23:33 -0700

    feat(3p/absl): import StatusOr from tensorflow
    Additionally, add tests for the macros. A future CL will enable the tests in CI.
    
    Change-Id: Id4445a1aa65bf6751b87606f37654f3fc6d20efc

--- third_party/abseil_cpp/default.nix
+++ third_party/abseil_cpp/default.nix
@@ -3,12 +3,15 @@
 let inherit (pkgs) cmake llvmPackages;
 in llvmPackages.libcxxStdenv.mkDerivation rec {
   pname = "abseil-cpp";
-  version = "20200519-768eb2ca";
+  version = "20200519-768eb2ca+tvl-1";
   src = ./.;
   nativeBuildInputs = [ cmake ];
+  # TODO: run tests
+  # doCheck = true;
 
   cmakeFlags = [
     "-DCMAKE_CXX_STANDARD=17"
+    #"-DABSL_RUN_TESTS=1"
   ];
 
   meta = with lib; {
