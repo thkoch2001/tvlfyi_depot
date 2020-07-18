e909bf083af3591a1d60655260f37c7576db1bd9
Vincent Ambo <mail@tazj.in>
2020-07-19 00:27:35 +0100

    fix(3p/nix/tests): Fail DummyStore::addToStore on non-existent srcPath
    Unfortunately, to guarantee correct behaviour of some evaluation (!)
    tests, addToStore needs to actually check whether passed in source
    files exist and fail appropriately.
    
    There is a chance that the dependency on this behaviour is actually a
    bug in the upstream test suite, but my attempts at finding out more
    about this from, say, the git history have so far been unsuccessful.
    
    Change-Id: I311999ea28fcedf5da13a4e627b1c1c8e4e59cbd

--- third_party/nix/src/tests/dummy-store.hh
+++ third_party/nix/src/tests/dummy-store.hh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <filesystem>
 #include "libstore/store-api.hh"
 
 namespace nix::tests {
@@ -20,7 +21,13 @@ class DummyStore final : public Store {
                   bool recursive = true, HashType hashAlgo = htSHA256,
                   PathFilter& filter = defaultPathFilter,
                   RepairFlag repair = NoRepair) {
-    return "/nix/store/g1w7hy3qg1w7hy3qg1w7hy3qg1w7hy3q-x";
+    // This check is required because the language tests actually make
+    // a distinction between files that do/don't exist.
+    if (!std::filesystem::exists(srcPath)) {
+      throw SysError("file does not exist");
+    }
+
+    return "/nix/store/jx19wa4xlh9n4324xdl9rjnykd19mmq3-x";
   }
 
   Path addTextToStore(const std::string& name, const std::string& s,
