bc7652d78323831f1b4520533299a8cbc4febcc6
Griffin Smith <grfn@gws.fyi>
2020-07-19 12:27:25 -0400

    chore(gs/keymap): Fix alignment
    Change-Id: If3847fb7133e2da9b6b1c97a76430ee118a15591
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/1286
    Reviewed-by: glittershark <grfn@gws.fyi>
    Tested-by: BuildkiteCI

--- users/glittershark/keyboard/keymap.c
+++ users/glittershark/keyboard/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_EQUAL,       KC_1,           KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
       KC_TAB,         KC_Q,           KC_W,   KC_E,   KC_R,   KC_T,   KC_LALT,
       KC_ESCAPE,      KC_A,           KC_S,   KC_D,   KC_F,   KC_G,
-      KC_RSFT, CTL_T(KC_Z),    KC_X,   KC_C,   KC_V,   KC_B,   KC_TAB,
+      KC_RSFT,        CTL_T(KC_Z),    KC_X,   KC_C,   KC_V,   KC_B,   KC_TAB,
       LT(1,KC_GRAVE), KC_QUOTE,       LALT(KC_LSHIFT),KC_LEFT,KC_RIGHT,
                                         ALT_T(KC_APPLICATION),      LAMBDA,
                                                                     KC_LBRACKET,
