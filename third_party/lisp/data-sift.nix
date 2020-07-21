08397fd7579c32a9a1d9bdd1b33abce9209f7c47
Griffin Smith <grfn@gws.fyi>
2020-07-21 18:53:18 -0400

    feat(3p/lisp): Add many packages
    Add the following packages along with their transitive dependencies:
    - trivial-ldap
    - defclass-std
    - easy-routes
    
    Change-Id: Ib24517f18d14d57540362132c9a24ff2a5a4bd9d
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/1336
    Tested-by: BuildkiteCI
    Reviewed-by: tazjin <mail@tazj.in>
    Reviewed-by: kanepyork <rikingcoding@gmail.com>

---
+++ third_party/lisp/data-sift.nix
@@ -0,0 +1,27 @@
+{ depot, pkgs, ... }:
+
+let
+
+  src = pkgs.fetchFromGitHub {
+    owner = "archimag";
+    repo = "data-sift";
+    rev = "fd617d8200cdcc1b87ecf45ab59bb38e8b16ef7e";
+    sha256 = "1v7gf0x4ibjzp0c56n9m77hxdgwcm9356zlk5n4l3fx4i0hj6146";
+  };
+
+in depot.nix.buildLisp.library {
+  name = "data-sift";
+  deps = with depot.third_party.lisp; [
+    cl-ppcre
+    parse-number
+    alexandria
+    puri
+  ];
+
+  srcs = map (f: src + ("/src/" + f)) [
+    "packages.lisp"
+    "conditions.lisp"
+    "sift.lisp"
+  ];
+
+}
---
+++ third_party/lisp/trivial-ldap.nix
@@ -0,0 +1,22 @@
+{ depot, pkgs, ... }:
+
+let src = pkgs.fetchFromGitHub {
+    owner = "rwiker";
+    repo = "trivial-ldap";
+    rev = "3b8f1ff85f29ea63e6ab2d0d27029d68b046faf8";
+    sha256 = "1zaa4wnk5y5ff211pkg6dl27j4pjwh56hq0246slxsdxv6kvp1z9";
+  };
+in depot.nix.buildLisp.library {
+  name = "trivial-ldap";
+
+  deps = with pkgs.lisp; [
+    usocket
+    cl-plus-ssl
+    cl-yacc
+  ];
+
+  srcs = map (f: src + ("/" + f)) [
+    "package.lisp"
+    "trivial-ldap.lisp"
+  ];
+}
