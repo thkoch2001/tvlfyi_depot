2ebb5dd4e21f1a5d7e1650bb20e610ba28ff2aff
Griffin Smith <grfn@gws.fyi>
2020-07-21 18:59:25 -0400

    feat(web/pannetone): The start of a very simple issue tracker
    Initial commit for Pannetone, a very simple issue tracker for TVL. In
    its current state this launches a web server with authenticates with our
    ldap server, and supports listing and creating issues via static html
    pages and simple forms.
    
    We've been needing an issue tracker for a while now, but none of the
    options out there seem very good - or there are some good ones, but
    they're AGPL licensed and we don't want to deal with them. Rather than
    muck around with Trac or Bugzilla, we've decided to write our own.
    
    Change-Id: I704f0996d15199329bbd5450f3d959046bf13973

---
+++ lisp/klatre/package.lisp
@@ -0,0 +1,9 @@
+(defpackage #:klatre
+  (:documentation "Grab-bag utility library for Common Lisp")
+  (:use #:cl #:iterate)
+  (:export
+   ;; Miscellanious utilities
+   #:comment #:posp
+
+   ;; Sequence functions
+   #:chunk-list #:mapconcat))
