40c1d907477f7c5ef664f1b4443927f9d5326009
Griffin Smith <grfn@gws.fyi>
2020-07-21 18:53:18 -0400

    feat(3p/lisp): Add many packages
    Add the following packages along with their transitive dependencies:
    - trivial-ldap
    - defclass-std
    - easy-routes
    
    Change-Id: Ib24517f18d14d57540362132c9a24ff2a5a4bd9d

---
+++ third_party/lisp/routes.nix
@@ -0,0 +1,28 @@
+{ depot, pkgs, ... }:
+
+let
+
+  src = pkgs.fetchFromGitHub {
+    owner = "archimag";
+    repo = "cl-routes";
+    rev = "1b79e85aa653e1ec87e21ca745abe51547866fa9";
+    sha256 = "1zpk3cp2v8hm50ppjl10yxr437vv4552r8hylvizglzrq2ibsbr1";
+  };
+
+in depot.nix.buildLisp.library {
+  name = "routes";
+
+  deps = with depot.third_party.lisp; [
+    puri
+    iterate
+    split-sequence
+  ];
+
+  srcs = map (f: src + ("/src/" + f)) [
+    "package.lisp"
+    "uri-template.lisp"
+    "route.lisp"
+    "mapper.lisp"
+  ];
+
+}
