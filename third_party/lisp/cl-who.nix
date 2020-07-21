b2441e22e6db150fbdada682cc2fa8dde51d3e66
Griffin Smith <grfn@gws.fyi>
2020-07-20 22:51:41 -0400

    feat(3p/lisp): Add cl-who
    Change-Id: I35fcb65966be2bc63ef534ab0d575aae8beb3cdf

---
+++ third_party/lisp/cl-who.nix
@@ -0,0 +1,21 @@
+{ depot, pkgs, ... }:
+
+let
+
+  src = pkgs.fetchFromGitHub {
+    owner = "edicl";
+    repo = "cl-who";
+    rev = "0d3826475133271ee8c590937136c1bc41b8cbe0";
+    sha256 = "0sc8nji9q1df04lhsiwsjy1a35996bibl31w5hp5sh8q6sa122dy";
+  };
+
+in depot.nix.buildLisp.library {
+  name = "cl-who";
+
+  srcs = map (f: src + ("/" + f)) [
+    "packages.lisp"
+    "specials.lisp"
+    "util.lisp"
+    "who.lisp"
+  ];
+}
