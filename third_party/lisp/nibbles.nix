46f4896e2da5dfe1f229be48701934e84fe31005
Griffin Smith <grfn@gws.fyi>
2020-07-21 14:29:58 -0400

    feat(3p/lisp): Add ironclad
    Add ironclad, a common lisp library for cryptography. This is a huge
    library with a lot of moving parts - probably most notable here is that
    I've had to turn off compiling with `:ironclad-assembly`, as it was
    causing an infinite loop in the compiler due to
    https://github.com/sharplispers/ironclad/blob/master/src/opt/sbcl/cpu-features.lisp#L9-L10,
    a mutually self-recursive function that looks like:
    
        (defun aes-ni-support-p ()
          (aes-ni-support-p))
    
    Without knowing much about how sbcl handles native-compiled assembly, it
    seems like this definition should actually be skipped entirely, due to
    it being defined as a `defknown` in `fndb.lisp`:
    
        (defknown ironclad::aes-ni-support-p
          ()
          (boolean)
          (any)
          :overwrite-fndb-silently t)
    
    But something about how we're compiling things was causing that not to
    happen, and the infinite recursion caused the compiler to hang. This
    should be fixed at some point, but given I only need this library as a
    transitive dependency down a level I'm not going to attempt to do so now.
    
    Change-Id: Id768717991404f959b003c7e2f28f1f4d532b94b

---
+++ third_party/lisp/nibbles.nix
@@ -0,0 +1,27 @@
+{ depot, pkgs, ... }:
+
+let
+  inherit (depot.nix.buildLisp) bundled;
+  src = pkgs.fetchFromGitHub {
+    owner = "froydnj";
+    repo = "nibbles";
+    rev = "9de8c755c2ff24117748a3271e8582bb8d4a6b6c";
+    sha256 = "11rznn33m950mp4zgnpyjaliy3z3rvibfdr8y4vnk2aq42kqi7dj";
+  };
+
+in depot.nix.buildLisp.library {
+  name = "nibbles";
+
+  deps = with depot.third_party.lisp; [
+    (bundled "asdf")
+  ];
+
+  srcs = map (f: src + ("/" + f)) [
+    "package.lisp"
+    "types.lisp"
+    "macro-utils.lisp"
+    "types.lisp"
+    "vectors.lisp"
+    "streams.lisp"
+  ];
+}
