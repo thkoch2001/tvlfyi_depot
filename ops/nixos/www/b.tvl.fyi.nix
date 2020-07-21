ba0a7544c07d323a8210c9a023cecdbcd9e15f61
Griffin Smith <grfn@gws.fyi>
2020-07-21 19:40:49 -0400

    feat(ops/nixos): Deploy Panettone to Whitby
    Deploy Panettone to whitby as a systemd service, proxied to from an
    nginx virtual host listening at b.tvl.fyi
    
    Change-Id: I69755566151a45120e6b3453751af0e9291fa241

---
+++ ops/nixos/www/b.tvl.fyi.nix
@@ -0,0 +1,21 @@
+{ config, ... }:
+
+{
+  imports = [
+    ./base.nix
+  ];
+
+  config = {
+    services.nginx.virtualHosts."b.tvl.fyi" = {
+      serverName = "b.tvl.fyi";
+      enableACME = true;
+      forceSSL = true;
+
+      extraConfig = ''
+        location / {
+          proxy_pass http://localhost:${toString config.services.depot.pannetone.port};
+        }
+      '';
+    };
+  };
+}
