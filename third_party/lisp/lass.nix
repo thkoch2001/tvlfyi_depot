b1c0866037ea63b670055883de88a9fe4e8891e2
Griffin Smith <grfn@gws.fyi>
2020-07-22 16:28:34 -0400

    feat(3p/lisp): Add LASS
    Add LASS, a CSS library
    
    Change-Id: I225959b12210895d0e5baf8a41ac09b0351f0aed
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/1349
    Tested-by: BuildkiteCI
    Reviewed-by: tazjin <mail@tazj.in>

---
+++ third_party/lisp/lass.nix
@@ -0,0 +1,34 @@
+{ depot, pkgs, ... }:
+
+let
+  src = pkgs.fetchFromGitHub {
+    owner = "Shinmera";
+    repo = "LASS";
+    rev = "f51b9e941ee0a2a1f76ba814dcef22f9fb5f69bf";
+    sha256 = "11mxzyx34ynsfsrs8pgrarqi9s442vkpmh7kdpzvarhj7i97g8yx";
+  };
+
+in depot.nix.buildLisp.library {
+  name = "lass";
+
+  deps = with depot.third_party.lisp; [
+    trivial-indent
+    trivial-mimes
+    physical-quantities
+    parse-float
+    cl-base64
+    (depot.nix.buildLisp.bundled "asdf")
+  ];
+
+  srcs = map (f: src + ("/" + f)) [
+    "package.lisp"
+    "readable-list.lisp"
+    "compiler.lisp"
+    "property-funcs.lisp"
+    "writer.lisp"
+    "lass.lisp"
+    "special.lisp"
+    "units.lisp"
+    "asdf.lisp"
+  ];
+}
