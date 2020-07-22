abff2c52f0ea7a772f08f1dbc89a418b44fbe342
Griffin Smith <grfn@gws.fyi>
2020-07-22 16:28:34 -0400

    feat(3p/lisp): Add LASS
    Add LASS, a CSS library
    
    Change-Id: I225959b12210895d0e5baf8a41ac09b0351f0aed

---
+++ third_party/lisp/trivial-indent.nix
@@ -0,0 +1,17 @@
+
+{ depot, pkgs, ... }:
+
+let
+  src = pkgs.fetchFromGitHub {
+    owner = "Shinmera";
+    repo = "trivial-indent";
+    rev = "2d016941751647c6cc5bd471751c2cf68861c94a";
+    sha256 = "1sj90nqz17w4jq0ixz00gb9g5g6d2s7l8r17zdby27gxxh51w266";
+  };
+in depot.nix.buildLisp.library {
+  name = "trivial-indent";
+
+  srcs = map (f: src + ("/" + f)) [
+    "indent.lisp"
+  ];
+}
