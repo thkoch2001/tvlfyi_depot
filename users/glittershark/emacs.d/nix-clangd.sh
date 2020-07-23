9b64cfe93a74a116d8afeac8bb34e21bfe1041e6
Griffin Smith <grfn@gws.fyi>
2020-07-23 12:38:31 -0400

    refactor(gs/emacs): Change nix cmake build dir
    Change-Id: I8d2caebecbaff3c265d8a358b126011b3fa03568

--- users/glittershark/emacs.d/nix-clangd.sh
+++ users/glittershark/emacs.d/nix-clangd.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-CLANGD_FLAGS=--compile-commands-dir=/home/grfn/builds/tazjix \
+CLANGD_FLAGS=--compile-commands-dir=/home/grfn/builds/tvix \
     nix-shell /home/grfn/code/depot \
     -A third_party.nix \
     --run nix-clangd
