660b8d43e5272e2b71b6092b4c879a82c4d861a8
William Carroll <wpcarro@gmail.com>
2020-07-24 19:00:29 +0100

    Support a basic API
    Use Servant to create a REST API supporting the following routes:
    - GET /number
    - POST /other
    
    The server interacts with a SQLite database.

--- shell.nix
+++ shell.nix
@@ -3,6 +3,11 @@ let
 in pkgs.mkShell {
   buildInputs = with pkgs; [
     (haskellPackages.ghcWithPackages (hpkgs: with hpkgs; [
+      hpkgs.servant-server
+      hpkgs.aeson
+      hpkgs.resource-pool
+      hpkgs.sqlite-simple
+      hpkgs.warp
     ]))
   ];
 }
