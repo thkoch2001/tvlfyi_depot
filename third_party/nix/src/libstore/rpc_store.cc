77749edec9a218bfe9902327d90ec90131a70be0
Griffin Smith <grfn@gws.fyi>
2020-07-25 13:49:03 -0400

    feat(3p/nix): Add stub RPC store client
    Add a stub class for wrapping a gRPC client to the new, proto-backed nix
    store protocol, along with one method implemented for the sake of
    demonstration. The plan is to fill all of these in and then rip out
    UDSRemoteStore in favor of this.
    
    Paired-With: Vincent Ambo <mail@tazj.in>
    Paired-With: Perry Lorier <isomer@tvl.fyi>
    Change-Id: Id943d4f6d75084b8498786d580e6c9f7c92c104d

---
+++ third_party/nix/src/libstore/rpc_store.cc
@@ -0,0 +1,218 @@
+#include "rpc_store.hh"
+
+#include <absl/strings/str_cat.h>
+#include <absl/strings/str_format.h>
+#include <grpcpp/create_channel.h>
+#include <grpcpp/impl/codegen/client_context.h>
+#include <grpcpp/impl/codegen/status.h>
+#include <grpcpp/security/credentials.h>
+
+#include "libproto/worker.grpc.pb.h"
+#include "libproto/worker.pb.h"
+
+namespace nix {
+
+namespace store {
+
+using grpc::ClientContext;
+using nix::proto::WorkerService;
+
+proto::StorePath StorePath(const Path& path) {
+  proto::StorePath store_path;
+  store_path.set_path(path);
+  return store_path;
+}
+
+// TODO(grfn): Obviously this should go away and be replaced by StatusOr... but
+// that would require refactoring the entire store api, which we don't feel like
+// doing right now. We should at some point though
+void SuccessOrThrow(const grpc::Status& status) {
+  if (!status.ok()) {
+    throw Error(absl::StrFormat("RPC call failed (%d): %s ",
+                                status.error_code(), status.error_message()));
+  }
+}
+
+bool RPCStore::isValidPathUncached(const Path& path) {
+  ClientContext ctx;
+  proto::IsValidPathResponse resp;
+  SuccessOrThrow(stub_->IsValidPath(&ctx, StorePath(path), &resp));
+  return resp.is_valid();
+}
+
+PathSet RPCStore::queryAllValidPaths() {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+PathSet RPCStore::queryValidPaths(const PathSet& paths,
+                                  SubstituteFlag maybeSubstitute) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+void RPCStore::queryPathInfoUncached(
+    const Path& path,
+    Callback<std::shared_ptr<ValidPathInfo>> callback) noexcept {
+  abort();  // implement me owo
+}
+
+void RPCStore::queryReferrers(const Path& path, PathSet& referrers) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+PathSet RPCStore::queryValidDerivers(const Path& path) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+PathSet RPCStore::queryDerivationOutputs(const Path& path) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+StringSet RPCStore::queryDerivationOutputNames(const Path& path) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+Path RPCStore::queryPathFromHashPart(const std::string& hashPart) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+PathSet RPCStore::querySubstitutablePaths(const PathSet& paths) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+void RPCStore::querySubstitutablePathInfos(const PathSet& paths,
+                                           SubstitutablePathInfos& infos) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+void RPCStore::addToStore(const ValidPathInfo& info, Source& narSource,
+                          RepairFlag repair = NoRepair,
+                          CheckSigsFlag checkSigs = CheckSigs,
+                          std::shared_ptr<FSAccessor> accessor = 0) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+void RPCStore::addToStore(const ValidPathInfo& info,
+                          const ref<std::string>& nar,
+                          RepairFlag repair = NoRepair,
+                          CheckSigsFlag checkSigs = CheckSigs,
+                          std::shared_ptr<FSAccessor> accessor = 0) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+Path RPCStore::addToStore(const std::string& name, const Path& srcPath,
+                          bool recursive = true, HashType hashAlgo = htSHA256,
+                          PathFilter& filter = defaultPathFilter,
+                          RepairFlag repair = NoRepair) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+Path RPCStore::addTextToStore(const std::string& name, const std::string& s,
+                              const PathSet& references,
+                              RepairFlag repair = NoRepair) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+void RPCStore::narFromPath(const Path& path, Sink& sink) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+void RPCStore::buildPaths(const PathSet& paths,
+                          BuildMode buildMode = bmNormal) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+BuildResult RPCStore::buildDerivation(const Path& drvPath,
+                                      const BasicDerivation& drv,
+                                      BuildMode buildMode = bmNormal) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+void RPCStore::ensurePath(const Path& path) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+void RPCStore::addTempRoot(const Path& path) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+void RPCStore::addIndirectRoot(const Path& path) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+void RPCStore::syncWithGC() {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+Roots RPCStore::findRoots(bool censor) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+void RPCStore::collectGarbage(const GCOptions& options, GCResults& results) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+void RPCStore::optimiseStore() {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+bool RPCStore::verifyStore(bool checkContents, RepairFlag repair = NoRepair) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+void RPCStore::addSignatures(const Path& storePath, const StringSet& sigs) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+void RPCStore::computeFSClosure(const PathSet& paths, PathSet& paths_,
+                                bool flipDirection = false,
+                                bool includeOutputs = false,
+                                bool includeDerivers = false) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+void RPCStore::queryMissing(const PathSet& targets, PathSet& willBuild,
+                            PathSet& willSubstitute, PathSet& unknown,
+                            unsigned long long& downloadSize,
+                            unsigned long long& narSize) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+std::shared_ptr<std::string> RPCStore::getBuildLog(const Path& path) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+void RPCStore::connect() { throw absl::StrCat("Not implemented ", __func__); }
+
+unsigned int RPCStore::getProtocol() {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+int RPCStore::getPriority() {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+Path RPCStore::toRealPath(const Path& storePath) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+void RPCStore::createUser(const std::string& userName, uid_t userId) {
+  throw absl::StrCat("Not implemented ", __func__);
+}
+
+}  // namespace store
+
+static std::string uriScheme = "unix://";
+
+// TODO(grfn): Make this a function that we call from main rather than... this
+static RegisterStoreImplementation regStore([](const std::string& uri,
+                                               const Store::Params& params)
+                                                -> std::shared_ptr<Store> {
+  if (std::string(uri, 0, uriScheme.size()) != uriScheme) {
+    return nullptr;
+  }
+  auto channel = grpc::CreateChannel(uri, grpc::InsecureChannelCredentials());
+  return std::make_shared<store::RPCStore>(
+      uri, params, proto::WorkerService::NewStub(channel));
+});
+
+}  // namespace nix
