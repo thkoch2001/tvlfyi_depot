696924b5ef11ca82ba8b66d67a9e85cab04ebf5e
Griffin Smith <grfn@gws.fyi>
2020-07-26 15:31:16 -0400

    feat(web/panettone): Add dev helpers for postgres db
    Add a docker-compose file and lorri-based direnv for aiding in
    running and connecting to a postgres database during development of
    panettone.
    
    Change-Id: I319eee52b52cd48e1f3d2e32c558989768dc19d8
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/1465
    Tested-by: BuildkiteCI
    Reviewed-by: tazjin <mail@tazj.in>
    Reviewed-by: eta <eta@theta.eu.org>

---
+++ web/panettone/shell.nix
@@ -0,0 +1,15 @@
+{ depot ? import ../.. {} }:
+
+with depot.third_party;
+
+mkShell {
+  buildInputs = [
+    docker-compose
+    postgresql
+  ];
+
+  PGPASSWORD = "password";
+  PGHOST = "localhost";
+  PGUSER = "panettone";
+  PGDATABASE = "panettone";
+}
