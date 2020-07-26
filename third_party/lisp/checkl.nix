33cf3fab8321af059ee629a87521036f54d8ffc6
Griffin Smith <grfn@gws.fyi>
2020-07-26 19:55:08 -0400

    feat(3p/lisp): Add checkl
    Change-Id: Ib73ed9637b7e22050727ac5ec117241e18d3cc45

---
+++ third_party/lisp/checkl.nix
@@ -0,0 +1,26 @@
+{ depot, pkgs, ... }:
+
+let
+  inherit (depot.nix.buildLisp) bundled;
+
+  src = pkgs.fetchFromGitHub {
+    owner = "rpav";
+    repo = "CheckL";
+    rev = "80328800d047fef9b6e32dfe6bdc98396aee3cc9";
+    sha256 = "0bpisihx1gay44xmyr1dmhlwh00j0zzi04rp9fy35i95l2r4xdlx";
+  };
+
+in depot.nix.buildLisp.library {
+  name = "checkl";
+  deps = with pkgs.lisp; [
+    (bundled "asdf")
+    marshal
+    fiveam
+  ];
+
+  srcs = map (f: src + ("/" + f)) [
+    "package.lisp"
+    "checkl.lisp"
+    "formalize.lisp"
+  ];
+}
