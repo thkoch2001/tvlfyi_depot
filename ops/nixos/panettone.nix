9c93428e282814703bf405d9e2e7d7a0c370d245
Griffin Smith <grfn@gws.fyi>
2020-07-26 15:41:15 -0400

    feat(ops/nixos): Create a Postgres database for Panettone
    Create a running Postgres database server along with a user and database
    for Panettone as part of its nixos module
    
    Change-Id: I333994288131be328e62069382d6d40f8034c400

--- ops/nixos/panettone.nix
+++ ops/nixos/panettone.nix
@@ -12,10 +12,50 @@ in {
       type = types.int;
       default = 7268;
     };
+
+    dbHost = mkOption {
+      description = "Postgresql host to connect to for Panettone";
+      type = types.string;
+      default = "localhost";
+    };
+
+    dbName = mkOption {
+      description = "Name of the database for Panettone";
+      type = types.string;
+      default = "panettone";
+    };
+
+    dbUser = mkOption {
+      description = "Name of the database user for Panettone";
+      type = types.string;
+      default = "panettone";
+    };
   };
 
   config = lib.mkIf cfg.enable {
     systemd.services.panettone = {
+      assertions = [{
+        assertion =
+          cfg.dbHost != "localhost" || config.services.postgresql.enable;
+        message = "Panettone requires a postgresql database";
+      } {
+        assertion =
+          cfg.dbHost != "localhost" || config.services.postgresql.enableTCPIP;
+        message = "Panettone can only connect to the postgresql database over TCP";
+      } {
+        assertion =
+          cfg.dbHost != "localhost" || (lib.any
+            (user: user.name == cfg.dbUser)
+            config.services.postgresql.ensureUsers);
+        message = "Panettone requires a database user";
+      } {
+        assertion =
+          cfg.dbHost != "localhost" || (lib.any
+            (db: db == cfg.dbName)
+            config.services.postgresql.ensureDatabases);
+        message = "Panettone requires a database";
+      }];
+
       wantedBy = [ "multi-user.target" ];
       script = "${depot.web.panettone}/bin/panettone";
 
@@ -28,6 +68,9 @@ in {
       environment = {
         PANETTONE_PORT = toString cfg.port;
         PANETTONE_DATA_DIR = "/var/lib/panettone";
+        PGHOST = "localhost";
+        PGUSER = cfg.dbUser;
+        PGDATABASE = cfg.dbName;
       };
     };
   };
