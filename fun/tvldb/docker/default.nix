6b830a6fb7333f1627190921bc50031ab658ba13
Vincent Ambo <mail@tazj.in>
2020-07-27 20:45:20 +0100

    chore: Move //fun/tvldb -> //fun/paroxysm
    Say ~my~ its name!
    
    Change-Id: I7890318aef984af0f6bc011de32282f16e01cbb3

--- fun/tvldb/docker/default.nix
+++
@@ -1,7 +0,0 @@
-{ depot, ... }:
-
-depot.third_party.dockerTools.buildLayeredImage {
-  name = "tvldb";
-  contents = [ depot.fun.tvldb ];
-  config.Entrypoint = [ "${depot.fun.tvldb}/bin/paroxysm" ];
-}
