ac090f1f950e41f7ab6dcc2ac559ca23ef035854
Vincent Ambo <mail@tazj.in>
2020-07-28 04:20:50 +0100

    feat(ops): Add 'tvlbot' module using ii
    This adds an IRC bot using ii[0], running on whitby. Essentially this
    exposes the ##tvl channel as files on the filesystem into which we can
    easily hook integrations, such as notifications from various services.
    
    [0]: https://tools.suckless.org/ii/
    
    Change-Id: Id935d117156933af631e82edb30933c2b29216a0

---
+++ ops/nixos/tvlbot.nix
@@ -0,0 +1,75 @@
+# tvlbot is an instance of ii[0], connected to the TVL channel.
+#
+# it can be used by arbitrary programs on the host to send messages to
+# the channel, making it very easy to provide integrations for logging
+# events to the channel and other stuff, like such as that.
+
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    types;
+
+  cfg = config.services.depot.tvlbot;
+
+  stunnelConfig = pkgs.writeText "stunnel-freenode" ''
+    foreground = yes
+    CAfile = ${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
+
+    [freenode]
+    client = yes
+    accept = 127.0.0.1:36633
+    connect = irc.freenode.net:6697
+    verifyChain = yes
+  '';
+in {
+  options.services.depot.tvlbot.enable = mkEnableOption "TVL IRC bot";
+
+  config = mkIf cfg.enable {
+    # ii does not have built-in TLS support, but we can work around that.
+    systemd.services.tvlbot-stunnel = {
+      description = "TLS proxy for tvlbot's ii";
+      script = "${pkgs.stunnel}/bin/stunnel ${stunnelConfig}";
+      wantedBy = [ "multi-user.target" ];
+
+      serviceConfig = {
+        DynamicUser = true;
+        Restart = "always";
+      };
+    };
+
+    systemd.services.tvlbot = {
+      description = "tvlbot via ii";
+      script = "${pkgs.ii}/bin/ii -s 127.0.0.1 -p 36633 -n tvlbot -f 'TVL Bot' -i %S/tvlbot";
+
+      serviceConfig = {
+        Restart = "always";
+        User = "tvlbot";
+        Group = "tvlbot";
+        StateDirectory = "tvlbot";
+        EnvironmentFile = "/etc/secrets/tvlbot";
+      };
+
+      postStart = ''
+        IRCDIR=/var/lib/tvlbot/127.0.0.1
+
+        # wait for connection to be up
+        tail -f  $IRCDIR/out | sed '/End of \/MOTD command/ q'
+
+        # auth to nickserv
+        echo '/j nickserv' > $IRCDIR/in
+        echo 'identify $TVLBOT_NICKSERV' > $IRCDIR/nickserv/in
+
+        # join ##tvl
+        echo '/j ##tvl' > $IRCDIR/in
+      '';
+    };
+
+    users = {
+      users.tvlbot.isNormalUser = false;
+      groups.tvlbot = {};
+    };
+  };
+}
