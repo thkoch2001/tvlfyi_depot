02d25b4fd4dd214e324a350ad44f6be0f60365a7
Vincent Ambo <mail@tazj.in>
2020-07-31 22:30:52 +0100

    feat(ops/nixos): Add module for running paroxysm on whitby
    Change-Id: I415e3b046d4e0fcd7e800ddab0c7f1aeb639c5e2

---
+++ ops/nixos/paroxysm.nix
@@ -0,0 +1,24 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.services.depot.paroxysm;
+  description = "TVL's majestic IRC bot";
+in {
+  options.services.depot.paroxysm.enable = mkEnableOption description;
+
+  config = lib.mkIf cfg.enable {
+    systemd.services.paroxysm = {
+      inherit description;
+
+      environment = {
+        PARX_DATABASE_URL = "postgresql://tvldb:tvldb@localhost/tvldb";
+        PARX_IRC_CONFIG_PATH = "/var/lib/tvldb/irc.conf";
+      };
+
+      serviceConfig = {
+        DynamicUser = true;
+        StateDirectory = "tvldb";
+      };
+    };
+  };
+}
