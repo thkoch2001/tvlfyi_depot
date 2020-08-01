9fa91a26c8560b8f4a3b2da731027e0039a958bf
Luke Granger-Brown <hg@lukegb.com>
2020-08-01 16:03:27 +0000

    chore(gerrit): Update to latest HEAD.
    Change-Id: I650777bbbd24a1922f26967fbbd7da06d14b6786

--- third_party/gerrit_plugins/default.nix
+++ third_party/gerrit_plugins/default.nix
@@ -7,7 +7,7 @@ in
   # https://gerrit.googlesource.com/plugins/owners
   owners = buildGerritBazelPlugin rec {
     name = "owners";
-    depsOutputHash = "0j60yn65kn27s7cjkj3z6irymq7j7rj3q5h3n6xfrs5inm4md2ad";
+    depsOutputHash = "1l99a1qa3bndk1cx4fsrq18z3p7rg6yp9r83snldp97kg25873cs";
     src = pkgs.fetchgit {
       url = "https://gerrit.googlesource.com/plugins/owners";
       rev = "17817c9e319073c03513f9d5177b6142b8fd567b";
@@ -26,11 +26,11 @@ in
   # https://gerrit.googlesource.com/plugins/checks
   checks = buildGerritBazelPlugin {
     name = "checks";
-    depsOutputHash = "1nk63ahlynqn5my7lzy7vsax07n585ndmvbnz2xbwkppl1m7j56l";
+    depsOutputHash = "0igixhkgvd8d51swbypsby553sdzghlj4b9wy9cnf2d8d0aqs2ky";
     src = pkgs.fetchgit {
       url = "https://gerrit.googlesource.com/plugins/checks";
-      rev = "36d9e5c61b95ade98883ded5eb1e168c28ef99bf";
-      sha256 = "03abn4m9x8cinx86c41hrazg0kgwvjfak3w155sca1ikq4605pma";
+      rev = "f508353fc5f93d35d07279857aba5d9cf38de658";
+      sha256 = "094j225cpgckxvnqzk3rif34iylgr0r7v4n6r1m1qymavbk9wg9j";
       deepClone = true;
       leaveDotGit = true;
     };
