6fe99c2d940dadcb76ca5c5401ce0c30ac1ad2aa
Griffin Smith <grfn@gws.fyi>
2020-08-03 20:20:30 -0400

    fix(3p/nix): Properly configure SANDBOX_SHELL
    point the SANDBOX_SHELL macro at the actual path to busybox on the build
    machine, or allow it to be configured at build-time with a cmake option.
    
    Change-Id: I044a1315ba9baa3bc9ceddf29f36d14f9f9ccd96

--- third_party/nix/default.nix
+++ third_party/nix/default.nix
@@ -47,12 +47,15 @@ in lib.fix (self: pkgs.llvmPackages.libcxxStdenv.mkDerivation {
 
  # TODO(tazjin): Some of these might only be required for native inputs
   buildInputs = with pkgs; [
+    busybox # needs to be first so other commands override it
+
     abseil_cpp
     aws-s3-cpp
     brotli
     bzip2
     c-ares
     curl
+    coreutils
     editline
     flex
     glog
@@ -87,7 +90,8 @@ in lib.fix (self: pkgs.llvmPackages.libcxxStdenv.mkDerivation {
       -DCMAKE_BUILD_TYPE=RelWithDebInfo \
       -DCMAKE_FIND_USE_SYSTEM_PACKAGE_REGISTRY=OFF \
       -DCMAKE_FIND_USE_PACKAGE_REGISTRY=OFF \
-      -DCMAKE_EXPORT_NO_PACKAGE_REGISTRY=ON
+      -DCMAKE_EXPORT_NO_PACKAGE_REGISTRY=ON \
+      -DSANDBOX_SHELL=${pkgs.busybox}/bin/busybox
   '';
 
   installCheckPhase = ''
