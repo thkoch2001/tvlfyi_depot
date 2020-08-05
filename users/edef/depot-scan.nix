ca0c2fca408fe91a7592cf73b34a82f4c4526eb7
Kane York <kanepyork@gmail.com>
2020-08-04 17:57:01 -0700

    fix(users/edef/depot-scan): properly depend on pl script
    Change-Id: I83c75ab75dae9c904e127ad6160c7641d4523e40

--- users/edef/depot-scan.nix
+++ users/edef/depot-scan.nix
@@ -1,12 +1,14 @@
 { pkgs, ... }:
 
-pkgs.writeShellScriptBin "depot-scan" ''
+let
+  plScript = ./depot-scan.pl;
+in pkgs.writeShellScriptBin "depot-scan" ''
   set -euo pipefail
 
-  path=$(git rev-parse --show-prefix)
+  path="''${1:-$(git rev-parse --show-prefix)}"
   path="''${path%%/}"
   attr="''${path//\//.}"
   root="$(git rev-parse --show-toplevel)"
   echo "scanning //$path" >&2
-  nix-instantiate "$root" -A "$attr" -vv 2> >(${pkgs.perl}/bin/perl sources.pl) >&2
+  nix-instantiate "$root" -A "$attr" -vv 2> >(${pkgs.perl}/bin/perl ${plScript}) >&2
 ''
