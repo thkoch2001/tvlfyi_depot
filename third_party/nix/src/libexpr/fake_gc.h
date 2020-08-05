997204f4bc29a378b731e9a73732bc97896e0650
Kane York <kanepyork@gmail.com>
2020-08-04 22:16:29 -0700

    feat(3p/nix): add DISABLE_GC flag
    Change-Id: I7c975716d6ba7806e96775978996eac7009a0440

---
+++ third_party/nix/src/libexpr/fake_gc.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#ifdef DISABLE_GC
+
+#include <new>
+#include <memory>
+
+class gc {};
+
+enum GCPlacement
+{
+  GC,
+  NoGC,
+};
+
+// ignore the finalizer variants
+inline void* operator new(size_t size, GCPlacement gcp) {
+  void* obj = malloc(size);
+  if (obj == nullptr) {
+    throw std::bad_alloc();
+  }
+  memset(obj, 0xA4, size);
+  return obj;
+}
+
+template <class GC_Tp>
+using traceable_allocator = std::allocator<GC_Tp>;
+
+template <class GC_Tp>
+using gc_allocator = std::allocator<GC_Tp>;
+
+#define GC_STRDUP(x) strdup(x)
+
+#define GC_word size_t
+
+extern "C" {
+
+void __asan_print_accumulated_stats();
+
+}
+
+#endif
