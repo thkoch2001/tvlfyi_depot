c3838e14300f686b5bc7a0615e6b279d723dc661
edef <edef@edef.eu>
2020-08-05 01:48:16 +0000

    fix(users/edef/depot-scan): properly trace readFile/readDir
    Change-Id: I493e7d20504d34f83a53db50b69e86668874756e

---
+++ users/edef/depot-scan-wrap.nix
@@ -0,0 +1,17 @@
+# this wraps import to override readFile and readDir to trace the files it touches
+# technique inspired by lorri
+{ ... }:
+
+let
+
+  global = {
+    import = global.scopedImport {};
+    scopedImport = x: builtins.scopedImport (global // x);
+    builtins = builtins // {
+      inherit (global) import scopedImport;
+      readFile = path: builtins.trace "depot-scan '${toString path}'" (builtins.readFile path);
+      readDir  = path: builtins.trace "depot-scan '${toString path}'" (builtins.readDir  path);
+    };
+  };
+
+in global.import
