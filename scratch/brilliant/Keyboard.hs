f369274a7742a252aaf839cc52220ca06896c2ba
William Carroll <wpcarro@gmail.com>
2020-08-05 21:52:10 +0100

    Define an instance for Show for a Keyboard
    This will help me debug.

---
+++ scratch/brilliant/Keyboard.hs
@@ -0,0 +1,23 @@
+--------------------------------------------------------------------------------
+module Keyboard where
+--------------------------------------------------------------------------------
+import Utils
+import qualified Data.List as List
+--------------------------------------------------------------------------------
+
+newtype Keyboard = Keyboard [[Char]]
+
+instance Show Keyboard where
+  show (Keyboard xxs) =
+    xxs |> fmap printRow |> List.intercalate "\n"
+    where
+      printRow :: [Char] -> String
+      printRow xs =
+        xs |> fmap (\x -> '[':x:']':"") |> List.intercalate ""
+
+qwerty :: Keyboard
+qwerty = Keyboard [ ['1','2','3','4','5','6','7','8','9','0']
+                  , ['Q','W','E','R','T','Y','U','I','O','P']
+                  , ['A','S','D','F','G','H','J','K','L',';']
+                  , ['Z','X','C','V','B','N','M',',','.','/']
+                  ]
