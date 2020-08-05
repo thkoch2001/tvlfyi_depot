d634b92b3d8955dcf66b7e3fe9006cc5b3e2378d
William Carroll <wpcarro@gmail.com>
2020-08-05 22:54:50 +0100

    Support parsing the list of transforms
    Using Haskell's Text.ParserCombinators.ReadP library for the first time, and I
    enjoyed it thoroughly! It's nice avoiding a third-party library like MegaParsec.

--- scratch/brilliant/Spec.hs
+++ scratch/brilliant/Spec.hs
@@ -4,16 +4,34 @@ module Spec where
 import Test.Hspec
 import Test.QuickCheck
 import Control.Exception (evaluate)
+import Transforms (Transform(..))
+
+import qualified Keyboard
+import qualified Transforms
 --------------------------------------------------------------------------------
 
 main :: IO ()
 main = hspec $ do
-  describe "Prelude.head" $ do
-    it "returns the first element of a list" $ do
-      head [23 ..] `shouldBe` (23 :: Integer)
+  describe "Keyboard.print" $ do
+    it "pretty-prints the keyboard" $ do
+      show Keyboard.qwerty == "[1][2][3][4][5][6][7][8][9][0]\n[Q][W][E][R][T][Y][U][I][O][P]\n[A][S][D][F][G][H][J][K][L][;]\n[Z][X][C][V][B][N][M][,][.][/]"
 
-    it "returns the first element of an arbitrary list" $
-      property $ \x xs -> head (x:xs) == (x :: Integer)
+  describe "Transforms.fromString" $ do
+    it "successfully parses a string of commands" $ do
+      Transforms.fromString "HHVS-12VHVHS3" ==
+        Just [ HorizontalFlip
+             , HorizontalFlip
+             , VerticalFlip
+             , Shift (-12)
+             , VerticalFlip
+             , HorizontalFlip
+             , VerticalFlip
+             , HorizontalFlip
+             , Shift 3
+             ]
 
-    it "throws an exception if used with an empty list" $ do
-      evaluate (head []) `shouldThrow` anyException
+    it "returns Nothing when the input is invalid" $ do
+      Transforms.fromString "potato" == Nothing
+
+    it "return Nothing when the input is valid except for the end" $ do
+      Transforms.fromString "HVS10potato" == Nothing
