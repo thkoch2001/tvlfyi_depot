thkoch2001/tvlfyi_depot

commit 976a36c2e482f416acd79a624e6d96cce2564b5b

chore(3p/nix/hash): eliminate exposed global variable
Change-Id: I3b34e3e17a751e225831ae599c6c6bb782a25679
Reviewed-on: https://cl.tvl.fyi/c/depot/+/1486
Tested-by: BuildkiteCI
Reviewed-by: tazjin <mail@tazj.in>

third_party/nix/src/libutil/hash.cc
@@ -78,6 +78,45 @@ static std::string printHash16(const Hash& hash) {
 // omitted: E O U T
 const std::string base32Chars = "0123456789abcdfghijklmnpqrsvwxyz";
 
+constexpr signed char kUnBase32[] = {
+    -1, -1, -1, -1, -1, -1, -1, -1, /* unprintables */
+    -1, -1, -1, -1, -1, -1, -1, -1, /* unprintables */
+    -1, -1, -1, -1, -1, -1, -1, -1, /* unprintables */
+    -1, -1, -1, -1, -1, -1, -1, -1, /* unprintables */
+    -1, -1, -1, -1, -1, -1, -1, -1, /* SP..' */
+    -1, -1, -1, -1, -1, -1, -1, -1, /* (../ */
+    0,  1,  2,  3,  4,  5,  6,  7,  /* 0..7 */
+    8,  9,  -1, -1, -1, -1, -1, -1, /* 8..? */
+    -1, -1, -1, -1, -1, -1, -1, -1, /* @..G */
+    -1, -1, -1, -1, -1, -1, -1, -1, /* H..O */
+    -1, -1, -1, -1, -1, -1, -1, -1, /* P..W */
+    -1, -1, -1, -1, -1, -1, -1, -1, /* X.._ */
+    -1, 10, 11, 12, 13, -1, 14, 15, /* `..g */
+    16, 17, 18, 19, 20, 21, 22, -1, /* h..o */
+    23, 24, 25, 26, -1, -1, 27, 28, /* p..w */
+    29, 30, 31, -1, -1, -1, -1, -1, /* x..DEL */
+
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* high */
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* high */
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* high */
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* high */
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* high */
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* high */
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* high */
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* high */
+};
+
+bool Hash::IsValidBase32(absl::string_view s) {
+  static_assert(sizeof(kUnBase32) == 256);
+
+  for (char c : s) {
+    if (kUnBase32[static_cast<unsigned char>(c)] == -1) {
+      return false;
+    }
+  }
+  return true;
+}
+
 static std::string printHash32(const Hash& hash) {
   assert(hash.hashSize);
   size_t len = hash.base32Len();

third_party/nix/src/libutil/hash.hh
@@ -14,8 +14,6 @@ const int sha1HashSize = 20;
 const int sha256HashSize = 32;
 const int sha512HashSize = 64;
 
-extern const std::string base32Chars;
-
 enum Base : int { Base64, Base32, Base16, SRI };
 
 struct Hash {
@@ -65,6 +63,10 @@ struct Hash {
      or base-64. By default, this is prefixed by the hash type
      (e.g. "sha256:"). */
   std::string to_string(Base base = Base32, bool includeType = true) const;
+
+  /* Returns whether the passed string contains entirely valid base32
+     characters. */
+  static bool IsValidBase32(absl::string_view s);
 };
 
 /* Print a hash in base-16 if it's MD5, or base-32 otherwise. */

third_party/nix/src/libstore/references.cc
@@ -13,38 +13,21 @@ namespace nix {
 
 static unsigned int refLength = 32; /* characters */
 
-static void search(const unsigned char* s, size_t len, StringSet& hashes,
+static void search(const char* s, size_t len, StringSet& hashes,
                    StringSet& seen) {
-  static bool initialised = false;
-  static bool isBase32[256];
-  if (!initialised) {
-    for (bool& i : isBase32) {
-      i = false;
-    }
-    for (char base32Char : base32Chars) {
-      isBase32[static_cast<unsigned char>(base32Char)] = true;
-    }
-    initialised = true;
-  }
-
   for (size_t i = 0; i + refLength <= len;) {
-    int j = 0;
-    bool match = true;
-    for (j = refLength - 1; j >= 0; --j) {
-      if (!isBase32[s[i + j]]) {
-        i += j + 1;
-        match = false;
-        break;
-      }
-    }
+    absl::string_view ref(s + i, refLength);
+    bool match = Hash::IsValidBase32(ref);
     if (!match) {
       continue;
     }
-    std::string ref(reinterpret_cast<const char*>(s) + i, refLength);
-    if (hashes.find(ref) != hashes.end()) {
+    // TODO(kanepyork): convert StringSet to flat_hash_set, delay owned string
+    // conversion into the 'if'
+    std::string sref(ref);
+    if (hashes.find(sref) != hashes.end()) {
       DLOG(INFO) << "found reference to '" << ref << "' at offset " << i;
-      seen.insert(ref);
-      hashes.erase(ref);
+      seen.insert(sref);
+      hashes.erase(sref);
     }
     ++i;
   }
@@ -65,22 +48,21 @@ struct RefScanSink : Sink {
 void RefScanSink::operator()(const unsigned char* data, size_t len) {
   hashSink(data, len);
 
+  const char* data_ = reinterpret_cast<const char*>(data);
+
   /* It's possible that a reference spans the previous and current
      fragment, so search in the concatenation of the tail of the
      previous fragment and the start of the current fragment. */
-  std::string s = tail + std::string(reinterpret_cast<const char*>(data),
-                                     len > refLength ? refLength : len);
-  search(reinterpret_cast<const unsigned char*>(s.data()), s.size(), hashes,
-         seen);
+  std::string s = tail + std::string(data_, len > refLength ? refLength : len);
+  search(s.data(), s.size(), hashes, seen);
 
-  search(data, len, hashes, seen);
+  search(data_, len, hashes, seen);
 
   size_t tailLen = len <= refLength ? len : refLength;
-  tail =
-      std::string(tail, tail.size() < refLength - tailLen
-                            ? 0
-                            : tail.size() - (refLength - tailLen)) +
-      std::string(reinterpret_cast<const char*>(data) + len - tailLen, tailLen);
+  tail = std::string(tail, tail.size() < refLength - tailLen
+                               ? 0
+                               : tail.size() - (refLength - tailLen)) +
+         std::string(data_ + len - tailLen, tailLen);
 }
 
 PathSet scanForReferences(const std::string& path, const PathSet& refs,

commit 7afbb64e5ef501ceff1eccffe7be6132aa53359c

test(tvix): Cover scanForReferences in a test
Aded a few test cases covering the scanForReferences function, which had
been accidentally broken in 976a36c (which is now partially-reverted).
As part of this, since the test needed to generate hashes for store
paths, the logic in MakeStorePath to compress a sha256 hash down to 20
bytes and convert it to base32 has been extracted to a member function
on the Hash class.

Fixes: #34
Change-Id: Ie2d914688a80f42d0234d351a7cc0714fd15709e

third_party/nix/src/libstore/store-api.cc
@@ -323,9 +323,8 @@ Path Store::makeStorePath(const std::string& type, const Hash& hash,
 
   checkStoreName(name);
 
-  return storeDir + "/" +
-         compressHash(hashString(htSHA256, s), 20).to_string(Base32, false) +
-         "-" + name;
+  return absl::StrCat(storeDir, "/", hashString(htSHA256, s).ToStorePathHash(),
+                      "-", name);
 }
 
 Path Store::makeOutputPath(const std::string& id, const Hash& hash,

third_party/nix/src/libutil/hash.cc
@@ -158,6 +158,10 @@ bool Hash::IsValidBase32(absl::string_view s) {
   return true;
 }
 
+std::string Hash::ToStorePathHash() const {
+  return compressHash(*this, kStorePathHashSize).to_string(Base32, false);
+}
+
 static std::string printHash32(const Hash& hash) {
   assert(hash.hashSize);
   size_t len = hash.base32Len();

third_party/nix/src/libutil/hash.hh
@@ -8,6 +8,9 @@
 
 namespace nix {
 
+// Size of the hashes rendered in store paths, in bytes
+constexpr unsigned int kStorePathHashSize = 20;
+
 MakeError(BadHash, Error);
 
 // TODO(grfn): Replace this with the hash type enum from the daemon proto so we
@@ -90,6 +93,9 @@ struct Hash {
   /* Returns whether the passed string contains entirely valid base32
      characters. */
   static bool IsValidBase32(absl::string_view s);
+
+  // Convert this Hash to the format expected in store paths
+  [[nodiscard]] std::string ToStorePathHash() const;
 };
 
 /* Print a hash in base-16 if it's MD5, or base-32 otherwise. */

third_party/nix/src/nix/hash.cc
@@ -36,8 +36,8 @@ struct CmdHash final : Command {
   void run() override {
     for (const auto& path : paths) {
       Hash h = mode == mFile ? hashFile(ht, path) : hashPath(ht, path).first;
-      if (truncate && h.hashSize > 20) {
-        h = compressHash(h, 20);
+      if (truncate && h.hashSize > nix::kStorePathHashSize) {
+        h = compressHash(h, nix::kStorePathHashSize);
       }
       std::cout << format("%1%\n") % h.to_string(base, base == SRI);
     }

third_party/nix/src/tests/CMakeLists.txt
@@ -30,6 +30,16 @@ target_link_libraries(hash_test
 
 gtest_discover_tests(hash_test)
 
+add_executable(references_test references_test.cc)
+target_link_libraries(references_test
+  nixstore
+  rapidcheck
+  rapidcheck_gtest
+  GTest::gtest_main
+)
+
+gtest_discover_tests(references_test)
+
 add_executable(store_test store_tests.cc)
 target_link_libraries(store_test
   nixstore

third_party/nix/src/tests/references_test.cc
@@ -1,0 +1,88 @@
+#include "libstore/references.hh"
+
+#include <cstdio>
+#include <fstream>
+#include <ostream>
+#include <unordered_set>
+
+#include <absl/strings/str_format.h>
+#include <gtest/gtest.h>
+#include <rapidcheck.h>
+#include <rapidcheck/gtest.h>
+
+#include "generated/parser-tab.hh"
+#include "libutil/hash.hh"
+
+class ReferencesTest : public ::testing::Test {};
+
+namespace nix {
+
+TEST(ReferencesTest, ScanForNoReferences) {
+  char path[] = "store_XXXXXX";
+  auto f = mkstemp(path);
+  ASSERT_EQ(errno, 0);
+
+  HashResult hash;
+  auto result = scanForReferences(path, {}, hash);
+
+  ASSERT_TRUE(result.empty());
+
+  EXPECT_EQ(close(f), 0);
+}
+
+TEST(ReferencesTest, ScanForOneReferenceNotFound) {
+  char path[] = "store_XXXXXXX";
+  auto f = mkstemp(path);
+
+  auto hash = hashString(htSHA256, "foo");
+  auto ref = absl::StrFormat("/nix/store/%s-foo", hash.ToStorePathHash());
+
+  HashResult hr;
+  auto result = scanForReferences(path, {ref}, hr);
+
+  ASSERT_EQ(result.find(ref), result.end());
+
+  EXPECT_EQ(close(f), 0);
+}
+
+TEST(ReferencesTest, ScanForOneReferenceFound) {
+  char path[] = "store_XXXXXXX";
+  auto f = mkstemp(path);
+
+  auto hash = hashString(htSHA256, "foo");
+  auto ref = absl::StrFormat("/nix/store/%s-foo", hash.ToStorePathHash());
+
+  EXPECT_GT(write(f, ref.c_str(), sizeof(char) * ref.size()), 0);
+
+  HashResult hr;
+  auto result = scanForReferences(path, {ref}, hr);
+
+  ASSERT_NE(result.find(ref), result.end());
+
+  ASSERT_EQ(close(f), 0);
+}
+
+RC_GTEST_PROP(ReferencesTest, ScanForReferences,
+              (std::unordered_set<std::string> strs)) {
+  char path[] = "store_XXXXXXX";
+  auto f = mkstemp(path);
+
+  PathSet refs;
+  for (const auto& s : strs) {
+    auto hash = hashString(htSHA256, s);
+    auto ref = absl::StrFormat("/nix/store/%s-foo", hash.ToStorePathHash());
+    refs.insert(ref);
+    EXPECT_GT(write(f, ref.c_str(), sizeof(char) * ref.size()), 0);
+  }
+
+  HashResult hr;
+  auto result = scanForReferences(path, refs, hr);
+
+  for (const auto& ref : refs) {
+    ASSERT_NE(result.find(ref), result.end());
+  }
+
+  EXPECT_EQ(close(f), 0);
+}
+
+}  // namespace nix