e6abf919dd7bc9edd086e6d6046c5b62466e1ff7
William Carroll <wpcarro@gmail.com>
2020-08-12 10:28:04 +0100

    Provide more useful instructions for building this project
    TL;DR:
    - include a default.nix to allow users to build an named executable
    - emphasize in the README that the user needs Nix to build this project
    - pin nixpkgs to a specific commit and fetch it from GitHub

---
+++ scratch/brilliant/default.nix
@@ -0,0 +1,25 @@
+let
+  pkgs = import (builtins.fetchGit {
+    url = "https://github.com/NixOS/nixpkgs-channels";
+    ref = "nixos-20.03";
+    rev = "afa9ca61924f05aacfe495a7ad0fd84709d236cc";
+  }) {};
+
+  ghc = pkgs.haskellPackages.ghcWithPackages (hpkgs: [
+    hpkgs.optparse-applicative
+    hpkgs.unordered-containers
+  ]);
+in pkgs.stdenv.mkDerivation {
+  name = "transform-keyboard";
+  buildInputs = [];
+  src = builtins.path {
+    path = ./.;
+    name = "transform-keyboard-src";
+  };
+  buildPhase = ''
+    ${ghc}/bin/ghc ./Main.hs
+  '';
+  installPhase = ''
+    mkdir -p $out && mv Main $out/transform-keyboard
+  '';
+}
--- scratch/brilliant/shell.nix
+++ scratch/brilliant/shell.nix
@@ -1,5 +1,9 @@
 let
-  pkgs = import /home/wpcarro/nixpkgs {};
+  pkgs = import (builtins.fetchGit {
+    url = "https://github.com/NixOS/nixpkgs-channels";
+    ref = "nixos-20.03";
+    rev = "afa9ca61924f05aacfe495a7ad0fd84709d236cc";
+  }) {};
 in pkgs.mkShell {
   buildInputs = with pkgs; [
     (haskellPackages.ghcWithPackages (hpkgs: with hpkgs; [
