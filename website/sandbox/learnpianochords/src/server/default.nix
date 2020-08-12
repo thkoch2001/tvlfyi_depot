2da4b12266322a3cfb7aa31cc24cfa591eb4e3d0
William Carroll <wpcarro@gmail.com>
2020-08-12 16:28:39 +0100

    Consume buildHaskell functions
    Use the newly defined `buildHaskell` function for a few of my existing Haskell
    projects. So far, it works as intended!

--- website/sandbox/learnpianochords/src/server/default.nix
+++ website/sandbox/learnpianochords/src/server/default.nix
@@ -1,27 +1,27 @@
 let
-  pkgs = import /home/wpcarro/nixpkgs {};
-  ghc = pkgs.haskellPackages.ghcWithPackages (hpkgs: [
-    hpkgs.servant-server
-    hpkgs.aeson
-    hpkgs.wai-cors
-    hpkgs.warp
-    hpkgs.jwt
-    hpkgs.unordered-containers
-    hpkgs.base64
-    hpkgs.http-conduit
-    hpkgs.rio
-  ]);
-in pkgs.stdenv.mkDerivation {
-  name = "LearnPianoChords-server";
+  briefcase = import /home/wpcarro/briefcase {};
+in briefcase.buildHaskell.program {
+  name = "server";
   srcs = builtins.path {
     path = ./.;
     name = "LearnPianoChords-server-src";
   };
-  buildPhase = ''
-    ${ghc} -O Main.hs \
-      -XOverloadedStrings \
-      -XNoImplicitPrelude \
-      -XRecordWildCards \
-      -XTypeApplications
-  '';
+  ghcExtensions = [
+    "OverloadedStrings"
+    "NoImplicitPrelude"
+    "RecordWildCards"
+    "TypeApplications"
+  ];
+  deps = hpkgs: with hpkgs; [
+    servant-server
+    aeson
+    wai-cors
+    warp
+    jwt
+    unordered-containers
+    base64
+    http-conduit
+    rio
+    envy
+  ];
 }
