e5d7f17839eeb21f3efea00f467d75cf55c584b2
William Carroll <wpcarro@gmail.com>
2020-08-13 22:22:39 +0100

    Move shift-time into top-level //zoo
    I'm still unsure whether or not this is a good idea, but experimenting is a good
    way to find out!

--- zoo/default.nix
+++ zoo/default.nix
@@ -1,10 +1,10 @@
-let
-  briefcase = import /home/wpcarro/briefcase {};
-in briefcase.buildHaskell.program {
-  name = "shift-time";
+{ briefcase, ... }:
+
+briefcase.buildHaskell.program {
+  name = "zoo";
   srcs = builtins.path {
     path = ./.;
-    name = "shift-time-src";
+    name = "zoo-src";
   };
   ghcExtensions = [
     "OverloadedStrings"
@@ -15,14 +15,7 @@ in briefcase.buildHaskell.program {
   deps = hpkgs: with hpkgs; [
     servant-server
     aeson
-    wai-cors
     warp
-    jwt
-    unordered-containers
-    base64
-    http-conduit
     rio
-    envy
-    req
   ];
 }
