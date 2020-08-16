6f5659c88b8159bf748c5015df3f79eda3d8f546
Vincent Ambo <mail@tazj.in>
2020-08-17 00:09:09 +0100

    chore(3p): Bump nixos-unstable channel
    This bumps the channel to a commit that includes fixes for an nginx
    issue we have been seeing:
    
    https://github.com/NixOS/nixpkgs/pull/95264
    
    Includes the following compatibility fixes:
    
    - tests disabled in third_party.bufbuild: These were enabled
      unexpectedly by the update, but don't run in the sandbox because
      they want to download things from github
    
    Change-Id: I98a3b5de57f62f1fd3a37701fa1896eddeedff85
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/1759
    Tested-by: BuildkiteCI
    Reviewed-by: glittershark <grfn@gws.fyi>
    Reviewed-by: tazjin <mail@tazj.in>

--- users/glittershark/system/system/modules/kernel.nix
+++ users/glittershark/system/system/modules/kernel.nix
@@ -1,17 +1,33 @@
 { config, lib, pkgs, ... }:
 with lib.versions;
 let
-  inherit (pkgs) runCommand;
+  inherit (pkgs) stdenvNoCC;
   kernelRelease = config.boot.kernelPackages.kernel.version;
   mj = major kernelRelease;
   mm = majorMinor kernelRelease;
-  linux-ck = runCommand "linux-ck-combined.patch" {} ''
-    ${pkgs.xz}/bin/unxz -kfdc ${builtins.fetchurl {
+  patched-linux-ck = stdenvNoCC.mkDerivation {
+    name = "linux-ck";
+    src = builtins.fetchurl {
       # example: http://ck.kolivas.org/patches/5.0/5.4/5.4-ck1/patch-5.4-ck1.xz
       url = "http://ck.kolivas.org/patches/${mj}.0/${mm}/${mm}-ck1/patch-${mm}-ck1.xz";
       sha256 = "01jyg9x2ligr0gjic8lg4f7hw3isz94kqwdbzdk9n8nghklh38p4";
-    }} > $out
-  '';
+    };
+
+    unpackPhase = ''
+      ${pkgs.xz}/bin/unxz -kfdc $src > patch-${mm}-ck1
+    '';
+
+    patches = [
+      (builtins.fetchurl {
+        url = "https://aur.archlinux.org/cgit/aur.git/plain/fix_ck1_for_5.7.14.patch\?h\=linux-ck";
+        sha256 = "0l8f2kph4f2lvcjn0s2fg6n9xa6f4khjz7rqc4zxk58r7fh4s5v4";
+      })
+    ];
+
+    installPhase = ''
+      cp patch-${mm}-ck1 $out
+    '';
+  };
 in
 {
   boot.kernelPackages = pkgs.linuxPackages_latest.extend (self: super: {
@@ -19,7 +35,7 @@ in
       ignoreConfigErrors = true;
       kernelPatches = super.kernel.kernelPatches ++ [{
         name = "linux-ck";
-        patch = linux-ck;
+        patch = patched-linux-ck;
       }];
       argsOverride = {
         modDirVersion = super.kernel.modDirVersion + "-ck1";
