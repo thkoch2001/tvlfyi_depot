7658ee31599b197ce82232afa64d35867faf02c6
Vincent Ambo <mail@tazj.in>
2020-08-17 02:09:01 +0100

    refactor(glittershark/chupacabra): Move boot config into a block
    This is kind of difficult to read otherwise because the boot
    configuration is scattered throughout the file.
    
    Change-Id: I8977b1bd2b9162c898c96aa249c40749b3d46180

--- users/glittershark/system/system/machines/chupacabra.nix
+++ users/glittershark/system/system/machines/chupacabra.nix
@@ -24,16 +24,31 @@
     thresholdPercentage = 5;
   };
 
-  boot.initrd.luks.devices."cryptswap".device = "/dev/disk/by-uuid/3b6e2fd4-bfe9-4392-a6e0-4f3b3b76e019";
+  boot = {
+    blacklistedKernelModules = [ "nouveau" "intel" ];
+    extraModulePackages = [ ];
 
-  boot.kernelParams = [ "acpi_rev_override" ];
+    initrd = {
+      availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+      kernelModules = [ ];
+
+      luks.devices = {
+        "cryptroot".device = "/dev/disk/by-uuid/c2fc7ce7-a45e-48a1-8cde-be966ef601db";
+        "cryptswap".device = "/dev/disk/by-uuid/3b6e2fd4-bfe9-4392-a6e0-4f3b3b76e019";
+      };
+    };
+
+    kernel.sysctl."vm.swappiness" = 1;
+    kernelModules = [ "kvm-intel" ];
+    kernelParams = [ "acpi_rev_override" ];
+  };
+
   services.thermald.enable = true;
 
   hardware.cpu.intel.updateMicrocode = true;
 
   # Intel-only graphics
   hardware.nvidiaOptimus.disable = true;
-  boot.blacklistedKernelModules = [ "nouveau" "intel" ];
   services.xserver.videoDrivers = [ "intel" ];
 
   # Nvidia Optimus (hybrid) - currently not working
@@ -55,17 +70,10 @@
 
   # From hardware-configuration.nix
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
-
   fileSystems."/" =
     { device = "/dev/mapper/cryptroot";
       fsType = "ext4";
     };
-
-  boot.initrd.luks.devices."cryptroot".device = "/dev/disk/by-uuid/c2fc7ce7-a45e-48a1-8cde-be966ef601db";
 
   fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/3492-9E3A";
@@ -83,7 +91,6 @@
 
   # from nixos-hardware TODO sort this around
   services.tlp.enable = true;
-  boot.kernel.sysctl."vm.swappiness" = 1;
   services.fstrim.enable = lib.mkDefault true;
 
   # Intel cpu stuff
