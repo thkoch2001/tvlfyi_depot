0e2698513c1fdb6ea243d09661ed1d43123481ee
Vincent Ambo <mail@tazj.in>
2020-08-17 02:10:00 +0100

    feat(glittershark/kernel): Use linuxPackages_latest
    Welcome to the future, baby!
    
    Change-Id: I23124d27df1f7597dbd2b28afd5882e62085665e

--- users/glittershark/system/system/modules/kernel.nix
+++ users/glittershark/system/system/modules/kernel.nix
@@ -2,19 +2,19 @@
 with lib.versions;
 let
   inherit (pkgs) runCommand;
-  kernelRelease = config.linuxPackages.kernel.version or pkgs.linux.version;
+  kernelRelease = config.boot.kernelPackages.kernel.version;
   mj = major kernelRelease;
   mm = majorMinor kernelRelease;
   linux-ck = runCommand "linux-ck-combined.patch" {} ''
     ${pkgs.xz}/bin/unxz -kfdc ${builtins.fetchurl {
-      # http://ck.kolivas.org/patches/5.0/5.4/5.4-ck1/patch-5.4-ck1.xz
+      # example: http://ck.kolivas.org/patches/5.0/5.4/5.4-ck1/patch-5.4-ck1.xz
       url = "http://ck.kolivas.org/patches/${mj}.0/${mm}/${mm}-ck1/patch-${mm}-ck1.xz";
-      sha256 = "0p2ccwlsmq0587x6cnbrk4h2bwpl9342bmhsbyi1a87cs2jfwigl";
+      sha256 = "01jyg9x2ligr0gjic8lg4f7hw3isz94kqwdbzdk9n8nghklh38p4";
     }} > $out
   '';
 in
 {
-  boot.kernelPackages = pkgs.linuxPackages.extend (self: super: {
+  boot.kernelPackages = pkgs.linuxPackages_latest.extend (self: super: {
     kernel = super.kernel.override {
       ignoreConfigErrors = true;
       kernelPatches = super.kernel.kernelPatches ++ [{
