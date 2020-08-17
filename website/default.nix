6ac189cb9648fff52322ff5f2b6d41ae7dbe675b
William Carroll <wpcarro@gmail.com>
2020-08-17 10:25:26 +0100

    Disable failing goals/default.nix
    Disabling failing packages until I can get a working CI build.

--- website/default.nix
+++ website/default.nix
@@ -7,9 +7,6 @@ pkgs.stdenv.mkDerivation {
     mkdir -p $out
     cp $src/index.html $out
 
-    mkdir -p $out/goals
-    cp -r ${briefcase.website.goals}/* $out/goals
-
     mkdir -p $out/habits
     cp ${briefcase.website.habits} $out/habits/index.html
   '';
