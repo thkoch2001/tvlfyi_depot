d6f17f48de91538c5b3623484672c1a81d207cba
V <v@anomalous.eu>
2020-08-19 03:12:28 +0200

    chore(tvl-slapd): decapitalise V's username, use displayName instead
    Change-Id: I59cf5e1c850960ae639c6a3ebeb273a4441c48bb
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/1788
    Tested-by: BuildkiteCI
    Reviewed-by: kanepyork <rikingcoding@gmail.com>
    Reviewed-by: tazjin <mail@tazj.in>

--- ops/nixos/tvl-slapd/default.nix
+++ ops/nixos/tvl-slapd/default.nix
@@ -118,7 +118,8 @@ let
       password = "{ARGON2}$argon2id$v=19$m=65536,t=2,p=1$SHRFps5sVgyUXYdmqGPw9g$tEx9DwKK1RjWlw52GLwOZ/iHep+QJboaZE83f1pXSwQ";
     }
     {
-      username = "V";
+      username = "v";
+      displayName = "V";
       email = "v@anomalous.eu";
       password = "{ARGON2}$argon2id$v=19$m=65536,t=2,p=1$Wa11vk3gQKhJr1uzvtRTRQ$RHfvcC2j6rDUgWfezm05N03LeGIEezeKtmFmt+rfvM4";
     }
