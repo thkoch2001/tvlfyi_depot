0c6d90b50411b431891da5d208fa465d93749323
William Carroll <wpcarro@gmail.com>
2020-08-21 18:28:06 +0100

    Ensure that the build step "depends on" the lint step
    This way, if the lint step fails, the build step doesn't run. Nice!

--- ci/pipelines/briefcase.nix
+++ ci/pipelines/briefcase.nix
@@ -3,12 +3,15 @@
 let
   pipeline.steps = [
     {
+      key = "lint";
       command = "${pkgs.git-secrets}/bin/git-secrets --scan-history";
       label = ":broom: lint";
     }
     {
+      key = "build";
       command = "nix-build . -I briefcase=$(pwd) --no-out-link --show-trace";
       label = ":nix: build";
+      depends_on = "lint";
     }
   ];
 in pkgs.writeText "briefcase.yaml" (builtins.toJSON pipeline)
