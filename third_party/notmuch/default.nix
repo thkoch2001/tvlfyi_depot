2991be8e95c6fa1bca268416dee866b0ca9e2520
Vincent Ambo <mail@tazj.in>
2020-08-26 23:55:32 +0100

    chore: Add meta.ci=false to various broken projects
    These projects, which are not currently included in CI runs, don't
    build at the moment.
    
    Upcoming logic changes would mean that we would start including them
    in CI, which is undesirable until they're fixed - but I'm not going to
    be doing that now.
    
    Change-Id: I7c337e098be8bff00db6d99fc7236a695f5a85f5

--- third_party/notmuch/default.nix
+++ third_party/notmuch/default.nix
@@ -3,4 +3,7 @@
 pkgs.originals.notmuch.overrideAttrs(old: {
   doCheck = false;
   patches = [ ./dottime.patch ] ++ (if old ? patches then old.patches else []);
+
+  # TODO(tazjin): Fix the build and re-enable CI.
+  meta.ci = false;
 })
