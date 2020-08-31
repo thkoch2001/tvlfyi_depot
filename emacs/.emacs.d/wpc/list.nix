b475f7166ea13593ce0e0eb30d57e0463268196f
William Carroll <wpcarro@gmail.com>
2020-08-31 15:48:25 +0100

    Delete unused .nix expressions
    These were never more than scratch buffers that I used when I was feeling around
    in the dark trying to learn Nix.

--- emacs/.emacs.d/wpc/list.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import (builtins.fetchTarball
-  "https://github.com/tazjin/depot/archive/master.tar.gz") {} }:
-
-pkgs.writeElispBin {
-  name = "list";
-  deps = epkgs: [ epkgs.dash ./prelude.nix ];
-  src = ./list.el;
-}
--- emacs/.emacs.d/wpc/string.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import (builtins.fetchTarball
-  "https://github.com/tazjin/depot/archive/master.tar.gz") {} }:
-
-pkgs.writeElispBin {
-  name = "string";
-  deps = epkgs: [ epkgs.dash epkgs.s ./prelude.nix ];
-  src = ./string.el;
-}
