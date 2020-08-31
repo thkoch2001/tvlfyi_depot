4472e32397be67f7274429be7a5622220b637036
William Carroll <wpcarro@gmail.com>
2020-08-31 14:42:06 +0100

    Lint vector.el
    - add Version, URL, Package-Requires sections
    - prefer `vector-` prefix to `vector/`

--- emacs/.emacs.d/wpc/vector.el
+++ emacs/.emacs.d/wpc/vector.el
@@ -1,5 +1,9 @@
 ;;; vector.el --- Working with Elisp's Vector data type -*- lexical-binding: t -*-
+
 ;; Author: William Carroll <wpcarro@gmail.com>
+;; Version: 0.0.1
+;; URL: https://git.wpcarro.dev/wpcarro/briefcase
+;; Package-Requires: ((emacs "25.1"))
 
 ;;; Commentary:
 ;; It might be best to think of Elisp vectors as tuples in languages like
@@ -25,12 +29,12 @@
 ;; Library
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defconst vector/enable-tests? t
+(defconst vector-enable-tests? t
   "When t, run the tests defined herein.")
 
-;; TODO: Consider labelling variadic functions like `vector/concat*'
-;; vs. `vector/concat'.
-(defun vector/concat (&rest args)
+;; TODO: Consider labelling variadic functions like `vector-concat*'
+;; vs. `vector-concat'.
+(defun vector-concat (&rest args)
   "Return a new vector composed of all vectors in `ARGS'."
   (apply #'vconcat args))
 
@@ -38,39 +42,39 @@
 ;; (definstance monoid vector
 ;;   :empty (lambda () []))
 
-(defun vector/prepend (x xs)
+(defun vector-prepend (x xs)
   "Add `X' to the beginning of `XS'."
-  (vector/concat `[,x] xs))
+  (vector-concat `[,x] xs))
 
-(defun vector/append (x xs)
+(defun vector-append (x xs)
   "Add `X' to the end of `XS'."
-  (vector/concat xs `[,x]))
+  (vector-concat xs `[,x]))
 
-(defun vector/get (i xs)
+(defun vector-get (i xs)
   "Return the value in `XS' at index, `I'."
   (aref xs i))
 
-(defun vector/set (i v xs)
+(defun vector-set (i v xs)
   "Set index `I' to value `V' in `XS'.
 Returns a copy of `XS' with the updates."
   (let ((copy (vconcat [] xs)))
     (aset copy i v)
     copy))
 
-(defun vector/set! (i v xs)
+(defun vector-set! (i v xs)
   "Set index `I' to value `V' in `XS'.
 This function mutates XS."
   (aset xs i v))
 
-(when vector/enable-tests?
+(when vector-enable-tests?
   (let ((xs [1 2 3])
         (ys [1 2 3]))
-    (prelude/assert (= 1 (vector/get 0 ys)))
-    (vector/set 0 4 ys)
-    (prelude/assert (= 1 (vector/get 0 ys)))
-    (prelude/assert (= 1 (vector/get 0 xs)))
-    (vector/set! 0 4 xs)
-    (prelude/assert (= 4 (vector/get 0 xs)))))
+    (prelude/assert (= 1 (vector-get 0 ys)))
+    (vector-set 0 4 ys)
+    (prelude/assert (= 1 (vector-get 0 ys)))
+    (prelude/assert (= 1 (vector-get 0 xs)))
+    (vector-set! 0 4 xs)
+    (prelude/assert (= 4 (vector-get 0 xs)))))
 
 ;; TODO: Decide between "remove" and "delete" as the appropriate verbs.
 ;; TODO: Implement this.
