cd497e61dab31fdb8a24c88c4022ebc04c6553bf
William Carroll <wpcarro@gmail.com>
2020-09-02 14:18:50 +0100

    Increase assertiveness of init-emacs script.el
    TL;DR:
    - Assert that the path to the init.el exists
    - Check *Errors* buffer in case an error is uncaught but logged
    - Log a message when Emacs successfully initializes

--- ci/pipelines/script.el
+++ ci/pipelines/script.el
@@ -8,21 +8,36 @@
 ;; Dependencies
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(require 'prelude)
+(require 'f)
 (require 'dash)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Script
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defvar init-el-path (-last-item argv)
+  "Path to the init.el file that this script attempts to load.")
+
+(prelude-assert (f-exists? init-el-path))
+
 (condition-case err
-    (load (-last-item argv))
+    (load init-el-path)
   (error
    (message "Encountered an error while attempting to load init.el: %s" err)
    (kill-emacs 1)))
 
-(if (bufferp "*Warnings*")
-    (progn
-      (with-current-buffer "*Warnings*"
-        (message "Encountered warnings in *Warnings* buffer: %s" (buffer-string)))
-      (kill-emacs 1))
-  (kill-emacs 0))
+(when (bufferp "*Errors*")
+  (progn
+    (with-current-buffer "*Errors*"
+      (message "Encountered errors in *Errors* buffer: %s" (buffer-string)))
+    (kill-emacs 1)))
+
+(when (bufferp "*Warnings*")
+  (progn
+    (with-current-buffer "*Warnings*"
+      (message "Encountered warnings in *Warnings* buffer: %s" (buffer-string)))
+    (kill-emacs 1)))
+
+(message "Successfully init'd Emacs without encountering errors or warnings!")
+(kill-emacs 0)
