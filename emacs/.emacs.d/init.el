cbbe21d531f576022943c3e48110928d1ce0d0de
William Carroll <wpcarro@gmail.com>
2020-09-02 14:09:15 +0100

    General Elisp linting
    TL;DR:
    - Remove `require` statements from init.el
    - Remove unused, auto-install KBDs for bookmark.el
    - Remove unused `require` statements from clipboard
    - Remove unused, commented-out code
    
    I would like for an Elisp linting stage to test for unused `require` statements,
    but I'm unsure how practical that is to support.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,42 +1,16 @@
-(require 'wpc-package)
-
 ;; load order is intentional
-(require 'constants)
+(require 'wpc-package)
 (require 'wpc-misc)
-
-;; my libraries
-(require 'functions)
-(require 'prelude)
-(require 'macros)
-
-;; Laptop XF-functionality
-(require 'pulse-audio)
-(require 'screen-brightness)
-
-;; miscellaneous
 (require 'ssh)
-(require 'clipboard)
-(require 'battery)
-(require 'bookmark)
 (require 'keyboard)
 (require 'irc)
 (require 'email)
-(require 'scrot)
-(require 'timestring)
-
-;; TODO: Remove path once published to MELPA.
-;; TODO: How can I package this using Nix?
-;; (require 'egg-timer "~/programming/egg-timer.el/egg-timer.el")
-
 (require 'keybindings)
 (require 'window-manager)
 (require 'wpc-ui)
 (require 'wpc-dired)
 (require 'wpc-org)
 (require 'wpc-company)
-;; TODO: Re-enable flycheck for all languages besides Elisp once I learn more
-;; about the issue with the `emacs-lisp' `flycheck-checker'.
-;; (require 'wpc-flycheck)
 (require 'wpc-shell)
 (require 'wpc-lisp)
 (require 'wpc-haskell)
