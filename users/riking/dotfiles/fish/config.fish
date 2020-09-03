d82f08490efce2b0cc1a12458962d199da939589
Kane York <kanepyork@gmail.com>
2020-09-03 14:42:24 -0700

    fix(u/riking/dotfiles): source rbenv config correctly
    Change-Id: I1cf3dfc2d1edd2cb97ca432de5eb03c434b566e3

--- users/riking/dotfiles/fish/config.fish
+++ users/riking/dotfiles/fish/config.fish
@@ -3,6 +3,6 @@ set -gx GPG_TTY (tty)
 set -gx DEPOT_ROOT "$GOPATH/src/code.tvl.fyi"
 
 set -gx PATH '/usr/local/go/bin' "$HOME/.cargo/bin" "$HOME/.rbenv/bin" $PATH
-eval (rbenv init -)
+status --is-interactive; and rbenv init - | source
 source ~/.opsrc.fish # work
 set -gx PATH "$HOME/go/bin" $PATH
