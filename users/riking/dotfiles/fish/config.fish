a14090054d8826c06bb49470cb3f3feb8cc10bbc
Kane York <kanepyork@gmail.com>
2020-09-03 14:28:15 -0700

    feat(u/riking/dotfiles): port non-functions of bashrc to fish
    Change-Id: I5fff28f67fe721b7662c5931e3b1b2226644598a

---
+++ users/riking/dotfiles/fish/config.fish
@@ -0,0 +1,10 @@
+
+set -gx GOPATH "$HOME/go"
+set -gx GPG_TTY (tty)
+set -gx DEPOT_ROOT "$GOPATH/src/code.tvl.fyi"
+
+set -gx PATH '/usr/local/go/bin' "$HOME/.cargo/bin" "$HOME/.rbenv/bin" $PATH
+eval (rbenv init -)
+source ~/.opsrc.fish # work
+set -gx PATH "$HOME/go/bin" $PATH
+
