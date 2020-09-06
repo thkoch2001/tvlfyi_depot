02cd8150b759a3674792a8088cc6985e980c14d8
Vincent Ambo <mail@tazj.in>
2020-09-06 18:12:10 +0100

    chore(tazjin/keys): Add S10e key
    Change-Id: I3efdd49e23bdc357f15ea189b890e25843cc0255

--- users/tazjin/keys.nix
+++ users/tazjin/keys.nix
@@ -4,6 +4,7 @@
 rec {
   frog = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKMZzRdcrHTuCPoaFy36MPr5IW/hnImlse/OBOn6udL/ tazjin@frog";
   vauxhall = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBHs+9QfZTD5qGsBQaWqp5whmXJ9qy/m9swE2M9QBaIQVoIYGemq3HXTzrQ6XekwudJCltP4EpM7h/Qc+Or309Yw=";
+  s10e = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDf7CNlYoauHcSYsMNnCZt5h9QSYH/7keYkg8g3hT32+";
 
-  all = [ frog vauxhall ];
+  all = [ frog vauxhall s10e ];
 }
