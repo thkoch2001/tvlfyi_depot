88a683e4080c60bd75c495557fb8e2d52932aac2
William Carroll <wpcarro@gmail.com>
2020-09-07 19:48:50 +0100

    Display all byte-compile-warnings
    I've reconsidered; setting this to just '(cl-functions) is not what I want.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,9 +1,3 @@
-;; This suppresses the warning about Emacs 27 deprecating the cl library in
-;; favor of cl-lib.
-;; See this thread for more details:
-;; https://github.com/kiwanami/emacs-epc/issues/35
-(setq byte-compile-warnings '(cl-functions))
-
 ;; load order is intentional
 (require 'wpc-package)
 (require 'wpc-misc)
