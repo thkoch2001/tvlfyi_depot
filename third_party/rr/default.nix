4f7bb40a31bb9b7776abc4e83cfca052980e183c
Vincent Ambo <mail@tazj.in>
2020-09-20 19:51:56 +0100

    chore(3p/rr): Upgrade to latest master with Zen workarounds
    * bumps to HEAD of master
    * include the zen_workaround script in the drv output
    
    Change-Id: I8f5f33a61b9395295c02f3ea5e930a81b3ffa7ac
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/1990
    Tested-by: BuildkiteCI
    Reviewed-by: kanepyork <rikingcoding@gmail.com>
    Reviewed-by: glittershark <grfn@gws.fyi>

--- third_party/rr/default.nix
+++ third_party/rr/default.nix
@@ -1,7 +1,16 @@
 { pkgs, ... }:
 
 pkgs.originals.rr.overrideAttrs(_: {
-  patches = [
-    ./0001-PerfCounters-don-t-exit-on-AMD-ThreadRipper-3960X.patch
-  ];
+  src = pkgs.fetchFromGitHub {
+    owner = "mozilla";
+    repo = "rr";
+    rev = "8fc7d2a09a739fee1883d262501e88613165c1dd";
+    sha256 = "0avq5lv082z2sasggfn2awnfrh08cr8f0i9iw1dnrcxa6pc3bi9k";
+    fetchSubmodules = false;
+  };
+
+  # Workaround as documented on https://github.com/mozilla/rr/wiki/Zen
+  postInstall = ''
+    cp $src/scripts/zen_workaround.py $out/bin/rr_zen_workaround
+  '';
 })
