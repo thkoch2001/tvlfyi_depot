e43620659ed575bcff19d18d11c8545c8d504cce
Griffin Smith <grfn@gws.fyi>
2020-09-23 11:12:33 -0400

    fix(gs/emacs): Set notmuch saved searches at the top-level
    For some reason having these in a with-eval-after-load isn't working, so
    let's try this out.
    
    Change-Id: Ia419962626c7bd26776ed6fde977698998a18155
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/1998
    Reviewed-by: glittershark <grfn@gws.fyi>
    Tested-by: BuildkiteCI

--- users/glittershark/emacs.d/email.el
+++ users/glittershark/emacs.d/email.el
@@ -14,3 +14,15 @@
         message-send-mail-function 'message-send-mail-with-sendmail)
 
   (add-hook! notmuch-message-mode-hook #'notmuch-company-setup))
+
+(setq notmuch-saved-searches
+        '((:name "inbox" :query "tag:inbox tag:important not tag:trash" :key "i")
+          (:name "flagged" :query "tag:flagged" :key "f")
+          (:name "sent" :query "tag:sent" :key "s")
+          (:name "drafts" :query "tag:draft" :key "d")
+
+          (:name "work" :query "tag:inbox and tag:important and path:work/**"
+                 :key "w")
+          (:name "personal" :query "tag:inbox and tag:important and path:personal/**"
+                 :key "p"))
+        message-send-mail-function 'message-send-mail-with-sendmail)
