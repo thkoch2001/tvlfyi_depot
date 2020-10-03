0f160a8029b5f353dc803f4f88df08058535d22d
William Carroll <wpcarro@gmail.com>
2020-10-03 11:00:11 +0100

    Ignore comments in output for grocery export
    TL;DR:
    - Ignore lines starting with "#"
    - Tidy up the code

--- scratch/groceries/export.hs
+++ scratch/groceries/export.hs
@@ -1,11 +1,22 @@
 module Main where
 
-import Data.Function ((&))
 import qualified Data.List as L
+
+(|>) :: a -> (a -> b) -> b
+x |> f = f x
+
+-- | Ignore items with zero quantity (i.e. "0x") and comments (i.e. "#")
+isUndesirableOutput :: String -> Bool
+isUndesirableOutput x =
+  (L.isPrefixOf "- 0x" x) || (L.isPrefixOf "#" x)
 
 -- | Run this to export the grocery list.
 main :: IO ()
 main = do
-  x <- readFile "./list.org"
-  x & lines & filter (not . L.isPrefixOf "- 0x") & unlines & putStrLn
+  content <- readFile "./list.org"
+  content
+    |> lines
+    |> filter (not . isUndesirableOutput)
+    |> unlines
+    |> putStrLn
   pure ()
