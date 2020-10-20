57a5f3c611e20463a1a88a298636e55957f98d1f
Ben Cartwright-Cox <tvl@Benjojo.co.uk>
2020-10-20 22:53:37 +0100

    Update the ebooks code to bring in line with what's running
    Change-Id: I1055992aab3a06aa1f4ba937fc3ef45f2f78cedc

--- fun/tvl-ebooks/mkov-engine/main.go
+++ fun/tvl-ebooks/mkov-engine/main.go
@@ -21,6 +21,7 @@ type incomingIRC struct {
 }
 
 var supressionUsernames map[string]bool
+var noMkov map[string]bool
 
 func main() {
 	redisc := redis.NewClient(&redis.Options{
@@ -31,6 +32,26 @@ func main() {
 
 	fireaway := make(chan incomingIRC, 10)
 	supressionUsernames = make(map[string]bool)
+
+	supressionList := redisc.HGetAll("supressionList")
+	supressionListA, _ := supressionList.Result()
+
+	supressionListMap, _ := stringMaptoIntMap(supressionListA)
+	for v, _ := range supressionListMap {
+		supressionUsernames[v] = true
+		supressionUsernames[strings.ToLower(v)] = true
+	}
+
+	noMkov = make(map[string]bool)
+
+	noMkovRedis := redisc.HGetAll("nomkov")
+	noMkovRedisA, _ := noMkovRedis.Result()
+
+	noMkovMap, _ := stringMaptoIntMap(noMkovRedisA)
+	for v, _ := range noMkovMap {
+		noMkov[v] = true
+		noMkov[strings.ToLower(v)] = true
+	}
 
 	go func() {
 		for {
@@ -83,6 +104,9 @@ func generateMesasge(msg incomingIRC, redisc *redis.Client) string {
 	username := strings.ToLower(msg.Name)
 	supressionUsernames[username] = true
 	supressionUsernames[username+":"] = true
+	supressionUsernames[msg.Name] = true
+	supressionUsernames[msg.Name+":"] = true
+	redisc.HIncrBy("supressionList", msg.Name, 1)
 
 	text = strings.ToLower(text)
 	text = strings.Replace(text, ",", "", -1)
@@ -92,7 +116,12 @@ func generateMesasge(msg incomingIRC, redisc *redis.Client) string {
 	text = strings.Replace(text, "?", "", -1)
 
 	words := strings.Split(text, " ")
-	lastWord := propwords(username, words[0], redisc)
+	lastWord := propwords(msg.Name, words[0], redisc)
+
+	if noMkov[username] {
+		lastWord = blockoutWord(lastWord)
+		words[0] = blockoutWord(words[0])
+	}
 
 	if supressionUsernames[words[0]] {
 		if len(words[0]) < 2 {
@@ -102,6 +131,9 @@ func generateMesasge(msg incomingIRC, redisc *redis.Client) string {
 	}
 
 	if lastWord == "_END_" {
+		if noMkov[username] {
+			return blockoutWord(words[0])
+		}
 		return words[0]
 	}
 	outputMsg := words[0] + " " + lastWord + " "
@@ -110,6 +142,10 @@ func generateMesasge(msg incomingIRC, redisc *redis.Client) string {
 		lastWord = propwords(username, lastWord, redisc)
 		if lastWord == "" || lastWord == "_END_" {
 			return outputMsg
+		}
+
+		if noMkov[username] {
+			lastWord = blockoutWord(lastWord)
 		}
 
 		if supressionUsernames[lastWord] {
@@ -124,6 +160,14 @@ func generateMesasge(msg incomingIRC, redisc *redis.Client) string {
 			return outputMsg
 		}
 	}
+}
+
+func blockoutWord(in string) string {
+	block := ""
+	for i := 0; i < len(in); i++ {
+		block += "█"
+	}
+	return block
 }
 
 func propwords(username string, start string, redisc *redis.Client) string {
@@ -190,7 +234,9 @@ func learnFromMessage(msg incomingIRC, redisc *redis.Client) {
 			nextWord = words[k+1]
 		}
 
-		redisc.HIncrBy(fmt.Sprintf("%s-%s", username, word), nextWord, 1)
+		if !noMkov[username] {
+			redisc.HIncrBy(fmt.Sprintf("%s-%s", username, word), nextWord, 1)
+		}
 		redisc.HIncrBy(fmt.Sprintf("generic-%s", word), nextWord, 1)
 	}
 }
