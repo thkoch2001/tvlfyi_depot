3a082e7bb2174219856e3e16df335750ae30aaf2
Vincent Ambo <mail@tazj.in>
2020-11-08 02:41:34 +0100

    feat(ops/irccat): Add a NixOS module for launching irccat
    This module configures irccat by creating a JSON configuration file
    from a user-supplied Nix struct (this is not checked for correctness),
    and merging it recursively with secrets from
    `/etc/secrets/irccat.json` at service launch time.
    
    This way we get the ability to configure (most) options declaratively
    via Nix, while providing the secrets outside of Nix.
    
    Side note: We need to figure out a secrets distribution mechanism.
    
    Tested: Wrote a dummy config in whitby/default.nix locally and checked
    that this builds, but I have not actually run the service yet. I
    expect that some minor tweaks will end up being necessary.
    
    Change-Id: I02a2e8dc40a7f8417fd77afcf8a12ac3df117988

---
+++ ops/nixos/irccat.nix
@@ -0,0 +1,49 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.services.depot.irccat;
+  description = "irccat - forward messages to IRC";
+
+  # irccat expects to read its configuration from the *current
+  # directory*, and its configuration contains secrets.
+  #
+  # To make this work we construct the JSON configuration file and
+  # then recursively merge it with an on-disk secret using jq on
+  # service launch.
+  configJson = pkgs.writeText "irccat.json" (builtins.toJSON cfg.config);
+  configMerge = pkgs.writeShellScript "merge-irccat-config" ''
+    if [ ! -f "/etc/secrets/irccat.json" ]; then
+      echo "irccat secrets file is missing"
+      exit 1
+    fi
+
+    # jq's * is the recursive merge operator
+    ${pkgs.jq}/bin/jq -s '.[0] * .[1]' ${configJson} /etc/secrets/irccat.json \
+      > /var/lib/irccat/irccat.json
+cat  '';
+in {
+  options.services.depot.irccat = {
+    enable = lib.mkEnableOption description;
+
+    config = lib.mkOption {
+      type = lib.types.attrs; # varying value types
+      description = "Configuration structure (unchecked!)";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    systemd.services.irccat = {
+      inherit description;
+      preStart = "${configMerge}";
+      script = "${config.depot.third_party.irccat}/bin/irccat";
+      wantedBy = [ "multi-user.target" ];
+
+      serviceConfig = {
+        DynamicUser = true;
+        StateDirectory = "irccat";
+        WorkingDirectory = "/var/lib/irccat";
+        Restart = "always";
+      };
+    };
+  };
+}
