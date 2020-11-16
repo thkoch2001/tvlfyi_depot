b1f2c2f48e2d83abea0a5396589a7551096a09f2
Griffin Smith <grfn@gws.fyi>
2020-11-16 11:01:28 -0500

    feat(glittershark): Add resume + deploy it to gws.fyi
    Change-Id: If9cf64c97621313af86074752d0959506059888a
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/2079
    Reviewed-by: glittershark <grfn@gws.fyi>
    Tested-by: BuildkiteCI

---
+++ users/glittershark/resume/default.nix
@@ -0,0 +1,37 @@
+{ pkgs, ... }:
+
+with pkgs.lib;
+
+pkgs.runCommandNoCC "resume.pdf" {
+  buildInputs = [(pkgs.texlive.combine {
+    inherit (pkgs.texlive)
+      capt-of
+      collection-fontsrecommended
+      enumitem
+      etoolbox
+      fancyvrb
+      float
+      fncychap
+      framed
+      l3packages
+      microtype
+      needspace
+      parskip
+      scheme-basic
+      tabulary
+      titlesec
+      ulem
+      upquote
+      varwidth
+      wrapfig
+      xcolor
+      ;
+  })];
+} ''
+  cp ${builtins.filterSource (path: type:
+    type == "regular" &&
+    any (ext: hasSuffix ext path) [".sty" ".cls" ".tex" ".png"]
+  ) ./.}/* .
+  pdflatex ./resume.tex
+  cp resume.pdf $out
+''
