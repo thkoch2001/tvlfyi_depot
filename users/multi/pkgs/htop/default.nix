4331bc8da76ae67a43d64ff5355598a2699a647f
multi <depot@in-addr.xyz>
2020-11-17 23:56:44 +0000

    chore(users/multi): Update htop derivation for new upstream.
    - htop has moved upstreams, which has been producing new releases, so
      update the derivation to pull from the new repository on GitHub.
    
    - All of the patches I have locally have been merged upstream, so drop
      them from the depot.
    
    - Pull from a reasonably recent git commit instead of from a numbered
      release, as the ZFS ARC stats and CPU meter columnation patches
      haven't made it into a release yet.
    
    Change-Id: I66ad4c035df07709abf4f75a9d4e1486920091d0
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/2105
    Reviewed-by: multi <depot@in-addr.xyz>
    Tested-by: BuildkiteCI

--- users/multi/pkgs/htop/default.nix
+++ users/multi/pkgs/htop/default.nix
@@ -1,33 +1,19 @@
 { pkgs, ... }:
 
 let
-  arcStats = [
-    ./zfs-arc-stats/0001-Specify-correct-MIB-length.patch
-    ./zfs-arc-stats/0002-Support-ZFS-ARC-stats-on-FreeBSD.patch
-    ./zfs-arc-stats/0003-ZFS-arcstats-for-Linux.patch
-    ./zfs-arc-stats/0004-ZFS-arcstats-for-Darwin-macOS-OS-X.patch
-    ./zfs-arc-stats/0005-Refactor-common-OpenZFS-sysctl-access.patch
-    ./zfs-arc-stats/0006-ZFS-arcstats-for-Solaris.patch
-    ./zfs-arc-stats/0007-Refactor-openzfs_sysctl_init-and-ZfsArcMeter.patch
-    ./zfs-arc-stats/0008-Support-for-ZFS-Compressed-ARC-statistics.patch
-    ./zfs-arc-stats/0009-Linux-fixes.patch
-  ];
+  newVer = "3.0.2";
 
-  arcCache = [
-    ./zfs-arc-cache/0001-Linux-consider-ZFS-ARC-to-be-cache.patch
-  ];
-
-  removeMousing = [
-    ./remove-mousing/0001-Tear-out-ncurses-mouse-mode.patch
-  ];
-
-  octoMeter = [
-    ./octo-meter/0001-Add-quad-and-octo-meter-display-meters.patch
-  ];
+  newSrc = pkgs.fetchFromGitHub {
+    owner = "htop-dev";
+    repo = "htop";
+    rev = "59ef15b2ad6037f40d7fe4207b2b59dd11b14b8b";
+    sha256 = "0sirwfvqwwq2x2k25vd4k4cf9d5qv17yjizidxq4y5xfh2v0djmd";
+  };
 in
   with pkgs; htop.overrideAttrs
-    ({ patches ? [], nativeBuildInputs ? [], ... }:
+    ({ nativeBuildInputs ? [], ... }:
       {
-        patches = patches ++ arcStats ++ arcCache ++ removeMousing ++ octoMeter;
         nativeBuildInputs = nativeBuildInputs ++ [ autoreconfHook ];
+        src = newSrc;
+        version = newVer;
       })
