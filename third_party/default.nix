3696bebb74cdc0bbc415a880bf105ee62a7b08ef
Vincent Ambo <mail@tazj.in>
2020-11-21 18:22:54 +0100

    chore(3p): Bump nixpkgs to nixos-unstable from 2020-11-21
    Included fixes for random breakage:
    
    * bumped grfn's linux-ck patch hash
    * bumped CFFI Lisp library
    * libsystemd attribute changed
    * bumped googletest library & removed patches that nixpkgs applies
    
    Change-Id: If2563e8a68e2750c4b913a976ff7b93b42e8b7f3

--- third_party/default.nix
+++ third_party/default.nix
@@ -5,11 +5,11 @@
 { ... }:
 
 let
-  # Tracking nixos-unstable as of 2020-08-16.
-  nixpkgsCommit = "16fc531784ac226fb268cc59ad573d2746c109c1";
+  # Tracking nixos-unstable as of 2020-11-21.
+  nixpkgsCommit = "a322b32e9d74fb476944ff6cfb55833dc69cfaaa";
   nixpkgsSrc = fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs-channels/archive/${nixpkgsCommit}.tar.gz";
-    sha256 = "0qw1jpdfih9y0dycslapzfp8bl4z7vfg9c7qz176wghwybm4sx0a";
+    url = "https://github.com/NixOS/nixpkgs/archive/${nixpkgsCommit}.tar.gz";
+    sha256 = "1r0mkiqxija75spnyksmh8x5j4smnrxv5f7768s81gsl570kls0l";
   };
   nixpkgs = import nixpkgsSrc {
     config.allowUnfree = true;
