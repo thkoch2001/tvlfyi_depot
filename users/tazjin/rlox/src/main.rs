88563a2dc0d385bb43d326586ced185b6ab9381a
Vincent Ambo <mail@tazj.in>
2020-11-23 00:59:03 +0100

    feat(tazjin/rlox): Add basic program structure
    ... as well as a Nix derivation, because why not.
    
    Change-Id: Iaf2591ab72676fe0732c3f807b3aa0cff13fb4ef

--- users/tazjin/rlox/src/main.rs
+++ users/tazjin/rlox/src/main.rs
@@ -1,23 +1,40 @@
-use std::process;
 use std::env;
+use std::fs;
+use std::io;
+use std::io::Write;
+use std::process;
 
-fn run_file(_file: &str) {
-    unimplemented!("no file support yet")
-}
-
-fn run_prompt() {
-    unimplemented!("no prompt support yet")
-}
+mod interpreter;
 
 fn main() {
     let mut args = env::args();
 
-    if args.len() > 1 {
+    if args.len() > 2 {
         println!("Usage: rlox [script]");
         process::exit(1);
-    } else if let Some(file) = args.next() {
+    } else if let Some(file) = args.nth(1) {
         run_file(&file);
     } else {
         run_prompt();
+    }
+}
+
+// Run Lox code from a file and print results to stdout
+fn run_file(file: &str) {
+    let contents = fs::read_to_string(file).expect("failed to read the input file");
+    interpreter::run(&contents);
+}
+
+// Evaluate Lox code interactively in a shitty REPL.
+fn run_prompt() {
+    let mut line = String::new();
+
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+        io::stdin()
+            .read_line(&mut line)
+            .expect("failed to read user input");
+        interpreter::run(&line);
     }
 }
