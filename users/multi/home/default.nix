81699c1d4f81754d9533bc454706630f72022a38
multi <depot@in-addr.xyz>
2020-11-22 16:58:19 +0000

    refactor(users/multi): make my user directory CI-compatible.
    My home-manager config is not currently readTree compatible, which means
    that it's not built by CI. This constructs a house of cards around
    home-manager to make this buildable in CI.
    
    Change-Id: I80480f24ff47347f46d708edbbf34d59fa76adac

---
+++ users/multi/home/default.nix
@@ -0,0 +1,22 @@
+{ depot, pkgs, lib, ... }:
+
+let
+  nixpkgs = import pkgs.nixpkgsSrc {};
+  home-modules = import "${nixpkgs.home-manager.src}/modules";
+
+  home = path:
+    let
+      config = import path { inherit depot pkgs; };
+    in
+      home-modules {
+        pkgs = nixpkgs;
+        configuration = config;
+      };
+
+in
+  rec {
+    whitby = home ./configs/whitby.nix;
+    whitbyHome = whitby.activationPackage;
+
+    meta.targets = [ "whitbyHome" ];
+  }
