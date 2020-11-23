e6793212f8109db61c0d55282e724a83af0f77cc
Griffin Smith <grfn@gws.fyi>
2020-11-23 10:00:34 -0500

    fix(gs/home): Don't override alacritty version
    This was only done to get an upgraded version, which is no longer
    necessary with the nixpkgs bump but was also causing incompatibilities
    with opengl
    
    Change-Id: Ic398b4ac6caf24a1d40b7a4917b5053fba12d97a

--- users/glittershark/system/home/modules/alacritty.nix
+++ users/glittershark/system/home/modules/alacritty.nix
@@ -1,22 +1,8 @@
 { config, lib, pkgs, ... }:
 
-let
-
-  # TODO(grfn): Find a way to extract this to third_party without too much
-  # effort
-  alacrittyNixpkgs = import (pkgs.fetchFromGitHub {
-    owner = "NixOS";
-    repo = "nixpkgs";
-    rev = "cd9286194a5597e28b8bfb747418dd6af1567eb3";
-    sha256 = "0rwhq7cy4xy25gg2in6vah55psdizz7r187zy7z4c2a7pj2hs7ak";
-  }) {};
-
-in
-
 {
   programs.alacritty = {
     enable = true;
-    package = alacrittyNixpkgs.alacritty;
     settings = {
       font.size = 6;
       font.normal.family = "Meslo LGSDZ Nerd Font";
