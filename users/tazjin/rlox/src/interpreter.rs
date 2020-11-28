800d2ccde875551a2aa2a54b122696818111a2d8
Vincent Ambo <mail@tazj.in>
2020-11-28 16:58:46 +0100

    fix(tazjin/rlox): Clear previous line in REPL mode
    Change-Id: I8acc5b0d07b2c656f9bba76a6ddac6b9088ea563
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/2189
    Reviewed-by: tazjin <mail@tazj.in>
    Tested-by: BuildkiteCI

--- users/tazjin/rlox/src/interpreter.rs
+++ users/tazjin/rlox/src/interpreter.rs
@@ -3,6 +3,7 @@ use crate::scanner;
 // Run some Lox code and print it to stdout
 pub fn run(code: &str) {
     let chars: Vec<char> = code.chars().collect();
-    let _tokens = scanner::scan(&chars);
-    println!("no interpreter yet, sorry")
+    for token in scanner::scan(&chars) {
+        println!("{:?}", token);
+    }
 }
