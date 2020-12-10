3212a8fc8e4addc4774a593eb2a09d66d95cb0d4
Vincent Ambo <mail@tazj.in>
2020-12-10 10:56:31 +0100

    fix(3p/notmuch): Rebase dottime patch on notmuch 0.31
    Change-Id: I43e8f59cd7f58b9c8b501983edbe55a3cb2f20d0

--- third_party/notmuch/default.nix
+++ third_party/notmuch/default.nix
@@ -3,7 +3,4 @@
 pkgs.originals.notmuch.overrideAttrs(old: {
   doCheck = false;
   patches = [ ./dottime.patch ] ++ (if old ? patches then old.patches else []);
-
-  # TODO(tazjin): Fix the build and re-enable CI.
-  meta.ci = false;
 })
