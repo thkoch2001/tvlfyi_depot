20cbb001d0f2be7e7ef4979e50133114a3f667e5
Griffin Smith <grfn@gws.fyi>
2020-12-14 12:46:15 -0500

    feat(gs/emacs): Move all rust-specific stuff to rust.el
    Change-Id: I50fa50745dfc60c6487108f50d57f1fde97ad920

--- users/glittershark/emacs.d/rust.el
+++ users/glittershark/emacs.d/rust.el
@@ -1,8 +1,24 @@
 ;;; ../code/depot/users/glittershark/emacs.d/rust.el -*- lexical-binding: t; -*-
 
+; (setq lsp-rust-analyzer-cargo-watch-command "clippy")
+
+(add-to-list 'auto-mode-alist '("\\.rs$" . rust-mode))
+
 (defun grfn/rust-setup ()
+  (interactive)
   (setq lsp-rust-server 'rust-analyzer)
+  (setq-local whitespace-line-column 100
+              fill-column 100)
+  (setq-local rustic-format-trigger 'on-save)
+  (setq rust-format-show-buffer nil)
   (rust-enable-format-on-save)
   (lsp))
 
 (add-hook 'rust-mode-hook #'grfn/rust-setup)
+
+(map!
+ (:map rust-mode-map
+  :n "g RET" #'cargo-process-current-file-tests
+  :n "g R" #'lsp-find-references
+  (:localleader
+   "m" #'lsp-rust-analyzer-expand-macro)))
