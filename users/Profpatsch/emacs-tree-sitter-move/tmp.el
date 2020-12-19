67457cd62376962b16f9d4da52420e3bbc4aa9ab
Profpatsch <mail@profpatsch.de>
2020-12-19 19:22:55 +0100

    feat(users/Profpatsch) moving around via the tree-sitter parse tree
    Has a little setup to get the cursor position and map it onto a tree
    sitter node. The current node is saved in a cursor variable, and a
    highlight overlay marks the range of the current node in the buffer.
    
    Change-Id: I0af56115f928732e993fbefe978a246ca7c757ee

---
+++ users/Profpatsch/emacs-tree-sitter-move/tmp.el
@@ -0,0 +1,13 @@
+(tree-sitter-load
+ 'python
+ (format "%s/bin/python"
+         (getenv "TREE_SITTER_GRAMMAR_DIR")))
+
+(setq tree-sitter-major-mode-language-alist
+      '((python-mode . python)))
+
+
+(define-key evil-normal-state-map (kbd "C-<right>") #'tree-sitter-move-right)
+;; (define-key evil-normal-state-map (kbd "C-<left>") 'sp-backward-parallel-sexp)
+;; (define-key evil-normal-state-map (kbd "C-<down>") 'sp-down-sexp)
+;; (define-key evil-normal-state-map (kbd "C-<up>") 'sp-backward-up-sexp)
