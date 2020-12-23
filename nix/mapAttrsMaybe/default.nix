c939782934f7a867e1168e0154cd8a8bef263673
Profpatsch <mail@profpatsch.de>
2020-12-23 22:15:56 +0100

    feat(nix/emptyDerivation): take an `extra` attrset that is passed
    `extra` is intended for tvl-specific well-known attrs, like passing
    through tests and shell definitions.
    
    Change-Id: I1227231462b6a1819bcd7ec1db81936372458afe

--- nix/mapAttrsMaybe/default.nix
+++ nix/mapAttrsMaybe/default.nix
@@ -13,7 +13,7 @@
 let
   mapAttrsMaybe = f: attrs: lib.pipe attrs [
     (lib.mapAttrsToList (k: v: { name = k; value = f k v; }))
-    (builtins.filter ({name, value}: if value == null then false else true))
+    (builtins.filter ({ name, value }: if value == null then false else true))
     lib.listToAttrs
   ];
 
@@ -28,28 +28,29 @@ let
       mapId = _: v: v;
       idTest = it "filters out simple nulls over id" [
         (assertEq "empty"
-          (mapAttrsMaybe mapId {})
-          {})
+          (mapAttrsMaybe mapId { })
+          { })
         (assertEq "onlyNull"
           (mapAttrsMaybe mapId { foo = null; bar = null; })
-          {})
+          { })
         (assertEq "moreStuff"
           (mapAttrsMaybe mapId { foo = null; bar = 42; baz = [ "hi" ]; })
           { bar = 42; baz = [ "hi" ]; })
       ];
       fnTest = it "uses the mapping function to filter out stuff that is null" [
         (assertEq "lists"
-          (mapAttrsMaybe (_: v: if lib.isList v then null else v) { foo = []; bar = 42; })
+          (mapAttrsMaybe (_: v: if lib.isList v then null else v) { foo = [ ]; bar = 42; })
           { bar = 42; })
       ];
 
     in
-      runTestsuite "mapAttrsMaybe" [
-        idTest
-        fnTest
-      ];
+    runTestsuite "mapAttrsMaybe" [
+      idTest
+      fnTest
+    ];
 
-in {
+in
+{
   __functor = _: mapAttrsMaybe;
 
   inherit tests;
