0a3eae1f56749d7567f3fa1ab135dbcc348af327
Profpatsch <mail@profpatsch.de>
2021-01-02 00:15:32 +0100

    feat(Profpatsch): add netstring helper function & python lib
    Simple helper function to generate a netstring that is a list of
    key-value pairs, to serialize a nix dict. Also adds a python lib to
    read the serialized form into a dict again.
    
    Change-Id: I306c0cfd51640c0658d32c8d3a4f3d332ba448f0

---
+++ users/Profpatsch/netstring/tests.nix
@@ -0,0 +1,45 @@
+{ depot, lib, pkgs, python-netstring, toNetstring, toNetstringKeyVal }:
+
+let
+  imports = {
+    inherit (depot.users.Profpatsch)
+      writers
+      ;
+  };
+
+  python-netstring-test = imports.writers.python3 "python-netstring" {
+    libraries = p: [
+      python-netstring
+    ];
+  } ''
+    import netstring
+
+    def assEq(left, right):
+      assert left == right, "{} /= {}".format(str(left), str(right))
+
+    assEq(
+      netstring.read_netstring(b"""${toNetstring "hi!"}"""),
+      (b"hi!", b"")
+    )
+
+    assEq(
+      netstring.read_netstring_key_val(
+        b"""${toNetstringKeyVal { foo = "42"; }}"""
+      ),
+      (b'foo', b'42', b"")
+    )
+
+    assEq(
+      netstring.read_netstring_key_val_list(
+        b"""${toNetstringKeyVal { foo = "42"; bar = "hi"; }}"""
+      ),
+      { b'foo': b'42', b'bar': b'hi' }
+    )
+
+  '';
+
+in {
+  inherit
+    python-netstring-test
+    ;
+}
