cba3f958eba97c5b15a54136102a4fe15e9c1cd7
Profpatsch <mail@profpatsch.de>
2021-01-01 23:55:14 +0100

    feat(Profpatsch/writers): add yants types to python3 writer
    Uses the new restrict type to make sure flake errors start with an E.
    
    Change-Id: I30369ade28e1ef612c91a368de2d5b128e6cf2a9

--- users/Profpatsch/writers/default.nix
+++ users/Profpatsch/writers/default.nix
@@ -2,14 +2,19 @@
 let
   bins = depot.nix.getBins pkgs.coreutils ["printf" "mkdir" "cat"];
 
-  python3 = name: args@{
+  inherit (depot.nix.yants) defun struct restrict attrs list string drv any;
+
+  FlakeError = restrict "flake error" (s: builtins.substring 0 1 s == "E") string;
+  Libraries = defun [ (attrs any) (list drv) ];
+  python3 = name: {
     libraries ? (_: []),
     flakeIgnore ? []
   }: pkgs.writers.writePython3 name {
-    libraries = libraries pkgs.python3Packages;
-    flakeIgnore = flakeIgnore;
+    libraries = Libraries libraries pkgs.python3Packages;
+    flakeIgnore = list FlakeError flakeIgnore;
   };
 
+  # TODO: add the same flake check as the pyhon3 writer
   python3Lib = { name, libraries ? (_: []) }: moduleString:
     let srcTree = depot.nix.runExecline.local name { stdin = moduleString; } [
       "importas" "out" "out"
