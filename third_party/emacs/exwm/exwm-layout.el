108fb23c47319518547d5e90622920cecc214cf7
Vincent Ambo <mail@tazj.in>
2021-01-09 18:10:19 +0300

    fix(3p/exwm): Work around Chrome input focus issue
    In recent Chrome versions, EXWM has some issue around handing focus
    back to the application. There is a Github issue about this and this
    commit implements the suggested workaround, which I've verified
    locally.
    
    Change-Id: Ib451e8d8b34921665c3015853850d12e04612929

--- third_party/emacs/exwm/exwm-layout.el
+++ third_party/emacs/exwm/exwm-layout.el
@@ -179,7 +179,8 @@
                          :window id :value-mask xcb:CW:EventMask
                          :event-mask (exwm--get-client-event-mask)))
       (exwm-layout--set-state id xcb:icccm:WM_STATE:IconicState)
-      (cl-pushnew xcb:Atom:_NET_WM_STATE_HIDDEN exwm--ewmh-state)
+      ;; Temporarily commented out because of https://github.com/ch11ng/exwm/issues/759
+      ;; (cl-pushnew xcb:Atom:_NET_WM_STATE_HIDDEN exwm--ewmh-state)
       (exwm-layout--set-ewmh-state id)
       (exwm-layout--auto-iconify)
       (xcb:flush exwm--connection))))
