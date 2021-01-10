3a5e92b1db057d1f097eda8b3de85ee557e47ab4
Vincent Ambo <mail@tazj.in>
2021-01-10 16:17:24 +0300

    refactor(3p/exwm): Inline build instructions instead of overriding
    Change-Id: If16a57e8dd905c8f709bee70dac575509dd22b02

--- third_party/emacs/exwm/default.nix
+++ third_party/emacs/exwm/default.nix
@@ -1,7 +1,22 @@
 # EXWM is present in nixpkgs and we do not (currently) intend to
 # change the code structure, so the existing drv can be reused.
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
-pkgs.emacsPackages.exwm.overrideAttrs(_: {
+let
+  inherit (pkgs.emacsPackages) melpaBuild xelb;
+in melpaBuild {
+  pname = "exwm";
+  ename = "exwm";
+  version = "0.24";
   src = ./.;
-})
+  packageRequires = [ xelb ];
+
+  recipe = builtins.toFile "recipe.el" ''
+    (exwm :fetcher github :repo "ch11ng/exwm")
+  '';
+
+  meta = {
+    homepage = "https://elpa.gnu.org/packages/exwm.html";
+    license = lib.licenses.free;
+  };
+}
