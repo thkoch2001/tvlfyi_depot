090c96eae998fd399975e85321189a136a7dbddd
Vincent Ambo <mail@tazj.in>
2021-01-14 03:13:51 +0300

    feat(tazjin/rlox): Scaffolding for builtin functions
    ... and adds an example builtin which returns the current epoch.
    
    The types introduced by this, especially in the interpreter module,
    are going to be used for user-defined functions, too.
    
    Change-Id: I0364a67241e94642cde08489ac711a340e30ebe8
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/2381
    Reviewed-by: tazjin <mail@tazj.in>
    Tested-by: BuildkiteCI

---
+++ users/tazjin/rlox/src/interpreter/builtins.rs
@@ -0,0 +1,25 @@
+use std::fmt;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use crate::errors::Error;
+use crate::interpreter::Value;
+use crate::parser::Literal;
+
+pub trait Builtin: fmt::Debug {
+    fn arity(&self) -> usize;
+    fn call(&self, args: Vec<Value>) -> Result<Value, Error>;
+}
+
+// Builtin to return the current timestamp.
+#[derive(Debug)]
+pub struct Clock {}
+impl Builtin for Clock {
+    fn arity(&self) -> usize {
+        0
+    }
+
+    fn call(&self, _args: Vec<Value>) -> Result<Value, Error> {
+        let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+        Ok(Value::Literal(Literal::Number(now.as_secs() as f64)))
+    }
+}
