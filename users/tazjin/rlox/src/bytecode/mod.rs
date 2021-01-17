c13d01757b101a4311ebef6d1e8970107d7fd827
Vincent Ambo <mail@tazj.in>
2021-01-17 21:05:05 +0300

    feat(tazjin/rlox): Track source lines along with bytecode
    Change-Id: I272e73b9b1c9571fbfe4fa983fb4283ddee02bd4

--- users/tazjin/rlox/src/bytecode/mod.rs
+++ users/tazjin/rlox/src/bytecode/mod.rs
@@ -13,8 +13,8 @@ pub fn main() {
     let mut chunk: Chunk = Default::default();
 
     let constant = chunk.add_constant(1.2);
-    chunk.add_op(OpCode::OpConstant(constant));
-    chunk.add_op(OpCode::OpReturn);
+    chunk.add_op(OpCode::OpConstant(constant), 1);
+    chunk.add_op(OpCode::OpReturn, 1);
 
     chunk::disassemble(&chunk, "test chunk");
 }
