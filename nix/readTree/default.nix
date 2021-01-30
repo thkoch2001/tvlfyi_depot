65cb17dcb63c2a7c57aa36903ce363c5b16b518b
Profpatsch <mail@profpatsch.de>
2021-01-30 13:19:38 +0100

    refact(nix/readTree): refactor code to be more declarative Pt. 1
    The `readTree` function handles a lot of cases, as shown by the
    documentation and the tests, but the code itself is not very
    declarative at the moment.
    
    This is step 0.5 towards making it so, aiming towards moving all
    import logic into a separate function which just exports a sum type of
    the different states a directory can be in and what it means for
    `readTree`.
    
    `readDirVisible` should now export all of the skipping behaviour of
    the different combinations of `.skip-subtree` and `default.nix`.
    
    `read-tree` is mostly kept as-is, but should get a lot simpler after
    pulling more logic into the match statements.
    
    The `match` function is copied from the (not yet merged) `nix.tags`
    module, since the idea of `readTree` is to only depend on nix
    builtins.
    
    Change-Id: Ie466dc037b96b1bda6f99e703e816adbaa1694c8

--- nix/readTree/default.nix
+++ nix/readTree/default.nix
@@ -12,8 +12,6 @@ let
     length
     listToAttrs
     map
-    match
-    readDir
     substring;
 
   assertMsg = pred: msg:
@@ -27,13 +25,37 @@ let
 
   readDirVisible = path:
     let
-      children = readDir path;
-      isVisible = f: f == ".skip-subtree" || (substring 0 1 f) != ".";
+      children = builtins.readDir path;
+      shouldBeSkipped =
+        if children ? ".skip-subtree"
+        then { why = builtins.readFile (path + "/.skip-subtree"); }
+        else { not-skipped = {}; };
+      isVisible = f: (substring 0 1 f) != ".";
       names = filter isVisible (attrNames children);
-    in listToAttrs (map (name: {
-      inherit name;
-      value = children.${name};
-    }) names);
+      visible-children =
+        listToAttrs (map (name: {
+          inherit name;
+          value = children.${name};
+        }) names);
+    in
+      if visible-children ? "default.nix"
+      then match shouldBeSkipped {
+        not-skipped = {}:
+          { with-default-nix = visible-children; };
+        why = why:
+          { skip-subdirs-default-nix = {
+              inherit
+                why
+                visible-children;
+            };
+          };
+      }
+      else match shouldBeSkipped {
+        not-skipped = {}:
+          { no-default-nix = visible-children; };
+        why = why:
+          { skip-dir = why; };
+      };
 
   # Create a mark containing the location of this attribute.
   marker = parts: {
@@ -56,7 +78,7 @@ let
       else imported;
 
   nixFileName = file:
-    let res = match "(.*)\.nix" file;
+    let res = builtins.match "(.*)\.nix" file;
     in if res == null then null else head res;
 
   readTree = args: initPath: parts:
@@ -72,21 +94,55 @@ let
       # This file can optionally contain information on why the tree
       # should be ignored, but its content is not inspected by
       # readTree
-      filterDir = f: dir."${f}" == "directory";
-      children = if hasAttr ".skip-subtree" dir then [] else map (c: {
-        name = c;
-        value = readTree args (joinChild c) (parts ++ [ c ]);
-      }) (filter filterDir (attrNames dir));
+      filterChildren = ch:
+          let onlyDirectories = f: ch."${f}" == "directory"; in
+          map (c: {
+            name = c;
+            value = readTree args (joinChild c) (parts ++ [ c ]);
+          }) (filter onlyDirectories (attrNames ch));
+      children = match dir {
+        # TODO: remove trace, convert to readDir debbugging mode
+        skip-dir = _why: builtins.trace "skipping dir ${initPath}: ${_why}" [];
+        # TODO: remove trace, convert to readDir debugging mode
+        skip-subdirs-default-nix = { why, visible-children }: builtins.trace "skipping subdirs of ${initPath}: ${why}" [];
+        with-default-nix = filterChildren;
+        no-default-nix = filterChildren;
+       };
 
       # Import Nix files
-      nixFiles = filter (f: f != null) (map nixFileName (attrNames dir));
+      filterNixFiles = d: filter (f: f != null) (map nixFileName (attrNames d));
+      nixFiles = match dir {
+        skip-dir = _why: [];
+        skip-subdirs-default-nix = { why, visible-children }: filterNixFiles visible-children;
+        with-default-nix = filterNixFiles;
+        no-default-nix = filterNixFiles;
+      };
       nixChildren = map (c: let p = joinChild (c + ".nix"); in {
         name = c;
         value = importWithMark args p (parts ++ [ c ]);
       }) nixFiles;
-    in if dir ? "default.nix"
+    in if (dir ? with-default-nix || dir ? skip-subdirs-default-nix)
       then (if isAttrs self then self // (listToAttrs children) else self)
       else (listToAttrs (nixChildren ++ children) // (marker parts));
+
+  # helpers
+
+  match = sum: matcher:
+    let cases = builtins.attrNames sum;
+    in assert
+      let len = builtins.length cases; in
+        assertMsg (builtins.length cases == 1)
+          ( "match: an instance of a sum is an attrset "
+          + "with exactly one element, yours had ${toString len}"
+          + ", namely: ${toString cases}" );
+    let case = builtins.head cases;
+    in assert
+        assertMsg (matcher ? ${case})
+        ( "match: \"${case}\" is not a valid case of this sum, "
+        + "the matcher accepts: ${toString
+            (builtins.attrNames matcher)}" );
+    matcher.${case} sum.${case};
+
 
 in {
    __functor = _: args: initPath: readTree args initPath [ (baseNameOf initPath) ];
