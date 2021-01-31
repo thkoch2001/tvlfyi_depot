0f4cd94d6fe8b6e4aaa8610a49fb5e8334a5482b
Profpatsch <mail@profpatsch.de>
2021-01-31 15:47:49 +0100

    feat(users/Profpatsch/execline): add args_for_exec
    `exec_into_args` would just read argv and exec into it, but we want to
    be able to write commands which take some positional arguments first.
    
    Thus we split the invocation into `args_for_exec`, which returns the
    positional arguments and prog, and then pass prog to `exec_into_args`
    when we want to exec eventually (prog is still an iterator at this
    point).
    
    Change-Id: I0b180c1a100b96363fe33ba2c42034ed41716b7a

--- users/Profpatsch/execline/default.nix
+++ users/Profpatsch/execline/default.nix
@@ -3,22 +3,7 @@
 let
   exec-helpers = depot.users.Profpatsch.writers.rustSimpleLib {
     name = "exec-helpers";
-  } ''
-    use std::os::unix::process::CommandExt;
-    use std::ffi::OsStr;
-    use std::os::unix::ffi::OsStrExt;
-    pub fn exec_into_args<'a, I>(prog_name: &str, env_additions: I) -> !
-      where
-        I: IntoIterator<Item = (&'a [u8], &'a [u8])>,
-    {
-        let mut argv = std::env::args_os();
-        let prog = argv.nth(1).expect(&format!("{}: first argument must be an executable", prog_name));
-        let args = argv;
-        let env = env_additions.into_iter().map(|(k,v)| (OsStr::from_bytes(k), OsStr::from_bytes(v)));
-        let err = std::process::Command::new(prog).args(args).envs(env).exec();
-        panic!("{}: exec failed: {:?}", prog_name, err);
-    }
-  '';
+  } (builtins.readFile ./exec_helpers.rs);
 
 in {
   inherit
