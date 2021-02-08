a42e4929f2993c718df2a486ec94aebe29bb4718
Profpatsch <mail@profpatsch.de>
2021-02-08 03:23:53 +0100

    feat(users/Profpatsch/arglib): use exec_helpers for rust
    Change-Id: I3056385eb11e45ae13456f4c47052651ba5fb62f

--- users/Profpatsch/arglib/default.nix
+++ users/Profpatsch/arglib/default.nix
@@ -5,28 +5,30 @@ let
     rust = depot.users.Profpatsch.writers.rustSimpleLib {
       name = "arglib-netencode";
       dependencies = [
+        depot.users.Profpatsch.execline.exec-helpers
         depot.users.Profpatsch.netencode.netencode-rs
       ];
     } ''
       extern crate netencode;
+      extern crate exec_helpers;
 
       use netencode::{T};
       use std::os::unix::ffi::OsStrExt;
 
-      pub fn arglib_netencode(env: Option<&std::ffi::OsStr>) -> Result<T, String> {
+      pub fn arglib_netencode(prog_name: &str, env: Option<&std::ffi::OsStr>) -> T {
           let env = match env {
               None => std::ffi::OsStr::from_bytes("ARGLIB_NETENCODE".as_bytes()),
               Some(a) => a
           };
           match std::env::var_os(env) {
-              None => Err(format!("could not read args, envvar {} not set", env.to_string_lossy())),
+              None => exec_helpers::die_user_error(prog_name, format!("could not read args, envvar {} not set", env.to_string_lossy())),
               // TODO: good error handling for the different parser errors
               Some(soup) => match netencode::parse::t_t(soup.as_bytes()) {
                   Ok((remainder, t)) => match remainder.is_empty() {
-                      true => Ok(t),
-                      false => Err(format!("there was some unparsed bytes remaining: {:?}", remainder))
+                      true => t,
+                      false => exec_helpers::die_environment_problem(prog_name, format!("arglib: there was some unparsed bytes remaining: {:?}", remainder))
                   },
-                  Err(err) => Err(format!("parsing error: {:?}", err))
+                  Err(err) => exec_helpers::die_environment_problem(prog_name, format!("arglib parsing error: {:?}", err))
               }
           }
       }
