f3986d970281cfd3117b2196cd783c275b4dd390
sterni <sternenseemann@systemli.org>
2021-02-20 18:02:02 +0100

    feat(sterni/nint): shebang interpreter for nix scripts
    nint (short for nix interpreter) is a tiny wrapper around nix-instantiate
    which allows to run nix scripts, i. e. nix expressions that conform to
    a certain calling convention. A nix script runnable using nint must
    conform to the following constraints:
    
    * It must evaluate to a function which has a set pattern with an
      ellipsis as the single argument.
    
    * It must produce a string as a return value or fail.
    
    When invoked, a the expression receives the following arguments:
    
    * `currentDir`: the current working directory as a nix path
    
    * `argv`: a list of strings containing `argv` including `argv[0]`
    
    * extra arguments which are manually specified which allows for
      passing along dependencies or libraries, for example:
    
        nint --arg depot '(import /depot {})' my-prog.nix [ argv[1] … ]
    
      would pass along depot to be used in `my-prog.nix`.
    
    Such nix scripts are purely functional in a sense: The way inputs can be
    taken is very limited and causing effects is also only possible in a
    very limited sense (using builtins.fetchurl if TARBALL_TTL is 0,
    adding files and directories to the nix store, realising derivations).
    As an approximation, a program executed using nint can be thought of
    as a function with the following signature:
    
      λ :: environment → working directory → argv → stdout
    
    where environment includes:
    
    * the time at the start of the program (`builtins.currentTime`)
    * other information about the machine (`builtins.currentSystem` …)
    * environment variables (`builtins.getEnv`)
    * the file system (`builtins.readDir`, `builtins.readFile`, …) which
      is the biggest input impurity as it may change during evaluation
    
    Additionally import from derivation and builtin fetchers are available
    which introduce further impurities to be utilized.
    
    Future work:
    
    * Streaming I/O via lazy lists. This would allow usage of
      stdin and output before the program terminates. However this would
      require using libexpr directly or writing a custom nix interpreter.
    
      A description of how this would work can be found on the website of the
      esoteric programming language Lazy K: https://tromp.github.io/cl/lazy-k.html
    
    * An effect system beyond stdin / stdout.
    
    * Better error handling, support setting exit codes etc.
    
    These features would require either using an alternative or custom
    interpreter for nix (tvix or hnix) or to link against libexpr directly
    to have more control over evaluation.
    
    Change-Id: I61528516eb418740df355852f23425acc4d0656a

---
+++ users/sterni/nint/default.nix
@@ -0,0 +1,49 @@
+{ depot, pkgs, ... }:
+
+let
+  inherit (depot.users.Profpatsch.writers)
+    rustSimpleBin
+    ;
+
+  inherit (pkgs)
+    buildRustCrate
+    ;
+
+  serde = buildRustCrate {
+    pname = "serde";
+    crateName = "serde";
+    version = "1.0.123";
+    sha256 = "05xl2s1vpf3p7fi2yc9qlzw88d5ap0z3qmhmd7axa6pp9pn1s5xc";
+    features = [ "std" ];
+  };
+
+  ryu = buildRustCrate {
+    pname = "ryu";
+    version = "1.0.5";
+    crateName = "ryu";
+    sha256 = "060y2ln1csix593ingwxr2y3wl236ls0ly1ffkv39h5im7xydhrc";
+  };
+
+  itoa = buildRustCrate {
+    pname = "itoa";
+    version = "0.4.7";
+    crateName = "itoa";
+    sha256 = "0079jlkcmcaw37wljrvb6r3dqq15nfahkqnl5npvlpdvkg31k11x";
+  };
+
+  serde_json = buildRustCrate {
+    pname = "serde_json";
+    version = "1.0.62";
+    crateName = "serde_json";
+    sha256 = "0sgc8dycigq0nxr4j613m4q733alfb2i10s6nz80lsbbqgrka21q";
+    dependencies = [ serde ryu itoa ];
+    features = [ "std" ];
+    edition = "2018";
+  };
+
+in
+
+  rustSimpleBin {
+    name = "nint";
+    dependencies = [ serde_json ];
+  } (builtins.readFile ./nint.rs)
