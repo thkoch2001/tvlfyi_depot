935bb8c672ca41781044550f8f9061ae368f5528
sterni <sternenseemann@systemli.org>
2021-02-21 12:57:40 +0100

    feat(web/bubblegum): nix CGI programming framework
    So here is what has been keeping me up at night: At some point I
    realized that nix actually made a somewhat passable language for CGI
    programming:
    
    * That `builtins.getEnv` exists as one of the impurities of Nix is
      perfect as environment variables are the main way of communication
      from the web server to the CGI application.
    
    * We can actually read from the filesystem via builtins.readDir and
      builtins.readFile with bearable overhead if we avoid importing the
      used paths into the nix store.
    
    * Templating and routing are convenient to implement via indented strings
      and attribute sets respectively.
    
    Of course there are obvious limitation:
    
    * The overhead of derivations is probably much to great for them to be
      useful via IfD.
    
    * Even without derivations, nix evaluation is very slow to the point
      were a trivial application takes between 100ms and 400ms to produce a
      response.
    
    * We can't really cause effects other than producing a response which
      makes it not viable for a lot of applications. There are some ways
      around this:
    
      * With a custom interpreter we could have streaming and multiplexed
        I/O (using lazy lists emulated via attrsets) to cause such effects,
        but it would probably perform terribly.
    
      * We can use builtins.fetchurl to call other HTTP-based microservices,
        but only in very limited constraints, i. e. only GET, no headers,
        and only if the tarball ttl is set to 0 in the global nix.conf.
    
    * Terrible error handling capabilities because builtins.tryEval actually
      doesn't catch a lot of errors.
    
    To prove that it actually works, there are some demo applications,
    which I invite you to run and potentially break horribly:
    
        nix-build -A web.bubblegum.examples && ./result
        # navigate to http://localhost:9000
    
    The setup uses thttpd and executes the nix CGI scripts using
    users.sterni.nint which automatically passed `depot`, so they can
    import the cgi library.
    
    Change-Id: I3a22a749612211627e5f8301c31ec2e7a872812c

---
+++ web/bubblegum/examples/hello.nix
@@ -0,0 +1,80 @@
+{ depot, ... }:
+
+let
+  inherit (depot)
+    lib
+    ;
+
+  inherit (depot.web.bubblegum)
+    pathInfo
+    respond
+    absolutePath
+    ;
+
+  routes = {
+    "/" = {
+      status = "OK";
+      title = "index";
+      content = ''
+        Hello World!
+      '';
+    };
+    "/clock" = {
+      status = "OK";
+      title = "clock";
+      content = ''
+        It is ${toString builtins.currentTime}s since 1970-01-01 00:00 UTC.
+      '';
+    };
+    "/coffee" = {
+      status = "I'm a teapot";
+      title = "coffee";
+      content = ''
+        No coffee, I'm afraid
+      '';
+    };
+  };
+
+  notFound = {
+    status = "Not Found";
+    title = "404";
+    content = ''
+      This page doesn't exist.
+    '';
+  };
+
+  navigation =
+    lib.concatStrings (lib.mapAttrsToList
+      (p: v: "<li><a href=\"${absolutePath p}\">${v.title}</a></li>")
+      routes);
+
+  template = { title, content, ... }: ''
+    <!doctype html>
+    <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <title>${title}</title>
+      <style>a:link, a:visited { color: blue; }</style>
+    </head>
+    <body>
+      <hgroup>
+      <h1><code>//web/bubblegum</code></h1>
+      <h2>example app</h2>
+      </hgroup>
+      <header>
+        <nav>
+          <ul>${navigation}</ul>
+        </nav>
+      </header>
+      <main>
+        <p>${content}</p>
+      </main>
+    </body>
+  '';
+
+  response = routes."${pathInfo}" or notFound;
+
+in
+  respond response.status {
+    "Content-type" = "text/html";
+  } (template response)
