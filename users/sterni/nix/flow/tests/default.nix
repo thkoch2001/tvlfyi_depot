d2e4d1a0bdf0cdd1846c856c2b63a61eb50b73fc
sterni <sternenseemann@systemli.org>
2021-02-22 14:32:45 +0100

    feat(users/sterni/nix): add sternis nix lib
    What you see here is mostly the fallout of my implementing a correct
    urlencode implementation in nix to Profpatsch pleasure or dismay -- I'm
    not sure.
    
    Where I want to go from here:
    
    * Add tests for all sub libraries.
    
    * Extend this libarry towards general purpose nix™, mainly by
      implementing missing interfaces which you'd still have to use
      <nixpkgs/lib> for right now.
    
    * Remove the dependency on <nixpkgs/lib>
    
    * Improve error messages possibly by carefully reintroducing yants. I
      originally typed essentially everything using yants, but turns out
      this can a) be dangerous when stuff you are handling throws because
      type checking means evaluating and b) has a incredible performance
      cost in some cases.
    
    * Reexpose builtins with better naming and slightly wrapped so they
      don't unrecoverably throw in cases where a null or something would
      suffice.
    
    Change-Id: I33ab08ca4e62dbc16b86c66c653935686e6b0e79

---
+++ users/sterni/nix/flow/tests/default.nix
@@ -0,0 +1,48 @@
+{ depot, ... }:
+
+let
+
+  inherit (depot.nix.runTestsuite)
+    runTestsuite
+    it
+    assertEq
+    assertThrows
+    ;
+
+  inherit (depot.users.sterni.nix.flow)
+    cond
+    match
+    ;
+
+  dontEval = builtins.throw "this should not get evaluated";
+
+  testCond = it "tests cond" [
+    (assertThrows "malformed cond list"
+      (cond [ [ true 1 2 ] [ false 1 ] ]))
+    (assertEq "last is true" "last"
+      (cond [
+        [ false dontEval]
+        [ false dontEval ]
+        [ true "last" ]
+      ]))
+    (assertEq "first is true" 1
+      (cond [
+        [ true 1 ]
+        [ true dontEval ]
+        [ true dontEval ]
+      ]))
+  ];
+
+  testMatch = it "tests match" [
+    (assertEq "basic match usage" 42
+      (match "answer" {
+        "answer" = 42;
+        "banana" = dontEval;
+        "maleur" = dontEval;
+      }))
+  ];
+
+in
+  runTestsuite "nix.flow" [
+    testCond
+  ]
