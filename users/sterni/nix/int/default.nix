72ea09458827741197c1cd0bf47a0825a802cc34
sterni <sternenseemann@systemli.org>
2021-02-22 14:32:45 +0100

    feat(users/sterni/nix): add sternis nix lib
    What you see here is mostly the fallout of my implementing a correct
    urlencode implementation in nix to Profpatsch pleasure or dismay -- I'm
    not sure.
    
    Where I want to go from here:
    
    * Add tests for all sub libraries.
    
    * Extend this libarry towards general purpose nix™, mainly by
      implementing missing interfaces which you'd still have to use
      <nixpkgs/lib> for right now.
    
    * Remove the dependency on <nixpkgs/lib>
    
    * Improve error messages possibly by carefully reintroducing yants. I
      originally typed essentially everything using yants, but turns out
      this can a) be dangerous when stuff you are handling throws because
      type checking means evaluating and b) has a incredible performance
      cost in some cases.
    
    * Reexpose builtins with better naming and slightly wrapped so they
      don't unrecoverably throw in cases where a null or something would
      suffice.
    
    Change-Id: I33ab08ca4e62dbc16b86c66c653935686e6b0e79

---
+++ users/sterni/nix/int/default.nix
@@ -0,0 +1,78 @@
+{ depot, ... }:
+
+let
+
+  inherit (depot.users.sterni.nix)
+    string
+    ;
+
+  inherit (builtins)
+    bitOr
+    bitAnd
+    bitXor
+    div
+    ;
+
+  abs = i:
+    if i < 0
+    then -1 * i
+    else i;
+
+  exp = base: pow:
+    if pow > 0
+    then base * (exp base (pow - 1))
+    else if pow < 0
+    then 1.0 / exp base (abs pow)
+    else 1;
+
+  bitShiftR = bit: count:
+    if count == 0
+    then bit
+    else div (bitShiftR bit (count - 1)) 2;
+
+  bitShiftL = bit: count:
+    if count == 0
+    then bit
+    else 2 * (bitShiftL bit (count - 1));
+
+  hexdigits = "0123456789ABCDEF";
+
+  toHex = int:
+    let
+      go = i:
+        if i == 0
+        then ""
+        else go (bitShiftR i 4)
+           + string.charAt (bitAnd i 15) hexdigits;
+    in
+      if int == 0
+      then "0"
+      else go int;
+
+  # A nix integer is a 64bit signed integer
+  maxBound = 9223372036854775807;
+
+  # fun fact: -9223372036854775808 is the lower bound
+  # for a nix integer (as you would expect), but you can't
+  # use it as an integer literal or you'll be greeted with:
+  # error: invalid integer '9223372036854775808'
+  # This is because all int literals when parsing are
+  # positive, negative "literals" are positive literals
+  # which are preceded by the arithmetric negation operator.
+  minBound = -9223372036854775807 - 1;
+
+in {
+  inherit
+    maxBound
+    minBound
+    abs
+    exp
+    div
+    bitShiftR
+    bitShiftL
+    bitOr
+    bitAnd
+    bitXor
+    toHex
+    ;
+}
