af0b1e2ed2bc4136f05ad1f59f4bab1da4989c99
sterni <sternenseemann@systemli.org>
2021-03-01 19:05:57 +0100

    feat(users/sterni/su): sterni's C utilities
    Various reimplementations of common stuff I've accumulated accumulated
    into a library of sorts. Needs to be cleaned up, documented and built
    upon each itself more.
    
    * s.c: simple, limited string library
    * check.c: laughably simple unit testing
    * memstream.c: thin wrapper around open_memstream(3)
    * json.c: simple json rendering library (needs correct unicode handling
      for the future)
    * xml.c: simple xml rendering
    * time.h: dot time utility, plus RFC3339, RFC822 rendering
    * url.c: urlencoding and -decoding
    * char.c: random character related utilities
    
    Change-Id: If8c0e8a71dcca22aea35751553c5f0cf730e74a8

---
+++ users/sterni/su/default.nix
@@ -0,0 +1,57 @@
+{ depot, lib, ... }:
+
+let
+
+  inherit (depot.nix)
+    buildC
+    ;
+
+  inherit (depot.third_party)
+    jq
+    ;
+
+  CFLAGS = [
+    "-Werror"
+    "-Wno-type-limits"
+    "-std=c99"
+  ];
+
+in
+  buildC.library {
+    name = "su";
+    include = [
+      ./include/su
+    ];
+
+    srcs = [
+      ./char.c
+      ./check.c
+      ./memstream.c
+      ./json.c
+      ./s.c
+      ./time.c
+      ./url.c
+    ];
+
+    inherit CFLAGS;
+
+    tests = [
+      (su: buildC.test {
+        name = "su-check";
+
+        runDeps = [
+          jq
+        ];
+
+        deps = [
+          su
+        ];
+
+        srcs = [
+          ./tests/main.c
+        ];
+
+        inherit CFLAGS;
+      })
+    ];
+  }
