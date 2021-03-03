8b87b5cc7c8ba3595110169d4feae85d9004ee1a
Profpatsch <mail@profpatsch.de>
2021-03-03 23:03:06 +0100

    feat(third_party): add dhall packages from easy-dhall-nix
    About to do some dhalllll! \o/
    
    Change-Id: Ie58c335d80f4a5abeb8296ece5a24377f07e6369

--- third_party/default.nix
+++ third_party/default.nix
@@ -38,13 +38,32 @@ in exposed.lib.fix(self: exposed // {
   # version of it.
   inherit nixpkgsCommit nixpkgsSrc stableNixpkgsSrc;
 
-  # Packages to be overridden
+  # Re-expose the original nixpkgs packages that we have overridden.
+  # In case somebody needs the upstream versions.
   originals = {
     inherit (nixpkgs) gtest openldap go grpc notmuch rr;
     inherit (stableNixpkgs) git tdlib;
     ffmpeg = nixpkgs.ffmpeg-full;
     telega = stableNixpkgs.emacsPackages.telega;
+
   };
+
+  # binary releases of dhall tools, since the build in nixpkgs is broken most of the time.
+  # The binaries are also fully static builds, instead of the half-static crap that nixpkgs produces.
+  easy-dhall-nix =
+    import (builtins.fetchTarball {
+      url = "https://github.com/justinwoo/easy-dhall-nix/archive/eae7f64c4d6c70681e5a56c84198236930ba425e.tar.gz";
+      sha256 = "1y2x15v8a679vlpxazjpibfwajp6zph60f8wjcm4xflbvazk0dx7";
+    }) { pkgs = nixpkgs; };
+
+  dhall = self.easy-dhall-nix.dhall-simple;
+  dhall-bash = self.easy-dhall-nix.dhall-bash-simple;
+  dhall-docs = self.easy-dhall-nix.dhall-docs-simple;
+  dhall-json = self.easy-dhall-nix.dhall-json-simple;
+  dhall-lsp-server = self.easy-dhall-nix.dhall-lsp-simple;
+  dhall-nix = self.easy-dhall-nix.dhall-nix-simple;
+  # dhall-nixpkgs = self.easy-dhall-nix.dhall-nixpkgs-simple;
+  dhall-yaml = self.easy-dhall-nix.dhall-yaml-simple;
 
   # Use LLVM 11
   llvmPackages = nixpkgs.llvmPackages_11;
