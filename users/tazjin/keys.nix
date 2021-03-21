e49e9ee7741982381dccb8c35267c350e938ea63
Vincent Ambo <mail@tazj.in>
2021-03-21 15:08:01 +0200

    chore(tazjin/keys): Add tverskoy, comment out frog
    frog is in storage and the key shouldn't have privileges while I'm
    guaranteed to not use it.
    
    Change-Id: If2ee8278fcb81f425dcfc151b11d207dfb6f45c7

--- users/tazjin/keys.nix
+++ users/tazjin/keys.nix
@@ -1,10 +1,10 @@
 # My SSH public keys
 { ... }:
 
-rec {
-  frog = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKMZzRdcrHTuCPoaFy36MPr5IW/hnImlse/OBOn6udL/ tazjin@frog";
+let withAll = keys: keys // { all = builtins.attrValues keys; };
+in withAll {
+  # frog = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKMZzRdcrHTuCPoaFy36MPr5IW/hnImlse/OBOn6udL/ tazjin@frog";
   vauxhall = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBHs+9QfZTD5qGsBQaWqp5whmXJ9qy/m9swE2M9QBaIQVoIYGemq3HXTzrQ6XekwudJCltP4EpM7h/Qc+Or309Yw=";
   s10e = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDf7CNlYoauHcSYsMNnCZt5h9QSYH/7keYkg8g3hT32+";
-
-  all = [ frog vauxhall s10e ];
+  tverskoy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM1fGWz/gsq+ZeZXjvUrV+pBlanw1c3zJ9kLTax9FWQy tazjin@tverskoy";
 }
