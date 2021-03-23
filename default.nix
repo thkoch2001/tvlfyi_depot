66d0c1f7bd18e4ebb832d95676e6d767e976ecff
Vincent Ambo <mail@tazj.in>
2021-03-23 23:46:00 +0200

    chore: Remove //overrides from depot
    This mechanism wasn't actually used for anything other than a minor
    external compatibility thing for buildGo.nix, which can also be solved
    with a function parameter.
    
    Change-Id: I1bf2240435e869cdc4e99bdd1a138fdd2e76f96e

--- default.nix
+++ default.nix
@@ -100,10 +100,4 @@ in fix(self: {
 
 # Add local packages as structured by readTree
 // (localPkgs (readTree' self.config))
-
-# Load overrides into the top-level.
-#
-# This can be used to move things from third_party into the top-level, too (such
-# as `lib`).
-// (readTree' { depot = self; pkgs = self.third_party; }) ./overrides
 )
