c7c1fef425c9f786a9fc992d41c91b62f1815c15
sterni <sternenseemann@systemli.org>
2021-03-23 20:35:27 +0100

    feat(sterni/dot-time-man-pages): mdoc port of dotti.me
    More or less direct port of https://dotti.me to mdoc(7) with the
    following changes:
    
    * Add a RFC3339 column to the EXAMPLES table. RFC3339 is a well
      specified subset of ISO8601 whose specification is also more
      accessible so this could help someone out.
    
    * Add a SEE ALSO section linking to the web site
    
    * Add an AUTHORS section
    
    The derivation is built using the custom, but fairly trivial
    //nix/buildManPages builder which builds each man page, lints it using
    mandoc (but only fails if requested to do so because (especially stable)
    mandoc is pretty strict), gzips it and installs it to $out in the
    correct directory structure.
    
    Additionally nix.buildManPages.single is provided to build just one man
    page where $out is the resulting man page.
    
    Change-Id: I8db00bd402697aa52f6f651f28692617b487f832

---
+++ nix/buildManPages/default.nix
@@ -0,0 +1,77 @@
+{ depot, ... }:
+
+let
+  inherit (depot.third_party)
+    lib
+    gzip
+    mandoc
+    coreutils
+    ;
+
+  inherit (depot.nix)
+    runExecline
+    getBins
+    ;
+
+  bins = getBins mandoc [ "mandoc" ]
+      // getBins gzip   [ "gzip" ]
+      // getBins coreutils [ "mkdir" "ln" "cp" ]
+      ;
+
+  defaultGzip = true;
+
+  basename = gzip: { name, section, ... }:
+    "${name}.${toString section}${lib.optionalString gzip ".gz"}";
+
+  manDir = { section, ... }:
+    "\${out}/share/man/man${toString section}";
+
+  target = gzip: args:
+    "${manDir args}/${basename gzip args}";
+
+  buildManPage =
+    { requireLint ? false
+    , gzip ? defaultGzip
+    , ...
+    }:
+    { content
+    , ...
+    }@page: let
+      source = builtins.toFile (basename false page) content;
+    in runExecline (basename gzip page) {} ([
+      (if requireLint then "if" else "foreground") [
+        bins.mandoc "-mdoc" "-T" "lint" source
+      ]
+      "importas" "out" "out"
+    ] ++ (if gzip then [
+      "redirfd" "-w" "1" "$out"
+      bins.gzip "-c" source
+    ] else [
+      bins.cp "--reflink=auto" source "$out"
+    ]));
+
+  buildManPages =
+    name:
+    { derivationArgs ? {}
+    , gzip ? defaultGzip
+    , ...
+    }@args:
+    pages:
+    runExecline "${name}-man-pages" {
+      inherit derivationArgs;
+    } ([
+      "importas" "out" "out"
+    ] ++ lib.concatMap ({ name, section, content }@page: [
+      "if" [ bins.mkdir "-p" (manDir page) ]
+      "if" [
+        bins.ln "-s"
+        (buildManPage args page)
+        (target gzip page)
+      ]
+    ]) pages);
+
+in {
+  __functor = _: buildManPages;
+
+  single = buildManPage;
+}
