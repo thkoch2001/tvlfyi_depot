3565d97dd5a14b07520a1c21a7bf425843e3c36c
Vincent Ambo <mail@tazj.in>
2021-03-24 01:59:29 +0200

    fix(gemma): Fix build by backporting Elm 0.18
    Backports an old version of the Elm language (before the release where
    they made everyone rewrite their programs), from the same old nixpkgs
    commit as was previously used in overrides (see CL/2646).
    
    Change-Id: I0ae4cc611aa40269b290651ab982c1db93518d8a

---
+++ third_party/elmPackages_0_18/default.nix
@@ -0,0 +1,15 @@
+# Backports Elm packages for Elm 0.18 from an older channel of
+# nixpkgs.
+#
+# Elm 0.19 changed the language & package ecosystem completely,
+# essentially requiring a partial rewrite of all Elm apps. However,
+# //fun/gemma uses Elm 0.18 and I don't have time to rewrite it.
+
+{ pkgs, ... }:
+
+(import (pkgs.fetchFromGitHub {
+  owner = "NixOS";
+  repo = "nixpkgs";
+  rev = "14f9ee66e63077539252f8b4550049381a082518";
+  sha256 = "1wn7nmb1cqfk2j91l3rwc6yhimfkzxprb8wknw5wi57yhq9m6lv1";
+}) {}).elmPackages
