7a59919f30d11f5f9299b2b114164c0c1c01ab02
sterni <sternenseemann@systemli.org>
2021-03-24 15:51:17 +0100

    feat(users/sterni/clock): dot time clock
    Should probably fix my sway bar with this…
    
    Change-Id: Ib25dd7f38a8e3097bcf6818156e7650e29f3cfe3

---
+++ users/sterni/clock/default.nix
@@ -0,0 +1,32 @@
+{ depot, ... }:
+
+let
+
+  inherit (depot.nix)
+    buildC
+    ;
+
+  inherit (depot.third_party)
+    writeText
+    ;
+
+in
+  buildC.program {
+    name = "clock";
+    buildConf = {
+      static = true;
+      commonFlags =[
+          "-Os"
+        ] ++ buildC.defaultBuildConf.commonFlags;
+    };
+    deps = [
+      depot.users.sterni.su
+    ];
+    srcs = [
+      ./main.c
+    ];
+    CFLAGS = [
+      "-Werror"
+    ];
+    meta.description = "dot time clock";
+  }
