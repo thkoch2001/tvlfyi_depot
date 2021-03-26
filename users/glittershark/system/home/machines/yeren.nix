6c6bf667a33a36705136ed5b48a39e51bbc43d90
Griffin Smith <grfn@gws.fyi>
2021-03-26 15:51:58 -0400

    feat(gs/home): Update discord to 0.0.14
    Change-Id: I115ffcf22f7a3bdbc2641d4a9f7858fd0e049248

--- users/glittershark/system/home/machines/yeren.nix
+++ users/glittershark/system/home/machines/yeren.nix
@@ -28,10 +28,10 @@ in
     xdot
 
     (discord.override rec {
-      version = "0.0.13";
+      version = "0.0.14";
       src = fetchurl {
         url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
-        sha256 = "0d5z6cbj9dg3hjw84pyg75f8dwdvi2mqxb9ic8dfqzk064ssiv7y";
+        sha256 = "1rq490fdl5pinhxk8lkfcfmfq7apj79jzf3m14yql1rc9gpilrf2";
       };
     })
 
