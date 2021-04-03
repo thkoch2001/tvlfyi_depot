4c7012cea5bb6634b80d8241322d9399ad3562d5
Luke Granger-Brown <git@lukegb.com>
2021-04-03 14:13:53 +0000

    chore(3p/gerrit_plugins): attempt to stabilise hashes a bit
    I'm dropping the leaveDotGit and deepClone bits; they were set like that
    purely to try to make the build stamping work. In practice, not only
    does the build stamping not work, but it also means we hit some
    inconveniently-different hashes from time to time when gitiles does...
    something??? on its backend.
    
    I'm also dropping some gcroots for these on whitby, which should also
    help a bit.
    
    Change-Id: Ie6082248393e62795c18b1971fc2d16f4e8cc81d

--- third_party/gerrit_plugins/default.nix
+++ third_party/gerrit_plugins/default.nix
@@ -6,13 +6,11 @@ in depot.nix.utils.drvTargets {
   # https://gerrit.googlesource.com/plugins/owners
   owners = buildGerritBazelPlugin rec {
     name = "owners";
-    depsOutputHash = "sha256:14h39q37lg9bydjqnhr52jakqvrx97jfqfg03l35qyadw780pny2";
+    depsOutputHash = "sha256:1cffbbn687dcl46jm70fd5h6an7jwalssvy0y1aqnhrwmbr4gwi9";
     src = pkgs.fetchgit {
       url = "https://gerrit.googlesource.com/plugins/owners";
       rev = "17817c9e319073c03513f9d5177b6142b8fd567b";
-      sha256 = "sha256:1p089shybp50svckcq51d0hfisjvbggndmvmhh8pvzvi6w8n9d89";
-      deepClone = true;
-      leaveDotGit = true;
+      sha256 = "sha256:06j1wp5xpwabmzyhjjl5rcmgsxqi8ffzgzf8avbr61qzgh76f22n";
     };
     overlayPluginCmd = ''
       chmod +w "$out" "$out/plugins/external_plugin_deps.bzl"
@@ -29,9 +27,7 @@ in depot.nix.utils.drvTargets {
     src = pkgs.fetchgit {
       url = "https://gerrit.googlesource.com/plugins/checks";
       rev = "ab49a63f5c159bda42d9ad1bdb9286bede6c5de4";
-      sha256 = "0ghhmzkfmpn0xc6imvxgsk2wqckxm5g62h0k3bymyl7r48blmrn2";
-      deepClone = true;
-      leaveDotGit = true;
+      sha256 = "sha256:0plvgx61pwksfdr4fpclzm9pxrn4pcydk08jp4y60qd3qlydks3j";
     };
   };
 }
