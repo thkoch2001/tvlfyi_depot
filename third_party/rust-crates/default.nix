ca55e99a3ee39dd30d044c5f406e784757232848
sterni <sternenseemann@systemli.org>
2021-04-04 00:22:37 +0200

    refactor(3p/rust-crates): use imported buildRustCrate everywhere
    A bit less noisy in the definitions and the nix parser can already
    detect it being misspelled.
    
    Change-Id: I979da11471187e36cde5c015aaf654f925757a8b

--- third_party/rust-crates/default.nix
+++ third_party/rust-crates/default.nix
@@ -9,35 +9,35 @@ in
 with depot.third_party.rust-crates;
 
 {
-  cfg-if = pkgs.buildRustCrate {
+  cfg-if = buildRustCrate {
     pname = "cfg-if";
     crateName = "cfg-if";
     version = "1.0.0";
     sha256 = "1fzidq152hnxhg4lj6r2gv4jpnn8yivp27z6q6xy7w6v0dp6bai9";
   };
 
-  cc = pkgs.buildRustCrate {
+  cc = buildRustCrate {
     pname = "cc";
     crateName = "cc";
     version = "1.0.66";
     sha256 = "12q71z6ck8wlqrwgi25x3lrryyks9djymswn9b1c6qq0i01jpc1p";
   };
 
-  ascii = pkgs.buildRustCrate {
+  ascii = buildRustCrate {
     pname = "ascii";
     crateName = "ascii";
     version = "1.0.0";
     sha256 = "0gam8xsn981wfa40srsniivffjsfz1pg0xnigmczk9k7azb1ks1m";
   };
 
-  regex-syntax = pkgs.buildRustCrate {
+  regex-syntax = buildRustCrate {
     pname = "regex-syntax";
     crateName = "regex-syntax";
     version = "0.6.22";
     sha256 = "0r00n2dgyixacl1sczqp18gxf0xh7x272hcdp62412lypba2gqyg";
   };
 
-  regex = pkgs.buildRustCrate {
+  regex = buildRustCrate {
     pname = "regex";
     crateName = "regex";
     version = "1.4.3";
@@ -46,7 +46,7 @@ with depot.third_party.rust-crates;
     sha256 = "0w0b4bh0ng20lf5y8raaxmxj46ikjqpgwy1iggzpby9lhv9vydkp";
   };
 
-  libloading = pkgs.buildRustCrate {
+  libloading = buildRustCrate {
     pname = "libloading";
     crateName = "libloading";
     version = "0.6.7";
@@ -54,7 +54,7 @@ with depot.third_party.rust-crates;
     sha256 = "111d8zsizswnxiqn43vcgnc2ym9spsx1i6pcfp35ca3yw2ixq95j";
   };
 
-  tree-sitter = pkgs.buildRustCrate {
+  tree-sitter = buildRustCrate {
     pname = "tree_sitter";
     crateName = "tree-sitter";
     # buildRustCrate isn’t really smart enough to detect the subdir
@@ -67,21 +67,21 @@ with depot.third_party.rust-crates;
     sha256 = "0jwwbvs4icpra7m1ycvnyri5h3sbw4qrfvgnnvnk72h4w93qhzhr";
   };
 
-  libc = pkgs.buildRustCrate {
+  libc = buildRustCrate {
     pname = "libc";
     crateName = "libc";
     version = "0.2.82";
     sha256 = "02zgn6c0xwh331hky417lbr29kmvrw3ylxs8822syyhjfjqszvsx";
   };
 
-  bitflags = pkgs.buildRustCrate {
+  bitflags = buildRustCrate {
     pname = "bitflags";
     crateName = "bitflags";
     version = "1.2.1";
     sha256 = "0b77awhpn7yaqjjibm69ginfn996azx5vkzfjj39g3wbsqs7mkxg";
   };
 
-  inotify-sys = pkgs.buildRustCrate {
+  inotify-sys = buildRustCrate {
     pname = "inotify-sys";
     crateName = "inotify-sys";
     version = "0.1.5";
@@ -89,7 +89,7 @@ with depot.third_party.rust-crates;
     sha256 = "1yiy577xxhi0j90nbg9nkd8cqwc1xix62rz55jjngvxa5jl5613v";
   };
 
-  inotify = pkgs.buildRustCrate {
+  inotify = buildRustCrate {
     pname = "inotify";
     crateName = "inotify";
     version = "0.9.2";
@@ -98,27 +98,27 @@ with depot.third_party.rust-crates;
     sha256 = "0fcknyvknglwwk1pdzdlb4m0ry2dym1yx8r5prf2v00pxnjk0hv2";
   };
 
-  httparse = pkgs.buildRustCrate {
+  httparse = buildRustCrate {
     pname = "httparse";
     version = "1.3.4";
     crateName = "httparse";
     sha256 = "0dggj4s0cq69bn63q9nqzzay5acmwl33nrbhjjsh5xys8sk2x4jw";
   };
 
-  version-check = pkgs.buildRustCrate {
+  version-check = buildRustCrate {
     pname = "version-check";
     version = "0.9.2";
     crateName = "version-check";
     sha256 = "1vwvc1mzwv8ana9jv8z933p2xzgj1533qwwl5zr8mi89azyhq21v";
   };
 
-  memchr = pkgs.buildRustCrate {
+  memchr = buildRustCrate {
     pname = "memchr";
     version = "2.3.3";
     crateName = "memchr";
     sha256 = "1ivxvlswglk6wd46gadkbbsknr94gwryk6y21v64ja7x4icrpihw";
   };
-  nom = pkgs.buildRustCrate {
+  nom = buildRustCrate {
     pname = "nom";
     version = "5.1.1";
     crateName = "nom";
@@ -128,7 +128,7 @@ with depot.third_party.rust-crates;
     features = [ "std" "alloc" ];
   };
 
-  base64 = pkgs.buildRustCrate {
+  base64 = buildRustCrate {
     pname = "base64";
     version = "0.13.0";
     crateName = "base64";
@@ -137,21 +137,21 @@ with depot.third_party.rust-crates;
     features = [ "alloc" "std" ];
   };
 
-  bufstream = pkgs.buildRustCrate {
+  bufstream = buildRustCrate {
     pname = "bufstream";
     version = "0.1.4";
     crateName = "bufstream";
     sha256 = "10rqm7jly5jjx7wcc19q6q4m2zsrw3l2v3m1054wnbwvdh42xxf1";
   };
 
-  autocfg = pkgs.buildRustCrate {
+  autocfg = buildRustCrate {
     pname = "autocfg";
     version = "1.0.1";
     crateName = "autocfg";
     sha256 = "1lsjz23jdcchcqbsmlzd4iksg3hc8bdvy677jy0938i2gp24frw1";
   };
 
-  num-traits = pkgs.buildRustCrate {
+  num-traits = buildRustCrate {
     pname = "num-traits";
     version = "0.2.14";
     crateName = "num-traits";
@@ -159,7 +159,7 @@ with depot.third_party.rust-crates;
     sha256 = "09ac9dcp6cr57vjzyiy213y7312jqcy84mkamp99zr40qd1gwnyk";
   };
 
-  num-integer = pkgs.buildRustCrate {
+  num-integer = buildRustCrate {
     pname = "num-integer";
     version = "0.1.44";
     crateName = "num-integer";
@@ -168,7 +168,7 @@ with depot.third_party.rust-crates;
     sha256 = "1gdbnfgnivp90h644wmqj4a20yfmdga2xxxacx53pjbcazvfvajc";
   };
 
-  chrono = pkgs.buildRustCrate {
+  chrono = buildRustCrate {
     pname = "chrono";
     version = "0.4.19";
     crateName = "chrono";
@@ -177,7 +177,7 @@ with depot.third_party.rust-crates;
     sha256 = "0cjf5dnfbk99607vz6n5r6bhwykcypq5psihvk845sxrhnzadsar";
   };
 
-  imap-proto = pkgs.buildRustCrate {
+  imap-proto = buildRustCrate {
     pname = "imap-proto";
     version = "0.10.2";
     crateName = "imap-proto";
@@ -185,14 +185,14 @@ with depot.third_party.rust-crates;
     sha256 = "1bf5r4d0z7c8wxrvr7kjy26500wr7cd4sxz49ix3b3yzc6ayyqv1";
   };
 
-  lazy_static = pkgs.buildRustCrate {
+  lazy_static = buildRustCrate {
     pname = "lazy_static";
     version = "1.4.0";
     crateName = "lazy_static";
     sha256 = "13h6sdghdcy7vcqsm2gasfw3qg7ssa0fl3sw7lq6pdkbk52wbyfr";
   };
 
-  imap = pkgs.buildRustCrate {
+  imap = buildRustCrate {
     pname = "imap";
     version = "2.4.0";
     crateName = "imap";
@@ -209,7 +209,7 @@ with depot.third_party.rust-crates;
     sha256 = "1nj6x45qnid98nv637623rrh7imcxk0kad89ry8j5dkkgccvjyc0";
   };
 
-  epoll = pkgs.buildRustCrate {
+  epoll = buildRustCrate {
     pname = "epoll";
     version = "4.3.1";
     crateName = "epoll";
@@ -249,7 +249,7 @@ with depot.third_party.rust-crates;
     edition = "2018";
   };
 
-  log = pkgs.buildRustCrate {
+  log = buildRustCrate {
     pname = "log";
     version = "0.4.11";
     crateName = "log";
@@ -257,14 +257,14 @@ with depot.third_party.rust-crates;
     dependencies = [ cfg-if ];
   };
 
-  serde_derive = pkgs.buildRustCrate {
+  serde_derive = buildRustCrate {
     pname = "serde";
     crateName = "serde";
     version = "1.0.123";
     sha256 = "05xl2s1vpf3p7fi2yc9qlzw88d5ap0z3qmhmd7axa6pp9pn1s5xc";
   };
 
-  mustache = pkgs.buildRustCrate {
+  mustache = buildRustCrate {
     pname = "mustache";
     version = "0.9.0";
     crateName = "mustache";
