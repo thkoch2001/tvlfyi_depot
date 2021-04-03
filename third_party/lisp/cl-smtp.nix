e908ccdbbbe60ba2dfecfb0bad37352d79f8527d
Griffin Smith <grfn@gws.fyi>
2021-04-03 12:10:41 -0400

    feat(3p/lisp): Add cl-smtp
    Change-Id: Idbf63e346b696fb6704390d7a76a2f2b2d3bc190
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/2802
    Tested-by: BuildkiteCI
    Reviewed-by: tazjin <mail@tazj.in>

---
+++ third_party/lisp/cl-smtp.nix
@@ -0,0 +1,28 @@
+{ depot, pkgs, ... }:
+
+let
+  src = pkgs.fetchFromGitLab {
+    domain = "gitlab.common-lisp.net";
+    owner = "cl-smtp";
+    repo = "cl-smtp";
+    rev = "ed47d326fad867ee11323fa3a0f307b5d40e8f2b";
+    sha256 = "0vjjfapcrdc5671jz2d24h8zvpz7skq1x6pi9fvw6ls5sgms6fr0";
+  };
+
+in depot.nix.buildLisp.library {
+  name = "cl-smtp";
+  deps = with pkgs.lisp; [
+    usocket
+    trivial-gray-streams
+    flexi-streams
+    cl-base64
+    cl-plus-ssl
+  ];
+
+  srcs = map (f: src + ("/" + f)) [
+    "package.lisp"
+    "attachments.lisp"
+    "cl-smtp.lisp"
+    "mime-types.lisp"
+  ];
+}
