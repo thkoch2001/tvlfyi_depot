7e0fe7e6d579b4c87c8ef668e02ab28cf57d31c1
sterni <sternenseemann@systemli.org>
2021-04-05 01:53:21 +0200

    feat(web/bubblegum): report some errors to the user via HTTP
    We can actually catch some errors that may be generated in bubblegum
    applications where we can report them to the user in a way that doesn't
    require curl -vv:
    
    * Type errors in the status argument: By removing yants completely we
      not only (presumably) gain some performance, but also the ability to
      return an internal server error on an unexpected type instead of
      throwing.
    
    * User generated evaluation errors: by using builtins.tryEval we can
      catch throws and asserts the user inserted when generating the body
      and report to the user that something went wrong. To do: also support
      for the headers.
    
    Change-Id: I8363b9825c6c730e624eb8016a5482d63cbc1890

--- web/bubblegum/default.nix
+++ web/bubblegum/default.nix
@@ -2,22 +2,10 @@
 
 let
 
-  inherit (depot.nix.yants)
-    defun
-    restrict
-    struct
-    string
-    int
-    attrs
-    enum
-    ;
-
   inherit (depot.nix)
     runExecline
     getBins
     ;
-
-  headers = attrs string;
 
   statusCodes = {
     # 1xx
@@ -90,9 +78,6 @@ let
     "Network Authentication Required" = 511;
   };
 
-  status = enum "bubblegum.status"
-    (builtins.attrNames statusCodes);
-
   /* Generate a CGI response. Takes three arguments:
 
      1. Status of the response as a string which is
@@ -106,19 +91,32 @@ let
 
     Type: Status -> Headers -> Body -> string
   */
-  respond = defun [ status headers string string ]
-    (s: hs: body:
-      let
-        code = status.match s statusCodes;
-        renderedHeaders = lib.concatStrings
-          (lib.mapAttrsToList (n: v: "${n}: ${v}\r\n") hs);
-      in
-        lib.concatStrings [
-          "Status: ${toString code} ${s}\r\n"
-          renderedHeaders
-          "\r\n"
-          body
-        ]);
+  respond = statusArg: headers: bodyArg:
+    let
+      status =
+        if builtins.isString statusArg then {
+          code = statusCodes."${statusArg}" or null;
+          line = statusArg;
+        } else {
+          code = null; line = null;
+        };
+      renderedHeaders = lib.concatStrings
+        (lib.mapAttrsToList (n: v: "${n}: ${toString v}\r\n") headers);
+      internalError = msg: respond 500 {
+        Content-type = "text/plain";
+      } "bubblegum error: ${msg}";
+      body = builtins.tryEval bodyArg;
+    in
+      if status.code == null || status.line == null
+      then internalError "Invalid status ${lib.generators.toPretty {} statusArg}."
+      else if !body.success
+      then internalError "Unknown evaluation error in user code"
+      else lib.concatStrings [
+        "Status: ${toString status.code} ${status.line}\r\n"
+        renderedHeaders
+        "\r\n"
+        body.value
+      ];
 
   /* Returns the value of the `SCRIPT_NAME` environment
      variable used by CGI.
@@ -147,11 +145,10 @@ let
 
      Type: string -> string
   */
-  absolutePath = defun [ string string ]
-    (path:
-      if builtins.substring 0 1 path == "/"
-      then "${scriptName}${path}"
-      else "${scriptName}/${path}");
+  absolutePath = path:
+    if builtins.substring 0 1 path == "/"
+    then "${scriptName}${path}"
+    else "${scriptName}/${path}";
 
   bins = getBins pkgs.coreutils [ "env" "tee" "cat" "printf" "chmod" ]
       // getBins depot.users.sterni.nint [ "nint" ];
