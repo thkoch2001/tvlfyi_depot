25c3cd481bbe65b0c1f8c49fc887e8bb09e14ae2
Profpatsch <mail@profpatsch.de>
2021-04-05 13:56:17 +0200

    feat(third_party/gopkgs): add charmbracelet/{bubbletea,bubbles}
    A small go TUI framework based on the Elm Architecture.
    
    Change-Id: I0c400a7b25af682735bfc5061db179e5d1dd75ab

---
+++ third_party/gopkgs/github.com/charmbracelet/bubbles/default.nix
@@ -0,0 +1,16 @@
+{ depot, pkgs, ... }:
+
+depot.nix.buildGo.external {
+  path = "github.com/charmbracelet/bubbles";
+  src = pkgs.fetchFromGitHub {
+    owner = "charmbracelet";
+    repo = "bubbles";
+    # unreleased version required by bubbletea
+    rev = "v0.7.6";
+    sha256 = "1gd4k4f2mj2dnqcbpdrh9plziz0l29ls6mgyy4mfdcdfijfyd30n";
+  };
+
+  deps = with depot.third_party; [
+    gopkgs."github.com".charmbracelet.bubbletea
+  ];
+}
