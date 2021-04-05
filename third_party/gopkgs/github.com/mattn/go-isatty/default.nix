2b2f364774bc317f7f971ebc237b544c3c05559a
Profpatsch <mail@profpatsch.de>
2021-04-05 13:56:17 +0200

    feat(third_party/gopkgs): add charmbracelet/{bubbletea,bubbles}
    A small go TUI framework based on the Elm Architecture.
    
    Change-Id: I0c400a7b25af682735bfc5061db179e5d1dd75ab

---
+++ third_party/gopkgs/github.com/mattn/go-isatty/default.nix
@@ -0,0 +1,15 @@
+{ depot, pkgs, ... }:
+
+depot.nix.buildGo.external {
+  path = "github.com/mattn/go-isatty";
+  src = pkgs.fetchFromGitHub {
+    owner = "mattn";
+    repo = "go-isatty";
+    rev = "v0.0.12";
+    sha256 = "1dfsh27d52wmz0nmmzm2382pfrs2fcijvh6cgir7jbb4pnigr5w4";
+  };
+
+  deps = with depot.third_party; [
+    gopkgs."golang.org".x.sys.unix
+  ];
+}
