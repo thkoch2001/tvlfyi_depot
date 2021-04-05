8e112ddf6d814d45ef6d8bb58a84214c785b8fa7
Vincent Ambo <mail@tazj.in>
2021-04-05 17:22:48 +0200

    chore(converse): Use naersk for building
    This drops the old Carnix-generated file. Before moving back to
    another builder I want to simplify the dependency tree of this.
    
    Back when it was written, we had a stack that used Actix a lot - but
    Actix has a ridiculous number of dependencies. We can trim this down a
    bit.
    
    Change-Id: I1dde9dc0325da7bdcb6608359fab33e27692dc1d

--- web/converse/default.nix
+++ web/converse/default.nix
@@ -1,18 +1,8 @@
-# This Nix derivation imports the generated Carnix sources and builds
-# Converse.
-#
-# To work around an issue in Carnix ([1] & [2]) the attributes of the
-# comrak crate have been overridden with a dummy environment variable
-# to simulate a Cargo-based build. This requires a manual change to
-# `Cargo.nix` when updating dependencies.
-#
-# [1]: https://nest.pijul.com/pmeunier/carnix/discussions/2
-# [2]: https://nest.pijul.com/pmeunier/carnix/discussions/3
+{ pkgs, ... }:
 
-{ pkgs ? import <nixpkgs> {}, ... }:
-
-let cargo = pkgs.callPackage ./Cargo.nix {};
-in {
-  # Build is not yet fixed up for the depot.
-  meta.ci = false;
+pkgs.naersk.buildPackage {
+  src = ./.;
+  buildInputs = with pkgs; [
+    openssl pkgconfig
+  ];
 }
