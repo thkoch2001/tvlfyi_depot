0ae2fe50bf51808a0a2d47c2c5dfab4cbaa5359d
Griffin Smith <grfn@gws.fyi>
2021-04-07 10:32:01 -0400

    feat(3p/buzz): Init
    Package buzz, a destkop email notifier that uses passive (push-based)
    IMAP connections to get email notifications quickly.
    
    Change-Id: Idbd175db882cdab975b0928cd6920954500954a1

---
+++ third_party/buzz/default.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }:
+
+pkgs.naersk.buildPackage {
+  src = pkgs.fetchFromGitHub {
+    owner = "jonhoo";
+    repo = "buzz";
+    rev = "aede85850bd4b919128da6267c8cf151e7246a35";
+    sha256 = "02z3cqqfaylasqyafv0fbzcfav0vby7nngh642gb4cxrm3jqkz2w";
+    fetchSubmodules = true;
+  };
+
+  buildInputs = with pkgs; [
+    pkgconfig
+    dbus_libs
+    glib
+    openssl
+    cairo
+    pango
+    atk
+    gdk-pixbuf
+    gtk3
+    dbus-glib
+    libappindicator-gtk3
+    clang_11
+    llvmPackages.llvm
+    llvmPackages.bintools
+    llvmPackages.clang
+    llvmPackages.libclang
+  ];
+
+  LIBCLANG_PATH = "${pkgs.llvmPackages.libclang}/lib/libclang.so";
+}
