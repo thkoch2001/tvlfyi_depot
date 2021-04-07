df994ad8d8f6a841d09824ca433eff91545fe685
Vincent Ambo <mail@tazj.in>
2021-04-07 14:34:13 +0200

    chore(3p): Bump channels to 2021-04-09
    Fixes included:
    
    * exposed gtest in the package set, required for protobuf
    * pinned SBCL to version 2.0.8: The channel moved it to >2.1, and a
      bunch of warnings seemed to be killing our builds - we should
      investigate this later.
    * removed kernel patches from //users/tazjin/frog: this machine is
      currently out of service anyways, not worth fixing while it's offline
    * removed steam & lutris from frog (they're currently broken)
    * removed Haskell overrides for hedgehog-classes & hgeometry-combinatorial
    * use gRPC sources from upstream and inject Abseil via Nix instead
    * fix for renamed grpc import in //third_party/nix
    * disable CI for //users/glittershark/fprintd - it doesn't build anymore
    
    Change-Id: Ia90e6f774f7b88bc9e60d28351b900ca43ee2695

--- third_party/nixpkgs-exposed/haskell_overlay/default.nix
+++ third_party/nixpkgs-exposed/haskell_overlay/default.nix
@@ -15,16 +15,4 @@ self: super: with pkgs.haskell.lib; rec {
   test-framework = doJailbreak super.test-framework;
   hashable = doJailbreak super.hashable;
   test-framework-quickcheck2 = doJailbreak super.test-framework-quickcheck2;
-
-  # can be removed if we have the following PR or equivalent
-  # https://github.com/NixOS/nixpkgs/pull/116931
-  hedgehog-classes = overrideCabal super.hedgehog-classes (attrs: {
-    # remove version bound on semirings which is inside a
-    # conditional, so doJailbreak doesn't work
-    prePatch = ''
-      sed -i 's|semirings.*0.6|semirings|g' hedgehog-classes.cabal
-    '';
-  });
-
-  hgeometry-combinatorial = dontCheck super.hgeometry-combinatorial;
 }
