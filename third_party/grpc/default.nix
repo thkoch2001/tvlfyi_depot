b4860dbc9b5c5564f55f883f068e386e2e557df3
Vincent Ambo <mail@tazj.in>
2021-04-07 14:34:13 +0200

    chore(3p): Bump channels to 2021-04-09
    Fixes included:
    
    * exposed gtest in the package set, required for protobuf
    * pinned SBCL to version 2.0.8: The channel moved it to >2.1, and a
      bunch of warnings seemed to be killing our builds - we should
      investigate this later.
    * removed kernel patches from //users/tazjin/frog: this machine is
      currently out of service anyways, not worth fixing while it's offline
    * removed steam & lutris from frog (they're currently broken)
    * removed Haskell overrides for hedgehog-classes & hgeometry-combinatorial
    * use gRPC sources from upstream and inject Abseil via Nix instead
    
    Change-Id: Ia90e6f774f7b88bc9e60d28351b900ca43ee2695

--- third_party/grpc/default.nix
+++ third_party/grpc/default.nix
@@ -3,17 +3,8 @@
 (pkgs.originals.grpc.override {
   protobuf = pkgs.protobuf;
   stdenv = pkgs.llvmPackages.libcxxStdenv;
+  abseil-cpp = pkgs.abseil_cpp;
 }).overrideAttrs(orig: rec {
-  version = "1.30.0";
-
-  src = pkgs.fetchFromGitHub {
-    owner = "grpc";
-    repo = "grpc";
-    rev = "v${version}";
-    sha256 = "01w5jdp318i7ncxbkfv75q0mf0rd3qwfr1ycdd3850nv6mprv7n0";
-    fetchSubmodules = true;
-  };
-
   cmakeFlags = orig.cmakeFlags ++ [
     "-DCMAKE_CXX_STANDARD=17"
     "-DCMAKE_CXX_STANDARD_REQUIRED=ON"
