49305cc721c5614e6d0e8cc75f8b1faca0d5638f
Vincent Ambo <mail@tazj.in>
2021-04-09 17:12:15 +0200

    feat(ops/pipelines): Add gcroots for depot builds on canon
    TODO
    
    This fixes b/102
    
    Change-Id: Id3d0bf4158bffcb1ed6929888a29d31609b6ece1

--- ops/pipelines/depot.nix
+++ ops/pipelines/depot.nix
@@ -27,16 +27,21 @@ let
       then "${label}:${target.__subtarget}"
       else label;
 
+  # Shell snippet for this build's temporary gcroot folder.
+  gcrootDir = "/tmp/$BUILDKITE_JOB_ID";
+
   # Create a pipeline step from a single target.
   #
   # If the build fails, Buildkite metadata is updated to mark the
   # pipeline as failed. Buildkite has a concept of a failed pipeline
   # regardless, but this data is not accessible.
-  mkStep = target: {
+  mkStep = target: let label = mkLabel target; in {
     command = ''
-      nix-build -E '${mkBuildExpr target}' || (buildkite-agent meta-data set "failure" "1"; exit 1)
+      nix-store --indirect --add-root ${gcrootDir}/label --realise \
+        $(nix-instantiate -E '${mkBuildExpr target}') || \
+        (buildkite-agent meta-data set "failure" "1"; exit 1)
     '';
-    label = ":nix: ${mkLabel target}";
+    label = ":nix: ${label}";
   };
 
   # Protobuf check step which validates that changes to .proto files
