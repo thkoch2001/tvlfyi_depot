378412a7b6f7220d7d17dc0eb629efb8a7dbc20d
Vincent Ambo <mail@tazj.in>
2021-04-10 18:05:16 +0200

    refactor: Move nixpkgs attribute to `third_party.nixpkgs`
    Please read b/108 to make sense of this.
    
    This gets rid of the explicit list of exposed packages from nixpkgs,
    and instead makes the entire package set available at
    `third_party.nixpkgs`.
    
    To accommodate this, a LOT of things have to be very slightly shuffled
    around. Some of this was done in already submitted CLs, but this
    change is unfortunately still quite noisy.
    
    Pay extra attention to:
    
    * overlay-like functionality that was partially moved to actual
      overlays (partially as in, the minimum required to get a green
      build)
    
    * modified uses of the package set path, esp. in NixOS systems
    
    Change-Id: I2f9c60a4d275fdb5209264be0addfd7e06c53118

--- third_party/protobuf/default.nix
+++ third_party/protobuf/default.nix
@@ -1,7 +1,7 @@
 # Pin protobuf to version 3.12, with LLVM.
 { depot, pkgs, ... }:
 
-pkgs.callPackage "${depot.third_party.nixpkgsSrc}/pkgs/development/libraries/protobuf/generic-v3.nix" {
+pkgs.callPackage "${pkgs.path}/pkgs/development/libraries/protobuf/generic-v3.nix" {
   version = "3.12.2";
   sha256 = "1lp368aa206vpic9fmax4k6llnmf28plfvkkm4vqhgphmjqykvl2";
   stdenv = pkgs.llvmPackages.libcxxStdenv;
