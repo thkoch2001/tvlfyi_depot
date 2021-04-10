e978d2384442d7ebbb8f5450a54863610f841340
sterni <sternenseemann@systemli.org>
2021-04-10 23:30:09 +0200

    refactor(users/gs): build all haskell packages without ifd
    Since we need to override random to 1.2.0 globally for xanthous
    (otherwise propagation causes two versions of random to be propagated
    for xanthous) evaluating haskell related stuff using import from
    derivation can be very expensive since utilities like hpack and
    cabal2nix need to be built for that. This means that for every channel
    bump we potentially need to do a world rebuild of haskellPackages first.
    
    To solve this we check in the cabal2nix-generated nix expressions for
    owothia and xanthous.
    
    Change-Id: I8fff70b4b6c303d1920f8bcac53520a09999b206

---
+++ users/glittershark/owothia/pkg.nix
@@ -0,0 +1,16 @@
+{ mkDerivation, base, bytestring, chatter, containers, envy
+, irc-client, lens, lib, random, relude, text
+}:
+mkDerivation {
+  pname = "owothia";
+  version = "0.0.1.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    base bytestring chatter containers envy irc-client lens random
+    relude text
+  ];
+  license = "unknown";
+  hydraPlatforms = lib.platforms.none;
+}
