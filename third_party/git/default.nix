8166ba03445b9df7cfabba08fd2d2dbebc11af60
Vincent Ambo <mail@tazj.in>
2021-04-10 18:05:16 +0200

    refactor: Move nixpkgs attribute to `third_party.nixpkgs`
    Please read b/108 to make sense of this.
    
    This gets rid of the explicit list of exposed packages from nixpkgs,
    and instead makes the entire package set available at
    `third_party.nixpkgs`.
    
    To accommodate this, a LOT of things have to be very slightly shuffled
    around. Some of this was done in already submitted CLs, but this
    change is unfortunately still quite noisy.
    
    Pay extra attention to:
    
    * overlay-like functionality that was partially moved to actual
      overlays (partially as in, the minimum required to get a green
      build)
    
    * modified uses of the package set path, esp. in NixOS systems
    
    Change-Id: I2f9c60a4d275fdb5209264be0addfd7e06c53118

--- third_party/git/default.nix
+++ third_party/git/default.nix
@@ -1,15 +1,13 @@
 # Use the upstream git derivation (there's a lot of stuff happening in
 # there!) and just override the source:
-{ depot, ... }:
+{ pkgs, ... }:
 
-with depot.third_party;
-
-(originals.git.overrideAttrs(_: {
+(pkgs.git.overrideAttrs(_: {
   version = "2.29.2";
   src = ./.;
   doInstallCheck = false;
   preConfigure = ''
-    ${autoconf}/bin/autoreconf -i
+    ${pkgs.autoconf}/bin/autoreconf -i
   '';
 })).override {
   sendEmailSupport = true;
