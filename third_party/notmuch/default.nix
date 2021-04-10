8c1722ef4c2ac4ff10c3e87accf863087ca7e4b5
Vincent Ambo <mail@tazj.in>
2021-04-10 18:05:16 +0200

    refactor: Move nixpkgs attribute to `third_party.nixpkgs`
    Please read b/108 to make sense of this.
    
    This gets rid of the explicit list of exposed packages from nixpkgs,
    and instead makes the entire package set available at
    `third_party.nixpkgs`.
    
    To accommodate this, a LOT of things have to be very slightly shuffled
    around. Some of this was done in already submitted CLs, but this
    change is unfortunately still quite noisy.
    
    Pay extra attention to:
    
    * overlay-like functionality that was partially moved to actual
      overlays (partially as in, the minimum required to get a green
      build)
    
    * modified uses of the package set path, esp. in NixOS systems
    
    Change-Id: I2f9c60a4d275fdb5209264be0addfd7e06c53118

--- third_party/notmuch/default.nix
+++ third_party/notmuch/default.nix
@@ -1,6 +1,7 @@
+# Notmuch, but with support for https://dotti.me/
 { pkgs, ... }:
 
-pkgs.originals.notmuch.overrideAttrs(old: {
+pkgs.notmuch.overrideAttrs(old: {
   doCheck = false;
   patches = [ ./dottime.patch ] ++ (if old ? patches then old.patches else []);
 })
