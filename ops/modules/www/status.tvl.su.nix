f3cc0edaf228ec91b6efddf09135e02ffc592bac
Vincent Ambo <mail@tazj.in>
2021-04-12 17:59:51 +0200

    feat(whitby): Enable Grafana at status.tvl.su
    Enables a Grafana service pointing to whitby's local Prometheus
    instance, accessible at status.tvl.su.
    
    I've no idea how to configure Grafana and if it's possible to link it
    to CAS, but we'll see about that later.
    
    Notes:
    * the explicit fixpoint for whitby config has been removed as we
      have the `config` parameter available now
    * backups are enabled for the Grafana storage location
    
    Change-Id: If5ffe0c1a3378d1c88529129487c643642705fd2

---
+++ ops/modules/www/status.tvl.su.nix
@@ -0,0 +1,25 @@
+{ config, ... }:
+
+{
+  imports = [
+    ./base.nix
+  ];
+
+  config = {
+    services.nginx.virtualHosts."status-fyi" = {
+      serverName = "status.tvl.fyi";
+      enableACME = true;
+      extraConfig = "return 302 https://status.tvl.fyi$request_uri;";
+    };
+
+    services.nginx.virtualHosts.grafana = {
+      serverName = "status.tvl.su";
+      enableACME = true;
+      forceSSL = true;
+
+      locations."/" = {
+        proxyPass = "http://localhost:${toString config.services.grafana.port}";
+      };
+    };
+  };
+}
