4d48c2ef225d3d3ed00661e44df5a6ad31bf5dce
sterni <sternenseemann@systemli.org>
2021-04-12 19:30:41 +0200

    feat(sterni/nix/fun): add hasEllipsis
    As a complementation to builtins.functionArgs this function checks if
    the function has a set pattern that contains an ellipsis
    (i. e. `{ [arg, [ arg1, [ … ]]] ... }:`). The implementation of this is
    pretty cursed however since there is no clean way to do this in vanilla
    nix: We need to match on the output of builtins.toXML which does try to
    serialize functions by outputting their argument and information about
    it (whether it is a normal argument or a attribute set pattern, in the
    latter case it also serialize every component of the pattern).
    
    Change-Id: I0f33721811a3180cec205a0c98e6d92e10e92075

---
+++ users/sterni/nix/fun/tests/default.nix
@@ -0,0 +1,29 @@
+{ depot, ... }:
+
+let
+  inherit (depot.nix.runTestsuite)
+    runTestsuite
+    it
+    assertEq
+    ;
+
+  inherit (depot.users.sterni.nix)
+    fun
+    ;
+
+  hasEllipsisTests = it "checks fun.hasEllipsis" [
+    (assertEq "Malicious string" false
+      (fun.hasEllipsis (builtins.toXML ({ foo, ... }: 12))))
+    (assertEq "No function" false
+      (fun.hasEllipsis 23))
+    (assertEq "No attribute set pattern" false
+      (fun.hasEllipsis (a: a + 2)))
+    (assertEq "No ellipsis" false
+      (fun.hasEllipsis ({ foo, bar }: foo + bar)))
+    (assertEq "Ellipsis" true
+      (fun.hasEllipsis ({ depot, pkgs, ... }: 42)))
+  ];
+in
+  runTestsuite "nix.fun" [
+    hasEllipsisTests
+  ]
