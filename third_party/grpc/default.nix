3dde469d2755f166113058b25a057aadc6476540
Vincent Ambo <mail@tazj.in>
2021-04-13 12:21:42 +0200

    chore(nixpkgs): Bump channels to 2021-04-13
    * users/grfn/system/home/yeren: remove obsolete awscli2 overrides
    
    Change-Id: Iff33b0ecb0ef07a82d1de35e23c40d2f4bf0f8ed

--- third_party/grpc/default.nix
+++ third_party/grpc/default.nix
@@ -4,6 +4,16 @@
   protobuf = depot.third_party.protobuf;
   stdenv = pkgs.llvmPackages.libcxxStdenv;
   abseil-cpp = depot.third_party.abseil_cpp;
+  re2 = pkgs.re2.overrideAttrs (old: {
+    # TODO(sterni): update upstream, so this isn't necessary
+    version = "20210401";
+    src = pkgs.fetchFromGitHub {
+      owner = "google";
+      repo = "re2";
+      rev = "2021-04-01";
+      sha256 = "1iia0883lssj7ckbsr0n7yb3gdw24c8wnl2q5hhzlml23h4ipbh3";
+    };
+  });
 }).overrideAttrs(orig: rec {
   cmakeFlags = orig.cmakeFlags ++ [
     "-DCMAKE_CXX_STANDARD=17"
