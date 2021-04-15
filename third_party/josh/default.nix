13bf949f00c418830557e7639e747f6f9ac07279
Vincent Ambo <mail@tazj.in>
2021-04-15 16:45:51 +0200

    feat(3p/josh): Import and build josh
    We've had josh in here previously, but it was kind of immature back
    then. The repository looks much better now and I'd like to give it
    another try.
    
    Josh is a Rust project, the build here is done with naersk.
    
    Change-Id: I3731340d00ce1eb4cef55de114e1915579e47ef3

---
+++ third_party/josh/default.nix
@@ -0,0 +1,25 @@
+# https://github.com/esrlabs/josh
+{ depot, pkgs, ... }:
+
+let
+  gitSrc = pkgs.fetchFromGitHub {
+    owner = "esrlabs";
+    repo = "josh";
+    rev = "1c1965deb0278de92452a597c092f16b531274ae";
+    sha256 = "0a0ybx4dqr9gfcn1kqy2nirdsfbdwkfz24ljnri564c5381ccldw";
+  };
+
+  src = pkgs.applyPatches {
+    name = "josh-src";
+    src = gitSrc;
+    patches = [
+      ./pin-git2-rev.patch
+    ];
+  };
+in depot.third_party.naersk.buildPackage {
+  inherit src;
+  buildInputs = with pkgs; [
+    libgit2
+    pkgconfig
+  ];
+}
