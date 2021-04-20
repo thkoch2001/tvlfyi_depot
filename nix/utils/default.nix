9fe79da058073a12dfe22f702f6f3153b330842c
sterni <sternenseemann@systemli.org>
2021-04-20 10:18:28 +0200

    feat(nix/utils): add storePathName, a more generic baseNameOf
    This is a wrapper around baseNameOf which also can deal with
    derivations. Added to //nix/utils since I've found myself introducing an
    ad-hoc implementation of this for both //web/bubblegum and //nix/buildC.
    
    Change-Id: I2fcd97a150d6eda21ab323fa0d881ff7442a892e

--- nix/utils/default.nix
+++ nix/utils/default.nix
@@ -17,8 +17,29 @@ let
       } // (attrs.meta or {});
     };
 
+  /* Get the basename of a store path without
+     the leading hash.
+
+     Type: (path | drv | string) -> string
+
+     Example:
+       storePathName ./foo.c
+       => "foo.c"
+
+       storePathName (writeText "foo.c" "int main() { return 0; }")
+       => "foo.c"
+  */
+  storePathName = p:
+    if builtins.isPath p || builtins.isString p
+    then builtins.baseNameOf p
+    else if lib.isDerivation p
+    then p.name
+    else builtins.throw "Don't know how to get (base)name of "
+      + lib.generators.toPretty {} p;
+
 in {
   inherit
     drvTargets
+    storePathName
     ;
 }
