d2188b95af2610a3ac3c1e03abbb15984a1de010
sterni <sternenseemann@systemli.org>
2021-04-20 10:18:28 +0200

    feat(nix/utils): add storePathName, a more generic baseNameOf
    This is a wrapper around baseNameOf which also can deal with
    derivations. Added to //nix/utils since I've found myself introducing an
    ad-hoc implementation of this for both //web/bubblegum and //nix/buildC.
    
    Change-Id: I2fcd97a150d6eda21ab323fa0d881ff7442a892e

--- nix/utils/default.nix
+++ nix/utils/default.nix
@@ -17,8 +17,50 @@ let
       } // (attrs.meta or {});
     };
 
+  /* Get the basename of a store path without
+     the leading hash.
+
+     Type: (path | drv | string) -> string
+
+     Example:
+       storePathName ./foo.c
+       => "foo.c"
+
+       storePathName (writeText "foo.c" "int main() { return 0; }")
+       => "foo.c"
+
+       storePathName "${hello}/bin/hello"
+       => "hello"
+  */
+  storePathName = p:
+    if lib.isDerivation p
+    then p.name
+    else if builtins.isPath p
+    then builtins.baseNameOf p
+    else if builtins.isString p
+    then
+      let
+        # strip leading storeDir and trailing slashes
+        noStoreDir = lib.removeSuffix "/"
+          (lib.removePrefix "${builtins.storeDir}/" p);
+        basename = builtins.unsafeDiscardStringContext
+          (builtins.baseNameOf p);
+      in
+        if noStoreDir == basename
+        # p is direct child of storeDir, so we need to remove
+        # the leading hash as well to make this useless to
+        # circumvent the string context.
+        then builtins.substring 33 (-1) basename
+        # a basename of a child of a store path isn't really
+        # referring to a store path, so removing the string
+        # context is safe (e. g. "hello" for "${hello}/bin/hello").
+        else basename
+    else builtins.throw "Don't know how to get (base)name of "
+      + lib.generators.toPretty {} p;
+
 in {
   inherit
     drvTargets
+    storePathName
     ;
 }
