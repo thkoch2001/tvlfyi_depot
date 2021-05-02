ffb9f11cc92bef563355cb5d34bb72de3bcde56f
Vincent Ambo <mail@tazj.in>
2021-05-02 16:37:23 +0200

    chore(3p/josh): Bump version to latest HEAD
    Change-Id: Ib8026f5f22e2e49371a2bad83aa726c2951570e4

--- third_party/josh/default.nix
+++ third_party/josh/default.nix
@@ -2,20 +2,20 @@
 { depot, pkgs, ... }:
 
 let
-  gitSrc = pkgs.fetchFromGitHub {
+  src = pkgs.fetchFromGitHub {
     owner = "esrlabs";
     repo = "josh";
-    rev = "1c1965deb0278de92452a597c092f16b531274ae";
-    sha256 = "0a0ybx4dqr9gfcn1kqy2nirdsfbdwkfz24ljnri564c5381ccldw";
+    rev = "92fd7fb193d0df25b106207675f233cfe1d26d0d";
+    sha256 = "0fzy9xbr0vnw239gl8famrqncn8pxlnwkfvy3gxl7c80d78yg9j7";
   };
 
-  src = pkgs.applyPatches {
-    name = "josh-src";
-    src = gitSrc;
-    patches = [
-      ./pin-git2-rev.patch
-    ];
-  };
+  # src = pkgs.applyPatches {
+  #   name = "josh-src";
+  #   src = gitSrc;
+  #   patches = [
+  #     ./pin-git2-rev.patch
+  #   ];
+  # };
 in depot.third_party.naersk.buildPackage {
   inherit src;
 
