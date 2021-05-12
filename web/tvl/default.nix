b84f6ef0469e708df723256d2cbd92744cbdd200
sterni <sternenseemann@systemli.org>
2021-05-12 16:57:07 +0200

    refactor(web/tvl): translate to markdown
    Unfortunately, due to cheddar quirks we can't embed the svg anymore and
    can't have the main tag around the first image as well (it doesn't like
    markdown inside HTML apparently).
    
    Change-Id: If9ed516623e81e24f600ee9f1b6d4d611b5bcedd

--- web/tvl/default.nix
+++ web/tvl/default.nix
@@ -12,24 +12,20 @@ let
   homepage = depot.web.tvl.template {
     title = "The Virus Lounge";
     content = ''
-      <main>
-        <img alt="The Virus Lounge" src="/static/virus_lounge.webp">
-      </main>
+      ![The Virus Lounge](/static/virus_lounge.webp)
 
-      <p>
-        Welcome to <b>The Virus Lounge</b>. We're a random group of
-        people who feel undersocialised in these trying times, and
-        we've decided that there isn't enough spontaneous socialising
-        on the internet.
-      </p>
+      Welcome to **The Virus Lounge**. We're a random group of
+      people who feel undersocialised in these trying times, and
+      we've decided that there isn't enough spontaneous socialising
+      on the internet.
 
       <hr>
-      <h2>Where did all these people come from?</h2>
 
-      <p>
-        It's pretty straightforward. Feel free to click on people, too.
-      </p>
-      ${builtins.readFile tvlGraph}
+      ## Where did all these people come from?
+
+      It's pretty straightforward. Feel free to click on people, too.
+
+      ![The TVL Graph](/static/tvl.svg)
     '';
     extraHead = ''
       <style>
@@ -44,6 +40,7 @@ in runCommandNoCC "website" {} ''
   mkdir -p $out/static
   cp ${homepage} $out/index.html
   cp -r ${./static}/* $out/static
+  cp ${tvlGraph} $out/static/tvl.svg
 
   # Some assets are stolen from tazjin's blog
   cp ${depot.users.tazjin.homepage}/static/jetbrains-* $out/static
