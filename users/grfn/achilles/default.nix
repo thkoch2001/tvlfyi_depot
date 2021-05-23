a944b59deefde3c945390be3e821a6fdd38b8050
Vincent Ambo <mail@tazj.in>
2021-05-23 13:33:59 +0200

    chore(3p/overlays): Remove LLVM11 bump from overlay
    This was mostly for //third_party/nix and its dependencies which also
    compiles with LLVM 7.1.0 it seems.
    
    For //users/grfn/achilles we can manually select the newer LLVM version.
    
    For the future we may want to have something similar again, but it may
    not be necessary to invest too much time into it: nixpkgs is set to
    upgrade their default llvmPackages to LLVM 11 as well at some point in
    the near future.
    
    Change-Id: Id83868dbc476a6c776b59518b856c933f30ea79d

--- users/grfn/achilles/default.nix
+++ users/grfn/achilles/default.nix
@@ -1,20 +1,25 @@
 { depot, pkgs, ... }:
 
+let
+  llvmPackages = pkgs.llvmPackages_11;
+in
+
 depot.third_party.naersk.buildPackage {
   src = ./.;
 
-  buildInputs = with pkgs; [
-    clang_11
+  buildInputs = [
+    llvmPackages.clang
     llvmPackages.llvm
     llvmPackages.bintools
     llvmPackages.clang
     llvmPackages.libclang.lib
+  ] ++ (with pkgs; [
     zlib
     ncurses
     libxml2
     libffi
     pkgconfig
-  ];
+  ]);
 
   doCheck = true;
 }
