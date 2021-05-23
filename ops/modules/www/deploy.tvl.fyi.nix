14e9be31db166867c046ef48dcc8afda032655d1
Griffin Smith <grfn@gws.fyi>
2021-05-23 18:18:40 +0200

    feat(whitby): Add static HTML dir for deploy.tvl.fyi
    Add a new domain and nginx virtual host at deploy.tvl.fyi, serving out
    of a static directory on whitby which there is a systemd oneshot service
    to create. This will be used to serve diffs rendered by nix-diff for
    pending deploys for whitby
    
    Refs: b/110
    Change-Id: I5869d40800bbf5fb8fb39878a857f66ff5787830

---
+++ ops/modules/www/deploy.tvl.fyi.nix
@@ -0,0 +1,27 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    ./base.nix
+  ];
+
+  config = {
+    systemd.services.ensure-deploy-dir = {
+      description = "Ensure the html directory for deploy.tvl.fyi exists";
+      wantedBy = [ "multi-user.target" ];
+      script = "mkdir -p /srv/www/deploy.tvl.fyi";
+
+      serviceConfig = {
+        User = "nginx";
+        Group = "nginx";
+        Type = "oneshot";
+      };
+    };
+
+    services.nginx.virtualHosts."deploy.tvl.fyi" = {
+      enableACME = true;
+      forceSSL = true;
+      root = "/srv/www/deploy.tvl.fyi";
+    };
+  };
+}
