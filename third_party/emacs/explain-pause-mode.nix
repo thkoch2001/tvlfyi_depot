252283ae8f638c53b4b800d09f1b9d5c97671b18
sterni <sternenseemann@systemli.org>
2021-07-16 21:13:04 +0200

    chore(3p/nixpkgs): update channels to 2021-07-25
    * 3p/buzz: bump to latest master (1.6.0)
    
    * 3p/emacs/explain-pause-mode: adjust to package-build update
      MELPA's package build now cares about git revisions, but calling VC
      commands in a nix build is usually a bad idea. Thus upstream nixpkgs
      passes `$commit` to the `buildPhase` and otherwise fails with an
      error message that doesn't really point to the issue. Upstream change:
      https://github.com/NixOS/nixpkgs/commit/9140d4b06ff09bce8dd8e384eeef832e7811d288
    
    * 3p/overlays/emacs: udpate to 2021-07-25 to support the package-build
      update. Without this emacsPackages.xelb (for tazjin's exwm) would fail.
    
    Change-Id: I7cd782fe7d66ed4ea78c529b79fe761d921f46a8

--- third_party/emacs/explain-pause-mode.nix
+++ third_party/emacs/explain-pause-mode.nix
@@ -2,9 +2,13 @@
 
 let
   inherit (pkgs) emacsPackages fetchFromGitHub;
-in emacsPackages.melpaBuild {
+
+  commit = "35f7d780a9c164b5c502023746473b1de3857904";
+
+in emacsPackages.melpaBuild rec {
   pname = "explain-pause-mode";
   version = "0.1"; # master on 20200603
+  inherit commit;
 
   recipe = builtins.toFile "recipe.el" ''
     (explain-pause-mode :fetcher github
@@ -14,7 +18,7 @@ in emacsPackages.melpaBuild {
   src = fetchFromGitHub {
     owner = "lastquestion";
     repo = "explain-pause-mode";
-    rev = "35f7d780a9c164b5c502023746473b1de3857904";
+    rev = commit;
     sha256 = "0d9lwzqqwmz0n94i7959rj7m24265yf3825a5g8cd7fyzxznl1pc";
   };
 }
