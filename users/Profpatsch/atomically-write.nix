56d4c0f9f2653a8e723a2f566e54362061d77016
Profpatsch <mail@profpatsch.de>
2021-08-01 18:41:18 +0200

    feat(users/Profpatsch): add atomically-write
    A little shell script to atomically write stdout to a file.
    
    Change-Id: Icca58909c9ad3f92d69af2f5e20c08d69878a77c

---
+++ users/Profpatsch/atomically-write.nix
@@ -0,0 +1,28 @@
+{ depot, pkgs, ... }:
+# Atomically write a file (just `>` redirection in bash
+# empties a file even if the command crashes).
+#
+# Maybe there is an existing tool for that?
+# But it’s easy enough to implement.
+#
+# Example:
+#   atomically-write
+#     ./to
+#     echo "foo"
+#
+# will atomically write the string "foo" into ./to
+let
+  atomically-write = pkgs.writers.writeDash "atomically-write" ''
+    set -e
+    to=$1
+    shift
+    # assumes that the tempfile is on the same file system, (or in memory)
+    # for the `mv` at the end to be more-or-less atomic.
+    tmp=$(${pkgs.coreutils}/bin/mktemp -d)
+    trap 'rm -r "$tmp"' EXIT
+    "$@" \
+      > "$tmp/out"
+    mv "$tmp/out" "$to"
+  '';
+
+in atomically-write
