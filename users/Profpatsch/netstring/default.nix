e4e6a54cfdfb4e6ae19b76cf820420d11429d0d1
Profpatsch <mail@profpatsch.de>
2021-08-01 18:59:33 +0200

    feat(users/Profpatsch/netstring): add nix toNetstring* functions
    We can easily construct netstrings from nix strings, which is super
    helpful to inject proper structured data into execline scripts.
    
    Change-Id: Ie8bffed285896223db83652eaea868ece43592b9

--- users/Profpatsch/netstring/default.nix
+++ users/Profpatsch/netstring/default.nix
@@ -1,5 +1,17 @@
 { lib, pkgs, depot, ... }:
 let
+  toNetstring = s:
+    "${toString (builtins.stringLength s)}:${s},";
+
+  toNetstringList = xs:
+    lib.concatStrings (map toNetstring xs);
+
+  toNetstringKeyVal = attrs:
+    lib.concatStrings
+      (lib.mapAttrsToList
+        (k: v: toNetstring (toNetstring k + toNetstring v))
+        attrs);
+
   python-netstring = depot.users.Profpatsch.writers.python3Lib {
     name = "netstring";
   } ''
