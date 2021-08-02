49599810a27f9be46a2fe30a8d191af467780249
sterni <sternenseemann@systemli.org>
2021-08-02 15:14:25 +0200

    feat(3p/lisp/sclf): build using buildLisp
    Adding the default.nix is quite straightforward, however we have to make
    today's SBCL happy: due to package locking it no longer likes sclf using
    an sb-impl internal constant for some reason. This is however a good
    opportunity to clean up the stat-*-time code: It converted the times in
    an implementation specific way even though time.lisp does provide a
    generic way to convert between unix and universal time. Note that the
    updated ASDF file is untested, but should be a trivial enough change.
    
    Change-Id: If193bf830ac704cc53e0855d8e9fff2b5a5ef291

---
+++ third_party/lisp/sclf/default.nix
@@ -0,0 +1,22 @@
+# Copyright (C) 2021 by the TVL Authors
+# SPDX-License-Identifier: LGPL-2.1-or-later
+{ depot, pkgs, ... }:
+
+depot.nix.buildLisp.library {
+  name = "sclf";
+
+  deps = [
+    (depot.nix.buildLisp.bundled "sb-posix")
+  ];
+
+  srcs = [
+    ./package.lisp
+    ./sclf.lisp
+    ./sysproc.lisp
+    ./lazy.lisp
+    ./time.lisp
+    ./directory.lisp
+    ./serial.lisp
+    ./mp/sbcl.lisp
+  ];
+}
