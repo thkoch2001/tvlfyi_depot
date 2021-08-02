7172dbf617269adc7d7a4423b82ea06c342a4d1d
sterni <sternenseemann@systemli.org>
2021-08-02 15:13:05 +0200

    feat(sterni/mblog): process apple notes messages from maildir
    Change-Id: Iee6d1558e939b932da1e70ca2d2ae75638d855df

---
+++ users/sterni/mblog/default.nix
@@ -0,0 +1,33 @@
+{ depot, pkgs, ... }:
+
+let
+  mblog = depot.nix.buildLisp.program {
+    name = "mblog";
+
+    srcs = [
+      ./package.lisp
+      ./transformer.lisp
+      ./mblog.lisp
+    ];
+
+    deps = [
+      (depot.nix.buildLisp.bundled "asdf") # for UIOP
+      depot.third_party.lisp.alexandria
+      depot.third_party.lisp.closure-html
+      depot.third_party.lisp.cl-who
+      depot.third_party.lisp.mime4cl
+    ];
+
+    main = "mblog:main";
+  };
+in
+
+pkgs.runCommandNoCC "mblog" {
+  inherit mblog;
+  nativeBuildInputs = [ pkgs.buildPackages.makeWrapper ];
+} ''
+  mkdir -p "$out/bin"
+  for bin in mblog mnote-html; do
+    ln -s "$mblog/bin/mblog" "$out/bin/$bin"
+  done
+''
