e59cdf94f60dbced98992fcd55ca305fedd74c35
Vincent Ambo <mail@tazj.in>
2021-08-06 14:53:41 +0300

    feat(ops/modules): Add module for running Nixery
    This sets up a very simple Nixery instance with some things lacking:
    
    * no support for garbage-collecting image fragments (yet)
    * no popularity setup
    
    The plan is to use this to get the ball rolling on a separate
    domain (e.g. images.tvl.fyi), iron things out and then look into
    flipping over nixery.dev
    
    Change-Id: Ic594809f9d487fec7a0f632d608752a3f9c61315

---
+++ ops/modules/nixery.nix
@@ -0,0 +1,43 @@
+# NixOS module to run Nixery, currently with local-storage as the
+# backend for storing/serving image layers.
+{ depot, config, lib, pkgs, ... }:
+
+let
+  cfg = config.services.depot.nixery;
+  description = "Nixery - container images on-demand";
+in {
+  options.services.depot.nixery = {
+    enable = lib.mkEnableOption description;
+
+    port = lib.mkOption {
+      type = lib.types.int;
+      default = 45243; # "image"
+      description = "Port on which Nixery should listen";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    systemd.services.nixery = {
+      inherit description;
+      wantedBy = [ "multi-user.target" ];
+
+      serviceConfig = {
+        DynamicUser = true;
+        StateDirectory = "nixery";
+        Restart = "always";
+        ExecStart = "${depot.third_party.nixery.nixery-server}/bin/nixery"
+      };
+
+      environment = {
+        PORT = toString cfg.port;
+        NIXERY_PKGS_PATH = depot.path;
+        NIXERY_STORAGE_BACKEND = "filesystem";
+        NIX_TIMEOUT = "60"; # seconds
+
+        # TODO(tazjin): Consider prefixing with the commit ID, to be
+        # able to garbage-collect things a little easier?
+        STORAGE_PATH = "/var/lib/nixery";
+      };
+    };
+  };
+}
