a4243c900aa7d7dd6ab0e90e0e7418dfe49fe5a2
Vincent Ambo <mail@tazj.in>
2021-08-11 12:04:42 +0300

    chore(3p/overlays): Bump Emacs overlay to 2021-08-11
    Maybe this one will include a fixed telega.el?
    
    Change-Id: I0de2dafdc8e5b82ed1174e83023cd96fab5f190e
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/3300
    Tested-by: BuildkiteCI
    Reviewed-by: sterni <sternenseemann@systemli.org>

--- third_party/overlays/emacs.nix
+++ third_party/overlays/emacs.nix
@@ -2,10 +2,10 @@
 { ... }:
 
 let
-  # from 2021-08-06
-  commit = "e37266bc538b0562b9c1ffb3618ba265cb6768e9";
+  # from 2021-08-11
+  commit = "3fbe6cf3459cf955e188444b68f085f7a45b6ffa";
   src = builtins.fetchTarball {
     url = "https://github.com/nix-community/emacs-overlay/archive/${commit}.tar.gz";
-    sha256 = "05m13nw5chmd9gnrxz2c4yndp3w4pf4rrci84yk99zkijmkvsd6i";
+    sha256 = "0hm7lkqxh08nq074fgn0naqr6lg12qqxwsr3i4wpvdskmdbhb5i5";
   };
 in import src
