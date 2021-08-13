2ca7fd20b97ab6094b840eed49a1ab04428541e7
Vincent Ambo <mail@tazj.in>
2021-08-13 03:50:10 +0300

    feat(ops/www): Point images.tvl.* at Nixery
    Change-Id: I39f979c68e7b74f6da6a7da0f07aaa470886d451

---
+++ ops/modules/www/images.tvl.fyi.nix
@@ -0,0 +1,22 @@
+{ config, ... }:
+
+{
+  imports = [
+    ./base.nix
+  ];
+
+  config = {
+    services.nginx.virtualHosts."images.tvl.fyi" = {
+      serverName = "images.tvl.fyi";
+      serverAliases = [ "images.tvl.su" ];
+      enableACME = true;
+      forceSSL = true;
+
+      extraConfig = ''
+        location / {
+          proxy_pass http://localhost:${toString config.services.depot.nixery.port};
+        }
+      '';
+    };
+  };
+}
