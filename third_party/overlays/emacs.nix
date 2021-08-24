7e909bba004bfbf1caed25f1087e7c1ea4025174
Vincent Ambo <mail@tazj.in>
2021-08-24 19:17:38 +0300

    chore(3p/overlays): Bump Emacs overlay to 2021-08-24
    Change-Id: Ief4fb21082011d4056af77b7dae06edf33bd5b2f

--- third_party/overlays/emacs.nix
+++ third_party/overlays/emacs.nix
@@ -2,10 +2,10 @@
 { ... }:
 
 let
-  # from 2021-08-11
-  commit = "3fbe6cf3459cf955e188444b68f085f7a45b6ffa";
+  # from 2021-08-24
+  commit = "b57f95c78042800bbd2b6b3c6c1f9fb85221507e";
   src = builtins.fetchTarball {
     url = "https://github.com/nix-community/emacs-overlay/archive/${commit}.tar.gz";
-    sha256 = "0hm7lkqxh08nq074fgn0naqr6lg12qqxwsr3i4wpvdskmdbhb5i5";
+    sha256 = "17xv9c3x5930zy6hbqk20pwk601w5xnrzpg5gsnrkzl1z726qrn1";
   };
 in import src
