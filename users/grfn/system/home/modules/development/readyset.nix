a1205f0565b41c9a2c8d3c723b5092e677822f4b
Griffin Smith <grfn@gws.fyi>
2021-08-24 09:53:25 -0400

    feat(gs/home): Alias tf->terraform
    Change-Id: Ic79fec6b19c185a6e094dfc9571ea783a1e07148
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/3423
    Reviewed-by: grfn <grfn@gws.fyi>
    Tested-by: BuildkiteCI

--- users/grfn/system/home/modules/development/readyset.nix
+++ users/grfn/system/home/modules/development/readyset.nix
@@ -9,4 +9,8 @@
     # This goes in $PATH so I can run it from rofi and parent to my WM
     (writeShellScriptBin "dotclip" "xclip -out -selection clipboard | dot -Tpng | feh -")
   ];
+
+  programs.zsh.shellAliases = {
+    "tf" = "terraform";
+  };
 }
