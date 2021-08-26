3b8fd699acc4e14a8387cc2f49c3a247c9f73f01
Griffin Smith <grfn@gws.fyi>
2021-08-26 11:55:08 -0400

    refactor(gs/system): Remove rebuilder script
    I no longer use this, I just use the rebuild-system that all nixos
    systems get now.
    
    Change-Id: I2272ff13b21b3194c06b51dbc340c19b8bb336a9

--- users/grfn/system/system/default.nix
+++ users/grfn/system/system/default.nix
@@ -43,33 +43,4 @@ rec {
 
     "iso"
   ];
-
-  rebuilder =
-    let
-      depotPath = "/home/grfn/code/depot";
-
-      caseFor = hostname: ''
-        ${hostname})
-          echo "Rebuilding NixOS for //users/grfn/nixos/${hostname}"
-          system=$(nix-build -E '(import ${depotPath} {}).users.grfn.system.system.${hostname}' --no-out-link)
-          ;;
-      '';
-    in pkgs.writeShellScriptBin "rebuilder" ''
-      set -ue
-      if [[ $EUID -ne 0 ]]; then
-        echo "Oh no! Only root is allowed to rebuild the system!" >&2
-        exit 1
-      fi
-
-      case $HOSTNAME in
-      ${caseFor "chupacabra"}
-      *)
-        echo "$HOSTNAME is not a known NixOS host!" >&2
-        exit 1
-        ;;
-      esac
-
-      nix-env -p /nix/var/nix/profiles/system --set $system
-      $system/bin/switch-to-configuration switch
-    '';
 }
