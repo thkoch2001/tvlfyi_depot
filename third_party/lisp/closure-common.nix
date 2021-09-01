e4f3964fa70fff4f7e14995ed85124b008167037
sterni <sternenseemann@systemli.org>
2021-09-02 01:17:50 +0200

    docs(3p/lisp/closure-common): mention need for ASDF, feature macros
    Luckily we don't need to deal with this mess since all our
    implementations work similarly wrt streams and “wide” characters.
    
    Change-Id: I3ccc606a59c42791f2591d752673c867d848a332

--- third_party/lisp/closure-common.nix
+++ third_party/lisp/closure-common.nix
@@ -14,25 +14,30 @@ in
 depot.nix.buildLisp.library {
   name = "closure-common";
 
-  # feature specific file inclusions in closure-common.asd;
-  # including all relevant fails for SBCL, may not build with
-  # other implementations in this form.
+  # closure-common.asd surpresses some warnings otherwise breaking
+  # compilation. Feature macros across implementations:
+  #
+  # ECL  #+rune-is-character #-rune-is-integer #-x&y-streams-are-stream
+  # CCL  #+rune-is-character #-rune-is-integer #-x&y-streams-are-stream
+  # SBCL #+rune-is-character #-rune-is-integer #-x&y-streams-are-stream
+  #
+  # Since all implementations agree, the alternative files aren't encoded here.
   srcs = getSrcs [
     "closure-common.asd"
     "package.lisp"
     "definline.lisp"
-    "characters.lisp"
+    "characters.lisp"     #+rune-is-character
     "syntax.lisp"
-    "encodings.lisp"
-    "encodings-data.lisp"
-    "xstream.lisp"
-    "ystream.lisp"
+    "encodings.lisp"      #-x&y-streams-are-stream
+    "encodings-data.lisp" #-x&y-streams-are-stream
+    "xstream.lisp"        #-x&y-streams-are-stream
+    "ystream.lisp"        #-x&y-streams-are-stream
     "hax.lisp"
   ];
 
   deps = [
     (depot.nix.buildLisp.bundled "asdf")
     depot.third_party.lisp.trivial-gray-streams
-    depot.third_party.lisp.babel
+    depot.third_party.lisp.babel #+rune-is-character
   ];
 }
