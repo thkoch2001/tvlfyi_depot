273e9cdd6dc1cd1690aa16995e743ee79a410f3f
Vincent Ambo <mail@tazj.in>
2021-09-23 19:57:08 +0300

    feat(tazjin/russian): Check in a bunch of word roots
    These are then loosely referenced by corresponding words in the big
    word list.
    
    I think what I'll be aiming for is a bunch of interesting lookup
    functions (give me all words I know with this root etc.)
    
    Change-Id: I664976c3c1521334ea58c7ba943f5c18d5513bf9

--- users/tazjin/russian/roots.el
+++ users/tazjin/russian/roots.el
@@ -1,2 +1,21 @@
 ;; '(root explanation)
-'(())
+;;
+;; All roots without explanations are TODOs.
+;;
+;; In some cases, roots are not direct morphological roots of their
+;; descendent words (e.g. -голов- => главный)
+
+'(("-весь-" "everything, all, every, etc.")
+  ("-сказ-" nil)
+  ("-говор-" "related to talking")
+  ("-зна-" "knowing, knowledge")
+  ("-врем-" "time")
+  ("-друг-" nil)
+  ("-вид-" "seeing, viewing etc.")
+  ("-мочь-" "ability, permission")
+  ("-голов-" "head, main, etc.")
+  ("-дум-" "thinking, thoughts")
+  ("-прос-" "question")
+  ("-хорош-" "goodness, niceness")
+  ("-нов-" "new")
+  ("-жи-" "life"))
