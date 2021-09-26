b23776458ea6438b18a4361a2a8a6feb3716633f
Vincent Ambo <mail@tazj.in>
2021-09-26 13:39:53 +0300

    refactor(tazjin/russian): Open wiktionary in eww instead
    Change-Id: I4e00168328e1129f43f4e2e4016ad0543607a73f

--- users/tazjin/russian/helpers.el
+++ users/tazjin/russian/helpers.el
@@ -3,5 +3,5 @@
 (defun wiktionary-lookup-at-point (ask-lang)
   (interactive "P")
   (let ((language (if ask-lang (read-string "Language code? ") "ru")))
-    (browse-url (concat "https://ru.wiktionary.org/wiki/"
-                        (thing-at-point 'word)))))
+    (eww (concat "https://ru.wiktionary.org/wiki/"
+                 (thing-at-point 'word)))))
