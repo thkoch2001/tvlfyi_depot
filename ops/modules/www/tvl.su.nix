9186cfa2e5c87ac395bc4cd171178c333a12c4b9
Vincent Ambo <mail@tazj.in>
2021-09-30 23:12:54 +0300

    feat(whitby): Serve //corp/website on tvl.su
    Change-Id: I21e1ddf9a32568cac8ad2595869ac8670867efa9

---
+++ ops/modules/www/tvl.su.nix
@@ -0,0 +1,20 @@
+{ depot, ... }:
+
+{
+  imports = [
+    ./base.nix
+  ];
+
+  config = {
+    services.nginx.virtualHosts."tvl.su" = {
+      serverName = "tvl.su";
+      root = depot.corp.website;
+      enableACME = true;
+      forceSSL = true;
+
+      extraConfig = ''
+        add_header Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" always;
+      '';
+    };
+  };
+}
