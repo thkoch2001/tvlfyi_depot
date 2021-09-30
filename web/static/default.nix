69ed4984e311d74b6d952a517599a9cd6c3c6fa4
Vincent Ambo <mail@tazj.in>
2021-10-01 02:12:07 +0300

    feat(web/static): Add TVL logo to static files
    Change-Id: Ie5ee4fe9d137cde9502ded1558fae3ad685b8863
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/3661
    Tested-by: BuildkiteCI
    Reviewed-by: sterni <sternenseemann@systemli.org>

--- web/static/default.nix
+++ web/static/default.nix
@@ -2,11 +2,16 @@
 # `drvHash` attribute which can be used for cache-busting.
 { depot, lib, pkgs, ... }:
 
-let storeDirLength = with builtins; (stringLength storeDir) + 1;
+let
+  storeDirLength = with builtins; (stringLength storeDir) + 1;
+  logo = depot.web.tvl.logo;
 in lib.fix(self: pkgs.runCommand "tvl-static" {
   passthru = {
     drvHash = lib.substring storeDirLength 32 self.drvPath;
   };
 } ''
-  cp -r ${./.} $out
+  mkdir $out
+  cp -r ${./.}/* $out
+  cp ${logo.pastelRainbow} $out/logo-animated.svg
+  cp ${logo.redPng} $out/logo-red.png
 '')
