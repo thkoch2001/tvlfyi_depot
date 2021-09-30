089017b05bc6d49c7d1b4539133e5add54b0dd4f
Vincent Ambo <mail@tazj.in>
2021-10-01 01:21:13 +0300

    feat(web/static): Expose drvHash attribute on derivation
    This can be used for easy cachebusting of static assets, so that we
    can serve them all with a really long cache-time easily.
    
    Change-Id: I7c9f6beddec58e1caf02cda33bc587590217a939

--- web/static/default.nix
+++ web/static/default.nix
@@ -1,6 +1,9 @@
-{ pkgs, ... }:
+# Expose all static assets as a folder. The derivation contains a
+# `drvHash` attribute which can be used for cache-busting.
+{ depot, lib, pkgs, ... }:
 
-# Needs to be a derivation ...
-pkgs.runCommand "tvl-static" {} ''
+lib.fix(self: pkgs.runCommand "tvl-static" {} ''
   cp -r ${./.} $out
-''
+'' // {
+  drvHash = lib.substring 11 32 self.drvPath;
+})
