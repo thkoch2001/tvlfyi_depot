9cad12aeb79e3b45fad012e5f93919c2ec25278f
Vincent Ambo <mail@tazj.in>
2021-09-30 23:11:29 +0300

    refactor(corp/website): Write index into a folder instead
    This makes it possible to directly use the folder as an nginx root.
    
    Change-Id: I8cb5236f8d6d01658926330a01c9dfdfa7d60dcc
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/3657
    Reviewed-by: tazjin <mail@tazj.in>
    Tested-by: BuildkiteCI

--- corp/website/default.nix
+++ corp/website/default.nix
@@ -9,18 +9,18 @@ let
     url = "https://tvl.su";
     logo = "https://tvl.fyi/static/tvl-animated.svg";
   };
-in depot.web.tvl.template {
-  title = "TVL (The Virus Lounge) - Software consulting";
-  content = builtins.readFile ./content.md;
-  useUrls = true; # load resources from tvl.fyi
-  extraFooter = "\n|\n © ООО ТВЛ";
+  index = depot.web.tvl.template {
+    title = "TVL (The Virus Lounge) - Software consulting";
+    content = builtins.readFile ./content.md;
+    useUrls = true; # load resources from tvl.fyi
+    extraFooter = "\n|\n © ООО ТВЛ";
 
-  # TODO(tazjin): The `.tvl-logo` thing can probably go in the shared CSS.
-  extraHead = ''
-    <meta name="description" content="TVL provides technology consulting for monorepos, Nix, and other SRE/DevOps/Software Engineering topics.">
-    <script type="application/ld+json">
-      ${builtins.toJSON structuredData}
-    </script>
+    # TODO(tazjin): The `.tvl-logo` thing can probably go in the shared CSS.
+    extraHead = ''
+      <meta name="description" content="TVL provides technology consulting for monorepos, Nix, and other SRE/DevOps/Software Engineering topics.">
+      <script type="application/ld+json">
+        ${builtins.toJSON structuredData}
+      </script>
       <style>
         .tvl-logo {
           width: 60%;
@@ -29,5 +29,9 @@ in depot.web.tvl.template {
           margin-right: auto;
         }
       </style>
-  '';
-}
+    '';
+  };
+in pkgs.runCommandNoCC "corp-website" {} ''
+  mkdir $out
+  cp ${index} $out/index.html
+''
