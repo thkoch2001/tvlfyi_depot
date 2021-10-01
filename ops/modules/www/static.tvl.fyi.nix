faca49656c70ea83ecd7c0715322aa6a7aa3a85a
Vincent Ambo <mail@tazj.in>
2021-10-01 19:22:20 +0300

    feat(whitby): serve static.tvl.{fyi|su} with max cache settings
    The setup is explained in the comment, but TL;DR: Use the derivation
    hash of static files to create permanent URLs.
    
    Relates to b/151.
    
    Change-Id: Ib1ca3a1a00c90a47f4bf39c29a8b4bbf5b215e7d

---
+++ ops/modules/www/static.tvl.fyi.nix
@@ -0,0 +1,36 @@
+# Host the static assets at static.tvl.fyi
+#
+# All assets are served from $base/$drvhash/$file, but can also be
+# included with `latest/` which will return a (non-permanent!)
+# redirect to the real location.
+#
+# For all purposes within depot, using the drvhash of web.static is
+# recommended.
+{ depot, pkgs, ... }:
+
+let staticHash = depot.web.static.drvhash;
+in {
+  imports = [
+    ./base.nix
+  ];
+
+  config = {
+    services.nginx.virtualHosts."static.tvl.fyi" = {
+      serverAliases = [ "static.tvl.su" ];
+      enableACME = true;
+      forceSSL = true;
+
+      extraConfig = ''
+        location /latest {
+          rewrite ^/latest/(.*) /${staticHash}/$1 redirect;
+        }
+
+        location /${staticHash}/ {
+          alias ${depot.web.static}/;
+          expires max;
+          add_header Cache-Control "public";
+        }
+      '';
+    };
+  };
+}
