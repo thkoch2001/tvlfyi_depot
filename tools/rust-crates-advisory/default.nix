a3958a7e030891395e349fbff31d28cd4e6a38d2
sterni <sternenseemann@systemli.org>
2021-10-09 17:22:44 +0200

    refactor(tools/rust-crates-advisory): move advisory-db to 3p
    Change-Id: Iaaed35de078292c0c99a7c83de9ca5fdf27b8135

--- tools/rust-crates-advisory/default.nix
+++ tools/rust-crates-advisory/default.nix
@@ -7,13 +7,7 @@ let
     // depot.nix.getBins pkgs.lr [ "lr" ]
     ;
 
-  crate-advisories = "${pkgs.fetchFromGitHub {
-    owner = "RustSec";
-    repo = "advisory-db";
-    # TODO(Profpatsch): this will have to be updated regularly, how?
-    rev = "113188c62380753f01ff0df5edb7d67a300b143a";
-    sha256 = "0v086ybwr71zgs5nv8yr4w2w2d4daxx6in2s1sjb4m41q1r9p0wj";
-  }}/crates";
+  crate-advisories = "${depot.third_party.rustsec-advisory-db}/crates";
 
   our-crates = lib.filter (v: v ? outPath)
     (builtins.attrValues depot.third_party.rust-crates);
