67ec19e854efd2f1e1161c60abd2424aeb95a1ac
Vincent Ambo <mail@tazj.in>
2021-10-15 00:08:45 +0200

    feat(3p/dfmt): Add derivation for the D code formatter
    Change-Id: I4e06676f45df334db4fbc4f1657a6f16828547dc
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/3727
    Tested-by: BuildkiteCI
    Reviewed-by: sterni <sternenseemann@systemli.org>

---
+++ third_party/dfmt/default.nix
@@ -0,0 +1,35 @@
+# dfmt is a code formatter for D
+{ pkgs, lib, ... }:
+
+pkgs.stdenv.mkDerivation rec {
+  pname = "dfmt";
+  version = "0.13.4";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "dlang-community";
+    repo = "dfmt";
+    rev = "v${version}";
+    sha256 = "02a8qvrmnl1c15y6irxlhqpr0hjj5s8qk0jc20ivj0fl6p4v9shj";
+    fetchSubmodules = true;
+  };
+
+  nativeBuildInputs = with pkgs; [
+    dmd
+
+    # fake git that will be used to fetch the version string
+    (pkgs.writeShellScriptBin "git" ''
+      echo 'v${version}'
+    '')
+  ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp bin/dfmt $out/bin/
+  '';
+
+  meta = {
+    description = "D code formatter";
+    homepage = "https://github.com/dlang-community/dfmt";
+    license = lib.licenses.boost;
+  };
+}
