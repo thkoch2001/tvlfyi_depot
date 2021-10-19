56745d9342ad34dbff93c9e5fa9bbc2814901de2
Vincent Ambo <mail@tazj.in>
2021-10-19 14:00:51 +0200

    chore(web/blog): Move out of //users/tazjin
    This will also be used for the TVL blog, with status updates of
    projects like Tvix.
    
    Note that while this commit evaluates, there are still some things
    specific to my blog in this code which I'll untangle in a future commit.
    
    Change-Id: If59431161b165d7249cbb856073a4cae84a1bfbf

--- users/tazjin/blog/default.nix
+++ users/tazjin/blog/default.nix
@@ -1,40 +1,12 @@
-# This creates the static files that make up my blog from the Markdown
-# files in this repository.
-#
-# All blog posts are rendered from Markdown by cheddar.
-{ depot, lib, pkgs, ... }@args:
+{ depot, lib, pkgs, ... }:
 
 with depot.nix.yants;
 
 let
-  inherit (builtins) filter hasAttr map;
+  inherit (builtins) hasAttr filter;
+  inherit (depot.web.blog) post includePost renderPost;
 
-  # Type definition for a single blog post.
-  post = struct "blog-post" {
-    key = string; #
-    title = string;
-    date = int;
-
-    # Optional time at which this post was last updated.
-    updated = option int;
-
-    # Path to the Markdown file containing the post content.
-    content = path;
-
-    # Should this post be included in the index? (defaults to true)
-    listed = option bool;
-
-    # Is this a draft? (adds a banner indicating that the link should
-    # not be shared)
-    draft = option bool;
-
-    # Previously each post title had a numeric ID. For these numeric
-    # IDs, redirects are generated so that old URLs stay compatible.
-    oldKey = option string;
-  };
-
-  posts = list post (import ./posts.nix);
-  fragments = import ./fragments.nix args;
+  posts = filter includePost (list post (import ./posts.nix));
 
   rendered = pkgs.runCommandNoCC "tazjins-blog" {} ''
     mkdir -p $out
@@ -44,13 +16,8 @@ let
     ) posts)}
   '';
 
-  includePost = post: !(fragments.isDraft post) && !(fragments.isUnlisted post);
 in {
-  inherit post rendered;
-  static = ./static;
-
-  # Only include listed posts
-  posts = filter includePost posts;
+  inherit posts rendered;
 
   # Generate embeddable nginx configuration for redirects from old post URLs
   oldRedirects = lib.concatStringsSep "\n" (map (post: ''
