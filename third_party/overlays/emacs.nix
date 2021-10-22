425637b63bf6b75908de4db0da90bfe410cd39ed
Vincent Ambo <mail@tazj.in>
2021-10-22 11:55:38 +0200

    chore(3p/nixpkgs): Bump nixpkgs and Emacs overlay
    ... bumping these in the same commit to avoid double-recompilation of
    Emacs stuff.
    
    Change-Id: Ieab114115b788c36e9d42246445cf17b960c11de
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/3741
    Tested-by: BuildkiteCI
    Reviewed-by: sterni <sternenseemann@systemli.org>

--- third_party/overlays/emacs.nix
+++ third_party/overlays/emacs.nix
@@ -2,10 +2,10 @@
 { ... }:
 
 let
-  # from 2021-10-10
-  commit = "de51ad003df13d41a1107a1ed766eb226ef9382c";
+  # from 2021-10-22
+  commit = "e594a3e8a7d0fbed07a4ed61a7b3eb8f15ece547";
   src = builtins.fetchTarball {
     url = "https://github.com/nix-community/emacs-overlay/archive/${commit}.tar.gz";
-    sha256 = "1spfd7wx7lz48kk7cs91gjx6zhvlliqc6376rxkc4idfb0q43xih";
+    sha256 = "1qqph536mw60cvyzick2bzrampjm840nf3vb84mmmk6sdqlhglwf";
   };
 in import src
