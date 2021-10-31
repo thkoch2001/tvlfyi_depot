78b6be77066aa443be9e53793effa0998584926f
William Carroll <wpcarro@gmail.com>
2021-10-30 19:05:34 -0700

    feat(wpcarro/scratch): create a proof-of-concept blockchain server
    > You cannot get educated by this self-propagating system in which people study
    > to pass exams, and teach others to pass exams, but nobody knows anything.  You
    > learn something by doing it yourself, by asking questions, by thinking, and by
    > experimenting.
    > - Richard Feynman
    
    In the spirit of learning by doing, I decided to implement a simple blockchain
    server. More work remains, but I'm tired after working on this for ~2-3h. I'd
    like to reimplement this from memory using a statically typed language like
    Haskell. I'd also like to implement node
    discovery (https://en.bitcoin.it/wiki/Satoshi_Client_Node_Discovery) because
    that is still something I don't quite understand. But I'm signing-off for
    now...
    
    Change-Id: I74f424e7f52ffbf81eaad420d7d5205da66d33b5

---
+++ scratch/blockchain/default.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+
+let
+  pypkgs = pkgs.python3Packages;
+in pkgs.python3Packages.buildPythonApplication {
+  pname = "main";
+  src = ./.;
+  version = "0.0.1";
+  propagatedBuildInputs = with pypkgs; [
+    flask
+    requests
+  ];
+}
