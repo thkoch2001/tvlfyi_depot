b09c4385b8e9f5671006f87d6b6bbe4de8363ae3
Vincent Ambo <mail@tazj.in>
2021-11-05 13:52:51 +0100

    feat(web/tvl/blog): Add scaffolding for first blog post
    No content yet, but I need this in place to chop up the other changes
    nicely.
    
    Change-Id: I4302e14ab513d6d4dd23e9e8ab5493223b9a80b4

---
+++ web/tvl/blog/default.nix
@@ -0,0 +1,13 @@
+{ depot, ... }:
+
+{
+  posts = [
+    {
+      key = "kicking-off-tvix";
+      title = "Kicking off Tvix";
+      date = 1636038556;
+      content = ./kicking-off-tvix.md;
+      draft = true;
+    }
+  ];
+}
