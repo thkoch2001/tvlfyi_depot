cddd5bcb3b9a642a31aa4d4cb08ec26dface9318
Vincent Ambo <mail@tazj.in>
2021-11-14 00:22:29 +0300

    chore(3p/overlays): Bump Emacs overlay to 2021-11-14
    I'm having issues with vterm and I wonder if it's caused by something
    in another package that is fixed in a newer overlay.
    
    Change-Id: Icb89636cd6c72b10558184634b7c9a5b7b8548c5

--- third_party/overlays/emacs.nix
+++ third_party/overlays/emacs.nix
@@ -2,10 +2,10 @@
 { ... }:
 
 let
-  # from 2021-11-11
-  commit = "72b8a1e5614b8ab2de30c7d25fa453e55152ac40";
+  # from 2021-11-14
+  commit = "c75b7c047cc4635b0ecdedfd4ad78e1ac76e41c5";
   src = builtins.fetchTarball {
     url = "https://github.com/nix-community/emacs-overlay/archive/${commit}.tar.gz";
-    sha256 = "1729nfci2vpc7yyfcan8dc2rw98amf2h5n1vrgm4nrbvyhyh3mv0";
+    sha256 = "06if42qy22xni3gd9hqqq6yhama20qn402asalj16mxi1rxx4kgz";
   };
 in import src
