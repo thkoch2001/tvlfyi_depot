a1af70431a56a37f3e776627f148b4d6a52dbadc
Vincent Ambo <mail@tazj.in>
2021-11-13 17:43:46 +0100

    fix(tazjin/emacs): Adjust tverskoy screen layout for TVL office
    Change-Id: I4d1ad19ee5af52b7f01e1e2003a5c95b5a133865

--- users/tazjin/emacs/config/desktop.el
+++ users/tazjin/emacs/config/desktop.el
@@ -210,13 +210,13 @@
   (exwm-randr-refresh))
 
 (defun randr-tverskoy-split-workspace ()
-  "Split the workspace across two screens, assuming external to the right."
+  "Split the workspace across two screens, assuming external to the left."
   (interactive)
   (set-randr-config
-   '(("eDP" 1 2 3 4 5 )
-     ("HDMI-A-0" 6 7 8 9 0)))
+   '(("HDMI-A-0" 1 2 3 4 5 6 7)
+     ("eDP" 8 9 0)))
 
-  (shell-command "xrandr --output HDMI-A-0 --right-of eDP --auto")
+  (shell-command "xrandr --output HDMI-A-0 --left-of eDP --auto")
   (exwm-randr-refresh))
 
 ;; Layouts for frog (desktop)
