d33a5916787997c12c891f433d1651e42accde9f
Profpatsch <mail@profpatsch.de>
2021-11-14 18:33:07 +0100

    fix(third_party/nix-diff): use version which handles non-utf8 drvs
    We had a problem on whitby where decoding of the drv files would fail
    with an utf8-decoding error.
    
    This version of nix-diff will leniently input files as utf-8, with
    replacement characters if necessary.
    
    Change-Id: I5cb245923c6db0875e63e420cb0783e235b6859f

--- third_party/overlays/haskell/default.nix
+++ third_party/overlays/haskell/default.nix
@@ -2,14 +2,22 @@
 # avoid breakage currently present in nixpkgs or to modify package
 # versions.
 
-{ ... }: # This file needs nothing from readTree
+{ lib, ... }:
 
 self: super: # overlay parameters for the nixpkgs overlay
 
 let
-  overrides = hsSelf: hsSuper: with super.haskell.lib; rec {
+  overrides = hsSelf: hsSuper: with super.haskell.lib; {
     generic-arbitrary = appendPatch hsSuper.generic-arbitrary
       [ ./patches/generic-arbitrary-export-garbitrary.patch ];
+
+    nix-diff = overrideSrc hsSuper.nix-diff {
+        src = builtins.fetchTarball {
+          # https://github.com/Gabriel439/nix-diff/pull/50
+          url = "https://github.com/Profpatsch/nix-diff/archive/6234936b26cd144d321039a44cf1791dcfaee026.tar.gz";
+          sha256 = "1i3ahh8jmiayf29i1xrl2n1dkldgdmrllmccxi4yx6hbcd0r2b1l";
+        };
+      };
   };
 in {
   haskellPackages = super.haskellPackages.override {
