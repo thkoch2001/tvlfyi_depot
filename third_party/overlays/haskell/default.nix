8db7a15cc83f49a44e60dd57c65c71882660ad09
Profpatsch <mail@profpatsch.de>
2021-11-14 18:33:07 +0100

    fix(third_party/nix-diff): use version with better decode errors
    We had a problem on whitby where decoding of the drv files would fail
    with an utf8-decoding error. My best guess is that it’s trying to read
    a binary file, but since the error had no paths in it it was hard to
    confirm.
    
    This version of nix-diff will throw a better error message in that
    case:
    https://github.com/Profpatsch/nix-diff/commit/ee5bd082d2d780b7ff0a82ae7ae6dd73aafb68be
    
    Change-Id: I5cb245923c6db0875e63e420cb0783e235b6859f

--- third_party/overlays/haskell/default.nix
+++ third_party/overlays/haskell/default.nix
@@ -2,14 +2,34 @@
 # avoid breakage currently present in nixpkgs or to modify package
 # versions.
 
-{ ... }: # This file needs nothing from readTree
+{ lib, ... }:
 
 self: super: # overlay parameters for the nixpkgs overlay
 
 let
-  overrides = hsSelf: hsSuper: with super.haskell.lib; rec {
+  overrides = hsSelf: hsSuper: with super.haskell.lib; {
     generic-arbitrary = appendPatch hsSuper.generic-arbitrary
       [ ./patches/generic-arbitrary-export-garbitrary.patch ];
+
+    error = lib.pipe {} [
+      (_: hsSelf.callCabal2nix "error" (builtins.fetchTarball {
+        url = "https://github.com/Profpatsch/error/archive/v0.2.1.0.tar.gz";
+        sha256 = "0fqglyjlrh3gqh2vskp6yyqdxhv0b1k7si5ai1mqyjznxn0c7kwk";
+      }) {})
+      dontCheck
+    ];
+
+    nix-diff = lib.pipe hsSuper.nix-diff [
+      (drv: overrideSrc drv {
+        src = builtins.fetchTarball {
+          # https://github.com/Profpatsch/nix-diff/commit/ee5bd082d2d780b7ff0a82ae7ae6dd73aafb68be
+          url = "https://github.com/Profpatsch/nix-diff/archive/ee5bd082d2d780b7ff0a82ae7ae6dd73aafb68be.tar.gz";
+          sha256 = "10x8551ic03hqxav5ngcyxngpfwjs0l3ygyxh6gn42n43w55ax6g";
+        };
+      })
+      (drv: addBuildDepend drv hsSelf.error)
+    ];
+
   };
 in {
   haskellPackages = super.haskellPackages.override {
