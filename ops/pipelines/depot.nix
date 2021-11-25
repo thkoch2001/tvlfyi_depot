7baf427f0216baaf319f56419ebb909eec99008b
Vincent Ambo <mail@tazj.in>
2021-11-25 12:58:22 +0300

    Experimental CL - playing around with Buildkite
    Working on finding a better way to report build statuses.
    
    Change-Id: I0fd1680da0f7b6b07d79ca90ee9d289ab3e9cfde

--- ops/pipelines/depot.nix
+++ ops/pipelines/depot.nix
@@ -75,8 +75,28 @@ let
   #
   # Pipeline steps need to stay in order.
   pipeline.steps =
+    # Experiment with GraphQL API
+    [{
+      command = let duck = pkgs.writeShellScript "duck" ''
+        set -ueo pipefail
+
+        export FAILED_JOBS=$(${pkgs.curl}/bin/curl 'https://graphql.buildkite.com/v1' \
+          --silent \
+          -H "Authorization: Bearer $(cat /etc/secrets/buildkite-besadii)" \
+          -d "{\"query\": \"query BuildStatusQuery { build(uuid: \\\"$BUILDKITE_BUILD_ID\\\") { jobs(passed: false) { count } } }\"}" | \
+          ${pkgs.jq}/bin/jq -r '.data.build.jobs.count')
+
+        echo "Number of failed jobs: $FAILED_JOBS"
+        exit $FAILED_JOBS
+      ''; in "${duck}";
+
+      label = ":chicken:";
+      key = ":chicken:";
+    }]
+
+
     # Create build steps for each CI target
-    (map mkStep depot.ci.targets)
+    # (map mkStep depot.ci.targets)
 
     ++ [
       # Simultaneously run protobuf checks
@@ -89,10 +109,10 @@ let
       })
 
       # Wait for all steps to complete, then exit with success or
-      # failure depending on whether any failure status was written.
+      # failure depending on whether any other steps failed.
       # This step must be :duck:! (yes, really!)
       ({
-        command = "exit $(buildkite-agent meta-data get 'failure')";
+        command = "exit 1";
         label = ":duck:";
         key = ":duck:";
       })
