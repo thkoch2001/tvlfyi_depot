397c3e70b3130e3ee76047725a48bf1a818354e0
sterni <sternenseemann@systemli.org>
2021-11-25 12:17:35 +0100

    chore(3p): bump NixOS channels to 2021-11-24
    * The API for gradleGen changed which makes our override a bit simpler
      at least.
    
    Change-Id: Ib5d7bc33d30a4545235a8a74eadbb418cd76d1d0

--- third_party/overlays/tvl.nix
+++ third_party/overlays/tvl.nix
@@ -9,17 +9,12 @@ self: super: {
 
   # Required for apereo-cas
   # TODO(lukegb): Document why?
-  gradle_6 = (super.gradleGen.override {
-    java = self.jdk11;
-    jdk = self.jdk11;
-  }).gradleGen rec {
-    name = "gradle-6.5.1";
+  gradle_6 = self.callPackage (super.gradleGen {
+    version = "6.5.1";
     nativeVersion = "0.22-milestone-3";
-
-    src = builtins.fetchurl {
-      url = "https://services.gradle.org/distributions/${name}-bin.zip";
-      sha256 = "0jmmipjh4fbsn92zpifa5cqg5ws2a4ha0s4jzqhrg4zs542x79sh";
-    };
+    sha256 = "0jmmipjh4fbsn92zpifa5cqg5ws2a4ha0s4jzqhrg4zs542x79sh";
+  }) {
+    java = self.jdk11;
   };
 
   clang-tools_11 = self.clang-tools.override {
