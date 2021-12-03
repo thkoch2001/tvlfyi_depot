aa8a5b0c12ba401d2033fb2d6500587bde47f162
Vincent Ambo <mail@tazj.in>
2021-12-03 16:40:03 +0300

    feat(3p/agenix): Import latest version from GitHub
    It's time to automate secrets deployment on hosts like whitby.
    
    Change-Id: If7006124b4b5fec16b4c3570488c11e484f93888

---
+++ third_party/agenix/default.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+
+let
+  commit = "52ea2f8c3231cc2b5302fa28c63588aacb77ea29";
+  src = builtins.fetchTarball {
+    url = "https://github.com/ryantm/agenix/archive/${commit}.tar.gz";
+    # replace this with an actual hash
+    sha256 = "1sqgbriwmvxcmqp0zbk7873psk9g60a53fgrr9p0jafki5zzgvdx";
+  };
+in {
+  inherit src;
+  cli = (import src {
+    inherit pkgs;
+  }).agenix;
+}
