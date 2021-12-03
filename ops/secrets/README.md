d612a13f5adcf9aeb144cad7ecf5bc16330075a9
Vincent Ambo <mail@tazj.in>
2021-12-03 17:12:45 +0300

    feat(ops/secrets): Bootstrap agenix secrets folder
    Sets up the key set and adds an initial secret (besadii config with
    tokens) to be deployed to whitby.
    
    Change-Id: Ic07fd5e66b9e7a533013e04c35e052c2aa11f77d

---
+++ ops/secrets/README.md
@@ -0,0 +1,1 @@
+TVL's deployment secrets, encrypted with [agenix](https://github.com/ryantm/agenix/commits/main)
---
+++ ops/secrets/secrets.nix
@@ -0,0 +1,12 @@
+let
+  tazjin = [
+    # tverskoy
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM1fGWz/gsq+ZeZXjvUrV+pBlanw1c3zJ9kLTax9FWQy"
+  ];
+
+  whitby = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILNh/w4BSKov0jdz3gKBc98tpoLta5bb87fQXWBhAl2I";
+
+  default.publicKeys = tazjin ++ [ whitby ];
+in {
+  "besadii.age" = default;
+}
