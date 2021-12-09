bc92d02cf1462a90320363884125c3dbe907c431
Vincent Ambo <mail@tazj.in>
2021-12-09 13:22:22 +0300

    feat(3p/gerrit-queue): Add derivation for gerrit-queue
    This is a Gerrit autosubmit bot (actually written by flokli) which we
    intend to use.
    
    For now we're using the plain upstream version, but we'll want to
    patch some of the behaviours of it so there's a vendoring on the
    horizon.
    
    Change-Id: I021d41b55f9f678435d9aec6d359545577cb9ec0

---
+++ third_party/gerrit-queue/default.nix
@@ -0,0 +1,25 @@
+{ pkgs, lib, ... }:
+
+pkgs.buildGoModule {
+  pname = "gerrit-queue";
+  version = "master";
+  vendorSha256 = "0hivr4yn9aa1vk7z1h1nwg75hzqnsaxypi1wwxdy1l1hnm5k8hhi";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "tweag";
+    repo = "gerrit-queue";
+    rev = "c67b3ba7ea769cd747ea2f43ee6d12943d599ae0";
+    hash = "sha256:1x0g6fd5hymf6a8wxj1b1xi4x1hmwpnx4f2cdidgvsyd77v902c1";
+  };
+
+  # gerrit-queue embeds static assets which need to be generated
+  preBuild = ''
+    ${pkgs.statik}/bin/statik -f
+  '';
+
+  meta = with lib; {
+    description = "Gerrit submit bot";
+    homepage = "https://github.com/tweag/gerrit-queue";
+    license = licenses.asl20;
+  };
+}
