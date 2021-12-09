2c2ac8b257f367da88eb5ce120950a8eea3592b7
Vincent Ambo <mail@tazj.in>
2021-12-09 13:53:25 +0300

    feat(3p/gerrit-queue): Patch to use Gerrit 'Autosubmit' label
    ... instead of a hashtag in Gerrit.
    
    Might be easier to review here:
    
    https://github.com/tvlfyi/gerrit-queue/commit/24f5a642af3aa1627bbff977f0a101907a02c69f
    
    Change-Id: I1ae8d4607f7cb858135f88411c82e1a353b28105

--- third_party/gerrit-queue/default.nix
+++ third_party/gerrit-queue/default.nix
@@ -12,6 +12,12 @@ pkgs.buildGoModule {
     hash = "sha256:1x0g6fd5hymf6a8wxj1b1xi4x1hmwpnx4f2cdidgvsyd77v902c1";
   };
 
+  # Add TVL patches to bend gerrit-queue functionality to our will.
+  patches = [
+    # Use 'Autosubmit' label instead of Gerrit hashtags.
+    ./0001-gerrit-Use-a-Gerrit-label-instead-of-hashtag-for-aut.patch
+  ];
+
   # gerrit-queue embeds static assets which need to be generated
   nativeBuildInputs = [ pkgs.statik ];
   preBuild = ''
