5fd3140cf32e37ca4869b6a8c230660bf0bc5a2a
Vincent Ambo <mail@tazj.in>
2021-12-09 13:24:06 +0300

    fix(tazjin/emacs): Gerrit remote is now 'origin'
    Change-Id: I44998510ff4be2fa137ea4c81f888e63ea438a56

--- users/tazjin/emacs/config/init.el
+++ users/tazjin/emacs/config/init.el
@@ -251,9 +251,7 @@
 (use-package terraform-mode)
 (use-package toml-mode)
 
-(use-package tvl
-  :custom
-  (tvl-gerrit-remote "gerrit"))
+(use-package tvl)
 
 (use-package web-mode)
 (use-package yaml-mode)
