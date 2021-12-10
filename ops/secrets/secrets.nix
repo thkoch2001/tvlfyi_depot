66a1d3d5d4f2d38b72fb8ed109d71f5710281a1d
Griffin Smith <grfn@gws.fyi>
2021-12-10 09:38:58 -0500

    feat(ops/secrets): Add key for grfn
    Change-Id: I8063ae804932e3815e9a499e0206806818b9b021

--- ops/secrets/secrets.nix
+++ ops/secrets/secrets.nix
@@ -4,9 +4,13 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM1fGWz/gsq+ZeZXjvUrV+pBlanw1c3zJ9kLTax9FWQy"
   ];
 
+  grfn = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMcBGBoWd5pPIIQQP52rcFOQN3wAY0J/+K2fuU6SffjA "
+  ];
+
   whitby = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILNh/w4BSKov0jdz3gKBc98tpoLta5bb87fQXWBhAl2I";
 
-  default.publicKeys = tazjin ++ [ whitby ];
+  default.publicKeys = tazjin ++ grfn ++ [ whitby ];
 in {
   "besadii.age" = default;
   "buildkite-agent-token.age" = default;
