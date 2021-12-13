d14d21526f8d8c91104a694daa61b1e4c7f9bc6a
Vincent Ambo <mail@tazj.in>
2021-12-13 18:31:25 +0300

    feat(tazjin/emacs): Check telega/tdlib compatibility in depot CI
    This keeps biting me at runtime whenever these are out of sync.
    
    Change-Id: If523974e6ad2754ea19123eda0e5fda0a865f408

--- users/tazjin/emacs/default.nix
+++ users/tazjin/emacs/default.nix
@@ -99,6 +99,27 @@ let
     tvlPackages.term-switcher
     tvlPackages.tvl
   ])));
+
+  # Tired of telega.el runtime breakages through tdlib
+  # incompatibility. Target to make that a build failure instead.
+  tdlibCheck =
+    let
+      tgEmacs = emacsWithPackages(epkgs: [ epkgs.telega ]);
+      verifyTdlibVersion = builtins.toFile "verify-tdlib-version.el" ''
+        (require 'telega)
+        (defvar tdlib-version "${pkgs.tdlib.version}")
+        (when (or (version< tdlib-version
+                            telega-tdlib-min-version)
+                  (version< telega-tdlib-max-version
+                            tdlib-version))
+          (message "Found TDLib version %s, but require %s to %s"
+                   tdlib-version telega-tdlib-min-version telega-tdlib-max-version)
+          (kill-emacs 1))
+       '';
+    in pkgs.runCommandNoCC "tdlibCheck" {} ''
+      export PATH="${emacsBinPath}:$PATH"
+      ${tgEmacs}/bin/emacs --script ${verifyTdlibVersion} && touch $out
+    '';
 in lib.fix(self: l: f: pkgs.writeShellScriptBin "tazjins-emacs" ''
   export PATH="${emacsBinPath}:$PATH"
   exec ${tazjinsEmacs f}/bin/emacs \
@@ -131,5 +152,10 @@ in lib.fix(self: l: f: pkgs.writeShellScriptBin "tazjins-emacs" ''
         ${if l != null then "--directory ${l}" else ""} \
         --eval "(require 'init)" $@
     '';
+
+    # Expose telega/tdlib version check as a target that is built in
+    # CI.
+    inherit tdlibCheck;
+    meta.targets = [ "tdlibCheck" ];
   }) null identity
 ) {}
