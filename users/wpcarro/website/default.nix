ed6ec6cee56090b308556104adde6b1558d2e9ee
William Carroll <wpcarro@gmail.com>
2021-12-13 19:51:48 -0800

    fix(wpcarro/nix): Remove <briefcase> references
    Angle-bracketed references are a Nix anti-pattern, and thankfully this
    repository enforces this as a standard.
    
    TL;DR:
    - Drop angle-bracketed references
    - Change `briefcase` -> `users.wpcarro`
    - Fix any resulting regressions
    
    Change-Id: I7153cbabafa617bfd6b199370cbec65cb75441f6

--- users/wpcarro/website/default.nix
+++ users/wpcarro/website/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, briefcase, ... }:
+{ pkgs, depot, ... }:
 
 pkgs.stdenv.mkDerivation {
   name = "wpcarro.dev";
@@ -8,6 +8,6 @@ pkgs.stdenv.mkDerivation {
     cp $src/index.html $out
 
     mkdir -p $out/habits
-    cp -r ${briefcase.website.habit-screens} $out/habits/index.html
+    cp -r ${depot.users.wpcarro.website.habit-screens} $out/habits/index.html
   '';
 }
