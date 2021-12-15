1d5a7ee0589b337158d696d7945a1fe986d15486
Vincent Ambo <mail@tazj.in>
2021-12-15 23:58:43 +0300

    chore(3p/lisp): use nixpkgs sources for trivial-backtrace
    Change-Id: If4ee3f9a0afea74759493de14c7f672714739f45

---
+++ third_party/lisp/trivial-backtrace.nix
@@ -0,0 +1,15 @@
+# Imported from http://common-lisp.net/project/trivial-backtrace/trivial-backtrace.git
+{ depot, pkgs, ... }:
+
+let src = with pkgs; srcOnly lispPackages.trivial-backtrace;
+in depot.nix.buildLisp.library {
+  name = "trivial-backtrace";
+
+srcs = map (f: src + ("/dev/" + f)) [
+    "packages.lisp"
+    "utilities.lisp"
+    "backtrace.lisp"
+    "map-backtrace.lisp"
+    "fallback.lisp"
+  ];
+}
