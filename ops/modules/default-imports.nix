5d9f89d7736f3146379434e83662357d8c9326fd
Griffin Smith <grfn@gws.fyi>
2021-12-15 18:09:46 -0500

    feat(ops/modules): Provide some modules to all nixoses
    For modules that are gated behind a mkEnableOption, it's reasonable to
    just provide them to all Depot-built nixos systems without requiring
    people to explicitly import them. This defines a special module called
    `default-imports.nix` which imports these modules (currently just
    tvl-cache.nix and automatic-gc.nix, as I'm being rather conservative
    adding things here to avoid breaking anyone's system), then provides
    that module as one of the `modules` passed at the top-level
    nixos/eval-config invocation.
    
    Change-Id: I3be299ab10ae4c451ef11c514edb3c89318a2278

---
+++ ops/modules/default-imports.nix
@@ -0,0 +1,14 @@
+{ depot, ... }:
+
+# Default set of modules that are imported in all Depot nixos systems
+#
+# All modules here should be properly gated behind a `lib.mkEnableOption` with a
+# `lib.mkIf` for the config.
+
+{ ... }:
+{
+  imports = [
+    "${depot.path + "/ops/modules/automatic-gc.nix"}"
+    "${depot.path + "/ops/modules/tvl-cache.nix"}"
+  ];
+}
