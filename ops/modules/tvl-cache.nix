52aa7e89bb49839798e7bb05184b1dac60b063c9
Griffin Smith <grfn@gws.fyi>
2021-12-15 17:44:37 -0500

    feat(ops/modules): Add shared module for TVL cache
    Add a shared nixos module for configuring whitby as a binary nix cache,
    and refactor tverskoy to use this module.
    
    This is enabled via an option to pave the way for including it as an
    import in all depot-generated nixos configs at some point in the future.
    
    Change-Id: I6dcc0e8eb48b1ac34457666dceebeedd5da6c526

---
+++ ops/modules/tvl-cache.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, ... }:
+
+{
+  options = {
+    tvl.cache.enable = lib.mkEnableOption "the TVL binary cache";
+  };
+
+  config = lib.mkIf config.tvl.cache.enable {
+    nix = {
+      binaryCachePublicKeys = [
+        "cache.tvl.su:kjc6KOMupXc1vHVufJUoDUYeLzbwSr9abcAKdn/U1Jk="
+      ];
+
+      binaryCaches = [
+        "https://cache.tvl.su"
+      ];
+    };
+  };
+}
