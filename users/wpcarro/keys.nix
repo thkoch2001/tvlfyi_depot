6cb00e90a0bbb4ce00a7b155cc13c092a049b19c
William Carroll <wpcarro@gmail.com>
2021-12-16 00:16:15 -0500

    feat(wpcarro/nixos): Define keys.nix
    This opens the door for a whole bunch of neat features.
    
    Change-Id: I15066ca9d8fe38921ebc8c4e995bd282162a4577

---
+++ users/wpcarro/keys.nix
@@ -0,0 +1,9 @@
+# wpcarro's public SSH keys
+{ ... }:
+
+rec {
+  marcus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJkNQJBXekuSzZJ8+gxT+V1+eXTm3hYsfigllr/ARXkf";
+  seneca = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDSVqb0ehIxp33j8wJgPoCcd45NPnZ3CisDlcO1f5isTvOTSLVf5WAdqgDk9fuTTcOMdaiPQcun7psZYEZ7EnoiDYiNM/iYoqN5ga0MdQVCU0pvkJuExuK8MqdwAnjwrLx1bqw8ivXxbmrKMyN/fUsG8I7IzC+D61ycbMwxM5qSz2mgQjzHIhpS1HgCPJXchD3jo1kw9FgSVMAJJMGpWU6BcJsQ2cNTb3W8Kg3kdeahcIssysC6TbD2MFCI6ucPOdBvP/nMHQ/zwK3CgR75M57lyzqDPqu29OpiFacefN8Jxjgwlg4h/TP3tCkb+fSV/5vkBl8aagoPb+nepC5AWF9ADsagJ6y7HYRqkXnI6FaYRbHg+NjcEu1ljYQqAIl8lRLcVqFEHfqll1V12f1UeciNoSrOBXpb0pQrUs4YlaZU1rbq0t9dQob5x+mm5BrhNhKagEvx5nV+X5bxPywCLpdrotjKpW1oS+EssRq75cv9Aw2vqdNmk2pLhgKkOJu5RrOuitHL9Ts7ax6Co5S086BT57g3BCjaiCZDoWUSRTPc6K+rDPriCGXJqfGncdUJh20QsZPYIrWQSSJuRDW59WxnNbKvIH5aFvHM2S+HyjhZC+d5pjm9mhfHuluL9+Hwis7kxlqNoX3i/i5ufGEODaLeRu5xWp0hc5fYype8BL+NNw== wpcarro@gmail.com";
+
+  all = [ marcus seneca ];
+}
