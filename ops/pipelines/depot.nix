5ecddc9242c32b393714ba4d984d8c800e292d42
Griffin Smith <grfn@gws.fyi>
2021-12-16 16:23:32 -0500

    feat(ops/pipelines): Allow targets to define extraSteps
    Change-Id: I04a23574be65d7fff41b19d4c781403f0d041093

--- ops/pipelines/depot.nix
+++ ops/pipelines/depot.nix
@@ -14,7 +14,24 @@ let
     length
     map
     mapAttrs
-    toJSON;
+    toJSON
+    concatMap
+  ;
+
+  inherit (depot.nix.yants)
+    defun
+    struct
+    string
+    attrs
+    any
+    option
+    bool
+  ;
+
+  extraStep = struct "extraStep" {
+    label = string;
+    command = string;
+  };
 
   inherit (pkgs) runCommandNoCC symlinkJoin writeText;
 
@@ -35,7 +52,7 @@ let
       else label;
 
   # Create a pipeline step from a single target.
-  mkStep = target: {
+  mkTargetStep = target: {
     command = let
       drvPath = builtins.unsafeDiscardStringContext target.drvPath;
     in lib.concatStringsSep " " [
@@ -75,6 +92,21 @@ let
     depends_on = ":init:";
   };
 
+  mkExtraStep = defun [
+    (attrs any)
+    extraStep
+    (attrs any)
+  ] (parentTarget: { label, command }: {
+    inherit label command;
+    depends_on = parentTarget.label;
+    skip = parentTarget.skip or false;
+  });
+
+  mkSteps = target: let
+    targetStep = mkTargetStep target;
+  in [targetStep]
+     ++ (map (mkExtraStep targetStep) (target.meta.extraSteps or []));
+
   # Protobuf check step which validates that changes to .proto files
   # between revisions don't cause backwards-incompatible or otherwise
   # flawed changes.
@@ -86,7 +118,7 @@ let
   # All pipeline steps before batching them into smaller chunks.
   allSteps =
     # Create build steps for each CI target
-    (map mkStep depot.ci.targets)
+    (concatMap mkSteps depot.ci.targets)
 
     ++ [
       # Simultaneously run protobuf checks
@@ -123,4 +155,4 @@ in runCommandNoCC "depot-pipeline" {} ''
     lib.concatMapStringsSep "\n"
       (chunk: "cp ${chunk.path} $out/${chunk.filename}") pipelineChunks
   }
-''
+'' // { inherit mkSteps mkExtraStep allSteps; }
