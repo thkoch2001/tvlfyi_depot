b8d8d9051690034703f0329343390a8f87beb1cb
Vincent Ambo <mail@tazj.in>
2021-12-19 21:12:32 +0300

    feat(depotfmt): run formatting checks in CI
    Right now this only includes Go, but more is to come.
    
    Change-Id: Idd8fc27c0eb25e82688ef8337ba20810d834f4b6

--- tools/depotfmt.nix
+++ tools/depotfmt.nix
@@ -1,6 +1,6 @@
 # Builds treefmt for depot, with a hardcoded configuration that
 # includes the right paths to formatters.
-{ pkgs, ... }:
+{ depot, pkgs, ... }:
 
 let
   config = pkgs.writeText "depot-treefmt-config" ''
@@ -9,8 +9,19 @@ let
     options = [ "-w" ]
     includes = ["*.go"]
   '';
-in pkgs.writeShellScriptBin "depotfmt" ''
-  exec ${pkgs.treefmt}/bin/treefmt ''${@} \
-    --config-file ${config} \
-    --tree-root $(${pkgs.git}/bin/git rev-parse --show-toplevel)
-''
+
+  # helper tool for formatting the depot interactively
+  depotfmt = pkgs.writeShellScriptBin "depotfmt" ''
+    exec ${pkgs.treefmt}/bin/treefmt ''${@} \
+      --config-file ${config} \
+      --tree-root $(${pkgs.git}/bin/git rev-parse --show-toplevel)
+  '';
+
+  # wrapper for failure in CI
+  check = pkgs.runCommandNoCC "depotfmt-check" {} ''
+    cp -r ${depot.path.origSrc} depot
+    exec ${pkgs.treefmt}/bin/treefmt --fail-on-change \
+      --config-file ${config} \
+      --tree-root depot
+  '';
+in depotfmt // depot.nix.readTree.drvTargets { inherit check; }
