15a9443e3668fc164b1a61095e6a5f22ae48f6f0
Griffin Smith <grfn@gws.fyi>
2021-12-24 14:26:26 -0500

    feat(grfn/bbbg): Add styling for flash messages
    Change-Id: Ic8594217e6acceaad041dda8acf135bf7f3408e9
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/4585
    Reviewed-by: grfn <grfn@gws.fyi>
    Autosubmit: grfn <grfn@gws.fyi>
    Tested-by: BuildkiteCI

--- users/grfn/bbbg/src/bbbg/views/flash.clj
+++ users/grfn/bbbg/src/bbbg/views/flash.clj
@@ -1,7 +1,7 @@
 (ns bbbg.views.flash
   (:require [clojure.spec.alpha :as s]))
 
-(s/def :flash/type #{:success :error :warning})
+(s/def :flash/type #{:success :error :warning :info})
 (s/def :flash/message string?)
 (s/def ::flash (s/keys :req [:flash/type :flash/message]))
 (s/fdef add-flash :args (s/cat :resp map? :flash ::flash) :ret map?)
@@ -31,3 +31,9 @@
         [:li.flash-message
          {:class (str "flash-" (-> message :flash/type name))}
          (:flash/message message)])])))
+
+(def test-flash
+  {:flash/messages
+   (for [type [:success :error :warning :info]]
+     {:flash/type type
+      :flash/message (str "Sample " type " message")})})
