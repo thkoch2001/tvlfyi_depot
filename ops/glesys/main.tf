31c0fdf39172b4b03020e6c5252c5bfadb79ed8b
Vincent Ambo <mail@tazj.in>
2021-12-24 20:29:31 +0300

    feat(ops/glesys): Create bucket & key for storing terraform state
    Change-Id: I73cfaa614d46afb65ba312e767d1e924669fbae1

--- ops/glesys/main.tf
+++ ops/glesys/main.tf
@@ -20,3 +20,17 @@ resource "glesys_objectstorage_instance" "tvl-backups" {
   description = "tvl-backups"
   datacenter = "dc-sto1"
 }
+
+resource "glesys_objectstorage_instance" "tvl-state" {
+  description = "tvl-state"
+  datacenter = "dc-sto1"
+
+  lifecycle {
+    ignore_changes = [ accesskey ]
+  }
+}
+
+resource "glesys_objectstorage_credential" "terraform-state" {
+  instanceid = glesys_objectstorage_instance.tvl-state.id
+  description = "key for terraform state"
+}
