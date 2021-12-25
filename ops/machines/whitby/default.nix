7a94fca37266e5025179a1cede2dd698eb19c8d7
Vincent Ambo <mail@tazj.in>
2021-12-26 00:03:41 +0300

    refactor(ops/whitby): Move Gerrit secrets into agenix
    Gerrit has OAuth2 and email related secrets which now live in agenix
    instead of a random file on disk.
    
    Change-Id: I6220fbb7a2e2ec0102a900b4bcf6150b8b4d32ef

--- ops/machines/whitby/default.nix
+++ ops/machines/whitby/default.nix
@@ -239,6 +239,13 @@ in {
         owner = "git";
       };
 
+      gerrit-secrets = {
+        file = secretFile "gerrit-secrets";
+        path = "/var/lib/gerrit/etc/secure.config";
+        owner = "git";
+        mode = "0400";
+      };
+
       clbot-ssh = {
         file = secretFile "clbot-ssh";
         owner = "clbot";
