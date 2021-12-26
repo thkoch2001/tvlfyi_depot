169d7fb87436603207e79cdcc9b51d84eb11e39e
Griffin Smith <grfn@gws.fyi>
2021-12-26 15:11:48 -0500

    feat(grfn/mugwump): Set up agenix
    Start setting up agenix with secrets in //users/grfn/secrets for
    mugwump, starting with my cloudflare API key which I use for the ddns
    from my home apartment
    
    Change-Id: Ida66cb91da3415357a512039d6c23402f0ae9388
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/4683
    Reviewed-by: grfn <grfn@gws.fyi>
    Autosubmit: grfn <grfn@gws.fyi>
    Tested-by: BuildkiteCI

---
+++ users/grfn/secrets/secrets.nix
@@ -0,0 +1,8 @@
+let
+  grfn = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMcBGBoWd5pPIIQQP52rcFOQN3wAY0J/+K2fuU6SffjA";
+  mugwump = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFE2fxPgWO+zeQoLBTgsgxP7Vg7QNHlrQ+Rb3fHFTomB";
+in
+
+{
+  "cloudflare.age".publicKeys = [ grfn mugwump ];
+}
