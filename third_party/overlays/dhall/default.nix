63bffa7f42539709beb5207c97ef0231e23d08c0
zseri <zseri.devel@ytrizja.de>
2021-12-26 05:23:38 +0100

    refactor(3p/overlays/dhall): {x -> e-d-n.$x-simple} mapping
    Change-Id: I1ea82a6f4a5bf874cd2a1dc437e9522af0488867

--- third_party/overlays/dhall/default.nix
+++ third_party/overlays/dhall/default.nix
@@ -11,14 +11,17 @@ let
       url = "https://github.com/justinwoo/easy-dhall-nix/archive/eae7f64c4d6c70681e5a56c84198236930ba425e.tar.gz";
       sha256 = "1y2x15v8a679vlpxazjpibfwajp6zph60f8wjcm4xflbvazk0dx7";
     }) { inherit pkgs; };
-in {
-  dhall = easy-dhall-nix.dhall-simple;
-  dhall-bash = easy-dhall-nix.dhall-bash-simple;
-  dhall-docs = easy-dhall-nix.dhall-docs-simple;
-  dhall-json = easy-dhall-nix.dhall-json-simple;
-  dhall-lsp-server = easy-dhall-nix.dhall-lsp-simple;
-  dhall-nix = easy-dhall-nix.dhall-nix-simple;
+
+in
+pkgs.lib.genAttrs [
+  "dhall"
+  "dhall-bash"
+  "dhall-docs"
+  "dhall-json"
+  "dhall-lsp-server"
+  "dhall-nix"
   # not yet in dhall-simple
-  # dhall-nixpkgs = easy-dhall-nix.dhall-nixpkgs-simple;
-  dhall-yaml = easy-dhall-nix.dhall-yaml-simple;
-}
+  # "dhall-nixpkgs"
+  "dhall-yaml"
+]
+  (name: easy-dhall-nix."${name}-simple")
