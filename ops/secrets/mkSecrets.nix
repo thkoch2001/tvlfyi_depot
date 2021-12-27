c8625a0f40e9c2d215ea54f6c1fb31475671db44
zseri <zseri.devel@ytrizja.de>
2021-12-27 02:07:45 +0100

    refactor(ops/secrets): optimize + typecheck mkSecrets
    Change-Id: I592c8f2f82cef8fe4509e90a8c48504a0c74d133

--- ops/secrets/mkSecrets.nix
+++ ops/secrets/mkSecrets.nix
@@ -3,17 +3,25 @@
 #
 # Note that encrypted secrets end up in the Nix store, but this is
 # fine since they're publicly available anyways.
-{ depot, pkgs, ... }:
-path: secrets:
+{ depot, lib, ... }:
 
 let
-  inherit (builtins) attrNames listToAttrs;
+  inherit (depot.nix.yants)
+    attrs
+    defun
+    list
+    path
+    restrict
+    string
+    struct
+    ;
+  TdrvTargets = depot.nix.yants.drvTargets;
+  ssh-pubkey = restrict "SSH pubkey" (lib.hasPrefix "ssh-") string;
+  secret-bunch = struct { publicKeys = list ssh-pubkey; };
+in
 
-  # Import a secret to the Nix store
-  declareSecret = name: pkgs.runCommandNoCC name {} ''
-    cp ${path + "/${name}"} $out
-  '';
-in depot.nix.readTree.drvTargets (listToAttrs (
-  map (name: { inherit name; value = declareSecret name; })
-    (attrNames secrets)
-))
+defun [ path (attrs secret-bunch) TdrvTargets ]
+  (path: secrets:
+    depot.nix.readTree.drvTargets
+      # Import each secret into the Nix store
+      (builtins.mapAttrs (name: _: "${path}/${name}") secrets))
