48fd90cdf5c98b55aacfa79389e2676d4d9c2c24
zseri <zseri.devel@ytrizja.de>
2021-12-27 19:30:45 +0100

    refactor(web/todolist): use jq instead of Nix for TODO generation
    I suppose the jq variant has the benefit that it supports text
    escaping (which could be ported to the extract script if necessary),
    in the rare case of rust updates or such it doesn't slow down
    the pipeline evaluation, and it fixes a few bugs in the output HTML
    (namely invalid paragraph (<p>) tags).
    
    original motivation:
    
    although it is nice to be able to generate something like this
    using Nix, `fromJSON (readFile ...)` slows down the pipeline
    evaluation a bit (~1 second) and can't be done in parallel to
    other build jobs. This CL "outsources" this generation to jq
    (especially because we don't gain much from using Nix here),
    which results in the benefit that we don't have to wait for
    the results of ripgrep+jq+readFile+fromJSON, which instead
    just gets put into the resulting pipeline, but does not block
    pipeline generation itself.
    
    Change-Id: I5baeecc4ccd8c134675616e73ace5d898c452955

--- web/todolist/default.nix
+++ web/todolist/default.nix
@@ -9,19 +9,13 @@ let
     jq
     ripgrep
     runCommandNoCC
-    writeTextFile
+    writeText
     ;
 
   inherit (builtins)
-    elem
-    filter
-    fromJSON
-    head
-    readFile
     map
+    toJSON
     ;
-
-  inherit (lib) concatStringsSep;
 
   inherit (depot.nix.yants)
     defun
@@ -30,7 +24,8 @@ let
     struct
     ;
 
-  knownUsers = map (u: u.username) depot.ops.users;
+  knownUsers = writeText "known-users.json"
+    (toJSON (map (u: u.username) depot.ops.users));
 
   todo = struct {
     file = string;
@@ -39,45 +34,16 @@ let
     user = string;
   };
 
-  allTodos = fromJSON (readFile (runCommandNoCC "depot-todos.json" {} ''
+  allTodos = runCommandNoCC "depot-todos.json" {} ''
     ${ripgrep}/bin/rg --json 'TODO\(\w+\):.*$' ${depot.path} | \
       ${jq}/bin/jq -s -f ${./extract-todos.jq} > $out
-  ''));
-
-  knownUserTodos = filter (todos: elem (head todos).user knownUsers) allTodos;
-
-  fileLink = defun [ todo string ] (t:
-    ''<a style="color: inherit;"
-         href="https://cs.tvl.fyi/depot/-/blob/${t.file}#L${toString t.line}">
-      //${t.file}:${toString t.line}</a>'');
-
-  todoElement = defun [ todo string ] (t: ''
-    <p>${fileLink t}:</p>
-    <blockquote>${t.todo}</blockquote>
-
-  '');
-
-  userParagraph = todos:
-  let user = (head todos).user;
-  in ''
-    <p>
-      <h3>
-        <a style="color:inherit; text-decoration: none;"
-           name="${user}"
-           href="#${user}">${user}</a>
-      </h3>
-      ${concatStringsSep "\n" (map todoElement todos)}
-    </p>
-    <hr>
   '';
 
   staticUrl = "https://static.tvl.fyi/${depot.web.static.drvHash}";
 
-in writeTextFile {
-  name = "tvl-todos";
-  destination = "/index.html";
-  text = ''
+  header = writeText "tvl-todo-header.html" ''
     <!DOCTYPE html>
+    <html>
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -90,6 +56,9 @@ in writeTextFile {
           max-width: inherit;
           height: auto;
         }
+        main h3 > a {
+          text-decoration: none;
+        }
       </style>
     </head>
     <body class="dark">
@@ -98,7 +67,9 @@ in writeTextFile {
         <hr>
       </header>
       <main>
-      ${concatStringsSep "\n" (map userParagraph knownUserTodos)}
+  '';
+
+  footer = writeText "tvl-todo-footer.html" ''
       </main>
       <footer>
         <p class="footer">
@@ -111,5 +82,24 @@ in writeTextFile {
         <p class="lod">ಠ_ಠ</p>
       </footer>
     </body>
+    </html>
   '';
-}
+
+in runCommandNoCC "tvl-todos" {} ''
+  set -e
+  mkdir -p $out
+
+  (
+    echo "{ \"allTodos\": "
+    cat "${allTodos}"
+    echo ", \"knownUsers\": "
+    cat "${knownUsers}"
+    echo "}"
+  ) > input.json
+
+  (
+    cat ${header}
+    ${jq}/bin/jq -r -f ${./render-todos.jq} < input.json | sed -E -e 's/^(.)/    \1/'
+    cat ${footer}
+  ) > $out/index.html
+''
