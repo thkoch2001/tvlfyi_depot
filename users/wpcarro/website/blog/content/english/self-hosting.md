9f778c38e83ee946e177c936c5fe51478b090cec
William Carroll <wpcarro@gmail.com>
2021-12-27 09:38:35 -0400

    refactor(wpcarro/blog): Prefer depot.web.blog
    Hugo is a bit too heavyweight for my taste.
    
    Change-Id: I331bc5898bd40f1a03bbde8ad69fe3cc9f72c18b

--- users/wpcarro/website/blog/content/english/self-hosting.md
+++
@@ -1,6 +0,0 @@
----
-title: "Self Hosting"
-date: 2020-03-11T22:53:56Z
-draft: true
----
-
---
+++ users/wpcarro/website/blog/posts.nix
@@ -0,0 +1,18 @@
+# To format the date, run:
+#   $ date -d "2020-04-02" +%s
+[
+  {
+    key = "cell-phone-experiment";
+    title = "Cell Phone Experiment";
+    date = 1585800000;
+    content = ./posts/cell-phone-experiment.md;
+    draft = false;
+  }
+  {
+    key = "quassel-google-vm";
+    title = "IRC, GCP, and NixOS";
+    date = 1640404800;
+    content = ./posts/quassel-google-vm.md;
+    draft = true;
+  }
+]
