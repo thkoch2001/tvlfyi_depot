134cda28ac623b75d149e18a882272fd0af241df
sterni <sternenseemann@systemli.org>
2021-12-28 12:40:49 +0100

    chore(3p/overlays/haskell): nix-diff patch has been upstreamed
    https: //hackage.haskell.org/package/nix-diff-1.0.17/changelog
    Change-Id: Ied02395151ec62619721ad5e78d0841fa87d1b3c
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/4729
    Tested-by: BuildkiteCI
    Reviewed-by: tazjin <mail@tazj.in>

--- third_party/overlays/haskell/default.nix
+++ third_party/overlays/haskell/default.nix
@@ -10,14 +10,6 @@ let
   overrides = hsSelf: hsSuper: with super.haskell.lib; {
     generic-arbitrary = appendPatch hsSuper.generic-arbitrary
       [ ./patches/generic-arbitrary-export-garbitrary.patch ];
-
-    nix-diff = overrideSrc hsSuper.nix-diff {
-        src = builtins.fetchTarball {
-          # https://github.com/Gabriel439/nix-diff/pull/50
-          url = "https://github.com/Profpatsch/nix-diff/archive/6234936b26cd144d321039a44cf1791dcfaee026.tar.gz";
-          sha256 = "1i3ahh8jmiayf29i1xrl2n1dkldgdmrllmccxi4yx6hbcd0r2b1l";
-        };
-      };
   };
 in {
   haskellPackages = super.haskellPackages.override {
