01b0287af816380a24373e39e9289c0b1c4dd028
Vincent Ambo <mail@tazj.in>
2021-12-28 15:37:20 +0300

    fix(ops/users): change my email to the @tvl.su one
    Change-Id: Id608fe66b203c1d08958c85be44506a86eec56d5

--- ops/users/default.nix
+++ ops/users/default.nix
@@ -105,7 +105,7 @@
   }
   {
     username = "tazjin";
-    email = "mail@tazj.in";
+    email = "tazjin@tvl.su";
     password = "{ARGON2}$argon2id$v=19$m=65536,t=2,p=1$wOPEl9D3kSke//oLtbvqrg$j0npwwXgaXQ/emefKUwL59tH8hdmtzbgH2rQzWSmE2Y";
   }
   {
