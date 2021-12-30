e5a62ca85e62b3a94ad85aa01f8a104f9a2642ff
Profpatsch <mail@profpatsch.de>
2021-12-30 23:24:56 +0100

    feat(tvix): set up a simple command line parser for nix-store
    We are going to have a 1:1 drop-in replacement for the old-style nix
    tools, and this starts implementing the cli parser part.
    
    The first step is to have a simple integration test suite that can
    verify that we match the nix CLI.
    
    clap is a super complicated parsing library, but looking through the
    rest they are either too opinioated to be of use for us, or depend on
    clap as implementation.
    
    Change-Id: I4cf6051f3a4f782c3242fd0d2b9eab3fbe33d8ad

--- tvix/src/bin/nix-store.rs
+++ tvix/src/bin/nix-store.rs
@@ -1,3 +1,97 @@
 fn main () {
-  println!("hello, nix");
+  main_args(std::env::args().collect()).unwrap_or_else(|e| e.exit());
+}
+
+pub fn main_args(args: Vec<String>) -> clap::Result<NixResult> {
+  let matches = clap::App::new("nix-store")
+      .subcommand(clap::SubCommand::with_name("--add")
+                  .arg(clap::Arg::with_name("FILE").required(true).index(1)))
+      .get_matches_from_safe(args.iter())?;
+   if let Some(add) = matches.subcommand_matches("--add") {
+     let file = add.value_of("FILE").expect("--add needs a file");
+     let file_contents = std::fs::read_to_string(file).expect(&format!("file {} does not exist", file));
+     Ok(NixResult::FileAddedToStore{ content: file_contents })
+   }
+   else {
+     panic!("read some arguments that we do not know: {:?}", args)
+   }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum NixResult {
+  FileAddedToStore{content: String}
+}
+
+
+#[cfg(test)]
+mod integration_tests {
+    use std::{io::Write, collections::VecDeque};
+
+    use super::*;
+
+    #[derive(Debug)]
+    enum NixOutput{
+      Err{
+        status: i32,
+        stdout: String,
+        stderr: String,
+      },
+      Ok{
+      stdout: String,
+      stderr: String,
+      }
+    }
+
+  fn run_nix_command(cmd: &str, args: Vec<String>) -> NixOutput {
+    let out = std::process::Command::new(cmd).args(args).output().expect("could not run nix-store");
+    match out.status.code().expect("no status code!") {
+      0 => {
+        NixOutput::Ok{
+          stdout: String::from_utf8_lossy(&out.stdout).trim_end().to_string(),
+          stderr: String::from_utf8_lossy(&out.stderr).trim_end().to_string()
+        }
+      },
+      status => NixOutput::Err{
+        status,
+        stdout: String::from_utf8_lossy(&out.stdout).trim_end().to_string(),
+        stderr: String::from_utf8_lossy(&out.stderr).trim_end().to_string()
+      }
+    }
+
+  }
+
+  fn nix_nix_store<'a>(args: Vec<String>) -> NixResult {
+
+    match run_nix_command("nix-store", args) {
+      err@NixOutput::Err{..} => panic!("nix-store --add failed: {:#?}", err),
+      NixOutput::Ok{stdout, ..} => {
+
+        NixResult::FileAddedToStore{ content:
+          std::fs::read_to_string(&stdout).expect(&format!("cannot open {} as store file", stdout))
+
+        }
+      }
+    }
+  }
+
+  fn tvix_nix_store<'a>(args: Vec<String>) -> NixResult {
+    eprintln!("running tvix with arguments {:?}", args);
+    let mut args = VecDeque::from(args);
+    args.push_front("tvix-store".to_string());
+    super::main_args(Vec::from(args)).unwrap_or_else(|e| panic!("clap command line parsing failed:\n{}", e))
+  }
+
+  #[test]
+  fn test_realise() {
+    let file_content = "I am a copied file";
+    let mut tempfile = tempfile::NamedTempFile::new().expect("cannot create temp file");
+    tempfile.write_all(file_content.as_bytes()).expect("could not write to tempfile");
+    assert_eq!(
+      tvix_nix_store(vec!["--add".to_string(), tempfile.path().as_os_str().to_string_lossy().into_owned()]),
+      nix_nix_store(vec!["--add".to_string(), tempfile.path().as_os_str().to_string_lossy().into_owned()]),
+      "added file contents were not the same"
+    );
+
+    drop(tempfile)
+  }
 }
