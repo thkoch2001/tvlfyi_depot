047ce14bb050108216737d40011503cacc79d19c
William Carroll <wpcarro@gmail.com>
2021-12-30 15:25:03 -0400

    feat(wpcarro): Define deploy-diogenes helper
    TL;DR:
    - Mimmick depot's bin -> __dispatch.sh for personal utils
    - Define deploy-diogenes to more tighten my feedback loop
    
    Change-Id: I2b12a1c32a955574f5be5d4f38025bd97e9c7b77

--- users/wpcarro/nixos/default.nix
+++ users/wpcarro/nixos/default.nix
@@ -1,8 +1,22 @@
-{ depot, ... }:
+{ depot, pkgs, ... }:
 
 let systemFor = sys: (depot.ops.nixos.nixosFor sys).system;
 in {
   marcusSystem = systemFor depot.users.wpcarro.nixos.marcus;
+  deploy-diogenes = pkgs.writeShellScriptBin "deploy-diogenes" ''
+    set -euo pipefail
+    readonly TF_STATE_DIR=/depot/users/wpcarro/terraform
+    rm -f $TF_STATE_DIR/*.json
+    readonly STORE_PATH="$(nix-build /depot -A users.wpcarro.nixos.diogenes)"
+    cp $STORE_PATH $TF_STATE_DIR
+
+    function cleanup() {
+      rm -f "$TF_STATE_DIR/$(basename $STORE_PATH)"
+    }
+
+    trap cleanup EXIT
+    ${pkgs.terraform}/bin/terraform -chdir="$TF_STATE_DIR" apply
+  '';
 
   meta.targets = [ "marcusSystem" ];
 }
