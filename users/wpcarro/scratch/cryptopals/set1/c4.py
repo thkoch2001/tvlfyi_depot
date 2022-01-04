7aaddb3d31dabff196eb9ef27666099c00dd5338
William Carroll <wpcarro@gmail.com>
2022-01-04 12:42:30 -0800

    fix(wpcarro/cryptopals): Update cleartext scoring algorithm
    Create a frequency table of alphabetic characters by reading each character in
    "Alice in Wonderland"; use this frequency table to score cleartext when decoding
    ciphers.
    
    Change-Id: Id322af64d792c15231a1a02794f396c46196c207
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/4788
    Tested-by: BuildkiteCI
    Reviewed-by: wpcarro <wpcarro@gmail.com>
    Autosubmit: wpcarro <wpcarro@gmail.com>

--- users/wpcarro/scratch/cryptopals/set1/c4.py
+++ users/wpcarro/scratch/cryptopals/set1/c4.py
@@ -2,11 +2,22 @@ import c3
 
 content = None
 with open('4.txt', 'r') as f:
-    c3.decode_cipher
     content = f.read().splitlines()
+if not content:
+    raise Error("Need content to proceed")
 
+xs = []
 for line in content:
     try:
-        print(c3.decode_cipher(line))
+        x = c3.decode_cipher(line)
+        if x: xs.append(x)
     except:
         continue
+
+freqs = c3.frequency_table()
+print(max(xs, key=lambda x: c3.score(x, freqs)))
+
+################################################################################
+# Answer
+################################################################################
+"Now that the party is jumping"
