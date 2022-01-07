c5886bd3aa42aada32ffceea7e6b1956305b84b2
Vincent Ambo <mail@tazj.in>
2022-01-07 17:27:57 +0300

    fix(whitby): Use podman instead of Docker
    There are all kinds of weird problems with systemd+docker
    interaction (usually manifesting as the sourcegraph container being
    unkillable and stopping deploys from proceeding).
    
    This might alleviate some of those issues as podman has a more
    straightforward model.
    
    Change-Id: I03ab5fd9fbd8bbce99a15d5a2bbe9ccd96533d4a

--- ops/machines/whitby/default.nix
+++ ops/machines/whitby/default.nix
@@ -688,6 +688,10 @@ in {
     };
   };
 
+  # Don't run Docker
+  virtualisation.oci-containers.backend = "podman";
+  virtualisation.podman.extraPackages = [ pkgs.zfs ];
+
   security.acme = {
     acceptTerms = true;
     email = "certs@tvl.fyi";
