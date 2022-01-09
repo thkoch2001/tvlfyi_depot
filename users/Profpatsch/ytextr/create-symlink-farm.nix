8eeaf385ebbe58ff38c23c4de5f44f5f8343f281
Profpatsch <mail@profpatsch.de>
2022-01-09 12:30:03 +0100

    feat(users/Profpatsch): add ytextr, a sandboxed yt-dlp wrapper
    Change-Id: Id0992e5c1f52ac2c95444721c7565a66ef484e2b

---
+++ users/Profpatsch/ytextr/create-symlink-farm.nix
@@ -0,0 +1,18 @@
+{
+  # list of package attribute names to get at run time
+  packageNamesAtRuntimeJsonPath,
+}:
+let
+  pkgs = import <nixpkgs> {};
+
+  getPkg = pkgName: pkgs.${pkgName};
+
+  packageNamesAtRuntime = builtins.fromJSON (builtins.readFile packageNamesAtRuntimeJsonPath);
+
+  runtime = map getPkg packageNamesAtRuntime;
+
+in
+  pkgs.symlinkJoin {
+    name = "symlink-farm";
+    paths = runtime;
+  }
