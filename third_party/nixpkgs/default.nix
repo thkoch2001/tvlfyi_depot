ce8deee4f7ebcc874dfd7647404ab754f4d3a1bb
sterni <sternenseemann@systemli.org>
2022-01-12 13:37:22 +0100

    chore(3p): bump NixOS channels to 2022-01-12
    Change-Id: I21c6a1963ffe4205f3577f531ce10b778a82e2ff

--- third_party/nixpkgs/default.nix
+++ third_party/nixpkgs/default.nix
@@ -13,16 +13,16 @@ let
   # nixos-unstable, and the current stable channel of the latest NixOS
   # release.
 
-  # Tracking nixos-unstable as of 2022-01-02.
+  # Tracking nixos-unstable as of 2022-01-12.
   unstableHashes = {
-    commit = "59bfda72480496f32787cec8c557182738b1bd3f";
-    sha256 = "18akd1chfvniq1q774rigfxgmxwi0wyjljpa1j9ls59szpzr316d";
+    commit = "b2737d4980a17cc2b7d600d7d0b32fd7333aca88";
+    sha256 = "012db5d6k0lajp4q37byhgamz3ry04av1dcpgf3ahm9kzjwsjcch";
   };
 
-  # Tracking nixos-21.11 as of 2022-01-02.
+  # Tracking nixos-21.11 as of 2022-01-11.
   stableHashes = {
-    commit = "d1e59cfc49961e121583abe32e2f3db1550fbcff";
-    sha256 = "03ldf1dlxqf3g8qh9x5vp6vd9zvvr481fyjds111imll69y60wpm";
+    commit = "386234e2a61e1e8acf94dfa3a3d3ca19a6776efb";
+    sha256 = "1qhfham6vhy67xjdvsmhb3prvsg854wfw4l4avxnvclrcm3k2yg8";
   };
 
   # import the nixos-unstable package set, or optionally use the
