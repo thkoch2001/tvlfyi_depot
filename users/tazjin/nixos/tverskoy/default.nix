d96fcdae0d1c9457c6ccb646642c6ab7fdedd71b
Vincent Ambo <mail@tazj.in>
2022-01-15 19:18:09 +0300

    feat(tazjin/tverskoy): Enable & persist gnome-keyring
    Required by Fractal.
    
    Change-Id: Ic82b0d83f6d7c30639c2e149dad79ba4c1a30e88

--- users/tazjin/nixos/tverskoy/default.nix
+++ users/tazjin/nixos/tverskoy/default.nix
@@ -201,6 +201,9 @@ in lib.fix(self: {
       maxFreed = 10; # GiB
       preserveGenerations = "14d";
     };
+
+    # Run keyring daemon, solely required by Fractal.
+    gnome.gnome-keyring.enable = true;
   };
 
   # Set variables to enable EXWM-XIM
@@ -229,12 +232,12 @@ in lib.fix(self: {
   };
 
   programs = {
+    adb.enable = true;
     fish.enable = true;
     light.enable = true;
-    ssh.startAgent = true;
     mosh.enable = true;
+    ssh.startAgent = true;
     steam.enable = true;
-    adb.enable = true;
 
     # Required by impermanence
     fuse.userAllowOther = true;
