5f3918ba6b960b283b952a618205a46e133a64c3
sterni <sternenseemann@systemli.org>
2022-01-15 11:58:44 +0100

    refactor(tools/emacs-pkgs/nix-util): drop unused var nix-depot-path
    Change-Id: Iffff21277a35e8da8125425412a4a3a3f5ffd1ce
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/4942
    Tested-by: BuildkiteCI
    Reviewed-by: tazjin <tazjin@tvl.su>

--- tools/emacs-pkgs/nix-util/nix-util.el
+++ tools/emacs-pkgs/nix-util/nix-util.el
@@ -17,8 +17,6 @@
 (require 'map)
 (require 's)
 
-(defvar nix-depot-path "/home/tazjin/depot")
-
 (defun nix/prefetch-github (owner repo) ; TODO(tazjin): support different branches
   "Fetch the master branch of a GitHub repository and insert the
   call to `fetchFromGitHub' at point."
