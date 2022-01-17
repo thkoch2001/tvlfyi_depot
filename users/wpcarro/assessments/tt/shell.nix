4753e2f28662ffd922d0729cf0996f3a0e7f0dfa
Vincent Ambo <mail@tazj.in>
2022-01-17 17:21:40 +0300

    chore(wpcarro/*): Remove some dead code
    Found this while looking for non-3p uses of builtins.fetchGit. The
    hailgun package is in the package set now, and the other thing was
    unused (and it's also in the package set).
    
    Change-Id: I6f519fd6014bbed90fc6cee695bed7afbcf55717
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/4984
    Tested-by: BuildkiteCI
    Autosubmit: tazjin <tazjin@tvl.su>
    Reviewed-by: wpcarro <wpcarro@gmail.com>

--- users/wpcarro/assessments/tt/shell.nix
+++ users/wpcarro/assessments/tt/shell.nix
@@ -1,24 +1,18 @@
 { pkgs, depot, ... }:
 
-let
-  hailgun-src = builtins.fetchGit {
-    url = "https://bitbucket.org/echo_rm/hailgun.git";
-    rev = "9d5da7c902b2399e0fcf3d494ee04cf2bbfe7c9e";
-  };
-  hailgun = pkgs.haskellPackages.callCabal2nix "hailgun" hailgun-src {};
-in pkgs.mkShell {
+pkgs.mkShell {
   buildInputs = with pkgs; [
     (haskellPackages.ghcWithPackages (hpkgs: with hpkgs; [
-      hpkgs.servant-server
       hpkgs.aeson
+      hpkgs.cryptonite
+      hpkgs.envy
+      hpkgs.hailgun
       hpkgs.resource-pool
+      hpkgs.servant-server
       hpkgs.sqlite-simple
+      hpkgs.uuid
       hpkgs.wai-cors
       hpkgs.warp
-      hpkgs.cryptonite
-      hpkgs.uuid
-      hpkgs.envy
-      hailgun
     ]))
   ];
 }
