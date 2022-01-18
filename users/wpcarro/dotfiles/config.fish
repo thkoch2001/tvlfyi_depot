901d7be37e10ba192df5665ab9c2a5c1a90c641a
William Carroll <wpcarro@gmail.com>
2022-01-18 12:05:24 -0800

    feat(wpcarro/fish): Define fish configuration as dotfile
    TODO(wpcarro): Figure out how this is sourced on NixOS machines.
    
    Change-Id: Iafddab003699f57bbfd09cd29cb9382dadf6a147

---
+++ users/wpcarro/dotfiles/config.fish
@@ -0,0 +1,30 @@
+alias c 'xclip -selection clipboard -i'
+alias p 'xclip -selection clipboard -o'
+alias cat 'bat --theme="Monokai Extended Light"'
+alias rgh 'rg --hidden'
+alias fdh 'fd --hidden'
+alias tpr 'tput reset'
+alias ls 'exa --sort=type'
+alias ll 'exa --long --sort=type'
+alias la 'exa --long --all --sort=type'
+alias gst 'git status'
+alias gsh 'git show HEAD'
+alias gpf 'git push --force-with-lease'
+alias gd 'git diff'
+alias edit 'emacsclient -n'
+# fs navigation
+alias d 'cd /depot'
+
+# environment variables
+set -gx EDITOR "emacsclient"
+set -gx ALTERNATE_EDITOR "emacs -q -nw"
+set -gx VISUAL "emacsclient"
+
+# Use my custom fish prompt
+source ~/.config/fish/prompt.fish
+
+# Configure fuzzy history, file, directory searching
+source (fzf-share)/key-bindings.fish && fzf_key_bindings
+
+# Install direnv
+eval (direnv hook fish)
