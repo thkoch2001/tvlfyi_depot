fb6ba6249634474f70e41f3a2e3d50865908cf75
Vincent Ambo <mail@tazj.in>
2022-01-18 18:33:05 +0300

    feat(stack): Export TVL stack as a josh workspace
    This creates a josh workspace called `stack` which exports all
    dependencies needed by external users of the TVL stack, using
    workspace configuration to map projects into sane paths.
    
    Change-Id: Ied24aaef117fc2e0c188ec742ae7cd993a5babd3

---
+++ stack/default.nix
@@ -0,0 +1,17 @@
+# Externally importable TVL depot stack. This is intended to be called
+# with a supplied package set, otherwise the package set currently in
+# use by the TVL depot will be used.
+#
+# For now, readTree is not used inside of this configuration.
+
+{ pkgs ? import ./nixpkgs { depotOverlays = false; } }:
+
+pkgs.lib.fix(self: {
+  buildGo = import ./buildGo { inherit pkgs; };
+  buildkite = import ./buildkite { inherit pkgs; };
+  readTree = import ./readTree { };
+
+  besadii = import ./besadii {
+    depot.nix.buildGo = self.buildGo;
+  };
+})
