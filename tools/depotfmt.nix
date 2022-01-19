b0f19bc27fbec7b29f065996e629b2c0badef7a4
Vincent Ambo <mail@tazj.in>
2022-01-19 21:23:36 +0300

    refactor(depotfmt): Move formatting check into an extra step
    Change-Id: I7e4cf6bb2351bd11a5396f1663c0d4cc97c0d94e

--- tools/depotfmt.nix
+++ tools/depotfmt.nix
@@ -35,5 +35,9 @@ let
       --tree-root .
   '';
 in depotfmt.overrideAttrs(_: {
-  passthru.check = check;
+  passthru.meta.ci.extraSteps.check = {
+    label = "depot formatting check";
+    script = check;
+    alwaysRun = true;
+  };
 })
