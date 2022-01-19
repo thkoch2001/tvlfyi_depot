2cf902d2f9f1dbad6e4823d9ba78c0a783d81fd0
sterni <sternenseemann@systemli.org>
2022-01-19 13:58:42 +0100

    feat(3p/lisp): add metabang-bind
    Change-Id: I6efcb07b7763c4d108baff717d07097cb324ba4f

---
+++ third_party/lisp/metabang-bind.nix
@@ -0,0 +1,16 @@
+{ depot, pkgs, ... }:
+
+let
+  getSrcs = builtins.map (p: "${pkgs.srcOnly pkgs.lispPackages.metabang-bind}/${p}");
+in
+
+depot.nix.buildLisp.library {
+  name = "metabang-bind";
+
+  srcs = getSrcs [
+    "dev/packages.lisp"
+    "dev/macros.lisp"
+    "dev/bind.lisp"
+    "dev/binding-forms.lisp"
+  ];
+}
