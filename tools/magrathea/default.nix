c8fef8a9736b1da6249e6f0eaa1b8e499357d861
Vincent Ambo <mail@tazj.in>
2022-01-20 18:12:22 +0300

    Test a bunch of different extra steps
    This is not a real CL, just a test run.
    
    Change-Id: Ib9b73cf6b246900c5ebfbd3304e99fd95a1d9622

--- tools/magrathea/default.nix
+++ tools/magrathea/default.nix
@@ -20,4 +20,55 @@ pkgs.stdenv.mkDerivation {
     mkdir -p $out/bin
     csc -o $out/bin/mg -static ${./mg.scm}
   '';
+
+  passthru.meta.ci.extraSteps = {
+    no-config = {
+      script = pkgs.writeShellScript "no-config" ''
+        echo 'no config'
+      '';
+    };
+
+    no-label = {
+      alwaysRun = true;
+      script = pkgs.writeShellScript "no-label" ''
+        echo 'no label'
+      '';
+    };
+
+    gated = {
+      alwaysRun = true;
+      label = "gated extra step";
+      gated = "Do you want to run this?";
+      script = pkgs.writeShellScript "gated-check" ''
+        echo 'ran the gated extra step'
+      '';
+    };
+
+    gated-post = {
+      alwaysRun = true;
+      label = "gated post step";
+      gated = "Do you want to run this?";
+      postCheck = true;
+      script = pkgs.writeShellScript "gated-check" ''
+        echo 'ran the gated post step'
+      '';
+    };
+
+    run-output = {
+      label = "run output";
+      needsOutput = true;
+      script = pkgs.writeShellScript "run-output" ''
+        result/bin/mg
+      '';
+    };
+
+    run-output-post = {
+      label = "run output post";
+      needsOutput = true;
+      postCheck = true;
+      script = pkgs.writeShellScript "run-output-post" ''
+        result/bin/mg
+      '';
+    };
+  };
 }
