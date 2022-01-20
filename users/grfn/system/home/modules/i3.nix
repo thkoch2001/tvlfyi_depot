d7a1e83cfb95b6ed0ddabf2742da12bbe6cd5b07
Griffin Smith <grfn@gws.fyi>
2022-01-20 09:29:17 -0500

    refactor(grfn/home): Tweak screenlayout for new desk layout
    Change-Id: Idca10ee66ed3b083fa914140fef421198462f794

--- users/grfn/system/home/modules/i3.nix
+++ users/grfn/system/home/modules/i3.nix
@@ -10,10 +10,10 @@ let
   screenlayout = {
     home = pkgs.writeShellScript "screenlayout_home.sh" ''
       xrandr \
-        --output eDP-1 --mode 1920x1200 --pos 0x2160 --rotate normal \
+        --output eDP-1 --mode 1920x1200 --pos 0x960 --rotate normal \
+        --output DP-3 --primary --mode 3840x2160 --pos 1920x0 --rotate normal \
         --output DP-1 --off \
         --output DP-2 --off \
-        --output DP-3 --mode 3840x2160 --pos 0x0 --rotate normal \
         --output DP-4 --off
     '';
   };
