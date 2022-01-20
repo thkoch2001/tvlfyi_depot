ce5ecba31d5a9df037680e63839e87a89dcb0a35
Vincent Ambo <mail@tazj.in>
2022-01-20 12:49:33 +0300

    debug change
    Change-Id: I4555c8638da450263fa2fd2c274dfdb69f65578e

--- nix/buildkite/fetch-parent-targets.sh
+++ nix/buildkite/fetch-parent-targets.sh
@@ -15,12 +15,18 @@ set -ueo pipefail
 
 : ${DRVMAP_PATH:=pipeline/drvmap.json}
 
-function most_relevant_builds {
-    git fetch -v origin "${BUILDKITE_PIPELINE_DEFAULT_BRANCH}"
-    local FIRST=$(git merge-base --fork-point HEAD FETCH_HEAD)
-    local SECOND=$(git rev-parse "$FIRST~1")
-    local THIRD=$(git rev-parse "$FIRST~2")
+git status
+git rev-parse HEAD
+cat .git/HEAD
 
+git fetch -v origin "${BUILDKITE_PIPELINE_DEFAULT_BRANCH}"
+
+FIRST=$(git merge-base --fork-point "${BUILDKITE_COMMIT}" FETCH_HEAD)
+SECOND=$(git rev-parse "$FIRST~1")
+THIRD=$(git rev-parse "$FIRST~2")
+
+function most_relevant_builds {
+    set -u
     curl 'https://graphql.buildkite.com/v1' \
          --silent \
          -H "Authorization: Bearer $(cat /run/agenix/buildkite-graphql-token)" \
