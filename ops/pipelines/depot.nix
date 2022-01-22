8324754561311dc60db49ae06778213171b236cf
Vincent Ambo <mail@tazj.in>
2022-01-22 14:47:32 +0300

    refactor(ops/pipelines): Move :anchor: into postBuildSteps
    There is no need for this step to be part of the static pipeline (it
    should not run if the build fails anyways).
    
    Change-Id: I71400a452d6f8f4708d146b346eaffda5da2f766

--- ops/pipelines/depot.nix
+++ ops/pipelines/depot.nix
@@ -19,6 +19,24 @@ let
     parentTargetMap = if (externalArgs ? parentTargetMap)
       then builtins.fromJSON (builtins.readFile externalArgs.parentTargetMap)
       else {};
+
+    postBuildSteps = [
+      # After successful builds, create a gcroot for builds on canon.
+      #
+      # This anchors *most* of the depot, in practice it's unimportant
+      # if there is a build race and we get +-1 of the targets.
+      #
+      # Unfortunately this requires a third evaluation of the graph, but
+      # since it happens after :duck: it should not affect the timing of
+      # status reporting back to Gerrit.
+      {
+        label = ":anchor:";
+        branches = "refs/heads/canon";
+        command = ''
+          nix-build -A ci.gcroot --out-link /nix/var/nix/gcroots/depot/canon
+        '';
+      }
+    ];
   };
 
   drvmap = depot.nix.buildkite.mkDrvmap depot.ci.targets;
