52298e5ba30b460f7e0d7915073809ed756e4e92
Vincent Ambo <mail@tazj.in>
2022-01-24 18:27:03 +0300

    feat(tvl-kit): Use readTree for reading the kit
    This adds more of the logic exposed in the kit to its own build
    pipeline, by relying on readTree to read the kit and expose besadii.
    
    Note that the depot nixpkgs pin has been remapped to '.nixpkgs' to
    avoid re-exposing it through readTree. I think doing it this way is
    cleaner than additional nesting + skip-subtree or some sort of
    `builtins.removeAttrs`-based hack.
    
    Change-Id: I0c38688a9652499b34c1ed6e63aa89df005c66aa

--- views/kit/default.nix
+++ views/kit/default.nix
@@ -1,19 +1,21 @@
 # Externally importable TVL depot stack. This is intended to be called
 # with a supplied package set, otherwise the package set currently in
 # use by the TVL depot will be used.
-#
-# For now, readTree is not used inside of this configuration to keep
-# it simple. Adding it may be useful if we set up test scaffolding
-# around the exported workspace.
 
-{ pkgs ? import ./nixpkgs { depotOverlays = false; }, ... }:
+{ pkgs ? import ./.nixpkgs { depotOverlays = false; }, ... }:
 
-pkgs.lib.fix(self: {
-  buildGo = import ./buildGo { inherit pkgs; };
-  buildkite = import ./buildkite { inherit pkgs; };
-  readTree = import ./readTree { };
-
-  besadii = import ./besadii {
-    depot.nix.buildGo = self.buildGo;
+let
+  readTree = import ./readTree {};
+  readKit = args: readTree {
+    inherit args;
+    path = ./.;
   };
-})
+in readTree.fix(kit: (readKit {
+  # Some targets in tvl-kit expect arguments of the form used in the
+  # main TVL depot tree. This attribute set mirrors that layout.
+  depot.nix.buildGo = kit.buildGo;
+
+  # Some targets expect a pkgs and lib parameter.
+  inherit pkgs;
+  inherit (pkgs) lib;
+}))
