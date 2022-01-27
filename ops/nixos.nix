1ea9189594af0e7f587dc199915304d35e8338d2
William Carroll <wpcarro@gmail.com>
2022-01-26 23:08:30 -0800

    feat(ops/nixos): Support upgrades
    Support calling `rebuild-system` with the latest revision from
    `origin/canon`. This is akin to "upgrading" a system, and -- in the case of
    systems that host web servers and the like -- doing "deploys".
    
    In the process, I rewrote `rebuild-system` as a Haskell program.
    
    Change-Id: Ie48edae705de3f825ec0812a2eb48dbd50c26f6d

--- ops/nixos.nix
+++ ops/nixos.nix
@@ -32,22 +32,6 @@ in rec {
       (throw "${hostname} is not a known NixOS host")
       (map nixosFor depot.ops.machines.all-systems));
 
-  rebuild-system = rebuildSystemWith depot.path;
-
-  rebuildSystemWith = depotPath: pkgs.writeShellScriptBin "rebuild-system" ''
-    set -ue
-    if [[ $EUID -ne 0 ]]; then
-      echo "Oh no! Only root is allowed to rebuild the system!" >&2
-      exit 1
-    fi
-
-    echo "Rebuilding NixOS for $HOSTNAME"
-    system=$(${pkgs.nix}/bin/nix-build -E "((import ${depotPath} {}).ops.nixos.findSystem \"$HOSTNAME\").system" --no-out-link --show-trace)
-
-    ${pkgs.nix}/bin/nix-env -p /nix/var/nix/profiles/system --set $system
-    $system/bin/switch-to-configuration switch
-  '';
-
   # Systems that should be built in CI
   whitbySystem = (nixosFor depot.ops.machines.whitby).system;
   meta.targets = [ "whitbySystem" ];
