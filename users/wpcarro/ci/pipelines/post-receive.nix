4b8998c9c8ff352b0bed96f70e4c2eaa162bf4b9
William Carroll <wpcarro@gmail.com>
2022-01-28 11:55:27 -0800

    feat(wpcarro/emacs): Test that my Emacs can initialize
    ...without warnings, errors.
    
    This was previously a step in my buildkite instance, but I'm no longer
    supporting that (in favor of the TVL one).
    
    Change-Id: Ifed39aef248b21a1eb5a6c9d8427ebe6996abc3a
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/5103
    Tested-by: BuildkiteCI
    Reviewed-by: wpcarro <wpcarro@gmail.com>
    Autosubmit: wpcarro <wpcarro@gmail.com>

--- users/wpcarro/ci/pipelines/post-receive.nix
+++ users/wpcarro/ci/pipelines/post-receive.nix
@@ -2,27 +2,12 @@
 
 let
   inherit (builtins) path toJSON;
-  inherit (depot.users.wpcarro.emacs) initEl runScript;
 
   pipeline.steps = [
     {
       key = "lint-secrets";
       command = "${pkgs.git-secrets}/bin/git-secrets --scan-history";
       label = ":broom: lint secrets";
-    }
-    {
-      key = "init-emacs";
-      command = let
-        scriptEl = path {
-          path = ./script.el;
-          name = "script.el";
-        };
-        runScriptEl = runScript {
-          script = scriptEl;
-        };
-      in "${runScriptEl} ${initEl}";
-      label = ":gnu: initialize Emacs";
-      depends_on = "lint-secrets";
     }
   ];
 in pkgs.writeText "pipeline.yaml" (toJSON pipeline)
