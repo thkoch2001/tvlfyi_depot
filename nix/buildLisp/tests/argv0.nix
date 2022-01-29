6da49e1acaa5d60fd9e390cae1ca0190eb411e5b
sterni <sternenseemann@systemli.org>
2022-01-29 13:59:24 +0100

    fix(nix/buildLisp): make uiop:argv0 work also for ccl and sbcl
    Detection is broken there, too, as UIOP relies on setting the variable
    before dumping the image in its portability wrapper dump-image which we
    don't use at all.
    
    Change-Id: If7bea5a8522a2e64707b1ee88d62d420bd00a952

---
+++ nix/buildLisp/tests/argv0.nix
@@ -0,0 +1,36 @@
+{ depot, pkgs, ... }:
+
+depot.nix.buildLisp.program {
+  name = "argv0-test";
+
+  srcs = [
+    (pkgs.writeText "argv0-test.lisp" ''
+      (defpackage :argv0-test (:use :common-lisp :uiop) (:export :main))
+      (in-package :argv0-test)
+
+      (defun main ()
+        (format t "~A~%" (uiop:argv0)))
+    '')
+  ];
+
+  deps = [
+    {
+      sbcl = depot.nix.buildLisp.bundled "uiop";
+      default = depot.nix.buildLisp.bundled "asdf";
+    }
+  ];
+
+  passthru.meta.ci = {
+    extraSteps.verify = {
+      label = "verify argv[0] output";
+      needsOutput = true;
+      command = pkgs.writeShellScript "check-argv0" ''
+        set -eux
+
+        for invocation in "$(pwd)/result/bin/argv0-test" "./result/bin/argv0-test"; do
+          test "$invocation" = "$("$invocation")"
+        done
+      '';
+    };
+  };
+}
