79b78ad142faf1732e29b4c095d41744c20b6817
Vincent Ambo <mail@tazj.in>
2022-02-07 19:29:52 +0300

    feat(users/tazjin): rustfmt code with non-default settings
    rustfmt only sometimes detects path-based nested config
    files (probably some kind of race?), so my users folder uses a
    separate formatting check for rustfmt to avoid flaky CI. Enough flakes
    around already ...
    
    Change-Id: Ifd862f9974f071b3a256643dd8e56c019116156a

---
+++ users/tazjin/default.nix
@@ -0,0 +1,14 @@
+# //users/tazjin-specific CI configuration.
+{ pkgs, ... }:
+
+let rustfmt = pkgs.writeShellScript "rustfmt-tazjin" ''
+  ${pkgs.fd}/bin/fd -e rs | \
+    ${pkgs.ripgrep}/bin/rg 'users/tazjin' | \
+    xargs ${pkgs.rustfmt}/bin/rustfmt --check --config-path users/tazjin
+'';
+
+in rustfmt.overrideAttrs(_: {
+  meta.ci.extraSteps.rustfmt = {
+    command = rustfmt;
+  };
+})
