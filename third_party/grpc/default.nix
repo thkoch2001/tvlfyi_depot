6a4d1a1e6ac94ca1e0a8d65746ec4f3d75d975bb
Vincent Ambo <mail@tazj.in>
2022-02-07 22:30:37 +0300

    chore(3p/abseil_cpp): unvendor abseil_cpp
    nixpkgs has caught up, but this required some changes in
    //third_party/nix because we manually backported some types
    previously.
    
    Change-Id: If701571d9716de308d3512e1eb22c35db0877a66

--- third_party/grpc/default.nix
+++ third_party/grpc/default.nix
@@ -3,7 +3,6 @@
 (pkgs.grpc.override {
   protobuf = depot.third_party.protobuf;
   stdenv = pkgs.fullLlvm11Stdenv;
-  abseil-cpp = depot.third_party.abseil_cpp;
   re2 = depot.third_party.re2;
 }).overrideAttrs (orig: rec {
   cmakeFlags = orig.cmakeFlags ++ [
