768935acc570a41a6d6885ec6c6f6c11b4b0dab1
Vincent Ambo <mail@tazj.in>
2022-02-17 13:36:57 +0300

    refactor(ops/modules): Move journaldriver configuration into module
    This makes the journaldriver configuration machine-independent.
    The secret is loaded from agenix instead of being persisted on disk.
    
    Change-Id: I592ae7f5726fcb7f37a406f69dcf5ac498eeb1b7

---
+++ ops/modules/journaldriver.nix
@@ -0,0 +1,23 @@
+# Configures journaldriver to forward to the tvl-fyi GCP project from
+# TVL machines.
+{ config, depot, ... }:
+
+{
+  imports = [
+    (depot.third_party.agenix.src + "/modules/age.nix")
+  ];
+
+  age.secrets.journaldriver.file = depot.ops.secrets."journaldriver.age";
+
+  services.journaldriver = {
+    enable = true;
+    googleCloudProject = "tvl-fyi";
+    logStream = config.networking.hostName;
+    applicationCredentials = "journaldriver.json";
+  };
+
+  # Override the systemd service defined in the nixpkgs module to pass
+  # the LoadCredential= option.
+  systemd.services.journaldriver.serviceConfig.LoadCredential =
+    "journaldriver.json:/run/agenix/journaldriver";
+}
