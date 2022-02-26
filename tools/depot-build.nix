bac7f64c8331d618025a02a10625b7f3cb416055
Vincent Ambo <mail@tazj.in>
2022-02-26 17:37:24 +0300

    chore(tools): Remove deprecated depot-build tool
    This has been superseded by magrathea.
    
    Change-Id: Ief4a3d1b81e51e7a9c9a0112584fa7efc8aca63f

--- tools/depot-build.nix
+++
@@ -1,8 +0,0 @@
-# Utility script for building any arbitrary depot path in its folder.
-{ pkgs, ... }:
-
-pkgs.writeShellScriptBin "depot-build" ''
-  TARGET=$(git rev-parse --show-prefix | sed 's|/$||')
-  echo "Building //$TARGET"
-  nix-build -A $(echo $TARGET | sed 's|/|.|g') $(${pkgs.git}/bin/git rev-parse --show-toplevel)
-''
