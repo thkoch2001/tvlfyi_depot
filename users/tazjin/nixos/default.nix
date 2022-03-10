a542649f32bd444f3643ce38dd75f4287cafe54b
Vincent Ambo <mail@tazj.in>
2022-03-10 16:19:07 +0200

    feat(tazjin/polyanka): Check in host configuration
    This host was previously managed manually, now adding it to depot
    because doing that was a huge hassle.
    
    Change-Id: I1e212fce13da735b5329578d9b2dd8f370f25cbc

--- users/tazjin/nixos/default.nix
+++ users/tazjin/nixos/default.nix
@@ -1,10 +1,9 @@
 { depot, lib, ... }:
 
 let systemFor = sys: (depot.ops.nixos.nixosFor sys).system;
-in {
+in depot.nix.readTree.drvTargets {
   camdenSystem = systemFor depot.users.tazjin.nixos.camden;
   frogSystem = systemFor depot.users.tazjin.nixos.frog;
   tverskoySystem = systemFor depot.users.tazjin.nixos.tverskoy;
-
-  meta.ci.targets = [ "camdenSystem" "frogSystem" "tverskoySystem" ];
+  polyankaSystem = (depot.ops.nixos.nixosFor depot.users.tazjin.nixos.polyanka).system;
 }
