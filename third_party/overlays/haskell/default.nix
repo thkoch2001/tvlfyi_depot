30a96503cfb3722b14d9f33a9dffba7946c5be3f
sterni <sternenseemann@systemli.org>
2022-03-31 18:40:08 +0200

    chore(3p/sources): Bump channels & overlays
    Use nixos-unstable-small which fixes CVE-2018-25032
    (out of bounds write while compressing).
    
    * //users/grfn/xanthous:
    
      - generic-arbitrary exposes a GenericArbitrary newtype now.
        This means we no longer have to implement it in xanthous
        downstream and patch generic-arbitrary to expose the
        GArbitrary type class.
    
      - Minor adjustments for lens 5.0:
    
        Xanthous.Game.Memo: clear needs to use ASetter' instead of Lens'
    
        Xanthous.Data.EntityMap: TraversableWithIndex no longer has an
        itraversed function.
    
      - Xanthous.Orphans: adjust for aeson's KeyMap, use KM.size explicitly
        instead of relying on MonoTraversable's length
    
    * //nix/buildLisp: the CCL issue has resurfaced, disabling the
      implementation once again.
    
    * //3p/arion: remove, as depot uses the nixpkgs package of it anyways.
    
    * //users/wpcarro: accomodate GHC 9.0.1's stricter parsing of operators.
    
    * //users/tazjin: disable rustfmt as it stopped respecting settings
    
    * //3p/overlays: upgrade home-manager until fix for serivce generation
      has landed upstream
    
    Change-Id: If229e7317ba48498f85170b57ee9053f6997ff8a

--- third_party/overlays/haskell/default.nix
+++ third_party/overlays/haskell/default.nix
@@ -7,13 +7,26 @@
 self: super: # overlay parameters for the nixpkgs overlay
 
 let
-  overrides = hsSelf: hsSuper: with super.haskell.lib; {
-    generic-arbitrary = appendPatch hsSuper.generic-arbitrary
-      [ ./patches/generic-arbitrary-export-garbitrary.patch ];
+  overrides = hsSelf: hsSuper: with self.haskell.lib.compose; {
+    # No overrides for the default package set necessary at the moment
   };
 in
 {
   haskellPackages = super.haskellPackages.override {
     inherit overrides;
+  };
+
+  haskell = lib.recursiveUpdate super.haskell {
+    packages.ghc8107 = super.haskell.packages.ghc8107.override {
+      overrides = hsSelf: hsSuper: with self.haskell.lib.compose; {
+        # TODO(sterni): TODO(grfn): patch xanthous to work with random-fu 0.3.*
+        # We could then use the default GHC and wouldn't need it at eval time.
+        random-fu = hsSelf.callHackage "random-fu" "0.2.7.7" {};
+        rvar = hsSelf.callHackage "rvar" "0.2.0.6" {};
+
+        # TODO(sterni): upstream this
+        universe-base = addBuildDepend hsSelf.OneTuple hsSuper.universe-base;
+      };
+    };
   };
 }
