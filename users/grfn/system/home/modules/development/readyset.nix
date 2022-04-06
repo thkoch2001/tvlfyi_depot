1c358f42f8c880f69d13f8de7bc58c56db261efa
Griffin Smith <grfn@gws.fyi>
2022-04-06 10:23:10 -0400

    feat(grfn/home): Install somee aws tools
    Change-Id: I599ee3567a61d9bbed1326c481e0def01e68f140

--- users/grfn/system/home/modules/development/readyset.nix
+++ users/grfn/system/home/modules/development/readyset.nix
@@ -8,6 +8,21 @@
   home.packages = with pkgs; [
     # This goes in $PATH so I can run it from rofi and parent to my WM
     (writeShellScriptBin "dotclip" "xclip -out -selection clipboard | dot -Tpng | feh -")
+    (buildGoModule rec {
+      pname = "rain";
+      version = "1.2.0";
+
+      src = fetchFromGitHub {
+        owner = "aws-cloudformation";
+        repo = pname;
+        rev = "v${version}";
+        sha256 = "168gkchshl5f1awqi1cgvdkm6q707702rnn0v4i5djqxmg5rk0p9";
+      };
+
+      vendorSha256 = "16bx7cjh5cq9zlis8lf28i016avgqf3j9fmcvkqzd8db2vxpqx3v";
+    })
+    awscli2
+    amazon-ecr-credential-helper
   ];
 
   programs.zsh.shellAliases = {
