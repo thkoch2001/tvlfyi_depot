edac4b4d22d84bc97bc29d81a9fccc4964a82b63
Vincent Ambo <mail@tazj.in>
2022-04-09 11:40:04 +0200

    feat(ops/open_eid): Add script for setting up browser integration
    Change-Id: Ib339d62d862fd99dab2fda30376b8e47b337a26b

--- ops/modules/open_eid.nix
+++ ops/modules/open_eid.nix
@@ -1,10 +1,28 @@
 # NixOS module to configure the Estonian e-ID software.
 { pkgs, ... }:
 
+let
+  # Wrapper script to add EID support to browsers. Each user needs to
+  # run this themselves, it does not work on a system level due to a
+  # bug in Chromium:
+  #
+  # https://bugs.chromium.org/p/chromium/issues/detail?id=16387
+  #
+  # This is based on the upstream setup script for Debian-derivatives:
+  # https://github.com/open-eid/linux-installer/blob/master/esteid-update-nssdb
+  setup-browser-eid = pkgs.writeShellScriptBin "setup-browser-eid" ''
+    NSSDB="''${HOME}/.pki/nssdb"
+    mkdir -p ''${NSSDB}
+
+    ${pkgs.nssTools}/bin/modutil -force -dbdir sql:$NSSDB -add opensc-pkcs11 \
+      -libfile ${pkgs.opensc}/lib/onepin-opensc-pkcs11.so -mechanisms FRIENDLY
+  '';
+in
 {
   services.pcscd.enable = true;
 
   environment.systemPackages = with pkgs; [
     qdigidoc
+    setup-browser-eid
   ];
 }
