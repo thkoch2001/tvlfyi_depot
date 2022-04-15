e48b0cc84fb5ee0b5931ddf42b30b54e9348f4ac
William Carroll <wpcarro@gmail.com>
2022-04-15 10:33:21 -0700

    feat(wpcarro/emacs): Support calling sudo on a remote buffer
    While pattern-matching in Elisp is a bit unsightly, it works :)
    
    Change-Id: I6766147095823f1a4e233832b0ef21f4c486e023
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/5452
    Tested-by: BuildkiteCI
    Reviewed-by: wpcarro <wpcarro@gmail.com>
    Autosubmit: wpcarro <wpcarro@gmail.com>

--- users/wpcarro/nixos/ava/hardware.nix
+++ users/wpcarro/nixos/ava/hardware.nix
@@ -2,7 +2,8 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
@@ -11,16 +12,18 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/60d92789-c44e-4620-885d-1d81d0759f1d";
+    {
+      device = "/dev/disk/by-uuid/60d92789-c44e-4620-885d-1d81d0759f1d";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/C62C-9B32";
+    {
+      device = "/dev/disk/by-uuid/C62C-9B32";
       fsType = "vfat";
     };
 
-  swapDevices = [];
+  swapDevices = [ ];
 
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   # high-resolution display
