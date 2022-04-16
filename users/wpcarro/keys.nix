9bb106ca46941bca97f663b51dff34608c003948
William Carroll <wpcarro@gmail.com>
2022-04-15 18:24:45 -0700

    feat(ops/wpcarro): Support ava's SSH key 🔑
    Another key on the keyring...
    
    Change-Id: If3660976e9bc0d96dfc5cec4fc1c1f2eb8738ea1
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/5459
    Reviewed-by: wpcarro <wpcarro@gmail.com>
    Autosubmit: wpcarro <wpcarro@gmail.com>
    Tested-by: BuildkiteCI

--- users/wpcarro/keys.nix
+++ users/wpcarro/keys.nix
@@ -5,6 +5,7 @@ rec {
   diogenes = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILFDRfpNXDxQuTJAqVg8+Mm/hOfE5VAJP+Lpw9kA5cDG wpcarro@gmail.com";
   marcus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJkNQJBXekuSzZJ8+gxT+V1+eXTm3hYsfigllr/ARXkf wpcarro@gmail.com";
   nathan = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP2NjuP722VUgpSu5bVUPTfdVNPO8fSW0Jlas8L4up13 bill@nathan";
+  ava = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB/5Fuo7wi8rNXVXgNaCK2X6ePCh9LQs/9h7Tj6UeXrl wpcarro@ava";
 
-  all = [ diogenes marcus nathan ];
+  all = [ diogenes marcus nathan ava ];
 }
