be54c8f3dc16c8329a2d2d7a02b9d3d4e1c1f188
sterni <sternenseemann@systemli.org>
2022-05-04 16:02:59 +0200

    chore(3p/sources): Bump channels & overlays
    * //nix/buildLisp: ccl dumped images have fixed themselves… again
    
    * //3p/git: rebase patch on 2.36.0
    
    * //3p/overlays/haskell: remove upstreamed workarounds
    
    Change-Id: I9dc11c0846641341adbdcc7162cbf149a15fe0cb

--- third_party/overlays/haskell/default.nix
+++ third_party/overlays/haskell/default.nix
@@ -9,14 +9,6 @@ self: super: # overlay parameters for the nixpkgs overlay
 let
   overrides = hsSelf: hsSuper: with self.haskell.lib.compose; {
     # No overrides for the default package set necessary at the moment
-    # TODO(sterni): upstreamable?
-    generic-arbitrary = appendPatch
-      (self.fetchpatch {
-        name = "generic-arbitrary-no-negative-resize.patch";
-        url = "https://github.com/typeable/generic-arbitrary/commit/c13d119d8ad0d43860ecdb93b357b0239e366a6c.patch";
-        sha256 = "1jgbd2jn575icqw9nfdzh57nacm3pn8n53ka52129pnfjqfzyhsi";
-      })
-      hsSuper.generic-arbitrary;
   };
 in
 {
@@ -32,9 +24,6 @@ in
           # so we can use GHC 9.0.2 and benefit from upstream binary cache.
           random-fu = hsSelf.callPackage ./extra-pkgs/random-fu-0.2.nix { };
           rvar = hsSelf.callPackage ./extra-pkgs/rvar-0.2.nix { };
-
-          # TODO(sterni): upstream this
-          universe-base = addBuildDepend hsSelf.OneTuple hsSuper.universe-base;
         }
       );
     };
