c56a0e276024da1cc0fef4d85d3e866f1b8e1eb0
zimbatm <zimbatm@zimbatm.com>
2022-05-05 19:18:01 +0200

    chore: simplify the .envrc loading
    nix-shell pollutes the environment with all sorts of variables. Let's
    just add the tools to the PATH?
    
    This also papers over the various differences in users `use_nix`
    implementations by not using it at all.
    
    Change-Id: If4282531fd6b7453b3611fe50217beacadc08bb5
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/5524
    Tested-by: BuildkiteCI
    Reviewed-by: tazjin <tazjin@tvl.su>

--- tools/depot-deps.nix
+++ tools/depot-deps.nix
@@ -2,33 +2,26 @@
 # that should be lazily made available in depot.
 { pkgs, depot, ... }:
 
-let
-  deps = depot.nix.lazy-deps {
-    age-keygen.attr = "third_party.nixpkgs.age";
-    age.attr = "third_party.nixpkgs.age";
-    depotfmt.attr = "tools.depotfmt";
-    gerrit-update.attr = "tools.gerrit-update";
-    gerrit.attr = "tools.gerrit-cli";
-    hash-password.attr = "tools.hash-password";
-    mg.attr = "tools.magrathea";
-    nint.attr = "nix.nint";
-    niv.attr = "third_party.nixpkgs.niv";
-    rebuild-system.attr = "ops.nixos.rebuildSystem";
-    rink.attr = "third_party.nixpkgs.rink";
+depot.nix.lazy-deps {
+  age-keygen.attr = "third_party.nixpkgs.age";
+  age.attr = "third_party.nixpkgs.age";
+  depotfmt.attr = "tools.depotfmt";
+  gerrit-update.attr = "tools.gerrit-update";
+  gerrit.attr = "tools.gerrit-cli";
+  hash-password.attr = "tools.hash-password";
+  mg.attr = "tools.magrathea";
+  nint.attr = "nix.nint";
+  niv.attr = "third_party.nixpkgs.niv";
+  rebuild-system.attr = "ops.nixos.rebuildSystem";
+  rink.attr = "third_party.nixpkgs.rink";
 
-    tf-glesys = {
-      attr = "ops.glesys.terraform";
-      cmd = "terraform";
-    };
-
-    tf-keycloak = {
-      attr = "ops.keycloak.terraform";
-      cmd = "terraform";
-    };
+  tf-glesys = {
+    attr = "ops.glesys.terraform";
+    cmd = "terraform";
   };
-in
-pkgs.mkShell {
-  buildInputs = [
-    deps
-  ];
+
+  tf-keycloak = {
+    attr = "ops.keycloak.terraform";
+    cmd = "terraform";
+  };
 }
