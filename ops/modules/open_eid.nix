8105ed0c9a4ebcc3ddb40cf81d1a306aade35305
Florian Klink <flokli@flokli.de>
2022-05-07 21:06:07 +0200

    feat(ops/modules/open_eid.nix): document firefox
    Firefox users can add p11-kit-proxy (or other SecurityDevices)
    system-wide, by making use of the extraPolicies functionality.
    
    Change-Id: Id58b6cab425199fb0e09e846db2a86d302c0de0d

--- ops/modules/open_eid.nix
+++ ops/modules/open_eid.nix
@@ -9,6 +9,9 @@ let
   #
   # https://bugs.chromium.org/p/chromium/issues/detail?id=16387
   #
+  # Firefox users can just set
+  # extraPolicies.SecurityDevices.p11-kit-proxy "${pkgs.p11-kit}/lib/p11-kit-proxy.so";
+  # when overriding the firefox derivation.
   setup-browser-eid = pkgs.writeShellScriptBin "setup-browser-eid" ''
     NSSDB="''${HOME}/.pki/nssdb"
     mkdir -p ''${NSSDB}
