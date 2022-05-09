791de261121a6ef75fbfacae8715b6665b662ae8
Vincent Ambo <mail@tazj.in>
2022-05-09 16:05:05 +0200

    chore(3p/agenix): Use pkgs.fetchFromGitHub for agenix source
    It costs me money and time to use uncacheable builtin fetchers.
    
    Change-Id: Ie38923667cc6ac54ee85701088c9a5245ccf1c25

--- third_party/agenix/default.nix
+++ third_party/agenix/default.nix
@@ -1,9 +1,10 @@
 { pkgs, ... }:
 
 let
-  commit = "52ea2f8c3231cc2b5302fa28c63588aacb77ea29";
-  src = builtins.fetchTarball {
-    url = "https://github.com/ryantm/agenix/archive/${commit}.tar.gz";
+  src = pkgs.fetchFromGitHub {
+    owner = "ryantm";
+    repo = "agenix";
+    rev = "52ea2f8c3231cc2b5302fa28c63588aacb77ea29";
     sha256 = "1sqgbriwmvxcmqp0zbk7873psk9g60a53fgrr9p0jafki5zzgvdx";
   };
   agenix = import src {
