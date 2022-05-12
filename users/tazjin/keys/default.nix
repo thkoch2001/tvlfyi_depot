8d0247fe631b20ba187a17353ef9f0d0bab71807
Vincent Ambo <mail@tazj.in>
2022-05-12 16:04:21 +0200

    feat(tazjin/keys): Add zamalek_sk public key
    Change-Id: I4a35c5cb691440c891af80290a3fd481e646815d
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/5580
    Reviewed-by: tazjin <tazjin@tvl.su>
    Autosubmit: tazjin <tazjin@tvl.su>
    Tested-by: BuildkiteCI

--- users/tazjin/keys/default.nix
+++ users/tazjin/keys/default.nix
@@ -7,4 +7,5 @@ in withAll {
   tverskoy = "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBAWvA3RpXpMAqruUbB+eVgvvHCzhs5R9khFRza3YSLeFiIqOxVVgyhzW/BnCSD9t/5JrqRdJIGQLnkQU9m4REhUAAAAEc3NoOg== tazjin@tverskoy";
   tverskoy_ed25519 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM1fGWz/gsq+ZeZXjvUrV+pBlanw1c3zJ9kLTax9FWQy tazjin@tverskoy";
   unihertz_sk = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMgLwumRAp7u9sgaFR5aneNbQo07ZXXAFWV45pwzUiP2 unihertz-sk";
+  zamalek_sk = "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIOAw3OaPAjnC6hArGYEmBoXhPf7aZdRGlDZcSqm6gbB8AAAABHNzaDo= tazjin@zamalek";
 }
