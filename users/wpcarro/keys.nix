11a8eea0e392114f5077658b629db30a4eea8b8d
William Carroll <wpcarro@gmail.com>
2022-05-22 20:19:27 -0700

    feat(wpcarro/tarasco): Support tarasco 🇲🇽
    Named after the Mexican restaurant, El Tarasco, in El Porto, which I live 3m
    walking distance from.
    
    Change-Id: I2cd4b68eaa974ad6c8fec73e0566bc0b831c57a8
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/5743
    Reviewed-by: wpcarro <wpcarro@gmail.com>
    Reviewed-by: tazjin <tazjin@tvl.su>
    Autosubmit: wpcarro <wpcarro@gmail.com>
    Tested-by: BuildkiteCI

--- users/wpcarro/keys.nix
+++ users/wpcarro/keys.nix
@@ -2,10 +2,17 @@
 { ... }:
 
 rec {
+  ava = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB/5Fuo7wi8rNXVXgNaCK2X6ePCh9LQs/9h7Tj6UeXrl wpcarro@ava";
   diogenes = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILFDRfpNXDxQuTJAqVg8+Mm/hOfE5VAJP+Lpw9kA5cDG wpcarro@gmail.com";
   marcus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJkNQJBXekuSzZJ8+gxT+V1+eXTm3hYsfigllr/ARXkf wpcarro@gmail.com";
   nathan = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP2NjuP722VUgpSu5bVUPTfdVNPO8fSW0Jlas8L4up13 bill@nathan";
-  ava = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB/5Fuo7wi8rNXVXgNaCK2X6ePCh9LQs/9h7Tj6UeXrl wpcarro@ava";
+  tarasco = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOh+wG4f7tI0IwGyF2sLi5mPlh3JKE7KqV2ab0tlcL36 wpcarro@tarasco";
 
-  all = [ diogenes marcus nathan ava ];
+  all = [
+    ava
+    diogenes
+    marcus
+    nathan
+    tarasco
+  ];
 }
