2492f1ae5458fe949570b7613c61d3abeb1ec3f1
Vincent Ambo <mail@tazj.in>
2022-05-25 18:17:42 +0200

    feat(tools/releases): Add release helper for mirroring to Github
    This adds an extra step definition which can push the result of
    running a josh filter on the repository to Github.
    
    Change-Id: I1f93ae78e1bf452fbd1b21ce943a60acc85c944f

---
+++ tools/releases/default.nix
@@ -0,0 +1,27 @@
+# Definitions for simple release mechanisms from depot.
+{ depot, lib, pkgs, ... }:
+
+let
+  inherit (lib.strings) sanitizeDerivationName;
+in
+{
+  # Use a josh filter to push a certain subset of canon to another git
+  # repository.
+  #
+  # This expects, of course, that the remote repository has granted
+  # push access to the CI SSH key.
+  filteredGitPush = { filter, remote, ref ? "refs/heads/canon", force ? false }: {
+    label = ":git: push '${filter}' to external git repository";
+    branches = [ "canon" ];
+
+    command = pkgs.writeShellScript "${sanitizeDerivationName filter}-push" ''
+      set -e
+      echo 'Filtering depot through ${filter}'
+      ${depot.third_party.josh}/bin/josh-filter '${filter}'
+
+      echo 'Pushing filtered repository to ${remote}:${ref}'
+      ${pkgs.git}/bin/git push ${lib.optionalString force "-f"} \
+        '${remote}' FILTERED_HEAD:${ref}
+    '';
+  };
+}
