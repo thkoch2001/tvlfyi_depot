b5fd56b8936c512c0e0dca8db1933da0e16a82b2
Vincent Ambo <mail@tazj.in>
2022-05-26 14:44:11 +0200

    refactor(grfn): Prepare for restricted-eval
    Change-Id: I672ad0898b2ef6a11f8bc9233da0ded4a296fe0e

--- users/grfn/xanthous/server/default.nix
+++ users/grfn/xanthous/server/default.nix
@@ -7,6 +7,13 @@ depot.third_party.naersk.buildPackage {
   name = "xanthous-server";
   version = "0.0.1";
   src = depot.third_party.gitignoreSource ./.;
+
+  # Workaround for a potential Nix bug related to restricted eval.
+  root = depot.nix.sparseTree ./. [
+    ./Cargo.toml
+    ./Cargo.lock
+  ];
+
   passthru = {
     docker = import ./docker.nix args;
   };
