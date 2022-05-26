bc42c5a61b90f299e60c5dccef8df58597880224
Vincent Ambo <mail@tazj.in>
2022-05-26 11:43:03 +0200

    fix(ops/modules): adapt for changed ssh.knownHosts
    Somehow this ended up generating an empty file, with this change it is
    fine again. I was looking at the recent commits of the module in
    nixpkgs but couldn't quite figure it out, there are also some vague
    references to the attribute set key being used as a hostname, but this
    doesn't seem to be true in practice.
    
    To be clear, the previous code was wrong, but at some point it
    generated a file that accidentally worked.
    
    Change-Id: I42d55730c09daafe6d6fe0eb3647135e84737bca
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/5670
    Reviewed-by: sterni <sternenseemann@systemli.org>
    Tested-by: BuildkiteCI
    Autosubmit: tazjin <tazjin@tvl.su>

--- ops/modules/known-hosts.nix
+++ ops/modules/known-hosts.nix
@@ -5,17 +5,17 @@
   programs.ssh.knownHosts = {
     whitby = {
       hostNames = [ "whitby.tvl.fyi" "whitby.tvl.su" ];
-      publicKey = "whitby.tvl.fyi ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILNh/w4BSKov0jdz3gKBc98tpoLta5bb87fQXWBhAl2I";
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILNh/w4BSKov0jdz3gKBc98tpoLta5bb87fQXWBhAl2I";
     };
 
     sanduny = {
       hostNames = [ "sanduny.tvl.su" ];
-      publicKey = "sanduny.tvl.su ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOag0XhylaTVhmT6HB8EN2Fv5Ymrc4ZfypOXONUkykTX";
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOag0XhylaTVhmT6HB8EN2Fv5Ymrc4ZfypOXONUkykTX";
     };
 
     github = {
       hostNames = [ "github.com" ];
-      publicKey = "github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
     };
   };
 }
