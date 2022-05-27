5926434495416989dd31b460c48616d993d085f6
Vincent Ambo <mail@tazj.in>
2022-05-28 00:58:05 +0200

    test(tools/hash-password): ensure that script can execute correctly
    This tests loading of the argon2 OpenLDAP module. Relates to b/184
    
    Change-Id: I661af4ddc238ad02d082b3a0cede55af5ef13f1b

--- tools/hash-password.nix
+++ tools/hash-password.nix
@@ -2,6 +2,15 @@
 # creating an ARGON2 password hash.
 { pkgs, ... }:
 
-pkgs.writeShellScriptBin "hash-password" ''
-  ${pkgs.openldap}/bin/slappasswd -o module-load=pw-argon2 -h '{ARGON2}'
-''
+let
+  script = pkgs.writeShellScriptBin "hash-password" ''
+    ${pkgs.openldap}/bin/slappasswd -o module-load=argon2 -h '{ARGON2}' "$@"
+  '';
+in
+script.overrideAttrs (old: {
+  doCheck = true;
+  checkPhase = ''
+    chmod +x $out/bin/hash-password # sic!
+    $out/bin/hash-password -s example-password > /dev/null
+  '';
+})
