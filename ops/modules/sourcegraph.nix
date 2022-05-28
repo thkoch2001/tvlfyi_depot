70511f4ba3f350a046c16644de7a3ba7956c152d
Vincent Ambo <mail@tazj.in>
2022-05-28 13:13:32 +0200

    chore(ops/sourcegraph): Bump to 3.34.2
    Change-Id: I865335006a091986f8a98e4d5da7161a25e948d9

--- ops/modules/sourcegraph.nix
+++ ops/modules/sourcegraph.nix
@@ -35,7 +35,7 @@ in
     };
 
     virtualisation.oci-containers.containers.sourcegraph = {
-      image = "sourcegraph/server:3.33.2";
+      image = "sourcegraph/server:3.34.2";
 
       ports = [
         "127.0.0.1:${toString cfg.port}:7080"
