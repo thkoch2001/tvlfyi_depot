62f44ffb47380fc13f96759e19d1f0165be598c0
Vincent Ambo <mail@tazj.in>
2022-06-03 23:15:22 +0000

    test(ops/keycloak): Validate Terraform configuration in CI
    Change-Id: I5602cf722b9fe9502c9d7610eefc7ba0ab647362

--- ops/keycloak/default.nix
+++ ops/keycloak/default.nix
@@ -1,8 +1,14 @@
 { depot, pkgs, ... }:
 
-depot.nix.readTree.drvTargets {
+depot.nix.readTree.drvTargets rec {
   # Provide a Terraform wrapper with the right provider installed.
   terraform = pkgs.terraform.withPlugins (p: [
     p.keycloak
   ]);
+
+  validate = pkgs.runCommand "tf-validate-keycloak" { } ''
+    cp -r ${lib.cleanSource ./.} . && chmod -R u+w .
+    ${terraform}/bin/terraform init -upgrade -backend=false -input=false
+    ${terraform}/bin/terraform validate | tee $out
+  '';
 }
