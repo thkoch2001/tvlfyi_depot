1922ba0ca0e2f3b43b3673dfe634d5f7b8c2b065
Vincent Ambo <mail@tazj.in>
2022-06-03 23:14:07 +0000

    test(ops/buildkite): Validate Terraform configuration in CI
    Change-Id: Ieef4d7d0a717107ee67432474683f3344b6561f8

--- ops/buildkite/default.nix
+++ ops/buildkite/default.nix
@@ -1,7 +1,13 @@
-{ depot, pkgs, ... }:
+{ depot, lib, pkgs, ... }:
 
-depot.nix.readTree.drvTargets {
+depot.nix.readTree.drvTargets rec {
   terraform = pkgs.terraform.withPlugins (p: [
     p.buildkite
   ]);
+
+  validate = pkgs.runCommand "tf-validate-buildkite" { } ''
+    cp -r ${lib.cleanSource ./.} . && chmod -R u+w .
+    ${terraform}/bin/terraform init -upgrade -backend=false -input=false
+    ${terraform}/bin/terraform validate | tee $out
+  '';
 }
