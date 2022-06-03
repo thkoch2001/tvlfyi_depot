243588dd3a7254ca664d1a7e6a09c5f3c4ee0396
Vincent Ambo <mail@tazj.in>
2022-06-03 23:15:13 +0000

    test(ops/glesys): Validate Terraform configuration in CI
    Change-Id: I8d251d3ee1de77feca865d0a677041c9c485d211

--- ops/glesys/default.nix
+++ ops/glesys/default.nix
@@ -1,8 +1,14 @@
 { depot, pkgs, ... }:
 
-depot.nix.readTree.drvTargets {
+depot.nix.readTree.drvTargets rec {
   # Provide a Terraform wrapper with the right provider installed.
   terraform = pkgs.terraform.withPlugins (_: [
     depot.third_party.terraform-provider-glesys
   ]);
+
+  validate = pkgs.runCommand "tf-validate-glesys" { } ''
+    cp -r ${lib.cleanSource ./.} . && chmod -R u+w .
+    ${terraform}/bin/terraform init -upgrade -backend=false -input=false
+    ${terraform}/bin/terraform validate | tee $out
+  '';
 }
