b29cf89e7d972ec05c4c56100cf055f6dd55af94
William Carroll <wpcarro@gmail.com>
2022-06-30 09:29:30 -0700

    fix(wpcarro/website): Redirect wpcarro.dev traffic
    To https://billandhiscomputer.com (instead of the nginx default landing page).
    
    Change-Id: Id08a3a54c0753c20370a45ff6344128f180d52dd

--- users/wpcarro/nixos/modules/www/wpcarro.dev.nix
+++ users/wpcarro/nixos/modules/www/wpcarro.dev.nix
@@ -5,6 +5,7 @@
     services.nginx.virtualHosts."wpcarro.dev" = {
       enableACME = true;
       forceSSL = true;
+      extraConfig = "return 302 https://billandhiscomputer.com$request_uri;";
     };
   };
 }
