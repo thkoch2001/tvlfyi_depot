01bfe729a279b8d8b398eeda922ba176743fe1fe
sterni <sternenseemann@systemli.org>
2022-07-02 11:22:05 +0200

    chore(3p/sources): Bump channels & overlays
    * //users/grfn/home/yeren: remove discord override which broke eval due
      to a changed overriding scheme adopted in nixpkgs. nixpkgs has discord
      0.0.18 now, so updating from 0.0.16 is probably fine.
    
    Change-Id: I540ff3621dcb548d33a929588fbe7fddc3078050
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/5912
    Tested-by: BuildkiteCI
    Autosubmit: sterni <sternenseemann@systemli.org>
    Reviewed-by: grfn <grfn@gws.fyi>
    Reviewed-by: tazjin <tazjin@tvl.su>

--- users/grfn/system/home/machines/yeren.nix
+++ users/grfn/system/home/machines/yeren.nix
@@ -33,15 +33,7 @@ in
     xdot
     tdesktop
     subsurface
-
-    (discord.override rec {
-      version = "0.0.16";
-      src = fetchurl {
-        url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
-        sha256 = "1s9qym58cjm8m8kg3zywvwai2i3adiq6sdayygk2zv72ry74ldai";
-      };
-    })
-
+    discord
     steam
   ];
 
