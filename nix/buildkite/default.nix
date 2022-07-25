578ac349bd3e11029474ce9e40115c83212c23c0
Jean-François Roche <jfroche@affinitic.be>
2022-07-25 16:46:51 +0200

    feat(ops/buildkite): allow explicit dependencies in extra steps
    When running multiple extra steps, it lets the user define dependencies
    between steps using the `identifier` and `depends_on` attributes.
    
    Change-Id: I5d3e34967bb20d751a2a736351edc5adc828b3d9

--- nix/buildkite/default.nix
+++ nix/buildkite/default.nix
@@ -283,6 +283,17 @@ rec {
   #   alwaysRun (optional): If set to true, this step will always run,
   #     even if its parent has not been rebuilt.
   #
+  #   skip (optional): If set to true, this step will not be run.
+  #     Useful in case the step is temporarily failing.
+  #
+  #   agent (optional): Target the buildkite agent that will be running
+  #     the step.
+  #
+  #   identifier (optional): Unique identifier to manage dependencies.
+  #
+  #   dependsOn (optional): List of steps identifier that this step depends on.
+  #
+  #
   # Note that gated steps are independent of each other.
 
   # Create a gated step in a step group, independent from any other
@@ -325,6 +336,8 @@ rec {
     , postBuild ? null
     , skip ? false
     , agents ? null
+    , identifier ? null
+    , dependsOn ? null
     }:
     let
       parent = overridableParent parentOverride;
@@ -350,7 +363,9 @@ rec {
         parent
         parentLabel
         skip
-        agents;
+        agents
+        identifier
+        dependsOn;
 
       # //nix/buildkite is growing a new feature for adding different
       # "build phases" which supersedes the previous `postBuild`
@@ -408,7 +423,14 @@ rec {
       } // (lib.optionalAttrs (cfg.agents != null) { inherit (cfg) agents; })
       // (lib.optionalAttrs (cfg.branches != null) {
         branches = lib.concatStringsSep " " cfg.branches;
+      })
+      // (lib.optionalAttrs (cfg.identifier != null) {
+        key = cfg.identifier;
+      })
+      // (lib.optionalAttrs (cfg.dependsOn != null) {
+        depends_on = cfg.dependsOn;
       });
+
     in
     if (isString cfg.prompt)
     then
