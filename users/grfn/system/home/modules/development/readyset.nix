45fbddbc6df10d6d9bb08ce6323a5b0ea2736597
sterni <sternenseemann@systemli.org>
2022-07-28 13:14:47 +0200

    chore(3p/sources): Bump channels & overlays
    * //users/grfn/system/home: disable rain in readyset development module
      because it fails to build for mysterious reasons.
    
    Change-Id: Id7d23b66bc3ba0202a01b4f8d670854e1f31175e

--- users/grfn/system/home/modules/development/readyset.nix
+++ users/grfn/system/home/modules/development/readyset.nix
@@ -8,19 +8,26 @@
   home.packages = with pkgs; [
     # This goes in $PATH so I can run it from rofi and parent to my WM
     (writeShellScriptBin "dotclip" "xclip -out -selection clipboard | dot -Tpng | feh -")
-    (buildGoModule rec {
-      pname = "rain";
-      version = "1.2.0";
+    # https://buildkite.com/tvl/depot/builds/14577#0182449d-11fa-489a-a768-e7b004172c56
+    #
+    #   cft/build/build.go:38:10: fmt.Errorf format %w has arg r of wrong type interface{}
+    #   cft/build/build.go:78:10: fmt.Errorf format %w has arg r of wrong type interface{}
+    #   cft/build/build.go:167:10: fmt.Errorf format %w has arg r of wrong type interface{}
+    #   FAIL    github.com/aws-cloudformation/rain/cft/build [build failed]
+    #
+    # (buildGoModule rec {
+    #   pname = "rain";
+    #   version = "1.2.0";
 
-      src = fetchFromGitHub {
-        owner = "aws-cloudformation";
-        repo = pname;
-        rev = "v${version}";
-        sha256 = "168gkchshl5f1awqi1cgvdkm6q707702rnn0v4i5djqxmg5rk0p9";
-      };
+    #   src = fetchFromGitHub {
+    #     owner = "aws-cloudformation";
+    #     repo = pname;
+    #     rev = "v${version}";
+    #     sha256 = "168gkchshl5f1awqi1cgvdkm6q707702rnn0v4i5djqxmg5rk0p9";
+    #   };
 
-      vendorSha256 = "16bx7cjh5cq9zlis8lf28i016avgqf3j9fmcvkqzd8db2vxpqx3v";
-    })
+    #   vendorSha256 = "00bx7cjh5cq9zlis8lf28i016avgqf3j9fmcvkqzd8db2vxpqx3v";
+    # })
     awscli2
     amazon-ecr-credential-helper
   ];
