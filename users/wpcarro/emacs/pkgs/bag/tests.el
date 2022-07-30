3935848a5dffc5b5193feca267427671525cbaf3
William Carroll <wpcarro@gmail.com>
2022-07-30 07:09:40 -0700

    fix(wpcarro/emacs): Use should macros for bag.el tests
    :facepalm: (more fixes forthcoming)
    
    Change-Id: Ibcbea21c44034a2fe6649c22ba2c140da15ec004
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/6008
    Reviewed-by: wpcarro <wpcarro@gmail.com>
    Autosubmit: wpcarro <wpcarro@gmail.com>
    Tested-by: BuildkiteCI

--- users/wpcarro/emacs/pkgs/bag/tests.el
+++ users/wpcarro/emacs/pkgs/bag/tests.el
@@ -12,25 +12,21 @@
 (setq fixture (bag-from-list '(1 1 1 2 2 3)))
 
 (ert-deftest bag-add ()
-  (and
-   (not (bag-contains? 4 fixture))
-   (bag-contains? 4 (bag-add 4 fixture))))
+  (should (not (bag-contains? 4 fixture)))
+  (should (bag-contains? 4 (bag-add 4 fixture))))
 
 (ert-deftest bag-remove ()
-  (and
-   (bag-contains? 1 fixture)
-   (not (bag-contains? 3 (bag-remove 3 fixture)))))
+  (should (bag-contains? 1 fixture))
+  (should (not (bag-contains? 3 (bag-remove 3 fixture)))))
 
 (ert-deftest bag-count ()
-  (and
-   (= 3 (bag-count 1 fixture))
-   (= 2 (bag-count 2 fixture))
-   (= 1 (bag-count 3 fixture))))
+  (should (= 3 (bag-count 1 fixture)))
+  (should (= 2 (bag-count 2 fixture)))
+  (should (= 1 (bag-count 3 fixture))))
 
 (ert-deftest bag-total ()
-  (= 6 (bag-total fixture)))
+  (should (= 6 (bag-total fixture))))
 
 (ert-deftest bag-contains? ()
-  (and
-   (bag-contains? 1 fixture)
-   (not (bag-contains? 4 fixture))))
+  (should (bag-contains? 1 fixture))
+  (should (not (bag-contains? 4 fixture))))
