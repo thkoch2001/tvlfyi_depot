4996463df3599fb7868be19e453d999e80c3aa42
Profpatsch <mail@profpatsch.de>
2022-08-20 02:34:02 +0200

    feat(users/Profpatsch): init haskell arglib-netencode
    Mirrors the rust exec-helpers
    
    Change-Id: I96f211d6545e228b8ae0c1dce4e60e6f6867690c

--- users/Profpatsch/arglib/netencode.nix
+++ users/Profpatsch/arglib/netencode.nix
@@ -36,6 +36,27 @@ let
           t
       }
     '';
+
+    haskell = pkgs.haskellPackages.mkDerivation {
+      pname = "arglib-netencode";
+      version = "0.1.0";
+
+      src = depot.users.Profpatsch.exactSource ./. [
+        ./arglib-netencode.cabal
+        ./ArglibNetencode.hs
+      ];
+
+      libraryHaskellDepends = [
+        depot.users.Profpatsch.my-prelude
+        depot.users.Profpatsch.netencode
+      ];
+
+      isLibrary = true;
+      license = lib.licenses.mit;
+
+
+    };
+
   };
 
 in
