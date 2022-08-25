b51bf0c13d3a7b4e6404afe722ee4c18b1d11091
sterni <sternenseemann@systemli.org>
2022-08-25 14:54:32 +0200

    fix: reflect renames of Nix configuration options
    Change-Id: I7e28ac3d71acd7d99a1d3ef97bef9422097e4abf

--- users/tazjin/nixos/modules/home-config.nix
+++ users/tazjin/nixos/modules/home-config.nix
@@ -12,9 +12,7 @@
     initialHashedPassword = "$2b$05$1eBPdoIgan/C/L8JFqIHBuVscQyTKw1L/4VBlzlLvLBEf6CXS3EW6";
   };
 
-  nix = {
-    trustedUsers = [ "tazjin" ];
-  };
+  nix.settings.trusted-users = [ "tazjin" ];
 
   home-manager.useGlobalPkgs = true;
   home-manager.users.tazjin = depot.users.tazjin.home."${config.networking.hostName}";
