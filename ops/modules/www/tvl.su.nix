d5729c8a2358e8b1c1884c6c38d497097de92893
Vincent Ambo <mail@tazj.in>
2022-09-12 16:25:45 +0300

    feat(ops/modules): deploy tvixbolt to tvl.su/tvixbolt
    Change-Id: I534cf918fc3e03ce8c14cf15f6d3280b6a657c8d

--- ops/modules/www/tvl.su.nix
+++ ops/modules/www/tvl.su.nix
@@ -8,13 +8,15 @@
   config = {
     services.nginx.virtualHosts."tvl.su" = {
       serverName = "tvl.su";
-      root = depot.corp.website;
       enableACME = true;
       forceSSL = true;
 
       extraConfig = ''
         add_header Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" always;
       '';
+
+      locations."/".root = depot.corp.website;
+      locations."/tvixbolt/".alias = depot.corp.tvixbolt;
     };
   };
 }
