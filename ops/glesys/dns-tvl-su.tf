5a52049e6f6723e4061f6551b2895603fd7cff27
Vincent Ambo <mail@tazj.in>
2022-09-12 16:57:41 +0300

    feat(ops/glesys): add CNAME for tvixbolt.tvl.su
    I could not get nginx to serve it from `tvl.su/tvixbolt`, and ran out
    of interest in trying to fix it, so lets put it on a subdomain
    instead.
    
    Change-Id: I1313d75cc9831d94a894191376534b1e5186a76a

--- ops/glesys/dns-tvl-su.tf
+++ ops/glesys/dns-tvl-su.tf
@@ -76,6 +76,17 @@ resource "glesys_dnsdomain_record" "tvl_su_whitby_services" {
   for_each = toset(local.whitby_services)
 }
 
+# Explicit records for corp-only services running on whitby.
+resource "glesys_dnsdomain_record" "tvl_su_corp_whitby_services" {
+  domain = glesys_dnsdomain.tvl_su.id
+  type   = "CNAME"
+  data   = "whitby.tvl.su."
+  host   = each.key
+  for_each = toset([
+    "tvixbolt",
+  ])
+}
+
 resource "glesys_dnsdomain_record" "tvl_su_TXT_google_site" {
   domain = glesys_dnsdomain.tvl_su.id
   host   = "@"
