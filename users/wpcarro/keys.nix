cbcb5163a3e98879483659028947f459268a0774
William Carroll <wpcarro@gmail.com>
2022-09-12 10:02:51 -0700

    feat(wpcarro/ssh): Support iPhone SSH key
    For termius access
    
    Change-Id: Ic91b80bfc8deb816b5a7634ca5e74bb026729067

--- users/wpcarro/keys.nix
+++ users/wpcarro/keys.nix
@@ -4,6 +4,7 @@
 rec {
   ava = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB/5Fuo7wi8rNXVXgNaCK2X6ePCh9LQs/9h7Tj6UeXrl wpcarro@ava";
   diogenes = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILFDRfpNXDxQuTJAqVg8+Mm/hOfE5VAJP+Lpw9kA5cDG wpcarro@gmail.com";
+  iphone = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEU1tsRQ/cMxi9Hd7Xo+YpiWB5i6qx24EJLCEFBK4q4W wpcarro@iphone";
   marcus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJkNQJBXekuSzZJ8+gxT+V1+eXTm3hYsfigllr/ARXkf wpcarro@gmail.com";
   nathan = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP2NjuP722VUgpSu5bVUPTfdVNPO8fSW0Jlas8L4up13 bill@nathan";
   tarasco = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOh+wG4f7tI0IwGyF2sLi5mPlh3JKE7KqV2ab0tlcL36 wpcarro@tarasco";
@@ -11,6 +12,7 @@ rec {
   all = [
     ava
     diogenes
+    iphone
     marcus
     nathan
     tarasco
