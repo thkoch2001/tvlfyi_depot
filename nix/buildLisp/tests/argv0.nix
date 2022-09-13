ca3a9379d5056038687ef5204cf5f5f611c6ece8
sterni <sternenseemann@systemli.org>
2022-09-13 18:33:18 +0200

    refactor(nix/buildLisp/tests/argv0): use derivation for tests
    There is no need to use an extraStep, actually, and using derivations
    reduces noise on CI.
    
    Change-Id: I897c3c3f7e0acee8f051fcc01450ff57176726f8

--- nix/buildLisp/tests/argv0.nix
+++ nix/buildLisp/tests/argv0.nix
@@ -1,36 +1,56 @@
-{ depot, pkgs, ... }:
+{ depot, pkgs, lib, ... }:
 
-depot.nix.buildLisp.program {
-  name = "argv0-test";
+let
+  # Trivial test program that outputs argv[0] and exits
+  prog =
+    depot.nix.buildLisp.program {
+      name = "argv0-test";
 
-  srcs = [
-    (pkgs.writeText "argv0-test.lisp" ''
-      (defpackage :argv0-test (:use :common-lisp :uiop) (:export :main))
-      (in-package :argv0-test)
+      srcs = [
+        (pkgs.writeText "argv0-test.lisp" ''
+          (defpackage :argv0-test (:use :common-lisp :uiop) (:export :main))
+          (in-package :argv0-test)
 
-      (defun main ()
-        (format t "~A~%" (uiop:argv0)))
-    '')
-  ];
+          (defun main ()
+            (format t "~A~%" (uiop:argv0)))
+        '')
+      ];
 
-  deps = [
-    {
-      sbcl = depot.nix.buildLisp.bundled "uiop";
-      default = depot.nix.buildLisp.bundled "asdf";
-    }
-  ];
-
-  passthru.meta.ci = {
-    extraSteps.verify = {
-      label = "verify argv[0] output";
-      needsOutput = true;
-      command = pkgs.writeShellScript "check-argv0" ''
-        set -eux
-
-        for invocation in "$(pwd)/result/bin/argv0-test" "./result/bin/argv0-test"; do
-          test "$invocation" = "$("$invocation")"
-        done
-      '';
+      deps = [
+        {
+          sbcl = depot.nix.buildLisp.bundled "uiop";
+          default = depot.nix.buildLisp.bundled "asdf";
+        }
+      ];
     };
-  };
+
+  # Extract the given implementation's drv (by name) and verify argv[0] output
+  checkImplementation = impl:
+    pkgs.runCommand "check-argv0" { } ''
+      set -eux
+
+      checkInvocation() {
+        invocation="$1"
+        test "$invocation" = "$("$invocation")"
+      }
+
+      checkInvocation "${prog.${impl}}/bin/argv0-test"
+
+      cd ${prog.${impl}}
+      checkInvocation "./bin/argv0-test"
+
+      cd bin
+      checkInvocation ./argv0-test
+
+      set +x
+
+      touch "$out"
+    '';
+
+  inherit (prog.meta.ci) targets;
+in
+
+# Wire up a subtarget for every (active) implementation
+lib.genAttrs targets checkImplementation // {
+  meta.ci = { inherit targets; };
 }
