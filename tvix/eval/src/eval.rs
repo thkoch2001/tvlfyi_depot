a60139550619a832f180a36b5bef816ae334c150
Griffin Smith <root@gws.fyi>
2022-09-18 18:27:31 -0400

    refactor(tvix/eval): Encapsulate compilation inside VM
    In preparation for allowing compiling additional files given only a VM
    (to implement `builtins.import`), encapsulate the construction and
    operation of a `Compiler` entirely within a `VM`. This requires the VM
    itself to own and prepare both the `globals` hash map and the `Observer`
    for the compiler, both of which are passed by-reference when
    constructing the compiler.
    
    Change-Id: Ibe4c7ac4fe7be25bbbfc781ee3f88c5c083eef6d

--- tvix/eval/src/eval.rs
+++ tvix/eval/src/eval.rs
@@ -1,20 +1,18 @@
-use std::{path::PathBuf, rc::Rc};
+use std::path::PathBuf;
 
 use crate::{
     builtins::global_builtins,
-    errors::{Error, ErrorKind, EvalResult},
-    observer::{DisassemblingObserver, NoOpObserver, TracingObserver},
+    errors::EvalResult,
+    observer::{NoOpObserver, TracingObserver},
+    upvalues::Upvalues,
     value::Value,
+    vm::VM,
 };
 
 /// Runtime options for the Tvix interpreter
 #[derive(Debug, Clone, Copy, Default)]
 #[cfg_attr(feature = "repl", derive(clap::Parser))]
 pub struct Options {
-    /// Dump the raw AST to stdout before interpreting
-    #[cfg_attr(feature = "repl", clap(long, env = "TVIX_DISPLAY_AST"))]
-    display_ast: bool,
-
     /// Dump the bytecode to stdout before evaluating
     #[cfg_attr(feature = "repl", clap(long, env = "TVIX_DUMP_BYTECODE"))]
     dump_bytecode: bool,
@@ -25,62 +23,12 @@ pub struct Options {
 }
 
 pub fn interpret(code: &str, location: Option<PathBuf>, options: Options) -> EvalResult<Value> {
-    let mut codemap = codemap::CodeMap::new();
-    let file = codemap.add_file(
-        location
-            .as_ref()
-            .map(|p| p.to_string_lossy().to_string())
-            .unwrap_or_else(|| "[tvix-repl]".into()),
-        code.into(),
-    );
-    let codemap = Rc::new(codemap);
-
-    let parsed = rnix::ast::Root::parse(code);
-    let errors = parsed.errors();
-
-    if !errors.is_empty() {
-        for err in errors {
-            eprintln!("parse error: {}", err);
-        }
-        return Err(Error {
-            kind: ErrorKind::ParseErrors(errors.to_vec()),
-            span: file.span,
-        });
-    }
-
-    // If we've reached this point, there are no errors.
-    let root_expr = parsed
-        .tree()
-        .expr()
-        .expect("expression should exist if no errors occured");
-
-    if options.display_ast {
-        println!("{:?}", root_expr);
-    }
-
-    let result = if options.dump_bytecode {
-        crate::compiler::compile(
-            root_expr,
-            location,
-            file.clone(),
-            global_builtins(),
-            &mut DisassemblingObserver::new(codemap.clone(), std::io::stderr()),
-        )
-    } else {
-        crate::compiler::compile(
-            root_expr,
-            location,
-            file.clone(),
-            global_builtins(),
-            &mut NoOpObserver::default(),
-        )
-    }?;
-
-    for warning in result.warnings {
-        warning.fancy_format_stderr(&codemap);
-    }
+    let mut vm = VM::new(global_builtins(), options);
+    let result = vm.compile(code, location)?;
 
     for error in &result.errors {
+        let file = vm.codemap().find_file(error.span.low());
+
         eprintln!(
             "compiler error: {:?} at `{}`[line {}]",
             error.kind,
@@ -93,12 +41,5 @@ pub fn interpret(code: &str, location: Option<PathBuf>, options: Options) -> Eva
         return Err(err.clone());
     }
 
-    if options.trace_runtime {
-        crate::vm::run_lambda(
-            Box::new(TracingObserver::new(std::io::stderr())),
-            result.lambda,
-        )
-    } else {
-        crate::vm::run_lambda(Box::new(NoOpObserver::default()), result.lambda)
-    }
+    vm.call_toplevel(result.lambda)
 }
