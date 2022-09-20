b7feee70b1baabacad2bfd8d9b2a7a4adddcf46d
William Carroll <wpcarro@gmail.com>
2022-09-20 10:54:24 -0700

    wip: support builtins.baseNameOf
    Work-in-progress because of some of the "strange inputs" unit tests.
    
    Side note: I wish Nix would have stricter validation on the strings for
    functions that operate on both strings and paths.
    
    Change-Id: I21692e4c11033e4d0752975ccccf7ea09d2ba163

---
+++ tvix/eval/src/tests/tvix_tests/eval-okay-builtins-baseNameOf.nix
@@ -0,0 +1,13 @@
+[
+  (builtins.baseNameOf "")
+  (builtins.baseNameOf ./foo.txt)
+  (builtins.baseNameOf "/abs/path/to/foo.txt")
+  (builtins.baseNameOf "./rel/dir/..")
+  (builtins.baseNameOf "/abs/dir/..")
+  # strange inputs
+  (builtins.baseNameOf "foo.txt/")
+  (builtins.baseNameOf "./foo.txt/")
+  (builtins.baseNameOf "/foo.txt/")
+  (builtins.baseNameOf "foo/////")
+  (builtins.baseNameOf "foo/////bar")
+]
