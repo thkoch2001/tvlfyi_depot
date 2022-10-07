97beddcf25e348c6e9686969efc2a377136f1ce9
sterni <sternenseemann@systemli.org>
2022-10-07 18:14:47 +0200

    chore(sterni/keys): add ludwig ssh key
    ancient thinkpad experiment 2
    
    Change-Id: Id8a1e9d70642289235dcdac75a07e35c78689d0b

--- users/sterni/keys.nix
+++ users/sterni/keys.nix
@@ -3,5 +3,6 @@
 {
   all = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJk+KvgvI2oJTppMASNUfMcMkA2G5ZNt+HnWDzaXKLlo lukas@wolfgang"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIZTrefeqOlXDz7nnDWw820+29vLgn6R3o4N1G3lRWrr lukas@ludwig"
   ];
 }
