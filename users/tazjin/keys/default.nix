edddefe961bc7cf43f9e41972bee98ca9abf6c76
Vincent Ambo <mail@tazj.in>
2022-10-12 10:21:32 +0300

    chore(tazjin/keys): update SSH key set
    frog is defunct for now, unihertz phone is replaced with palm (on
    which I don't run SSH), and for b/210 I'd like a non-sk key on zamalek
    
    Change-Id: Ie99e650dcf9deef91a0f63d4171a332a8e596de0

--- users/tazjin/keys/default.nix
+++ users/tazjin/keys/default.nix
@@ -3,9 +3,8 @@
 
 let withAll = keys: keys // { all = builtins.attrValues keys; };
 in withAll {
-  # frog = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKMZzRdcrHTuCPoaFy36MPr5IW/hnImlse/OBOn6udL/ tazjin@frog";
   tverskoy = "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBAWvA3RpXpMAqruUbB+eVgvvHCzhs5R9khFRza3YSLeFiIqOxVVgyhzW/BnCSD9t/5JrqRdJIGQLnkQU9m4REhUAAAAEc3NoOg== tazjin@tverskoy";
   tverskoy_ed25519 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM1fGWz/gsq+ZeZXjvUrV+pBlanw1c3zJ9kLTax9FWQy tazjin@tverskoy";
-  unihertz_sk = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMgLwumRAp7u9sgaFR5aneNbQo07ZXXAFWV45pwzUiP2 unihertz-sk";
   zamalek_sk = "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIOAw3OaPAjnC6hArGYEmBoXhPf7aZdRGlDZcSqm6gbB8AAAABHNzaDo= tazjin@zamalek";
+  zamalek_ed25519 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDBRXeb8EuecLHP0bW4zuebXp4KRnXgJTZfeVWXQ1n1R tazjin@zamalek";
 }
