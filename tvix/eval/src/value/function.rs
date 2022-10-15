342e59c7853d0cf6a6a845093294556998118150
Adam Joseph <adam@westernsemico.com>
2022-10-15 16:10:10 -0700

    feat(tvix/eval): deduplicate overlap between Closure and Thunk
    This commit deduplicates the Thunk-like functionality from of
    Closure and unifies it with Thunk.
    
    Specifically, we now have one and only one way of breaking reference
    cycles in the Value-graph: Thunk.  No other variant contains a
    RefCell.  As a consequence InnerClosure and UpvaluesCarrier
    disappear.
    
    Change-Id: I04131501029772f30e28da8281d864427685097f

--- tvix/eval/src/value/function.rs
+++ tvix/eval/src/value/function.rs
@@ -1,13 +1,7 @@
 //! This module implements the runtime representation of functions.
-use std::{
-    cell::{Ref, RefCell, RefMut},
-    rc::Rc,
-};
+use std::rc::Rc;
 
-use crate::{
-    chunk::Chunk,
-    upvalues::{UpvalueCarrier, Upvalues},
-};
+use crate::{chunk::Chunk, upvalues::Upvalues};
 
 #[derive(Debug, PartialEq)]
 pub struct Lambda {
@@ -31,42 +25,28 @@ impl Lambda {
 }
 
 #[derive(Clone, Debug, PartialEq)]
-pub struct InnerClosure {
+pub struct Closure {
     pub lambda: Rc<Lambda>,
-    upvalues: Upvalues,
+    pub upvalues: Upvalues,
 }
-
-#[repr(transparent)]
-#[derive(Clone, Debug, PartialEq)]
-pub struct Closure(Rc<RefCell<InnerClosure>>);
 
 impl Closure {
     pub fn new(lambda: Rc<Lambda>) -> Self {
-        Closure(Rc::new(RefCell::new(InnerClosure {
+        Closure {
             upvalues: Upvalues::with_capacity(lambda.upvalue_count),
             lambda,
-        })))
+        }
     }
 
-    pub fn chunk(&self) -> Ref<'_, Chunk> {
-        Ref::map(self.0.borrow(), |c| &c.lambda.chunk)
+    pub fn chunk(&self) -> &Chunk {
+        &self.lambda.chunk
     }
 
     pub fn lambda(&self) -> Rc<Lambda> {
-        self.0.borrow().lambda.clone()
-    }
-}
-
-impl UpvalueCarrier for Closure {
-    fn upvalue_count(&self) -> usize {
-        self.0.borrow().lambda.upvalue_count
+        self.lambda.clone()
     }
 
-    fn upvalues(&self) -> Ref<'_, Upvalues> {
-        Ref::map(self.0.borrow(), |c| &c.upvalues)
-    }
-
-    fn upvalues_mut(&self) -> RefMut<'_, Upvalues> {
-        RefMut::map(self.0.borrow_mut(), |c| &mut c.upvalues)
+    pub fn upvalues(&self) -> &Upvalues {
+        &self.upvalues
     }
 }
