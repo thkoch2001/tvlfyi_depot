1c827f65893bd2b343d98648afb0356b3b9fbd5e
sterni <sternenseemann@systemli.org>
2022-10-19 13:30:13 +0200

    fix(tvix/eval): don't thunk home path resolution to prevent catching
    builtins.tryEval shouldn't catch home path resolution errors which it
    currently can in tvix. To prevent this cl/7025 proposed a new flag in
    the PathResolution error variant which described if the error was
    catchable or not. This CL proposes an alternative implementation which
    is simpler, but also a bit more implicit.
    
    tryEval relies on thunking to catch errors, so we can bypass this by
    simply not thunking the home path resolution. This means that we need to
    resolve them unconditionally (and probably in a bit more expensive way
    than in the compiler originally), but save on the extra flag in the
    error variant.
    
    Change-Id: Ie4b9dc68f62c86d6c7fd5f1c9460c850d97ed1ca

--- tvix/eval/src/compiler/mod.rs
+++ tvix/eval/src/compiler/mod.rs
@@ -272,15 +272,15 @@ impl Compiler<'_> {
         let path = if raw_path.starts_with('/') {
             Path::new(&raw_path).to_owned()
         } else if raw_path.starts_with('~') {
-            return self.thunk(slot, node, move |c, _| {
-                // We assume that paths that home paths start with ~/ or fail to parse
-                // TODO: this should be checked using a parse-fail test.
-                debug_assert!(raw_path.len() > 2 && raw_path.starts_with("~/"));
+            // We assume that paths that home paths start with ~/ or fail to parse
+            // TODO: this should be checked using a parse-fail test.
+            debug_assert!(raw_path.len() > 2 && raw_path.starts_with("~/"));
 
-                let home_relative_path = &raw_path[2..(raw_path.len())];
-                c.emit_constant(Value::UnresolvedPath(home_relative_path.into()), node);
-                c.push_op(OpCode::OpResolveHomePath, node);
-            });
+            let home_relative_path = &raw_path[2..(raw_path.len())];
+            self.emit_constant(Value::UnresolvedPath(home_relative_path.into()), node);
+            // This resolution is not thunked so it can't be caught by tryEval
+            self.push_op(OpCode::OpResolveHomePath, node);
+            return;
         } else if raw_path.starts_with('.') {
             let mut buf = self.root_dir.clone();
             buf.push(&raw_path);
