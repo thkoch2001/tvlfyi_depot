4d5fdb781d9893e3dcd5ea030ad24e3fd52b06e7
sterni <sternenseemann@systemli.org>
2022-10-23 02:32:24 +0200

    chore(3p/sources): Bump channels & overlays
    * //users/grfn/system/home/modules:
    
      The MultiMC saga continues with:
    
      > PolyMC has been removed from nixpkgs due to a hostile takeover
      > by a rogue maintainer. The rest of the maintainers have made a
      > fork which is packaged as 'prismlauncher'.
    
    * //third_party/overlays:
    
      Override tdlib to 1.8.7 while waiting for nixpkgs to catch up.
    
    * //users/tazjin/finito:
    
      Disable on CI temporarily as it has been broken by some change
      in nixpkgs, now failing to compile a dependency.
    
    Change-Id: Ide038a8d466bfdc19dc9016beb03ae4817939a2b
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/7066
    Reviewed-by: tazjin <tazjin@tvl.su>
    Reviewed-by: grfn <grfn@gws.fyi>
    Autosubmit: sterni <sternenseemann@systemli.org>
    Tested-by: BuildkiteCI

--- users/tazjin/finito/default.nix
+++ users/tazjin/finito/default.nix
@@ -2,4 +2,8 @@
 
 depot.third_party.naersk.buildPackage {
   src = ./.;
+
+  # Got broken by a rustc update (?)
+  # https://buildkite.com/tvl/depot/builds/17910#01841493-dc42-44f8-b904-32bf3d835485
+  meta.ci.skip = true;
 }
