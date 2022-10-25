55359eae3cb784fea59e7d3de81af0d07077ed0a
Adam Joseph <adam@westernsemico.com>
2022-10-25 02:16:59 -0700

    refactor(tvix/eval): rename Opcode::DataLocalIdx to DataStackIdx
    It is very confusing that this opcode is called DataLocalIdx, but it carries a
    StackIdx rather than a LocalIdx.  It seems like this really ought to be called
    DataStackIdx, but maybe I've misunderstood; if so please explain it to me.
    
    Change-Id: I91f6ffa759412beef0b91d3c19ec0d873fe51b99
    Signed-off-by: Adam Joseph <adam@westernsemico.com>

--- tvix/eval/src/vm.rs
+++ tvix/eval/src/vm.rs
@@ -734,7 +734,7 @@ impl<'o> VM<'o> {
 
             // Data-carrying operands should never be executed,
             // that is a critical error in the VM.
-            OpCode::DataLocalIdx(_)
+            OpCode::DataStackIdx(_)
             | OpCode::DataDeferredLocal(_)
             | OpCode::DataUpvalueIdx(_)
             | OpCode::DataCaptureWith => {
@@ -813,8 +813,8 @@ impl<'o> VM<'o> {
     ) -> EvalResult<()> {
         for _ in 0..count {
             match self.inc_ip() {
-                OpCode::DataLocalIdx(StackIdx(local_idx)) => {
-                    let idx = self.frame().stack_offset + local_idx;
+                OpCode::DataStackIdx(StackIdx(stack_idx)) => {
+                    let idx = self.frame().stack_offset + stack_idx;
 
                     let val = match self.stack.get(idx) {
                         Some(val) => val.clone(),
@@ -823,8 +823,8 @@ impl<'o> VM<'o> {
                                 msg: "upvalue to be captured was missing on stack",
                                 metadata: Some(Rc::new(json!({
                                     "ip": format!("{:#x}", self.frame().ip.0 - 1),
-                                    "local_idx": local_idx,
-                                    "stack_idx": idx,
+                                    "stack_idx(relative)": stack_idx,
+                                    "stack_idx(absolute)": idx,
                                 }))),
                             }))
                         }
