f9352e89a8da634e1f5b8e421380c07e1d8cdc10
Griffin Smith <root@gws.fyi>
2022-11-05 19:50:31 -0400

    feat(tvix/eval): Add proc-macros for defining builtins
    Add a pair of proc macros to a new proc-macro crate,
    `tvix-eval-proc-macros` - one for distributed registry of builtins and
    one for collecting all those registered builtins into a single vec.
    
    This is relatively straightforward as written, but may not
    actually *work* long term, as expansion order of proc-macros is in
    general unspecified - but let's give it a shot!
    
    Change-Id: Ie4afae438914d2af93d15637151a49b4c68aa352

---
+++ tvix/eval/builtin-macros/tests/tests.rs
@@ -0,0 +1,13 @@
+use tvix_eval::{ErrorKind, Value, VM};
+use tvix_eval_builtin_macros::{builtin, builtins};
+
+#[builtin("identity", [true])]
+pub fn builtin_identity(_vm: &mut VM, x: Value) -> Result<Value, ErrorKind> {
+    Ok(x)
+}
+
+#[test]
+fn builtins() {
+    let builtins = builtins!();
+    assert_eq!(builtins.len(), 1);
+}
