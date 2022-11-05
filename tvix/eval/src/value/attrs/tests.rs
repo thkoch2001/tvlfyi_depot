cdb59dc49f8f776b506348cf3edce89437e551ad
Adam Joseph <adam@westernsemico.com>
2022-11-05 13:56:48 -0700

    refactor(tvix/eval): remove lifetime parameter from VM<'o>
    Update, 2022-Nov-06:
    
    It turns out that is possible to retain genericity over the lifetime of the observer, if that is a critical requirement.
    
    The trick is that instead of making the VM generic, you instead make Value generic! Then you can thread any genericity you like (types, lifetimes, etc) through the entire program. And you can even thread that genericity through the rust std traits: impl<'o> PartialEq for Value<'o> { ... }.
    
    In hindsight this is sort of obvious; as a pure functional language, the central object of Nix is its values, not its call stack.
    
    
    **** original commit message below ****
    
    This commit simply changes the type of VM::observer from
    
      &'o mut dyn RuntimeObserver
    
    to
    
      Box<dyn RuntimeObserver>
    
    which allows to drop the lifetime paramter from VM.
    
    Rust's borrow checker requires a bit of trivial+local let-floating
    to keep it happy; the resulting diff-noise is amplified by rustfmt
    which makes the patch look considerably larger than it really is.
    
    Calling code (such as tvix) can use Rc<RefCell<>> instead of lending
    into VM::new().
    
    Signed-off-by: Adam Joseph <adam@westernsemico.com>
    Change-Id: If1e48d2c83e7e762747be4e36f1f256a1f0c04f7

--- tvix/eval/src/value/attrs/tests.rs
+++ tvix/eval/src/value/attrs/tests.rs
@@ -9,16 +9,16 @@ mod nix_eq {
 
     #[proptest(ProptestConfig { cases: 2, ..Default::default() })]
     fn reflexive(x: NixAttrs) {
-        let mut observer = NoOpObserver {};
-        let mut vm = VM::new(Default::default(), &mut observer);
+        let observer = NoOpObserver {};
+        let mut vm = VM::new(Default::default(), Box::new(observer));
 
         assert!(x.nix_eq(&x, &mut vm).unwrap())
     }
 
     #[proptest(ProptestConfig { cases: 2, ..Default::default() })]
     fn symmetric(x: NixAttrs, y: NixAttrs) {
-        let mut observer = NoOpObserver {};
-        let mut vm = VM::new(Default::default(), &mut observer);
+        let observer = NoOpObserver {};
+        let mut vm = VM::new(Default::default(), Box::new(observer));
 
         assert_eq!(
             x.nix_eq(&y, &mut vm).unwrap(),
@@ -28,8 +28,8 @@ mod nix_eq {
 
     #[proptest(ProptestConfig { cases: 2, ..Default::default() })]
     fn transitive(x: NixAttrs, y: NixAttrs, z: NixAttrs) {
-        let mut observer = NoOpObserver {};
-        let mut vm = VM::new(Default::default(), &mut observer);
+        let observer = NoOpObserver {};
+        let mut vm = VM::new(Default::default(), Box::new(observer));
 
         if x.nix_eq(&y, &mut vm).unwrap() && y.nix_eq(&z, &mut vm).unwrap() {
             assert!(x.nix_eq(&z, &mut vm).unwrap())
