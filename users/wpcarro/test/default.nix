aad8d97ef8a8ce9a069dca2751d69f0327a7a645
William Carroll <wpcarro@gmail.com>
2022-11-05 14:03:46 -0700

    wip: test extraSteps.dependsOn
    Change-Id: Iddb82274eb2a58742e1694859b06ea54edcac506

---
+++ users/wpcarro/test/default.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+
+let
+  dummy = pkgs.runCommand "dummy" { } "touch $out";
+in
+dummy.overrideAttrs (_: {
+  meta.ci.extraSteps = {
+    foo = {
+      group = "do thing";
+      steps = [
+        { command = "echo 1"; }
+        { command = "echo 2"; }
+        { command = "echo 3"; }
+      ];
+    };
+    foo = {
+      group = "do other thing";
+      steps = [
+        { command = "echo 4"; }
+        { command = "echo 5"; }
+        { command = "echo 6"; }
+      ];
+    };
+  };
+})
