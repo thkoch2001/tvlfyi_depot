f6929808772ea40dda76ae871429849889b73513
Florian Klink <flokli@flokli.de>
2022-11-06 22:30:55 +0000

    WIP: feat(tvix/nar_bridge): init
    Change-Id: I1dcdab7b267eb0be06ad54a19e07888b39ad30c9

---
+++ tvix/nar_bridge/src/api.rs
@@ -0,0 +1,22 @@
+use prost::Message;
+
+tonic::include_proto!("tvix.proto.v1");
+
+impl Directory {
+    // The size of a directory is the number of all regular and symlink elements,
+    // the number of directory elements, and their size fields.
+    pub fn size(&self) -> u32 {
+        self.files.len() as u32
+            + self.symlinks.len() as u32
+            + self
+                .directories
+                .iter()
+                .fold(0, |acc: u32, e| (acc + 1 + e.size) as u32)
+    }
+
+    pub fn digest(&self) -> Vec<u8> {
+        let mut hasher = blake3::Hasher::new();
+
+        hasher.update(&self.encode_to_vec()).finalize().as_bytes()[..].to_vec()
+    }
+}
