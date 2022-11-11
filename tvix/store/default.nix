d392fccf03fc194f4e2f7d0ea85539d09a6d0b0a
Florian Klink <flokli@flokli.de>
2022-11-11 23:07:48 +0000

    feat(tvix/store): build rust proto files
    This uses prost-build to build the proto files.
    
    Change-Id: Ia99fcfa4a19c741683cf28a45202e1d9211f9131

--- tvix/store/default.nix
+++ tvix/store/default.nix
@@ -1,9 +1,19 @@
 { depot, pkgs, lib, ... }:
 
+let
+  protoRoot = depot.nix.sparseTree depot.path.origSrc [
+    ./protos/castore.proto
+    ./protos/pathinfo.proto
+  ];
+in
 lib.fix (self: depot.third_party.naersk.buildPackage (lib.fix (naerskArgs: {
   src = depot.third_party.gitignoreSource ./.;
   # see https://github.com/nix-community/naersk/issues/169
   root = depot.nix.sparseTree ./. [ ./Cargo.lock ./Cargo.toml ];
+
+  nativeBuildInputs = [ pkgs.protobuf ];
+
+  PROTO_ROOT = protoRoot;
 
   doCheck = true;
 }))
