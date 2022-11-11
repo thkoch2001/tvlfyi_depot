9915e076ce635bad0e4059974ed157598f72d2a9
Florian Klink <flokli@flokli.de>
2022-11-11 22:48:24 +0000

    feat(tvix/store): init
    Change-Id: Id846647fa456ff8b187ed4f16d035ce030f11d25

---
+++ tvix/store/default.nix
@@ -0,0 +1,12 @@
+{ depot, pkgs, lib, ... }:
+
+lib.fix (self: depot.third_party.naersk.buildPackage (lib.fix (naerskArgs: {
+  src = depot.third_party.gitignoreSource ./.;
+  # see https://github.com/nix-community/naersk/issues/169
+  root = depot.nix.sparseTree ./. [ ./Cargo.lock ./Cargo.toml ];
+
+  doCheck = true;
+
+  meta.ci.targets = builtins.attrNames self.passthru;
+}))
+)
