95bde47e3cb9fb00b6c36fecb2d25a3046f7e46c
sterni <sternenseemann@systemli.org>
2022-11-18 15:01:41 +0100

    chore(3p/sources): Bump channels & overlays
    * //users/tazjin/emacs: string-edit was renamed to string-edit-at-point
      https://github.com/melpa/melpa/commit/1b2c64ce1b4ce5d94dfefff25fc15537bd427ea4
    
    * //users/grfn/…/modules:
    
        - readyset.nix: use rain from nixpkgs
    
        - obs.nix: use input-overlay from nixpkgs (newer version, untested)
    
    Change-Id: I5887078cee22673c129d186a8448855b92d7b250

--- users/grfn/system/home/modules/development/readyset.nix
+++ users/grfn/system/home/modules/development/readyset.nix
@@ -10,21 +10,7 @@
     (writeShellScriptBin "dotclip" "xclip -out -selection clipboard | dot -Tpng | feh -")
     (writeShellScriptBin "dotcontroller" "curl -s localhost:6033/graph | dot -Tpng | feh -")
 
-    # TODO(grfn): rain fails to build with Go >=1.18, investigate.
-    (buildGo117Module rec {
-      pname = "rain";
-      version = "1.2.0";
-
-      src = fetchFromGitHub {
-        owner = "aws-cloudformation";
-        repo = pname;
-        rev = "v${version}";
-        sha256 = "168gkchshl5f1awqi1cgvdkm6q707702rnn0v4i5djqxmg5rk0p9";
-      };
-
-      vendorHash = "sha256:16bx7cjh5cq9zlis8lf28i016avgqf3j9fmcvkqzd8db2vxpqx3v";
-    })
-
+    rain
     awscli2
     amazon-ecr-credential-helper
   ];
