c7588501a4e5baadfd05ee449a0b7b3b16b4b790
sterni <sternenseemann@systemli.org>
2022-11-18 15:01:41 +0100

    chore(3p/sources): Bump channels & overlays
    * //users/tazjin/emacs: string-edit was renamed to string-edit-at-point
      https://github.com/melpa/melpa/commit/1b2c64ce1b4ce5d94dfefff25fc15537bd427ea4
    
    * //users/grfn/…/modules:
    
        - readyset.nix: use rain from nixpkgs
    
        - obs.nix: use input-overlay from nixpkgs (newer version, untested)
    
    Change-Id: I5887078cee22673c129d186a8448855b92d7b250

--- users/grfn/system/home/modules/obs.nix
+++ users/grfn/system/home/modules/obs.nix
@@ -1,60 +1,10 @@
 { config, lib, pkgs, ... }:
 
-with pkgs;
-
 let
-  libuiohook = stdenv.mkDerivation rec {
-    pname = "libuiohook";
-    version = "1.1";
-    src = fetchFromGitHub {
-      owner = "kwhat";
-      repo = "libuiohook";
-      rev = version;
-      sha256 = "1isfxn3cfrdqq22d3mlz2lzm4asf9gprs7ww2xy9c3j3srk9kd7r";
-    };
-
-    preConfigure = ''
-      ./bootstrap.sh
-    '';
-
-    nativeBuildInputs = [ pkg-config ];
-    buildInputs = [
-      libtool
-      autoconf
-      automake
-      xlibsWrapper
-      xorg.libXtst
-      xorg.libXinerama
-      xorg.libxkbfile
-      libxkbcommon
-    ];
-  };
-
-  obs-input-overlay = stdenv.mkDerivation rec {
-    pname = "obs-input-overlay";
-    version = "4.8";
-    src = fetchFromGitHub {
-      owner = "univrsal";
-      repo = "input-overlay";
-      rev = "v${version}";
-      sha256 = "1dklg0dx9ijwyhgwcaqz859rbpaivmqxqvh9w3h4byrh5pnkz8bf";
-      fetchSubmodules = true;
-    };
-
-    nativeBuildInputs = [ cmake ];
-    buildInputs = [ obs-studio libuiohook ];
-
-    postPatch = ''
-      sed -i CMakeLists.txt \
-        -e '2iinclude(${obs-studio.src}/cmake/Modules/ObsHelpers.cmake)' \
-        -e '2ifind_package(LibObs REQUIRED)'
-    '';
-
-    cmakeFlags = [
-      "-Wno-dev"
-    ];
-  };
+  inherit (pkgs) obs-studio;
+  obs-input-overlay = pkgs.obs-studio-plugins.input-overlay;
 in
+
 {
   home.packages = [
     obs-studio
