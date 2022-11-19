d10fd4f0680d43475ca081c7ae113a8d812e94de
Florian Klink <flokli@flokli.de>
2022-11-19 23:21:13 +0000

    feat(tvix/store/protos): implement Size() and Digest() for Directory
    Change-Id: If71445a9bb26100bb4076ac4f5c96945b33919f9

---
+++ tvix/store/protos/castore.go
@@ -0,0 +1,37 @@
+package storev1
+
+import (
+	"fmt"
+
+	"google.golang.org/protobuf/proto"
+	"lukechampine.com/blake3"
+)
+
+// The size of a directory is the number of all regular and symlink elements,
+// the number of directory elements, and their size fields.
+func (d *Directory) Size() {
+	var size uint32
+	size = uint32(len(d.Files) + len(d.Symlinks))
+	for _, d := range d.Directories {
+		size += 1 + d.Size
+	}
+}
+
+func (d *Directory) Digest() ([]byte, error) {
+	b, err := proto.MarshalOptions{
+		Deterministic: true,
+	}.Marshal(d)
+
+	if err != nil {
+		return nil, fmt.Errorf("error while marshalling directory: %w", err)
+	}
+
+	h := blake3.New(0, nil)
+
+	_, err = h.Write(b)
+	if err != nil {
+		return nil, fmt.Errorf("error writing to hasher: %w", err)
+	}
+
+	return h.Sum(nil), nil
+}
