7461a38634e34e7e2b77f5d1df303e0f6d4101c8
William Carroll <wpcarro@gmail.com>
2022-11-18 18:08:47 -0800

    feat(wpcarro/tools): Support systemd-shell
    Drop into a new shell environment with the same variables defined in a systemd
    unit file (for debugging purposes).
    
    Change-Id: Iaf513809b524f3f3e845b512450da71694bb7c7f

---
+++ users/wpcarro/tools/systemd-shell/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="systemd-shell",
+    version="0.0.1",
+    author="William Carroll",
+    author_email="wpcarro@gmail.com",
+    scripts=["systemd-shell"],
+)
