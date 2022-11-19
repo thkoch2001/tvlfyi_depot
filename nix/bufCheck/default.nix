d9379f8f74a26af9b76438a9d0e883f357a82af6
Florian Klink <flokli@flokli.de>
2022-11-19 21:14:22 +0000

    feat(nix/bufCheck): ensure proto files in //tvix/store/protos are up to date
    Change-Id: I03e1c3aced266966f6959dbce05c1f1fb1480b5a

--- nix/bufCheck/default.nix
+++ nix/bufCheck/default.nix
@@ -3,7 +3,20 @@
 { depot, pkgs, ... }:
 
 pkgs.writeShellScriptBin "ci-buf-check" ''
-  ${depot.third_party.nixpkgs.buf}/bin/buf lint .
+  export PATH="$PATH:${pkgs.lib.makeBinPath [ pkgs.buf pkgs.protoc-gen-go pkgs.protoc-gen-go-grpc ]}"
+  buf lint .
+
+  # Run buf generate, and bail out if generated files are changed.
+  buf generate --path tvix/store/protos
+  # Check if any files have changed
+  if [[ -n "$(git status --porcelain -unormal)" ]]; then
+      echo "-----------------------------"
+      echo ".pb.go files need to be updated"
+      echo "-----------------------------"
+      git status -unormal
+      exit 1
+  fi
+
   # Report-only
-  ${depot.third_party.nixpkgs.buf}/bin/buf breaking . --against "./.git#ref=HEAD~1" || true
+  buf breaking . --against "./.git#ref=HEAD~1" || true
 ''
