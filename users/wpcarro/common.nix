28b55c470853e4088e428b4434c06230a57b4dd1
William Carroll <wpcarro@gmail.com>
2022-11-19 10:20:12 -0800

    fix(wpcarro/emacs): Support OSX build of Emacs
    **TL;DR:**
    Most of these changes predicate behavior on the platform:
    - At buildtime this is `builtins.currentSystem == $something`.
    - At runtime this is `(memq window-system '(ns))`.
    
    **Background:**
    I need to support a bunch of OSX users at $WORK. As such, I'm planning
    on using my MBP for the next few weeks to build empathy for our
    userbase and polish some currently rough edges.
    
    If I'm going to get an serious work done, I need my Emacs setup. Step
    one is making sure it can build and run.
    
    Change-Id: I918efccfa5f149e218aeea476c2c7df1c7b64ae8

--- users/wpcarro/common.nix
+++ users/wpcarro/common.nix
@@ -44,7 +44,6 @@ in
   # machines.
   shell-utils = with pkgs; [
     bat
-    dig
     direnv
     diskus
     emacs
@@ -55,13 +54,11 @@ in
     gnupg
     htop
     jq
-    mkpasswd
     nmap
     pass
     python3
     rink
     ripgrep
-    sysz
     tldr
     tokei
     tree
@@ -71,5 +68,14 @@ in
     # wpcarro.tools.simple_vim
     xclip
     zip
-  ];
+  ] ++
+  (if builtins.currentSystem == "x86-64_linux" then [
+    mkpasswd
+    sysz
+    # This depends on compiler-rt-libc-10.0.1, which is marked as broken on
+    # aarch64-darwin, but depot sets `allowBroken = true`, which means any
+    # build that depends on dig will fail on OSX (e.g. emacs).
+    # https://cs.tvl.fyi/github.com/NixOS/nixpkgs@e9b195248c6cd7961a453b10294aea9ab58e01b4/-/blob/pkgs/development/compilers/llvm/10/compiler-rt/default.nix?L122
+    dig
+  ] else [ ]);
 }
