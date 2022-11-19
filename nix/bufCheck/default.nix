771c95a7d75a70c6e1b144655238ca86d41b0861
Florian Klink <flokli@flokli.de>
2022-11-19 21:24:42 +0000

    feat(nix/bufCheck): always run from repo root
    This doesn't work when run from subdirectories, and going there manually before
    running `mg run //nix/bufCheck` is annoying.
    
    Change-Id: Icd30a7596ff0dfe2781f7cfa1b4085cbfdebd6ac

--- nix/bufCheck/default.nix
+++ nix/bufCheck/default.nix
@@ -4,10 +4,10 @@
 
 pkgs.writeShellScriptBin "ci-buf-check" ''
   export PATH="$PATH:${pkgs.lib.makeBinPath [ pkgs.buf pkgs.protoc-gen-go pkgs.protoc-gen-go-grpc ]}"
-  buf lint .
+  (cd $(git rev-parse --show-toplevel) && buf lint .)
 
   # Run buf generate, and bail out if generated files are changed.
-  buf generate --path tvix/store/protos
+  (cd $(git rev-parse --show-toplevel) && buf generate --path tvix/store/protos)
   # Check if any files have changed
   if [[ -n "$(git status --porcelain -unormal)" ]]; then
       echo "-----------------------------"
@@ -18,5 +18,5 @@ pkgs.writeShellScriptBin "ci-buf-check" ''
   fi
 
   # Report-only
-  buf breaking . --against "./.git#ref=HEAD~1" || true
+  (cd $(git rev-parse --show-toplevel) && (buf breaking . --against "./.git#ref=HEAD~1" || true))
 ''
