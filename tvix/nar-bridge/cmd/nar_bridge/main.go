eb10a0fe4f712f473648da1e01b47d6af4de7c4c
Florian Klink <flokli@flokli.de>
2022-11-19 20:34:49 +0000

    feat(tvix/nar-bridge): init
    Change-Id: Iadba5ec4b96eb8c3367d4b4a2cfa537b8a14a700

---
+++ tvix/nar-bridge/cmd/nar_bridge/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"code.tvl.fyi/tvix/nar-bridge/pkg/server"
+	storev1pb "code.tvl.fyi/tvix/store/protos"
+	"github.com/alecthomas/kong"
+	log "github.com/sirupsen/logrus"
+)
+
+var CLI struct {
+	ListenAddr      string `name:"listen-addr" help:"The address this service listens on" type:"string" default:"[::]:9000"` //nolint:lll
+	EnableAccessLog bool   `name:"access-log" help:"Enable access logging" type:"bool" default:"true" negatable:""`          //nolint:lll
+	StoreAddr       string `name:"store-addr" help:"The address to the tvix-store RPC interface this will connect to"`
+}
+
+// `help:"Expose a tvix-store RPC interface as NAR/NARInfo"`
+
+func main() {
+	retcode := 0
+
+	defer func() { os.Exit(retcode) }()
+
+	_ = kong.Parse(&CLI)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	go func() {
+		for range c {
+			log.Info("Received Signal, shutting down…")
+			//s.Close()
+			os.Exit(1)
+		}
+	}()
+
+	// connect to tvix-store
+	log.Debugf("Dialing to %v", CLI.StoreAddr)
+	conn, err := grpc.Dial(CLI.StoreAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	log.Printf("Starting nar-bridge at %v", CLI.ListenAddr)
+	s := server.New(
+		storev1pb.NewDirectoryServiceClient(conn),
+		storev1pb.NewBlobServiceClient(conn),
+		storev1pb.NewPathInfoServiceClient(conn),
+		CLI.EnableAccessLog,
+		30,
+	)
+
+	err = s.ListenAndServe(CLI.ListenAddr)
+	if err != nil {
+		log.Error("Server failed: %w", err)
+	}
+}
