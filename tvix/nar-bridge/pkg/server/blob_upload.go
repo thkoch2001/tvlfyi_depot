0bf30b2144549135d5cd27aa9d7972d2411342c4
Florian Klink <flokli@flokli.de>
2022-11-22 16:42:21 +0000

    feast(tvix/nar-bridge): factor out callbacks into separate file
    This will make it usable from other importers that invoke reader.Import,
    outside an HTTP handler.
    
    Change-Id: Idb2830e3059ae044f4b1d627baf9fd2ce6e2accf

---
+++ tvix/nar-bridge/pkg/server/blob_upload.go
@@ -0,0 +1,35 @@
+package server
+
+import (
+	storev1pb "code.tvl.fyi/tvix/store/protos"
+	"context"
+	"encoding/hex"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"io"
+)
+
+// this returns a callback function that can be used as fileCb
+// for the reader.Import function call
+func genBlobServiceWriteCb(ctx context.Context, blobServiceClient storev1pb.BlobServiceClient) func(io.Reader) error {
+	return func(fileReader io.Reader) error {
+		// Read from fileReader into a buffer.
+		// We currently buffer all contents and send them to blobServiceClient at once,
+		// but that's about to change.
+		contents, err := io.ReadAll(fileReader)
+		resp, err := blobServiceClient.Put(
+			ctx,
+			&storev1pb.PutBlobRequest{
+				Data: contents,
+			},
+		)
+		if err != nil {
+			// return error to the importer
+			return fmt.Errorf("error from blob service: %w", err)
+		}
+
+		log.WithField("digest", hex.EncodeToString(resp.GetDigest())).Info("uploaded blob")
+
+		return nil
+	}
+}
