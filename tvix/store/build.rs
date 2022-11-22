afe160ab164508fdb64da6f77958c72c6ab74fec
Adam Joseph <adam@westernsemico.com>
2022-11-22 15:58:01 -0800

    chore(tvix/store): avoid "../.." in build.rs, which may not work
    This is an alternative approach to addressing one of the problems
    fixed by cl/7320.  See discussion here:
    
      https://cl.tvl.fyi/c/depot/+/7320/comment/610787c2_c57dc6fc/
    
    Change-Id: I170cc7a56eef0cf009417ceda5d7fe0f479a4b86

--- tvix/store/build.rs
+++ tvix/store/build.rs
@@ -6,18 +6,18 @@ fn main() -> Result<()> {
         .build_client(true)
         .compile(
             &[
-                "tvix/store/protos/castore.proto",
-                "tvix/store/protos/pathinfo.proto",
-                "tvix/store/protos/rpc_blobstore.proto",
-                "tvix/store/protos/rpc_directory.proto",
-                "tvix/store/protos/rpc_pathinfo.proto",
+                "protos/castore.proto",
+                "protos/pathinfo.proto",
+                "protos/rpc_blobstore.proto",
+                "protos/rpc_directory.proto",
+                "protos/rpc_pathinfo.proto",
             ],
             // If we are in running `cargo build` manually, using `../..` works fine,
             // but in case we run inside a nix build, we need to instead point PROTO_ROOT
             // to a sparseTree containing that structure.
             &[match std::env::var_os(&"PROTO_ROOT") {
                 Some(proto_root) => proto_root.to_str().unwrap().to_owned(),
-                None => "../..".to_string(),
+                None => ".".to_string(),
             }],
         )?;
     Ok(())
