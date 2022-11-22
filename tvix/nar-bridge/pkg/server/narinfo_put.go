ed76dea33ea69884f8905491244a7f11f30c636b
Florian Klink <flokli@flokli.de>
2022-11-22 15:26:12 +0000

    feat(tvix/nar-bridge): handle PUT /*.narinfo
    … and temporarily switch go-nix to a custom branch until
    https://github.com/nix-community/go-nix/pull/97 is merged.
    
    Change-Id: I6f6a16dad717d5ae5e3c32b626ad233894819ef9

---
+++ tvix/nar-bridge/pkg/server/narinfo_put.go
@@ -0,0 +1,163 @@
+package server
+
+import (
+	"encoding/hex"
+	"net/http"
+
+	storev1pb "code.tvl.fyi/tvix/store/protos"
+	"github.com/go-chi/chi/v5"
+	mh "github.com/multiformats/go-multihash/core"
+	"github.com/nix-community/go-nix/pkg/narinfo"
+	"github.com/nix-community/go-nix/pkg/nixbase32"
+	"github.com/nix-community/go-nix/pkg/nixpath"
+	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
+)
+
+func registerNarinfoPut(s *Server) {
+	s.handler.Put("/{outputhash:^["+nixbase32.Alphabet+"]{32}}.narinfo", func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+
+		ctx := r.Context()
+		log := log.WithField("outputhash", chi.URLParamFromCtx(ctx, "outputhash"))
+
+		// parse the output hash sent in the request URL
+		outputHash, err := nixbase32.DecodeString(chi.URLParamFromCtx(ctx, "outputhash"))
+		if err != nil {
+			log.WithError(err).Error("unable to decode output hash from url")
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("unable to decode output hash from url"))
+
+			return
+		}
+
+		// TODO: decide on merging behaviour.
+		// Maybe it's fine to add if contents are the same, but more sigs can be added?
+		// Right now, just fail if we try to upload a .narinfo for a path that already exists?
+
+		_, err = s.pathInfoServiceClient.Get(ctx, &storev1pb.GetPathInfoRequest{
+			ByWhat: &storev1pb.GetPathInfoRequest_ByOutputHash{
+				ByOutputHash: outputHash,
+			},
+		})
+		if err != nil {
+			// TODO: how does a 404 look like?
+			log.WithError(err).Error("Unable to get pathinfo from store")
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Unable to get pathinfo from store"))
+		}
+
+		// read and parse the .narinfo file
+		narInfo, err := narinfo.Parse(r.Body)
+		if err != nil {
+			log.WithError(err).Error("unable to parse narinfo")
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("unable to parse narinfo"))
+
+			return
+		}
+
+		log = log.WithFields(logrus.Fields{
+			"narhash":     narInfo.NarHash.NixString(),
+			"output_path": narInfo.StorePath,
+		})
+
+		var pathInfo *storev1pb.PathInfo
+
+		// look up the narHash in our temporary map(s) - sha256
+		if narInfo.NarHash.HashType == mh.SHA2_256 {
+			foundPathInfo, found := s.narHashSha256ToPathInfo[hex.EncodeToString(narInfo.NarHash.Digest())]
+			if found {
+				pathInfo = foundPathInfo
+			}
+		} else if narInfo.NarHash.HashType == mh.SHA2_512 {
+			foundPathInfo, found := s.narHashSha512ToPathInfo[hex.EncodeToString(narInfo.NarHash.Digest())]
+			if found {
+				pathInfo = foundPathInfo
+			}
+		}
+
+		if pathInfo == nil {
+			log.Error("unable to find referred NAR")
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("unable to find referred NAR"))
+
+			return
+		}
+
+		// compare fields with what we computed while receiving the NAR file
+
+		// NarSize needs to match
+		if pathInfo.Narinfo.NarSize != uint32(narInfo.NarSize) {
+			log.Error("narsize mismatch")
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("unable to parse narinfo"))
+
+			return
+		}
+		// We know the narhash in the .narinfo matches one of the two narhashes in the partial pathInfo,
+		// because that's how we found it.
+
+		// FUTUREWORK: We can't compare References yet, but it'd be a good idea to
+		// do reference checking on .nar files server-side during upload.
+		// We however still need to be parse them, because we store
+		// the bytes in pathInfo.References, and the full strings in pathInfo.Narinfo.ReferenceNames.
+		referencesBytes := make([][]byte, len(narInfo.References))
+		for _, reference := range narInfo.References {
+			np, err := nixpath.FromString(reference)
+			if err != nil {
+				log.WithField("reference", reference).WithError(err).Error("unable to parse reference")
+			}
+			referencesBytes = append(referencesBytes, np.Digest)
+		}
+
+		// assemble the []*storev1pb.NARInfo_Signature{} from narinfo.Signatures.
+		pbNarinfoSignatures := make([]*storev1pb.NARInfo_Signature, len(narInfo.Signatures))
+		for _, narinfoSig := range narInfo.Signatures {
+			// narinfoSig.KeyName
+			// // TODO: improve narinfo signature parser?
+			// narinfoSigSegments := strings.Split(narinfoSig.String(), ":")
+			// if len(narinfoSigSegments) != 2 {
+			// 	log.WithField("narinfo_signature", narinfoSig.String()).Error("unable to parse narinfo signature")
+			// 	w.WriteHeader(http.StatusBadRequest)
+			// 	log.WithError(err).Error("unable to parse narinfo signature")
+			// }
+
+			//
+
+			pbNarinfoSignatures = append(pbNarinfoSignatures, &storev1pb.NARInfo_Signature{
+				Name: narinfoSig.Name,
+				Data: narinfoSig.Data,
+			})
+		}
+
+		// If everything matches, We will add References, NAR signatures and the
+		// output path name, and then upload to the pathinfo service.
+		// We want a copy here, because we don't want to mutate the contents in the lookup table
+		// until we get things back from the remote store.
+		pathInfoToUpload := &storev1pb.PathInfo{
+			Node:       nil,
+			References: referencesBytes,
+			Narinfo: &storev1pb.NARInfo{
+				NarSize:   pathInfo.Narinfo.NarSize,
+				NarHashes: pathInfo.Narinfo.NarHashes,
+				// TODO: convert signatures
+				Signatures:     []*storev1pb.NARInfo_Signature{},
+				ReferenceNames: narInfo.References,
+			},
+		}
+
+		receivedPathInfo, err := s.pathInfoServiceClient.Put(ctx, pathInfoToUpload)
+		if err != nil {
+			log.Error("unable to upload pathinfo to service")
+			w.WriteHeader(http.StatusInternalServerError)
+			log.WithError(err).Error("unable to upload pathinfo to service")
+
+			return
+		}
+
+		log.Infof("received new pathInfo: %v+", receivedPathInfo)
+
+		// TODO: update the local temporary pathinfo with this?
+	})
+}
