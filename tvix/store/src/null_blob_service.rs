50f7ec3b45ce1777f077afdf63aa8c30b98897dd
Márton Boros <martonboros@gmail.com>
2022-11-23 18:13:47 +0200

    feat(tvix/store): add FileBlobService
    Change-Id: Ic82edbe2d34c5e03df315bbeb559663690635008

---
+++ tvix/store/src/null_blob_service.rs
@@ -0,0 +1,51 @@
+use tonic::{Request, Response, Result, Status};
+
+pub mod proto {
+    tonic::include_proto!("tvix.store.v1");
+}
+
+use crate::proto::blob_service_server::BlobService;
+use crate::proto::{GetBlobRequest, GetBlobResponse, PutBlobRequest, PutBlobResponse};
+
+#[derive(Debug, Default)]
+pub struct NullBlobService {}
+
+#[tonic::async_trait]
+/// BlobService that always returns error
+impl BlobService for NullBlobService {
+    async fn get(&self, _request: Request<GetBlobRequest>) -> Result<Response<GetBlobResponse>> {
+        Err(Status::unknown("error"))
+    }
+    async fn put(&self, _request: Request<PutBlobRequest>) -> Result<Response<PutBlobResponse>> {
+        Err(Status::unknown("error"))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::null_blob_service::NullBlobService;
+    use crate::proto::blob_service_server::BlobService;
+    use crate::proto::{GetBlobRequest, PutBlobRequest};
+
+    #[tokio::test]
+    async fn test_err() {
+        let test_data = "Hello, World!".as_bytes().to_vec();
+        let service = NullBlobService::default();
+
+        let result = service
+            .put(tonic::Request::new(PutBlobRequest {
+                data: test_data.clone(),
+            }))
+            .await;
+
+        assert!(result.is_err());
+
+        let result = service
+            .get(tonic::Request::new(GetBlobRequest {
+                digest: blake3::hash(b"Hello").as_bytes().to_vec(),
+            }))
+            .await;
+
+        assert!(result.is_err());
+    }
+}
