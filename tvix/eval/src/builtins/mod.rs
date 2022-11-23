f313803993e83b945297f5387731932fb44a2840
Adam Joseph <adam@westernsemico.com>
2022-11-23 04:28:11 -0800

    feat(tvix/eval): builtins.derivation: mock `out` attribute
    gross but whatever
    
    Signed-off-by: Adam Joseph <adam@westernsemico.com>
    Change-Id: I2f959a4a3bcb34f8b08fcda93173888f8c64320c

--- tvix/eval/src/builtins/mod.rs
+++ tvix/eval/src/builtins/mod.rs
@@ -996,18 +996,50 @@ fn placeholders() -> Vec<Builtin> {
                 //
                 // Crucially this means we do not yet *validate* the values either.
                 let attrs = unwrap_or_clone_rc(args[0].to_attrs()?);
+                let attrs = attrs.update(NixAttrs::from_map(BTreeMap::from([(
+                    "allowedRequisites".into(),
+                    Value::Null,
+                )])));
+                let attrs_orig = attrs.clone();
                 let attrs = attrs.update(NixAttrs::from_map(BTreeMap::from([
                     (
                         "outPath".into(),
                         "/nix/store/00000000000000000000000000000000-mock".into(),
                     ),
                     (
+                        "all".into(),
+                        "/nix/store/00000000000000000000000000000000-mock".into(),
+                    ),
+                    ("outputName".into(), "out".into()),
+                    (
                         "drvPath".into(),
                         "/nix/store/00000000000000000000000000000000-mock.drv".into(),
                     ),
                     ("type".into(), "derivation".into()),
                 ])));
-
+                let outputs = attrs
+                    .select("outputs")
+                    .unwrap_or(&Value::from(vec![Value::from("out")]))
+                    .clone()
+                    .force(vm)?
+                    .to_list()?
+                    .clone();
+                let attrs0 = attrs.clone();
+                let attrs = attrs.update(NixAttrs::from_map(
+                    outputs
+                        .iter()
+                        .map(|output| {
+                            (
+                                output.to_str().unwrap(),
+                                Value::Attrs(Rc::new(attrs0.clone())),
+                            )
+                        })
+                        .collect(),
+                ));
+                let attrs = attrs.update(NixAttrs::from_map(BTreeMap::from([(
+                    "drvAttrs".into(),
+                    Value::Attrs(Rc::new(attrs_orig)),
+                )])));
                 Ok(Value::Attrs(Rc::new(attrs)))
             },
         ),
