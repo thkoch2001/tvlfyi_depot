da94328358e7dce1c801398b628daed8a17e3287
Márton Boros <martonboros@gmail.com>
2022-11-23 18:13:47 +0200

    feat(tvix/store): add FileBlobService
    Change-Id: Ic82edbe2d34c5e03df315bbeb559663690635008

--- tvix/store/src/main.rs
+++ tvix/store/src/main.rs
@@ -1,5 +1,39 @@
+use clap::Parser;
+use std::path::PathBuf;
+use tonic::{transport::Server, Result};
+
+use crate::proto::blob_service_server::BlobServiceServer;
+
+mod file_blob_service;
+mod null_blob_service;
 mod proto;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(long)]
+    store_path: Option<PathBuf>,
+
+    #[arg()]
+    listen_address: Option<String>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = Cli::parse();
+    let listen_address = cli
+        .listen_address
+        .unwrap_or("[::]:8000".to_string())
+        .parse()
+        .unwrap();
+    let service = crate::file_blob_service::FileBlobService {
+        store_path: cli.store_path.unwrap_or(PathBuf::from("tvix-store")),
+    };
+    println!("BlobService listening on {}", listen_address);
+
+    Server::builder()
+        .add_service(BlobServiceServer::new(service))
+        .serve(listen_address)
+        .await?;
+    Ok(())
 }
