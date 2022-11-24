f6202a11d9835087c638b0f4e7c327937cc70ec6
Márton Boros <martonboros@gmail.com>
2022-11-24 14:23:37 +0200

    feat(tvix/store): add null_directory_service
    Change-Id: I7ba5ddb6bd4b6ecd766f144614206cbde610a752

--- tvix/store/src/main.rs
+++ tvix/store/src/main.rs
@@ -3,9 +3,11 @@ use std::path::PathBuf;
 use tonic::{transport::Server, Result};
 
 use crate::proto::blob_service_server::BlobServiceServer;
+use crate::proto::directory_service_server::DirectoryServiceServer;
 
 mod file_blob_service;
 mod null_blob_service;
+mod null_directory_service;
 mod proto;
 
 #[derive(Parser)]
@@ -26,13 +28,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .unwrap_or("[::]:8000".to_string())
         .parse()
         .unwrap();
-    let service = crate::file_blob_service::FileBlobService {
-        store_path: cli.store_path.unwrap_or(PathBuf::from("tvix-store")),
+    let blob_service = crate::file_blob_service::FileBlobService {
+        store_path: cli
+            .store_path
+            .unwrap_or(PathBuf::from("tvix-store"))
+            .join("blob"),
     };
-    println!("BlobService listening on {}", listen_address);
+    let directory_service = crate::null_directory_service::NullDirectoryService {};
 
+    println!("tvix-store listening on {}", listen_address);
     Server::builder()
-        .add_service(BlobServiceServer::new(service))
+        .add_service(BlobServiceServer::new(blob_service))
+        .add_service(DirectoryServiceServer::new(directory_service))
         .serve(listen_address)
         .await?;
     Ok(())
