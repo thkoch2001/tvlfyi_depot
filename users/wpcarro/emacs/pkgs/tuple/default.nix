cc054ce49a86102a53dae71c8f2f51ed4515fcd8
William Carroll <wpcarro@gmail.com>
2022-11-24 22:19:02 -0800

    feat(wpcarro/emacs): Package tuple.el
    Should delete this pkg, but bytes.el depends on it, so let's first package it,
    and then we can delete it once CI passes/failures are more reliable.
    
    Change-Id: Ifa939264c44adf302085a19790cf25225cd3cb5e

---
+++ users/wpcarro/emacs/pkgs/tuple/default.nix
@@ -0,0 +1,10 @@
+{ pkgs, depot, ... }:
+
+pkgs.callPackage
+  ({ emacsPackages }:
+    emacsPackages.trivialBuild {
+      pname = "tuple";
+      version = "1.0.0";
+      src = ./tuple.el;
+    })
+  { }
