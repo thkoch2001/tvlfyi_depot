40eca908574a20caaaa61de6ea63db294dc85ab3
William Carroll <wpcarro@gmail.com>
2022-11-24 22:26:57 -0800

    feat(wpcarro/emacs): Package vector.el
    Paying-off more packaging debt...
    
    Change-Id: Ide641229d6c8efe70c0fd6c625d07aa1a3be98a0
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/7395
    Tested-by: BuildkiteCI
    Reviewed-by: wpcarro <wpcarro@gmail.com>

--- users/wpcarro/emacs/pkgs/vector/vector.el
+++ users/wpcarro/emacs/pkgs/vector/vector.el
@@ -22,24 +22,13 @@
 
 ;;; Code:
 
-;; TODO: Consider supporting an alias named tuple for vector.
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Library
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defconst vector-enable-tests? t
-  "When t, run the tests defined herein.")
-
-;; TODO: Consider labelling variadic functions like `vector-concat*'
-;; vs. `vector-concat'.
 (defun vector-concat (&rest args)
   "Return a new vector composed of all vectors in `ARGS'."
   (apply #'vconcat args))
-
-;; TODO: Here's a sketch of a protocol macro being consumed.
-;; (definstance monoid vector
-;;   :empty (lambda () []))
 
 (defun vector-prepend (x xs)
   "Add `X' to the beginning of `XS'."
@@ -64,21 +53,6 @@ Returns a copy of `XS' with the updates."
   "Set index `I' to value `V' in `XS'.
 This function mutates XS."
   (aset xs i v))
-
-(when vector-enable-tests?
-  (let ((xs [1 2 3])
-        (ys [1 2 3]))
-    (prelude-assert (= 1 (vector-get 0 ys)))
-    (vector-set 0 4 ys)
-    (prelude-assert (= 1 (vector-get 0 ys)))
-    (prelude-assert (= 1 (vector-get 0 xs)))
-    (vector-set! 0 4 xs)
-    (prelude-assert (= 4 (vector-get 0 xs)))))
-
-;; TODO: Decide between "remove" and "delete" as the appropriate verbs.
-;; TODO: Implement this.
-;; (defun vector/delete (i xs)
-;;   "Remove the element at `I' in `XS'.")
 
 (provide 'vector)
 ;;; vector.el ends here
