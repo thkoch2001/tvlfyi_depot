7ca560dd9d873d527b2f6be5f8de66a395d0bff7
Florian Klink <flokli@flokli.de>
2022-11-30 14:49:57 +0000

    feat(tvix/nar-bridge): move rendering logic to separate function
    This allows simply returning an error, instead of dealing with http error
    handling everywhere.
    
    Change-Id: I55238159aab3944ab7a5a38c5b78e45d2b079efa

--- tvix/nar-bridge/pkg/server/narinfo_get.go
+++ tvix/nar-bridge/pkg/server/narinfo_get.go
@@ -1,10 +1,16 @@
 package server
 
 import (
+	"context"
+	"encoding/base64"
+	"errors"
+	"fmt"
 	"io"
+	"io/fs"
 	"net/http"
 	"path"
 	"strings"
+	"sync"
 
 	storev1pb "code.tvl.fyi/tvix/store/protos"
 	"github.com/go-chi/chi/v5"
@@ -17,6 +23,122 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
+
+// renderNarinfo writes narinfo contents to a passes io.Writer, or a returns a
+// (wrapped) io.ErrNoExist error if something doesn't exist.
+// if headOnly is set to true, only the existence is checked, but no content is
+// actually written.
+func renderNarinfo(
+	ctx context.Context,
+	log *log.Entry,
+	pathInfoServiceClient storev1pb.PathInfoServiceClient,
+	narHashToPathInfoMu *sync.Mutex,
+	narHashToPathInfo map[string]*storev1pb.PathInfo,
+	outputHash []byte,
+	w io.Writer,
+	headOnly bool,
+) error {
+	pathInfo, err := pathInfoServiceClient.Get(ctx, &storev1pb.GetPathInfoRequest{
+		ByWhat: &storev1pb.GetPathInfoRequest_ByOutputHash{
+			ByOutputHash: outputHash,
+		},
+	})
+	if err != nil {
+		st, ok := status.FromError(err)
+		if ok {
+			if st.Code() == codes.NotFound {
+				return fmt.Errorf("output hash %v not found: %w", base64.StdEncoding.EncodeToString(outputHash), fs.ErrNotExist)
+			}
+			return fmt.Errorf("unable to get pathinfo, code %v: %w", st.Code(), err)
+		}
+
+		return fmt.Errorf("unable to get pathinfo: %w", err)
+	}
+
+	// extract the narhash (sha256 and sha512)
+	var piNarHashSha256 *nixhash.Hash
+	var piNarHashSha512 *nixhash.Hash
+
+	// TOOD: this is a bit silly right now. We should be able to construct a nixhash
+	// by passing its bytes along, without having to encode and decode again.
+	for _, piNarHash := range pathInfo.Narinfo.NarHashes {
+		if piNarHash.GetAlgo() == storev1pb.NARInfo_SHA256 {
+			piNarHashSha256, err = nixhash.ParseNixBase32(
+				"sha256:" + nixbase32.EncodeToString(piNarHash.GetDigest()),
+			)
+			if err != nil {
+				panic(err)
+			}
+		}
+		if piNarHash.GetAlgo() == storev1pb.NARInfo_SHA512 {
+			piNarHashSha512, err = nixhash.ParseNixBase32(
+				"sha512:" + nixbase32.EncodeToString(piNarHash.GetDigest()),
+			)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+
+	// add things to the lookup table, in case the same process didn't handle the NAR hash yet.
+	narHashToPathInfoMu.Lock()
+	if piNarHashSha256 != nil {
+		narHashToPathInfo[piNarHashSha256.SRIString()] = pathInfo
+	}
+	if piNarHashSha512 != nil {
+		narHashToPathInfo[piNarHashSha512.SRIString()] = pathInfo
+	}
+	narHashToPathInfoMu.Unlock()
+
+	// We need to use one of the nar hashes specified in the pathInfo object to
+	// use in the URL field in the .narinfo file we render.
+	// TODO: we should use the hash that has signatures on it, or filter signatures
+	if piNarHashSha256 == nil && piNarHashSha512 == nil {
+		return fmt.Errorf("No usable NarHash found in PathInfo")
+	}
+
+	if headOnly {
+		return nil
+	}
+
+	// convert the signatures from storev1pb signatures to narinfo signatures
+	narinfoSignatures := make([]signature.Signature, 0)
+	for _, pathInfoSignature := range pathInfo.Narinfo.Signatures {
+		narinfoSignatures = append(narinfoSignatures, signature.Signature{
+			Name: pathInfoSignature.GetName(),
+			Data: pathInfoSignature.GetData(),
+		})
+	}
+
+	// extract the name of the node in the pathInfo structure, which will become the output path
+	nodeName := ""
+	switch v := (pathInfo.Node).(type) {
+	case *storev1pb.PathInfo_File:
+		nodeName = v.File.GetName()
+	case *storev1pb.PathInfo_Symlink:
+		nodeName = v.Symlink.GetName()
+	case *storev1pb.PathInfo_Directory:
+		nodeName = v.Directory.GetName()
+	}
+
+	narInfo := narinfo.NarInfo{
+		StorePath:   path.Join(nixpath.StoreDir, nodeName),
+		URL:         "nar/" + nixbase32.EncodeToString(piNarHashSha256.Digest()) + ".nar",
+		Compression: "none", // TODO: implement zstd compression
+		NarHash:     piNarHashSha256,
+		NarSize:     uint64(pathInfo.Narinfo.NarSize),
+		References:  pathInfo.Narinfo.GetReferenceNames(),
+		Signatures:  narinfoSignatures,
+	}
+
+	// render .narinfo from pathInfo
+	_, err = io.Copy(w, strings.NewReader(narInfo.String()))
+	if err != nil {
+		return fmt.Errorf("unable to write narinfo to client: %w", err)
+	}
+
+	return nil
+}
 
 func registerNarinfoGet(s *Server) {
 	// GET $outHash.narinfo looks up the PathInfo from the tvix-store,
@@ -42,95 +164,14 @@ func registerNarinfoGet(s *Server) {
 			return
 		}
 
-		pathInfo, err := s.pathInfoServiceClient.Get(ctx, &storev1pb.GetPathInfoRequest{
-			ByWhat: &storev1pb.GetPathInfoRequest_ByOutputHash{
-				ByOutputHash: outputHash,
-			},
-		})
+		err = renderNarinfo(ctx, log, s.pathInfoServiceClient, &s.narHashToPathInfoMu, s.narHashToPathInfo, outputHash, w, false)
 		if err != nil {
-			// TODO: how does a 404 look like?
-			st, ok := status.FromError(err)
-			if ok {
-				if st.Code() == codes.NotFound {
-					w.WriteHeader(http.StatusNotFound)
-					w.Write([]byte("not found"))
-				}
-				return
-
+			log.WithError(err).Info("unable to render narinfo")
+			if errors.Is(err, fs.ErrNotExist) {
+				w.WriteHeader(http.StatusNotFound)
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
 			}
-			log.WithError(err).Error("Unable to get pathinfo from store")
-			w.WriteHeader(http.StatusInternalServerError)
-			_, err := w.Write([]byte("Unable to get pathinfo from store"))
-			if err != nil {
-				log.WithError(err).Errorf("unable to write error message to client")
-			}
-
-			return
-		}
-
-		// We need to use one of the nar hashes specified in the pathInfo object to
-		// use in the URL field in the .narinfo file we render.
-		// TODO: we should use the hash that has signatures on it, or filter signatures
-		var narHash *nixhash.Hash
-		for _, piNarHash := range pathInfo.Narinfo.GetNarHashes() {
-			if piNarHash.GetAlgo() == storev1pb.NARInfo_SHA256 {
-				narHash, err = nixhash.ParseNixBase32("sha256:" + nixbase32.EncodeToString(piNarHash.GetDigest()))
-				if err != nil {
-					panic(err)
-				}
-				break
-			} else if piNarHash.GetAlgo() == storev1pb.NARInfo_SHA512 {
-				narHash, err = nixhash.ParseNixBase32("sha512:" + nixbase32.EncodeToString(piNarHash.GetDigest()))
-				if err != nil {
-					panic(err)
-				}
-				break
-			}
-		}
-		if narHash == nil {
-			log.Error("No usable NarHash found in PathInfo")
-			w.WriteHeader(http.StatusInternalServerError)
-			_, err := w.Write([]byte("Unable to get pathinfo from store"))
-			if err != nil {
-				log.WithError(err).Errorf("unable to write error message to client")
-			}
-
-			return
-		}
-		// convert the signatures from storev1pb signatures to narinfo signatures
-		narinfoSignatures := make([]signature.Signature, 0)
-		for _, pathInfoSignature := range pathInfo.Narinfo.Signatures {
-			narinfoSignatures = append(narinfoSignatures, signature.Signature{
-				Name: pathInfoSignature.GetName(),
-				Data: pathInfoSignature.GetData(),
-			})
-		}
-
-		// extract the name of the node in the pathInfo structure, which will become the output path
-		nodeName := ""
-		switch v := (pathInfo.Node).(type) {
-		case *storev1pb.PathInfo_File:
-			nodeName = v.File.GetName()
-		case *storev1pb.PathInfo_Symlink:
-			nodeName = v.Symlink.GetName()
-		case *storev1pb.PathInfo_Directory:
-			nodeName = v.Directory.GetName()
-		}
-
-		narInfo := narinfo.NarInfo{
-			StorePath:   path.Join(nixpath.StoreDir, nodeName),
-			URL:         "nar/" + nixbase32.EncodeToString(narHash.Digest()) + ".nar",
-			Compression: "none", // TODO: implement zstd compression
-			NarHash:     narHash,
-			NarSize:     uint64(pathInfo.Narinfo.NarSize),
-			References:  pathInfo.Narinfo.GetReferenceNames(),
-			Signatures:  narinfoSignatures,
-		}
-
-		// render .narinfo from pathInfo
-		_, err = io.Copy(w, strings.NewReader(narInfo.String()))
-		if err != nil {
-			log.WithError(err).Errorf("unable to write narinfo to client")
 		}
 	})
 }
