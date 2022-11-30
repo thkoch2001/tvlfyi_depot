bdd45491e94a376cc52c9f5467133b734769a29e
Florian Klink <flokli@flokli.de>
2022-11-30 14:49:57 +0000

    feat(tvix/nar-bridge): move rendering logic to separate function
    This allows simply returning an error, instead of dealing with http error
    handling everywhere.
    
    Change-Id: I55238159aab3944ab7a5a38c5b78e45d2b079efa

--- tvix/nar-bridge/pkg/server/nar_get.go
+++ tvix/nar-bridge/pkg/server/nar_get.go
@@ -2,15 +2,20 @@ package server
 
 import (
 	"bytes"
+	"context"
+	"encoding/base64"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"io"
 	"io/fs"
 	"net/http"
+	"sync"
 
 	"code.tvl.fyi/tvix/nar-bridge/pkg/writer"
 	storev1pb "code.tvl.fyi/tvix/store/protos"
 	"github.com/go-chi/chi/v5"
+	nixhash "github.com/nix-community/go-nix/pkg/hash"
 	"github.com/nix-community/go-nix/pkg/nixbase32"
 	log "github.com/sirupsen/logrus"
 )
@@ -18,6 +23,109 @@ import (
 const (
 	narUrl = "/nar/{narhash:^([" + nixbase32.Alphabet + "]{52}|[" + nixbase32.Alphabet + "]{103})$}.nar"
 )
+
+func renderNar(
+	ctx context.Context,
+	log *log.Entry,
+	directoryServiceClient storev1pb.DirectoryServiceClient,
+	blobServiceClient storev1pb.BlobServiceClient,
+	narHashToPathInfoMu *sync.Mutex,
+	narHashToPathInfo map[string]*storev1pb.PathInfo,
+	w io.Writer,
+	narHash *nixhash.Hash,
+	headOnly bool,
+) error {
+	// look in the lookup table
+	narHashToPathInfoMu.Lock()
+	pathInfo, found := narHashToPathInfo[narHash.SRIString()]
+	narHashToPathInfoMu.Unlock()
+
+	// if we didn't find anything, return 404.
+	if !found {
+		return fmt.Errorf("narHash not found: %w", fs.ErrNotExist)
+	}
+
+	// if this was only a head request, we're done.
+	if headOnly {
+		return nil
+	}
+
+	directories := make(map[string]*storev1pb.Directory)
+
+	// If the root node is a directory, ask the directory service for all directories
+	if pathInfoDirectory, isDirectory := pathInfo.GetNode().(*storev1pb.PathInfo_Directory); isDirectory {
+		rootDirectoryDigest := pathInfoDirectory.Directory.GetDigest()
+		log = log.WithField("root_directory_digest", rootDirectoryDigest)
+
+		directoryStream, err := directoryServiceClient.Get(ctx, &storev1pb.GetDirectoryRequest{
+			ByWhat: &storev1pb.GetDirectoryRequest_Digest{
+				Digest: rootDirectoryDigest,
+			},
+			Recursive: true,
+		})
+		if err != nil {
+			return fmt.Errorf("unable to query directory stream: %w", err)
+		}
+
+		// For now, we just stream all of these locally and put them into a hashmap,
+		// which is used in the lookup function below.
+		for {
+			directory, err := directoryStream.Recv()
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				return fmt.Errorf("unable to receive from directory stream: %w", err)
+			}
+
+			// calculate directory digest
+			// TODO: do we need to do any more validation?
+			directoryDgst, err := directory.Digest()
+			if err != nil {
+				return fmt.Errorf("unable to calculate directory digest: %w", err)
+			}
+
+			log.WithField("directory", base64.StdEncoding.EncodeToString(directoryDgst)).Info("received directory node")
+
+			directories[hex.EncodeToString(directoryDgst)] = directory
+		}
+
+	}
+
+	// render the NAR file
+	err := writer.Export(
+		w,
+		pathInfo,
+		func(directoryRef []byte) (*storev1pb.Directory, error) {
+			directoryRefStr := hex.EncodeToString(directoryRef)
+			directory, found := directories[directoryRefStr]
+			if !found {
+				return nil, fmt.Errorf(
+					"directory with hash %v does not exist: %w",
+					directoryRef,
+					fs.ErrNotExist,
+				)
+			}
+
+			return directory, nil
+		},
+		func(blobRef []byte) (io.ReadCloser, error) {
+			log.Infof("Get blob with digest: %x", blobRef)
+			resp, err := blobServiceClient.Get(ctx, &storev1pb.GetBlobRequest{
+				Digest: blobRef,
+			})
+			if err != nil {
+				return nil, fmt.Errorf("unable to get blob: %w", err)
+
+			}
+			return io.NopCloser(bytes.NewReader(resp.GetData())), nil
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("unable to export nar: %w", err)
+	}
+	return nil
+}
 
 func registerNarGet(s *Server) {
 	// TODO: properly compose this
@@ -41,22 +149,15 @@ func registerNarGet(s *Server) {
 
 		log := log.WithField("narhash_url", narHash.SRIString())
 
-		// look in the lookup table
-		s.narHashToPathInfoMu.Lock()
-		_, found := s.narHashToPathInfo[narHash.SRIString()]
-		s.narHashToPathInfoMu.Unlock()
-
-		// if we didn't find anything, return 404.
-		if !found {
-			w.WriteHeader(http.StatusNotFound)
-			_, err := w.Write([]byte("narHash not found"))
-			if err != nil {
-				log.WithError(err).Errorf("unable to write error message to client")
+		err = renderNar(ctx, log, s.directoryServiceClient, s.blobServiceClient, &s.narHashToPathInfoMu, s.narHashToPathInfo, w, narHash, true)
+		if err != nil {
+			log.WithError(err).Info("unable to render nar")
+			if errors.Is(err, fs.ErrNotExist) {
+				w.WriteHeader(http.StatusNotFound)
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
 			}
-
-			return
 		}
-		w.WriteHeader(http.StatusNoContent)
 
 	})
 	s.handler.Get(narUrl, func(w http.ResponseWriter, r *http.Request) {
@@ -78,8 +179,6 @@ func registerNarGet(s *Server) {
 		}
 
 		log := log.WithField("narhash_url", narHash.SRIString())
-
-		var pathInfo *storev1pb.PathInfo
 
 		// look in the lookup table
 		s.narHashToPathInfoMu.Lock()
@@ -158,40 +257,15 @@ func registerNarGet(s *Server) {
 
 				directories[hex.EncodeToString(directoryDgst)] = directory
 			}
-
 		}
 
-		// render the NAR file
-		err = writer.Export(
-			w,
-			pathInfo,
-			func(directoryRef []byte) (*storev1pb.Directory, error) {
-				directoryRefStr := hex.EncodeToString(directoryRef)
-				directory, found := directories[directoryRefStr]
-				if !found {
-					return nil, fmt.Errorf(
-						"directory with hash %v does not exist: %w",
-						directoryRef,
-						fs.ErrNotExist,
-					)
-				}
-
-				return directory, nil
-			},
-			func(blobRef []byte) (io.ReadCloser, error) {
-				log.Infof("Get blob with digest: %x", blobRef)
-				resp, err := s.blobServiceClient.Get(ctx, &storev1pb.GetBlobRequest{
-					Digest: blobRef,
-				})
-				if err != nil {
-					return nil, fmt.Errorf("unable to get blob: %w", err)
-
-				}
-				return io.NopCloser(bytes.NewReader(resp.GetData())), nil
-			},
-		)
+		err = renderNar(ctx, log, s.directoryServiceClient, s.blobServiceClient, &s.narHashToPathInfoMu, s.narHashToPathInfo, w, narHash, true)
 		if err != nil {
-			log.WithError(err).Error("unable to export nar")
+			if errors.Is(err, fs.ErrNotExist) {
+				w.WriteHeader(http.StatusNotFound)
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
 		}
 	})
 }
--- tvix/nar-bridge/pkg/server/narinfo_get.go
+++ tvix/nar-bridge/pkg/server/narinfo_get.go
@@ -1,7 +1,12 @@
 package server
 
 import (
+	"context"
+	"encoding/base64"
+	"errors"
+	"fmt"
 	"io"
+	"io/fs"
 	"net/http"
 	"path"
 	"strings"
@@ -17,6 +22,99 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
+
+// renderNarinfo writes narinfo contents to a passes io.Writer, or a returns a
+// (wrapped) io.ErrNoExist error if something doesn't exist.
+// if headOnly is set to true, only the existence is checked, but no content is
+// actually written.
+func renderNarinfo(
+	ctx context.Context,
+	pathInfoServiceClient storev1pb.PathInfoServiceClient,
+	outputHash []byte,
+	w io.Writer,
+	headOnly bool,
+) error {
+	pathInfo, err := pathInfoServiceClient.Get(ctx, &storev1pb.GetPathInfoRequest{
+		ByWhat: &storev1pb.GetPathInfoRequest_ByOutputHash{
+			ByOutputHash: outputHash,
+		},
+	})
+	if err != nil {
+		st, ok := status.FromError(err)
+		if ok {
+			if st.Code() == codes.NotFound {
+				return fmt.Errorf("output hash %v not found: %w", base64.StdEncoding.EncodeToString(outputHash), fs.ErrNotExist)
+			}
+			return fmt.Errorf("unable to get pathinfo, code %v: %w", st.Code(), err)
+		}
+
+		return fmt.Errorf("unable to get pathinfo: %w", err)
+	}
+
+	if headOnly {
+		return nil
+	}
+
+	// We need to use one of the nar hashes specified in the pathInfo object to
+	// use in the URL field in the .narinfo file we render.
+	// TODO: we should use the hash that has signatures on it, or filter signatures
+	var narHash *nixhash.Hash
+	for _, piNarHash := range pathInfo.Narinfo.GetNarHashes() {
+		if piNarHash.GetAlgo() == storev1pb.NARInfo_SHA256 {
+			narHash, err = nixhash.ParseNixBase32("sha256:" + nixbase32.EncodeToString(piNarHash.GetDigest()))
+			if err != nil {
+				panic(err)
+			}
+			break
+		} else if piNarHash.GetAlgo() == storev1pb.NARInfo_SHA512 {
+			narHash, err = nixhash.ParseNixBase32("sha512:" + nixbase32.EncodeToString(piNarHash.GetDigest()))
+			if err != nil {
+				panic(err)
+			}
+			break
+		}
+	}
+	if narHash == nil {
+		return fmt.Errorf("No usable NarHash found in PathInfo")
+	}
+	// convert the signatures from storev1pb signatures to narinfo signatures
+	narinfoSignatures := make([]signature.Signature, 0)
+	for _, pathInfoSignature := range pathInfo.Narinfo.Signatures {
+		narinfoSignatures = append(narinfoSignatures, signature.Signature{
+			Name: pathInfoSignature.GetName(),
+			Data: pathInfoSignature.GetData(),
+		})
+	}
+
+	// extract the name of the node in the pathInfo structure, which will become the output path
+	nodeName := ""
+	switch v := (pathInfo.Node).(type) {
+	case *storev1pb.PathInfo_File:
+		nodeName = v.File.GetName()
+	case *storev1pb.PathInfo_Symlink:
+		nodeName = v.Symlink.GetName()
+	case *storev1pb.PathInfo_Directory:
+		nodeName = v.Directory.GetName()
+	}
+
+	narInfo := narinfo.NarInfo{
+		StorePath:   path.Join(nixpath.StoreDir, nodeName),
+		URL:         "nar/" + nixbase32.EncodeToString(narHash.Digest()) + ".nar",
+		Compression: "none", // TODO: implement zstd compression
+		NarHash:     narHash,
+		NarSize:     uint64(pathInfo.Narinfo.NarSize),
+		References:  pathInfo.Narinfo.GetReferenceNames(),
+		Signatures:  narinfoSignatures,
+	}
+
+	// render .narinfo from pathInfo
+	_, err = io.Copy(w, strings.NewReader(narInfo.String()))
+	if err != nil {
+		return fmt.Errorf("unable to write narinfo to client: %w", err)
+	}
+
+	return nil
+}
 
 func registerNarinfoGet(s *Server) {
 	// GET $outHash.narinfo looks up the PathInfo from the tvix-store,
@@ -42,95 +140,14 @@ func registerNarinfoGet(s *Server) {
 			return
 		}
 
-		pathInfo, err := s.pathInfoServiceClient.Get(ctx, &storev1pb.GetPathInfoRequest{
-			ByWhat: &storev1pb.GetPathInfoRequest_ByOutputHash{
-				ByOutputHash: outputHash,
-			},
-		})
+		err = renderNarinfo(ctx, s.pathInfoServiceClient, outputHash, w, false)
 		if err != nil {
-			// TODO: how does a 404 look like?
-			st, ok := status.FromError(err)
-			if ok {
-				if st.Code() == codes.NotFound {
-					w.WriteHeader(http.StatusNotFound)
-					w.Write([]byte("not found"))
-				}
-				return
-
+			log.WithError(err).Info("unable to render narinfo")
+			if errors.Is(err, fs.ErrNotExist) {
+				w.WriteHeader(http.StatusNotFound)
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
 			}
-			log.WithError(err).Error("Unable to get pathinfo from store")
-			w.WriteHeader(http.StatusInternalServerError)
-			_, err := w.Write([]byte("Unable to get pathinfo from store"))
-			if err != nil {
-				log.WithError(err).Errorf("unable to write error message to client")
-			}
-
-			return
-		}
-
-		// We need to use one of the nar hashes specified in the pathInfo object to
-		// use in the URL field in the .narinfo file we render.
-		// TODO: we should use the hash that has signatures on it, or filter signatures
-		var narHash *nixhash.Hash
-		for _, piNarHash := range pathInfo.Narinfo.GetNarHashes() {
-			if piNarHash.GetAlgo() == storev1pb.NARInfo_SHA256 {
-				narHash, err = nixhash.ParseNixBase32("sha256:" + nixbase32.EncodeToString(piNarHash.GetDigest()))
-				if err != nil {
-					panic(err)
-				}
-				break
-			} else if piNarHash.GetAlgo() == storev1pb.NARInfo_SHA512 {
-				narHash, err = nixhash.ParseNixBase32("sha512:" + nixbase32.EncodeToString(piNarHash.GetDigest()))
-				if err != nil {
-					panic(err)
-				}
-				break
-			}
-		}
-		if narHash == nil {
-			log.Error("No usable NarHash found in PathInfo")
-			w.WriteHeader(http.StatusInternalServerError)
-			_, err := w.Write([]byte("Unable to get pathinfo from store"))
-			if err != nil {
-				log.WithError(err).Errorf("unable to write error message to client")
-			}
-
-			return
-		}
-		// convert the signatures from storev1pb signatures to narinfo signatures
-		narinfoSignatures := make([]signature.Signature, 0)
-		for _, pathInfoSignature := range pathInfo.Narinfo.Signatures {
-			narinfoSignatures = append(narinfoSignatures, signature.Signature{
-				Name: pathInfoSignature.GetName(),
-				Data: pathInfoSignature.GetData(),
-			})
-		}
-
-		// extract the name of the node in the pathInfo structure, which will become the output path
-		nodeName := ""
-		switch v := (pathInfo.Node).(type) {
-		case *storev1pb.PathInfo_File:
-			nodeName = v.File.GetName()
-		case *storev1pb.PathInfo_Symlink:
-			nodeName = v.Symlink.GetName()
-		case *storev1pb.PathInfo_Directory:
-			nodeName = v.Directory.GetName()
-		}
-
-		narInfo := narinfo.NarInfo{
-			StorePath:   path.Join(nixpath.StoreDir, nodeName),
-			URL:         "nar/" + nixbase32.EncodeToString(narHash.Digest()) + ".nar",
-			Compression: "none", // TODO: implement zstd compression
-			NarHash:     narHash,
-			NarSize:     uint64(pathInfo.Narinfo.NarSize),
-			References:  pathInfo.Narinfo.GetReferenceNames(),
-			Signatures:  narinfoSignatures,
-		}
-
-		// render .narinfo from pathInfo
-		_, err = io.Copy(w, strings.NewReader(narInfo.String()))
-		if err != nil {
-			log.WithError(err).Errorf("unable to write narinfo to client")
 		}
 	})
 }
