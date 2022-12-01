0ba5aeea6016e02e82b67848274786762e6d17ca
sterni <sternenseemann@systemli.org>
2022-12-01 17:36:50 +0100

    feat(sterni/aoc/2022): day01 solutions
    Bonus solution in k as I got sniped by leah2.
    
    Change-Id: I806f5b2ac1388159a427bb239bfb1bb7aeb329d7

--- users/sterni/exercises/aoc/2022/nix.nix
+++ users/sterni/exercises/aoc/2022/nix.nix
@@ -1,3 +1,30 @@
-{ ... }:
+{ lib ? import <nixpkgs/lib> }:
 
-{ }
+let
+  chomp = lib.removeSuffix "\n";
+  lines = s: builtins.filter builtins.isString (builtins.split "\n" (chomp s));
+  sum = builtins.foldl' builtins.add 0;
+
+  day01 =
+    let
+      input =
+        builtins.map
+          (elf:
+            sum (builtins.map builtins.fromJSON (lines elf))
+          )
+          (
+            builtins.filter builtins.isString (
+              builtins.split "\n\n" (builtins.readFile ./01/input)
+            )
+          );
+    in
+    {
+      "1" = builtins.foldl' lib.max (-1) input;
+      "2" = sum (lib.sublist 0 3 (lib.sort (a: b: a >= b) input));
+    };
+
+in
+
+{
+  inherit day01;
+}
