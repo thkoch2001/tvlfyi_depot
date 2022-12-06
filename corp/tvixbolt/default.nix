c4dc1f3826394a8a2e61be3498fcda333ed3cc5c
Vincent Ambo <mail@tazj.in>
2022-12-06 16:31:09 +0300

    fix(tvix/eval): use rnix-parser from crates.io
    A few weeks ago, oberblastmeister did a release to crates.io so we can
    stop importing it via GitHub.
    
    Change-Id: I9d5fa5cd281685779c71b12fed45ed201a1db17e

--- corp/tvixbolt/default.nix
+++ corp/tvixbolt/default.nix
@@ -37,19 +37,20 @@ let
   # Cargo.toml needs to be patched with the /nix/store source path of
   # tvix-eval.
   cargoTomlPatch = pkgs.writeText "tvix-eval-src.patch" ''
-    diff --git a/Cargo.toml b/Cargo.toml
-    index 2e6c793..67280e7 100644
-    --- a/Cargo.toml
-    +++ b/Cargo.toml
-    @@ -18,5 +18,5 @@ git = "https://github.com/nix-community/rnix-parser.git"
-     rev = "97b438e34be5211a4b48aeed9cc3ded489b4d6da"
+    diff --git a/corp/tvixbolt/Cargo.toml b/corp/tvixbolt/Cargo.toml
+    index 75006bec18..6ca244bbb2 100644
+    --- a/corp/tvixbolt/Cargo.toml
+    +++ b/corp/tvixbolt/Cargo.toml
+    @@ -16,7 +16,7 @@ rnix = "0.11.0"
+     wasm-bindgen = "= 0.2.83"
 
      [dependencies.tvix-eval]
     -path = "../../tvix/eval"
     +path = "${depot.tvix.eval.src}"
      default-features = false
-  '';
 
+     [dependencies.serde]
+  '';
 in
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "tvixbolt";
@@ -57,9 +58,6 @@ pkgs.rustPlatform.buildRustPackage rec {
   src = lib.cleanSource ./.;
 
   cargoLock.lockFile = ./Cargo.lock;
-  cargoLock.outputHashes = {
-    "rnix-0.11.0-dev" = "sha256:01c3fdsfyp8iwr36nv2mvr2xw33ci3vcx6pw8a9qrc8pjr6q22f8";
-  };
 
   patches = [
     cargoTomlPatch
