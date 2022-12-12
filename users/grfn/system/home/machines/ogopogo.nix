8a4c564f88022d3cf57b52d47f483b145f13ebbb
Griffin Smith <root@gws.fyi>
2022-12-12 10:23:38 -0500

    feat(grfn/system): Upgrade discord
    discord just... refuses to launch if you're running an old version, so
    we basically always have to override
    
    Change-Id: Ia12b31da1d235fe023c06342e00b53be38d8dcd9

--- users/grfn/system/home/machines/ogopogo.nix
+++ users/grfn/system/home/machines/ogopogo.nix
@@ -33,7 +33,13 @@ in
     xdot
     tdesktop
     subsurface
-    discord
+    (discord.override rec {
+      version = "0.0.22";
+      src = fetchurl {
+        url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
+        sha256 = "19xbmrd782m4lp2l0ww5v3ip227g0z8pplxigxga96q43rvp6p0p";
+      };
+    })
     steam
   ];
 
