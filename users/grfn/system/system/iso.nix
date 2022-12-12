7e29f19e474d049f6558cb28abc64546b9114674
sterni <sternenseemann@systemli.org>
2022-12-12 10:48:21 +0100

    chore(3p/sources): Bump channels & overlays
    * //users/grfn/modules/games: StoneSense requires DFHack to be enabled
    
    * //users/grfn/system/iso: gtk needs cairo with xlibs
    
    * //users: deal with emacs-overlay attribute renames
    
    * //3p/overlays: downgrade sbcl to workaround lisp-binary compilation
      failure
    
    Change-Id: I1573280a923e5021ef399734cb5519b37e72c670

--- users/grfn/system/system/iso.nix
+++ users/grfn/system/system/iso.nix
@@ -11,6 +11,10 @@ let
     networking.useDHCP = false;
     networking.firewall.enable = false;
     networking.wireless.enable = lib.mkForce false;
+
+    # TODO(grfn): enabling this (in the minimal profile) fails the iso build,
+    # since gtk+3 needs to be built which fails due to cairo without xlibs
+    environment.noXlibs = false;
   };
 in
 (depot.third_party.nixos {
