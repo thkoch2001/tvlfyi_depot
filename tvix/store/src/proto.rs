b97453485208ca8adf59c599fedc5273da3a42ce
Florian Klink <flokli@flokli.de>
2022-12-27 18:10:46 +0100

    feat(tvix/store): implement Directory::validate()
    Change-Id: I4c6ae79d705b8e19a3e2ed54812366e88935d7a6

--- tvix/store/src/proto.rs
+++ tvix/store/src/proto.rs
@@ -1,6 +1,22 @@
+use anyhow::Result;
+use std::collections::HashSet;
+use thiserror::Error;
+
 use prost::Message;
 
 tonic::include_proto!("tvix.store.v1");
+
+#[derive(Debug, Error, PartialEq)]
+pub enum ValidateDirectoryError {
+    #[error("{0} is not sorted")]
+    WrongSorting(String),
+    #[error("{0} is a duplicate name")]
+    DuplicateName(String),
+    #[error("Invalid name in {0}")]
+    InvalidName(String),
+    #[error("Ivalid Digest length: {0}")]
+    InvalidDigestLen(usize),
+}
 
 impl Directory {
     // The size of a directory is the number of all regular and symlink elements,
@@ -19,11 +35,126 @@ impl Directory {
 
         hasher.update(&self.encode_to_vec()).finalize().as_bytes()[..].to_vec()
     }
+
+    // name_is_valid checks a name for validity.
+    // We disallow slashes, null bytes, '.', '..' and the empty string.
+    // Depending on the context, a *Node message with an empty string as name isValidName
+    // allowed, but they don't occur inside a Directory message.
+    fn name_is_valid(name: &String) -> bool {
+        if name == "" || name == ".." || name == "." || name.contains("\x00") || name.contains("/")
+        {
+            return false;
+        }
+        return true;
+    }
+
+    // validate checks the directory for invalid data, such as:
+    // - violations of name restrictions
+    // - invalid digest lengths
+    // - not properly sorted lists
+    // - duplicate names in the three lists
+    pub fn validate(&self) -> Result<(), ValidateDirectoryError> {
+        let mut seen_names = HashSet::new();
+
+        let mut last_directory_name = "".to_string();
+        let mut last_file_name = "".to_string();
+        let mut last_symlink_name = "".to_string();
+
+        // check directories
+        for directory_node in &self.directories {
+            // check name for validity
+            if !Directory::name_is_valid(&directory_node.name) {
+                return Err(ValidateDirectoryError::InvalidName(
+                    directory_node.name.clone(),
+                ));
+            }
+
+            // check digest to be 32 bytes
+            if directory_node.digest.len() != 32 {
+                return Err(ValidateDirectoryError::InvalidDigestLen(
+                    directory_node.digest.len(),
+                ));
+            }
+
+            // ensure names are sorted
+            if directory_node.name < last_directory_name {
+                return Err(ValidateDirectoryError::WrongSorting(
+                    directory_node.name.clone(),
+                ));
+            }
+            last_directory_name = directory_node.name.clone();
+
+            // ensure names are unique
+            if seen_names.get(&directory_node.name).is_some() {
+                return Err(ValidateDirectoryError::DuplicateName(
+                    directory_node.name.clone(),
+                ));
+            }
+            seen_names.insert(directory_node.name.clone());
+        }
+
+        // check files
+        for file_node in &self.files {
+            // check name for validity
+            if !Directory::name_is_valid(&file_node.name) {
+                return Err(ValidateDirectoryError::InvalidName(file_node.name.clone()));
+            }
+
+            // check digest to be 32 bytes
+            if file_node.digest.len() != 32 {
+                return Err(ValidateDirectoryError::InvalidDigestLen(
+                    file_node.digest.len(),
+                ));
+            }
+
+            // ensure names are sorted
+            if file_node.name < last_file_name {
+                return Err(ValidateDirectoryError::WrongSorting(file_node.name.clone()));
+            }
+            last_file_name = file_node.name.clone();
+
+            // ensure names are unique
+            if seen_names.get(&file_node.name).is_some() {
+                return Err(ValidateDirectoryError::DuplicateName(
+                    file_node.name.clone(),
+                ));
+            }
+            seen_names.insert(file_node.name.clone());
+        }
+
+        // check symlinks
+        for symlink_node in &self.symlinks {
+            // check name for validity
+            if !Directory::name_is_valid(&symlink_node.name) {
+                return Err(ValidateDirectoryError::InvalidName(
+                    symlink_node.name.clone(),
+                ));
+            }
+
+            // ensure names are sorted
+            if symlink_node.name < last_symlink_name {
+                return Err(ValidateDirectoryError::WrongSorting(
+                    symlink_node.name.clone(),
+                ));
+            }
+            last_symlink_name = symlink_node.name.clone();
+
+            // ensure names are unique
+            if seen_names.get(&symlink_node.name).is_some() {
+                return Err(ValidateDirectoryError::DuplicateName(
+                    symlink_node.name.clone(),
+                ));
+            }
+            seen_names.insert(symlink_node.name.clone());
+        }
+
+        Ok(())
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{Directory, DirectoryNode, FileNode, SymlinkNode};
+    use super::{Directory, DirectoryNode, FileNode, SymlinkNode, ValidateDirectoryError};
     use lazy_static::lazy_static;
 
     lazy_static! {
@@ -109,5 +240,228 @@ mod tests {
                 0xe4, 0x1f, 0x32, 0x62
             ]
         )
+    }
+
+    #[test]
+    fn test_directory_validate_empty() {
+        let d = Directory {
+            directories: vec![],
+            files: vec![],
+            symlinks: vec![],
+        };
+        assert_eq!(d.validate(), Ok(()));
+    }
+
+    #[test]
+    fn test_directory_validate_invalid_names() {
+        {
+            let d = Directory {
+                directories: vec![DirectoryNode {
+                    name: "".to_string(),
+                    digest: DUMMY_DIGEST.to_vec(),
+                    size: 42,
+                }],
+                files: vec![],
+                symlinks: vec![],
+            };
+            match d.validate().expect_err("must fail") {
+                ValidateDirectoryError::InvalidName(n) => {
+                    assert_eq!(n, "")
+                }
+                _ => panic!("unexpected error"),
+            };
+        }
+
+        {
+            let d = Directory {
+                directories: vec![DirectoryNode {
+                    name: ".".to_string(),
+                    digest: DUMMY_DIGEST.to_vec(),
+                    size: 42,
+                }],
+                files: vec![],
+                symlinks: vec![],
+            };
+            match d.validate().expect_err("must fail") {
+                ValidateDirectoryError::InvalidName(n) => {
+                    assert_eq!(n, ".")
+                }
+                _ => panic!("unexpected error"),
+            };
+        }
+
+        {
+            let d = Directory {
+                directories: vec![],
+                files: vec![FileNode {
+                    name: "..".to_string(),
+                    digest: DUMMY_DIGEST.to_vec(),
+                    size: 42,
+                    executable: false,
+                }],
+                symlinks: vec![],
+            };
+            match d.validate().expect_err("must fail") {
+                ValidateDirectoryError::InvalidName(n) => {
+                    assert_eq!(n, "..")
+                }
+                _ => panic!("unexpected error"),
+            };
+        }
+
+        {
+            let d = Directory {
+                directories: vec![],
+                files: vec![],
+                symlinks: vec![SymlinkNode {
+                    name: "\x00".to_string(),
+                    target: "foo".to_string(),
+                }],
+            };
+            match d.validate().expect_err("must fail") {
+                ValidateDirectoryError::InvalidName(n) => {
+                    assert_eq!(n, "\x00")
+                }
+                _ => panic!("unexpected error"),
+            };
+        }
+
+        {
+            let d = Directory {
+                directories: vec![],
+                files: vec![],
+                symlinks: vec![SymlinkNode {
+                    name: "foo/bar".to_string(),
+                    target: "foo".to_string(),
+                }],
+            };
+            match d.validate().expect_err("must fail") {
+                ValidateDirectoryError::InvalidName(n) => {
+                    assert_eq!(n, "foo/bar")
+                }
+                _ => panic!("unexpected error"),
+            };
+        }
+    }
+
+    #[test]
+    fn test_directory_validate_invalid_digest() {
+        let d = Directory {
+            directories: vec![DirectoryNode {
+                name: "foo".to_string(),
+                digest: vec![],
+                size: 42,
+            }],
+            files: vec![],
+            symlinks: vec![],
+        };
+        match d.validate().expect_err("must fail") {
+            ValidateDirectoryError::InvalidDigestLen(n) => {
+                assert_eq!(n, 0)
+            }
+            _ => panic!("unexpected error"),
+        }
+    }
+
+    #[test]
+    fn test_directory_validate_sorting() {
+        // "b" comes before "a", bad.
+        {
+            let d = Directory {
+                directories: vec![
+                    DirectoryNode {
+                        name: "b".to_string(),
+                        digest: DUMMY_DIGEST.to_vec(),
+                        size: 42,
+                    },
+                    DirectoryNode {
+                        name: "a".to_string(),
+                        digest: DUMMY_DIGEST.to_vec(),
+                        size: 42,
+                    },
+                ],
+                files: vec![],
+                symlinks: vec![],
+            };
+            match d.validate().expect_err("must fail") {
+                ValidateDirectoryError::WrongSorting(s) => {
+                    assert_eq!(s, "a".to_string());
+                }
+                _ => panic!("unexpected error"),
+            }
+        }
+
+        // "a" exists twice, bad.
+        {
+            let d = Directory {
+                directories: vec![
+                    DirectoryNode {
+                        name: "a".to_string(),
+                        digest: DUMMY_DIGEST.to_vec(),
+                        size: 42,
+                    },
+                    DirectoryNode {
+                        name: "a".to_string(),
+                        digest: DUMMY_DIGEST.to_vec(),
+                        size: 42,
+                    },
+                ],
+                files: vec![],
+                symlinks: vec![],
+            };
+            match d.validate().expect_err("must fail") {
+                ValidateDirectoryError::DuplicateName(s) => {
+                    assert_eq!(s, "a".to_string());
+                }
+                _ => panic!("unexpected error"),
+            }
+        }
+
+        // "a" comes before "b", all good.
+        {
+            let d = Directory {
+                directories: vec![
+                    DirectoryNode {
+                        name: "a".to_string(),
+                        digest: DUMMY_DIGEST.to_vec(),
+                        size: 42,
+                    },
+                    DirectoryNode {
+                        name: "b".to_string(),
+                        digest: DUMMY_DIGEST.to_vec(),
+                        size: 42,
+                    },
+                ],
+                files: vec![],
+                symlinks: vec![],
+            };
+
+            d.validate().expect("validate shouldn't error");
+        }
+
+        // [b, c] and [a] are both properly sorted.
+        {
+            let d = Directory {
+                directories: vec![
+                    DirectoryNode {
+                        name: "b".to_string(),
+                        digest: DUMMY_DIGEST.to_vec(),
+                        size: 42,
+                    },
+                    DirectoryNode {
+                        name: "c".to_string(),
+                        digest: DUMMY_DIGEST.to_vec(),
+                        size: 42,
+                    },
+                ],
+                files: vec![],
+                symlinks: vec![SymlinkNode {
+                    name: "a".to_string(),
+                    target: "foo".to_string(),
+                }],
+            };
+
+            d.validate().expect("validate shouldn't error");
+        }
     }
 }
