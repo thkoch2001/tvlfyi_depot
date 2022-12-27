83addd91fc9723162f46fb6d43f6331492fd4c9f
sterni <sternenseemann@systemli.org>
2022-12-27 14:32:52 +0100

    chore(3p/sources): Bump channels & overlays
    * //3p/napalm: patch for compat with hashable 1.4 until
      upstream PR is merged
    
    Change-Id: I2170438c2ce8130b65f1a9fe07c4fecab5683d66

--- third_party/napalm/default.nix
+++ third_party/napalm/default.nix
@@ -1,7 +1,23 @@
-{ depot, pkgs, ... }:
+{ depot, pkgs, lib, ... }:
 
-pkgs.callPackage depot.third_party.sources.napalm { } // {
-  meta.ci.targets = [
-    "napalm-registry"
-  ];
-}
+lib.updateManyAttrsByPath
+  [
+    {
+      path = [ "napalm-registry" ];
+      update = pkgs.haskell.lib.compose.appendPatch
+        # https://github.com/nix-community/napalm/pull/52
+        (pkgs.fetchpatch {
+          name = "napalm-registry-hashable-1.4.patch";
+          url = "https://github.com/nix-community/napalm/pull/52/commits/09ba608b4845bf1d832f562191eb6a13c12a1e2d.patch";
+          sha256 = "05j73w6fhqhlvr5h7cfk342lpsi9khm3gsc5fsz3ch3iv2pcrwgk";
+          stripLen = 1;
+        });
+    }
+  ]
+  (
+    pkgs.callPackage depot.third_party.sources.napalm { } // {
+      meta.ci.targets = [
+        "napalm-registry"
+      ];
+    }
+  )
