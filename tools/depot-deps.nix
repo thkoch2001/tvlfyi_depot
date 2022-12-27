12bf65d8ae5ce025333b3a6739fb81243097c0c7
Vincent Ambo <mail@tazj.in>
2022-12-27 14:18:49 +0300

    feat(tools/fetch-depot-inbox): wrapper script to fetch depot maildir
    This script fetches the inbox for depot@tvl.su into the specified
    directory in maildir format.
    
    The layout of the folder follows the structure generated by public
    inbox, i.e. the directory containing the current maildir will be
    `$TARGET/su.tvl.depot.0`, but most mail clients (e.g notmuch) will
    figure this out on their own.
    
    ----
    
    In addition, we would ideally find a CLI mail client that can be
    pointed at an arbitrary maildir (or an IMAP server) and works with
    local `sendmail` config so that people can have a single command entry
    point to interacting with depot@tvl.su.
    
    Change-Id: Iaf9fcce73e9caa2f202327488c43d0394be26ca6

--- tools/depot-deps.nix
+++ tools/depot-deps.nix
@@ -6,6 +6,7 @@ depot.nix.lazy-deps {
   age-keygen.attr = "third_party.nixpkgs.age";
   age.attr = "third_party.nixpkgs.age";
   depotfmt.attr = "tools.depotfmt";
+  fetch-depot-inbox.attr = "tools.fetch-depot-inbox";
   gerrit-update.attr = "tools.gerrit-update";
   gerrit.attr = "tools.gerrit-cli";
   hash-password.attr = "tools.hash-password";
