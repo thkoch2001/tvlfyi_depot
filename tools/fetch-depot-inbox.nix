10ce54dbc80c257ea1bdd88e72a4c85d406f289e
Vincent Ambo <mail@tazj.in>
2022-12-27 14:18:49 +0300

    feat(tools/fetch-depot-inbox): wrapper script to fetch depot maildir
    This script fetches the inbox for depot@tvl.su into the specified
    directory in maildir format.
    
    The layout of the folder follows the structure generated by public
    inbox, i.e. the directory containing the current maildir will be
    `$TARGET/su.tvl.depot.0`, but most mail clients (e.g notmuch) will
    figure this out on their own.
    
    ----
    
    In addition, we would ideally find a CLI mail client that can be
    pointed at an arbitrary maildir (or an IMAP server) and works with
    local `sendmail` config so that people can have a single command entry
    point to interacting with depot@tvl.su.
    
    Change-Id: Iaf9fcce73e9caa2f202327488c43d0394be26ca6

---
+++ tools/fetch-depot-inbox.nix
@@ -0,0 +1,49 @@
+# Wrapper script that uses offlineimap to fetch the depot inbox from
+# inbox.tvl.su.
+#
+# Run with the desired output directory as the only argument.
+#
+# Alternatively, users can browse the inbox on https://inbox.tvl.su
+# and interact with public-inbox in any other supported way (IMAP,
+# NNTP, git, etc.).
+{ pkgs, depot, ... }:
+
+let
+  config = pkgs.writeText "offlineimaprc" ''
+    [general]
+    accounts = depot
+
+    [Account depot]
+    localrepository = Local
+    remoterepository = Remote
+
+    [Repository Local]
+    type = Maildir
+    # localfolders set by CLI
+
+    [Repository Remote]
+    type = IMAP
+    ssl = yes
+    sslcacertfile = /etc/ssl/certs/ca-bundle.crt
+    remotehost = inbox.tvl.su
+    remoteuser = anonymous
+    remotepass = anonymous
+  '';
+in
+pkgs.writeShellScriptBin "fetch-depot-inbox" ''
+  readonly MAILDIR=''${1}
+
+  if [ -z "''${MAILDIR}" ]; then
+    echo "[inbox] must specify target maildir as the first argument!" >&2
+    exit 1
+  fi
+
+  if [ ! -d "''${MAILDIR}" ]; then
+    echo "[inbox] specified maildir must exist and be a directory!" >&2
+    exit 1
+  fi
+
+  echo "[inbox] Synchronising TVL depot inbox into ''${MAILDIR}"
+  ${pkgs.offlineimap}/bin/offlineimap -c ${config} \
+    -k "Repository_Local:localfolders=''${MAILDIR}"
+''
