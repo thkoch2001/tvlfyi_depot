28286d9747f387f43ac896bb107d29bfedee5d58
Florian Klink <flokli@flokli.de>
2022-12-28 16:40:28 +0100

    feat(tvix/store): initial dummy implementation
    This replaces the hello world example from tvix-store with an actual
    gRPC endpoint, implementing all of BlobService, DirectoryService and
    PathInfoService.
    
    All RPC methods currently respond with the unimplemented gRPC status.
    
    Co-Authored-By: Márton Boros <martonboros@gmail.com>
    Change-Id: Ieba333cca44dc1e3f2ffbe676ba7a99e672b9bfb

--- tvix/store/src/main.rs
+++ tvix/store/src/main.rs
@@ -1,9 +1,47 @@
+use crate::proto::blob_service_server::BlobServiceServer;
+use crate::proto::directory_service_server::DirectoryServiceServer;
+use crate::proto::path_info_service_server::PathInfoServiceServer;
+
+use clap::Parser;
+use tonic::{transport::Server, Result};
+
+mod dummy_blob_service;
+mod dummy_directory_service;
+mod dummy_path_info_service;
 mod proto;
 mod nixbase32;
 
 #[cfg(test)]
 mod tests;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(long, short = 'l')]
+    listen_address: Option<String>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = Cli::parse();
+    let listen_address = cli
+        .listen_address
+        .unwrap_or("[::]:8000".to_string())
+        .parse()
+        .unwrap();
+
+    let blob_service = dummy_blob_service::DummyBlobService {};
+    let directory_service = dummy_directory_service::DummyDirectoryService {};
+    let path_info_service = dummy_path_info_service::DummyPathInfoService {};
+
+    println!("tvix-store listening on {}", listen_address);
+
+    Server::builder()
+        .add_service(BlobServiceServer::new(blob_service))
+        .add_service(DirectoryServiceServer::new(directory_service))
+        .add_service(PathInfoServiceServer::new(path_info_service))
+        .serve(listen_address)
+        .await?;
+
+    Ok(())
 }
