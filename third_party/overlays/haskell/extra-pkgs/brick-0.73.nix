bcb1cb3691d7b236ce1337cb64f32a5f8413d981
sterni <sternenseemann@systemli.org>
2022-12-29 11:36:08 +0100

    chore(grfn/xanthous): port to brick-0.73
    This is the latest version before 1.0 which has some bigger changes as
    brick's EventM gains the ability to be MonadState which requires
    adjusting basically all App code.
    
    Change-Id: Ic6a1ce6e21ba46177d3ce0b8a124abe7d8951464

---
+++ third_party/overlays/haskell/extra-pkgs/brick-0.73.nix
@@ -0,0 +1,25 @@
+{ mkDerivation, base, bytestring, config-ini, containers
+, contravariant, data-clist, deepseq, directory, dlist, exceptions
+, filepath, lib, microlens, microlens-mtl, microlens-th, QuickCheck
+, stm, template-haskell, text, text-zipper, transformers, unix
+, vector, vty, word-wrap
+}:
+mkDerivation {
+  pname = "brick";
+  version = "0.73";
+  sha256 = "741c8d0717f0ab5addd5d3acc88cb36d645a0c73907bde509b2fd9d9bc02039c";
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    base bytestring config-ini containers contravariant data-clist
+    deepseq directory dlist exceptions filepath microlens microlens-mtl
+    microlens-th stm template-haskell text text-zipper transformers
+    unix vector vty word-wrap
+  ];
+  testHaskellDepends = [
+    base containers microlens QuickCheck vector vty
+  ];
+  homepage = "https://github.com/jtdaugherty/brick/";
+  description = "A declarative terminal user interface library";
+  license = lib.licenses.bsd3;
+}
