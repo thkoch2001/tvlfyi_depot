277c46513b53e695048568480cf65beefa66f062
Florian Klink <flokli@flokli.de>
2022-12-29 22:47:02 +0100

    feat(tvix/store): add nixbase32 mod
    This implements the nix-specific base32 encoding and decoding, exposing
    a subset of the API that the data-encoding crate provides.
    
    Nix uses a custom alphabet, no padding, and encodes bytes in reverse
    order. The latter one is the reason we can't just use the data-encoding
    crate directly.
    
    Change-Id: I73fab6ddd67177d882e4c3f2b48761c95853d558

--- tvix/Cargo.nix
+++ tvix/Cargo.nix
@@ -1438,6 +1438,20 @@ rec {
         ];
 
       };
+      "data-encoding" = rec {
+        crateName = "data-encoding";
+        version = "2.3.3";
+        edition = "2018";
+        sha256 = "1yq8jnivxsjzl3mjbjdjg5kfvd17wawbmg1jvsfw6cqmn1n6dn13";
+        authors = [
+          "Julien Cretin <git@ia0.eu>"
+        ];
+        features = {
+          "default" = [ "std" ];
+          "std" = [ "alloc" ];
+        };
+        resolvedDefaultFeatures = [ "alloc" "default" "std" ];
+      };
       "derivation" = rec {
         crateName = "derivation";
         version = "0.1.0";
@@ -1450,31 +1464,23 @@ rec {
           else ./derivation;
         dependencies = [
           {
-            name = "blake3";
-            packageId = "blake3";
-            features = [ "rayon" "std" ];
+            name = "glob";
+            packageId = "glob";
           }
           {
-            name = "maplit";
-            packageId = "maplit";
-          }
-          {
-            name = "prost";
-            packageId = "prost";
-          }
-          {
-            name = "tonic";
-            packageId = "tonic";
+            name = "serde";
+            packageId = "serde";
+            features = [ "derive" ];
           }
         ];
-        buildDependencies = [
+        devDependencies = [
           {
-            name = "prost-build";
-            packageId = "prost-build";
+            name = "serde_json";
+            packageId = "serde_json";
           }
           {
-            name = "tonic-build";
-            packageId = "tonic-build";
+            name = "test-generator";
+            packageId = "test-generator";
           }
         ];
 
@@ -2723,16 +2729,6 @@ rec {
           "sval" = [ "dep:sval" ];
           "value-bag" = [ "dep:value-bag" ];
         };
-      };
-      "maplit" = rec {
-        crateName = "maplit";
-        version = "1.0.2";
-        edition = "2015";
-        sha256 = "07b5kjnhrrmfhgqm9wprjw8adx6i225lqp49gasgqg74lahnabiy";
-        authors = [
-          "bluss"
-        ];
-
       };
       "matchit" = rec {
         crateName = "matchit";
@@ -6336,6 +6332,14 @@ rec {
             features = [ "rayon" "std" ];
           }
           {
+            name = "data-encoding";
+            packageId = "data-encoding";
+          }
+          {
+            name = "lazy_static";
+            packageId = "lazy_static";
+          }
+          {
             name = "prost";
             packageId = "prost";
           }
@@ -6356,12 +6360,6 @@ rec {
           {
             name = "tonic-build";
             packageId = "tonic-build";
-          }
-        ];
-        devDependencies = [
-          {
-            name = "lazy_static";
-            packageId = "lazy_static";
           }
         ];
 
---
+++ tvix/store/src/nixbase32.rs
@@ -0,0 +1,91 @@
+use data_encoding::{DecodeError, Specification};
+use lazy_static::lazy_static;
+
+/// Nixbase32Encoding wraps a data_encoding::Encoding internally.
+/// We can't use it directly, as nix also reads in characters in reverse order.
+struct Nixbase32Encoding {
+    encoding: data_encoding::Encoding,
+}
+
+lazy_static! {
+    static ref NIXBASE32: Nixbase32Encoding = nixbase32_encoding();
+}
+
+impl Nixbase32Encoding {
+    pub fn encode(&self, input: &[u8]) -> String {
+        // Reverse the input, reading in the bytes in reverse order.
+        let mut reversed = Vec::with_capacity(input.len());
+        reversed.extend(input.iter().rev());
+        self.encoding.encode(&reversed)
+    }
+
+    pub fn decode(&self, input: &[u8]) -> Result<Vec<u8>, DecodeError> {
+        // Decode first, then reverse the bytes of the output.
+        let output = self.encoding.decode(&input)?;
+
+        let mut reversed = Vec::with_capacity(output.len());
+        reversed.extend(output.iter().rev());
+        Ok(reversed)
+    }
+
+    /// Returns the decoded length of an input of length len.
+    pub fn decode_len(&self, len: usize) -> Result<usize, DecodeError> {
+        self.encoding.decode_len(len)
+    }
+
+    /// Returns the encoded length of an input of length len
+    pub fn encode_len(&self, len: usize) -> usize {
+        self.encoding.encode_len(len)
+    }
+}
+
+fn nixbase32_encoding() -> Nixbase32Encoding {
+    let mut spec = Specification::new();
+    spec.symbols.push_str("0123456789abcdfghijklmnpqrsvwxyz");
+
+    Nixbase32Encoding {
+        encoding: spec.encoding().unwrap(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::nixbase32::NIXBASE32;
+
+    #[test]
+    fn decode() {
+        let dec = NIXBASE32
+            .decode("00bgd045z0d4icpbc2yyz4gx48ak44la".as_bytes())
+            .unwrap();
+
+        assert_eq!(
+            dec,
+            [
+                0x8a, 0x12, 0x32, 0x15, 0x22, 0xfd, 0x91, 0xef, 0xbd, 0x60, 0xeb, 0xb2, 0x48, 0x1a,
+                0xf8, 0x85, 0x80, 0xf6, 0x16, 0x00,
+            ]
+        )
+    }
+
+    #[test]
+    fn encode_len() {
+        assert_eq!(NIXBASE32.encode_len(20), 32)
+    }
+
+    #[test]
+    fn decode_len() {
+        assert_eq!(NIXBASE32.decode_len(32).unwrap(), 20)
+    }
+
+    #[test]
+    fn encode() {
+        let enc = NIXBASE32.encode(
+            &[
+                0x8a, 0x12, 0x32, 0x15, 0x22, 0xfd, 0x91, 0xef, 0xbd, 0x60, 0xeb, 0xb2, 0x48, 0x1a,
+                0xf8, 0x85, 0x80, 0xf6, 0x16, 0x00,
+            ][..],
+        );
+
+        assert_eq!(enc, "00bgd045z0d4icpbc2yyz4gx48ak44la");
+    }
+}
