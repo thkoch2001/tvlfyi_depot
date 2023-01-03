6078d6eade84c7504bdeda3f8fcd7ca001e401a7
William Carroll <wpcarro@gmail.com>
2023-01-02 17:41:45 -0800

    feat(wpcarro/diogenes): Delete diogenes
    Trying to prune my monthly GCP bill, which is ~$60. Will run my website as a
    stateless Google Cloud Run service and see if that'll help.
    
    I still need to figure out what to do with my Quassel instance...
    
    Change-Id: I934b55029f14132af74cabde5e0ddb9e2d3bb933
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/7734
    Tested-by: BuildkiteCI
    Reviewed-by: wpcarro <wpcarro@gmail.com>

--- users/wpcarro/nixos/default.nix
+++ users/wpcarro/nixos/default.nix
@@ -3,7 +3,6 @@
 let
   inherit (depot.users.wpcarro.nixos)
     ava
-    diogenes
     kyoko
     marcus
     tarasco;
@@ -15,43 +14,6 @@ in
   kyokoSystem = systemFor kyoko;
   marcusSystem = systemFor marcus;
   tarascoSystem = systemFor ava;
-
-  # Apply terraform updates and rebuild NixOS for diogenes.
-  deploy-diogenes = pkgs.writeShellScriptBin "deploy-diogenes" ''
-    set -euo pipefail
-    readonly TF_STATE_DIR=/depot/users/wpcarro/terraform
-    rm -f $TF_STATE_DIR/*.json
-    readonly STORE_PATH="${diogenes.json}"
-    # We can't use the result symlink because terraform looks for a *.json file
-    # in the current working directory.
-    cp $STORE_PATH $TF_STATE_DIR
-
-    if [ ! -d $TF_STATE_DIR/.terraform ]; then
-      ${pkgs.terraform}/bin/terraform -chdir="$TF_STATE_DIR" init
-    fi
-
-    function cleanup() {
-      rm -f "$TF_STATE_DIR/$(basename $STORE_PATH)"
-    }
-    trap cleanup EXIT
-
-    ${pkgs.terraform}/bin/terraform -chdir="$TF_STATE_DIR" apply
-  '';
-
-  # Rebuild NixOS for diogenes without applying terraform updates.
-  rebuild-diogenes = pkgs.writeShellScriptBin "rebuild-diogenes" ''
-    set -euo pipefail
-    readonly target="root@billandhiscomputer.com"
-
-    # We need to call nix-build here on the drvPath because it may not be in
-    # /nix/store yet.
-    readonly STORE_PATH="$(nix-build ${diogenes.drvPath} --no-out-link --show-trace)"
-    nix-copy-closure --to $target ${diogenes.osPath} \
-      --gzip --use-substitutes $STORE_PATH
-
-    ssh $target 'nix-env --profile /nix/var/nix/profiles/system --set ${diogenes.osPath}'
-    ssh $target '${diogenes.osPath}/bin/switch-to-configuration switch'
-  '';
 
   meta.ci.targets = [
     "avaSystem"
