6f7a904c3cb0b84dab5c63a45d6619a944c9bd30
William Carroll <wpcarro@gmail.com>
2023-01-02 17:42:49 -0800

    feat(wpcarro/website): Prefer docker image
    Google Cloud Run uses images to define services, so:
    
    ```shell
    $ mg build :image
    $ docker load <./result
    $ docker tag website:latest gcr.io/wpcarros-infrastructure/website:latest
    $ docker push gcr.io/wpcarros-infrastructure/website:latest
    ```
    
    And then restart the service with `:latest`.
    
    TODO: Figure-out some ~sane CI solution (maybe personal Buildkite).
    Change-Id: I5734e3344779552aba7e0478321ba99610204e29

--- users/wpcarro/website/default.nix
+++ users/wpcarro/website/default.nix
@@ -23,11 +23,28 @@ let
   withBrand = contentHtml: renderTemplate ./fragments/template.html {
     inherit contentHtml;
   };
+
+  # Create a simple static file server using nginx to serve `content`.
+  nginxCfgFor = content: pkgs.writeText "nginx.conf" ''
+    user nobody nobody;
+    daemon off;
+    error_log /dev/stdout info;
+    pid /dev/null;
+    events {}
+    http {
+      server {
+        listen 8080;
+        location / {
+          root ${content};
+        }
+      }
+    }
+  '';
 in
-{
+rec {
   inherit domain renderTemplate withBrand;
 
-  root = pkgs.runCommand "wpcarro.dev" { } ''
+  content = pkgs.runCommand "wpcarro.dev" { } ''
     mkdir -p $out
 
     # /
@@ -40,4 +57,21 @@ in
     # /blog
     cp -r ${wpcarro.website.blog} $out/blog
   '';
+
+  # Create a docker image suitable for Google Cloud Run (to save costs).
+  image = pkgs.dockerTools.buildLayeredImage {
+    name = "website";
+    tag = "latest";
+    contents = [ pkgs.fakeNss ];
+    extraCommands = ''
+      mkdir -p tmp/nginx_client_body
+      mkdir -p var/log/nginx
+    '';
+    config = {
+      Cmd = [ "${pkgs.nginx}/bin/nginx" "-c" (nginxCfgFor content) ];
+      ExposedPorts = { "8080/tcp" = { }; };
+    };
+  };
+
+  meta.ci.targets = [ "root" "image" ];
 }
