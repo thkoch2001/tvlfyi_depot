13354195f2b0d17e42527178e08f3e896d7f2aa4
Vincent Ambo <mail@tazj.in>
2023-01-04 14:08:15 +0300

    WIP feat(tvix/cli): implement builtins.derivationStrict
    Change-Id: I0db69dcf12716180de0eb0b126e3da4683712966

---
+++ tvix/cli/src/derivation.rs
@@ -0,0 +1,158 @@
+//! Implements `builtins.derivation`, the core of what makes Nix build packages.
+
+use tvix_derivation::Derivation;
+use tvix_eval::builtin_macros::builtins;
+use tvix_eval::{CoercionKind, ErrorKind, Value, VM};
+
+// Constants used for strangely named fields in derivation inputs.
+
+const STRUCTURED_ATTRS: &'static str = "__structuredAttrs";
+const IGNORE_NULLS: &'static str = "__ignoreNulls";
+
+#[builtins]
+mod derivation_builtins {
+    use super::*;
+
+    use std::collections::BTreeMap;
+    use tvix_derivation::{Hash, Output};
+    use tvix_eval::NixList;
+
+    /// Helper function for populating the `drv.outputs` field from a
+    /// manually specified set of outputs, instead of the default
+    /// `outputs`.
+    fn populate_outputs(
+        vm: &mut VM,
+        outputs: &mut BTreeMap<String, Output>,
+        arg: NixList,
+    ) -> Result<(), ErrorKind> {
+        for output in arg {
+            let output_name = output.force(vm)?.to_str()?;
+
+            if output_name.as_str() == "drv" {
+                todo!("error: invalid output name")
+            }
+
+            // TODO: further validate output name
+            if let Some(_) = outputs.insert(output_name.to_string(), Default::default()) {
+                todo!("error: duplicate output")
+            }
+        }
+
+        Ok(())
+    }
+
+    /// Strictly construct a Nix derivation from the supplied arguments.
+    ///
+    /// This is considered an internal function, users usually want to
+    /// use the higher-level `builtins.derivation` instead.
+    #[builtin("derivationStrict")]
+    fn builtin_derivation_strict(vm: &mut VM, input: Value) -> Result<Value, ErrorKind> {
+        let input = input.to_attrs()?;
+        let _name = input.select_required("name")?.force(vm)?.to_str()?;
+
+        // Check whether attributes should be passed as a JSON file.
+        // TODO: the JSON serialisation has to happen here.
+        if let Some(sa) = input.select(STRUCTURED_ATTRS) {
+            if sa.force(vm)?.as_bool()? {
+                return Err(ErrorKind::NotImplemented(STRUCTURED_ATTRS));
+            }
+        }
+
+        // Check whether null attributes should be ignored or passed through.
+        let ignore_nulls = match input.select(IGNORE_NULLS) {
+            Some(b) => b.force(vm)?.as_bool()?,
+            None => false,
+        };
+
+        let mut drv = Derivation::default();
+        drv.outputs.insert("out".to_string(), Default::default());
+
+        // Check for fixed-output derivation keys.
+        match (input.select("outputHash"), input.select("outputHashAlgo")) {
+            (Some(hash), Some(algo)) => match drv.outputs.get_mut("out") {
+                None => todo!("fixed-output & multiple outputs is an error"),
+                Some(out) => {
+                    out.hash = Some(Hash {
+                        algo: algo
+                            .force(vm)?
+                            .coerce_to_string(CoercionKind::Strong, vm)?
+                            .to_string(),
+
+                        digest: hash
+                            .force(vm)?
+                            .coerce_to_string(CoercionKind::Strong, vm)?
+                            .to_string(),
+                    });
+                }
+            },
+
+            (Some(_), None) | (None, Some(_)) => todo!("error: missing FOD fields"),
+            (None, None) => {}
+        }
+        // TODO: outputHashMode? :thonking:
+
+        for (name, value) in input.into_iter_sorted() {
+            if ignore_nulls && matches!(*value.force(vm)?, Value::Null) {
+                continue;
+            }
+
+            let val_str = value
+                .force(vm)?
+                .coerce_to_string(CoercionKind::Strong, vm)?
+                .to_string();
+
+            match name.as_str() {
+                IGNORE_NULLS => continue,
+
+                // Command line arguments to the builder.
+                "args" => {
+                    let args = value.to_list()?;
+                    for arg in args {
+                        drv.arguments.push(arg.force(vm)?.to_str()?.to_string());
+                    }
+                    continue;
+                }
+
+                // Explicitly specified drv outputs (instead of default [ "out" ])
+                "outputs" => {
+                    let outputs = value.to_list()?;
+                    populate_outputs(vm, &mut drv.outputs, outputs)?;
+                }
+
+                "builder" => {
+                    drv.builder = val_str.clone();
+                }
+
+                "system" => {
+                    drv.system = val_str.clone();
+                }
+
+                _ => {}
+            }
+
+            // Most of these are also added to the builder's environment in "raw" form.
+            if let Some(_) = drv.environment.insert(name.to_string(), val_str) {
+                todo!("error: duplicate env in drv yada yada")
+            }
+        }
+
+        // TODO: handle reference scanning for contexts
+
+        todo!()
+    }
+
+    /// Construct a Nix derivation from the supplied arguments.
+    ///
+    /// Calling this function immediately causes a build to start.
+    #[builtin("derivation")]
+    fn builtin_derivation(_vm: &mut VM, _args: Value) -> Result<Value, ErrorKind> {
+        Err(ErrorKind::NotImplemented("builtins.derivation"))
+    }
+}
+
+pub fn derivation_builtins() -> Vec<(&'static str, Value)> {
+    derivation_builtins::builtins()
+        .into_iter()
+        .map(|builtin| (builtin.name(), Value::Builtin(builtin)))
+        .collect()
+}
