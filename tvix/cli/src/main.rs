bb9780f199f4c75b7de956b015140ebe6b4fa28c
Vincent Ambo <mail@tazj.in>
2023-01-04 14:08:15 +0300

    WIP feat(tvix/cli): implement builtins.derivationStrict
    Change-Id: I0db69dcf12716180de0eb0b126e3da4683712966

--- tvix/cli/src/main.rs
+++ tvix/cli/src/main.rs
@@ -1,3 +1,4 @@
+mod derivation;
 mod known_paths;
 mod nix_compat;
 mod refscan;
@@ -10,7 +11,7 @@ use clap::Parser;
 use known_paths::KnownPaths;
 use rustyline::{error::ReadlineError, Editor};
 use tvix_eval::observer::{DisassemblingObserver, TracingObserver};
-use tvix_eval::Value;
+use tvix_eval::{Builtin, BuiltinArgument, Value, VM};
 
 #[derive(Parser)]
 struct Args {
@@ -53,8 +54,9 @@ fn interpret(code: &str, path: Option<PathBuf>, args: &Args, explain: bool) -> b
     let mut eval = tvix_eval::Evaluation::new_impure(code, path);
     let known_paths: Rc<RefCell<KnownPaths>> = Default::default();
 
-    eval.io_handle = Box::new(nix_compat::NixCompatIO::new(known_paths));
+    eval.io_handle = Box::new(nix_compat::NixCompatIO::new(known_paths.clone()));
     eval.nix_path = args.nix_search_path.clone();
+    eval.builtins.extend(derivation::derivation_builtins(known_paths));
 
     let source_map = eval.source_map();
     let result = {
