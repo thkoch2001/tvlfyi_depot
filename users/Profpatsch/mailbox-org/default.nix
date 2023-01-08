48686ca0d60e5be1d909ee9921ce66152367eb2d
Profpatsch <mail@profpatsch.de>
2023-01-08 23:49:32 +0100

    feat(users/Profpatsch/mailbox-org): Set up passing of sieve-test
    Implement a parser for tools, and instantiate once for
    arglib-netencode arguments (parsed by the new netencode parser) and
    one just from the PATH for testing from the repl.
    
    Change-Id: Id0cf264100123a87700880c7230d68426224fd0d
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/7798
    Reviewed-by: Profpatsch <mail@profpatsch.de>
    Tested-by: BuildkiteCI

--- users/Profpatsch/mailbox-org/default.nix
+++ users/Profpatsch/mailbox-org/default.nix
@@ -2,18 +2,26 @@
 
 let
 
-  cas-serve = depot.users.Profpatsch.writers.writeHaskellInteractive "mailbox-org"
-    {
-      libraries = [
-        depot.users.Profpatsch.my-prelude
-        depot.users.Profpatsch.execline.exec-helpers-hs
-        pkgs.haskellPackages.aeson
-        pkgs.haskellPackages.http-conduit
-        pkgs.haskellPackages.aeson-better-errors
+  cas-serve =
+    lib.pipe ./MailboxOrg.hs [
+      (depot.users.Profpatsch.writers.writeHaskellInteractive "mailbox-org"
+        {
+          libraries = [
+            depot.users.Profpatsch.my-prelude
+            depot.users.Profpatsch.execline.exec-helpers-hs
+            depot.users.Profpatsch.arglib.netencode.haskell
+            pkgs.haskellPackages.aeson
+            pkgs.haskellPackages.http-conduit
+            pkgs.haskellPackages.aeson-better-errors
 
-      ];
-      ghcArgs = [ "-threaded" ];
-    } ./MailboxOrg.hs;
+          ];
+          ghcArgs = [ "-threaded" ];
+        })
+      (depot.users.Profpatsch.arglib.netencode.with-args {
+        BINS = depot.nix.getBins pkgs.dovecot_pigeonhole [ "sieve-test" ];
+      })
+    ];
+
 
 in
 cas-serve
