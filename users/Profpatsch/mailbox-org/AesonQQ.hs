3b60bfab4123f4b1f3cd3bbd3ed2e952750f4300
Profpatsch <mail@profpatsch.de>
2023-01-15 21:20:40 +0100

    feat(users/Profpatsch/mailbox-org): add simple request json example
    Adds a simple json quasiquoter thingy.
    
    Json can be sent to the `/mailfilter?action=update` endpoint.
    
    Change-Id: Iba80c2ab69178e431519933c4a01cd68aaa9f637

---
+++ users/Profpatsch/mailbox-org/AesonQQ.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE TemplateHaskellQuotes #-}
+
+module AesonQQ where
+
+import Data.Aeson qualified as Json
+import Data.Either qualified as Either
+import MyPrelude
+import PyF qualified
+import PyF.Internal.QQ qualified as PyFConf
+
+aesonQQ =
+  PyF.mkFormatter
+    "aesonQQ"
+    PyF.defaultConfig
+      { PyFConf.delimiters = Just ('|', '|'),
+        PyFConf.postProcess = \exp -> do
+          -- TODO: this does not throw an error at compilation time if the json does not parse
+          [|
+            case Json.eitherDecodeStrict' @Json.Value $ textToBytesUtf8 $ stringToText $(exp) of
+              Left err -> error err
+              Right a -> a
+            |]
+      }
