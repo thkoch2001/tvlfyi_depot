f8cb13bf54414789fdc39200538701cfd2e543b9
Vincent Ambo <mail@tazj.in>
2023-01-27 15:39:07 +0300

    WIP chore(ops/modules): serve new corp website structure
    Change-Id: I4e36f7d3e2a43c6e661025ce9b3113a365f5e619

--- ops/modules/www/tvl.su.nix
+++ ops/modules/www/tvl.su.nix
@@ -13,6 +13,14 @@
       forceSSL = true;
 
       extraConfig = ''
+        location /ru {
+          index index-ru.html;
+        }
+
+        location /en {
+          index index-en.html;
+        }
+
         add_header Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" always;
       '';
     };
