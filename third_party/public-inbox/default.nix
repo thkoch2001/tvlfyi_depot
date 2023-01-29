91146b204ff7a47eb67d6fb18bbcad0e1d3bbecd
Vincent Ambo <mail@tazj.in>
2023-01-29 13:44:13 +0300

    feat(3p/public-inbox): always set list-id when ingesting in watch
    There is a code path in public-inbox that will (under certain
    conditions) set the expected list-id as derived from the recipient
    mail address (so in our case depot@tvl.su -> `List-ID: depot.tvl.su`).
    
    However, when/how this triggers seems to be particular to the code
    path taken based on certain config settings. I couldn't fully figure
    it out, and to be honest I don't really know Perl, and I don't think
    this warrants a super-high-effort investigation.
    
    For that reason, this patches the appropriate line in
    public-inbox-watch to always trigger this code path regardless of what
    is going on with the email.
    
    I tested this locally with a public-inbox config that does *not* have
    a `listid` setting set, as that just adds an additional filter which
    would be a no-op in this case. All emails are ingested correctly with
    List-ID set.
    
    There might be a better place to put this in (e.g. right before the
    actual mail ingestion), if a Perl expert is interested in figuring
    this out and considers it relevant, feel free to send a CL.
    
    Note that this will not update old emails. Probably.
    
    Change-Id: I4a8a42653aa2f408a85c9301a1ee3545f0e74eed
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/7946
    Tested-by: BuildkiteCI
    Autosubmit: tazjin <tazjin@tvl.su>
    Reviewed-by: flokli <flokli@flokli.de>

---
+++ third_party/public-inbox/default.nix
@@ -0,0 +1,9 @@
+{ pkgs, ... }:
+
+pkgs.public-inbox.overrideAttrs (old: {
+  patches = (old.patches or [ ]) ++ [
+    ./0001-feat-always-set-the-List-ID-header-even-in-watch.patch
+  ];
+
+  doCheck = false; # too slow, and nixpkgs already runs them
+})
