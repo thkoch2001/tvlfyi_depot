9ee71ec0a9e9c1b3303e942e40fc128daca98a1a
Florian Klink <flokli@flokli.de>
2023-01-29 20:48:23 +0100

    feat(tvix/store): add write_nar function
    This adds a function that consumes a [proto::node::Node] pointing to the
    root of a (store) path, and writes the contents in NAR serialization to
    the passed [std::io::Write].
    
    We need this in various places:
    
     - tvix-store's calculate_nar() RPC method needs to render a NAR stream
       to get the nar hash, which is necessary to give things imported in
       the store a "NAR-based" store path.
    
     - communication with (remote) Nix (via daemon protocol) needs a NAR
       representation.
    
     - Things like nar-bridge, exposing a NAR/NARInfo HTTP interface need a
       NAR representation.
    
    Change-Id: I7fb2e0bf01814a1c09094c0e35394d9d6b3e43b6

---
+++ tvix/store/src/client.rs
@@ -0,0 +1,11 @@
+pub trait Blob: std::io::BufRead /* + std::io::Seek */ {}
+use crate::proto::Directory;
+
+pub trait StoreClient {
+    fn open_blob(&self, digest: Vec<u8>) -> std::io::Result<Box<dyn Blob>>;
+
+    // TODO: stat_blob, put_blob?
+    fn get_directory(&self, digest: Vec<u8>) -> std::io::Result<Option<Directory>>;
+
+    // TODO: put_directory
+}
