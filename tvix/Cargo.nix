90b2ed36076e284c4a161c7c85c641562036a777
Florian Klink <flokli@flokli.de>
2023-01-29 20:48:23 +0100

    feat(tvix/store): add write_nar function
    This adds a function that consuming a [proto::node::Node] pointing to
    the root of a (store) path, and writes the contents in NAR serialization
    to the passed [std::io::Write].
    
    We need this in various places:
    
     - tvix-store's calculate_nar() RPC method needs to render a NAR stream
       to get the nar hash, which is necessary to give things imported in
       the store a "NAR-based" store path.
    
     - communication with (remote) Nix (via daemon protocol) needs a NAR
       representation.
    
     - Things like nar-bridge, exposing a NAR/NARInfo HTTP interface need a
       NAR representation.
    
    Change-Id: I7fb2e0bf01814a1c09094c0e35394d9d6b3e43b6

--- tvix/Cargo.nix
+++ tvix/Cargo.nix
@@ -8105,6 +8105,10 @@ rec {
             name = "tracing-subscriber";
             packageId = "tracing-subscriber";
           }
+          {
+            name = "tvix-nar";
+            packageId = "tvix-nar";
+          }
         ];
         buildDependencies = [
           {
