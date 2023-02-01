c7da9d4ba5e504ed178a9c4b6b475cc67fb9db6e
Vincent Ambo <mail@tazj.in>
2023-02-01 12:25:57 +0300

    fix(ops/www): increase buffer memory size for auth.tvl.fyi
    Keycloak seems to have decided today that it will now send headers
    that are larger than what the nginx default configuration can handle.
    
    The numbers are a mix of made up and taken from random nginx voodoo
    posts on the internet, so they're as good a guess as anyone's.
    
    Change-Id: If037bcba48eee371cc96304b150276c669930c75

--- ops/modules/www/auth.tvl.fyi.nix
+++ ops/modules/www/auth.tvl.fyi.nix
@@ -12,6 +12,10 @@
       forceSSL = true;
 
       extraConfig = ''
+        # increase buffer size for large headers
+        proxy_buffers 8 16k;
+        proxy_buffer_size 16k;
+
         location / {
           proxy_pass http://localhost:${toString config.services.keycloak.settings.http-port};
           proxy_set_header X-Forwarded-For $remote_addr;
