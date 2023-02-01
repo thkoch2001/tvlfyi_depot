d2e091b8812257bdd8c456d1ec9e5267707b9956
Florian Klink <flokli@flokli.de>
2023-02-01 18:17:44 +0100

    fix(ops/buildkite): set default_branch explicitly
    It looks like this needs to be set for the tvix pipeline to succeed.
    
    It was set to `canon` for `tvl-kit` (not sure if manually, or some
    autodetection previously did it for us that's not present anymore).
    
    Anyways, this sets it to how it's set in the web interface, to hopefully
    fix it.
    
    Change-Id: Ic3eb60e3f421fa949a84dcdaa928823ff45f679a

--- ops/buildkite/tvl.tf
+++ ops/buildkite/tvl.tf
@@ -24,22 +24,25 @@ provider "buildkite" {
 }
 
 resource "buildkite_pipeline" "depot" {
-  name        = "depot"
-  description = "Run full CI pipeline of the depot, TVL's monorepo."
-  repository  = "https://cl.tvl.fyi/depot"
-  steps       = file("./steps-depot.yml")
+  name           = "depot"
+  description    = "Run full CI pipeline of the depot, TVL's monorepo."
+  repository     = "https://cl.tvl.fyi/depot"
+  steps          = file("./steps-depot.yml")
+  default_branch = "refs/heads/canon"
 }
 
 resource "buildkite_pipeline" "tvix" {
-  name        = "tvix"
-  description = "Tvix, an exported subset of TVL depot"
-  repository  = "https://code.tvl.fyi/depot.git:workspace=views/tvix.git"
-  steps       = file("./steps-tvix.yml")
+  name           = "tvix"
+  description    = "Tvix, an exported subset of TVL depot"
+  repository     = "https://code.tvl.fyi/depot.git:workspace=views/tvix.git"
+  steps          = file("./steps-tvix.yml")
+  default_branch = "canon"
 }
 
 resource "buildkite_pipeline" "tvl_kit" {
-  name        = "tvl-kit"
-  description = "TVL Kit, an exported subset of TVL depot"
-  repository  = "https://code.tvl.fyi/depot.git:workspace=views/kit.git"
-  steps       = file("./steps-tvl-kit.yml")
+  name           = "tvl-kit"
+  description    = "TVL Kit, an exported subset of TVL depot"
+  repository     = "https://code.tvl.fyi/depot.git:workspace=views/kit.git"
+  steps          = file("./steps-tvl-kit.yml")
+  default_branch = "canon"
 }
