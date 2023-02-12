8e865009ccd4adb4a46282d33bb243878d1abcc3
Florian Klink <flokli@flokli.de>
2023-02-12 12:03:26 +0100

    feat(tvix/store): add blobservice
    This adds a BlobService trait, and an implementation for it using sled.
    
    Change-Id: Id6bc1b629195d0b26fc503bd7d2dc9e43c41c317

---
+++ tvix/store/src/blobservice/mod.rs
@@ -0,0 +1,18 @@
+use crate::{proto, Error};
+
+mod sled;
+
+pub use self::sled::SledBlobService;
+
+/// The base trait all BlobService services need to implement.
+/// It provides information about how a blob is chunked,
+/// and allows creating new blobs by creating a BlobMeta (referring to chunks
+/// in a [ChunkService]).
+pub trait BlobService {
+    /// Retrieve chunking information for a given blob
+    fn stat(&self, req: &proto::StatBlobRequest) -> Result<Option<proto::BlobMeta>, Error>;
+
+    /// Insert chunking information for a given blob.
+    /// Implementations SHOULD make sure chunks referred do exist.
+    fn put(&self, blob_digest: &[u8], blob_meta: proto::BlobMeta) -> Result<(), Error>;
+}
