53b9a960493b9bd2bb610a3215703737e846b2ee
Florian Klink <flokli@flokli.de>
2023-02-13 16:44:26 +0100

    chore(tvix/store): mvoe NAR rendering logic into Renderer struct
    This moves the logic rendering NARs to a struct using the
    previously introduced, more granular BlobService, ChunkService and
    DirectoryService.
    
    Instead of passing them around to the helper functions, they're kept as
    members of a struct.
    
    Change-Id: Ic6d24aaad68a1fda46ce29f2cdb5f7b87f481d5c

---
+++ tvix/store/src/nar/mod.rs
@@ -0,0 +1,21 @@
+use crate::proto;
+use data_encoding::BASE64;
+use thiserror::Error;
+
+mod renderer;
+
+/// Errors that can encounter while rendering NARs.
+#[derive(Debug, Error)]
+pub enum RenderError {
+    #[error("failure talking to a backing store client: {0}")]
+    StoreError(crate::Error),
+
+    #[error("unable to find directory {}, referred from {}", BASE64.encode(.0), .1)]
+    DirectoryNotFound(Vec<u8>, String),
+
+    #[error("unable to find blob {}, referred from {}", BASE64.encode(.0), .1)]
+    BlobNotFound(Vec<u8>, String),
+
+    #[error("failure using the NAR writer: {0}")]
+    NARWriterError(std::io::Error),
+}
