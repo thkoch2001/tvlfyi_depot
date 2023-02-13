4c3deace5ec1699636b452d6e4a65f198b052bb7
Florian Klink <flokli@flokli.de>
2023-02-13 16:47:22 +0100

    feat(tvix/store): add NARCalculationService trait and impl
    This adds a NARCalculationService trait, which will take a root node,
    and provide a proto::CalculateNarResponse in return.
    
    It also adds a NonCachingNARCalculationService implementation, that will
    simply always render the NAR in memory to calculate the size and sha256,
    without any caching.
    
    Change-Id: Id1ffb18559212fa6001f70f2634bbc3dfd0aa343

--- tvix/store/src/nar/mod.rs
+++ tvix/store/src/nar/mod.rs
@@ -2,7 +2,10 @@ use crate::proto;
 use data_encoding::BASE64;
 use thiserror::Error;
 
+mod non_caching_calculation_service;
 mod renderer;
+
+pub use non_caching_calculation_service::NonCachingNARCalculationService;
 
 /// Errors that can encounter while rendering NARs.
 #[derive(Debug, Error)]
@@ -18,4 +21,12 @@ pub enum RenderError {
 
     #[error("failure using the NAR writer: {0}")]
     NARWriterError(std::io::Error),
+}
+
+/// The base trait for something calculating NARs, and returning their size and sha256.
+pub trait NARCalculationService {
+    fn calculate_nar(
+        &self,
+        root_node: proto::node::Node,
+    ) -> Result<proto::CalculateNarResponse, RenderError>;
 }
