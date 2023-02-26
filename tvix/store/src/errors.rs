c312e0765b79f4154d64677b6ab39085dca5877f
Florian Klink <flokli@flokli.de>
2023-02-26 22:31:50 +0100

    refactor(tvix/store): move blob splitting into a BlobWriter struct
    This will moves the chunking-as-we-receive logic that so far only lived
    in grpc_blobservice_wrapper.rs into a generic BlobWriter.
    
    Change-Id: Ief7d1bda3c6280129f7139de3f6c4174be2ca6ea

--- tvix/store/src/errors.rs
+++ tvix/store/src/errors.rs
@@ -1,5 +1,6 @@
 use std::sync::PoisonError;
 use thiserror::Error;
+use tonic::Status;
 
 /// Errors related to communication with the store.
 #[derive(Debug, Error)]
@@ -14,5 +15,23 @@ pub enum Error {
 impl<T> From<PoisonError<T>> for Error {
     fn from(value: PoisonError<T>) -> Self {
         Error::StorageError(value.to_string())
+    }
+}
+
+impl From<Error> for std::io::Error {
+    fn from(value: Error) -> Self {
+        match value {
+            Error::InvalidRequest(msg) => Self::new(std::io::ErrorKind::InvalidInput, msg),
+            Error::StorageError(msg) => Self::new(std::io::ErrorKind::Other, msg),
+        }
+    }
+}
+
+impl From<Error> for Status {
+    fn from(value: Error) -> Self {
+        match value {
+            Error::InvalidRequest(msg) => Status::invalid_argument(msg),
+            Error::StorageError(msg) => Status::data_loss(format!("storage error: {}", msg)),
+        }
     }
 }
