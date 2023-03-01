2c04c76e3d70ab1d8fce8db5916a5cfd067ef6d3
sterni <sternenseemann@systemli.org>
2023-03-01 15:15:11 +0100

    fix(3p/overlays/tvl): use our patched Nix 2.3 for nix-serve
    This should avoid having two builds of Nix 2.3 in the closure of whitby
    and sanduny.
    
    Change-Id: Id4b8d34da73d3f579c97adcda44df26992290764

--- third_party/overlays/tvl.nix
+++ third_party/overlays/tvl.nix
@@ -105,7 +105,7 @@ depot.nix.readTree.drvTargets {
 
   # nix-serve does not work with nix 2.4
   # https://github.com/edolstra/nix-serve/issues/28
-  nix-serve = super.nix-serve.override { nix = super.nix_2_3; };
+  nix-serve = super.nix-serve.override { nix = self.nix_2_3; };
 
   # Avoid builds of mkShell derivations in CI.
   mkShell = super.lib.makeOverridable (args: (super.mkShell args).overrideAttrs (_: {
