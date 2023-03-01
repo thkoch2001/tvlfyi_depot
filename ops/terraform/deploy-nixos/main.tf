cd3bedea1491f909a730a96b8cf685b2c0455b30
Florian Klink <flokli@flokli.de>
2023-03-01 15:11:01 +0100

    feat(ops/terraform): add trigger to deploy-nixos, remove target_name
    This allows passing in custom triggers to trigger a (re)deploy.
    
    For example, a caller can put an AWS instance ID into the triggers to
    cause a redeploy whenever the instance ID has changed.
    
    The `target_name` terraform variable was doing something similar, but
    `triggers` is more generic, allowing multiple triggers, without having
    to stringify them.
    
    Change-Id: I94ce787a50830b87b6f53c08e042e4abe4036bdd

--- ops/terraform/deploy-nixos/main.tf
+++ ops/terraform/deploy-nixos/main.tf
@@ -9,11 +9,6 @@
 # machine that runs `terraform apply`, then copied and activated on
 # the target machine using `nix-copy-closure`.
 
-variable "target_name" {
-  description = "unique name of the target machine"
-  type        = string
-}
-
 variable "closure" {
   description = "attribute set path pointing to the NixOS system closure"
   type        = string
@@ -43,6 +38,12 @@ variable "target_user_key" {
   description = "SSH key to use for connecting to the target"
   type        = string
   sensitive   = true
+}
+
+variable "triggers" {
+  type        = map(string)
+  description = "Triggers for deploy"
+  default     = {}
 }
 
 # Fetch the derivation hash for the NixOS system.
@@ -96,12 +97,11 @@ resource "null_resource" "nixos_deploy" {
     ]
   }
 
-  triggers = {
+  triggers = merge({
     nixos_drv      = data.external.nixos_system.result.drv
     closure        = var.closure
     target_address = var.target_address
-    target_name    = var.target_name
-  }
+  }, var.triggers)
 }
 
 output "nixos_drv" {
