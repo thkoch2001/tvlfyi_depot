8b839adc58113e9672da54dbe094b07ce0ae6ee2
Luke Granger-Brown <git@lukegb.com>
2023-03-12 03:29:39 +0000

    users/lukegb/keys: -porcorosso-wsl +lukegb-build +lukegb-ca
    Change-Id: Ia03d1449eca4c23728ae64be8c0e9f7397c10c2e

--- users/lukegb/keys.nix
+++ users/lukegb/keys.nix
@@ -3,8 +3,9 @@
 
 rec {
   termius = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINytpHct7PLdLNp6MoaOPP7ccBPUQKymVNMqix//Wt1f";
-  porcorosso-wsl = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMhQ3yjf59eQjOfVXzXz5u8BS5c6hdL1yY8GqccaIjx3";
   porcorosso-nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILid+1rq3k3k7Kbaw8X63vrPrQdanH55TucQwp3ZWfo+";
   clouvider-lon01-nix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINQU7Y+Ha5m0ebwUjA55xXT/xbWZAWx1fVNFufle+vQj";
-  all = [ termius porcorosso-wsl porcorosso-nixos clouvider-lon01-nix ];
+  lukegb-build = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICESF0H+OCxY/CfyG9VjM6iJe+VbYc4NmGjRrwPCHaD9";
+  lukegb-ca = "cert-authority,principals=\"lukegb\" ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEqNOwlR7Qa8cbGpDfSCOweDPbAGQOZIcoRgh6s/J8DR";
+  all = [ termius porcorosso-nixos clouvider-lon01-nix lukegb-build lukegb-ca ];
 }
