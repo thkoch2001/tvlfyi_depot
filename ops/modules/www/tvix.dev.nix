b023b25d0f9d16c5b81d0b0bdd6a7f7db2456d54
Vincent Ambo <mail@tazj.in>
2023-03-14 22:27:30 +0300

    feat(ops): serve Tvix website & docs on (docs.)tvix.dev
    Change-Id: I198ea197867f9b9a48e51665d0665f722202e02e

---
+++ ops/modules/www/tvix.dev.nix
@@ -0,0 +1,39 @@
+{ depot, ... }:
+
+{
+  imports = [
+    ./base.nix
+  ];
+
+  config = {
+    services.nginx.virtualHosts."tvix.dev" = {
+      serverName = "tvix.dev";
+      enableACME = true;
+      forceSSL = true;
+      root = depot.tvix.website;
+
+      extraConfig = ''
+        add_header Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" always;
+      '';
+    };
+
+    services.nginx.virtualHosts."docs.tvix.dev" = {
+      serverName = "docs.tvix.dev";
+      enableACME = true;
+      forceSSL = true;
+
+      extraConfig = ''
+        add_header Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" always;
+
+        location = / {
+          # until we have a better default page here
+          return 301 https://docs.tvix.dev/rust/tvix_eval/index.html;
+        }
+
+        location /rust/ {
+          alias ${depot.tvix.rust-docs};
+        }
+      '';
+    };
+  };
+}
