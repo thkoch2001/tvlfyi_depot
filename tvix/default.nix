a4bc648401c6aeb6429de87a866a7599073e9d30
Vincent Ambo <mail@tazj.in>
2023-03-14 13:47:20 +0300

    feat(tvix): add build target for generating LSIF files
    ... though nothing happens with these, yet.
    
    Change-Id: Icacd26cfd18f2b5cd6eeab59005c4eba3a22b8ea

--- tvix/default.nix
+++ tvix/default.nix
@@ -58,6 +58,34 @@ in
     ];
   };
 
+  # Build an LSIF file for the Tvix Cargo project, which can be
+  # uploaded to SourceGraph.
+  lsif = pkgs.stdenv.mkDerivation {
+    name = "tvix-lsif";
+    src = depot.third_party.gitignoreSource ./.;
+
+    cargoDeps = pkgs.rustPlatform.importCargoLock {
+      lockFile = ./Cargo.lock;
+      outputHashes = {
+        "test-generator-0.3.0" = "08brp3qqa55hijc7xby3lam2cc84hvx1zzfqv6lj7smlczh8k32y";
+        "tonic-mock-0.1.0" = "0lwa03hpp0mxa6aa1zv5w68k61y4hccfm0q2ykyq392fwal8vb50";
+        "wu-manber-0.1.0" = "02byhfiw41mlgr1c43n2iq6jw5sbyn8l1acv5v71a07h5l18q0cy";
+      };
+    };
+
+    nativeBuildInputs = with pkgs; [
+      cargo
+      rust-analyzer
+      rustPlatform.cargoSetupHook
+      rustc
+    ];
+
+    buildPhase = ''
+      mkdir $out
+      rust-analyzer lsif . > $out/dump.lsif
+    '';
+  };
+
   export = (pkgs.runCommandLocal "export-tvix" { } ''
     echo "carrier for repo export extra-step" > $out
   '').overrideAttrs (_: {
@@ -68,5 +96,5 @@ in
     };
   });
 
-  meta.ci.targets = [ "shell" "export" ];
+  meta.ci.targets = [ "shell" "export" "lsif" ];
 }
