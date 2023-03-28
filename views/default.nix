93b9c800730f43312cbcac2632b76a43213da0f2
Vincent Ambo <mail@tazj.in>
2023-03-28 10:40:20 +0300

    feat(views): export tvl-kit view
    Change-Id: I739adb7d32d03294f8ec9962c3d93e35842af83b

--- views/default.nix
+++ views/default.nix
@@ -7,6 +7,12 @@ let
     remote = "git@github.com:tvlfyi/tvix.git";
     ref = "refs/heads/canon";
   };
+
+  export-kit = depot.tools.releases.filteredGitPush {
+    filter = ":workspace=views/kit";
+    remote = "git@github.com:tvlfyi/kit.git";
+    ref = "refs/heads/canon";
+  };
 in
 pkgs.runCommandLocal "export-views" { }
   ''
@@ -14,7 +20,7 @@ pkgs.runCommandLocal "export-views" { }
   ''.overrideAttrs
   (_: {
     meta.ci.extraSteps = {
-      inherit export-tvix;
+      inherit export-tvix export-kit;
     };
   })
 
