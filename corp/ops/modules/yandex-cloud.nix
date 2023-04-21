f46c64ac6c8eddb4c4f07694491f0236ee04ed74
Vincent Ambo <mail@tazj.in>
2023-04-21 19:56:48 +0300

    WIP feat(corp/ops): add NixOS profile for Yandex Cloud machines
    Change-Id: I57dcb7fcfa6872a28855dc1347f73a6db3c56828

---
+++ corp/ops/modules/yandex-cloud.nix
@@ -0,0 +1,64 @@
+# Profile for virtual machines on Yandex Cloud, intended for disk
+#images.
+#
+# https://cloud.yandex.com/en/docs/compute/operations/image-create/custom-image
+#
+# TODO(tazjin): Upstream to nixpkgs once it works well.
+{ config, lib, pkgs, modulesPath, ... }:
+
+let
+  cfg = config.virtualisation.yandexCloud;
+in
+{
+  imports = [
+    "${modulesPath}/profiles/headless.nix"
+  ];
+
+  options = {
+    virtualisation.yandexCloud.rootPartitionUuid = with lib; mkOption {
+      type = types.str;
+      default = "C55A5EE2-E5FA-485C-B3AE-CC928429AB6B";
+
+      description = ''
+        UUID to use for the root partition of the disk image. Yandex
+        Cloud requires that root partitions are mounted by UUID.
+
+        Most users do not need to set this to a non-default value.
+      '';
+    };
+  };
+
+  config = {
+    fileSystems."/" = {
+      device = "/dev/disk/by-uuid/${cfg.rootPartitionUuid}";
+      fsType = "ext4";
+    };
+
+    boot.kernelModules = [
+      "virtio-net"
+      "virtio-blk"
+      "virtio-pci"
+      "virtiofs"
+    ];
+
+    boot.kernelParams = [
+      "console=ttyS0"
+    ];
+
+    environment.etc.securetty = {
+      text = "ttyS0";
+      mode = "0644";
+    };
+
+    systemd.services."serial-getty@ttyS0".enable = true;
+
+    services.openssh.enable = true;
+    services.cloud-init.enable = true;
+
+    system.build.yandexCloudImage = import (pkgs.path + "/nixos/lib/make-disk-image.nix") {
+      inherit lib config pkgs;
+      format = "qcow2";
+      rootGPUID = cfg.rootPartitionUuid;
+    };
+  };
+}
