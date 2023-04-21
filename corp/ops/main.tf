55c9f4a8036b7eb4cc56b700d5d975f47029337a
Vincent Ambo <mail@tazj.in>
2023-04-21 17:18:33 +0300

    feat(corp/ops): bootstrap separate corp terraform config
    Doesn't actually contain any configuration yet, just setting up TF
    with the right providers and so on.
    
    Change-Id: Ia7128dd977b4ff69eebaa36c6cad6ac104cafcdb
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/8492
    Tested-by: BuildkiteCI
    Reviewed-by: tazjin <tazjin@tvl.su>

---
+++ corp/ops/main.tf
@@ -0,0 +1,10 @@
+# Terraform configuration for TVL corp infrastructure (on Yandex
+# Cloud).
+
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
