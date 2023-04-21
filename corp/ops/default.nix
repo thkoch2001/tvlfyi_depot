34067a1672f72a188e8b2bc7424d97e79f447a6e
Vincent Ambo <mail@tazj.in>
2023-04-21 17:38:09 +0300

    feat(corp/ops): add yc-cli
    Change-Id: If6578693a5d5ef49d059735eeade3bebf13c4d16

--- corp/ops/default.nix
+++ corp/ops/default.nix
@@ -12,10 +12,26 @@ depot.nix.readTree.drvTargets rec {
     src = lib.cleanSource ./.;
   };
 
+  # Yandex Cloud CLI
+  yc-cli = pkgs.stdenv.mkDerivation rec {
+    pname = "yc-cli";
+    version = "0.104.0";
+
+    src = pkgs.fetchurl {
+      url = "https://storage.yandexcloud.net/yandexcloud-yc/release/${version}/linux/amd64/yc";
+      sha256 = "sha256:1k1dfqqmpy1kdzgg2d8byhsfpfh3fxrckpbvffwngb712isvqpdb";
+    };
+
+    phases = [ "installPhase" ];
+    installPhase = "install -D $src $out/bin/yc";
+  };
+
   deps = depot.tools.depot-deps.overrideDeps {
     tf-yandex = {
       attr = "corp.ops.terraform";
       cmd = "terraform";
     };
+
+    yc.attr = "corp.ops.yc-cli";
   };
 }
