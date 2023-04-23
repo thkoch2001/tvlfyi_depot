7b133987a443423f41bc405546cb8878e27c710a
Griffin Smith <grfn@gws.fyi>
2023-04-23 00:06:43 -0400

    feat(grfn/yeren): enable avahi
    this is for a... party
    
    Change-Id: Ida5e0effb071ac39194cabec507eef58de2bf279
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/8506
    Reviewed-by: grfn <grfn@gws.fyi>
    Tested-by: BuildkiteCI
    Autosubmit: grfn <grfn@gws.fyi>

--- users/grfn/system/system/machines/yeren.nix
+++ users/grfn/system/system/machines/yeren.nix
@@ -19,6 +19,11 @@
 
   time.timeZone = "America/New_York";
 
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+  };
+
   boot = {
     initrd = {
       availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
