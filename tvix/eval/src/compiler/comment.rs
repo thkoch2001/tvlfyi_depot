1f694416727c6a8fc6a6da690ccdedeb5e9c67c3
Johannes Kirschbauer <hsjobeki@gmail.com>
2023-04-28 13:39:32 +0200

    feat(tvix/eval): Add doc-comments to lambda values
    Adds doc-comments to lambda values, can be printed by the explain function.
    tvix-repl> :d /**Doc-comment*/<lambda>
    "Doc-comment"
    
    Known Issues: Partially applied functions do not work yet as expected.
    
    Change-Id: Ic7eb836c53e02d1de5b93f0b667fbb828e5cd2df

---
+++ tvix/eval/src/compiler/comment.rs
@@ -0,0 +1,30 @@
+use rnix::ast::{self, AstToken};
+
+/// Implements functions to distinguish
+/// a comment from doc-comment and retrieve the content as raw string
+pub trait DocComment {
+    fn is_doc(&self) -> bool;
+    fn doc_text(&self) -> &str;
+}
+
+impl DocComment for ast::Comment {
+    /// Check whether this is a doc-comment
+    /// Format: /** */
+    fn is_doc(&self) -> bool {
+        let text = self.syntax().text();
+        // Check if the comment starts with
+        // /** but not with /**/ which is a regular comment
+        text.starts_with(r#"/**"#) && self.text().starts_with("*")
+    }
+    /// following Comment::text
+    /// but for doc-comments
+    /// .is_doc() should be called before to check
+    /// whether this is a doc-comment; otherwise this just yields the raw-text
+    fn doc_text(&self) -> &str {
+        let text = self.syntax().text();
+        text.strip_prefix(r#"/**"#)
+            .unwrap_or(text)
+            .strip_suffix(r#"*/"#)
+            .unwrap_or(text)
+    }
+}
