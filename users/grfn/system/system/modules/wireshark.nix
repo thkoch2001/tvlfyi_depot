37b0515f120003d1358ebb5b096b01519b923a3c
Griffin Smith <root@gws.fyi>
2023-05-05 17:26:05 -0400

    fix(grfn/system): Use the qt-ui wireshark package
    Change-Id: I97cf04ae6a950fa84262e94d297775edc5273852

--- users/grfn/system/system/modules/wireshark.nix
+++ users/grfn/system/system/modules/wireshark.nix
@@ -1,6 +1,9 @@
 { config, lib, pkgs, ... }:
 
 {
-  programs.wireshark.enable = true;
+  programs.wireshark = {
+    enable = true;
+    package = pkgs.wireshark;
+  };
   users.users.grfn.extraGroups = [ "wireshark" ];
 }
