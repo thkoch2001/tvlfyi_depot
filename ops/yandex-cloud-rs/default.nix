aea49fb8dc06fe0d7055c8ced3197fcfa43c5d5b
Vincent Ambo <mail@tazj.in>
2023-05-19 12:16:41 +0300

    chore(ops/yandex-cloud-rs): bump API definitions (2023-05-19)
    Change-Id: I0c4e77587b9fac14017449eb6a4630265b07950e

--- ops/yandex-cloud-rs/default.nix
+++ ops/yandex-cloud-rs/default.nix
@@ -4,8 +4,8 @@ let
   protoSrc = pkgs.fetchFromGitHub {
     owner = "yandex-cloud";
     repo = "cloudapi";
-    rev = "67589e7503bd8997b7aa27af86a81fb6b9eec655";
-    sha256 = "0d8w98h58xzzcjq9s3y949gv5b4z4r05vgpz3n22asyfrfx6x6kf";
+    rev = "ca3d23bc2e9e56042a23a6cf0bade98b21aa0f31";
+    sha256 = "1qsmycd0nkbskb9dj36l45914b0hpr6vpymn47vwa6qsfqraxj1m";
   };
 in
 pkgs.rustPlatform.buildRustPackage rec {
