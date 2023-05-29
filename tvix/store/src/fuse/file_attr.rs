a3e719154ce60a2c1f1759b56a8824b3a8cef914
Florian Klink <flokli@flokli.de>
2023-05-29 15:11:31 +0300

    feat(tvix/store/fuse): hacky initial implementation
    This is a first implementation of a FUSE filesystem, mounting tvix-store
    to a given location.
    
    This is mostly meant as one additional lens into a store, and could be
    used for builds. It's not meant to be used as a general-purpose thing.
    
    It still has some rough edges:
    
     - The code is a bit messy could use some better and more idiomatic
       error handling, or documentation on why it's fine to unwrap.
     - Some of the code could be factored out into helper functions.
     - It doesn't implement open/close, so it doesn't use file handles.
       Which means, we need to open blobs for partial reads over and over
       again.
     - It doesn't implement seek, as BlobReader doesn't implement seek yet.
     - It doesn't track "lifetimes" of inodes by listening on forget,
       meaning it might hold more in memory than necessary.
     - As we don't have store composition (and a caching layer) yet,
       operations might be slow.
    
    Change-Id: Ib1812ed761dfaf6aeb548443ae939c87530b7be8

---
+++ tvix/store/src/fuse/file_attr.rs
@@ -0,0 +1,55 @@
+use std::time::SystemTime;
+
+use super::inodes::InodeData;
+use fuser::FileAttr;
+
+/// The [FileAttr] describing the root
+pub const ROOT_FILE_ATTR: FileAttr = FileAttr {
+    ino: fuser::FUSE_ROOT_ID,
+    size: 0,
+    blksize: 1024,
+    blocks: 0,
+    atime: SystemTime::UNIX_EPOCH,
+    mtime: SystemTime::UNIX_EPOCH,
+    ctime: SystemTime::UNIX_EPOCH,
+    crtime: SystemTime::UNIX_EPOCH,
+    kind: fuser::FileType::Directory,
+    perm: 0o555,
+    nlink: 0,
+    uid: 0,
+    gid: 0,
+    rdev: 0,
+    flags: 0,
+};
+
+/// for given &Node and inode, construct a [FileAttr]
+pub fn gen_file_attr(node: &InodeData, inode: u64) -> FileAttr {
+    FileAttr {
+        ino: inode,
+        size: match node {
+            InodeData::Regular(_, size, _) => *size as u64,
+            InodeData::Symlink(target) => target.len() as u64,
+            InodeData::Directory(_, ref children) => {
+                children.as_ref().expect("must be some").len() as u64 + 1
+            }
+        },
+        // TODO: play with this numbers. Does it affect read sizes?
+        blksize: 1024,
+        blocks: 0,
+        atime: SystemTime::UNIX_EPOCH,
+        mtime: SystemTime::UNIX_EPOCH,
+        ctime: SystemTime::UNIX_EPOCH,
+        crtime: SystemTime::UNIX_EPOCH,
+        kind: node.into(),
+        perm: match node {
+            InodeData::Regular(..) => 0o444,
+            InodeData::Symlink(_) => 0o444,
+            InodeData::Directory(..) => 0o555,
+        },
+        nlink: 0,
+        uid: 0,
+        gid: 0,
+        rdev: 0,
+        flags: 0,
+    }
+}
