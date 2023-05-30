9484ea3d99af304670fc175c777fce33fffc2a57
Profpatsch <mail@profpatsch.de>
2023-05-31 01:57:56 +0200

    chore(users/Profpatsch/*): more cabal maintenance
    Change-Id: Ib1714abce2815873eb50dbeac088e812fa9098ab

--- users/Profpatsch/arglib/ArglibNetencode.hs
+++ users/Profpatsch/arglib/ArglibNetencode.hs
@@ -1,20 +1,17 @@
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 
 module ArglibNetencode where
 
-import qualified Data.Attoparsec.ByteString as Atto
-import Data.Maybe (fromMaybe)
+import Data.Attoparsec.ByteString qualified as Atto
 import ExecHelpers
-import MyPrelude
-import Netencode
-import qualified System.Environment as Env
-import qualified System.Posix.Env.ByteString as ByteEnv
+import Label
+import Netencode qualified
+import PossehlAnalyticsPrelude
+import System.Posix.Env.ByteString qualified as ByteEnv
 
-arglibNetencode :: CurrentProgramName -> Maybe Text -> IO T
+arglibNetencode :: CurrentProgramName -> Maybe (Label "arglibEnvvar" Text) -> IO Netencode.T
 arglibNetencode progName mEnvvar = do
-  let envvar = mEnvvar & fromMaybe "ARGLIB_NETENCODE" & textToBytesUtf8
+  let envvar = mEnvvar <&> (.arglibEnvvar) & fromMaybe "ARGLIB_NETENCODE" & textToBytesUtf8
   ByteEnv.getEnv envvar >>= \case
     Nothing -> dieUserError progName [fmt|could not read args, envvar {envvar} not set|]
     Just bytes ->
