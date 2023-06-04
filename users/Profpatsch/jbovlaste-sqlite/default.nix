6d156c693f9a8c8c3d0b5ccdb4e2a996949f806a
Profpatsch <mail@profpatsch.de>
2023-06-04 02:34:48 +0200

    feat(users/Profpatsch/jbovlaste-sqlite): add XML parser
    nice.
    
    Change-Id: Iea90578742bfb689cd0508dbaf641c31aed577ad

--- users/Profpatsch/jbovlaste-sqlite/default.nix
+++ users/Profpatsch/jbovlaste-sqlite/default.nix
@@ -16,6 +16,7 @@ let
       pkgs.haskellPackages.pa-prelude
       pkgs.haskellPackages.pa-label
       pkgs.haskellPackages.pa-error-tree
+      pkgs.haskellPackages.pa-field-parser
       pkgs.haskellPackages.sqlite-simple
       pkgs.haskellPackages.xml-conduit
       depot.users.Profpatsch.arglib.netencode.haskell
