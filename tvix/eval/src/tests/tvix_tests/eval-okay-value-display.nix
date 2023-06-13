7aa503679e7bcd62c8b6d2af923e60fafaad80d1
sterni <sternenseemann@systemli.org>
2023-06-13 15:15:11 +0200

    fix(tvix/eval): make tvix display values like nix-instantiate(1)
    In order for the test suite we have currently to be comparable to C++
    Nix, we need to display values in the same way. This was largely the
    case except in some weird cases.
    
    * <CODE> for thunks and <CYCLE> for repeated thunks (?) are already in
      use. <CODE> formatting is tested by the oracle test suite already.
    
    * Instead of lambda, we need to use <LAMBDA>
    
    * <<primop>> and <<primop-app>> (a formatting C++ Nix uses nowhere)
      now are <PRIMOP> and <PRIMOP-APP>.
    
    We'll probably want to have a fancier display of values (in a separate
    trait) down the line. This could be used for interactive usage, e.g. the
    REPL or a potential debugger.
    
    There is a peculiarity with C++ Nix 2.3 formatting primops: import is
    considered a <<PRIMOP-APP>>, since it is internally implemented by means
    of scopedImport. This implementation detail no longer leaks in C++ Nix
    2.13 nor in Tvix.
    
    Change-Id: I1d534434b02e470bf5475b3758920ea81e3420dc

---
+++ tvix/eval/src/tests/tvix_tests/eval-okay-value-display.nix
@@ -0,0 +1,17 @@
+# Sanity check of how values are rendered by tvix vs. nix-instantiate(1).
+# Ensures that we can use this test suite to compare against C++ Nix.
+[
+  null
+  true
+  false
+  42
+  42.0
+  "foo\t\nbar"
+  /home/arthur
+  [ 1 2 3 ]
+  (x: x)
+  builtins.add
+  (builtins.substring 0 1)
+  { hello = "world"; }
+  (let ll = { car = 42; cdr = ll; }; in ll)
+]
