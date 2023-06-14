1cb5a8a2cfefb61ef8ff8d583e875b8393bc7ef8
sterni <sternenseemann@systemli.org>
2023-06-14 15:19:42 +0200

    test(tvix/eval): add test case for builtins set pointer equality
    Unsupported by Tvix at the moment. Documents b/280.
    
    Change-Id: I48844feeefa9da8ed7e5d85300d52bb5650f82d2

---
+++ tvix/eval/src/tests/tvix_tests/notyetpassing/eval-okay-builtins-set-pointer-equality.nix
@@ -0,0 +1,18 @@
+let
+  alias = builtins;
+in
+
+[
+  (builtins == builtins)
+  (alias == builtins)
+  (builtins == builtins.builtins)
+  (builtins.builtins == builtins.builtins)
+  (builtins.builtins == builtins.builtins.builtins)
+  (alias == alias)
+  (alias == builtins.builtins)
+  ([ builtins ] == [ builtins ])
+
+  # Surprisingly this only works with the set
+  ([ builtins.add ] == [ builtins.add ])
+  ({ inherit (builtins) import; } == { inherit (builtins) import; })
+]
