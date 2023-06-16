6678e768a08e993c586756ad1815a5fcf1f520b6
Vincent Ambo <mail@tazj.in>
2023-06-16 16:08:26 +0300

    feat(tazjin/secrets): initialise secrets management for my machines
    Change-Id: I93ad61f3e6e4a4a2185448ad128fa6836c81ab26
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/8825
    Reviewed-by: tazjin <tazjin@tvl.su>
    Tested-by: BuildkiteCI

---
+++ users/tazjin/secrets/secrets.nix
@@ -0,0 +1,13 @@
+let
+  myKeys = import ../keys { };
+  allKeys = [
+    # local keys
+    myKeys.tverskoy_ed25519
+    myKeys.zamalek_ed25519
+    # koptevo
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMw2ZfdNZCXCOtbQNT6hztXCIkTcO9MBrOuDqMlmGOYK root@koptevo"
+  ];
+in
+{
+  "tgsa-yandex.age".publicKeys = allKeys;
+}
