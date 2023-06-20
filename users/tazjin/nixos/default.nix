399d23eaf62a08e49d085daa7d4b4c2433e85090
Vincent Ambo <mail@tazj.in>
2023-06-20 15:11:42 +0300

    chore(tazjin/polyanka): goodbye
    This machine is now being decomissioned.
    
    Change-Id: Ib7f016c7de84dab2cdf3c071445cf830d2eccf5f
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/8838
    Reviewed-by: tazjin <tazjin@tvl.su>
    Autosubmit: tazjin <tazjin@tvl.su>
    Tested-by: BuildkiteCI

--- users/tazjin/nixos/default.nix
+++ users/tazjin/nixos/default.nix
@@ -5,7 +5,6 @@ in depot.nix.readTree.drvTargets {
   camdenSystem = systemFor depot.users.tazjin.nixos.camden;
   frogSystem = systemFor depot.users.tazjin.nixos.frog;
   tverskoySystem = systemFor depot.users.tazjin.nixos.tverskoy;
-  polyankaSystem = (depot.ops.nixos.nixosFor depot.users.tazjin.nixos.polyanka).system;
   zamalekSystem = systemFor depot.users.tazjin.nixos.zamalek;
   koptevoSystem = systemFor depot.users.tazjin.nixos.koptevo;
 }
