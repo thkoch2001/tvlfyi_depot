e5727ff5a96c824e1f40f584c53c63c7bd4670e5
Adam Joseph <adam@westernsemico.com>
2023-07-08 19:58:04 -0700

    feat(nix): add gensym, use it in mapAttrsMaybe
    This builds on ProfPatsch's `mapAttrsMaybe` proposal by addressing
    sterni's (and my) concern about `null` having special status.
    Instead of using `null` as a marker value, this commit (ab)uses Nix
    function equality to implement `gensym()` so that each call (and
    indeed nested calls) to `mapAttrsMaybe` cannot mix up or hoard each
    others' marker values.
    
    This does not implement a long-term dependence on function equality;
    we could add a `builtins.gensym` prior to eliminating function
    equality at some point in the future.
    
    Change-Id: I209bc25f2b1e8d4a6c004feca24a22751e2096ad

--- nix/mapAttrsMaybe/default.nix
+++ nix/mapAttrsMaybe/default.nix
@@ -2,20 +2,32 @@
 # Like mapAttrs, but if `null` is returned from the mapping function,
 # the element is removed from the attrset.
 #
-# mapAttrsMaybe :: (k -> v -> a) -> Attrs k v -> Attrs k a
+# mapAttrsMaybe :: (drop -> k -> v -> a) -> Attrs k v -> Attrs k a
 #
 # Examples:
-#   mapAttrsMaybe (_: v: v) { foo = null; bar = 42; }
-#   => { bar = 42; }
-#   mapAttrsMaybe (_: v: if lib.isList v then null else v)
+#   mapAttrsMaybe (drop: _: v: v) { foo = null; bar = 42; }
+#   => { bar = 42; foo = null; }
+#   mapAttrsMaybe (drop: _: v: if lib.isList v then drop else v)
 #                 { foo = []; bar = 42; baz = null; }
-#   => { bar = 42; }
+#   => { bar = 42; baz = null; }
 let
-  mapAttrsMaybe = f: attrs: lib.pipe attrs [
-    (lib.mapAttrsToList (k: v: { name = k; value = f k v; }))
-    (builtins.filter ({ name, value }: if value == null then false else true))
-    lib.listToAttrs
-  ];
+
+  # this should be upstreamed into nixpkgs as lib.trivial.gensym,
+  # and replaced with builtins.gensym at some far-future date
+  gensym = ignored: {
+    inner = _: {
+      inherit ignored;
+      touch_me_and_die = throw "you tried to force gensym, fool";
+    };
+  };
+
+  mapAttrsMaybe = f: attrs:
+    let drop = gensym true;
+    in lib.pipe attrs [
+      (lib.mapAttrsToList (k: v: { name = k; value = f drop k v; }))
+      (builtins.filter ({ name, value }: if value == drop then false else true))
+      lib.listToAttrs
+    ];
 
   inherit (depot.nix.runTestsuite)
     runTestsuite
@@ -25,21 +37,21 @@ let
 
   tests =
     let
-      mapId = _: v: v;
+      mapId = drop: _: v: v;
       idTest = it "filters out simple nulls over id" [
         (assertEq "empty"
           (mapAttrsMaybe mapId { })
           { })
         (assertEq "onlyNull"
           (mapAttrsMaybe mapId { foo = null; bar = null; })
-          { })
+          { foo = null; bar = null; })
         (assertEq "moreStuff"
           (mapAttrsMaybe mapId { foo = null; bar = 42; baz = [ "hi" ]; })
-          { bar = 42; baz = [ "hi" ]; })
+          { foo = null; bar = 42; baz = [ "hi" ]; })
       ];
-      fnTest = it "uses the mapping function to filter out stuff that is null" [
+      fnTest = it "uses the mapping function to filter out stuff that is a list" [
         (assertEq "lists"
-          (mapAttrsMaybe (_: v: if lib.isList v then null else v) { foo = [ ]; bar = 42; })
+          (mapAttrsMaybe (drop: _: v: if lib.isList v then drop else v) { foo = [ ]; bar = 42; })
           { bar = 42; })
       ];
 
@@ -53,5 +65,5 @@ in
 {
   __functor = _: mapAttrsMaybe;
 
-  meta.ci.targets = ["tests"];
+  meta.ci.targets = [ "tests" ];
 }
