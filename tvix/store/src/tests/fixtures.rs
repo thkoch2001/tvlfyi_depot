11c0d1fa0d4c98233a157393f79a6d66ab1b996a
Florian Klink <flokli@flokli.de>
2023-07-19 18:52:50 +0300

    feat(tvix/store/proto): use Bytes instead of Vec<u8>
    Makes use of https://github.com/tokio-rs/prost/pull/341, which makes our
    bytes field cheaper to clone.
    
    It's a bit annoying to configure due to
    https://github.com/hyperium/tonic/issues/908, but the workaround does
    get the job done.
    
    Change-Id: I25714600b041bb5432d3adf5859b151e72b12778

--- tvix/store/src/tests/fixtures.rs
+++ tvix/store/src/tests/fixtures.rs
@@ -8,11 +8,14 @@ pub const HELLOWORLD_BLOB_CONTENTS: &[u8] = b"Hello World!";
 pub const EMPTY_BLOB_CONTENTS: &[u8] = b"";
 
 lazy_static! {
-    pub static ref DUMMY_DIGEST: Vec<u8> = vec![
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-        0x00, 0x00,
-    ];
+    pub static ref DUMMY_DIGEST: B3Digest = {
+        let u: &[u8; 32] = &[
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00,
+        ];
+        u.into()
+    };
     pub static ref DUMMY_DATA_1: Vec<u8> = vec![0x01, 0x02, 0x03];
     pub static ref DUMMY_DATA_2: Vec<u8> = vec![0x04, 0x05];
 
@@ -33,8 +36,8 @@ lazy_static! {
     pub static ref DIRECTORY_WITH_KEEP: proto::Directory = proto::Directory {
         directories: vec![],
         files: vec![FileNode {
-            name: b".keep".to_vec(),
-            digest: EMPTY_BLOB_DIGEST.to_vec(),
+            name: b".keep".to_vec().into(),
+            digest: EMPTY_BLOB_DIGEST.clone().into(),
             size: 0,
             executable: false,
         }],
@@ -42,26 +45,26 @@ lazy_static! {
     };
     pub static ref DIRECTORY_COMPLICATED: proto::Directory = proto::Directory {
         directories: vec![DirectoryNode {
-            name: b"keep".to_vec(),
-            digest: DIRECTORY_WITH_KEEP.digest().to_vec(),
+            name: b"keep".to_vec().into(),
+            digest: DIRECTORY_WITH_KEEP.digest().into(),
             size: DIRECTORY_WITH_KEEP.size(),
         }],
         files: vec![FileNode {
-            name: b".keep".to_vec(),
-            digest: EMPTY_BLOB_DIGEST.to_vec(),
+            name: b".keep".to_vec().into(),
+            digest: EMPTY_BLOB_DIGEST.clone().into(),
             size: 0,
             executable: false,
         }],
         symlinks: vec![SymlinkNode {
-            name: b"aa".to_vec(),
-            target: b"/nix/store/somewhereelse".to_vec(),
+            name: b"aa".to_vec().into(),
+            target: b"/nix/store/somewhereelse".to_vec().into(),
         }],
     };
     pub static ref DIRECTORY_A: Directory = Directory::default();
     pub static ref DIRECTORY_B: Directory = Directory {
         directories: vec![DirectoryNode {
-            name: b"a".to_vec(),
-            digest: DIRECTORY_A.digest().to_vec(),
+            name: b"a".to_vec().into(),
+            digest: DIRECTORY_A.digest().into(),
             size: DIRECTORY_A.size(),
         }],
         ..Default::default()
@@ -69,13 +72,13 @@ lazy_static! {
     pub static ref DIRECTORY_C: Directory = Directory {
         directories: vec![
             DirectoryNode {
-                name: b"a".to_vec(),
-                digest: DIRECTORY_A.digest().to_vec(),
+                name: b"a".to_vec().into(),
+                digest: DIRECTORY_A.digest().into(),
                 size: DIRECTORY_A.size(),
             },
             DirectoryNode {
-                name: b"a'".to_vec(),
-                digest: DIRECTORY_A.digest().to_vec(),
+                name: b"a'".to_vec().into(),
+                digest: DIRECTORY_A.digest().into(),
                 size: DIRECTORY_A.size(),
             }
         ],
