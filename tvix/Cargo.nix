be5a95dc3ac8491f9f3951256c0b5cbb7db33549
Florian Klink <flokli@flokli.de>
2023-07-30 00:38:55 +0200

    refactor(tvix/cli/refscan): use wu-manber crate with &[u8] support
    PR'ed at https://github.com/tvlfyi/wu-manber/pull/1.
    
    Change-Id: I8c71e359196396a1d42a3ea2ab7ac15b137b2db0

--- tvix/Cargo.nix
+++ tvix/Cargo.nix
@@ -10251,9 +10251,9 @@ rec {
         edition = "2015";
         workspace_member = null;
         src = pkgs.fetchgit {
-          url = "https://github.com/tvlfyi/wu-manber.git";
-          rev = "e77628cafcf45d41b9e455be86a6b1b9f46b2092";
-          sha256 = "02byhfiw41mlgr1c43n2iq6jw5sbyn8l1acv5v71a07h5l18q0cy";
+          url = "https://github.com/flokli/wu-manber.git";
+          rev = "26a00d1bd6669928cdc2995bb85f1594096b6c21";
+          sha256 = "0ki0r6aimymml93q557v0ia0vm15y4s1bipswa8899wadvfhq5yl";
         };
         authors = [
           "Joe Neeman <joeneeman@gmail.com>"
