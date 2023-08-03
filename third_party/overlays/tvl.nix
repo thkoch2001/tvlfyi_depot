910ab4c27b98d7531d528ae3fab4f1280faf1965
sterni <sternenseemann@systemli.org>
2023-08-03 23:25:34 +0200

    chore(3p/sources): Bump channels & overlays
    * //users/grfn: vendor ddclient module and package into depot
    
      //3p/ddclient now contains the removed package expression and
      NixOS module with the following changes:
    
      - Include former uid/gid settings from ids.nix which have been removed
        by upstream with the ddclient module.
    
      - Rename to deprecate-ddclient, since it is impossible at the moment
        to prevent the corresponding mkRemovedOptionModule from being
        imported (https://github.com/NixOS/nixpkgs/issues/245265).
        I wrote a patch for nixpkgs that would at least allow individual
        mkRemovedOptionModule to be disable, but it is stuck for now:
        https://github.com/NixOS/nixpkgs/pull/245274.
    
    * //tools/magrathea:
    
      We need to pass -host to csc due to
      https://github.com/NixOS/nixpkgs/pull/246923 now. I don't fully grasp
      what this means, but it works and we are hardly cross-compiling, so it
      should be fine until I can get some answers from the change author.
    
    * //3p/nixpkgs:nixos-option: provide latest Nix as input
    
      https://github.com/NixOS/nixpkgs/pull/237442 adapted nixos-option to
      API changes in Nix's libraries which means it needs to be built
      against Nix 2.15, not 2.3. Let's hope it stays up to date with the
      latest Nix version in the future, so we can keep this override as is.
    
      Sadly this means that machines in depot will depend on two
      versions of Nix going forward.
    
    Change-Id: Iac4dba58a076ecf25e8647fd9a06cbabf2f7809e

--- third_party/overlays/tvl.nix
+++ third_party/overlays/tvl.nix
@@ -39,6 +39,14 @@ depot.nix.readTree.drvTargets {
   nix = self.nix_2_3;
   nix_latest = super.nix;
 
+  # nixos-option now unfortunately depends on (at the time of writing) Nix 2.15
+  # instead of Nix 2.3 as before. The intention seems to be to keep it in sync
+  # with the latest Nix and it uses unstable interfaces of Nix (the libraries).
+  # TODO(sterni): can we link it statically and avoid a second Nix store path?
+  nixos-option = super.nixos-option.override {
+    nix = self.nix_latest;
+  };
+
   # Too match telega in emacs-overlay or wherever
   tdlib = super.tdlib.overrideAttrs (_: {
     version = "1.8.14";
