78982e5928b39a96021eddef04a0a1e3730d0814
Profpatsch <mail@profpatsch.de>
2023-08-06 12:46:50 +0200

    chore(users/Profpatsch): clean up haskell libs a little
    Change-Id: Ia9a6c5a754ca8f2912308feb5a26f5276a08d24c

--- users/Profpatsch/mailbox-org/src/AesonQQ.hs
+++ users/Profpatsch/mailbox-org/src/AesonQQ.hs
@@ -3,20 +3,21 @@
 module AesonQQ where
 
 import Data.Aeson qualified as Json
-import Data.Either qualified as Either
+import Language.Haskell.TH.Quote (QuasiQuoter)
 import PossehlAnalyticsPrelude
 import PyF qualified
 import PyF.Internal.QQ qualified as PyFConf
 
+aesonQQ :: QuasiQuoter
 aesonQQ =
   PyF.mkFormatter
     "aesonQQ"
     PyF.defaultConfig
       { PyFConf.delimiters = Just ('|', '|'),
-        PyFConf.postProcess = \exp -> do
+        PyFConf.postProcess = \exp_ -> do
           -- TODO: this does not throw an error at compilation time if the json does not parse
           [|
-            case Json.eitherDecodeStrict' @Json.Value $ textToBytesUtf8 $ stringToText $(exp) of
+            case Json.eitherDecodeStrict' @Json.Value $ textToBytesUtf8 $ stringToText $(exp_) of
               Left err -> error err
               Right a -> a
             |]
