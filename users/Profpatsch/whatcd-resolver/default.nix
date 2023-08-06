b41af857ae5357ecfd4b4dd15d297ce8ab5ae2ea
Profpatsch <mail@profpatsch.de>
2023-08-06 12:47:20 +0200

    chore(users/Profpatsch/whatcd-resolver): build with nix
    Change-Id: I2d6ee30671bb60a96bcbfede1fd7a17fcf7c3ca8
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/9012
    Reviewed-by: Profpatsch <mail@profpatsch.de>
    Autosubmit: Profpatsch <mail@profpatsch.de>
    Tested-by: BuildkiteCI

---
+++ users/Profpatsch/whatcd-resolver/default.nix
@@ -0,0 +1,47 @@
+{ depot, pkgs, lib, ... }:
+
+let
+  #   bins = depot.nix.getBins pkgs.sqlite ["sqlite3"];
+
+  whatcd-resolver = pkgs.haskellPackages.mkDerivation {
+    pname = "whatcd-resolver";
+    version = "0.1.0";
+
+    src = depot.users.Profpatsch.exactSource ./. [
+      ./whatcd-resolver.cabal
+      ./src/Multipart2.hs
+      ./src/WhatcdResolver.hs
+    ];
+
+    libraryHaskellDepends = [
+      depot.users.Profpatsch.my-prelude
+      pkgs.haskellPackages.pa-prelude
+      pkgs.haskellPackages.pa-label
+      pkgs.haskellPackages.pa-json
+      pkgs.haskellPackages.pa-error-tree
+      pkgs.haskellPackages.pa-field-parser
+      pkgs.haskellPackages.pa-pretty
+      pkgs.haskellPackages.pa-run-command
+      pkgs.haskellPackages.aeson-better-errors
+      pkgs.haskellPackages.blaze-html
+      pkgs.haskellPackages.dlist
+      pkgs.haskellPackages.http-conduit
+      pkgs.haskellPackages.http-types
+      pkgs.haskellPackages.ihp-hsx
+      pkgs.haskellPackages.monad-logger
+      pkgs.haskellPackages.resource-pool
+      pkgs.haskellPackages.postgresql-simple
+      pkgs.haskellPackages.selective
+      pkgs.haskellPackages.tmp-postgres
+      pkgs.haskellPackages.unliftio
+      pkgs.haskellPackages.wai-extra
+      pkgs.haskellPackages.warp
+    ];
+
+    isExecutable = true;
+    isLibrary = false;
+    license = lib.licenses.mit;
+  };
+
+in
+whatcd-resolver
