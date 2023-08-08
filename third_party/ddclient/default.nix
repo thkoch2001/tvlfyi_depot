0d4465297a221270816dc5ac4ded840ea88710d1
Vincent Ambo <mail@tazj.in>
2023-08-08 23:37:20 +0300

    docs(3p/ddclient): add TODO-comment for eventually cleaning this up
    Change-Id: Id02e9e69890d00dedafa1700a1ea98dd68767c1d
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/9045
    Reviewed-by: sterni <sternenseemann@systemli.org>
    Tested-by: BuildkiteCI
    Autosubmit: tazjin <tazjin@tvl.su>

--- third_party/ddclient/default.nix
+++ third_party/ddclient/default.nix
@@ -1,3 +1,8 @@
+# Users of this package & module should replace it with something like
+# inadyn, after https://github.com/NixOS/nixpkgs/issues/242330 is
+# landed.
+#
+# TODO(aspen): replace ddclient with inadyn or something else.
 { pkgs, ... }:
 
 (pkgs.callPackage ./pkg.nix { }).overrideAttrs (old: {
