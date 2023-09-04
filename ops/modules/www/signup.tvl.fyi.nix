e3c2ee8ff08ceac6ccaa408c3827d26dc9ff7dda
Vincent Ambo <mail@tazj.in>
2023-09-05 01:19:55 +0300

    feat(ops/modules): deploy //web/pwcrypt to signup.tvl.fyi
    I verified on whitby that the password hashes generated by
    //web/pwcrypt are compatible with our OpenLDAP, so it's time to make
    this thing public.
    
    Change-Id: Icc2f095ca7ce4acff6de91a1642dea6461177423

---
+++ ops/modules/www/signup.tvl.fyi.nix
@@ -0,0 +1,19 @@
+{ depot, ... }:
+
+{
+  imports = [
+    ./base.nix
+  ];
+
+  config = {
+    services.nginx.virtualHosts."signup.tvl.fyi" = {
+      root = depot.web.pwcrypt;
+      enableACME = true;
+      forceSSL = true;
+
+      extraConfig = ''
+        add_header Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" always;
+      '';
+    };
+  };
+}
