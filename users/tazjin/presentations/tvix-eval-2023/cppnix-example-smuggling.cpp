6fc21f6288b98be23282f998b84dca72971b90a8
Vincent Ambo <tazjin@tvl.su>
2023-09-07 15:02:03 +0300

    chore(tazjin/presentations): last minute changes to tvix-eval talk
    Change-Id: I788a17f846b35f5635ed7a9ec0d3249b05653d44

--- users/tazjin/presentations/tvix-eval-2023/cppnix-example-smuggling.cpp
+++ users/tazjin/presentations/tvix-eval-2023/cppnix-example-smuggling.cpp
@@ -1,12 +1,12 @@
 struct Env {
-  Value * values[0];
-  // ... some more fields
+  // ... some struct fields ...
+  Value* values[0];
 };
 
 // ....
 
 if (env->type == Env::HasWithExpr) {
-  Value * v = allocValue();
-  evalAttrs(*env->up, (Expr *) env->values[0], *v, noPos, "<borked>");
   // ...
+  evalAttrs(*env->up, (Expr *) env->values[0], *v, noPos, "<borked>");
+  //                  ^^^^^^^^^^^^^^^^^^^^^^^
 }
