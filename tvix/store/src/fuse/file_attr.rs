226a8a9fc4a2e326a859e3a28cbb1a2f8fa96f73
Connor Brewster <cbrewster@hey.com>
2023-09-16 10:20:34 -0500

    refactor(tvix/store/fuse): Switch from fuser to fuse-backend-rs
    This switches the FUSE implementation from fuser to fuse-backend-rs.
    fuse-backend-rs is designed to work with both FUSE and virtiofs.
    Virtiofs support will make it possible to plug the tvix-store into a
    microvm and have `/nix/store` access without having to setup FUSE inside
    the guest.
    
    Additionally fuse-backend-rs has nice support for running multiple FUSE
    threads and has some async support.
    
    The goal of this commit is to mechanically switch over to
    fuse-backend-rs with minimal changes. I did have to add some locks here
    and there because fuse-backend-rs uses `&self` on all methods whereas
    fuser uses `&mut self`. `&self` is required for concurrent access to the
    FUSE server, so this makes sense.
    
    We can consider switching to concurrent maps and use some other
    techniques to reduce lock contention and critical section size.
    
    Issue: https://b.tvl.fyi/issues/305
    
    Change-Id: Icde5a58c6eef98f8984c1e04e980b756dfb76b47

--- tvix/store/src/fuse/file_attr.rs
+++ tvix/store/src/fuse/file_attr.rs
@@ -1,56 +1,41 @@
-use std::time::SystemTime;
+use std::mem::MaybeUninit;
 
 use super::inodes::{DirectoryInodeData, InodeData};
-use fuser::FileAttr;
+use const_zero::const_zero;
 
 /// The [FileAttr] describing the root
-pub const ROOT_FILE_ATTR: FileAttr = FileAttr {
-    ino: fuser::FUSE_ROOT_ID,
-    size: 0,
-    blksize: 1024,
-    blocks: 0,
-    atime: SystemTime::UNIX_EPOCH,
-    mtime: SystemTime::UNIX_EPOCH,
-    ctime: SystemTime::UNIX_EPOCH,
-    crtime: SystemTime::UNIX_EPOCH,
-    kind: fuser::FileType::Directory,
-    perm: 0o555,
-    nlink: 0,
-    uid: 0,
-    gid: 0,
-    rdev: 0,
-    flags: 0,
-};
+pub const ROOT_FILE_ATTR: libc::stat64 = get_root_file_attr();
 
-/// for given &Node and inode, construct a [FileAttr]
-pub fn gen_file_attr(inode_data: &InodeData, inode: u64) -> FileAttr {
-    FileAttr {
-        ino: inode,
-        size: match inode_data {
-            InodeData::Regular(_, size, _) => *size as u64,
-            InodeData::Symlink(target) => target.len() as u64,
-            InodeData::Directory(DirectoryInodeData::Sparse(_, size)) => *size as u64,
-            InodeData::Directory(DirectoryInodeData::Populated(_, ref children)) => {
-                children.len() as u64
-            }
-        },
-        // FUTUREWORK: play with this numbers, as it affects read sizes for client applications.
-        blksize: 1024,
-        blocks: 0,
-        atime: SystemTime::UNIX_EPOCH,
-        mtime: SystemTime::UNIX_EPOCH,
-        ctime: SystemTime::UNIX_EPOCH,
-        crtime: SystemTime::UNIX_EPOCH,
-        kind: inode_data.into(),
-        perm: match inode_data {
-            InodeData::Regular(..) => 0o444,
-            InodeData::Symlink(_) => 0o444,
-            InodeData::Directory(..) => 0o555,
-        },
-        nlink: 0,
-        uid: 0,
-        gid: 0,
-        rdev: 0,
-        flags: 0,
-    }
+const fn get_root_file_attr() -> libc::stat64 {
+    // TODO: replace `const_zero` once [std::mem::MaybeUninit::zeroed] in const is stabilized.
+    let mut stat64: libc::stat64 = unsafe { const_zero!(libc::stat64) };
+    stat64.st_ino = fuse_backend_rs::api::filesystem::ROOT_ID;
+    stat64.st_size = 0;
+    stat64.st_blocks = 1024;
+    stat64.st_mode = libc::S_IFDIR | 0o555;
+    stat64
+}
+
+/// for given &Node and inode, construct a [libc::stat64]
+pub fn gen_file_attr(inode_data: &InodeData, inode: u64) -> libc::stat64 {
+    let mut stat64: libc::stat64 = unsafe { MaybeUninit::zeroed().assume_init() };
+
+    stat64.st_ino = inode;
+    stat64.st_size = match inode_data {
+        InodeData::Regular(_, size, _) => *size as i64,
+        InodeData::Symlink(target) => target.len() as i64,
+        InodeData::Directory(DirectoryInodeData::Sparse(_, size)) => *size as i64,
+        InodeData::Directory(DirectoryInodeData::Populated(_, ref children)) => {
+            children.len() as i64
+        }
+    };
+    // FUTUREWORK: play with this numbers, as it affects read sizes for client applications.
+    stat64.st_blocks = 1024;
+    stat64.st_mode = match inode_data {
+        InodeData::Regular(_, _, _) => libc::S_IFREG | 0o444,
+        InodeData::Symlink(_) => libc::S_IFLNK | 0o444,
+        InodeData::Directory(_) => libc::S_IFDIR | 0o555,
+    };
+
+    stat64
 }
