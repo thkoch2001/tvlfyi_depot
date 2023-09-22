3a955789c7172ed31df766c8308c8e3aadd50b17
Vincent Ambo <tazjin@tvl.su>
2023-09-22 19:51:35 +0300

    feat(ops): add nixery-01 instance for hosting nixery.dev
    Change-Id: Ida21ac7240a532bb6063b362155f2b14b2859aae

---
+++ ops/machines/nixery-01/default.nix
@@ -0,0 +1,29 @@
+# nixery.dev backing host in ru-central1-b
+{ depot, lib, pkgs, ... }: # readTree options
+{ config, ... }: # passed by module system
+
+let
+  mod = name: depot.path.origSrc + ("/ops/modules/" + name);
+in
+{
+  imports = [
+    (mod "known-hosts.nix")
+    (mod "nixery.nix")
+    (mod "tvl-users.nix")
+    (mod "www/nixery.dev.nix")
+    (mod "yandex-cloud.nix")
+
+    (depot.third_party.agenix.src + "/modules/age.nix")
+  ];
+
+  networking = {
+    hostName = "nixery-01";
+    domain = "tvl.fyi";
+    firewall.allowedTCPPorts = [ 22 80 443 ];
+  };
+
+  security.sudo.extraRules = lib.singleton {
+    groups = [ "wheel" ];
+    commands = [{ command = "ALL"; options = [ "NOPASSWD" ]; }];
+  };
+}
