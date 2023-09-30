9cde51a161ff4c71b26e0cefbf015b2b7ff819f6
Vincent Ambo <mail@tazj.in>
2023-09-30 21:25:57 +0300

    chore(tazjin/secrets): reencrypt with new key set
    Change-Id: Ib2111bf10babf42ad7984c39a25acd84cecd0d76

--- users/tazjin/secrets/secrets.nix
+++ users/tazjin/secrets/secrets.nix
@@ -4,6 +4,7 @@ let
     # local keys
     myKeys.tverskoy_ed25519
     myKeys.zamalek_ed25519
+    myKeys.khamovnik_yk
     # koptevo
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMw2ZfdNZCXCOtbQNT6hztXCIkTcO9MBrOuDqMlmGOYK root@koptevo"
   ];
