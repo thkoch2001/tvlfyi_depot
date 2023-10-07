7219880d953bccfc557fecc1b8ba6518b4ba6646
Florian Klink <flokli@flokli.de>
2023-10-07 23:01:18 +0200

    WIP: try to use DuplexReadStream?
    Change-Id: I6b79823bc6209cbcb343b7a498c64a2ba6e0aee7

--- tvix/castore/src/utils.rs
+++ tvix/castore/src/utils.rs
@@ -3,12 +3,12 @@
 //! Only used for testing purposes, but across crates.
 //! Should be removed once we have a better concept of a "Service registry".
 
+use async_stream::stream;
 use core::time;
-use std::{path::Path, sync::Arc, thread};
-
-use tokio::net::{UnixListener, UnixStream};
-use tokio_stream::wrappers::UnixListenerStream;
-use tonic::transport::{Channel, Endpoint, Server, Uri};
+use pin_project_lite::pin_project;
+use std::{path::Path, sync::Arc, task::Poll, thread};
+use tokio::io::{AsyncRead, AsyncWrite, DuplexStream};
+use tonic::transport::{server::Connected, Channel, Endpoint, Server, Uri};
 
 use crate::{
     blobservice::{BlobService, MemoryBlobService},
@@ -27,6 +27,62 @@ pub fn gen_directory_service() -> Arc<dyn DirectoryService> {
     Arc::new(MemoryDirectoryService::default())
 }
 
+pin_project! {
+    /// A wrapper around [DuplexStreamStream],
+    /// implementing [AsyncRead] and [Connected].
+    pub struct DuplexStreamWrapper {
+            #[pin]
+        inner: DuplexStream
+    }
+}
+
+impl DuplexStreamWrapper {
+    fn new(inner: DuplexStream) -> Self {
+        Self { inner }
+    }
+}
+impl AsyncRead for DuplexStreamWrapper {
+    fn poll_read(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+        buf: &mut tokio::io::ReadBuf<'_>,
+    ) -> Poll<std::io::Result<()>> {
+        self.project().inner.poll_read(cx, buf)
+    }
+}
+
+impl AsyncWrite for DuplexStreamWrapper {
+    fn poll_write(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+        buf: &[u8],
+    ) -> Poll<Result<usize, std::io::Error>> {
+        todo!()
+    }
+
+    fn poll_flush(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> Poll<Result<(), std::io::Error>> {
+        todo!()
+    }
+
+    fn poll_shutdown(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> Poll<Result<(), std::io::Error>> {
+        todo!()
+    }
+}
+
+impl Connected for DuplexStreamWrapper {
+    type ConnectInfo = ();
+
+    fn connect_info(&self) -> Self::ConnectInfo {
+        ()
+    }
+}
+
 /// This will spawn a separate thread, with its own tokio runtime, and start a gRPC server there.
 /// Once it's listening, it'll start a gRPC client from the original thread, and return it.
 /// FUTUREWORK: accept a closure to create the service, so we can test this with different ones.
@@ -34,9 +90,10 @@ pub fn gen_directory_service() -> Arc<dyn DirectoryService> {
 pub(crate) async fn gen_directorysvc_grpc_client(tmpdir: &Path) -> DirectoryServiceClient<Channel> {
     let socket_path = tmpdir.join("socket");
 
+    // TODO: not sure if a duplex can span different runtimes?
+    let (mut s1, mut s2) = tokio::io::duplex(64);
     // Spin up a server, in a thread far away, which spawns its own tokio runtime,
     // and blocks on the task.
-    let socket_path_clone = socket_path.clone();
     thread::spawn(move || {
         // Create the runtime
         let rt = tokio::runtime::Runtime::new().unwrap();
@@ -44,15 +101,19 @@ pub(crate) async fn gen_directorysvc_grpc_client(tmpdir: &Path) -> DirectoryServ
         let handle = rt.handle();
 
         let task = handle.spawn(async {
-            let uds = UnixListener::bind(socket_path_clone).unwrap();
-            let uds_stream = UnixListenerStream::new(uds);
-
             // spin up a new DirectoryService
             let mut server = Server::builder();
             let router = server.add_service(DirectoryServiceServer::new(
                 GRPCDirectoryServiceWrapper::from(gen_directory_service()),
             ));
-            router.serve_with_incoming(uds_stream).await
+
+            router
+                .serve_with_incoming(stream! {
+                    yield {
+                        Result::<_, std::io::Error>::Ok(DuplexStreamWrapper::new(s1))
+                    };
+                })
+                .await
         });
 
         handle.block_on(task)
@@ -80,8 +141,10 @@ pub(crate) async fn gen_directorysvc_grpc_client(tmpdir: &Path) -> DirectoryServ
     // The URI is unused.
     let channel = Endpoint::try_from("http://[::]:50051")
         .unwrap()
-        .connect_with_connector_lazy(tower::service_fn(move |_: Uri| {
-            UnixStream::connect(socket_path.clone())
+        .connect_with_connector(tower::service_fn(move |_: Uri| {
+            stream! {
+                yield Result::<_, std::io::Error>::Ok(DuplexStreamWrapper::new(s2));
+            }
         }));
 
     let grpc_client = DirectoryServiceClient::new(channel);
