5f32f7610abb116e68d6d3fbb57fe528100ec58b
Vincent Ambo <tazjin@tvl.su>
2023-10-08 19:31:58 +0300

    chore(users): remove inactive users
    Change-Id: I3cfb425e4dac0a467e3917df996e9800a3ebe875
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/9576
    Reviewed-by: isomer <isomer@tvl.fyi>
    Tested-by: BuildkiteCI
    Autosubmit: tazjin <tazjin@tvl.su>

--- users/riking/dotfiles/fish/config.fish
+++
@@ -1,8 +0,0 @@
-set -gx GOPATH "$HOME/go"
-set -gx GPG_TTY (tty)
-set -gx DEPOT_ROOT "$GOPATH/src/code.tvl.fyi"
-
-set -gx PATH '/usr/local/go/bin' "$HOME/.cargo/bin" "$HOME/.rbenv/bin" $PATH
-status --is-interactive; and rbenv init - | source
-source ~/.opsrc.fish # work
-set -gx PATH "$HOME/go/bin" $PATH
--- users/riking/dotfiles/fish/functions/ddate.fish
+++
@@ -1,3 +0,0 @@
-function ddate --description 'current date in Discourse format'
-    TZ=UTC date '+[date=%Y-%m-%d time=%H:%M:%S timezone=\"%Z\"]'
-end
