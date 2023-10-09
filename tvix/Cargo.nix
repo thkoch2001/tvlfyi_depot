52aca54070170b59722b161e18d232c908193a41
edef <edef@edef.eu>
2023-10-09 21:06:02 +0000

    feat(tvix/nix-compat): add nar::writer::async
    Change-Id: Ib962f889836659fd278beec461ee329b00e64ea5

--- tvix/Cargo.nix
+++ tvix/Cargo.nix
@@ -2550,7 +2550,7 @@ rec {
           "unstable" = [ "futures-core/unstable" "futures-task/unstable" ];
           "write-all-vectored" = [ "io" ];
         };
-        resolvedDefaultFeatures = [ "alloc" "async-await" "async-await-macro" "channel" "futures-channel" "futures-io" "futures-macro" "futures-sink" "io" "memchr" "sink" "slab" "std" ];
+        resolvedDefaultFeatures = [ "alloc" "async-await" "async-await-macro" "channel" "default" "futures-channel" "futures-io" "futures-macro" "futures-sink" "io" "memchr" "sink" "slab" "std" ];
       };
       "fxhash" = rec {
         crateName = "fxhash";
@@ -4129,6 +4129,12 @@ rec {
             packageId = "data-encoding";
           }
           {
+            name = "futures-util";
+            packageId = "futures-util";
+            optional = true;
+            features = [ "io" ];
+          }
+          {
             name = "serde";
             packageId = "serde";
             features = [ "derive" ];
@@ -4160,7 +4166,11 @@ rec {
             packageId = "test-generator";
           }
         ];
-
+        features = {
+          "async" = [ "futures-util" ];
+          "futures-util" = [ "dep:futures-util" ];
+        };
+        resolvedDefaultFeatures = [ "async" "futures-util" ];
       };
       "nom8" = rec {
         crateName = "nom8";
