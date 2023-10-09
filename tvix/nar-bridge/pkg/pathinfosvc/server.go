2223c7b5ca1bf8b0da2ba2fb9149f0ad56b58a0f
Florian Klink <flokli@flokli.de>
2023-10-09 14:03:29 +0200

    refactor(tvix/nar-bridge): add RenamedNode helper
    This takes a castorev1pb.Node and updates its name, making sure to copy
    it.
    
    Change-Id: I57bf2b2b4a74496a1a697bcbce72bf67a12e69fe

--- tvix/nar-bridge/pkg/pathinfosvc/server.go
+++ tvix/nar-bridge/pkg/pathinfosvc/server.go
@@ -247,16 +247,8 @@ func (p *PathInfoServiceServer) Get(ctx context.Context, getPathInfoRequest *sto
 		panic(err)
 	}
 
-	// set the root name in all three cases.
-	if node := pathInfo.Node.GetDirectory(); node != nil {
-		node.Name = []byte(outPath.String())
-	} else if node := pathInfo.Node.GetFile(); node != nil {
-		node.Name = []byte(outPath.String())
-	} else if node := pathInfo.Node.GetSymlink(); node != nil {
-		node.Name = []byte(outPath.String())
-	} else {
-		panic("node may not be nil")
-	}
+	renamedNode := importer.RenamedNode(pathInfo.Node, outPath.String())
+	pathInfo.Node = &renamedNode
 
 	// run Validate on the PathInfo, more as an additional sanity check our code is sound,
 	// to make sure we populated everything properly, before returning it.
