c8dfb643d12e0cbab3a122f6365ab2b69fb0b859
Florian Klink <flokli@flokli.de>
2023-10-09 14:03:29 +0200

    refactor(tvix/nar-bridge): add RenamedNode helper
    This takes a castorev1pb.Node and updates its name, making sure to copy
    it.
    
    Change-Id: I57bf2b2b4a74496a1a697bcbce72bf67a12e69fe

---
+++ tvix/nar-bridge/pkg/importer/util.go
@@ -0,0 +1,48 @@
+package importer
+
+import (
+	"fmt"
+
+	castorev1pb "code.tvl.fyi/tvix/castore/protos"
+	storev1pb "code.tvl.fyi/tvix/store/protos"
+	"github.com/nix-community/go-nix/pkg/narinfo"
+	"github.com/nix-community/go-nix/pkg/storepath"
+)
+
+// RenamedNode returns a node with a new name.
+func RenamedNode(node *castorev1pb.Node, name string) castorev1pb.Node {
+	if directoryNode := node.GetDirectory(); directoryNode != nil {
+		return castorev1pb.Node{
+			Node: &castorev1pb.Node_Directory{
+				Directory: &castorev1pb.DirectoryNode{
+					Name:   []byte(name),
+					Digest: directoryNode.GetDigest(),
+					Size:   directoryNode.GetSize(),
+				},
+			},
+		}
+	} else if fileNode := node.GetFile(); fileNode != nil {
+		return castorev1pb.Node{
+			Node: &castorev1pb.Node_File{
+				File: &castorev1pb.FileNode{
+					Name:       []byte(name),
+					Digest:     fileNode.GetDigest(),
+					Size:       fileNode.GetSize(),
+					Executable: fileNode.GetExecutable(),
+				},
+			},
+		}
+	} else if symlinkNode := node.GetSymlink(); symlinkNode != nil {
+		return castorev1pb.Node{
+			Node: &castorev1pb.Node_Symlink{
+				Symlink: &castorev1pb.SymlinkNode{
+					Name:   []byte(name),
+					Target: symlinkNode.GetTarget(),
+				},
+			},
+		}
+	} else {
+		panic("unreachable")
+	}
+
+}
