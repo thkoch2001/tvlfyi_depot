2881d4f06fb6f9e3dc40080e1174c827819307fd
Connor Brewster <cbrewster@hey.com>
2023-10-14 11:58:23 -0500

    refactor(tvix/store): Switch to tokio-listener with tonic support
    Opened a PR to add support for tonic directly to tokio-listener
    https://github.com/vi/tokio-listener/pull/2
    
    While waiting for that PR to land, we can switch to my fork which
    removes the need for us to have an extra compatibility shim between
    tokio-listener and tonic.
    
    Change-Id: I04a2dbb3bc3c8bfe9339583c0b46070c7ec97811

--- tvix/store/src/bin/tvix-store.rs
+++ tvix/store/src/bin/tvix-store.rs
@@ -7,6 +7,7 @@ use std::io;
 use std::path::Path;
 use std::path::PathBuf;
 use tokio::task::JoinHandle;
+use tokio_listener::Listener;
 use tracing_subscriber::prelude::*;
 use tvix_castore::blobservice;
 use tvix_castore::directoryservice;
@@ -17,7 +18,6 @@ use tvix_castore::proto::node::Node;
 use tvix_castore::proto::GRPCBlobServiceWrapper;
 use tvix_castore::proto::GRPCDirectoryServiceWrapper;
 use tvix_castore::proto::NamedNode;
-use tvix_store::listener::ListenerStream;
 use tvix_store::pathinfoservice;
 use tvix_store::proto::path_info_service_server::PathInfoServiceServer;
 use tvix_store::proto::GRPCPathInfoServiceWrapper;
@@ -228,7 +228,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             info!("tvix-store listening on {}", listen_address);
 
-            let listener = ListenerStream::bind(&listen_address).await?;
+            let listener =
+                Listener::bind(&listen_address, &Default::default(), &Default::default()).await?;
 
             router.serve_with_incoming(listener).await?;
         }
