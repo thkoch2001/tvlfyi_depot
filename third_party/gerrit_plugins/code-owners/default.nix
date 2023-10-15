a3543f60e0a98007cd704e5700f537a526a50cd9
Luke Granger-Brown <git@lukegb.com>
2023-10-15 00:43:44 +0000

    chore(3p/gerrit): 3.7.0-rc4 -> 3.8.2
    Bump gerrit plugins dep hashes, and for code-owners rebase it against
    master.
    
    Change-Id: If7da0ca391b4a5c0102560ca8d52b6f5a2dfd223

--- third_party/gerrit_plugins/code-owners/default.nix
+++ third_party/gerrit_plugins/code-owners/default.nix
@@ -5,11 +5,11 @@ let
 in
 buildGerritBazelPlugin rec {
   name = "code-owners";
-  depsOutputHash = "sha256:07mgvd7fvg1xqlabjn644505yx98vjrmwxx1arwsykir1h82h0b2";
+  depsOutputHash = "sha256:1hd63b54zkgv8j7323inp7rdnhs2jdsb232jqlwsd9pai2f12m7n";
   src = pkgs.fetchgit {
     url = "https://gerrit.googlesource.com/plugins/code-owners";
-    rev = "6fdf3ce2e52904b35e2a5824a4197155c2c6b4e4";
-    sha256 = "sha256:17k6310py71wax3881mf3vsf9zas648j4xzs9h0d7migv5nzsdzs";
+    rev = "e654ae5bda2085bce9a99942bec440e004a114f3";
+    sha256 = "sha256:14d3x3iqskgw16pvyaa0swh252agj84p9pzlf24l8lgx9d7y4biz";
   };
   patches = [
     ./using-usernames.patch
