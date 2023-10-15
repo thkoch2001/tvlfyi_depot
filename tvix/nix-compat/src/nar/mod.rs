e0ba5e21c4ff1ea03e63b5d928091c925be4616c
edef <edef@edef.eu>
2023-10-15 14:59:39 +0000

    feat(tvix/nix-compat): NAR reader
    Change-Id: I50d51baf62c0419eaf17f0dc262f728aaff9794d

--- tvix/nix-compat/src/nar/mod.rs
+++ tvix/nix-compat/src/nar/mod.rs
@@ -1,1 +1,24 @@
+use std::io::{self, BufReader};
+
+mod wire;
+
+pub mod reader;
 pub mod writer;
+
+pub fn copy(reader: reader::Node, writer: writer::Node) -> io::Result<()> {
+    match reader {
+        reader::Node::Symlink { target } => writer.symlink(&target),
+        reader::Node::File { executable, reader } => {
+            writer.file(executable, reader.len(), &mut BufReader::new(reader))
+        }
+        reader::Node::Directory(mut reader) => {
+            let mut writer = writer.directory()?;
+
+            while let Some(entry) = reader.next()? {
+                copy(entry.node, writer.entry(&entry.name)?)?;
+            }
+
+            writer.close()
+        }
+    }
+}
