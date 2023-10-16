167ac9b8ca7f98264a2bd0ad0e8e9c4825eaa47f
Florian Klink <flokli@flokli.de>
2023-10-17 01:44:32 +0200

    test(tvix/nix-compat/nar/writer/sync): add tests
    Change-Id: I04c556e7b57f20dd33f71b0379e6104e1702a8ee

--- tvix/nix-compat/default.nix
+++ tvix/nix-compat/default.nix
@@ -2,4 +2,6 @@
 
 depot.tvix.crates.workspaceMembers.nix-compat.build.override {
   runTests = true;
+  # make sure we also enable async here, so run the tests behind that feature flag.
+  features = [ "default" "async" ];
 }
