dc4372937feb3280467e999a3c14c067cea99b96
Florian Klink <flokli@flokli.de>
2023-10-16 23:24:38 +0100

    docs(tvix/nix-compat): add doc comments
    Change-Id: Idd1bcb0e1e870983e34e374cf15671bb1aaac6c9

--- tvix/nix-compat/src/nar/reader/mod.rs
+++ tvix/nix-compat/src/nar/reader/mod.rs
@@ -1,3 +1,7 @@
+//! Implements an interface for reaching the Nix archive format (NAR).
+//!
+//! NAR files (and their hashed representations) are used in C++ Nix for
+//! addressing fixed-output derivations and a variety of other things.
 use std::io::{
     self,
     ErrorKind::{InvalidData, UnexpectedEof},
@@ -10,11 +14,19 @@ mod read;
 
 pub type Reader<'a> = dyn Read + Send + 'a;
 
+/// Start reading a new NAR, reading from the specified reader.
+/// It returns a [Node], or an error.
 pub fn open<'a, 'r>(reader: &'a mut Reader<'r>) -> io::Result<Node<'a, 'r>> {
     read::token(reader, &wire::TOK_NAR)?;
     Node::new(reader)
 }
 
+/// Single node in a NAR file.
+///
+/// It can either be a Symlink, File or Directory.
+/// It is the caller's responsibility to read files and directories to the end,
+/// via [FileReader] and [DirReader], respectively.
+/// TODO(edef): add debug_assertions to detect wrong usage
 pub enum Node<'a, 'r> {
     Symlink {
         target: Vec<u8>,
@@ -27,8 +39,10 @@ pub enum Node<'a, 'r> {
 }
 
 impl<'a, 'r> Node<'a, 'r> {
+    /// Start reading the next node, matching on the next Node tag.
     fn new(reader: &'a mut Reader<'r>) -> io::Result<Self> {
         Ok(match read::tag(reader)? {
+            // In the case of a symlink, read the target
             wire::Node::Sym => {
                 let target = read::bytes(reader, wire::MAX_TARGET_LEN)?;
 
@@ -36,12 +50,17 @@ impl<'a, 'r> Node<'a, 'r> {
                     return Err(InvalidData.into());
                 }
 
+                // for symlinks, we're done reading everything after having read
+                // the target (which we're about to return), so consume the
+                // closing paren and return the [Node::Symlink].
+                // In the case of files and directories, it's handled by the
+                // corresponding readers.
                 read::token(reader, &wire::TOK_PAR)?;
 
                 Node::Symlink { target }
             }
             tag @ (wire::Node::Reg | wire::Node::Exe) => {
-                let len = read::u64(reader)?;
+                let len = read::u64(reader)?; // file length
 
                 Node::File {
                     executable: tag == wire::Node::Exe,
@@ -52,7 +71,7 @@ impl<'a, 'r> Node<'a, 'r> {
         })
     }
 }
-
+/// Reads a file from the NAR stream (and the closing paren)
 pub struct FileReader<'a, 'r> {
     reader: &'a mut Reader<'r>,
     len: u64,
@@ -61,6 +80,7 @@ pub struct FileReader<'a, 'r> {
 
 impl<'a, 'r> FileReader<'a, 'r> {
     fn new(reader: &'a mut Reader<'r>, len: u64) -> io::Result<Self> {
+        // early exit for zero-length files
         if len == 0 {
             read::token(reader, &wire::TOK_PAR)?;
         }
@@ -98,6 +118,8 @@ impl Read for FileReader<'_, '_> {
             return Err(UnexpectedEof.into());
         }
 
+        // if we're at the end of the file, also consume the padding,
+        // ensure it's all zeroes before returning the number of bytes read.
         if self.is_empty() {
             let pad = (self.pad & 7) as usize;
 
@@ -117,6 +139,7 @@ impl Read for FileReader<'_, '_> {
     }
 }
 
+/// Reads the entries of a directory from the NAR stream
 pub struct DirReader<'a, 'r> {
     reader: &'a mut Reader<'r>,
     prev_name: Option<Vec<u8>>,
@@ -128,6 +151,7 @@ pub struct Entry<'a, 'r> {
 }
 
 impl<'a, 'r> DirReader<'a, 'r> {
+    /// initialize a new DirReader.
     fn new(reader: &'a mut Reader<'r>) -> Self {
         Self {
             reader,
@@ -135,18 +159,30 @@ impl<'a, 'r> DirReader<'a, 'r> {
         }
     }
 
+    /// returns the next [Entry] from the directory listing.
+    /// We don't implement Iterator here, because we can't really treat it like
+    /// that, and expecting to be able to use it like that will cause problems:
+    /// - we can't consume halfway and be fine
+    /// - we can't continue on an error and be fine
+    /// - once you get None back, you shouldn't try to keep reading.
+    /// - even if we'd know the number of elements upfront, we *must* read until
+    ///   we get a None.
     #[allow(clippy::should_implement_trait)]
     pub fn next(&mut self) -> io::Result<Option<Entry>> {
         if self.prev_name.is_some() {
             read::token(self.reader, &wire::TOK_PAR)?;
         }
 
+        // this is either a [wire::Entry::None] or a [wire::Entry::Some],
+        // we're done if it's the former.
         if let wire::Entry::None = read::tag(self.reader)? {
             return Ok(None);
         }
 
+        // new entry, read its name.
         let name = read::bytes(self.reader, wire::MAX_NAME_LEN)?;
 
+        // validate the name
         if name.is_empty()
             || name.contains(&0)
             || name.contains(&b'/')
@@ -160,6 +196,7 @@ impl<'a, 'r> DirReader<'a, 'r> {
             None => {
                 self.prev_name = Some(name.clone());
             }
+            // if there was previous entry, ensure it's sorted.
             Some(prev_name) => {
                 if *prev_name >= name {
                     return Err(InvalidData.into());
@@ -170,8 +207,10 @@ impl<'a, 'r> DirReader<'a, 'r> {
             }
         }
 
+        // consume more data until we could match on the next type again.
         read::token(self.reader, &wire::TOK_NOD)?;
 
+        // return the entry.
         Ok(Some(Entry {
             name,
             node: Node::new(&mut self.reader)?,
--- tvix/nix-compat/src/nar/wire/tag.rs
+++ tvix/nix-compat/src/nar/wire/tag.rs
@@ -1,3 +1,9 @@
+//! This module defines the [Tag] trait and the [make] macro,
+//! which provide efficient ways to match on tokens that can be distinguished by
+//! a single discriminant byte.
+
+/// A Tag is an enumeration of byte strings that in a particular position all
+/// have a unique byte.
 pub trait Tag: Sized {
     /// Discriminant offset
     const OFF: usize;
@@ -8,16 +14,21 @@ pub trait Tag: Sized {
     /// HACK: This is a workaround for const generics limitations.
     type Buf: AsMut<[u8]> + Send;
 
-    /// Make an instance of [Self::Buf]
+    /// Make an instance of [Self::Buf].
     fn make_buf() -> Self::Buf;
 
-    /// Convert a discriminant into the corresponding variant
+    /// Convert a discriminant into the corresponding variant.
     fn from_u8(x: u8) -> Option<Self>;
 
-    /// Convert a variant back into the wire representation
+    /// Convert a variant back into the wire representation.
     fn as_bytes(&self) -> &'static [u8];
 }
 
+/// Consumes an enum structure as described in the below grammar, and rewrites
+/// it.
+/// It validates that the `$off` specified points to a position
+/// in `$TOK` (normally a [u8]) that is a unique discriminant.
+/// Specifying a non-unique discriminant is a compile-time error.
 macro_rules! make {
     (
         $(
@@ -34,16 +45,21 @@ macro_rules! make {
             }
 
             impl Tag for $Enum {
+                /// Discriminant offset
                 const OFF: usize = $off;
+                /// Minimum variant length
                 const MIN: usize = tag::min_of(&[$($TOK.len()),+]);
 
+                /// Minimal suitably sized buffer for reading the wire representation
                 type Buf = [u8; tag::buf_of(&[$($TOK.len()),+])];
 
+                /// Make an instance of [Self::Buf].
                 #[inline(always)]
                 fn make_buf() -> Self::Buf {
                     [0u8; tag::buf_of(&[$($TOK.len()),+])]
                 }
 
+                /// Convert a discriminant into the corresponding variant.
                 #[inline(always)]
                 fn from_u8(x: u8) -> Option<Self> {
                     #[allow(non_upper_case_globals)]
@@ -59,6 +75,7 @@ macro_rules! make {
                     }
                 }
 
+                /// Convert a variant back into the wire representation.
                 #[inline(always)]
                 fn as_bytes(&self) -> &'static [u8] {
                     match self {
@@ -72,6 +89,7 @@ macro_rules! make {
 
 pub(crate) use make;
 
+/// Get the largest value in a slice.
 const fn max_of(mut xs: &[usize]) -> usize {
     let mut y = usize::MIN;
     while let &[x, ref tail @ ..] = xs {
@@ -81,6 +99,7 @@ const fn max_of(mut xs: &[usize]) -> usize {
     y
 }
 
+/// Get the smallest value in a slice.
 pub const fn min_of(mut xs: &[usize]) -> usize {
     let mut y = usize::MAX;
     while let &[x, ref tail @ ..] = xs {
@@ -90,6 +109,20 @@ pub const fn min_of(mut xs: &[usize]) -> usize {
     y
 }
 
+/// Returns a buffer size that's enough to read the first half (until MIN), and
+/// the second half.
+///
+/// Example:
+/// ```plain
+/// OFF = 16
+/// MIN = 24
+/// MAX = 64
+///
+/// BUF = max(MIN, MAX-MIN)
+///     = max(24, 64-24)
+///     = max(24, 40)
+///     = 40
+/// ```
 pub const fn buf_of(xs: &[usize]) -> usize {
     max_of(&[min_of(xs), max_of(xs) - min_of(xs)])
 }
