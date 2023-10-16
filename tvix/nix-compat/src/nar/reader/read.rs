f7ebdf68d6ee150d2e7ac73a28774f47ee5bc9a9
Florian Klink <flokli@flokli.de>
2023-10-16 23:24:38 +0100

    docs(tvix/nix-compat): add doc comments
    Change-Id: Idd1bcb0e1e870983e34e374cf15671bb1aaac6c9

--- tvix/nix-compat/src/nar/reader/read.rs
+++ tvix/nix-compat/src/nar/reader/read.rs
@@ -6,23 +6,29 @@ use std::io::{
 use super::Reader;
 use crate::nar::wire::Tag;
 
+/// Consume a u64 from the reader and return.
 pub fn u64(reader: &mut Reader) -> io::Result<u64> {
     let mut buf = [0; 8];
     reader.read_exact(&mut buf)?;
     Ok(u64::from_le_bytes(buf))
 }
 
+/// Consumes a byte string of up to max_len bytes and return.
 pub fn bytes(reader: &mut Reader, max_len: usize) -> io::Result<Vec<u8>> {
+    // read the length field, reject if it's bigger than allowed.
     let len = self::u64(reader)?;
     if len > max_len as u64 {
         return Err(InvalidData.into());
     }
+    // we know len fits in a usize now.
     let len = len as usize;
 
-    let buf_len = (len + 7) & !7;
+    // allocate a buf for it, and read data into it.
+    let buf_len = (len + 7) & !7; // padded length
     let mut buf = vec![0; buf_len];
     reader.read_exact(&mut buf)?;
 
+    // verify the padding is all zeros and drain it from the returned buf.
     for b in buf.drain(len..) {
         if b != 0 {
             return Err(InvalidData.into());
@@ -32,6 +38,7 @@ pub fn bytes(reader: &mut Reader, max_len: usize) -> io::Result<Vec<u8>> {
     Ok(buf)
 }
 
+/// Consume a known token from the reader.
 pub fn token<const N: usize>(reader: &mut Reader, token: &[u8; N]) -> io::Result<()> {
     let mut token = &token[..];
     let mut buf = [0u8; N];
@@ -62,6 +69,12 @@ pub fn token<const N: usize>(reader: &mut Reader, token: &[u8; N]) -> io::Result
     Ok(())
 }
 
+/// Read from the reader, return a Tag.
+/// We read two times, first we read [T::MIN] bytes.
+/// Compare by peeking at the discriminator position,
+/// then compare all bytes read.
+/// If this was all of the token size, we're already done.
+/// Otherwise, read and compare the rest.
 pub fn tag<T: Tag>(reader: &mut Reader) -> io::Result<T> {
     let mut buf = T::make_buf();
     let buf = buf.as_mut();
