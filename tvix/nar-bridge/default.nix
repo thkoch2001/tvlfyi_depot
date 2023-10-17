718af22dbba83ddebafdc849eb8645d646a0e163
Florian Klink <flokli@flokli.de>
2023-10-17 21:41:33 +0100

    chore(tvix/nar-bridge): update to latest go mod renames
     - code.tvl.fyi/tvix/castore/protos -> code.tvl.fyi/tvix/castore-go
     - code.tvl.fyi/tvix/store/protos -> code.tvl.fyi/tvix/store-go
    
     See cl/9791, cl/9792 for context.
    
    Change-Id: I44614c6ed40b9f52d9dcdea8e61fe2c3c830ce78
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/9793
    Reviewed-by: Connor Brewster <cbrewster@hey.com>
    Tested-by: BuildkiteCI
    Autosubmit: flokli <flokli@flokli.de>

--- tvix/nar-bridge/default.nix
+++ tvix/nar-bridge/default.nix
@@ -6,5 +6,5 @@ pkgs.buildGoModule {
   name = "nar-bridge";
   src = depot.third_party.gitignoreSource ./.;
 
-  vendorHash = "sha256-drbjsty1hhVAvn/R/AiVIOPQREzHOB+90VpuM5D5COc=";
+  vendorHash = "sha256-kvovi5HJafg3Um64XJxBYm392tih0P6C/SDyCLhKHIA=";
 }
