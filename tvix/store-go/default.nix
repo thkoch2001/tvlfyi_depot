45d8a78a8b39561622fafdc495edf2dbac49465d
Florian Klink <flokli@flokli.de>
2023-10-17 21:00:00 +0100

    chore(tvix): move regenerate scripts to passthru
    This keeps things isolated a bit more.
    
    Change-Id: I437f2f63ee1567c0cbc02298514ad4d89f1dce85
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/9790
    Autosubmit: flokli <flokli@flokli.de>
    Reviewed-by: Connor Brewster <cbrewster@hey.com>
    Tested-by: BuildkiteCI

--- tvix/store-go/default.nix
+++ tvix/store-go/default.nix
@@ -1,5 +1,10 @@
 { depot, pkgs, ... }:
 
+let
+  regenerate = pkgs.writeShellScriptBin "regenerate" ''
+    (cd $(git rev-parse --show-toplevel)/tvix/store-go && rm *.pb.go && cp ${depot.tvix.store.protos.go-bindings}/*.pb.go . && chmod +w *.pb.go)
+  '';
+in
 (pkgs.buildGoModule {
   name = "store-go";
   src = depot.third_party.gitignoreSource ./.;
@@ -10,10 +15,10 @@
       label = ":water_buffalo: ensure generated protobuf files match";
       needsOutput = true;
       command = pkgs.writeShellScript "pb-go-check" ''
-        ${depot.tvix.store-go-generate}
+        ${regenerate}
         if [[ -n "$(git status --porcelain -unormal)" ]]; then
             echo "-----------------------------"
-            echo ".pb.go files need to be updated, run //tvix:store-go-generate"
+            echo ".pb.go files need to be updated, mg run //tvix/store-go/generate"
             echo "-----------------------------"
             git status -unormal
             exit 1
@@ -22,4 +27,5 @@
       alwaysRun = true;
     };
   };
+  passthru.regenerate = regenerate;
 })
