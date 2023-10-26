0c5d6cbbb26c46e27af90ec1a7816e62187528b9
Vincent Ambo <tazjin@tvl.su>
2023-10-27 01:34:07 +0300

    chore(3p/sources): bump channels & overlays (2023-10-29)
    * //3p/nixpkgs: use py3status from stable for //users/grfn
    
    * //3p/overlays: update crate2nix vendor hash
      not sure why this hash is not stable if the package isn't updated,
      but whatever
    
    Change-Id: Ia7a13ac35b9c5534eefa53eff66e47ea1c32c62a

--- third_party/nixpkgs/default.nix
+++ third_party/nixpkgs/default.nix
@@ -48,8 +48,11 @@ let
   # Overlay for packages that should come from the stable channel
   # instead (e.g. because something is broken in unstable).
   # Use `stableNixpkgs` from above.
-  stableOverlay = _unstableSelf: _unstableSuper: {
-    # nothing is currently broken on unstable. yay!
+  stableOverlay = _unstableSelf: unstableSuper: {
+    # py3status is broken on nixpkgs-unstable (2023-10-29)
+    python3Packages = unstableSuper.python3Packages // {
+      py3status = stableNixpkgs.python3Packages.py3status;
+    };
   };
 
   # Overlay to expose the nixpkgs commits we are using to other Nix code.
