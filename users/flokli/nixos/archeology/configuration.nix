9e2f1f4583b8fdc43ba7762fdb4924e3f844f180
Florian Klink <flokli@flokli.de>
2023-10-30 11:56:17 +0200

    refactor(users/flokli): move common stuff to `archeology` profile
    Change-Id: I8470c0a2416c0c397e009affb44f8c7a852cd526
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/9837
    Reviewed-by: flokli <flokli@flokli.de>
    Tested-by: BuildkiteCI
    Autosubmit: flokli <flokli@flokli.de>

--- users/flokli/nixos/archeology/configuration.nix
+++ users/flokli/nixos/archeology/configuration.nix
@@ -2,12 +2,9 @@
 
 {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ../profiles/archeology.nix
   ];
-
-  # Use the TVL binary cache
-  tvl.cache.enable = true;
 
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
@@ -15,19 +12,8 @@
 
   boot.kernelParams = [ "console=ttyS0" ];
 
-  services.clickhouse.enable = true;
-
   networking.hostName = "archeology";
 
-  services.openssh.enable = true;
-
-  users.users.root.openssh.authorizedKeys.keys = [
-    "cert-authority ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCvb/7ojfcbKvHIyjnrNUOOgzy44tCkgXY9HLuyFta1jQOE9pFIK19B4dR9bOglPKf145CCL0mSFJNNqmNwwavU2uRn+TQrW+U1dQAk8Gt+gh3O49YE854hwwyMU+xD6bIuUdfxPr+r5al/Ov5Km28ZMlHOs3FoAP0hInK+eAibioxL5rVJOtgicrOVCkGoXEgnuG+LRbOYTwzdClhRUxiPjK8alCbcJQ53AeZHO4G6w9wTr+W5ILCfvW4OmUXCX01sKzaBiQuuFCF6M/H4LlnsPWLMra2twXxkOIhZblwC+lncps9lQaUgiD4koZeOCORvHW00G0L39ilFbbnVcL6Itp/m8RRWm/xRxS4RMnsdV/AhvpRLrhL3lfQ7E2oCeSM36v1S9rdg6a47zcnpL+ahG76Gz39Y7KmVRQciNx7ezbwxj3Q5lZtFykgdfGIAN+bT8ijXMO6m68g60i9Bz4IoMZGkiJGqMYLTxMQ+oRgR3Ro5lbj7E11YBHyeimoBYXYGHMkiuxopQZ7lIj3plxIzhmUlXJBA4jMw9KGHdYaLhaicIYhvQmCTAjrkt2HvxEe6lU8iws2Qv+pB6tAGundN36RVVWAckeQPZ4ZsgDP8V2FfibZ1nsrQ+zBKqaslYMAHs01Cf0Hm0PnCqagf230xaobu0iooNuXx44QKoDnB+w== edef"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPTVTXOutUZZjXLB0lUSgeKcSY/8mxKkC0ingGK1whD2 flokli"
-  ];
-
   system.stateVersion = "23.05"; # Did you read the comment?
-
-  environment.systemPackages = [ pkgs.helix pkgs.kakoune pkgs.tmux ];
 }
 
