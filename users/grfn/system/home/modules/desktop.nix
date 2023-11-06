81047cfcd7e527722a32dc7987cfb0e288ddd1eb
Aspen Smith <root@gws.fyi>
2023-11-06 10:39:39 -0500

    feat(grfn/system): Command to edit the current text input in emacs
    First pass at an xdotool-based command to edit the current text input in
    emacs
    
    Change-Id: I1e04612478292fe83083d197d481e034a9fce97f
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/9971
    Reviewed-by: grfn <grfn@gws.fyi>
    Autosubmit: grfn <grfn@gws.fyi>
    Tested-by: BuildkiteCI

--- users/grfn/system/home/modules/desktop.nix
+++ users/grfn/system/home/modules/desktop.nix
@@ -14,6 +14,23 @@
       # Slack support is broken as of 2023-06-15
       withSlack = false;
     })
+    (writeShellApplication {
+      name = "edit-input";
+
+      runtimeInputs = [ xdotool xclip ];
+      text = ''
+        set -euo pipefail
+
+        sleep 0.2
+        xdotool key ctrl+a ctrl+c
+        xclip -out -selection clipboard > /tmp/EDIT
+        emacsclient -c /tmp/EDIT
+        xclip -in -selection clipboard < /tmp/EDIT
+        sleep 0.2
+        xdotool key ctrl+v
+        rm /tmp/EDIT
+      '';
+    })
   ];
 
   services.syncthing.tray.enable = true;
