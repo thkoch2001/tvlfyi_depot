4662bce25572aaa97f7de600851202e28bf61bf3
Florian Klink <flokli@flokli.de>
2023-11-11 12:43:53 +0200

    fix(users/flokli/archeology): make clickhouse use ambient AWS creds
    Rather than picking up from clickhouse-specific config files, this gets
    it to pick up from the ambient environment, which is closer to (but not
    the same as) the AWS default credentials chain.
    
    Change-Id: I9c498c231974ed345c3e3d354ec230052b4d0ff2

--- users/flokli/archeology/default.nix
+++ users/flokli/archeology/default.nix
@@ -1,13 +1,34 @@
 { depot, pkgs, ... }:
 
+let
+  clickhouseConfigAWS = builtins.toFile "clickhouse-local.xml" ''
+    <clickhouse>
+        <s3>
+          <use_environment_credentials>true</use_environment_credentials>
+        </s3>
+    </clickhouse>
+  '';
+  # clickhouse has a very odd AWS config concept.
+  # Configure it to be a bit more sane.
+  clickhoseLocalFixedAWS = pkgs.runCommand "clickhouse-local-fixed"
+    {
+      nativeBuildInputs = [ pkgs.makeWrapper ];
+    } ''
+    mkdir -p $out/bin
+    makeWrapper ${pkgs.clickhouse}/bin/clickhouse-local $out/bin/clickhouse-local \
+      --append-flags "-C ${clickhouseConfigAWS}"
+  '';
+in
+
 depot.nix.readTree.drvTargets {
+  inherit clickhoseLocalFixedAWS;
   parse-bucket-logs = pkgs.runCommand "archeology-parse-bucket-logs"
     {
       nativeBuildInputs = [ pkgs.makeWrapper ];
     } ''
     mkdir -p $out/bin
     makeWrapper ${(pkgs.writers.writeRust "parse-bucket-logs-unwrapped" {} ./parse_bucket_logs.rs)} $out/bin/archeology-parse-bucket-logs \
-      --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.clickhouse ]}
+      --prefix PATH : ${pkgs.lib.makeBinPath [ clickhoseLocalFixedAWS ]}
   '';
 
   shell = pkgs.mkShell {
