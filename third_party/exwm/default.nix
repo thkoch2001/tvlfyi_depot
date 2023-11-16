a8d48d4d9c2c4f1f4c97007271c1bfc36d989c74
Vincent Ambo <tazjin@tvl.su>
2023-11-16 11:18:10 +0300

    fix(3p/exwm): fix EXWM build with depot sources
    Something changed in the machinery and broke the overriding. I didn't
    notice this, as a I was temporarily using an unpatched EXWM.
    
    Change-Id: I1a4e8ea63bd116d86a430e680c2b631474e9a0fe
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/10047
    Reviewed-by: tazjin <tazjin@tvl.su>
    Tested-by: BuildkiteCI

---
+++ third_party/exwm/default.nix
@@ -0,0 +1,14 @@
+{ depot, pkgs, lib, ... }:
+
+# special dance for overriding this into the fixed-point of emacs
+# packages, but having it be separately buildable.
+
+pkgs.emacsPackages.callPackage
+  ({ trivialBuild, xelb }: trivialBuild {
+    pname = "depot-exwm";
+    version = "canon";
+    src = ./.;
+
+    packageRequires = [ xelb ];
+  })
+{ }
