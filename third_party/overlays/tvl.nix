39deccb942c9f8ba5fc8aa5db0c6987aa9ea449f
Florian Klink <flokli@flokli.de>
2023-11-19 22:42:45 +0200

    chore(third_party/nixpkgs): add more patches to crate2nix
    Change-Id: I2c2c3d4722a69a1ce5a4f144d0c450d88f40856a

--- third_party/overlays/tvl.nix
+++ third_party/overlays/tvl.nix
@@ -118,6 +118,9 @@ depot.nix.readTree.drvTargets {
       # https://github.com/nix-community/crate2nix/pull/309
       ./patches/crate2nix-take-lndir-from-buildPackages.patch
       ./patches/crate2nix-skip-running-tests-when-cross-compiling.patch
+
+      # https://github.com/nix-community/crate2nix/pull/307
+      ./patches/crate2nix-fix-resolve-missing-target-features-to-null.patch
     ];
   });
 
