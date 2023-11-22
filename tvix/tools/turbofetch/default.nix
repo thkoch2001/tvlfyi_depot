360c98ce38a8eb94ab3955f4ed46c3a2e709bb74
Vincent Ambo <tazjin@tvl.su>
2023-11-22 10:41:28 +0300

    chore(3p/sources): bump channels & overlays (2023-11-27)
    * picked avrdude from stable channel
    * removed override for texlive, as the upstream fix is merged
    * picked awscli2 from stable channel
    * bump tdlib to 1.8.21 (new minimum for telega.el)
    * tvix/turbofetch: switch to nixpkgs-native mechanism for
      CARGO_MANIFEST_LINKS (whatever that is)
    
    Change-Id: Ic695721b5ca750b89d21cab7a257e1db682b23c0
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/10083
    Reviewed-by: flokli <flokli@flokli.de>
    Tested-by: BuildkiteCI
    Autosubmit: tazjin <tazjin@tvl.su>

--- tvix/tools/turbofetch/default.nix
+++ tvix/tools/turbofetch/default.nix
@@ -2,9 +2,9 @@
 
 (pkgs.callPackage ./Cargo.nix {
   defaultCrateOverrides = pkgs.defaultCrateOverrides // {
+
     ring = prev: {
-      # TODO(edef): implement CARGO_MANIFEST_LINKS in crate2nix
-      CARGO_MANIFEST_LINKS = ''ring_core_${lib.replaceStrings ["."] ["_"] prev.version}'';
+      links = ''ring_core_${lib.replaceStrings ["."] ["_"] prev.version}'';
     };
   };
 }).rootCrate.build.override {
