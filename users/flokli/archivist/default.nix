aab20c93bbdd365e928e52d8e1f7906a0a2f2e95
Florian Klink <flokli@flokli.de>
2023-11-23 21:10:42 +0200

    feat(users/flokli/archivist): init
    This introduces a new structure, as activities are moved to a separate
    AWS Account for reasons (tm).
    
    Change-Id: Ic310eca2dc0d4ee81bae8944238b26910067336a

---
+++ users/flokli/archivist/default.nix
@@ -0,0 +1,29 @@
+{
+  depot,
+  pkgs,
+  ...
+}:
+depot.nix.readTree.drvTargets {
+  shell = pkgs.mkShell {
+    name = "archivist-shell";
+    packages = with pkgs; [awscli2];
+
+    AWS_PROFILE = "archivist";
+    AWS_CONFIG_FILE = pkgs.writeText "aws-config" ''
+      [sso-session nixos]
+      sso_region = eu-north-1
+      sso_start_url = https://nixos.awsapps.com/start
+      sso_registration_scopes = sso:account:access
+
+      [profile "archivist"]
+      sso_session = nixos
+      sso_account_id = 286553126452
+      sso_role_name = AWSAdministratorAccess
+
+      [profile "archeologist"]
+      sso_session = nixos
+      sso_account_id = 080433136561
+      sso_role_name = archeologist
+    '';
+  };
+}
