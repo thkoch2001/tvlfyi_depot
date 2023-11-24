a77134fc0df3cfffcbfbd7fba23e522c016eac01
sterni <sternenseemann@systemli.org>
2023-11-24 16:55:46 +0100

    refactor(sterni/edwin): move generic settings into module
    These settings would also be applied to a machine that'd replace edwin,
    so it's useful to have them outside edwin's default.nix.
    
    Change-Id: I4e8f464118a103645e53909a87c6ee4446022fa3

--- users/sterni/machines/edwin/default.nix
+++ users/sterni/machines/edwin/default.nix
@@ -4,6 +4,8 @@
   imports = [
     # Third party modules we use
     "${depot.third_party.agenix.src}/modules/age.nix"
+    # Basic settings
+    ../../modules/common.nix
     # These modules touch things related to booting (filesystems, initrd network…)
     ./hardware.nix
     ./network.nix
@@ -18,70 +20,6 @@
   ];
 
   config = {
-    time.timeZone = "Europe/Berlin";
-
-    nix = {
-      package = pkgs.nix_2_3;
-      settings = {
-        trusted-public-keys = lib.mkAfter [
-          "headcounter.org:/7YANMvnQnyvcVB6rgFTdb8p5LG1OTXaO+21CaOSBzg="
-        ];
-        substituters = lib.mkAfter [
-          "https://hydra.build"
-        ];
-      };
-    };
-    tvl.cache.enable = true;
-
-    services = {
-      journald.extraConfig = ''
-        SystemMaxUse=1024M
-      '';
-
-      openssh.enable = true;
-    };
-
-    security.acme = {
-      defaults.email = builtins.getAttr "email" (
-        builtins.head (
-          builtins.filter (attrs: attrs.username == "sterni") depot.ops.users
-        )
-      );
-      acceptTerms = true;
-    };
-
-    programs = {
-      fish.enable = true;
-      mosh.enable = true;
-      tmux.enable = true;
-    };
-
-    environment.systemPackages = [
-      pkgs.weechat
-      pkgs.wget
-      pkgs.git
-      pkgs.stow
-      pkgs.htop
-      pkgs.foot.terminfo
-      pkgs.vim
-    ];
-
-    users = {
-      users = {
-        root.openssh.authorizedKeys.keys = depot.users.sterni.keys.all;
-        lukas = {
-          isNormalUser = true;
-          extraGroups = [ "wheel" "http" "git" ];
-          openssh.authorizedKeys.keys = depot.users.sterni.keys.all;
-          shell = "${pkgs.fish}/bin/fish";
-        };
-      };
-    };
-
-    nix.settings.trusted-users = [
-      "lukas"
-    ];
-
     system.stateVersion = "20.09";
   };
 }
