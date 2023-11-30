d5ad1815c66eeabc81bbbbb7b622781ae66d14e8
sterni <sternenseemann@systemli.org>
2023-11-30 17:26:20 +0100

    feat(sterni/ingeborg): set up monitoring via netdata
    Main objective was to get SMART/md monitoring working, alerts go (via
    some awful glue code) to #sterni.lv on hackint. Bot nick should also be
    registered in the future.
    
    Change-Id: Ia73c5a64ee9f6df62f5fbe21fc1606477e3d6e73

---
+++ users/sterni/machines/ingeborg/irccat.nix
@@ -0,0 +1,23 @@
+{ depot, config, pkgs, lib, ... }:
+
+{
+  imports = [
+    (depot.path.origSrc + "/ops/modules/irccat.nix")
+  ];
+
+  config = {
+    services.depot.irccat = {
+      enable = true;
+      secretsFile = builtins.toFile "empty.json" "{}"; # TODO(sterni): register
+      config = {
+        tcp.listen = ":4722"; # ircc
+        irc = {
+          server = "irc.hackint.org:6697";
+          tls = true;
+          nick = config.networking.hostName;
+          realname = "irccat";
+        };
+      };
+    };
+  };
+}
