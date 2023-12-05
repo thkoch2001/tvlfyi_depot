77c0b8e4e3a61772d5d7bc0edea150902ff03aec
Aspen Smith <root@gws.fyi>
2023-12-05 17:25:52 -0500

    fix(tvix): Represent strings as byte arrays
    C++ nix uses C-style zero-terminated char pointers to represent strings
    internally - however, up to this point, tvix has used Rust `String` and
    `str` for string values. Since those are required to be valid utf-8, we
    haven't been able to properly represent all the string values that Nix
    supports.
    
    To fix that, this change converts the internal representation of the
    NixString struct from `Box<str>` to `BString`, from the `bstr` crate -
    this is a wrapper around a `Vec<u8>` with extra functions for treating
    that byte vector as a "morally string-like" value, which is basically
    exactly what we need.
    
    Since this changes a pretty fundamental assumption about a pretty core
    type, there are a *lot* of changes in a lot of places to make this work,
    but I've tried to keep the general philosophy and intent of most of the
    code in most places intact. Most notably, there's nothing that's been
    done to make the derivation stuff in //tvix/glue work with non-utf8
    strings everywhere, instead opting to just convert to String/str when
    passing things into that - there *might* be something to be done there,
    but I don't know what the rules should be and I don't want to figure
    them out in this change.
    
    To deal with OS-native paths in a way that also works in WASM for
    tvixbolt, this also adds a dependency on the "os_str_bytes" crate.
    
    Fixes: b/189
    Fixes: b/337
    Change-Id: I5e6eb29c62f47dd91af954f5e12bfc3d186f5526

--- tvix/cli/src/main.rs
+++ tvix/cli/src/main.rs
@@ -218,7 +218,7 @@ fn run_file(mut path: PathBuf, args: &Args) {
 
 fn println_result(result: &Value, raw: bool) {
     if raw {
-        println!("{}", result.to_contextful_str().unwrap().as_str())
+        println!("{}", result.to_contextful_str().unwrap())
     } else {
         println!("=> {} :: {}", result, result.type_of())
     }
