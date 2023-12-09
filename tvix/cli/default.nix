6b0cbdb0da19986b68156fd70f575b11d755f5ed
Adam Joseph <adam@westernsemico.com>
2023-12-08 20:15:31 -0800

    feat(tvix/cli): add macrobenchmark
    This commit adds a simple MVP benchmark, built on our nix
    infrastructure instead of cargo.  It simply runs `tvix-eval` inside
    of GNU time, and prints the three essential statistics in a short
    JSON blob.
    
    You can run the benchmark with a simple `nix run`, like:
    
      nix run -f . tvix.cli.meta.ci.extraSteps.benchmark-hello
    
    Currently these blobs are stored only in the CI logs, which I'm sure
    get garbage-collected at some point.  We should be putting them in
    the git trailers, but that can wait for a future CL.
    
    I tried using `cargo bench` for this but found it incredibly
    frustrating.  Maybe I'm doing it wrong.  It seems to be designed for
    microbenchmarks only, and very hard to control.  It kept building
    all sorts of unnecessary stuff (like the tests), and unlike
    crate2nix it was doing all the builds on only a single machine
    instead of using more than one machine.  Worse, for that single
    machine it kept picking my laptop instead of my fast servers!  It
    seems excessively cargo-flavored for such a straightforward task.
    
    Benchmark: {"hello.outPath":{"kbytes":"244736","system":"0.36","user":"2.76"}}
    Change-Id: I85bc046ec551360284d7ecfc81a03914f0085909

--- tvix/cli/default.nix
+++ tvix/cli/default.nix
@@ -18,11 +18,60 @@ let
       echo "Output was correct."
     '';
   };
+
+  benchmark-gnutime-format-string =
+    description:
+    "Benchmark: " +
+    (builtins.toJSON {
+      "${description}" = {
+        kbytes = "%M";
+        system = "%S";
+        user = "%U";
+      };
+    });
+
 in
 
 (depot.tvix.crates.workspaceMembers.tvix-cli.build.override {
   runTests = true;
-}).overrideAttrs (_: {
+}).overrideAttrs (finalAttrs: previousAttrs:
+
+let
+  tvix-cli = finalAttrs.finalPackage;
+
+  # You can run the benchmark with a simple `nix run`, like:
+  #
+  #  nix run -f . tvix.cli.meta.ci.extraSteps.benchmark-nixpkgs-cross-hello-outpath
+  #
+  # TODO(amjoseph): store these results someplace more durable, like git trailers
+  #
+  mkExprBenchmark = { expr, description }:
+    let name = "tvix-cli-benchmark-${description}"; in
+    (pkgs.writeShellScriptBin name ''
+      ${lib.escapeShellArgs [
+        "${pkgs.time}/bin/time"
+        "--format" "${benchmark-gnutime-format-string description}"
+        "${tvix-cli}/bin/tvix"
+        "--no-warnings"
+        "-E" expr
+      ]}
+    '').overrideAttrs (finalAttrs: previousAttrs: {
+      passthru = (previousAttrs.passthru or { }) // {
+        ci = {
+          label = ":nix: benchmark ${description} in tvix";
+          needsOutput = true;
+          command = "${finalAttrs.finalPackage}/bin/${finalAttrs.meta.mainProgram}";
+        };
+      };
+    });
+
+  mkNixpkgsBenchmark = attrpath:
+    mkExprBenchmark {
+      description = builtins.replaceStrings [ ".drv" ] [ "-drv" ] attrpath;
+      expr = "(import ${pkgs.path} {}).${attrpath}";
+    };
+in
+{
   meta = {
     ci.extraSteps = {
       eval-nixpkgs-stdenv-drvpath = (mkNixpkgsEvalCheck "stdenv.drvPath" pkgs.stdenv.drvPath);
@@ -30,6 +79,8 @@ in
       eval-nixpkgs-hello-outpath = (mkNixpkgsEvalCheck "hello.outPath" pkgs.hello.outPath);
       eval-nixpkgs-cross-stdenv-outpath = (mkNixpkgsEvalCheck "pkgsCross.aarch64-multiplatform.stdenv.outPath" pkgs.pkgsCross.aarch64-multiplatform.stdenv.outPath);
       eval-nixpkgs-cross-hello-outpath = (mkNixpkgsEvalCheck "pkgsCross.aarch64-multiplatform.hello.outPath" pkgs.pkgsCross.aarch64-multiplatform.hello.outPath);
+      benchmark-hello = (mkNixpkgsBenchmark "hello.outPath");
+      benchmark-cross-hello = (mkNixpkgsBenchmark "pkgsCross.aarch64-multiplatform.hello.outPath");
     };
   };
 })
