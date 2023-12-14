5e820ab2798d14a019b3d31cf3728c895cb8ba0f
Aspen Smith <root@gws.fyi>
2023-12-14 14:21:39 -0500

    fix(tvix): Fix tvix nixpkgs eval benchmark
    Fix the nixpkgs eval hyperfine benchmark in tvix/scripts.bench.sh to:
    
    - Eval both a pinned (never-changing) nixpkgs commit and the nixpkgs
      commit used by the depot
    - Not depend on fetchTarball, which is not yet implemented.
    
    Change-Id: Id2ae18f983ab7327625320b5b16c082ecc369a49

--- tvix/scripts/bench.sh
+++ tvix/scripts/bench.sh
@@ -13,9 +13,15 @@ popd
 
 echo "Running tvix macrobenchmarks..."
 pushd "$(dirname "$(dirname "$0")")"
+
+depot_nixpkgs_path="$(nix eval --raw '("${((import ../third_party/sources {}).nixpkgs)}")')"
+pinned_nixpkgs_path="$(nix eval --raw '(builtins.fetchTarball {url = "https://github.com/NixOS/nixpkgs/archive/91050ea1e57e50388fa87a3302ba12d188ef723a.tar.gz"; sha256 = "1hf6cgaci1n186kkkjq106ryf8mmlq9vnwgfwh625wa8hfgdn4dm";})')"
+
 cargo build --release --bin tvix
 hyperfine --export-json ./results.json \
-    -n 'eval-nixpkgs-hello' "target/release/tvix -E '(import ../../nixpkgs {}).hello.outPath'" \
-    -n 'eval-nixpkgs-cross-hello' "target/release/tvix -E '(import ../../nixpkgs {}).pkgsCross.aarch64-multiplatform.hello.outPath'"
+    -n 'tvix-eval-depot-nixpkgs-hello' "target/release/tvix -E '(import ${depot_nixpkgs_path} {}).hello.outPath'" \
+    -n 'tvix-eval-depot-nixpkgs-cross-hello' "target/release/tvix -E '(import ${depot_nixpkgs_path} {}).pkgsCross.aarch64-multiplatform.hello.outPath'" \
+    -n 'tvix-eval-pinned-nixpkgs-hello' "target/release/tvix -E '(import ${pinned_nixpkgs_path} {}).hello.outPath'" \
+    -n 'tvix-eval-pinned-nixpkgs-cross-hello' "target/release/tvix -E '(import ${pinned_nixpkgs_path} {}).pkgsCross.aarch64-multiplatform.hello.outPath'" \
 windtunnel-cli report -f hyperfine-json ./results.json
 popd
