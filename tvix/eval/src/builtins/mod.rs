37fd5bd83071c344e49bffdb8523a36556721627
Ryan Lahfa <tvl@lahfa.xyz>
2023-12-25 22:06:52 +0100

    feat(tvix/eval): implement builtin `hasContext`
    `hasContext` is now functional.
    
    Change-Id: I23b128afc9150b833bc0d9b042d31fee35badadb

--- tvix/eval/src/builtins/mod.rs
+++ tvix/eval/src/builtins/mod.rs
@@ -550,6 +550,9 @@ mod pure_builtins {
         Ok(Value::Catchable(CatchableErrorKind::UnimplementedFeature(
             "hasContext".to_string(),
         )))
+    async fn builtin_hasContext(co: GenCo, e: Value) -> Result<Value, ErrorKind> {
+        let v = e.to_str()?;
+        Ok(Value::Bool(v.has_context()))
     }
 
     #[builtin("hashString")]
