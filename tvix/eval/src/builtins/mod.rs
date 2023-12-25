21124c9a1f767688dfbb8645d2dfdb1cfd855521
Ryan Lahfa <tvl@lahfa.xyz>
2023-12-26 00:40:55 +0100

    feat(tvix/eval): context-aware `split`
    Actually, it does not in C++ Nix, but oh well.
    
    Change-Id: I4fee1936600ce86d99d00893ca3f64013213935b

--- tvix/eval/src/builtins/mod.rs
+++ tvix/eval/src/builtins/mod.rs
@@ -973,7 +973,7 @@ mod pure_builtins {
 
     #[builtin("split")]
     async fn builtin_split(co: GenCo, regex: Value, str: Value) -> Result<Value, ErrorKind> {
-        let s = str.to_str()?;
+        let s = str.to_contextful_str()?;
         let text = s.as_str();
         let re = regex.to_str()?;
         let re: Regex = Regex::new(re.as_str()).unwrap();
@@ -984,7 +984,10 @@ mod pure_builtins {
 
         while let Some(thematch) = re.captures_read_at(&mut capture_locations, text, pos) {
             // push the unmatched characters preceding the match
-            ret.push_back(Value::from(&text[pos..thematch.start()]));
+            ret.push_back(Value::from(NixString::new_inherit_context_from(
+                &s,
+                &text[pos..thematch.start()],
+            )));
 
             // Push a list with one element for each capture
             // group in the regex, containing the characters
@@ -993,8 +996,10 @@ mod pure_builtins {
             let v: imbl::Vector<Value> = (1..num_captures)
                 .map(|i| capture_locations.get(i))
                 .map(|o| {
-                    o.map(|(start, end)| Value::from(&text[start..end]))
-                        .unwrap_or(Value::Null)
+                    o.map(|(start, end)| {
+                        Value::from(NixString::new_inherit_context_from(&s, &text[start..end]))
+                    })
+                    .unwrap_or(Value::Null)
                 })
                 .collect();
             ret.push_back(Value::List(NixList::from(v)));
@@ -1002,7 +1007,10 @@ mod pure_builtins {
         }
 
         // push the unmatched characters following the last match
-        ret.push_back(Value::from(&text[pos..]));
+        ret.push_back(Value::from(NixString::new_inherit_context_from(
+            &s,
+            &text[pos..],
+        )));
 
         Ok(Value::List(NixList::from(ret)))
     }
@@ -1047,7 +1055,7 @@ mod pure_builtins {
                 span,
             )
             .await?;
-        Ok(Value::Integer(s.to_str()?.as_str().len() as i64))
+        Ok(Value::Integer(s.to_contextful_str()?.as_str().len() as i64))
     }
 
     #[builtin("sub")]
