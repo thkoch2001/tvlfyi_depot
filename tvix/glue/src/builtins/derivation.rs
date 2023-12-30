6f6c693546064e852c75271ff78f3e223fb7ad47
Ryan Lahfa <tvl@lahfa.xyz>
2023-12-30 03:21:45 +0100

    fix(tvix/eval): catchable-aware builtins
    A bunch of operations in Tvix are not aware of catchable values
    and does not propagate them.
    
    In the meantime, as we wait for a better solution, we just offer this
    commit for moving the needle.
    
    Change-Id: Ic3f0e1550126b0847b597dfc1402c35e0eeef469

--- tvix/glue/src/builtins/derivation.rs
+++ tvix/glue/src/builtins/derivation.rs
@@ -215,6 +215,10 @@ pub(crate) mod derivation_builtins {
 
     #[builtin("placeholder")]
     async fn builtin_placeholder(co: GenCo, input: Value) -> Result<Value, ErrorKind> {
+        if input.is_catchable() {
+            return Ok(input);
+        }
+
         let placeholder = hash_placeholder(
             input
                 .to_str()
@@ -235,9 +239,19 @@ pub(crate) mod derivation_builtins {
         co: GenCo,
         input: Value,
     ) -> Result<Value, ErrorKind> {
+        if input.is_catchable() {
+            return Ok(input);
+        }
+
         let input = input.to_attrs()?;
         let name = generators::request_force(&co, input.select_required("name")?.clone())
-            .await
+            .await;
+
+        if name.is_catchable() {
+            return Ok(name);
+        }
+
+        let name = name
             .to_str()
             .context("determining derivation name")?;
 
@@ -445,6 +459,14 @@ pub(crate) mod derivation_builtins {
 
     #[builtin("toFile")]
     async fn builtin_to_file(co: GenCo, name: Value, content: Value) -> Result<Value, ErrorKind> {
+        if name.is_catchable() {
+            return Ok(name);
+        }
+
+        if content.is_catchable() {
+            return Ok(content);
+        }
+
         let name = name
             .to_str()
             .context("evaluating the `name` parameter of builtins.toFile")?;
