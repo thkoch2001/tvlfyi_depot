b2b1919afae4f424cbecefd92fe3c171d6f8d1e8
Ryan Lahfa <tvl@lahfa.xyz>
2023-12-30 03:21:15 +0100

    fix(tvix/eval): catchable-aware `attrNames`
    Change-Id: I002103bef7ea57dd1178afa859efd0b0690d7aeb

--- tvix/eval/src/builtins/mod.rs
+++ tvix/eval/src/builtins/mod.rs
@@ -145,6 +145,10 @@ mod pure_builtins {
 
     #[builtin("attrValues")]
     async fn builtin_attr_values(co: GenCo, set: Value) -> Result<Value, ErrorKind> {
+        if set.is_catchable() {
+            return Ok(set);
+        }
+
         let xs = set.to_attrs()?;
         let mut output = Vec::with_capacity(xs.len());
 
