7c4f69c892b65e82f17937da938810e170ad58aa
Ryan Lahfa <tvl@lahfa.xyz>
2024-01-06 00:41:40 +0100

    feat(tvix/castore): ingestion does DFS and invert it
    To make use of the filtering feature, we need to revert the internal walker to a real DFS.
    
    We will therefore just invert the whole tree by storing all of its
    contents in a level-keyed vector. This is horribly expensive in memory,
    but there's no other approach that can work because of a simple fact:
    
    When you encounter a directory, it's either a leaf or not, i.e. it
    contains subdirectories or not.
    
    To know this fact, you can:
    
    - wait until you notice subdirectories under it, i.e. you need to store
      any intermediate nodes you see in the meantime -> memory penalty.
    - getdents or readdir on it to determine *NOW* its subdirectories -> CPU
      penalty and I/O penalty.
    
    This is an implementation of the first proposal, we pay memory.
    
    In practice, we are paying O(#nb of nodes) in memory.
    
    There's a smarter albeit much more complicated algorithm that pays only
    O(\sum_i #siblings(p_i)) nodes where (p_1, ..., p_n) is the path to a leaf.
    
    which means for:
    
                 A
                / \
               B   C
              /   / \
             D   E   F
    
    We would never store D, E, F but only E, F at a given time.
    But we would still store B, C no matter what.
    
    Change-Id: I456ed1c3f0db493e018ba1182665d84bebe29c11

--- tvix/castore/src/import.rs
+++ tvix/castore/src/import.rs
@@ -16,6 +16,7 @@ use std::{
     path::{Path, PathBuf},
 };
 use tracing::instrument;
+use walkdir::DirEntry;
 use walkdir::WalkDir;
 
 #[derive(Debug, thiserror::Error)]
@@ -163,14 +164,13 @@ where
 
     let mut directory_putter = directory_service.put_multiple_start();
 
+    let mut entries_per_depths: Vec<Vec<DirEntry>> = vec![Vec::new()];
     for entry in WalkDir::new(p.as_ref())
         .follow_links(false)
         .follow_root_links(false)
-        // We need to process a directory's children before processing
-        // the directory itself in order to have all the data needed
-        // to compute the hash.
-        .contents_first(true)
+        .contents_first(false)
         .sort_by_file_name()
+        .into_iter()
     {
         // Entry could be a NotFound, if the root path specified does not exist.
         let entry = entry.map_err(|e| {
@@ -180,48 +180,70 @@ where
             )
         })?;
 
-        // process_entry wants an Option<Directory> in case the entry points to a directory.
-        // make sure to provide it.
-        // If the directory has contents, we already have it in
-        // `directories` due to the use of contents_first on WalkDir.
-        let maybe_directory: Option<Directory> = {
-            if entry.file_type().is_dir() {
-                Some(
-                    directories
-                        .entry(entry.path().to_path_buf())
-                        .or_default()
-                        .clone(),
-                )
-            } else {
-                None
-            }
-        };
+        if entry.depth() >= entries_per_depths.len() {
+            debug_assert!(
+                entry.depth() == entries_per_depths.len(),
+                "We should not be allocating more than one level at once, requested node at depth {}, had entries per depth containing {} levels",
+                entry.depth(),
+                entries_per_depths.len()
+            );
 
-        let node = process_entry(
-            blob_service.clone(),
-            &mut directory_putter,
-            &entry,
-            maybe_directory,
-        )
-        .await?;
-
-        if entry.depth() == 0 {
-            // Make sure all the directories are flushed.
-            if entry.file_type().is_dir() {
-                directory_putter.close().await?;
-            }
-            return Ok(node);
+            entries_per_depths.push(vec![entry]);
         } else {
-            // calculate the parent path, and make sure we register the node there.
-            // NOTE: entry.depth() > 0
-            let parent_path = entry.path().parent().unwrap().to_path_buf();
+            entries_per_depths[entry.depth()].push(entry);
+        }
+    }
 
-            // record node in parent directory, creating a new [proto:Directory] if not there yet.
-            let parent_directory = directories.entry(parent_path).or_default();
-            match node {
-                Node::Directory(e) => parent_directory.directories.push(e),
-                Node::File(e) => parent_directory.files.push(e),
-                Node::Symlink(e) => parent_directory.symlinks.push(e),
+    debug_assert!(!entries_per_depths[0].is_empty(), "No root node available!");
+
+    // We need to process a directory's children before processing
+    // the directory itself in order to have all the data needed
+    // to compute the hash.
+    for level in entries_per_depths.into_iter().rev() {
+        for entry in level.into_iter() {
+            // process_entry wants an Option<Directory> in case the entry points to a directory.
+            // make sure to provide it.
+            // If the directory has contents, we already have it in
+            // `directories` due to the use of contents_first on WalkDir.
+            let maybe_directory: Option<Directory> = {
+                if entry.file_type().is_dir() {
+                    Some(
+                        directories
+                            .entry(entry.path().to_path_buf())
+                            .or_default()
+                            .clone(),
+                    )
+                } else {
+                    None
+                }
+            };
+
+            let node = process_entry(
+                blob_service.clone(),
+                &mut directory_putter,
+                &entry,
+                maybe_directory,
+            )
+            .await?;
+
+            if entry.depth() == 0 {
+                // Make sure all the directories are flushed.
+                if entry.file_type().is_dir() {
+                    directory_putter.close().await?;
+                }
+                return Ok(node);
+            } else {
+                // calculate the parent path, and make sure we register the node there.
+                // NOTE: entry.depth() > 0
+                let parent_path = entry.path().parent().unwrap().to_path_buf();
+
+                // record node in parent directory, creating a new [proto:Directory] if not there yet.
+                let parent_directory = directories.entry(parent_path).or_default();
+                match node {
+                    Node::Directory(e) => parent_directory.directories.push(e),
+                    Node::File(e) => parent_directory.files.push(e),
+                    Node::Symlink(e) => parent_directory.symlinks.push(e),
+                }
             }
         }
     }
