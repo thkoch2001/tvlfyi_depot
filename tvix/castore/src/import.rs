20310a68c0a63790df935f6a8fbf1928d4a5d49b
Brian Olsen <brian@maven-group.org>
2024-01-09 21:51:11 +0100

    wip(tvix/castore): Ingest concurrently
    This makes import_path ingest each item concurrently but in a naive
    way that risks running out of file handles and loads the entire
    directory tree into memory.
    
    Change-Id: I7399b036b7c559a868e84b125f412b28c9f40a07

--- tvix/castore/src/import.rs
+++ tvix/castore/src/import.rs
@@ -10,12 +10,16 @@ use crate::Error as CastoreError;
 use async_recursion::async_recursion;
 use async_stream::try_stream;
 use bytes::Bytes;
+use futures::FutureExt;
+use futures::future::try_join3;
+use futures::future::try_join_all;
 use futures::stream::Peekable;
 use futures::Stream;
 use futures::StreamExt;
 use std::os::unix::ffi::OsStrExt;
 use std::pin::Pin;
 use std::{
+    future::Future,
     fmt::Debug,
     os::unix::prelude::PermissionsExt,
     path::{Path, PathBuf},
@@ -154,15 +158,15 @@ pub async fn ingest_directory<S, BS>(
     blob_service: BS,
     parent_path: Option<&'async_recursion Path>,
     directory_putter: Box<dyn DirectoryPutter>,
-) -> std::io::Result<(DirectoryNode, Peekable<S>)>
+) -> std::io::Result<(Pin<Box<dyn Future<Output=std::io::Result<DirectoryNode>> + Send>>, Peekable<S>)>
 where
-    BS: AsRef<dyn BlobService> + Clone + Send,
+    BS: AsRef<dyn BlobService> + Clone + Send + 'static,
     S: Stream<Item = Result<walkdir::DirEntry, walkdir::Error>> + Unpin + Send + 'static,
-
 {
-    let mut directory = Directory::default();
-
     let mut elems = elems;
+    let mut directories = Vec::new();
+    let mut symlinks = Vec::new();
+    let mut files = Vec::new();
 
     loop {
         let elem = Pin::new(&mut elems).peek().await;
@@ -192,15 +196,15 @@ where
                         directory_putter.clone(),
                     )
                     .await?;
-                    directory.directories.push(directory_node);
+
+                    directories.push(directory_node);
 
                     elems = rest_elems;
                 } else if e.file_type().is_file() {
-                    directory
-                        .files
-                        .push(ingest_blob(blob_service.clone(), e.path()).await?);
+                    files
+                        .push(ingest_blob(blob_service.clone(), e.path().to_path_buf()));
                 } else if e.file_type().is_symlink() {
-                    directory.symlinks.push(ingest_symlink(e.path()).await?);
+                    symlinks.push(ingest_symlink(e.path().to_path_buf()));
                 } else {
                     return Err(std::io::Error::new(
                         std::io::ErrorKind::InvalidData,
@@ -225,13 +229,27 @@ where
             Some(name) => Bytes::copy_from_slice(name.as_bytes()),
         },
     };
+    let fut = async move {
+        // Join all futures made when processing this directory node
+        let (directories, symlinks, files) = try_join3(
+            try_join_all(directories),
+            try_join_all(symlinks),
+            try_join_all(files)).await?;
 
-    Ok((
-        DirectoryNode {
+        let mut directory = Directory::default();
+        directory.directories = directories;
+        directory.symlinks = symlinks;
+        directory.files = files;
+
+        Ok(DirectoryNode {
             name,
             digest: directory.digest().into(),
             size: directory.size(),
-        },
+        })
+    }.boxed();
+
+    Ok((
+        fut,
         elems,
     ))
 }
@@ -242,7 +260,7 @@ pub async fn ingest_path<'a, BS, DS, P>(
     path: P,
 ) -> std::io::Result<Node>
 where
-    BS: AsRef<dyn BlobService> + Clone + Send,
+    BS: AsRef<dyn BlobService> + Clone + Send + 'static,
     DS: AsRef<dyn DirectoryService>,
     P: AsRef<Path> + Debug,
 {
@@ -270,7 +288,7 @@ where
 
         // TODO: we need to close the putter to ensure it's flushed!
 
-        Ok(Node::Directory(root_directory_node))
+        Ok(Node::Directory(root_directory_node.await?))
     } else if ft.is_file() {
         Ok(Node::File(
             ingest_blob(blob_service.clone(), path.as_ref()).await?,
