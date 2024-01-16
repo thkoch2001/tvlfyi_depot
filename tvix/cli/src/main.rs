7435c51abdb0564eb4c7d2eac80f40257a37f961
Florian Klink <flokli@flokli.de>
2024-01-16 13:14:07 +0200

    refactor(tvix/glue): add BuildService to TvixStoreIO
    TvixStoreIO triggers builds whenever IO into a not-yet-built store path
    is requested, if it knows how to build that path.
    
    Change-Id: If30e9db6be2f2a30cbc9d0576f357f3ecfa0d35a

--- tvix/cli/src/main.rs
+++ tvix/cli/src/main.rs
@@ -87,6 +87,7 @@ fn interpret(code: &str, path: Option<PathBuf>, args: &Args, explain: bool) -> b
         blob_service.clone(),
         directory_service.clone(),
         path_info_service,
+        Box::<DummyBuildService>::default() as Box<dyn BuildService>,
         tokio_runtime.handle().clone(),
     ));
 
