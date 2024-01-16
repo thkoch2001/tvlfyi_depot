23a93c3671e8b75c699f5cea5d1a1f357c84950b
Ryan Lahfa <tvl@lahfa.xyz>
2024-01-16 05:31:20 +0100

    feat(tvix/glue): insert `TvixStoreIO` inside derivation builtin state
    We propagate a generic `TvixStoreIO` inside the `state` of our
    derivation-specific builtins in the glue crate.
    
    The evaluators `io_handle` itself is using a Rc<dyn EvalIO>.
    
    Change-Id: I535c476f06b840858fa3070c4a237ece47f7a15b

--- tvix/glue/benches/eval.rs
+++ tvix/glue/benches/eval.rs
@@ -1,13 +1,13 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use lazy_static::lazy_static;
-use std::{cell::RefCell, env, rc::Rc, sync::Arc, time::Duration};
+use std::{env, rc::Rc, sync::Arc, time::Duration};
 use tvix_castore::{
     blobservice::{BlobService, MemoryBlobService},
     directoryservice::{DirectoryService, MemoryDirectoryService},
 };
+use tvix_eval::EvalIO;
 use tvix_glue::{
-    builtins::add_derivation_builtins, configure_nix_path, known_paths::KnownPaths,
-    tvix_store_io::TvixStoreIO,
+    builtins::add_derivation_builtins, configure_nix_path, tvix_store_io::TvixStoreIO,
 };
 use tvix_store::pathinfoservice::{MemoryPathInfoService, PathInfoService};
 
@@ -26,23 +26,25 @@ fn interpret(code: &str) {
     // TODO: this is a bit annoying.
     // It'd be nice if we could set this up once and then run evaluate() with a
     // piece of code. b/262
-    let mut eval = tvix_eval::Evaluation::new_impure();
 
-    let known_paths: Rc<RefCell<KnownPaths>> = Default::default();
-    add_derivation_builtins(&mut eval, known_paths);
+    // We assemble a complete store in memory.
+    let io = Rc::new(TvixStoreIO::new(
+        BLOB_SERVICE.clone(),
+        DIRECTORY_SERVICE.clone(),
+        PATH_INFO_SERVICE.clone(),
+        TOKIO_RUNTIME.handle().clone(),
+    ));
+
+    let mut eval = tvix_eval::Evaluation::new(io.clone() as Rc<dyn EvalIO>, false);
+
+    add_derivation_builtins(&mut eval, io);
+
     configure_nix_path(
         &mut eval,
         // The benchmark requires TVIX_BENCH_NIX_PATH to be set, so barf out
         // early, rather than benchmarking tvix returning an error.
         &Some(env::var("TVIX_BENCH_NIX_PATH").expect("TVIX_BENCH_NIX_PATH must be set")),
     );
-
-    eval.io_handle = Box::new(tvix_glue::tvix_io::TvixIO::new(TvixStoreIO::new(
-        BLOB_SERVICE.clone(),
-        DIRECTORY_SERVICE.clone(),
-        PATH_INFO_SERVICE.clone(),
-        TOKIO_RUNTIME.handle().clone(),
-    )));
 
     let result = eval.evaluate(code, None);
 
