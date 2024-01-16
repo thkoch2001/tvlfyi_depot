e110febfce1d1abd68c5e9ab286b46fbc623c9ec
Ryan Lahfa <tvl@lahfa.xyz>
2024-01-16 05:31:20 +0100

    feat(tvix/glue): insert `TvixStoreIO` inside derivation builtin state
    We propagate a generic `TvixStoreIO` inside the `state` of our derivation-specific builtins
    in the glue crate.
    
    For this, we need `'static` to ensure that all `BS, DS, PS` are **owned** are not simply references
    that could hold themselves references to short lifetimes.
    
    In practice, we always can own those type parameters by virtue of `Arc` being cheap to clone and protected
    behind the atomic reference counting.
    
    We take a shortcut in this implementation and do not offer any simple way to clone a `TvixStoreIO` which
    would make the setup more concise in the situations where we weaken the full capabilities by wrapping
    it in an more generic, albeit forgetful structure, e.g. an impl of EvalIO. The reason for this is
    this can be done in a further CL and it's not clear what setup style we want to adopt, so it's needless
    to add extra noise that we may undo later on.
    
    Also, we have to turn certain `Box<_>` for the path info service into `Arc<_>`, reasoning on this further:
    
    In those setups, we have two handles on `TvixStoreIO` pointing to the same `PathInfoService`, various things
    to note here.
    
    In practice, excluding concurrent store manipulations, there cannot be a concurrent usage of `PathInfoService`.
    
    - In `tvix-eval`, the VM is able to import paths and therefore make use of `PathInfoService` inadvertly.
    - In `tvix-glue`, we can produce derivations and files in the store and therefore make use of `PathInfoService`
      (inadvertly too via `EvalIO`, this can change with this commit.)
    
    Ideally, we should really only have _one_ TvixStoreIO in the address space, there's no need to have multiple
    ones in the setup of the evaluator machinery.
    
    Now, the question becomes: who should own the store? Do we need to make the EvalIO interface owned by the structure
    containing the `io_handle` or should we make it borrow it from something else? It's really unclear to me
    as we are not really exploited more advanced usecases than preparing an evaluator for evaluating and building
    stuff.
    
    We are not creating 2 evaluators in the same process, etc.
    
    Change-Id: I535c476f06b840858fa3070c4a237ece47f7a15b

--- tvix/glue/src/tvix_store_io.rs
+++ tvix/glue/src/tvix_store_io.rs
@@ -2,6 +2,7 @@
 
 use nix_compat::store_path::StorePath;
 use std::{
+    cell::RefCell,
     io,
     path::{Path, PathBuf},
 };
@@ -17,6 +18,8 @@ use tvix_castore::{
 };
 use tvix_store::pathinfoservice::PathInfoService;
 
+use crate::known_paths::KnownPaths;
+
 /// Implements [EvalIO], asking given [PathInfoService], [DirectoryService]
 /// and [BlobService].
 ///
@@ -24,12 +27,21 @@ use tvix_store::pathinfoservice::PathInfoService;
 /// This is to both cover cases of syntactically valid store paths, that exist
 /// on the filesystem (still managed by Nix), as well as being able to read
 /// files outside store paths.
+///
+/// This structure is also directly used by the derivation builtins
+/// and tightly coupled to it.
+///
+/// In the future, we may revisit that coupling and figure out how to generalize this interface and
+/// hide this implementation detail of the glue itself so that glue can be used with more than one
+/// implementation of "Tvix Store IO" which does not necessarily bring the concept of blob service,
+/// directory service or path info service.
 pub struct TvixStoreIO<BS, DS, PS> {
     blob_service: BS,
     directory_service: DS,
     path_info_service: PS,
     std_io: StdIO,
     tokio_handle: tokio::runtime::Handle,
+    pub(crate) known_paths: RefCell<KnownPaths>,
 }
 
 impl<BS, DS, PS> TvixStoreIO<BS, DS, PS>
@@ -49,6 +61,7 @@ where
             path_info_service,
             std_io: StdIO {},
             tokio_handle,
+            known_paths: Default::default(),
         }
     }
 
@@ -284,17 +297,17 @@ where
 
 #[cfg(test)]
 mod tests {
-    use std::{cell::RefCell, path::Path, rc::Rc, sync::Arc};
+    use std::{path::Path, rc::Rc, sync::Arc};
 
     use tempfile::TempDir;
     use tvix_castore::{
         blobservice::{BlobService, MemoryBlobService},
         directoryservice::{DirectoryService, MemoryDirectoryService},
     };
-    use tvix_eval::EvaluationResult;
+    use tvix_eval::{EvalIO, EvaluationResult};
     use tvix_store::pathinfoservice::{MemoryPathInfoService, PathInfoService};
 
-    use crate::{builtins::add_derivation_builtins, known_paths::KnownPaths};
+    use crate::builtins::add_derivation_builtins;
 
     use super::TvixStoreIO;
 
@@ -302,27 +315,24 @@ mod tests {
     /// Takes care of setting up the evaluator so it knows about the
     // `derivation` builtin.
     fn eval(str: &str) -> EvaluationResult {
-        let mut eval = tvix_eval::Evaluation::new_impure();
-
         let blob_service = Arc::new(MemoryBlobService::default()) as Arc<dyn BlobService>;
         let directory_service =
             Arc::new(MemoryDirectoryService::default()) as Arc<dyn DirectoryService>;
-        let path_info_service = Box::new(MemoryPathInfoService::new(
+        let path_info_service = Arc::new(MemoryPathInfoService::new(
             blob_service.clone(),
             directory_service.clone(),
-        )) as Box<dyn PathInfoService>;
+        )) as Arc<dyn PathInfoService>;
         let runtime = tokio::runtime::Runtime::new().unwrap();
 
-        eval.io_handle = Box::new(TvixStoreIO::new(
-            blob_service,
-            directory_service,
-            path_info_service,
+        let io = Rc::new(TvixStoreIO::new(
+            blob_service.clone(),
+            directory_service.clone(),
+            path_info_service.clone(),
             runtime.handle().clone(),
         ));
+        let mut eval = tvix_eval::Evaluation::new(io.clone() as Rc<dyn EvalIO>, true);
 
-        let known_paths: Rc<RefCell<KnownPaths>> = Default::default();
-
-        add_derivation_builtins(&mut eval, known_paths.clone());
+        add_derivation_builtins(&mut eval, io.clone());
 
         // run the evaluation itself.
         eval.evaluate(str, None)
