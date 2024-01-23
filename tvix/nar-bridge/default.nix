f9175c01e72a1b5457142dfe1a9a41b646e146f4
Connor Brewster <cbrewster@hey.com>
2024-01-22 20:37:22 -0600

    perf(tvix/store/nar-info): Support async blob upload
    Right now all blob uploads are performed synchronously, this means if a
    NAR contains many small files, the import time is dominated by round
    trip time to the blob service. For small files, we can buffer them in
    memory and upload them asynchronously to the blob service. Before
    returning we make sure to join all the uploads to make sure they
    complete successfully before responding OK.
    
    This reduces time to import a bash-interactive closure on my machine
    from 1m19s to 7s.
    
    Change-Id: Ica3695c159e6c8ad8769281ac20d037e3143e856

--- tvix/nar-bridge/default.nix
+++ tvix/nar-bridge/default.nix
@@ -6,5 +6,5 @@ pkgs.buildGoModule {
   name = "nar-bridge";
   src = depot.third_party.gitignoreSource ./.;
 
-  vendorHash = "sha256-9tEpICef6xCuRQwyXCg15KugcvknnP53WHotohcbeis=";
+  vendorHash = "sha256-nFwkq6GTQ1maw6rDeR69M6r+gvkd/7U0vuBfOFd5iFw=";
 }
