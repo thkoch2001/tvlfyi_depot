17a041dc6a2fc049de547b6128b6701408533cd6
Vincent Ambo <tazjin@tvl.su>
2024-02-08 23:50:49 +0300

    chore(tazjin/secrets): rotate key for agenix on khamovnik
    Change-Id: I36fa4e0530771c63fcee515a15ec6df76247105a

--- users/tazjin/secrets/secrets.nix
+++ users/tazjin/secrets/secrets.nix
@@ -4,7 +4,7 @@ let
     # local keys
     myKeys.tverskoy_ed25519
     myKeys.zamalek_ed25519
-    myKeys.khamovnik_yk
+    myKeys.khamovnik_agenix
     # koptevo
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMw2ZfdNZCXCOtbQNT6hztXCIkTcO9MBrOuDqMlmGOYK root@koptevo"
   ];
