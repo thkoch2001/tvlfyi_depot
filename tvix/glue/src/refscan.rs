8c0c6b4d2b7be36bee88bd7611773c0025570846
Aspen Smith <root@gws.fyi>
2024-02-10 15:25:39 -0500

    feat(tvix/glue): Add recursive output path scanner to refscan
    Add a new function `scan_node` to ReferenceScanner, which recursively
    scans all the child nodes of a Node in the content-addressible store for
    references to the known source paths. This will be used for scanning the
    outputs of a build for references to the possible inputs in that build.
    
    As implemented, this ought to work, but has a bunch of sub-optimal
    pieces, notably:
    
    - It buffers whole blobs in memory rather than streaming them
    - We don't use a trie when scanning for symlink targets
    - Allocation is generally pretty suboptimal
    
    All of those can be fixed once we have something that we know is
    working.
    
    Fixes: b/376
    Change-Id: Ia5949a8abf51c4ff50a4a8c78444001a45c74d4f

--- tvix/glue/src/refscan.rs
+++ tvix/glue/src/refscan.rs
@@ -8,6 +8,12 @@
 //! our fork of the `wu-mamber` crate.
 
 use std::collections::BTreeSet;
+use tokio::io::{self, AsyncReadExt};
+use tvix_castore::{
+    blobservice::BlobService,
+    directoryservice::DirectoryService,
+    proto::{node::Node, Directory},
+};
 use wu_manber::TwoByteWM;
 
 pub const STORE_PATH_LEN: usize = "/nix/store/00000000000000000000000000000000".len();
@@ -51,6 +57,76 @@ impl<P: Clone + Ord + AsRef<[u8]>> ReferenceScanner<P> {
         }
     }
 
+    /// Scan the given [`Node`] in the content-addressible store for all matches (including symbolic
+    /// links targeting the matches!) and collect them in the scanner
+    pub async fn scan_node<BS, DS>(
+        &mut self,
+        blob_service: BS,
+        directory_service: DS,
+        root_node: Node,
+    ) -> io::Result<()>
+    where
+        BS: AsRef<dyn BlobService> + Send + Sync + Clone + 'static,
+        DS: AsRef<dyn DirectoryService> + Send + Sync + Clone + 'static,
+    {
+        let directory_service = directory_service.as_ref();
+        let blob_service = blob_service.as_ref();
+
+        let mut stack = vec![root_node];
+        while let Some(node) = stack.pop() {
+            match node {
+                Node::Directory(dir) => {
+                    let Some(Directory {
+                        directories,
+                        files,
+                        symlinks,
+                    }) = directory_service
+                        .get(
+                            &dir.digest
+                                .try_into()
+                                .expect("Invalid directory digest in node"),
+                        )
+                        .await?
+                    else {
+                        continue; // TODO(aspen): Error if dir doesn't exist?
+                    };
+
+                    stack.extend(
+                        directories
+                            .into_iter()
+                            .map(Node::Directory)
+                            .chain(files.into_iter().map(Node::File))
+                            .chain(symlinks.into_iter().map(Node::Symlink)),
+                    )
+                }
+                Node::File(f) => {
+                    let Some(mut blob) = blob_service
+                        .open_read(&f.digest.try_into().expect("Invalid file digest in node"))
+                        .await?
+                    else {
+                        continue; // TODO(aspen): Error if dir doesn't exist?
+                    };
+
+                    // TODO(aspen): with_capacity? maybe BlobReader should have size_hint?
+                    let mut contents = vec![];
+                    // TODO(aspen): Is streaming possible?
+                    blob.read_to_end(&mut contents).await?;
+                    self.scan(contents);
+                }
+                Node::Symlink(sl) => {
+                    if let Some(idx) = self.candidates.iter().position(|p| {
+                        let p = p.as_ref();
+                        &sl.target[..p.len()] == p
+                    }) {
+                        self.matches.push(idx);
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
     /// Finalise the reference scanner and return the resulting matches.
     pub fn finalise(self) -> BTreeSet<P> {
         self.matches
@@ -64,52 +140,123 @@ impl<P: Clone + Ord + AsRef<[u8]>> ReferenceScanner<P> {
 mod tests {
     use super::*;
 
-    // The actual derivation of `nixpkgs.hello`.
-    const HELLO_DRV: &str = r#"Derive([("out","/nix/store/33l4p0pn0mybmqzaxfkpppyh7vx1c74p-hello-2.12.1","","")],[("/nix/store/6z1jfnqqgyqr221zgbpm30v91yfj3r45-bash-5.1-p16.drv",["out"]),("/nix/store/ap9g09fxbicj836zm88d56dn3ff4clxl-stdenv-linux.drv",["out"]),("/nix/store/pf80kikyxr63wrw56k00i1kw6ba76qik-hello-2.12.1.tar.gz.drv",["out"])],["/nix/store/9krlzvny65gdc8s7kpb6lkx8cd02c25b-default-builder.sh"],"x86_64-linux","/nix/store/4xw8n979xpivdc46a9ndcvyhwgif00hz-bash-5.1-p16/bin/bash",["-e","/nix/store/9krlzvny65gdc8s7kpb6lkx8cd02c25b-default-builder.sh"],[("buildInputs",""),("builder","/nix/store/4xw8n979xpivdc46a9ndcvyhwgif00hz-bash-5.1-p16/bin/bash"),("cmakeFlags",""),("configureFlags",""),("depsBuildBuild",""),("depsBuildBuildPropagated",""),("depsBuildTarget",""),("depsBuildTargetPropagated",""),("depsHostHost",""),("depsHostHostPropagated",""),("depsTargetTarget",""),("depsTargetTargetPropagated",""),("doCheck","1"),("doInstallCheck",""),("mesonFlags",""),("name","hello-2.12.1"),("nativeBuildInputs",""),("out","/nix/store/33l4p0pn0mybmqzaxfkpppyh7vx1c74p-hello-2.12.1"),("outputs","out"),("patches",""),("pname","hello"),("propagatedBuildInputs",""),("propagatedNativeBuildInputs",""),("src","/nix/store/pa10z4ngm0g83kx9mssrqzz30s84vq7k-hello-2.12.1.tar.gz"),("stdenv","/nix/store/cp65c8nk29qq5cl1wyy5qyw103cwmax7-stdenv-linux"),("strictDeps",""),("system","x86_64-linux"),("version","2.12.1")])"#;
+    mod scan_strings {
+        use super::*;
 
-    #[test]
-    fn test_no_patterns() {
-        let mut scanner: ReferenceScanner<String> = ReferenceScanner::new(vec![]);
+        // The actual derivation of `nixpkgs.hello`.
+        const HELLO_DRV: &str = r#"Derive([("out","/nix/store/33l4p0pn0mybmqzaxfkpppyh7vx1c74p-hello-2.12.1","","")],[("/nix/store/6z1jfnqqgyqr221zgbpm30v91yfj3r45-bash-5.1-p16.drv",["out"]),("/nix/store/ap9g09fxbicj836zm88d56dn3ff4clxl-stdenv-linux.drv",["out"]),("/nix/store/pf80kikyxr63wrw56k00i1kw6ba76qik-hello-2.12.1.tar.gz.drv",["out"])],["/nix/store/9krlzvny65gdc8s7kpb6lkx8cd02c25b-default-builder.sh"],"x86_64-linux","/nix/store/4xw8n979xpivdc46a9ndcvyhwgif00hz-bash-5.1-p16/bin/bash",["-e","/nix/store/9krlzvny65gdc8s7kpb6lkx8cd02c25b-default-builder.sh"],[("buildInputs",""),("builder","/nix/store/4xw8n979xpivdc46a9ndcvyhwgif00hz-bash-5.1-p16/bin/bash"),("cmakeFlags",""),("configureFlags",""),("depsBuildBuild",""),("depsBuildBuildPropagated",""),("depsBuildTarget",""),("depsBuildTargetPropagated",""),("depsHostHost",""),("depsHostHostPropagated",""),("depsTargetTarget",""),("depsTargetTargetPropagated",""),("doCheck","1"),("doInstallCheck",""),("mesonFlags",""),("name","hello-2.12.1"),("nativeBuildInputs",""),("out","/nix/store/33l4p0pn0mybmqzaxfkpppyh7vx1c74p-hello-2.12.1"),("outputs","out"),("patches",""),("pname","hello"),("propagatedBuildInputs",""),("propagatedNativeBuildInputs",""),("src","/nix/store/pa10z4ngm0g83kx9mssrqzz30s84vq7k-hello-2.12.1.tar.gz"),("stdenv","/nix/store/cp65c8nk29qq5cl1wyy5qyw103cwmax7-stdenv-linux"),("strictDeps",""),("system","x86_64-linux"),("version","2.12.1")])"#;
 
-        scanner.scan(HELLO_DRV);
+        #[test]
+        fn test_no_patterns() {
+            let mut scanner: ReferenceScanner<String> = ReferenceScanner::new(vec![]);
 
-        let result = scanner.finalise();
+            scanner.scan(HELLO_DRV);
 
-        assert_eq!(result.len(), 0);
+            let result = scanner.finalise();
+
+            assert_eq!(result.len(), 0);
+        }
+
+        #[test]
+        fn test_single_match() {
+            let mut scanner = ReferenceScanner::new(vec![
+                "/nix/store/4xw8n979xpivdc46a9ndcvyhwgif00hz-bash-5.1-p16".to_string(),
+            ]);
+            scanner.scan(HELLO_DRV);
+
+            let result = scanner.finalise();
+
+            assert_eq!(result.len(), 1);
+            assert!(result.contains("/nix/store/4xw8n979xpivdc46a9ndcvyhwgif00hz-bash-5.1-p16"));
+        }
+
+        #[test]
+        fn test_multiple_matches() {
+            let candidates = vec![
+                // these exist in the drv:
+                "/nix/store/33l4p0pn0mybmqzaxfkpppyh7vx1c74p-hello-2.12.1".to_string(),
+                "/nix/store/pf80kikyxr63wrw56k00i1kw6ba76qik-hello-2.12.1.tar.gz.drv".to_string(),
+                "/nix/store/cp65c8nk29qq5cl1wyy5qyw103cwmax7-stdenv-linux".to_string(),
+                // this doesn't:
+                "/nix/store/fn7zvafq26f0c8b17brs7s95s10ibfzs-emacs-28.2.drv".to_string(),
+            ];
+
+            let mut scanner = ReferenceScanner::new(candidates.clone());
+            scanner.scan(HELLO_DRV);
+
+            let result = scanner.finalise();
+            assert_eq!(result.len(), 3);
+
+            for c in candidates[..3].iter() {
+                assert!(result.contains(c));
+            }
+        }
     }
 
-    #[test]
-    fn test_single_match() {
-        let mut scanner = ReferenceScanner::new(vec![
-            "/nix/store/4xw8n979xpivdc46a9ndcvyhwgif00hz-bash-5.1-p16".to_string(),
-        ]);
-        scanner.scan(HELLO_DRV);
+    mod scan_nodes {
+        use std::os::unix;
+        use std::sync::Arc;
+        use std::{fs, path::Path};
 
-        let result = scanner.finalise();
+        use tempfile::TempDir;
+        use tvix_castore::utils::{gen_blob_service, gen_directory_service};
 
-        assert_eq!(result.len(), 1);
-        assert!(result.contains("/nix/store/4xw8n979xpivdc46a9ndcvyhwgif00hz-bash-5.1-p16"));
-    }
+        use super::*;
 
-    #[test]
-    fn test_multiple_matches() {
-        let candidates = vec![
-            // these exist in the drv:
-            "/nix/store/33l4p0pn0mybmqzaxfkpppyh7vx1c74p-hello-2.12.1".to_string(),
-            "/nix/store/pf80kikyxr63wrw56k00i1kw6ba76qik-hello-2.12.1.tar.gz.drv".to_string(),
-            "/nix/store/cp65c8nk29qq5cl1wyy5qyw103cwmax7-stdenv-linux".to_string(),
-            // this doesn't:
-            "/nix/store/fn7zvafq26f0c8b17brs7s95s10ibfzs-emacs-28.2.drv".to_string(),
-        ];
+        const HELLO: &str = "/nix/store/sbldylj3clbkc0aqvjjzfa6slp4zdvlj-hello-2.12.1";
 
-        let mut scanner = ReferenceScanner::new(candidates.clone());
-        scanner.scan(HELLO_DRV);
+        #[tokio::test]
+        async fn single_reference() {
+            let blob_service = Arc::<dyn BlobService>::from(gen_blob_service());
+            let directory_service = Arc::<dyn DirectoryService>::from(gen_directory_service());
 
-        let result = scanner.finalise();
-        assert_eq!(result.len(), 3);
+            let dir = TempDir::new().unwrap();
+            fs::write(dir.path().join("file"), format!("referencing: {HELLO}")).unwrap();
 
-        for c in candidates[..3].iter() {
-            assert!(result.contains(c));
+            let dir_node = tvix_castore::import::ingest_path(
+                blob_service.clone(),
+                directory_service.clone(),
+                dir.path(),
+            )
+            .await
+            .unwrap();
+
+            let mut scanner = ReferenceScanner::new(vec![HELLO]);
+            scanner
+                .scan_node(blob_service.clone(), directory_service.clone(), dir_node)
+                .await
+                .unwrap();
+            let res = scanner.finalise();
+            assert_eq!(res, BTreeSet::from([HELLO]))
+        }
+
+        #[tokio::test]
+        async fn symlink_reference() {
+            let blob_service = Arc::<dyn BlobService>::from(gen_blob_service());
+            let directory_service = Arc::<dyn DirectoryService>::from(gen_directory_service());
+
+            let dir = TempDir::new().unwrap();
+            unix::fs::symlink(
+                Path::new(HELLO).join("bin").join("hello"),
+                dir.path().join("file"),
+            )
+            .unwrap();
+
+            let dir_node = tvix_castore::import::ingest_path(
+                blob_service.clone(),
+                directory_service.clone(),
+                dir.path(),
+            )
+            .await
+            .unwrap();
+
+            let mut scanner = ReferenceScanner::new(vec![HELLO]);
+            scanner
+                .scan_node(blob_service.clone(), directory_service.clone(), dir_node)
+                .await
+                .unwrap();
+            let res = scanner.finalise();
+            assert_eq!(res, BTreeSet::from([HELLO]))
         }
     }
 }
