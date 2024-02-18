c04c56f1fb92c4f543129610dd9ccb0626396f91
Aspen Smith <root@gws.fyi>
2024-02-18 13:57:12 -0500

    feat(nix/writeTree): Init
    Add nix/writeTree, a function to make a derivation to build a directory
    structure from a Nix attribute set.
    
    Change-Id: I9c0fc91611a55a20ad33de6f2b27abde4b6abd21

---
+++ nix/writeTree/default.nix
@@ -0,0 +1,35 @@
+{ depot, lib, pkgs, ... }:
+let
+  inherit (lib) fix pipe mapAttrsToList isAttrs concatLines;
+
+  inherit (depot.nix.utils) isDirectory isRegularFile;
+
+  writeTreeAtPath = path: tree:
+    pipe tree [
+      (mapAttrsToList (k: v:
+        if isRegularFile v then
+          "cp ${v} $out/${path}${k}"
+        else if isDirectory v then ''
+          mkdir -p $out/${path}
+          cp -r ${v} $out/${path}${k}
+        '' else if isAttrs v then
+          writeTreeAtPath "${path}/${k}" v
+        else
+          throw "invalid type (expected file, directory, or attrs)"))
+      concatLines
+    ];
+
+  /* Create a directory tree specified by a Nix attribute set structure.
+
+     Each value in `tree` should either be a file, a directory, or another tree
+     attribute set. Those paths will be written to a directory tree
+     corresponding to the structure of the attribute set.
+
+     Type: string -> attrSet -> derivation
+  */
+in
+name: tree:
+pkgs.runCommandLocal name { } ''
+  mkdir $out
+  ${(writeTreeAtPath "" tree)}
+''
