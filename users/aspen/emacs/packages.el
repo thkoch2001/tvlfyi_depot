b1faa689e106a079d9ab206c4c42863a5f4c2130
Aspen Smith <grfn@gws.fyi>
2024-02-20 16:31:30 -0500

    feat(aspen/emacs): Begin a complete emacs config revamp
    I'm still on doom, but without all the cruft I've accumulated over the
    last ~6 years it's actually good and fast and nice. Also, the config is
    literate now!
    
    The old emacs is still in .emacs.d, since occasionally I want to
    reference it, but eventually I'll just get rid of it
    
    Change-Id: Icda840d798594f7e6b188dba044494597d5f6043

---
+++ users/aspen/emacs/packages.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; -*-
+;;; $DOOMDIR/packages.el
+
+(package! dash)
+(package! paxedit)
+(package! predd
+  :recipe (:host github :repo "skeeto/predd"))
+(package! alert)
+(package! flycheck-clojure)
+(package! evil-matchit)
