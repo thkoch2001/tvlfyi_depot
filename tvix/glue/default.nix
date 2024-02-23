e5c6158211e7e84963d5014ab2bbda5e62406c76
Aspen Smith <root@gws.fyi>
2024-02-23 10:09:20 -0500

    feat(tvix/glue): Implement builtins.fetchurl
    Implement the fetchurl builtin, and lay the groundwork for implementing
    the fetchTarball builtin (which works very similarly, and is implemented
    using almost the same code in C++ nix).
    
    An overview of how this works:
    
    1. First, we check if the store path that *would* result from the
       download already exists in the store - if it does, we just return
       that
    2. If we need to download the URL, TvixStoreIO has an `http_client:
       reqwest::Client` field now which we use to make the request
    3. As we're downloading the blob, we hash the data incrementally into a
       SHA256 hasher
    4. We compare the hash against the expected hash (if any) and bail out
       if it doesn't match
    5. Finally, we put the blob in the store and return the store path
    
    Since the logic is very similar, this commit also implements a *chunk*
    of `fetchTarball` (though the actual implementation will likely include
    a refactor to some of the code reuse here).
    
    The main thing that's missing here is caching of downloaded blobs when
    fetchurl is called without a hash - I've opened b/381 to track the TODO
    there.
    
    Adding the `SSL_CERT_FILE` here is necessary to teach reqwest how to
    load it during tests - see 1c16dee20 (feat(tvix/store): use reqwests'
    rustls-native-roots feature, 2024-03-03) for  more info.
    
    Change-Id: I83c4abbc7c0c3bfe92461917e23d6d3430fbf137

--- tvix/glue/default.nix
+++ tvix/glue/default.nix
@@ -1,5 +1,8 @@
-{ depot, ... }:
+{ depot, pkgs, ... }:
 
 (depot.tvix.crates.workspaceMembers.tvix-glue.build.override {
   runTests = true;
+  testPreRun = ''
+    export SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt;
+  '';
 })
