de06a60122db5e0f6e9b39080639fc8af413c8cb
Profpatsch <mail@profpatsch.de>
2024-03-16 13:20:12 +0100

    chore(third_party/haskell): pa-label: 0.1.0.1 -> 0.1.1.0
    Change-Id: I3ef2ae9b92f2c798842ae001588b2c6d9f3ab971

--- third_party/overlays/haskell/extra-pkgs/pa-label.nix
+++ third_party/overlays/haskell/extra-pkgs/pa-label.nix
@@ -1,8 +1,8 @@
 { mkDerivation, base, lib }:
 mkDerivation {
   pname = "pa-label";
-  version = "0.1.0.1";
-  sha256 = "0131ab7718d910a94cd8cc881e51b7371a060dadfeabc8fd78513a7f27ee8d35";
+  version = "0.1.1.0";
+  sha256 = "b40183900c045641c0632ed8e53a326c0c0e9c2806568613c03b3131d9016183";
   libraryHaskellDepends = [ base ];
   homepage = "https://github.com/possehl-analytics/pa-hackage";
   description = "Labels, and labelled tuples and enums (GHC >9.2)";
