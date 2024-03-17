9b303a646913a283fb27fba0228361dabe7fcf5b
Florian Klink <flokli@flokli.de>
2024-03-17 16:11:22 +0200

    chore(users/flokli/ipu6-softisp): refresh libcamera patches
    Refresh them with the patches from
    https://patchwork.libcamera.org/cover/19663/.
    
    This is still based off v0.2.0.
    
    Change-Id: I875fd64e3bb71a95c92af1108a23d27c0f3494e0

--- users/flokli/ipu6-softisp/config.nix
+++ users/flokli/ipu6-softisp/config.nix
@@ -3,39 +3,36 @@
 let
   libcamera = pkgs.libcamera.overrideAttrs (old: {
     mesonFlags = old.mesonFlags or [ ] ++ [
-      "-Dpipelines=simple/simple,ipu3,uvcvideo"
-      "-Dipas=simple/simple,ipu3"
+      "-Dpipelines=simple,ipu3,uvcvideo"
+      "-Dipas=simple,ipu3"
     ];
 
     # This is
     # https://copr-dist-git.fedorainfracloud.org/cgit/jwrdegoede/ipu6-softisp/libcamera.git/plain/libcamera-0.2.0-softisp.patch?h=f39&id=60e6b3d5e366a360a75942073dc0d642e4900982,
     # but manually piped to git and back, as some renames were not processed properly.
+    # It was later refreshed with https://patchwork.libcamera.org/cover/19663/
     patches = old.patches or [ ] ++ [
       ./libcamera/0001-libcamera-pipeline-simple-fix-size-adjustment-in-val.patch
       ./libcamera/0002-libcamera-internal-Move-dma_heaps.-h-cpp-to-common-d.patch
       ./libcamera/0003-libcamera-dma_heaps-extend-DmaHeap-class-to-support-.patch
       ./libcamera/0004-libcamera-internal-Move-SharedMemObject-class-to-a-c.patch
-      ./libcamera/0005-libcamera-internal-Document-the-SharedMemObject-clas.patch
-      ./libcamera/0006-libcamera-introduce-SoftwareIsp-class.patch
-      ./libcamera/0007-libcamera-software_isp-Add-SwStats-base-class.patch
-      ./libcamera/0008-libcamera-software_isp-Add-SwStatsCpu-class.patch
-      ./libcamera/0009-libcamera-software_isp-Add-Debayer-base-class.patch
-      ./libcamera/0010-libcamera-software_isp-Add-DebayerCpu-class.patch
-      ./libcamera/0011-libcamera-ipa-add-Soft-IPA-common-files.patch
-      ./libcamera/0012-libcamera-ipa-Soft-IPA-add-a-Simple-Soft-IPA-impleme.patch
-      ./libcamera/0013-libcamera-software_isp-add-Simple-SoftwareIsp-implem.patch
-      ./libcamera/0014-libcamera-pipeline-simple-rename-converterBuffers_-a.patch
-      ./libcamera/0015-libcamera-pipeline-simple-enable-use-of-Soft-ISP-and.patch
-      ./libcamera/0016-libcamera-swstats_cpu-Add-support-for-8-10-and-12-bp.patch
-      ./libcamera/0017-libcamera-debayer_cpu-Add-support-for-8-10-and-12-bp.patch
-      ./libcamera/0018-libcamera-debayer_cpu-Add-BGR888-output-support.patch
-      ./libcamera/0019-libcamera-pipeline-simple-Enable-simplepipeline-for-.patch
-      ./libcamera/0020-libcamera-Add-support-for-IGIG_GBGR_IGIG_GRGB-bayer-.patch
-      ./libcamera/0021-libcamera-swstats_cpu-Add-support-for-10bpp-IGIG_GBG.patch
-      ./libcamera/0022-libcamera-debayer_cpu-Add-support-for-10bpp-IGIG_GBG.patch
-      ./libcamera/0023-libcamera-Add-Software-ISP-benchmarking-documentatio.patch
-      ./libcamera/0024-ov01a1s-HACK.patch
-      ./libcamera/0025-libcamera-debayer_cpu-Make-the-minimum-size-1280x720.patch
+      ./libcamera/0005-libcamera-shared_mem_object-reorganize-the-code-and-.patch
+      ./libcamera/0006-libcamera-software_isp-Add-SwStatsCpu-class.patch
+      ./libcamera/0007-libcamera-software_isp-Add-Debayer-base-class.patch
+      ./libcamera/0008-libcamera-software_isp-Add-DebayerCpu-class.patch
+      ./libcamera/0009-libcamera-ipa-add-Soft-IPA.patch
+      ./libcamera/0010-libcamera-introduce-SoftwareIsp.patch
+      ./libcamera/0011-libcamera-pipeline-simple-rename-converterBuffers_-a.patch
+      ./libcamera/0012-libcamera-pipeline-simple-enable-use-of-Soft-ISP-and.patch
+      ./libcamera/0013-libcamera-swstats_cpu-Add-support-for-8-10-and-12-bp.patch
+      ./libcamera/0014-libcamera-debayer_cpu-Add-support-for-8-10-and-12-bp.patch
+      ./libcamera/0015-libcamera-debayer_cpu-Add-BGR888-output-support.patch
+      ./libcamera/0016-libcamera-Add-support-for-IGIG_GBGR_IGIG_GRGB-bayer-.patch
+      ./libcamera/0017-libcamera-Add-Software-ISP-benchmarking-documentatio.patch
+      ./libcamera/0018-libcamera-software_isp-Apply-black-level-compensatio.patch
+      ./libcamera/0019-libcamera-Soft-IPA-use-CameraSensorHelper-for-analog.patch
+      ./libcamera/0020-ov01a1s-HACK.patch
+      ./libcamera/0021-libcamera-debayer_cpu-Make-the-minimum-size-1280x720.patch
     ];
   });
 
