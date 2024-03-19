b86eb952f8378fafe216de5d3ef4555e266243f7
Florian Klink <flokli@flokli.de>
2024-03-19 12:12:03 +0200

    feat(tvix/castore/directory): add bigtable backend
    This adds a Directory service using
    https://cloud.google.com/bigtable/docs/ as a K/V store.
    
    Directory (closures) are put in individual keys.
    
    We don't do any bucketed upload of directory closures (yet), as castore/
    fs does query individually, does not request recursively (and buffers).
    This will be addressed by store composition at some point.
    
    Change-Id: I7fada45bf386a78b7ec93be38c5f03879a2a6e22

--- tvix/castore/default.nix
+++ tvix/castore/default.nix
@@ -4,5 +4,9 @@ depot.tvix.crates.workspaceMembers.tvix-castore.build.override {
   runTests = true;
   testPreRun = ''
     export SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt;
+    export PATH="$PATH:${pkgs.lib.makeBinPath [pkgs.cbtemulator pkgs.google-cloud-bigtable-tool]}"
   '';
+
+  # enable some optional features.
+  features = [ "default" "cloud" ];
 }
