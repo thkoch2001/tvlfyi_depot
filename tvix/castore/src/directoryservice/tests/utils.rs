b88e8fa626c43c854701322bcd0c9ae693ee947f
Florian Klink <flokli@flokli.de>
2024-03-19 12:12:03 +0200

    feat(tvix/castore/directory): add bigtable backend
    This adds a Directory service using
    https://cloud.google.com/bigtable/docs/ as a K/V store.
    
    Directory (closures) are put in individual keys.
    
    We don't do any bucketed upload of directory closures (yet), as castore/
    fs does query individually, does not request recursively (and buffers).
    This will be addressed by store composition at some point.
    
    Change-Id: I7fada45bf386a78b7ec93be38c5f03879a2a6e22

--- tvix/castore/src/directoryservice/tests/utils.rs
+++ tvix/castore/src/directoryservice/tests/utils.rs
@@ -1,11 +1,17 @@
-use crate::directoryservice::{DirectoryService, GRPCDirectoryService};
+use crate::directoryservice::{DirectoryPutter, DirectoryService, GRPCDirectoryService};
 use crate::proto::directory_service_client::DirectoryServiceClient;
-use crate::proto::GRPCDirectoryServiceWrapper;
+use crate::proto::{self, GRPCDirectoryServiceWrapper};
 use crate::{
     directoryservice::MemoryDirectoryService,
     proto::directory_service_server::DirectoryServiceServer,
 };
+use crate::{B3Digest, Error};
+use futures::stream::BoxStream;
+use tonic::async_trait;
 use tonic::transport::{Endpoint, Server, Uri};
+
+#[cfg(feature = "cloud")]
+use crate::directoryservice::BigtableDirectoryService;
 
 /// Constructs and returns a gRPC DirectoryService.
 /// The server part is a [MemoryDirectoryService], exposed via the
@@ -42,4 +48,118 @@ pub async fn make_grpc_directory_service_client() -> Box<dyn DirectoryService> {
                 .unwrap(),
         ),
     ))
+}
+
+/// A Directory Service holding onto other things.
+/// This can be used to construct a DirectoryService cleaning up test resources on drop.
+struct WrappedDirectoryService<T, O> {
+    inner: T,
+    #[allow(dead_code)]
+    others: O,
+}
+
+#[async_trait]
+impl<T, O> DirectoryService for WrappedDirectoryService<T, O>
+where
+    T: DirectoryService,
+    O: Send + Sync,
+{
+    async fn get(&self, digest: &B3Digest) -> Result<Option<proto::Directory>, Error> {
+        self.inner.get(digest).await
+    }
+    async fn put(&self, directory: proto::Directory) -> Result<B3Digest, Error> {
+        self.inner.put(directory).await
+    }
+    fn get_recursive(
+        &self,
+        root_directory_digest: &B3Digest,
+    ) -> BoxStream<Result<proto::Directory, Error>> {
+        self.inner.get_recursive(root_directory_digest)
+    }
+
+    fn put_multiple_start(&self) -> Box<(dyn DirectoryPutter + 'static)> {
+        self.inner.put_multiple_start()
+    }
+}
+
+#[cfg(feature = "cloud")]
+/// Returns a `Box<dyn DirectoryService>` that's connected to a fresh Google
+/// BigTable emulator.
+pub async fn make_bigtable() -> Box<dyn DirectoryService> {
+    use std::time::Duration;
+
+    use async_process::{Command, Stdio};
+    use tempfile::TempDir;
+    use tokio_retry::{strategy::ExponentialBackoff, Retry};
+
+    let project_id = "project-1";
+    let instance_name = "instance-1";
+
+    let tmpdir = TempDir::new().unwrap();
+
+    let socket_path = tmpdir.path().join("cbtemulator.sock");
+
+    let emulator_process = Command::new("cbtemulator")
+        .arg("-address")
+        .arg(socket_path.clone())
+        .stderr(Stdio::piped())
+        .stdout(Stdio::piped())
+        .kill_on_drop(true)
+        .spawn()
+        .expect("failed to spwan emulator");
+
+    Retry::spawn(
+        ExponentialBackoff::from_millis(20).max_delay(Duration::from_secs(1)),
+        || async {
+            if socket_path.exists() {
+                Ok(())
+            } else {
+                Err(())
+            }
+        },
+    )
+    .await
+    .expect("failed to wait for socket");
+
+    let table_name = "table-1";
+    let family_name = "cf1";
+
+    // populate the emulator
+    for cmd in &[
+        vec!["createtable", table_name],
+        vec!["createfamily", table_name, family_name],
+    ] {
+        Command::new("cbt")
+            .args({
+                let mut args = vec!["-instance", instance_name, "-project", project_id];
+                args.extend_from_slice(cmd);
+                args
+            })
+            .env(
+                "BIGTABLE_EMULATOR_HOST",
+                format!("unix://{}", socket_path.to_string_lossy()),
+            )
+            .output()
+            .await
+            .expect("failed to run cbt setup command");
+    }
+
+    let bigtable_conn = bigtable_rs::bigtable::BigTableConnection::new_with_emulator(
+        &format!("unix://{}", socket_path.to_string_lossy()),
+        project_id,
+        instance_name,
+        false,
+        None,
+    )
+    .expect("must connect");
+
+    Box::new(WrappedDirectoryService {
+        inner: BigtableDirectoryService::from_client(
+            bigtable_conn.client(),
+            table_name.into(),
+            family_name.into(),
+            "default".into(),
+        ),
+        others: (tmpdir, emulator_process),
+    })
 }
