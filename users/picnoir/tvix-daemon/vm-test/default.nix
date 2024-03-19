b06d7fbf8936fb9c67fa6cf257453e2183edd93e
Picnoir <picnoir@alternativebit.fr>
2024-03-19 14:35:11 +0100

    feat(users/picnoir/tvix-daemon) add small VM integration test
    Adding a VM integration test setup for tvix-daemon. This test acts as
    our first milestone: implementing enough daemon operations to be able
    to add a new store path to tvix-store.
    
    The test is expected to fail for now. We don't want to run it on CI
    yet.
    
    Change-Id: I2bd8eb9a07c5de2ef91099e10fcac23c087b880b

---
+++ users/picnoir/tvix-daemon/vm-test/default.nix
@@ -0,0 +1,28 @@
+{ depot, pkgs, lib, ... }:
+
+let
+  nixosTestDrv = pkgs.nixosTest {
+    name = "tvix-daemon-vm-test";
+    nodes.machine = { config, pkgs, ... }: {
+      environment.systemPackages = [
+        (pkgs.writers.writeBashBin "poke-daemon" ''
+          NIX_REMOTE=unix:///nix/var/nix/daemon-socket/socket nix-instantiate -E '"''${/etc/nscd.conf}"'
+        '')
+      ];
+      systemd.services.nix-daemon.serviceConfig.ExecStart = [
+        ""
+        "${depot.users.picnoir.tvix-daemon.tvix-daemon}/bin/tvix-daemon"
+      ];
+
+    };
+    testScript = ''
+      machine.wait_for_unit("multi-user.target")
+      machine.succeed("poke-daemon")
+    '';
+  };
+in
+nixosTestDrv // {
+  # The test fails for now. TOREMOVE when we reach the stage where we
+  # can add stuff to the store.
+  meta.ci.skip = true;
+}
