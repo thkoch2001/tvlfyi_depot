32e1f732476325fd6e65ad25e6caeb522ce197a9
Florian Klink <flokli@flokli.de>
2024-03-20 20:21:51 +0200

    feat(tvix/castore/directory): add ClosureValidator
    This can be used to validate a Directory closure (graph of connected
    Directories), and their insertion order.
    
    Directories need to be inserted (via `add`), in an order from the leaves
    to the root. During insertion, We validate as much as we can at that
    time:
    
     - individual validation of Directory messages
     - validation of insertion order (no upload of not-yet-known Directories)
     - validation of size fields of referred Directories
    
    Internally it keeps all received Directories (and their sizes) in a HashMap,
    keyed by digest.
    
    Once all Directories have been inserted, a drain() function can be
    called to get a (deduplicated and) validated list of directories, in
    from-leaves-to-root order (to be stored somewhere).
    
    While assembling that list, a check for graph connectivity is performed
    too, to ensure there's no disconnected graphs, and only one root.
    
    It adds a test suite for these cases, which is much nicer to test than
    where we previously had these checks (only in the gRPC server wrapper).
    
    Followup CLs will move the existing putters to use this.
    
    Change-Id: Ie88c832924c170a24626e9e3e91d868497b5d7a4

--- tvix/castore/src/directoryservice/mod.rs
+++ tvix/castore/src/directoryservice/mod.rs
@@ -8,12 +8,14 @@ mod memory;
 mod sled;
 mod traverse;
 mod utils;
+mod validate_closure;
 
 pub use self::from_addr::from_addr;
 pub use self::grpc::GRPCDirectoryService;
 pub use self::memory::MemoryDirectoryService;
 pub use self::sled::SledDirectoryService;
 pub use self::traverse::descend_to;
+pub use self::validate_closure::ClosureValidator;
 
 /// The base trait all Directory services need to implement.
 /// This is a simple get and put of [crate::proto::Directory], returning their
