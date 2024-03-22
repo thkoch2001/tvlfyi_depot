8ac3218d1c46c048c1e10f5830aa35eb3e6bafb4
Aspen Smith <root@gws.fyi>
2024-03-22 18:36:00 -0400

    wip(aspen/system): Add lusca
    Change-Id: I646e705d12b76c83e8cdcf11c618d07db3a21f0c

---
+++ users/aspen/system/home/machines/lusca.nix
@@ -0,0 +1,29 @@
+{ pkgs, lib, config, ... }:
+
+let
+  inherit (builtins) pathExists;
+  laptopKeyboardId = "5";
+
+in {
+  imports =
+    [ ../platforms/linux.nix ../modules/common.nix ../modules/desktop.nix ]
+    ++ (lib.optional (pathExists ../modules/private.nix)
+      ../modules/private.nix);
+
+  home.username = lib.mkForce "aspen";
+  home.homeDirectory = lib.mkForce "/home/aspen";
+
+  # for when hacking
+  programs.home-manager.enable = true;
+  home.stateVersion = "20.03";
+
+  system.machine = {
+    wirelessInterface = "wlp1s0";
+    i3FontSize = 9;
+    battery = 1;
+  };
+
+  home.packages = with pkgs; [ discord steam tdesktop ];
+
+  xsession.windowManager.i3.config.keybindings.XF86AudioMedia = "exec lock";
+}
