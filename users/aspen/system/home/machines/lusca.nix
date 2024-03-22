ba66f16fa4669391bff0015d60a7896b6040ede0
Aspen Smith <root@gws.fyi>
2024-03-22 18:36:00 -0400

    feat(aspen/system): Add lusca
    A Framework laptop
    
    Change-Id: I646e705d12b76c83e8cdcf11c618d07db3a21f0c

---
+++ users/aspen/system/home/machines/lusca.nix
@@ -0,0 +1,31 @@
+{ pkgs, lib, config, ... }:
+
+let
+  inherit (builtins) pathExists;
+in {
+  imports = [
+    ../platforms/linux.nix
+    ../modules/common.nix
+
+    ../modules/email.nix
+    ../modules/desktop.nix
+  ] ++ (lib.optional (pathExists ../modules/private.nix)
+    ../modules/private.nix);
+
+  home.username = lib.mkForce "aspen";
+  home.homeDirectory = lib.mkForce "/home/aspen";
+
+  # for when hacking
+  programs.home-manager.enable = true;
+  home.stateVersion = "20.03";
+
+  system.machine = {
+    wirelessInterface = "wlp1s0";
+    i3FontSize = 9;
+    battery = 1;
+  };
+
+  home.packages = with pkgs; [ discord steam tdesktop ];
+
+  xsession.windowManager.i3.config.keybindings.XF86AudioMedia = "exec lock";
+}
