99bf662408094a4f3eafa58dc66520fb93ce59d9
Aspen Smith <root@gws.fyi>
2024-03-23 10:20:25 -0400

    feat(3p/lisp): Add str package and dependencies
    Change-Id: Iffb66f4580517b1dbfee8c79e766552508695e5f

---
+++ third_party/lisp/str.nix
@@ -0,0 +1,27 @@
+{ depot, pkgs, ... }:
+
+let
+  inherit (depot.nix) buildLisp;
+  src = with pkgs; srcOnly lispPackages.str;
+in buildLisp.library {
+  name = "str";
+
+  deps = with depot.third_party.lisp; [
+    {
+      sbcl = buildLisp.bundled "uiop";
+      default = buildLisp.bundled "asdf";
+    }
+    cl-ppcre
+    cl-ppcre.unicode
+    cl-change-case
+  ];
+
+  srcs = [
+    (pkgs.runCommand "str.lisp" { } ''
+      substitute ${src}/str.lisp $out \
+        --replace-fail \
+          '(asdf:component-version (asdf:find-system "str"))' \
+          '"${pkgs.lispPackages.str.meta.version}"'
+    '')
+  ];
+}
