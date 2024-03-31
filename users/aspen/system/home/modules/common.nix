343b15f531705079a3f29e565a8ec2eba395ecc1
Aspen Smith <root@gws.fyi>
2024-03-31 14:13:54 -0400

    fix(aspen/home): Remove ssh match block configuration
    This is not necessary now that I'm using tailscale everywhere
    
    Change-Id: I36d8060d4ad5f6955cd18c68b5fc667e64669004

--- users/aspen/system/home/modules/common.nix
+++ users/aspen/system/home/modules/common.nix
@@ -79,31 +79,6 @@
 
   programs.ssh = {
     enable = true;
-
-    matchBlocks = {
-      "home" = {
-        host = "home.gws.fyi";
-        forwardAgent = true;
-      };
-
-      "cerberus" = {
-        host = "cerberus";
-        hostname = "172.16.0.3";
-        forwardAgent = true;
-        user = "griffin";
-      };
-
-      "mugwump" = {
-        host = "mugwump";
-        hostname = "172.16.0.5";
-        forwardAgent = true;
-      };
-
-      "roswell" = {
-        host = "roswell";
-        forwardAgent = true;
-      };
-    };
   };
 
   programs.direnv = {
