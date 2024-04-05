e90d3bd269ab1b6e16d531f82ff8ab8caeafa424
Picnoir <picnoir@alternativebit.fr>
2024-04-05 11:33:17 +0200

    feat(tvix/nix-compat): read client setting from wire
    Add the primitives necessary to read the client settings from the Nix
    daemon wire protocol.
    
    Change-Id: Ie1253523a6bd4e31e7924e9898a0898109da2fa0

--- tvix/nix-compat/src/wire/worker_protocol.rs
+++ tvix/nix-compat/src/wire/worker_protocol.rs
@@ -1,10 +1,15 @@
-use std::io::{Error, ErrorKind};
+use std::{
+    collections::HashMap,
+    io::{Error, ErrorKind},
+};
 
 use enum_primitive_derive::Primitive;
 use num_traits::{FromPrimitive, ToPrimitive};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 use crate::wire::primitive;
+
+use super::bytes::read_bytes;
 
 pub static STDERR_LAST: u64 = 0x616c7473;
 
@@ -64,6 +69,95 @@ pub enum Operation {
     AddBuildLog = 45,
     BuildPathsWithResults = 46,
     AddPermRoot = 47,
+}
+
+/// Log verbosity. In the Nix wire protocol, the client requests a
+/// verbosity level to the daemon, which in turns does not produce any
+/// log below this verbosity.
+#[derive(Debug, PartialEq, Primitive)]
+pub enum Verbosity {
+    LvlError = 0,
+    LvlWarn = 1,
+    LvlNotice = 2,
+    LvlInfo = 3,
+    LvlTalkative = 4,
+    LvlChatty = 5,
+    LvlDebug = 6,
+    LvlVomit = 7,
+}
+
+/// Settings requested by the client. These settings are applied to a
+/// connection to between the daemon and a client.
+#[derive(Debug)]
+pub struct ClientSettings {
+    pub keep_failed: bool,
+    pub keep_going: bool,
+    pub try_fallback: bool,
+    pub verbosity: Verbosity,
+    pub max_build_jobs: u64,
+    pub max_silent_time: u64,
+    pub verbose_build: bool,
+    pub build_cores: u64,
+    pub use_substitutes: bool,
+    /// Key/Value dictionary in charge of overriding the settings set
+    /// by the Nix config file.
+    ///
+    /// Some settings can be safely overidden,
+    /// some other require the user running the Nix client to be part
+    /// of the trusted users group.
+    pub overrides: HashMap<Vec<u8>, Vec<u8>>,
+}
+
+/// Reads the client settings from the wire.
+///
+/// Note: this function **only** reads the settings. It does not
+/// manage the log state with the daemon. You'll have to do that on
+/// your own. A minimal log implementation will consist in sending
+/// back [STDERR_LAST] to the client after reading the client
+/// settings.
+pub async fn read_client_settings<R: AsyncReadExt + Unpin>(
+    r: &mut R,
+    client_version: u64,
+) -> std::io::Result<ClientSettings> {
+    let keep_failed = primitive::read_bool(r).await?;
+    let keep_going = primitive::read_bool(r).await?;
+    let try_fallback = primitive::read_bool(r).await?;
+    let verbosity_uint = primitive::read_u64(r).await?;
+    let verbosity = Verbosity::from_u64(verbosity_uint).ok_or_else(|| {
+        Error::new(
+            ErrorKind::Other,
+            format!("Can't convert integer {} to verbosity", verbosity_uint),
+        )
+    })?;
+    let max_build_jobs = primitive::read_u64(r).await?;
+    let max_silent_time = primitive::read_u64(r).await?;
+    _ = primitive::read_u64(r).await?;
+    let verbose_build = primitive::read_bool(r).await?;
+    _ = primitive::read_u64(r).await?;
+    _ = primitive::read_u64(r).await?;
+    let build_cores = primitive::read_u64(r).await?;
+    let use_substitutes = primitive::read_bool(r).await?;
+    let mut overrides = HashMap::new();
+    if client_version >= 12 {
+        let overrides_nb = primitive::read_u64(r).await?;
+        for _ in 0..overrides_nb {
+            let name = read_bytes(r, 0..1024).await?;
+            let value = read_bytes(r, 0..1024).await?;
+            overrides.insert(name, value);
+        }
+    }
+    Ok(ClientSettings {
+        keep_failed,
+        keep_going,
+        try_fallback,
+        verbosity,
+        max_build_jobs,
+        max_silent_time,
+        verbose_build,
+        build_cores,
+        use_substitutes,
+        overrides,
+    })
 }
 
 /// Read a worker [Operation] from the wire.
