072017c33c8828dbc4fe10ef921cbd08f9327746
Florian Klink <flokli@flokli.de>
2024-04-10 14:54:11 +0300

    refactor(tvix/nix-compat): reorganize wire and bytes
    Move everything bytes-related into its own module, and re-export
    both bytes and primitive in a flat space from wire/mod.rs.
    
    Expose this if a `wire` feature flag is set. We only have `async` stuff
    in here.
    
    Change-Id: Ia4ce4791f13a5759901cc9d6ce6bd6bbcca587c7

--- tvix/nix-compat/default.nix
+++ tvix/nix-compat/default.nix
@@ -3,5 +3,5 @@
 depot.tvix.crates.workspaceMembers.nix-compat.build.override {
   runTests = true;
   # make sure we also enable async here, so run the tests behind that feature flag.
-  features = [ "default" "async" ];
+  features = [ "default" "async" "wire" ];
 }
