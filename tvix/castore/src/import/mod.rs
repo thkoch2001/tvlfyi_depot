b5de8ad101eb3efc9b876a24fb7f7cd3a58be347
Florian Klink <flokli@flokli.de>
2024-04-20 15:01:49 +0300

    refactor(tvix/castore/import): make module, split off fs and error
    Move error types and filesystem-specific functions to a separate file,
    and keep the fs:: namespace in public exports.
    
    Change-Id: I5e9e83ad78d9aea38553fafc293d3e4f8c31a8c1

--- tvix/castore/src/import/mod.rs
+++ tvix/castore/src/import/mod.rs
@@ -7,12 +7,9 @@ use crate::proto::DirectoryNode;
 use crate::proto::FileNode;
 use crate::proto::SymlinkNode;
 use crate::B3Digest;
-use crate::Error as CastoreError;
-use futures::stream::BoxStream;
 use futures::Future;
 use futures::{Stream, StreamExt};
 use std::fs::FileType;
-use std::os::unix::fs::MetadataExt;
 use std::pin::Pin;
 use tracing::Level;
 
@@ -21,154 +18,14 @@ use std::os::unix::ffi::OsStrExt;
 
 use std::{
     collections::HashMap,
-    fmt::Debug,
-    os::unix::prelude::PermissionsExt,
     path::{Path, PathBuf},
 };
 use tracing::instrument;
-use walkdir::DirEntry;
-use walkdir::WalkDir;
 
-#[derive(Debug, thiserror::Error)]
-pub enum Error {
-    #[error("failed to upload directory at {0}: {1}")]
-    UploadDirectoryError(PathBuf, CastoreError),
+mod error;
+pub use error::Error;
 
-    #[error("invalid encoding encountered for entry {0:?}")]
-    InvalidEncoding(PathBuf),
-
-    #[error("unable to stat {0}: {1}")]
-    UnableToStat(PathBuf, std::io::Error),
-
-    #[error("unable to open {0}: {1}")]
-    UnableToOpen(PathBuf, std::io::Error),
-
-    #[error("unable to read {0}: {1}")]
-    UnableToRead(PathBuf, std::io::Error),
-
-    #[error("unsupported file {0} type: {1:?}")]
-    UnsupportedFileType(PathBuf, FileType),
-}
-
-impl From<CastoreError> for Error {
-    fn from(value: CastoreError) -> Self {
-        match value {
-            CastoreError::InvalidRequest(_) => panic!("tvix bug"),
-            CastoreError::StorageError(_) => panic!("error"),
-        }
-    }
-}
-
-impl From<Error> for std::io::Error {
-    fn from(value: Error) -> Self {
-        std::io::Error::new(std::io::ErrorKind::Other, value)
-    }
-}
-
-/// Walk the filesystem at a given path and returns a stream of ingestion entries.
-///
-/// This is how [`ingest_path`] assembles the set of entries to pass on [`ingest_entries`].
-/// This low-level function can be used if additional filtering or processing is required on the
-/// entries.
-///
-/// It does not follow symlinks at the root, they will be ingested as actual symlinks.
-///
-/// This function will walk the filesystem using `walkdir` and will consume
-/// `O(#number of entries)` space.
-#[instrument(fields(path), skip(blob_service))]
-fn walk_path_for_ingestion<'a, BS>(
-    blob_service: BS,
-    path: &'a Path,
-) -> BoxStream<'a, Result<IngestionEntry<'a>, Error>>
-where
-    BS: BlobService + Clone + 'a,
-{
-    let iter = WalkDir::new(path)
-        .follow_links(false)
-        .follow_root_links(false)
-        .contents_first(true)
-        .into_iter();
-
-    dir_entry_iter_to_ingestion_stream(blob_service, iter, path)
-}
-
-/// Converts an iterator of [walkdir::DirEntry]s into a stream of ingestion entries.
-/// This can then be fed into [ingest_entries] to ingest all the entries into the castore.
-///
-/// The root is the [Path] in the filesystem that is being ingested into the castore.
-pub fn dir_entry_iter_to_ingestion_stream<'a, BS, I>(
-    blob_service: BS,
-    iter: I,
-    root: &'a Path,
-) -> BoxStream<'a, Result<IngestionEntry<'a>, Error>>
-where
-    BS: BlobService + Clone + 'a,
-    I: Iterator<Item = Result<DirEntry, walkdir::Error>> + Send + 'a,
-{
-    let prefix = root.parent().unwrap_or_else(|| Path::new(""));
-
-    let iter = iter.map(move |entry| match entry {
-        Ok(entry) => dir_entry_to_ingestion_entry(blob_service.clone(), &entry, prefix),
-        Err(error) => Err(Error::UnableToStat(
-            root.to_path_buf(),
-            error.into_io_error().expect("walkdir err must be some"),
-        )),
-    });
-
-    Box::pin(futures::stream::iter(iter))
-}
-
-/// Converts a [walkdir::DirEntry] into an [IngestionEntry], uploading blobs to the
-/// provided [BlobService].
-///
-/// The prefix path is stripped from the path of each entry. This is usually the parent path
-/// of the path being ingested so that the last element of the stream only has one component.
-fn dir_entry_to_ingestion_entry<'a, BS>(
-    blob_service: BS,
-    entry: &DirEntry,
-    prefix: &Path,
-) -> Result<IngestionEntry<'a>, Error>
-where
-    BS: BlobService + 'a,
-{
-    let file_type = entry.file_type();
-
-    let path = entry
-        .path()
-        .strip_prefix(prefix)
-        .expect("Tvix bug: failed to strip root path prefix")
-        .to_path_buf();
-
-    if file_type.is_dir() {
-        Ok(IngestionEntry::Dir { path })
-    } else if file_type.is_symlink() {
-        let target = std::fs::read_link(entry.path())
-            .map_err(|e| Error::UnableToStat(entry.path().to_path_buf(), e))?;
-
-        Ok(IngestionEntry::Symlink { path, target })
-    } else if file_type.is_file() {
-        let metadata = entry
-            .metadata()
-            .map_err(|e| Error::UnableToStat(entry.path().to_path_buf(), e.into()))?;
-
-        // TODO: In the future, for small files, hash right away and upload async.
-        let digest = Box::pin(upload_blob_at_path(
-            blob_service,
-            entry.path().to_path_buf(),
-        ));
-
-        Ok(IngestionEntry::Regular {
-            path,
-            size: metadata.size(),
-            // If it's executable by the user, it'll become executable.
-            // This matches nix's dump() function behaviour.
-            executable: metadata.permissions().mode() & 64 != 0,
-            digest,
-        })
-    } else {
-        Ok(IngestionEntry::Unknown { path, file_type })
-    }
-}
+pub mod fs;
 
 /// Uploads the file at the provided [Path] the the [BlobService].
 #[instrument(skip(blob_service), fields(path), err)]
@@ -193,25 +50,6 @@ where
         .map_err(|e| Error::UnableToRead(path, e))?;
 
     Ok(digest)
-}
-
-/// Ingests the contents at a given path into the tvix store, interacting with a [BlobService] and
-/// [DirectoryService]. It returns the root node or an error.
-///
-/// It does not follow symlinks at the root, they will be ingested as actual symlinks.
-#[instrument(skip(blob_service, directory_service), fields(path), err)]
-pub async fn ingest_path<BS, DS, P>(
-    blob_service: BS,
-    directory_service: DS,
-    path: P,
-) -> Result<Node, Error>
-where
-    P: AsRef<Path> + std::fmt::Debug,
-    BS: BlobService + Clone,
-    DS: AsRef<dyn DirectoryService>,
-{
-    let entry_stream = walk_path_for_ingestion(blob_service, path.as_ref());
-    ingest_entries(directory_service, entry_stream).await
 }
 
 /// Ingests elements from the given stream of [DirEntry] into a the passed [DirectoryService].
