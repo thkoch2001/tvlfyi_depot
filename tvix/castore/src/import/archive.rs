34c72c341e031c653a7fcf19795430f0395155ae
Connor Brewster <cbrewster@hey.com>
2024-04-20 09:19:05 -0500

    refactor(tvix/castore): switch to `ingest_entries` for tarball ingestion
    With `ingest_entries` being more generalized, we can now use it for
    ingesting the directory entries generated from tarballs.
    
    Change-Id: Ie1f7a915c456045762e05fcc9af45771f121eb43

--- tvix/castore/src/import/archive.rs
+++ tvix/castore/src/import/archive.rs
@@ -1,21 +1,18 @@
-#[cfg(target_family = "unix")]
-use std::os::unix::ffi::OsStrExt;
-use std::{
-    collections::HashMap,
-    path::{Path, PathBuf},
-};
+use std::path::Path;
+use std::{collections::HashMap, path::PathBuf};
 
+use petgraph::graph::{DiGraph, NodeIndex};
+use petgraph::visit::{DfsPostOrder, EdgeRef};
+use petgraph::Direction;
 use tokio::io::AsyncRead;
 use tokio_stream::StreamExt;
 use tokio_tar::Archive;
 use tracing::{instrument, Level};
 
-use crate::{
-    blobservice::BlobService,
-    directoryservice::{DirectoryPutter, DirectoryService},
-    import::Error,
-    proto::{node::Node, Directory, DirectoryNode, FileNode, SymlinkNode},
-};
+use crate::blobservice::BlobService;
+use crate::directoryservice::DirectoryService;
+use crate::import::{ingest_entries, Error, IngestionEntry};
+use crate::proto::node::Node;
 
 /// Ingests elements from the given tar [`Archive`] into a the passed [`BlobService`] and
 /// [`DirectoryService`].
@@ -35,23 +32,13 @@ where
     // contents and entries to meet the requires of the castore.
 
     // In the first phase, collect up all the regular files and symlinks.
-    let mut paths = HashMap::new();
-    let mut entries = archive.entries().map_err(Error::Archive)?;
-    while let Some(mut entry) = entries.try_next().await.map_err(Error::Archive)? {
-        let path = entry.path().map_err(Error::Archive)?.into_owned();
-        let name = path
-            .file_name()
-            .ok_or_else(|| {
-                Error::Archive(std::io::Error::new(
-                    std::io::ErrorKind::InvalidInput,
-                    "invalid filename in archive",
-                ))
-            })?
-            .as_bytes()
-            .to_vec()
-            .into();
+    let mut nodes = ArchiveNodes::new();
 
-        let node = match entry.header().entry_type() {
+    let mut entries_iter = archive.entries().map_err(Error::Archive)?;
+    while let Some(mut entry) = entries_iter.try_next().await.map_err(Error::Archive)? {
+        let path: PathBuf = entry.path().map_err(Error::Archive)?.into();
+
+        let entry = match entry.header().entry_type() {
             tokio_tar::EntryType::Regular
             | tokio_tar::EntryType::GNUSparse
             | tokio_tar::EntryType::Continuous => {
@@ -62,140 +49,213 @@ where
                     .await
                     .map_err(Error::Archive)?;
                 let digest = writer.close().await.map_err(Error::Archive)?;
-                Node::File(FileNode {
-                    name,
-                    digest: digest.into(),
+
+                IngestionEntry::Regular {
+                    path: path.clone(),
                     size,
                     executable: entry.header().mode().map_err(Error::Archive)? & 64 != 0,
-                })
+                    digest,
+                }
             }
-            tokio_tar::EntryType::Symlink => Node::Symlink(SymlinkNode {
-                name,
+            tokio_tar::EntryType::Symlink => IngestionEntry::Symlink {
+                path: path.clone(),
                 target: entry
                     .link_name()
                     .map_err(Error::Archive)?
-                    .expect("symlink missing target")
-                    .as_os_str()
-                    .as_bytes()
-                    .to_vec()
+                    .ok_or_else(|| Error::MissingSymlinkTarget(path.clone()))?
                     .into(),
-            }),
+            },
             // Push a bogus directory marker so we can make sure this directoy gets
             // created. We don't know the digest and size until after reading the full
             // tarball.
-            tokio_tar::EntryType::Directory => Node::Directory(DirectoryNode {
-                name,
-                digest: Default::default(),
-                size: 0,
-            }),
+            tokio_tar::EntryType::Directory => IngestionEntry::Dir { path: path.clone() },
 
             tokio_tar::EntryType::XGlobalHeader | tokio_tar::EntryType::XHeader => continue,
 
             entry_type => return Err(Error::UnsupportedTarEntry(path, entry_type)),
         };
 
-        paths.insert(path, node);
+        nodes.add(entry);
     }
 
-    // In the second phase, construct all of the directories.
+    ingest_entries(
+        directory_service,
+        futures::stream::iter(nodes.drain().into_iter().map(Ok)),
+    )
+    .await
+}
 
-    // Collect into a list and then sort so all entries in the same directory
-    // are next to each other.
-    // We can detect boundaries between each directories to determine
-    // when to construct or push directory entries.
-    let mut ordered_paths = paths.into_iter().collect::<Vec<_>>();
-    ordered_paths.sort_by(|a, b| a.0.cmp(&b.0));
+pub struct ArchiveNodes {
+    graph: DiGraph<IngestionEntry, ()>,
+    path_to_index: HashMap<PathBuf, NodeIndex>,
+    root_node: Option<NodeIndex>,
+}
 
-    let mut directory_putter = directory_service.as_ref().put_multiple_start();
+impl Default for ArchiveNodes {
+    fn default() -> Self {
+        Self::new()
+    }
+}
 
-    // Start with an initial directory at the root.
-    let mut dir_stack = vec![(PathBuf::from(""), Directory::default())];
+impl ArchiveNodes {
+    pub fn new() -> Self {
+        ArchiveNodes {
+            graph: DiGraph::new(),
+            path_to_index: HashMap::new(),
+            root_node: None,
+        }
+    }
 
-    async fn pop_directory(
-        dir_stack: &mut Vec<(PathBuf, Directory)>,
-        directory_putter: &mut Box<dyn DirectoryPutter>,
-    ) -> Result<DirectoryNode, Error> {
-        let (path, directory) = dir_stack.pop().unwrap();
-
-        directory
-            .validate()
-            .map_err(|e| Error::InvalidDirectory(path.to_path_buf(), e))?;
-
-        let dir_node = DirectoryNode {
-            name: path
-                .file_name()
-                .unwrap_or_default()
-                .as_bytes()
-                .to_vec()
-                .into(),
-            digest: directory.digest().into(),
-            size: directory.size(),
+    pub fn add(&mut self, entry: IngestionEntry) -> NodeIndex {
+        let parent_index = match entry.path().parent() {
+            Some(parent_path) if !parent_path.as_os_str().is_empty() => {
+                self.create_dir_all(parent_path)
+            }
+            _ => None,
         };
 
-        if let Some((_, parent)) = dir_stack.last_mut() {
-            parent.directories.push(dir_node.clone());
-        }
+        let path = entry.path().to_path_buf();
 
-        directory_putter.put(directory).await?;
-
-        Ok(dir_node)
-    }
-
-    fn push_directories(path: &Path, dir_stack: &mut Vec<(PathBuf, Directory)>) {
-        if path == dir_stack.last().unwrap().0 {
-            return;
-        }
-        if let Some(parent) = path.parent() {
-            push_directories(parent, dir_stack);
-        }
-        dir_stack.push((path.to_path_buf(), Directory::default()));
-    }
-
-    for (path, node) in ordered_paths.into_iter() {
-        // Pop stack until the top dir is an ancestor of this entry.
-        loop {
-            let top = dir_stack.last().unwrap();
-            if path.ancestors().any(|ancestor| ancestor == top.0) {
-                break;
+        let index = match self.path_to_index.get(entry.path()) {
+            Some(index) => {
+                // If we see a directory and the existing node is already a directory
+                // no need to replace.
+                if self.node_is_dir(*index) && entry.is_dir() {
+                    *index
+                } else {
+                    self.replace_node(*index, entry)
+                }
             }
+            None => self.graph.add_node(entry),
+        };
 
-            pop_directory(&mut dir_stack, &mut directory_putter).await?;
+        if let Some(parent_index) = parent_index {
+            self.graph.add_edge(parent_index, index, ());
         }
 
-        // For directories, just ensure the directory node exists.
-        if let Node::Directory(_) = node {
-            push_directories(&path, &mut dir_stack);
-            continue;
+        if path.components().count() == 1 {
+            self.root_node = Some(index)
         }
 
-        // Push all ancestor directories onto the stack.
-        push_directories(path.parent().unwrap(), &mut dir_stack);
+        self.path_to_index.insert(path, index);
 
-        let top = dir_stack.last_mut().unwrap();
-        debug_assert_eq!(Some(top.0.as_path()), path.parent());
-
-        match node {
-            Node::File(n) => top.1.files.push(n),
-            Node::Symlink(n) => top.1.symlinks.push(n),
-            // We already handled directories above.
-            Node::Directory(_) => unreachable!(),
-        }
+        index
     }
 
-    let mut root_node = None;
-    while !dir_stack.is_empty() {
-        // If the root directory only has 1 directory entry, we return the child entry
-        // instead... weeeee
-        if dir_stack.len() == 1 && dir_stack.last().unwrap().1.directories.len() == 1 {
-            break;
+    pub fn drain(self) -> Vec<IngestionEntry> {
+        // TODO: Not sure if this should be allowed or not?
+        let Some(root_node_index) = self.root_node else {
+            return Vec::new();
+        };
+
+        let mut traversal = DfsPostOrder::new(&self.graph, root_node_index);
+        let mut nodes = Vec::with_capacity(self.graph.node_count());
+        while let Some(node_index) = traversal.next(&self.graph) {
+            nodes.push(
+                self.graph
+                    .node_weight(node_index)
+                    .expect("Tvix bug: missing node entry")
+                    .clone(),
+            );
         }
-        root_node = Some(pop_directory(&mut dir_stack, &mut directory_putter).await?);
+
+        nodes
     }
-    let root_node = root_node.expect("no root node");
 
-    let root_digest = directory_putter.close().await?;
+    fn create_dir_all(&mut self, path: &Path) -> Option<NodeIndex> {
+        // Check if this directory already exists.
+        if let Some(index) = self.path_to_index.get(path) {
+            // If the node isn't a directory, we need to replace it.
+            if self.node_is_dir(*index) {
+                return Some(*index);
+            }
+        }
 
-    debug_assert_eq!(root_digest.as_slice(), &root_node.digest);
+        Some(self.add(IngestionEntry::Dir {
+            path: path.to_path_buf(),
+        }))
+    }
 
-    Ok(Node::Directory(root_node))
+    fn node_is_dir(&self, index: NodeIndex) -> bool {
+        let Some(entry) = self.graph.node_weight(index) else {
+            panic!("Tvix bug: missing node entry");
+        };
+
+        entry.is_dir()
+    }
+
+    fn replace_node(&mut self, index: NodeIndex, new_entry: IngestionEntry) -> NodeIndex {
+        let Some(entry) = self.graph.node_weight_mut(index) else {
+            panic!("Tvix bug: missing node entry");
+        };
+
+        // Replace the node itself.
+        *entry = new_entry;
+
+        // Remove any outgoing edges.
+        let edges = self
+            .graph
+            .edges_directed(index, Direction::Outgoing)
+            .map(|edge| edge.id())
+            .collect::<Vec<_>>();
+
+        for edge in edges {
+            self.graph.remove_edge(edge);
+        }
+
+        index
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::ops::Deref;
+
+    use crate::{import::IngestionEntry, B3Digest};
+
+    use super::ArchiveNodes;
+
+    use lazy_static::lazy_static;
+    use rstest::rstest;
+
+    lazy_static! {
+        pub static ref EMPTY_DIGEST: B3Digest = blake3::hash(&[]).as_bytes().into();
+        pub static ref DIR_A: IngestionEntry = IngestionEntry::Dir { path: "a".into() };
+        pub static ref DIR_A_B: IngestionEntry = IngestionEntry::Dir { path: "a/b".into() };
+        pub static ref FILE_A: IngestionEntry = IngestionEntry::Regular {
+            path: "a".into(),
+            size: 0,
+            executable: false,
+            digest: EMPTY_DIGEST.clone(),
+        };
+        pub static ref FILE_A_B_C: IngestionEntry = IngestionEntry::Regular {
+            path: "a/b/c".into(),
+            size: 0,
+            executable: false,
+            digest: EMPTY_DIGEST.clone(),
+        };
+    }
+
+    #[rstest]
+    #[case::empty(&[], &[])]
+    #[case::implicit_directories(&[FILE_A_B_C.deref()], &[FILE_A_B_C.deref(), DIR_A_B.deref(), DIR_A.deref()])]
+    #[case::explicit_directories(&[DIR_A.deref(), DIR_A_B.deref(), FILE_A_B_C.deref()], &[FILE_A_B_C.deref(), DIR_A_B.deref(), DIR_A.deref()])]
+    #[case::inaccesible_tree(&[DIR_A.deref(), DIR_A_B.deref(), FILE_A.deref()], &[FILE_A.deref()])]
+    fn node_ingestion(
+        #[case] in_entries: &[&IngestionEntry],
+        #[case] exp_entries: &[&IngestionEntry],
+    ) {
+        let mut nodes = ArchiveNodes::new();
+
+        for entry in in_entries {
+            nodes.add((*entry).clone());
+        }
+
+        let nodes = nodes.drain();
+
+        let exp_entries: Vec<IngestionEntry> =
+            exp_entries.iter().map(|entry| (*entry).clone()).collect();
+
+        assert_eq!(nodes, exp_entries);
+    }
 }
