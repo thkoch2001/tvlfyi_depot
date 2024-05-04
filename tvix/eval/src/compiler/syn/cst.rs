d11fa036e52269b9cae5d8354d7371b83b063582
Adam Joseph <adam@westernsemico.com>
2024-05-04 14:05:11 -0700

    feat(tvix/eval): abstracted abstract syntax tree
    Reviewer note: please read src/compiler/syn/mod.rs *first*.
    
    This commit abstracts away everything that compiler::bindings knows
    about the syntax tree.  After applying this commit there are no
    references to `rnix::ast` anywhere in compiler/bindings.rs.
    
    Despite its misleading package name "rnix::ast", rnix-parser is in
    fact a *concrete* syntax tree.  It is based on rowan, a library for
    concrete syntax trees, whose author (understandably) rejects all PRs
    which fall outside that scope.  You cannot build an AST using rowan.
    Even if you use the three-letter word "ast" in your module names.
    
    This CL allows to substitute other syntax tree representations in
    place of the rnix-parser CST.  For example, we could use an AST,
    which would allow many parts of the compiler to be factored out into
    separate, modular, AST-to-AST rewrites.  This is how the GHC renamer
    works.
    
    Remaining tasks (subsequent CLs):
    
    - move the rest of tvix::eval::compiler over to compiler::syn
      (i.e. not just compiler::bindings)
    
    - add a second implementation of compiler::syn using an AST (easy,
      and will not require touching the rest of the compiler)
    
    - factor out parts of the compiler into AST->AST passes, like how
      the GHC renamer works.  (easy and fun)
    
    Change-Id: I6481f7dfcd6ed6c23bd084ae1e00d9bb3733465b

---
+++ tvix/eval/src/compiler/syn/cst.rs
@@ -0,0 +1,401 @@
+///!
+///! An implementation of syn::Syntax implementation using the rnix-parser CST
+///!
+use super::*;
+
+// Zero-sized type.
+pub struct SyntaxImpl {}
+
+impl Syntax for SyntaxImpl {
+    type AttrPath = self::AttrPathImpl;
+    type AttrPathValue = self::AttrPathValueImpl;
+    type Expr = rnix::ast::Expr;
+    type Inherit = self::InheritImpl;
+    type Attr = self::AttrImpl;
+    type LetIn = self::LetInImpl;
+    type AttrSet = self::AttrSetImpl;
+    type LegacyLet = self::LegacyLetImpl;
+    type Ident = self::IdentImpl;
+}
+
+#[derive(Clone)]
+pub struct AttrPathImpl {
+    // Note: these are in reverse order to allow the use of Vec::pop()
+    components: Vec<AttrImpl>,
+}
+
+impl AttrPath for AttrPathImpl {
+    type Syntax = self::SyntaxImpl;
+    fn first(&mut self) -> Option<&<Self::Syntax as syn::Syntax>::Attr> {
+        self.components.last()
+    }
+    fn take_first(&mut self) -> Option<<Self::Syntax as syn::Syntax>::Attr> {
+        self.components.pop()
+    }
+}
+
+impl FromCst<rnix::ast::Attrpath> for AttrPathImpl {
+    fn from_cst(c: &Compiler, entry: rnix::ast::Attrpath) -> Self {
+        let mut coll: Vec<_> = entry.attrs().map(|a| AttrImpl::from_cst(c, a)).collect();
+        coll.reverse();
+        AttrPathImpl { components: coll }
+    }
+}
+
+#[derive(Clone)]
+pub struct AttrPathValueImpl {
+    pub span: Span,
+    pub remaining_path: AttrPathImpl,
+    pub value: ast::Expr,
+}
+
+impl FromCst<rnix::ast::AttrpathValue> for AttrPathValueImpl {
+    fn from_cst(c: &Compiler, entry: rnix::ast::AttrpathValue) -> Self {
+        AttrPathValueImpl {
+            span: entry.span_for(c.file),
+            remaining_path: if let Some(attrpath) = entry.attrpath() {
+                AttrPathImpl::from_cst(c, attrpath)
+            } else {
+                AttrPathImpl { components: vec![] }
+            },
+            value: entry.value().unwrap(),
+        }
+    }
+}
+
+impl ToSpan for AttrPathValueImpl {
+    fn span_for(&self, _file: &codemap::File) -> Span {
+        self.span
+    }
+}
+
+impl AttrPathValue for AttrPathValueImpl {
+    type Syntax = self::SyntaxImpl;
+    fn path(&self) -> <Self::Syntax as syn::Syntax>::AttrPath {
+        self.remaining_path.clone()
+    }
+    fn value(&self) -> <Self::Syntax as syn::Syntax>::Expr {
+        self.value.clone()
+    }
+    fn new(
+        span: Span,
+        remaining_path: <Self::Syntax as syn::Syntax>::AttrPath,
+        value: ast::Expr,
+    ) -> Self {
+        AttrPathValueImpl {
+            span,
+            remaining_path,
+            value,
+        }
+    }
+}
+
+impl<N: rnix::ast::HasEntry> HasEntry for N {
+    type Syntax = self::SyntaxImpl;
+    fn inherits(&self, c: &Compiler) -> Vec<InheritImpl> {
+        rnix::ast::HasEntry::inherits(self)
+            .into_iter()
+            .map(|inherit| InheritImpl::from_cst(c, inherit))
+            .collect()
+    }
+
+    fn attributes<'a>(&self, c: &'a Compiler) -> Vec<AttrPathValueImpl> {
+        rnix::ast::HasEntry::attrpath_values(self)
+            .map(move |entry| AttrPathValueImpl::from_cst(c, entry))
+            .collect()
+    }
+}
+
+#[derive(Clone)]
+pub struct InheritImpl {
+    namespace: Option<ast::Expr>,
+    attrs: Vec<AttrImpl>,
+    span: Span,
+}
+
+impl FromCst<rnix::ast::Inherit> for InheritImpl {
+    fn from_cst(c: &Compiler, entry: rnix::ast::Inherit) -> Self {
+        let namespace = entry.from().map(|from| from.expr()).flatten();
+        let attrs: Vec<AttrImpl> = entry
+            .attrs()
+            .map(|node| AttrImpl::from_cst(c, node))
+            .collect();
+        let span = c.span_for(&entry);
+        InheritImpl {
+            namespace,
+            attrs,
+            span,
+        }
+    }
+}
+
+impl Inherit for InheritImpl {
+    type Syntax = self::SyntaxImpl;
+    fn namespace(&self) -> Option<ast::Expr> {
+        self.namespace.clone()
+    }
+    fn attrs(&self) -> Vec<<Self::Syntax as syn::Syntax>::Attr> {
+        self.attrs.clone()
+    }
+    fn new(
+        namespace: Option<<Self::Syntax as syn::Syntax>::Expr>,
+        attrs: Vec<<Self::Syntax as syn::Syntax>::Attr>,
+        span: Span,
+    ) -> Self {
+        InheritImpl {
+            namespace,
+            attrs,
+            span,
+        }
+    }
+}
+
+impl ToSpan for InheritImpl {
+    fn span_for(&self, _file: &codemap::File) -> Span {
+        self.span
+    }
+}
+
+impl Expr for rnix::ast::Expr {
+    type Syntax = SyntaxImpl;
+    fn as_cst(&self) -> &rnix::ast::Expr {
+        &self
+    }
+    fn to_cst(self) -> rnix::ast::Expr {
+        self
+    }
+    fn is_attrset(&self) -> bool {
+        matches!(self.as_cst(), ast::Expr::AttrSet(_))
+    }
+    fn as_attrset(self, c: &Compiler) -> Option<AttrSetImpl> {
+        match self {
+            ast::Expr::AttrSet(attrset) => Some(AttrSetImpl::from_cst(c, attrset)),
+            _ => None,
+        }
+    }
+}
+
+impl FromCst<rnix::ast::Expr> for rnix::ast::Expr {
+    fn from_cst(_c: &Compiler, cst: rnix::ast::Expr) -> Self {
+        cst
+    }
+}
+
+#[derive(Clone)]
+pub struct AttrImpl {
+    span: Span,
+    attr: StaticOrDynamic<SmolStr, rnix::ast::Expr>,
+}
+impl Attr for AttrImpl {
+    type Syntax = self::SyntaxImpl;
+    fn attr(&self) -> &StaticOrDynamic<SmolStr, <Self::Syntax as syn::Syntax>::Expr> {
+        &self.attr
+    }
+    fn into_attr(self) -> StaticOrDynamic<SmolStr, <Self::Syntax as syn::Syntax>::Expr> {
+        self.attr
+    }
+    fn from_static(str: SmolStr, span: Span) -> Self {
+        AttrImpl {
+            span,
+            attr: StaticOrDynamic::Static(str),
+        }
+    }
+    fn from_dynamic(expr: <Self::Syntax as syn::Syntax>::Expr, span: Span) -> Self {
+        AttrImpl {
+            span,
+            attr: StaticOrDynamic::Dynamic(expr),
+        }
+    }
+}
+impl ToSpan for AttrImpl {
+    fn span_for(&self, _file: &codemap::File) -> Span {
+        self.span
+    }
+}
+impl FromCst<rnix::ast::Attr> for AttrImpl {
+    fn from_cst(c: &Compiler, cst: rnix::ast::Attr) -> Self {
+        let span = c.span_for(&cst);
+        AttrImpl {
+            span,
+            attr: if let Some(s) = expr_static_attr_str(&cst) {
+                StaticOrDynamic::Static(s.into())
+            } else {
+                StaticOrDynamic::Dynamic(match cst {
+                    ast::Attr::Dynamic(expr) => {
+                        <SyntaxImpl as syn::Syntax>::Expr::from_cst(c, expr.expr().unwrap())
+                    }
+                    ast::Attr::Str(str) => {
+                        <SyntaxImpl as syn::Syntax>::Expr::from_cst(c, rnix::ast::Expr::Str(str))
+                    }
+                    ast::Attr::Ident(_expr) => {
+                        unreachable!(
+                            "if you see this, it means that expr_static_attr_str() has a bug in it"
+                        )
+                    }
+                })
+            },
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct LetInImpl {
+    span: Span,
+    entry: rnix::ast::LetIn,
+}
+
+impl ToSpan for LetInImpl {
+    fn span_for(&self, _file: &codemap::File) -> Span {
+        self.span
+    }
+}
+
+impl LetIn for LetInImpl {
+    type Syntax = self::SyntaxImpl;
+    fn body(self) -> <SyntaxImpl as Syntax>::Expr {
+        self.entry.body().unwrap()
+    }
+}
+
+impl FromCst<rnix::ast::LetIn> for LetInImpl {
+    fn from_cst(c: &Compiler, entry: rnix::ast::LetIn) -> Self {
+        LetInImpl {
+            span: c.span_for(&entry),
+            entry: entry,
+        }
+    }
+}
+
+impl HasEntry for LetInImpl {
+    type Syntax = self::SyntaxImpl;
+    fn inherits(&self, c: &Compiler) -> Vec<InheritImpl> {
+        rnix::ast::HasEntry::inherits(&self.entry)
+            .into_iter()
+            .map(|inherit| InheritImpl::from_cst(c, inherit))
+            .collect()
+    }
+
+    fn attributes<'a>(&self, c: &'a Compiler) -> Vec<AttrPathValueImpl> {
+        rnix::ast::HasEntry::attrpath_values(&self.entry)
+            .map(move |entry| AttrPathValueImpl::from_cst(c, entry))
+            .collect()
+    }
+}
+
+#[derive(Clone)]
+pub struct AttrSetImpl {
+    is_rec: bool,
+    span: Span,
+    entry: rnix::ast::AttrSet,
+}
+
+impl ToSpan for AttrSetImpl {
+    fn span_for(&self, _file: &codemap::File) -> Span {
+        self.span
+    }
+}
+
+impl HasEntry for AttrSetImpl {
+    type Syntax = self::SyntaxImpl;
+    fn inherits(&self, c: &Compiler) -> Vec<InheritImpl> {
+        rnix::ast::HasEntry::inherits(&self.entry)
+            .into_iter()
+            .map(|inherit| InheritImpl::from_cst(c, inherit))
+            .collect()
+    }
+
+    fn attributes<'a>(&self, c: &'a Compiler) -> Vec<AttrPathValueImpl> {
+        rnix::ast::HasEntry::attrpath_values(&self.entry)
+            .map(move |entry| AttrPathValueImpl::from_cst(c, entry))
+            .collect()
+    }
+}
+
+impl AttrSet for AttrSetImpl {
+    type Syntax = self::SyntaxImpl;
+    fn is_rec(&self) -> bool {
+        self.is_rec
+    }
+    fn to_cst(self) -> rnix::ast::AttrSet {
+        self.entry
+    }
+}
+
+impl FromCst<rnix::ast::AttrSet> for AttrSetImpl {
+    fn from_cst(c: &Compiler, entry: rnix::ast::AttrSet) -> Self {
+        AttrSetImpl {
+            span: c.span_for(&entry),
+            is_rec: entry.rec_token().is_some(),
+            entry,
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct LegacyLetImpl {
+    span: Span,
+    entry: rnix::ast::LegacyLet,
+}
+
+impl LegacyLet for LegacyLetImpl {
+    type Syntax = SyntaxImpl;
+}
+
+impl HasEntry for LegacyLetImpl {
+    type Syntax = self::SyntaxImpl;
+    fn inherits(&self, c: &Compiler) -> Vec<InheritImpl> {
+        rnix::ast::HasEntry::inherits(&self.entry)
+            .into_iter()
+            .map(|inherit| InheritImpl::from_cst(c, inherit))
+            .collect()
+    }
+
+    fn attributes<'a>(&self, c: &'a Compiler) -> Vec<AttrPathValueImpl> {
+        rnix::ast::HasEntry::attrpath_values(&self.entry)
+            .map(move |entry| AttrPathValueImpl::from_cst(c, entry))
+            .collect()
+    }
+}
+
+impl FromCst<rnix::ast::LegacyLet> for LegacyLetImpl {
+    fn from_cst(c: &Compiler, entry: rnix::ast::LegacyLet) -> Self {
+        LegacyLetImpl {
+            span: c.span_for(&entry),
+            entry,
+        }
+    }
+}
+
+impl ToSpan for LegacyLetImpl {
+    fn span_for(&self, _file: &codemap::File) -> Span {
+        self.span
+    }
+}
+
+#[derive(Clone)]
+pub struct IdentImpl {
+    span: Span,
+    ident: String,
+}
+
+impl Ident for IdentImpl {
+    type Syntax = SyntaxImpl;
+    fn ident(&self) -> &str {
+        &self.ident
+    }
+}
+
+impl ToSpan for IdentImpl {
+    fn span_for(&self, _file: &codemap::File) -> Span {
+        self.span
+    }
+}
+
+impl FromCst<rnix::ast::Ident> for IdentImpl {
+    fn from_cst(c: &Compiler, ident: rnix::ast::Ident) -> Self {
+        IdentImpl {
+            span: c.span_for(&ident),
+            ident: ident.to_string(),
+        }
+    }
+}
---
+++ tvix/eval/src/compiler/syn/mod.rs
@@ -0,0 +1,163 @@
+//! syn is an *abstractly* abstract syntax tree for Nix.  That isn't a typo.
+//!
+//! This module provides traits which abstract away everything the compiler
+//! might want to know about the parsed Nix syntax tree.  It abstracts over
+//! the implementation of abstract syntax trees.  Yo dawg, I heard you like...
+//!
+//! By abstracting away only the types and methods that the compiler needs, we
+//! can easily swap out the representation.  For example, instead of rnix's
+//! (misleadingly-named) concrete syntax tree `rnix::ast` (which should be
+//! named `rnix::cst`) we can switch to a true abstract syntax tree.  This
+//! would allow to implement parts of the compiler as AST-to-AST
+//! transformations, making them completely independent from the rest of the
+//! compiler, which would then be much simpler.  Since `syn` completely hides
+//! the underlying implementation this change can be made without touching the
+//! rest of the compiler.  In fact, arbitrarily many AST-to-AST passes can be
+//! added this way.  The GHC renamer provides many examples of useful passes.
+//!
+//! The contents of this file, including its public declarations, are
+//! considered an implementation detail of tvix::eval::compiler.  They are not
+//! visible anywhere outside of tvix::eval::compiler and probably never will
+//! be.
+
+use super::*;
+pub mod cst;
+
+/// Implementations of the `Syntax` trait exist so that things outside this
+/// module can have one single polymorphic paramter <Syntax> instead of one
+/// for every different flavor of ast-node (<AttrPath, Entry, PhamNuwen, ...>)
+///
+/// The structs which implement this trait are typically zero-sized types.
+///
+pub trait Syntax {
+    type Ident: Ident<Syntax = Self>;
+    type Inherit: Inherit<Syntax = Self>;
+    type Attr: Attr<Syntax = Self>;
+    type AttrPath: AttrPath<Syntax = Self>;
+    type AttrPathValue: AttrPathValue<Syntax = Self>;
+    type AttrSet: AttrSet<Syntax = Self> + HasEntry<Syntax = Self>;
+    type LetIn: LetIn<Syntax = Self> + HasEntry<Syntax = Self>;
+    type LegacyLet: LegacyLet<Syntax = Self> + HasEntry<Syntax = Self>;
+    type Expr: Expr<Syntax = Self>;
+}
+
+#[derive(Clone)]
+pub enum StaticOrDynamic<Static, Dynamic> {
+    Static(Static),
+    Dynamic(Dynamic),
+}
+
+/// Types which implement this trait represent an attribute name -- a single
+/// component of an attribute path.
+pub trait Attr: Clone + ToSpan + FromCst<rnix::ast::Attr> {
+    type Syntax: self::Syntax;
+    fn attr(&self) -> &StaticOrDynamic<SmolStr, <Self::Syntax as self::Syntax>::Expr>;
+    fn into_attr(self) -> StaticOrDynamic<SmolStr, <Self::Syntax as self::Syntax>::Expr>;
+    fn from_static(str: SmolStr, span: Span) -> Self;
+    fn from_dynamic(expr: <Self::Syntax as self::Syntax>::Expr, span: Span) -> Self;
+    fn as_static(&self) -> Option<SmolStr> {
+        match self.attr() {
+            StaticOrDynamic::Static(smol_str) => Some(smol_str.clone()),
+            StaticOrDynamic::Dynamic(_) => None,
+        }
+    }
+    fn as_dynamic(self) -> Option<<Self::Syntax as self::Syntax>::Expr> {
+        match self.into_attr() {
+            StaticOrDynamic::Static(_) => None,
+            StaticOrDynamic::Dynamic(expr) => Some(expr),
+        }
+    }
+    fn is_static(&self) -> bool {
+        match self.attr() {
+            StaticOrDynamic::Static(_) => true,
+            StaticOrDynamic::Dynamic(_) => false,
+        }
+    }
+    fn is_dynamic(&self) -> bool {
+        !self.is_static()
+    }
+}
+
+/// Types which implement this trait represent attribute paths, like
+/// `stdenv.cc.cc` and `bob."${fred}".carl`.  Both examples have three
+/// components.
+pub trait AttrPath: Clone + FromCst<rnix::ast::Attrpath> {
+    type Syntax: self::Syntax;
+    fn first(&mut self) -> Option<&<Self::Syntax as self::Syntax>::Attr>;
+    fn take_first(&mut self) -> Option<<Self::Syntax as self::Syntax>::Attr>;
+}
+
+/// Types which implement this trait represent an attribute path and an
+/// expression for the value assigned to it.  For example, `meta.license =
+/// lib.licenses.gpl3`.
+pub trait AttrPathValue: Clone + ToSpan + FromCst<rnix::ast::AttrpathValue> {
+    type Syntax: self::Syntax;
+    fn path(&self) -> <Self::Syntax as self::Syntax>::AttrPath;
+    fn value(&self) -> <Self::Syntax as self::Syntax>::Expr;
+    fn new(
+        span: Span,
+        path: <Self::Syntax as self::Syntax>::AttrPath,
+        value: <Self::Syntax as self::Syntax>::Expr,
+    ) -> Self;
+}
+
+/// Types which implement this trait represent `inherit` clauses, both
+/// namespaced and non-namespaced.
+pub trait Inherit: Clone + ToSpan + FromCst<rnix::ast::Inherit> {
+    type Syntax: self::Syntax;
+    fn namespace(&self) -> Option<<Self::Syntax as self::Syntax>::Expr>;
+    fn attrs(&self) -> Vec<<Self::Syntax as self::Syntax>::Attr>;
+    fn new(
+        namespace: Option<<Self::Syntax as self::Syntax>::Expr>,
+        attrs: Vec<<Self::Syntax as self::Syntax>::Attr>,
+        span: Span,
+    ) -> Self;
+}
+
+/// Types which implement this trait represent `let..in` expressions.
+pub trait LetIn: ToSpan + Clone + FromCst<rnix::ast::LetIn> {
+    type Syntax: self::Syntax;
+    fn body(self) -> <<Self as LetIn>::Syntax as syn::Syntax>::Expr;
+}
+
+/// Types which implement this trait represent attribute set literals.
+pub trait AttrSet: ToSpan + Clone + FromCst<rnix::ast::AttrSet> {
+    type Syntax: self::Syntax;
+    fn is_rec(&self) -> bool;
+    fn to_cst(self) -> rnix::ast::AttrSet;
+}
+
+pub trait LegacyLet: ToSpan + Clone + FromCst<rnix::ast::LegacyLet> {
+    type Syntax: self::Syntax;
+}
+
+/// Types which implement this trait represent identifier expressions.
+pub trait Ident: ToSpan + Clone + FromCst<rnix::ast::Ident> {
+    type Syntax: self::Syntax;
+    fn ident(&self) -> &str;
+}
+
+/// Types which implement this trait represent Nix expressions.  This trait
+/// will accrue more methods as the rest of the compiler is migrated to
+/// syn::Syntax.
+pub trait Expr: ToSpan + FromCst<ast::Expr> + Clone {
+    type Syntax: self::Syntax;
+    fn to_cst(self) -> rnix::ast::Expr;
+    fn as_cst(&self) -> &rnix::ast::Expr;
+    fn is_attrset(&self) -> bool;
+    fn as_attrset(self, c: &Compiler) -> Option<<Self::Syntax as self::Syntax>::AttrSet>;
+}
+
+/// Trait for things which have an attribute entry associated with them.  An
+/// attribute entry is either an `Inherit` or an `AttrPathValue`.
+pub trait HasEntry {
+    type Syntax: self::Syntax;
+    fn inherits(&self, c: &Compiler) -> Vec<<Self::Syntax as self::Syntax>::Inherit>;
+    fn attributes<'a>(&self, c: &Compiler) -> Vec<<Self::Syntax as self::Syntax>::AttrPathValue>;
+}
+
+/// Trait for things that can be created from an rnix Concrete Syntax Tree of
+/// type `CstType`.
+pub trait FromCst<CstType> {
+    fn from_cst(c: &Compiler, cst: CstType) -> Self;
+}
