d100d21c542280614417f0e6169b52634ccd6c86
Profpatsch <mail@profpatsch.de>
2024-05-13 16:02:05 +0200

    feat(users/Profpatsch/MyPrelude): add Arg
    Change-Id: I218562c924489ff9cba783dc88ecb8e777c8aac3

--- users/Profpatsch/my-prelude/default.nix
+++ users/Profpatsch/my-prelude/default.nix
@@ -7,6 +7,7 @@ pkgs.haskellPackages.mkDerivation {
   src = depot.users.Profpatsch.exactSource ./. [
     ./my-prelude.cabal
     ./src/Aeson.hs
+    ./src/Arg.hs
     ./src/AtLeast.hs
     ./src/MyPrelude.hs
     ./src/Test.hs
