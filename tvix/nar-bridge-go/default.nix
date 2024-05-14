ee681a2704ae071544c70e051bbf02f036d183bc
Florian Klink <flokli@flokli.de>
2024-05-14 12:35:55 +0200

    chore(tvix/nar-bridge): move to nar-bridge-go
    Make some space for the rust implementation.
    
    Change-Id: I924dc1657be10abe5a11951c3b9de50bae06db19

--- tvix/nar-bridge-go/default.nix
+++ tvix/nar-bridge-go/default.nix
@@ -3,7 +3,7 @@
 { depot, pkgs, lib, ... }:
 
 pkgs.buildGoModule {
-  name = "nar-bridge";
+  name = "nar-bridge-go";
   src = depot.third_party.gitignoreSource ./.;
 
   vendorHash = "sha256-7jugbC5sEGhppjiZgnoLP5A6kQSaHK9vE6cXVZBG22s=";
