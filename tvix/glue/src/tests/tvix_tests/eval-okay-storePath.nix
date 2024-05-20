6a0d67aada31b97fdc19305946655e8ec9fc03c5
Aspen Smith <root@gws.fyi>
2024-05-19 20:43:46 -0400

    feat(tvix/glue): Implement builtins.storePath
    This one's relatively simple - we just check if the store path exists,
    and if it does we make a new contextful string containing the store path
    as its only context element.
    
    Automatic testing seems tricky for this (I think?) so I tested it
    manually:
    
    tvix-repl> builtins.storePath /nix/store/yn46i4xx5alh7gs6fpkxk430i34rp2q9-hello-2.12.1
    => "/nix/store/yn46i4xx5alh7gs6fpkxk430i34rp2q9-hello-2.12.1" :: string
    
    Change-Id: I8a0d9726e4102ab872c53c2419679c2c855a5a18

---
+++ tvix/glue/src/tests/tvix_tests/eval-okay-storePath.nix
@@ -0,0 +1,8 @@
+let
+  path = builtins.unsafeDiscardStringContext "${../empty-file}";
+  storePath = builtins.storePath path;
+in
+{
+  hasContext = builtins.hasContext storePath;
+  context = builtins.getContext storePath;
+}
