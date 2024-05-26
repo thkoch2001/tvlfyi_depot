626661ccc164eca6ddf6b3eb2a98dd43939f2785
Ilan Joselevich <personal@ilanjoselevich.com>
2024-05-26 17:27:10 +0300

    feat(tvix): add CI targets for checking crate features powerset
    Closes: https://b.tvl.fyi/issues/401
    
    Change-Id: I966dde1413d057770787da3296cce9c1924570e0

---
+++ tvix/utils.nix
@@ -0,0 +1,25 @@
+{ lib, depot, ... }:
+
+{
+  mkFeaturePowerset = { crateName, features, override ? { } }:
+    let
+      powerset = xs:
+        let
+          addElement = set: element:
+            set ++ map (e: [ element ] ++ e) set;
+        in
+        lib.foldl' addElement [ [ ] ] xs;
+    in
+    lib.listToAttrs (map
+      (featuresPowerset: {
+        name = if featuresPowerset != [ ] then "with-features-${lib.concatStringsSep "-" featuresPowerset}" else "no-features";
+        value = depot.tvix.crates.workspaceMembers.${crateName}.build.override (old: lib.recursiveUpdate
+          {
+            runTests = true;
+            features = featuresPowerset;
+          }
+          (if lib.isFunction override then override old else override)
+        );
+      })
+      (powerset features));
+}
