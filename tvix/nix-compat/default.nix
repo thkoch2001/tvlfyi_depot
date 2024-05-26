60c0663aa9eb5b03d8781a9d816361ec35b5ebf7
Ilan Joselevich <personal@ilanjoselevich.com>
2024-05-26 17:27:10 +0300

    WIP
    Change-Id: I966dde1413d057770787da3296cce9c1924570e0

--- tvix/nix-compat/default.nix
+++ tvix/nix-compat/default.nix
@@ -1,7 +1,18 @@
-{ depot, ... }:
+{ depot, pkgs, ... }:
 
-depot.tvix.crates.workspaceMembers.nix-compat.build.override {
+(depot.tvix.crates.workspaceMembers.nix-compat.build.override {
   runTests = true;
-  # make sure we also enable async here, so run the tests behind that feature flag.
-  features = [ "default" "async" "wire" ];
+}).overrideAttrs {
+  meta.ci.targets = [ "integration-tests" ];
+  passthru.integration-tests = pkgs.symlinkJoin {
+    name = "nix-compat-integration-tests";
+    postBuild = "rm -rf $out/*"; # We want to clean up $out
+    paths = (map
+      (featuresPowerset: depot.tvix.crates.workspaceMembers.nix-compat.build.override ({
+        runTests = true;
+        features = featuresPowerset;
+      }))
+      (depot.tvix.utils.powerset [ "async" "wire" ])
+    );
+  };
 }
