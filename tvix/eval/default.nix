3ba9e65e927c39e115cf65809aeeaec437101145
Ilan Joselevich <personal@ilanjoselevich.com>
2024-05-26 17:27:10 +0300

    feat(tvix): add CI targets for checking crate features powerset
    Closes: https://b.tvl.fyi/issues/401
    
    Change-Id: I966dde1413d057770787da3296cce9c1924570e0

--- tvix/eval/default.nix
+++ tvix/eval/default.nix
@@ -1,9 +1,16 @@
 # TODO: find a way to build the benchmarks via crate2nix
-{ depot, pkgs, ... }:
+{ depot, pkgs, lib, ... }:
 
-depot.tvix.crates.workspaceMembers.tvix-eval.build.override {
+(depot.tvix.crates.workspaceMembers.tvix-eval.build.override {
   runTests = true;
 
   # Make C++ Nix available, to compare eval results against.
   testInputs = [ pkgs.nix ];
-}
+}).overrideAttrs (old: rec {
+  meta.ci.targets = lib.filter (x: lib.hasPrefix "with-features" x || x == "no-features") (lib.attrNames passthru);
+  passthru = depot.tvix.utils.mkFeaturePowerset {
+    inherit (old) crateName;
+    features = [ "nix_tests" ];
+    override.testInputs = [ pkgs.nix ];
+  };
+})
