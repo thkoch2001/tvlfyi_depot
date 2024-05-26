5295b5e751e2006ef95047e60131a4cf110e5442
Ilan Joselevich <personal@ilanjoselevich.com>
2024-05-26 17:27:10 +0300

    feat(tvix): add CI targets for checking crate features powerset
    Closes: #401
    
    Change-Id: I966dde1413d057770787da3296cce9c1924570e0

--- tvix/build/default.nix
+++ tvix/build/default.nix
@@ -1,5 +1,11 @@
-{ depot, pkgs, ... }:
+{ depot, lib, ... }:
 
-depot.tvix.crates.workspaceMembers.tvix-build.build.override {
+(depot.tvix.crates.workspaceMembers.tvix-build.build.override {
   runTests = true;
-}
+}).overrideAttrs (old: rec {
+  meta.ci.targets = lib.filter (x: lib.hasPrefix "with-features" x || x == "no-features") (lib.attrNames passthru);
+  passthru = depot.tvix.utils.mkFeaturePowerset {
+    inherit (old) crateName;
+    features = [ "tonic-reflection" ];
+  };
+})
