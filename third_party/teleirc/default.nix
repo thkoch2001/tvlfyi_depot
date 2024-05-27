2894caab6d22ed74ce0423a70e041ecccbbdd5c9
Vincent Ambo <tazjin@tvl.su>
2024-05-27 15:21:43 +0300

    chore(3p/teleirc): use TVL fork temporarily
    This contains a fix for an issue where the bridge would forward
    messages from the wrong channel, because the source channel was not
    checked.
    
    tvlbot runs in 3 channels, so this caused issues.
    
    Change-Id: Icc85406b273f375ac90287364df83fb76f028b59

--- third_party/teleirc/default.nix
+++ third_party/teleirc/default.nix
@@ -5,10 +5,10 @@ pkgs.buildGoModule rec {
   version = "2.3.0-4";
 
   src = pkgs.fetchFromGitHub {
-    owner = "RITlug";
+    owner = "tvlfyi";
     repo = "teleirc";
-    rev = "v2.3.0-4-g9ad5f9a";
-    sha256 = "08h9ady2vz6nfcii2ac41b2lfqzjfiwv8apbcx4fybaymwy5vn7g";
+    rev = "51aaf11a0a1c8d86fadf50db44215b06555e3c9b";
+    sha256 = "09lr7zs93w5ipr4l820g40jj0q5j9y6mqwwhwry8pc99zbqchjm0";
   };
 
   vendorHash = "sha256:06f2wyxbphj73wknpp6dsn7rb4yhvdl6x0gj729cns7r4bsviscs";
