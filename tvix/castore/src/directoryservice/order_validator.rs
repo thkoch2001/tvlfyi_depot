d8f30d0349755bebd04328b1c5125d43d4ad2696
Yureka <tvl@yuka.dev>
2024-06-15 18:24:42 +0200

    refactor(castore/directory): separate order logic from ClosureValidator
    ClosureValidator was previously only suitable for a very narrow use case:
    Validating incoming uploads, which are in leaves-to-root order.
    This is because the ordering validation was hard-wired into the add()
    function.
    This
    - Re-name ClosureValidator to DirectoryGraph, which is more suitable
      since it actually stores the Directory structs and is drained in the end.
    - Move the ordering-related logic to a separate OrderValidator, which
      can be used independently.
    - re-write DirectoryGraph to be a general purpose
      validator which can accept the input in both orders
      and can be drained in both orders as well.
    
    This means the DirectoryGraph and OrderValidator can now serve
    multiple new purposes:
    - Validating the incoming closure on the client while downloading.
    - Validating the incoming closure downloaded in a caching layer from the
      `far` cache, and re-order it for insertion into the `near` cache.
    
    Change-Id: Ib6984a080e6f6ee722a3fa00b64354f70f6a078d

---
+++ tvix/castore/src/directoryservice/order_validator.rs
@@ -0,0 +1,80 @@
+use std::collections::HashSet;
+use tracing::warn;
+
+use crate::{proto::Directory, B3Digest};
+
+pub trait OrderValidator {
+    /// Update the order validator's state with the directory
+    /// Returns if the insert was successful
+    fn add_directory(&mut self, directory: &Directory) -> bool;
+}
+
+#[derive(Default)]
+pub struct RootToLeavesValidator {
+    /// Only used to remember the root node, not for validation
+    expected_digests: HashSet<B3Digest>,
+}
+
+impl RootToLeavesValidator {
+    pub(crate) fn new_with_root_digest(root_digest: B3Digest) -> Self {
+        let mut this = Self::default();
+        this.expected_digests.insert(root_digest);
+        this
+    }
+
+    pub(crate) fn digest_allowed(&self, digest: &B3Digest) -> bool {
+        self.expected_digests.is_empty() // we don't know the root node; allow any
+            || self.expected_digests.contains(digest)
+    }
+
+    pub(crate) fn add_directory_unchecked(&mut self, directory: &Directory) {
+        if self.expected_digests.is_empty() {
+            self.expected_digests.insert(directory.digest());
+        }
+
+        for subdir in &directory.directories {
+            // Allow the children to appear next
+            let subdir_digest = subdir.digest.clone().try_into().unwrap();
+            self.expected_digests.insert(subdir_digest);
+        }
+    }
+}
+
+impl OrderValidator for RootToLeavesValidator {
+    fn add_directory(&mut self, directory: &Directory) -> bool {
+        if !self.digest_allowed(&directory.digest()) {
+            return false;
+        }
+        self.add_directory_unchecked(directory);
+        true
+    }
+}
+
+#[derive(Default)]
+pub struct LeavesToRootValidator {
+    /// This is empty in the beginning, and gets filled as leaves and intermediates are
+    /// inserted
+    allowed_references: HashSet<B3Digest>,
+}
+
+impl OrderValidator for LeavesToRootValidator {
+    fn add_directory(&mut self, directory: &Directory) -> bool {
+        let digest = directory.digest();
+
+        for subdir in &directory.directories {
+            let subdir_digest = subdir.digest.clone().try_into().unwrap(); // this has been validated in validate_directory()
+            if !self.allowed_references.contains(&subdir_digest) {
+                warn!(
+                    "unexpected directory reference: {} -> {}",
+                    data_encoding::HEXLOWER.encode(digest.as_slice()),
+                    data_encoding::HEXLOWER.encode(subdir_digest.as_slice())
+                );
+                return false;
+            }
+        }
+
+        self.allowed_references.insert(digest.clone());
+
+        true
+    }
+}
