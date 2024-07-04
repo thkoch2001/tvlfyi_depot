4f6d575d8cd460dede3ac2d660d858e460f9e365
Aspen Smith <root@gws.fyi>
2024-07-04 13:03:42 -0400

    wip: REPL variables
    Change-Id: Id87e26dd1ffb3df7cff294a6cdadf79b7c05a7f1

--- tvix/eval/src/lib.rs
+++ tvix/eval/src/lib.rs
@@ -36,6 +36,7 @@ mod test_utils;
 #[cfg(test)]
 mod tests;
 
+use std::collections::HashMap;
 use std::path::PathBuf;
 use std::rc::Rc;
 use std::str::FromStr;
@@ -68,7 +69,7 @@ pub use crate::io::StdIO;
 ///
 /// Public fields are intended to be set by the caller. Setting all
 /// fields is optional.
-pub struct Evaluation<'co, 'ro, IO> {
+pub struct Evaluation<'co, 'ro, 'ctx, IO> {
     /// Source code map used for error reporting.
     source_map: SourceCode,
 
@@ -82,6 +83,9 @@ pub struct Evaluation<'co, 'ro, IO> {
     /// Set of builtins that are implemented in Nix itself and should
     /// be compiled and inserted in the builtins set.
     pub src_builtins: Vec<(&'static str, &'static str)>,
+
+    /// Top-level variables to define in the evaluation
+    pub context: Option<&'ctx HashMap<String, Value>>,
 
     /// Implementation of file-IO to use during evaluation, e.g. for
     /// impure builtins.
@@ -131,7 +135,7 @@ pub struct EvaluationResult {
     pub expr: Option<rnix::ast::Expr>,
 }
 
-impl<'co, 'ro, IO> Evaluation<'co, 'ro, IO>
+impl<'co, 'ro, 'ctx, IO> Evaluation<'co, 'ro, 'ctx, IO>
 where
     IO: AsRef<dyn EvalIO> + 'static,
 {
@@ -146,6 +150,7 @@ where
             io_handle,
             builtins,
             src_builtins: vec![],
+            context: None,
             strict: false,
             nix_path: None,
             compiler_observer: None,
@@ -154,7 +159,7 @@ where
     }
 }
 
-impl<'co, 'ro> Evaluation<'co, 'ro, Box<dyn EvalIO>> {
+impl<'co, 'ro, 'ctx> Evaluation<'co, 'ro, 'ctx, Box<dyn EvalIO>> {
     /// Initialize an `Evaluation`, without the import statement available, and
     /// all IO operations stubbed out.
     pub fn new_pure() -> Self {
@@ -188,7 +193,7 @@ impl<'co, 'ro> Evaluation<'co, 'ro, Box<dyn EvalIO>> {
     }
 }
 
-impl<'co, 'ro, IO> Evaluation<'co, 'ro, IO>
+impl<'co, 'ro, 'ctx, IO> Evaluation<'co, 'ro, 'ctx, IO>
 where
     IO: AsRef<dyn EvalIO> + 'static,
 {
@@ -229,6 +234,7 @@ where
             source,
             self.builtins,
             self.src_builtins,
+            self.context,
             self.enable_import,
             compiler_observer,
         );
@@ -270,6 +276,7 @@ where
             source.clone(),
             self.builtins,
             self.src_builtins,
+            self.context,
             self.enable_import,
             compiler_observer,
         ) {
@@ -341,6 +348,7 @@ fn parse_compile_internal(
     source: SourceCode,
     builtins: Vec<(&'static str, Value)>,
     src_builtins: Vec<(&'static str, &'static str)>,
+    context: Option<&HashMap<String, Value>>,
     enable_import: bool,
     compiler_observer: &mut dyn CompilerObserver,
 ) -> Option<(Rc<Lambda>, Rc<GlobalsMap>)> {
@@ -368,6 +376,7 @@ fn parse_compile_internal(
         result.expr.as_ref().unwrap(),
         location,
         builtins,
+        context,
         &source,
         &file,
         compiler_observer,
