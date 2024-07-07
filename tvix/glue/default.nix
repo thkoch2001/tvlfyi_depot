e72bb83b4e9353c16790251b7077a296a156e1cb
Florian Klink <flokli@flokli.de>
2024-07-07 17:16:36 +0300

    refactor(tvix): point SSL_CERT_FILE to /dev/null
    reqwest wants to be able to read a file of trust roots when constructed,
    but as it doesn't actually do any HTTPS connections inside the nix
    build, an empty list of trust roots is totally sufficient.
    
    Thankfully /dev/null provides such a file.
    
    Change-Id: I9bd1619b2c9f8ff2a6640d2ac410d4de5b20c2ea

--- tvix/glue/default.nix
+++ tvix/glue/default.nix
@@ -3,7 +3,7 @@
 (depot.tvix.crates.workspaceMembers.tvix-glue.build.override {
   runTests = true;
   testPreRun = ''
-    export SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt;
+    export SSL_CERT_FILE=/dev/null
   '';
 }).overrideAttrs (old: rec {
   meta.ci.targets = lib.filter (x: lib.hasPrefix "with-features" x || x == "no-features") (lib.attrNames passthru);
@@ -11,7 +11,7 @@
     inherit (old) crateName;
     features = [ "nix_tests" ];
     override.testPreRun = ''
-      export SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt;
+      export SSL_CERT_FILE=/dev/null
     '';
   };
 })
