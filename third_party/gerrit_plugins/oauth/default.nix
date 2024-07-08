6d7713572f35e804fbd04b486902bf535fddb6e0
Luke Granger-Brown <git@lukegb.com>
2024-07-08 10:32:08 +0100

    chore(3p/gerrit): downgrade mina SSHd again
    This breaks it with "ssh_dispatch_run_fatal: Connection to
    2a01:4f8:242:5b21:0:feed:edef:beef port 29418: incorrect signature"
    
    Change-Id: I9bafc5fb6d4743ce4f097158ec14fecc791366ca
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/11965
    Tested-by: BuildkiteCI
    Autosubmit: lukegb <lukegb@tvl.fyi>
    Reviewed-by: flokli <flokli@flokli.de>
    Reviewed-by: lukegb <lukegb@tvl.fyi>

--- third_party/gerrit_plugins/oauth/default.nix
+++ third_party/gerrit_plugins/oauth/default.nix
@@ -11,7 +11,7 @@ buildGerritBazelPlugin rec {
     rev = "98231604d60788bb43490f1a301d792817ac8008";
     hash = "sha256-AuVO1Yys8BYqGHZI/adszCUg0JM2v4Td4fe26LdOPLM=";
   };
-  depsHash = "sha256-qEeKQPyt3uIS+BStrputBUnE3ft5c4oeazgsWd+lbN8=";
+  depsHash = "sha256-7SC4NXm4zGeJrYBqtEvcrLmsZmXEX8P21J0kwHBDBZ4=";
   postOverlayPlugin = ''
     cp "${src}/external_plugin_deps.bzl" "$out/plugins/external_plugin_deps.bzl"
   '';
