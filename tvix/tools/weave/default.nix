64fd1d3e56cb0edcbe77227cf18ca8425438a68a
Ilan Joselevich <personal@ilanjoselevich.com>
2024-07-09 17:15:41 +0200

    chore(tvix/tools/weave): Regenerate Cargo.nix and filter src
    The Cargo.lock also got changed because of crate2nix using --freezed.
    
    Change-Id: I79b062eddb13f59c4656a62735df1e527fc91a44
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/11970
    Autosubmit: Ilan Joselevich <personal@ilanjoselevich.com>
    Tested-by: BuildkiteCI
    Reviewed-by: flokli <flokli@flokli.de>

--- tvix/tools/weave/default.nix
+++ tvix/tools/weave/default.nix
@@ -1,3 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, depot, ... }:
 
-(pkgs.callPackage ./Cargo.nix { }).rootCrate.build
+(pkgs.callPackage ./Cargo.nix {
+  defaultCrateOverrides = (depot.tvix.utils.defaultCrateOverridesForPkgs pkgs) // {
+    weave = prev: {
+      src = depot.tvix.utils.filterRustCrateSrc { root = prev.src.origSrc; };
+    };
+  };
+}).rootCrate.build
