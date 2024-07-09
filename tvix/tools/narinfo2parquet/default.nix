6811198ccd30adca1c92e06ecd769b03cbb70f08
Ilan Joselevich <personal@ilanjoselevich.com>
2024-07-09 16:06:58 +0200

    chore(tvix/tools/narinfo2parquet): Regenerate Cargo.nix and filter src
    The Cargo.lock also got changed because of crate2nix using --freezed.
    
    Change-Id: Id5225e73ec5894bc324f363660472a7efdd803cf

--- tvix/tools/narinfo2parquet/default.nix
+++ tvix/tools/narinfo2parquet/default.nix
@@ -1,3 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, depot, ... }:
 
-(pkgs.callPackage ./Cargo.nix { }).rootCrate.build
+(pkgs.callPackage ./Cargo.nix {
+  defaultCrateOverrides = (depot.tvix.utils.defaultCrateOverridesForPkgs pkgs) // {
+    narinfo2parquet = prev: {
+      src = depot.tvix.utils.filterRustCrateSrc { root = prev.src.origSrc; };
+    };
+  };
+}).rootCrate.build
