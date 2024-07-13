d1794e0ecc8381a8bfa0f50281b052ef9b7fd310
Aspen Smith <root@gws.fyi>
2024-07-13 11:47:47 -0400

    wip: Packed encoding of opcodes
    Change-Id: I90820358329219be09ed2b4c64a474dc1a2f3f43

--- tvix/eval/benches/eval.rs
+++ tvix/eval/benches/eval.rs
@@ -38,6 +38,21 @@ fn eval_merge_attrs(c: &mut Criterion) {
             interpret(black_box(&expr));
         })
     });
+
+    c.bench_function("merge large attrs with large attrs", |b| {
+        let large_attrs1 = format!(
+            "{{{}}}",
+            (0..10000).map(|n| format!("a{n} = {n};")).join(" ")
+        );
+        let large_attrs2 = format!(
+            "{{{}}}",
+            (100000..50000).map(|n| format!("a{n} = {n};")).join(" ")
+        );
+        let expr = format!("{large_attrs1} // {large_attrs2}");
+        b.iter(move || {
+            interpret(black_box(&expr));
+        })
+    });
 }
 
 criterion_group!(benches, eval_literals, eval_merge_attrs);
