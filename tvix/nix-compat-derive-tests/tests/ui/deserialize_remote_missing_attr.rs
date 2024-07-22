9165f9c2879ba916f59f69a049602ebf27dcb309
Brian Olsen <brian@maven-group.org>
2024-07-22 16:51:42 +0200

    feat(tvix/nix-compat-derive): Add deriver for NixDeserialize
    This adds a nix-compat-derive derive crate that implements a deriver
    for NixDeserialize implementations. This is to reduce the amount of
    code needed to implement deserialization for all the types used by
    the Nix daemon protocol.
    
    Change-Id: I484724b550e8a1d5e9adad9555d9dc1374ae95c2

---
+++ tvix/nix-compat-derive-tests/tests/ui/deserialize_remote_missing_attr.rs
@@ -0,0 +1,12 @@
+use nix_compat_derive::nix_deserialize_remote;
+
+pub struct Value(String);
+impl From<String> for Value {
+    fn from(s: String) -> Value {
+        Value(s)
+    }
+}
+
+nix_deserialize_remote!(#[nix()] Value);
+
+fn main() {}
