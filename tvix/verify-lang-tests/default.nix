983dec663bb05e2ca0aaee6c157f4113909e2eda
Ilan Joselevich <personal@ilanjoselevich.com>
2024-08-04 01:00:25 +0300

    feat(tvix/eval): Implement builtins.readFileType
    builtins.readFileType was added to Nix back in version 2.14.
    
    The tests were also moved out of notyetpassing in addition to the
    readDir fixtures they depend on.
    
    I caught a bug where we previously used std::fs::metadata (via the
    .metadata() method on File) which follows symlinks so it would always
    return false for is_symlink(). Instead we now use
    std::fs::symlink_metadata directly which does not follow symlinks, so
    tests now pass. This wasn't an issue for builtins.readDir as it uses
    walkdir and walkdir doesn't follow symlinks either.
    
    Change-Id: I58eb97bdb5ec95df4f6882f495f8c572fe7c6793

--- tvix/verify-lang-tests/default.nix
+++ tvix/verify-lang-tests/default.nix
@@ -68,7 +68,7 @@ let
     "eval-okay-cycle-display-cpp-nix-2.13.nix" = [ nix ];
     # builtins.replaceStrings becomes lazier in Nix 2.16
     "eval-okay-replacestrings.nix" = [ nix ];
-    # builtins.readFileType is added in Nix 2.15
+    # builtins.readFileType is added in Nix 2.14
     "eval-okay-readFileType.nix" = [ nix ];
     # builtins.fromTOML gains support for timestamps in Nix 2.16
     "eval-okay-fromTOML-timestamps.nix" = [ nix ];
