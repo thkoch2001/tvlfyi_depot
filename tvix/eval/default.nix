39ef171e001bae6ba14b6aa9b6df2f36139a44ee
Brian Olsen <brian@maven-group.org>
2024-08-16 01:06:25 +0200

    chore(tvix): Retain original passthru in Rust builds
    When using the runTests feature of crate2nix the derivation that runs the
    tests is put into passthru.test but all default.nix files for Rust crates
    in Tvix threw that away.
    
    This commit retains passthru so that you can get access to the test
    derivation.
    
    Change-Id: I8b7b7db57a49069348f08c12c00a3b1a41a0c05b
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/12215
    Reviewed-by: flokli <flokli@flokli.de>
    Tested-by: BuildkiteCI

--- tvix/eval/default.nix
+++ tvix/eval/default.nix
@@ -8,9 +8,9 @@
   testInputs = [ pkgs.nix ];
 }).overrideAttrs (old: rec {
   meta.ci.targets = lib.filter (x: lib.hasPrefix "with-features" x || x == "no-features") (lib.attrNames passthru);
-  passthru = depot.tvix.utils.mkFeaturePowerset {
+  passthru = old.passthru // (depot.tvix.utils.mkFeaturePowerset {
     inherit (old) crateName;
     features = [ "nix_tests" ];
     override.testInputs = [ pkgs.nix ];
-  };
+  });
 })
--- tvix/glue/default.nix
+++ tvix/glue/default.nix
@@ -7,11 +7,11 @@
   '';
 }).overrideAttrs (old: rec {
   meta.ci.targets = lib.filter (x: lib.hasPrefix "with-features" x || x == "no-features") (lib.attrNames passthru);
-  passthru = depot.tvix.utils.mkFeaturePowerset {
+  passthru = old.passthru // (depot.tvix.utils.mkFeaturePowerset {
     inherit (old) crateName;
     features = [ "nix_tests" ];
     override.testPreRun = ''
       export SSL_CERT_FILE=/dev/null
     '';
-  };
+  });
 })
