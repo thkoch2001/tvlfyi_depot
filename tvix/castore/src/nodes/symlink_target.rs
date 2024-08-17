c5051a4cd6411ea0e73e091358c7d32ae8ad778c
Florian Klink <flokli@flokli.de>
2024-08-17 22:00:50 +0300

    refactor(tvix/castore): have SymlinkTarget-specific errors
    Don't use ValidateNodeError, but SymlinkTargetError.
    
    Also, add checks for too long symlink targets.
    
    Change-Id: I4b533325d494232ff9d0b3f4f695f5a1a0a36199

--- tvix/castore/src/nodes/symlink_target.rs
+++ tvix/castore/src/nodes/symlink_target.rs
@@ -1,6 +1,3 @@
-// TODO: split out this error
-use crate::ValidateNodeError;
-
 use bstr::ByteSlice;
 use std::fmt::{self, Debug, Display};
 
@@ -12,6 +9,10 @@ use std::fmt::{self, Debug, Display};
 pub struct SymlinkTarget {
     inner: bytes::Bytes,
 }
+
+/// The maximum length a symlink target can have.
+/// Linux allows 4095 bytes here.
+pub const MAX_TARGET_LEN: usize = 4095;
 
 impl AsRef<[u8]> for SymlinkTarget {
     fn as_ref(&self) -> &[u8] {
@@ -25,46 +26,48 @@ impl From<SymlinkTarget> for bytes::Bytes {
     }
 }
 
+fn validate_symlink_target<B: AsRef<[u8]>>(symlink_target: B) -> Result<B, SymlinkTargetError> {
+    let v = symlink_target.as_ref();
+
+    if v.is_empty() {
+        return Err(SymlinkTargetError::Empty);
+    }
+    if v.len() > MAX_TARGET_LEN {
+        return Err(SymlinkTargetError::TooLong);
+    }
+    if v.contains(&0x00) {
+        return Err(SymlinkTargetError::Null);
+    }
+
+    Ok(symlink_target)
+}
+
 impl TryFrom<bytes::Bytes> for SymlinkTarget {
-    type Error = ValidateNodeError;
+    type Error = SymlinkTargetError;
 
     fn try_from(value: bytes::Bytes) -> Result<Self, Self::Error> {
-        if value.is_empty() || value.contains(&b'\0') {
-            return Err(ValidateNodeError::InvalidSymlinkTarget(value));
-        }
-
-        Ok(Self { inner: value })
+        Ok(Self {
+            inner: validate_symlink_target(value)?,
+        })
     }
 }
 
 impl TryFrom<&'static [u8]> for SymlinkTarget {
-    type Error = ValidateNodeError;
+    type Error = SymlinkTargetError;
 
     fn try_from(value: &'static [u8]) -> Result<Self, Self::Error> {
-        if value.is_empty() || value.contains(&b'\0') {
-            return Err(ValidateNodeError::InvalidSymlinkTarget(
-                bytes::Bytes::from_static(value),
-            ));
-        }
-
         Ok(Self {
-            inner: bytes::Bytes::from_static(value),
+            inner: bytes::Bytes::from_static(validate_symlink_target(value)?),
         })
     }
 }
 
 impl TryFrom<&str> for SymlinkTarget {
-    type Error = ValidateNodeError;
+    type Error = SymlinkTargetError;
 
     fn try_from(value: &str) -> Result<Self, Self::Error> {
-        if value.is_empty() {
-            return Err(ValidateNodeError::InvalidSymlinkTarget(
-                bytes::Bytes::copy_from_slice(value.as_bytes()),
-            ));
-        }
-
         Ok(Self {
-            inner: bytes::Bytes::copy_from_slice(value.as_bytes()),
+            inner: bytes::Bytes::copy_from_slice(validate_symlink_target(value.as_bytes())?),
         })
     }
 }
@@ -79,4 +82,15 @@ impl Display for SymlinkTarget {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         Display::fmt(self.inner.as_bstr(), f)
     }
+}
+
+/// Errors created when constructing / converting to [SymlinkTarget].
+#[derive(Debug, PartialEq, Eq, thiserror::Error)]
+pub enum SymlinkTargetError {
+    #[error("cannot be empty")]
+    Empty,
+    #[error("cannot contain null bytes")]
+    Null,
+    #[error("cannot be over {} bytes long", MAX_TARGET_LEN)]
+    TooLong,
 }
