c11ab905c6f1e9b271feef9ff2c86b05a26e65c7
Ryan Lahfa <tvl@lahfa.xyz>
2024-08-19 20:12:43 +0200

    feat(tvix/nix-compat): entry locator in listing structures
    This implements a simple DFS locator in listing structures.
    
    It is interoperable with the Rust standard library paths, we also build our
    own errors to restrict path values to reasonable secure defaults, e.g.
    relative paths with no `..` component.
    
    Tests are added for this new feature for a positive and a negative
    check.
    
    In addition, a path validation test was added. The Windows-style prefix
    is gated on the Windows platform as UNIX does not parse `C:\\` as a
    `Component::Prefix(_)` but as a `Component::Normal(_)`.
    
    Change-Id: Iae2a80bebd8138e41af94aa7d09f2842c3c5a786
    Signed-off-by: Ryan Lahfa <tvl@lahfa.xyz>

--- tvix/nix-compat/src/nar/listing/mod.rs
+++ tvix/nix-compat/src/nar/listing/mod.rs
@@ -8,12 +8,27 @@
 //! NOTE: LS entries does not offer any integrity field to validate the retrieved file at the provided
 //! offset. Validating the contents is the caller's responsibility.
 
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    path::{Component, Path},
+};
 
 use serde::Deserialize;
 
 #[cfg(test)]
 mod test;
+
+#[derive(Debug, thiserror::Error)]
+pub enum ListingError {
+    // TODO: add an enum of what component was problematic
+    // reusing `std::path::Component` is not possible as it contains a lifetime.
+    /// An unsupported path component can be:
+    /// - either a Windows prefix (`C:\\`, `\\share\\`)
+    /// - either a parent directory (`..`)
+    /// - either a root directory (`/`)
+    #[error("unsupported path component")]
+    UnsupportedPathComponent,
+}
 
 #[derive(Debug, Deserialize)]
 #[serde(tag = "type", rename_all = "lowercase")]
@@ -31,6 +46,40 @@ pub enum ListingEntry {
     Symlink {
         target: String,
     },
+}
+
+impl ListingEntry {
+    pub fn locate<P: AsRef<Path>>(&self, path: P) -> Result<Option<&ListingEntry>, ListingError> {
+        // We perform a simple DFS on the components of the path
+        // while rejecting dangerous components, e.g. `..` or `/`
+        // Files and symlinks are *leaves*, i.e. we return them
+        let mut cur = self;
+        for component in path.as_ref().components() {
+            println!("{:?}", component);
+            match component {
+                Component::CurDir => continue,
+                Component::RootDir => return Err(ListingError::UnsupportedPathComponent),
+                Component::Prefix(_) => return Err(ListingError::UnsupportedPathComponent),
+                Component::ParentDir => return Err(ListingError::UnsupportedPathComponent),
+                Component::Normal(file_or_dir_name) => {
+                    if let Self::Directory { entries } = cur {
+                        if let Some(new_entry) =
+                            entries.get(file_or_dir_name.to_string_lossy().as_ref())
+                        {
+                            cur = new_entry;
+                        } else {
+                            return Ok(None);
+                        }
+                    } else {
+                        return Ok(None);
+                    }
+                }
+            }
+        }
+
+        // By construction, we found the node that corresponds to the path traversal.
+        Ok(Some(cur))
+    }
 }
 
 #[derive(Debug)]
