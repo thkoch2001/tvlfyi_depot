7b1acc5c16967b72f8a3aef7b47967808329744f
Ilan Joselevich <personal@ilanjoselevich.com>
2024-08-19 15:02:45 +0300

    feat(third_party/gitignoreSource): Get gitignore-nix from sources (niv)
    It's easier to implement readTree/depot polyfills for gitignoreSource
    when it's imported from third_party.sources, rather than in a file at
    //third_party.gitignoreSource.
    
    Change-Id: I1323f932bd0feeb2c50ccc76397a80e035842992
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/12248
    Tested-by: BuildkiteCI
    Reviewed-by: tazjin <tazjin@tvl.su>

--- third_party/gitignoreSource/default.nix
+++ third_party/gitignoreSource/default.nix
@@ -1,15 +1,7 @@
-{ pkgs, ... }:
+{ depot, lib, ... }:
 
 let
-  gitignoreNix = import
-    (pkgs.fetchFromGitHub {
-      owner = "hercules-ci";
-      repo = "gitignore";
-      rev = "f9e996052b5af4032fe6150bba4a6fe4f7b9d698";
-      sha256 = "0jrh5ghisaqdd0vldbywags20m2cxpkbbk5jjjmwaw0gr8nhsafv";
-    })
-    { inherit (pkgs) lib; };
-
+  gitignoreNix = import depot.third_party.sources."gitignore.nix" { inherit lib; };
 in
 {
   __functor = _: gitignoreNix.gitignoreSource;
