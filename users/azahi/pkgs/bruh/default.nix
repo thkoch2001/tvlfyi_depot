d1e1ac949c436a3c50d0e8fa1bca83adce881e36
azahi <azat@bahawi.net>
2024-08-27 15:05:53 +0300

    feat(users/azahi/pkgs): add bruh
    Just testing out depot workflow by adding random stuff.
    
    Change-Id: I8acafe73b86d1936b64f8a4dd084c21c04e305bf

---
+++ users/azahi/pkgs/bruh/default.nix
@@ -0,0 +1,43 @@
+{
+  pkgs,
+  lib,
+  ...
+}:
+
+let
+  inherit (pkgs)
+    alsa-utils
+    fetchFromGitHub
+    stdenv
+    ;
+in
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "bruh";
+  version = "2.1";
+
+  src =
+    with finalAttrs;
+    fetchFromGitHub {
+      owner = "kejpies";
+      repo = pname;
+      rev = version;
+      hash = "sha256-Uw6Qes0IZkkfBchFnvnX9l1ZG5T5pyExmV7yUJLPOJ0=";
+    };
+
+  postPatch = ''
+    substituteInPlace bruh.c \
+      --replace-fail "aplay" "${alsa-utils}/bin/aplay"
+  '';
+
+  makeFlags = [ "PREFIX=$(out)" ];
+
+  meta = with lib; {
+    description = "Bruh sound, but as a program";
+    inherit (finalAttrs.src.meta) homepage;
+    license = licenses.gpl3Only;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ azahi ];
+    mainProgram = "bruh";
+  };
+})
