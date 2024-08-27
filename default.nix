4fb2792830dabc786b00524c9e0106017c663758
Emery Hemingway <ehmry@posteo.net>
2024-08-27 18:44:11 +0300

    Build with unreleased Nix library

--- default.nix
+++ default.nix
@@ -5,6 +5,15 @@
 let
   inherit (pkgs) lib;
   buildNimSbom = pkgs.callPackage ./build-nim-sbom.nix { };
+  nix' = pkgs.nixVersions.latest.overrideAttrs (_: {
+    version = "2024-08-23";
+    src = pkgs.fetchFromGitHub {
+      owner = "nixos";
+      repo = "nix";
+      rev = "85f1aa6b3df5c5fcc924a74e2a9cc8acea9ba0e1";
+      hash = "sha256-3+UgAktTtkGUNpxMxr+q+R+z3r026L3PwJzG6RD2IXM=";
+    };
+  });
 in
 buildNimSbom (finalAttrs: {
   outputs = [
@@ -12,7 +21,7 @@ buildNimSbom (finalAttrs: {
     "cfg"
   ];
   nativeBuildInputs = [ pkgs.pkg-config ];
-  buildInputs = [ pkgs.nixVersions.latest ];
+  buildInputs = [ nix' ];
   src = if lib.inNixShell then null else lib.cleanSource ./.;
   postInstall = ''
     mkdir $cfg
