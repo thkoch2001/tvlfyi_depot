6fb3304508d14e51e656c5a10f04d396da63e138
Vincent Ambo <tazjin@tvl.su>
2024-08-28 04:23:57 +0300

    feat(tazjin/eaglemode): add my eaglemode config
    Very barebones for now.
    
    Change-Id: I05b89f5f392eda221d0a61999d47d83f9bf3354a

---
+++ users/tazjin/eaglemode/default.nix
@@ -0,0 +1,9 @@
+# Derivation for my fully configured Eagle Mode.
+{ depot, ... }:
+
+let config = depot.tools.eaglemode.etcDir {
+  extraPaths = [ depot.tools.eaglemode.commands.emacsclient ];
+};
+in depot.tools.eaglemode.withConfig {
+  inherit config;
+}
