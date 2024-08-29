0f519a6f13138f9b7108361bd792d88e7cb39ca4
Emery Hemingway <emery@dmz.rs>
2024-08-29 10:31:54 +0300

    feat(tools/eaglemode/commands): add Sam/Acme plumb command
    Change-Id: I80443e88180e87c7f7b7c8ae322c5076d1ce0a02
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/12422
    Tested-by: BuildkiteCI
    Reviewed-by: tazjin <tazjin@tvl.su>

---
+++ tools/eaglemode/commands/B.nix
@@ -0,0 +1,23 @@
+{ depot, pkgs, ... }:
+
+let
+  em = depot.tools.eaglemode;
+in
+em.mkCommand {
+  name = "9 B";
+  hotkey = "Ctrl+E";
+  icon = "${./plan9.tga}";
+
+  description = ''
+    Plumb target to Sam or Acme
+  '';
+
+  code = ''
+    ErrorIfNotSingleTarget();
+
+    my @tgt=GetTgt();
+    my $dir=$tgt[0];
+
+    ExecOrError('${pkgs.plan9port}/bin/9', 'B', $dir);
+  '';
+}
