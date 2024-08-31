4f0775e7b179f236a380dd98e914e1ff29132f6b
Vincent Ambo <tazjin@tvl.su>
2024-09-01 01:03:16 +0300

    chore(3p/sources): bump channels & overlays (2024-09-01)
    Included changes:
    
    * users/aspen: explicitly use open-source nvidia driver
    
      This now has to be specified explicitly, otherwise evaluation fails with an
      error.
    * users/aspen: nixfmt -> nixfmt-classic
    * users/aspen: fixes for renamed packages & options
    * users/tazjin: fixes for renamed packages & options
    * 3p/overlays: remove cbtemulator patch (merged upstream)
    * tvix/shell: remove unnecessary patches (merged upstream)
    * 3p/rust-crates: mark libgit2_sys as broken
    * users/Profpatsch: mark git-db as broken
    * 3p/overlays: pick `mypaint` from stable channel
    * tvix: fix comments that clippy doesn't like anymore
    * tvix/glue: disable a misfiring clippy lint (applying its suggestion breaks
      code below)
    
    Change-Id: I6d3fc027694bbe7425a2d25dc53d65467a44f3b0

--- users/Profpatsch/git-db/default.nix
+++ users/Profpatsch/git-db/default.nix
@@ -1,10 +1,12 @@
 { depot, pkgs, lib, ... }:
 
-depot.nix.writers.rustSimple
-{
-  name = "git-db";
-  dependencies = [
-    depot.third_party.rust-crates.git2
-  ];
+(depot.nix.writers.rustSimple
+  {
+    name = "git-db";
+    dependencies = [
+      depot.third_party.rust-crates.git2
+    ];
+  }
+  (builtins.readFile ./git-db.rs)) // {
+  meta.broken = true;
 }
-  (builtins.readFile ./git-db.rs)
