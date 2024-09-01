23f97d0df00dc22b255caf12760e1d0aaa147f3f
Florian Klink <flokli@flokli.de>
2024-09-01 15:11:50 +0300

    fix(ops/keycloak): fix terraform state config
    The same fix from cl/11021 also needs to be applied to other states.
    
    Change-Id: I0df3ee2e8970e0d08a119ecc6347f24aef0448c2
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/12409
    Reviewed-by: tazjin <tazjin@tvl.su>
    Autosubmit: flokli <flokli@flokli.de>
    Tested-by: BuildkiteCI

--- ops/keycloak/main.tf
+++ ops/keycloak/main.tf
@@ -10,14 +10,18 @@ terraform {
   }
 
   backend "s3" {
-    endpoint = "https://objects.dc-sto1.glesys.net"
-    bucket   = "tvl-state"
-    key      = "terraform/tvl-keycloak"
-    region   = "glesys"
+    endpoints = {
+      s3 = "https://objects.dc-sto1.glesys.net"
+    }
+    bucket = "tvl-state"
+    key    = "terraform/tvl-keycloak"
+    region = "glesys"
 
     skip_credentials_validation = true
     skip_region_validation      = true
     skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
   }
 }
 
