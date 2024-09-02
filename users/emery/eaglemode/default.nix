e4714db2d5f2b38b60f1ec8a200d0eb3bea0d79f
Emery Hemingway <emery@dmz.rs>
2024-09-03 02:28:31 +0300

    feat(tools/eaglemode/plugins): QOI image plugin
    https://qoiformat.org/
    
    Change-Id: I0c11095c1ac0e65075d032f7c29649cbba9f213f
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/12427
    Reviewed-by: tazjin <tazjin@tvl.su>
    Tested-by: BuildkiteCI

--- users/emery/eaglemode/default.nix
+++ users/emery/eaglemode/default.nix
@@ -3,9 +3,10 @@
 
 let
   config = depot.tools.eaglemode.etcDir {
-    extraPaths = [ depot.tools.eaglemode.commands.B ];
+    extraPaths = [
+      depot.tools.eaglemode.commands.B
+      depot.tools.eaglemode.plugins.qoi
+    ];
   };
 in
-depot.tools.eaglemode.withConfig {
-  inherit config;
-}
+depot.tools.eaglemode.withConfig { inherit config; }
