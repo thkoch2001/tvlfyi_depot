742685bf7806a853873be2b0fc7a9e042ffd8241
Vincent Ambo <tazjin@tvl.su>
2024-09-10 15:45:21 +0300

    chore(3p/sources): bump channels & overlays (2024-09-10)
    Includes the following fixes:
    
    * users/wpcarro: disable pulseaudio option (can't have pipewire _and_ PA)
    * users/aspen: disable pipewire (there's PA config here, so whatever)
    * bump wasm-bindgen in Rust frontend projects
    * users/tazjin: disable builds for frog (it's in storage)
    
    Change-Id: Ia508b14b84619d06c1d98f7245e84d66bc791592

--- users/tazjin/nixos/default.nix
+++ users/tazjin/nixos/default.nix
@@ -4,10 +4,12 @@ let systemFor = sys: (depot.ops.nixos.nixosFor sys).system;
 in depot.nix.readTree.drvTargets {
   arbatSystem = systemFor depot.users.tazjin.nixos.arbat;
   camdenSystem = systemFor depot.users.tazjin.nixos.camden;
-  frogSystem = systemFor depot.users.tazjin.nixos.frog;
   tverskoySystem = systemFor depot.users.tazjin.nixos.tverskoy;
   zamalekSystem = systemFor depot.users.tazjin.nixos.zamalek;
   koptevoRaw = depot.ops.nixos.nixosFor depot.users.tazjin.nixos.koptevo;
   koptevoSystem = systemFor depot.users.tazjin.nixos.koptevo;
   khamovnikSystem = systemFor depot.users.tazjin.nixos.khamovnik;
+
+  # no need to build this while the machine is in storage
+  # frogSystem = systemFor depot.users.tazjin.nixos.frog;
 }
