d41e6706e1745c47850ce17397dc0c556c2338c3
Vincent Ambo <tazjin@tvl.su>
2024-09-11 23:23:23 +0300

    refactor(eaglemode/avif): use build input parameter from buildPlugin
    This somewhat condenses the derivation, which is always nice.
    
    Change-Id: I4e2dd81620b54ab000898337007ffcea509a054b

--- tools/eaglemode/plugins/avif/default.nix
+++ tools/eaglemode/plugins/avif/default.nix
@@ -1,16 +1,10 @@
 { depot, pkgs, ... }:
 
-let
-  em = depot.tools.eaglemode;
-  emSrc = pkgs.srcOnly pkgs.em;
-in
-(em.buildPlugin {
+depot.tools.eaglemode.buildPlugin {
   name = "avif";
   version = "canon";
   src = ./.;
   target = "PlAvif";
-}).overrideAttrs
-  ({ buildInputs ? [ ], nativeBuildInputs ? [ ], ... }: {
-    buildInputs = buildInputs ++ [ pkgs.libavif ];
-    nativeBuildInputs = nativeBuildInputs ++ [ pkgs.pkg-config ];
-  })
+  extraBuildInputs = [ pkgs.libavif ];
+  extraNativeBuildInputs = [ pkgs.pkg-config ];
+}
