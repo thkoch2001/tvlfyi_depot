278ffa2b05d92e4eb3e16ff10fe8307cb8f91144
Aspen Smith <root@gws.fyi>
2024-09-29 10:46:42 -0400

    feat(aspen/system): Setup a systemd timer to sync the depot public inbox
    Change-Id: Iaeae18af25b00b04bd805dabb320c218cb21fd8f
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/12545
    Tested-by: BuildkiteCI
    Autosubmit: aspen <root@gws.fyi>
    Reviewed-by: aspen <root@gws.fyi>

---
+++ users/aspen/system/home/modules/depot-inbox.nix
@@ -0,0 +1,26 @@
+{ config, lib, pkgs, ... }:
+
+let
+  depot = config.lib.depot;
+in
+
+{
+  systemd.user = {
+    services.sync-depot-public-inbox = {
+      Service.ExecStart = pkgs.writeShellScript "sync-depot-public-inbox" ''
+        ${depot.tools.fetch-depot-inbox}/bin/fetch-depot-inbox \
+          /home/aspen/mail/tvl/
+        ${pkgs.notmuch}/bin/notmuch new
+      '';
+    };
+
+    timers.sync-depot-public-inbox = {
+      Unit.Description = "Sync the depot public inbox";
+      Timer = {
+        OnCalendar = "*:*";
+        Unit = "sync-depot-public-inbox.service";
+      };
+      Install.WantedBy = [ "timers.target" ];
+    };
+  };
+}
