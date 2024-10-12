398a9b531758297417280ccf06b82c85f3d236dd
Vincent Ambo <mail@tazj.in>
2024-10-13 01:33:28 +0300

    chore(tvix/tools): move crunch-v2 to //users/edef
    This is not a core Tvix tool, it's some sort of one-off analysis thing.
    
    Change-Id: I05fcbed45abad27d6b5cfd49db1727249dad3971
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/12603
    Autosubmit: tazjin <tazjin@tvl.su>
    Tested-by: BuildkiteCI
    Reviewed-by: edef <edef@edef.eu>

--- users/edef/crunch-v2/Cargo.nix
+++ users/edef/crunch-v2/Cargo.nix
@@ -4087,7 +4087,7 @@ rec {
         version = "0.1.0";
         edition = "2021";
         crateBin = [ ];
-        src = lib.cleanSourceWith { filter = sourceFilter; src = ../../nix-compat; };
+        src = lib.cleanSourceWith { filter = sourceFilter; src = ../../../tvix/nix-compat; };
         libName = "nix_compat";
         dependencies = [
           {
@@ -4199,7 +4199,7 @@ rec {
         crateName = "nix-compat-derive";
         version = "0.1.0";
         edition = "2021";
-        src = lib.cleanSourceWith { filter = sourceFilter; src = ../../nix-compat-derive; };
+        src = lib.cleanSourceWith { filter = sourceFilter; src = ../../../tvix/nix-compat-derive; };
         procMacro = true;
         libName = "nix_compat_derive";
         dependencies = [
