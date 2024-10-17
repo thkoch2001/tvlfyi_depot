bf2cf23ed82985f6dac0f4f07daa4d43fcedf032
edef <edef@edef.eu>
2024-10-17 14:48:25 +0000

    feat(users/edef/weave): use FxHashSet and dedupe early
    Deduping early saves a fair bit of memory, but the extra hashing is
    costly.
    
    We switch to FxHash, since we don't need a DoS-proof hash, but we do
    need it to be *fast*.
    
    Change-Id: Ic6b7010874c417862baa9b882593208c8dd1d5e6

--- users/edef/weave/src/main.rs
+++ users/edef/weave/src/main.rs
@@ -9,8 +9,9 @@ use anyhow::Result;
 use hashbrown::{hash_table, HashTable};
 use nix_compat::nixbase32;
 use rayon::prelude::*;
+use rustc_hash::FxHashSet;
 use std::{
-    collections::{BTreeMap, HashSet},
+    collections::BTreeMap,
     fs::{self, File},
     ops::Index,
     sync::atomic::{AtomicU32, Ordering},
@@ -55,7 +56,8 @@ fn main() -> Result<()> {
         eprintln!("{DONE}");
     }
 
-    let mut todo = HashSet::with_capacity(roots.len());
+    let mut todo = FxHashSet::default();
+    todo.reserve(roots.len());
     {
         let mut unknown_roots = 0usize;
         for (_, idx) in roots.table {
@@ -100,14 +102,14 @@ fn main() -> Result<()> {
             .par_iter()
             .flat_map(|&parent| {
                 if parent == INDEX_NULL {
-                    return vec![];
+                    return FxHashSet::default();
                 }
 
                 ri_array[parent as usize]
                     .iter()
                     .cloned()
                     .filter(|child| !seen.contains(child))
-                    .collect::<Vec<u32>>()
+                    .collect::<FxHashSet<u32>>()
             })
             .collect();
 
