dbbe5dd28af4924e9ec44924bb3114a780f5d552
edef <edef@edef.eu>
2024-10-17 14:48:25 +0000

    feat(users/edef/weave): use FxHashSet and dedupe early
    Deduping early saves a fair bit of memory, but the extra hashing is
    costly.
    
    We switch to FxHash, since we don't need a DoS-proof hash, but we do
    need it to be *fast*.
    
    Change-Id: Ic6b7010874c417862baa9b882593208c8dd1d5e6

--- users/edef/weave/Cargo.nix
+++ users/edef/weave/Cargo.nix
@@ -5649,6 +5649,21 @@ rec {
           "rustc-dep-of-std" = [ "core" "compiler_builtins" ];
         };
       };
+      "rustc-hash" = rec {
+        crateName = "rustc-hash";
+        version = "2.0.0";
+        edition = "2021";
+        sha256 = "0lni0lf846bzrf3jvci6jaf4142n1mdqxvcpczk5ch9pfgyk8c2q";
+        libName = "rustc_hash";
+        authors = [
+          "The Rust Project Developers"
+        ];
+        features = {
+          "default" = [ "std" ];
+          "rand" = [ "dep:rand" "std" ];
+        };
+        resolvedDefaultFeatures = [ "default" "std" ];
+      };
       "rustc_version" = rec {
         crateName = "rustc_version";
         version = "0.4.1";
@@ -6993,6 +7008,10 @@ rec {
           {
             name = "rayon";
             packageId = "rayon";
+          }
+          {
+            name = "rustc-hash";
+            packageId = "rustc-hash";
           }
           {
             name = "tokio";
