dfe0b57a6ffe7fae4603881e8a1d7ca0dc4d08dc
Vova Kryachko <v.kryachko@gmail.com>
2024-10-30 10:22:07 -0400

    feat(nix-compat-derive): Add nix serialization support.
    As part of its operation, the nix-daemon needs to both read and write
    structured data as it communicates with its clients.
    
    nix-compat-derive already has support for deserializing values off the
    wire. This change adds serialization support and automatic deriving
    based on `NixSerialize` trait.
    
    This cl does not allow automatic deriving of `NixSerialize` for enums
    mainly due to the fact that conditional serialization like the one
    `NixDeserialize` supports is not possible. That's because by the time
    the version is known, the value to serialize has already been created,
    so it's up to the application logic to determine which enum to
    construct.
    
    When(and most importantly *if*) enum support becomes needed, it can be
    added at that point.
    
    Change-Id: If171fb182b936da88287133deb8bbf01259e03f7

---
+++ tvix/nix-compat/src/nix_daemon/en/int.rs
@@ -0,0 +1,63 @@
+use super::{NixSerialize, NixWrite};
+
+impl NixSerialize for u64 {
+    async fn serialize<W>(self, writer: &mut W) -> Result<(), W::Error>
+    where
+        W: ?Sized + NixWrite + Send,
+    {
+        writer.write_number(self).await
+    }
+}
+
+impl NixSerialize for bool {
+    async fn serialize<W>(self, writer: &mut W) -> Result<(), W::Error>
+    where
+        W: ?Sized + NixWrite + Send,
+    {
+        writer.write_number(if self { 1 } else { 0 }).await
+    }
+}
+
+impl NixSerialize for i64 {
+    async fn serialize<W>(self, writer: &mut W) -> Result<(), W::Error>
+    where
+        W: ?Sized + NixWrite + Send,
+    {
+        writer.write_number(self as u64).await
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use hex_literal::hex;
+    use rstest::rstest;
+    use tokio_test::io::Builder;
+
+    use crate::{
+        nix_daemon::en::{writer::NixWriter, NixWrite},
+        ProtocolVersion,
+    };
+
+    #[rstest]
+    #[case::simple_false(false, &hex!("0000 0000 0000 0000"))]
+    #[case::simple_true(true, &hex!("0100 0000 0000 0000"))]
+    #[tokio::test]
+    async fn test_write_bool(#[case] expected: bool, #[case] data: &[u8]) {
+        let mock = Builder::new().write(data).build();
+
+        let mut writer = NixWriter::new(mock, ProtocolVersion::from_parts(1, 37));
+        writer.write(expected).await.unwrap();
+    }
+
+    #[rstest]
+    #[case::zero(0, &hex!("0000 0000 0000 0000"))]
+    #[case::one(1, &hex!("0100 0000 0000 0000"))]
+    #[case::other(0x563412, &hex!("1234 5600 0000 0000"))]
+    #[case::max_value(u64::MAX, &hex!("FFFF FFFF FFFF FFFF"))]
+    #[tokio::test]
+    async fn test_write_u64(#[case] expected: u64, #[case] data: &[u8]) {
+        let mock = Builder::new().write(data).build();
+        let mut writer = NixWriter::new(mock, ProtocolVersion::from_parts(1, 37));
+        writer.write(expected).await.unwrap();
+    }
+}
