47cb5006b57567e54e3bb0b818091961a9dc531a
Bob van der Linden <bobvanderlinden@gmail.com>
2024-11-03 21:54:53 +0100

    test(tvix/eval): test throw in __toString with toJSON
    Change-Id: Ia4a9a04c7e157b6add94dc8901ffab35486fe344

--- tvix/eval/src/tests/tvix_tests/eval-okay-builtins-to-json-propagate-catchable.nix
+++ tvix/eval/src/tests/tvix_tests/eval-okay-builtins-to-json-propagate-catchable.nix
@@ -10,5 +10,8 @@ map (e: (builtins.tryEval (builtins.toJSON e)).success) [
     x = 32;
     y = builtins.throw "second argument";
   }
+  {
+    __toString = _: builtins.throw "__toString a";
+  }
   # FIXME(raitobezarius): we would like to test coercions, i.e. `toFile` and `derivation` containing throwables.
 ]
