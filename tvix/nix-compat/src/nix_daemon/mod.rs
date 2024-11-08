004108b16db2ac3b5778c827402a97402559b7d2
Vova Kryachko <v.kryachko@gmail.com>
2024-11-08 10:44:27 -0500

    feat(nix-daemon): Implement client handler.
    This change includes only the basic nix handshake protocol handling and
    sets up a client session. The only supported operation at this point is
    SetOptions.
    
    Additional operations will be implemented in subsequent cls.
    
    Change-Id: I3eccd9e0ceb270c3865929543c702f1491768852

--- tvix/nix-compat/src/nix_daemon/mod.rs
+++ tvix/nix-compat/src/nix_daemon/mod.rs
@@ -1,1 +1,8 @@
+pub mod handler;
+pub mod types;
 pub mod worker_protocol;
+
+/// Represents all possible operations over the nix-daemon protocol.
+pub trait NixDaemonIO {
+    // TODO add methods to it.
+}
